c4b024c2c17dfc7a484260bc30fca19eea0f585d
florianv <voutzinos.florian@gmail.com>
2012-09-21 20:38:24 +0200

    cleaning controllers backend + remove unused variables (thanks phpstorm)

--- administrator/components/com_redshop/models/country_detail.php
+++ administrator/components/com_redshop/models/country_detail.php
@@ -1,112 +1,110 @@
 <?php
-defined( '_JEXEC' ) or die( 'Restricted access' );
+defined('_JEXEC') or die('Restricted access');
 
 jimport('joomla.application.component.model');
 
 
 class country_detailModelcountry_detail extends JModel
 {
-	var $_id = null;
-	var $_data = null;
-	var $_table_prefix = null;
+    var $_id = null;
+    var $_data = null;
+    var $_table_prefix = null;
 
-	function __construct()
-	{
-		parent::__construct();
+    function __construct ()
+    {
+        parent::__construct();
 
-		$this->_table_prefix = '#__'.TABLE_PREFIX.'_';		
-	  
-		$array = JRequest::getVar('cid',  0, '', 'array');
-		$this->setId((int)$array[0]);
-		
-	}
-	function setId($id)
-	{
-		$this->_id		= $id;
-		$this->_data	= null;
-	}
+        $this->_table_prefix = '#__' . TABLE_PREFIX . '_';
 
-	function &getData()
-	{
-		if ($this->_loadData())
-		{
-			
-		}else  $this->_initData();
+        $array = JRequest::getVar('cid', 0, '', 'array');
+        $this->setId((int)$array[0]);
+    }
 
-	   	return $this->_data;
-	}
-	
-	function _loadData()
-	{
-		if (empty($this->_data))
-		{
-			$query = 'SELECT * FROM '.$this->_table_prefix.'country WHERE country_id = '. $this->_id;
-			$this->_db->setQuery($query);
-			$this->_data = $this->_db->loadObject();
-			return (boolean) $this->_data;
-		}
-		return true;
-	}
+    function setId ($id)
+    {
+        $this->_id   = $id;
+        $this->_data = null;
+    }
 
-	
-	function _initData()
-	{
-		if (empty($this->_data))
-		{
-			$detail = new stdClass();
-			
-			$detail->country_id			= 0;
-			$detail->country_name			= null;
-			$detail->country_3_code	 =  null;
-			$detail->country_2_code		= null;
-			$detail->country_jtext	= null;
-			$this->_data		 		= $detail;
-			
-			return (boolean) $this->_data;
-		}
-		
-		return true;
-	}
-  	function store($data)
-	{		
-		
-		$row =& $this->getTable();
-		
+    function &getData ()
+    {
+        if ($this->_loadData()) {
+        } else  {
+            $this->_initData();
+        }
 
-		if (!$row->bind($data)) {
-			$this->setError($this->_db->getErrorMsg());
-			return false;
-		}
-		
-		if (!$row->check()) {
-			$this->setError($this->_db->getErrorMsg());
-			return false;
-		}
-		
-		if (!$row->store()) {			
-			$this->setError($this->_db->getErrorMsg());
-			return false;
-		}
-		
-		return $row;
-	}
+        return $this->_data;
+    }
 
-	function delete($cid = array())
-	{
-		if (count( $cid ))
-		{
-			$cids = implode( ',', $cid );
-			 
-			$query = 'DELETE FROM '.$this->_table_prefix.'country WHERE country_id IN ( '.$cids.' )';
-			$this->_db->setQuery( $query );
-			if(!$this->_db->query()) {
-				$this->setError($this->_db->getErrorMsg());
-				return false;
-			}
-		}
+    function _loadData ()
+    {
+        if (empty($this->_data)) {
+            $query = 'SELECT * FROM ' . $this->_table_prefix . 'country WHERE country_id = ' . $this->_id;
+            $this->_db->setQuery($query);
+            $this->_data = $this->_db->loadObject();
+            return (boolean)$this->_data;
+        }
+        return true;
+    }
 
-		return true;
-	}
 
+    function _initData ()
+    {
+        if (empty($this->_data)) {
+            $detail = new stdClass();
+
+            $detail->country_id     = 0;
+            $detail->country_name   = null;
+            $detail->country_3_code = null;
+            $detail->country_2_code = null;
+            $detail->country_jtext  = null;
+            $this->_data            = $detail;
+
+            return (boolean)$this->_data;
+        }
+
+        return true;
+    }
+
+    function store ($data)
+    {
+
+        $row = $this->getTable();
+
+
+        if (!$row->bind($data)) {
+            $this->setError($this->_db->getErrorMsg());
+            return false;
+        }
+
+        if (!$row->check()) {
+            $this->setError($this->_db->getErrorMsg());
+            return false;
+        }
+
+        if (!$row->store()) {
+            $this->setError($this->_db->getErrorMsg());
+            return false;
+        }
+
+        return $row;
+    }
+
+    function delete ($cid = array())
+    {
+        if (count($cid)) {
+            $cids = implode(',', $cid);
+
+            $query = 'DELETE FROM ' . $this->_table_prefix . 'country WHERE country_id IN ( ' . $cids . ' )';
+            $this->_db->setQuery($query);
+            if (!$this->_db->query()) {
+                $this->setError($this->_db->getErrorMsg());
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
+
 ?>
