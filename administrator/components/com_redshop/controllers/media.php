a7e6d0f550812a9c755c5092d67da7ef9db40678
florianv <voutzinos.florian@gmail.com>
2012-09-25 16:35:18 +0200

    core controller + JInput in a few controllers

--- administrator/components/com_redshop/controllers/media.php
+++ administrator/components/com_redshop/controllers/media.php
@@ -9,7 +9,9 @@
 
 defined('_JEXEC') or die('Restricted access');
 
-class mediaController extends JControllerLegacy
+require_once JPATH_COMPONENT_ADMINISTRATOR . DS . 'core' . DS . 'controller.php';
+
+class mediaController extends RedshopCoreController
 {
     public function cancel()
     {
