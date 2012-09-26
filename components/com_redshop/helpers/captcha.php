264579cac2990b49d85e10e1112151f520ce9001
florianv <voutzinos.florian@gmail.com>
2012-09-26 11:57:16 +0200

    add JEXEC to one helper

--- components/com_redshop/helpers/captcha.php
+++ components/com_redshop/helpers/captcha.php
@@ -7,6 +7,8 @@
  * @license     GNU General Public License version 2 or later, see LICENSE.
  */
 
+defined('_JEXEC') or die ('restricted access');
+
 class CaptchaSecurityImages
 {
     public $font = 'components/com_redshop/assets/captcha/monofont.ttf';
