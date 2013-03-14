223429f490af85b3240358dd01514d7ed0bd8a14
Javier <javier.gomez.corio@gmail.com>
2013-03-14 18:26:44 +0100

    Review Code Style in Plugins

--- component/plugins/default_shipping/default_shipping.php
+++ component/plugins/default_shipping/default_shipping.php
@@ -1,28 +1,23 @@
 <?php
 /**
-* @version		$Id: log.php 14401 2010-01-26 14:10:00Z louis $
-* @package		Joomla
-* @copyright	Copyright (C) 2005 - 2010 Open Source Matters. All rights reserved.
-* @license		GNU/GPL, see LICENSE.php
-* Joomla! is free software. This version may have been modified pursuant
-* to the GNU General Public License, and as distributed it includes or
-* is derivative of works licensed under the GNU General Public License or
-* other free or open source software licenses.
-* See COPYRIGHT.php for copyright notices and details.
-*/
+ * @package     RedSHOP.Plugin
+ * @subpackage  Default_shipping
+ *
+ * @copyright   Copyright (C) 2005 - 2013 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
 
-// no direct access
-defined( '_JEXEC' ) or die( 'Restricted access' );
+defined('_JEXEC') or die('Restricted access');
 
-jimport( 'joomla.plugin.plugin' );
+jimport('joomla.plugin.plugin');
 
 /**
  * Joomla! System Logging Plugin
  *
- * @package		Joomla
- * @subpackage	System
+ * @package        Joomla
+ * @subpackage     System
  */
-include_once (JPATH_ADMINISTRATOR.DS.'components'.DS.'com_redshop'.DS.'helpers'.DS.'shipping.php');
+include_once (JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_redshop' . DS . 'helpers' . DS . 'shipping.php');
 class  plgredshop_shippingdefault_shipping extends JPlugin
 {
 	var $payment_code = "default_shipping";
@@ -35,54 +30,60 @@ class  plgredshop_shippingdefault_shipping extends JPlugin
 	 * because func_get_args ( void ) returns a copy of all passed arguments NOT references.
 	 * This causes problems with cross-referencing necessary for the observer design pattern.
 	 *
-	 * @access	protected
-	 * @param	object	$subject The object to observe
-	 * @param 	array   $config  An array that holds the plugin configuration
-	 * @since	1.5
+	 * @access    protected
+	 *
+	 * @param    object $subject The object to observe
+	 * @param    array  $config  An array that holds the plugin configuration
+	 *
+	 * @since    1.5
 	 */
-	function onShowconfig(){
+	function onShowconfig()
+	{
 		return true;
 	}
 
 	function onWriteconfig($values)
 	{
-		  return true;
+		return true;
 	}
-	
+
 	function onListRates(&$d)
 	{
 		$shippinghelper = new shipping();
-		$shippingrate = array();
-		$rate = 0;
-		$shipping = $shippinghelper->getShippingMethodByClass($this->classname);
-		$shippingArr = $shippinghelper->getShopperGroupDefaultShipping();
-		if(!empty($shippingArr) )
+		$shippingrate   = array();
+		$rate           = 0;
+		$shipping       = $shippinghelper->getShippingMethodByClass($this->classname);
+		$shippingArr    = $shippinghelper->getShopperGroupDefaultShipping();
+		if (!empty($shippingArr))
 		{
-			$shopper_shipping			= $shippingArr['shipping_rate'];
-			$shippingVatRate			= $shippingArr['shipping_vat'];
-			$default_shipping 			= JText::_('COM_REDSHOP_DEFAULT_SHOPPER_GROUP_SHIPPING');
-			$shopper_shipping_id 		= $shippinghelper->encryptShipping( __CLASS__."|".$shipping->name."|".$default_shipping."|".number_format( $shopper_shipping, 2, '.', '' )."|".$default_shipping."|single|".$shippingVatRate);
-			$shippingrate[$rate]->text 	= $default_shipping;
+			$shopper_shipping           = $shippingArr['shipping_rate'];
+			$shippingVatRate            = $shippingArr['shipping_vat'];
+			$default_shipping           = JText::_('COM_REDSHOP_DEFAULT_SHOPPER_GROUP_SHIPPING');
+			$shopper_shipping_id        = $shippinghelper->encryptShipping(__CLASS__ . "|" . $shipping->name . "|" . $default_shipping . "|" . number_format($shopper_shipping, 2, '.', '') . "|" . $default_shipping . "|single|" . $shippingVatRate);
+			$shippingrate[$rate]->text  = $default_shipping;
 			$shippingrate[$rate]->value = $shopper_shipping_id;
-			$shippingrate[$rate]->rate 	= $shopper_shipping;
+			$shippingrate[$rate]->rate  = $shopper_shipping;
 			$rate++;
 		}
 
-		$ratelist = $shippinghelper->listshippingrates($shipping->element,$d['users_info_id'],$d);
-		for($i=0; $i<count($ratelist); $i++)
+		$ratelist = $shippinghelper->listshippingrates($shipping->element, $d['users_info_id'], $d);
+		for ($i = 0; $i < count($ratelist); $i++)
 		{
-			$rs = $ratelist[$i];
-			$shippingRate 			 	= $rs->shipping_rate_value;
-			$rs->shipping_rate_value 	= $shippinghelper->applyVatOnShippingRate($rs,$d['user_id']);
-			$shippingVatRate		 	= $rs->shipping_rate_value - $shippingRate;
-			$economic_displaynumber		= $rs->economic_displaynumber;
-			$shipping_rate_id 			= $shippinghelper->encryptShipping( __CLASS__."|".$shipping->name."|".$rs->shipping_rate_name."|".number_format( $rs->shipping_rate_value, 2, '.', '' )."|".$rs->shipping_rate_id."|single|".$shippingVatRate.'|'.$economic_displaynumber) ;
-			$shippingrate[$rate]->text 	= $rs->shipping_rate_name;
+			$rs                         = $ratelist[$i];
+			$shippingRate               = $rs->shipping_rate_value;
+			$rs->shipping_rate_value    = $shippinghelper->applyVatOnShippingRate($rs, $d['user_id']);
+			$shippingVatRate            = $rs->shipping_rate_value - $shippingRate;
+			$economic_displaynumber     = $rs->economic_displaynumber;
+			$shipping_rate_id           = $shippinghelper->encryptShipping(__CLASS__ . "|" . $shipping->name . "|" . $rs->shipping_rate_name . "|" . number_format($rs->shipping_rate_value, 2, '.', '') . "|" . $rs->shipping_rate_id . "|single|" . $shippingVatRate . '|' . $economic_displaynumber);
+			$shippingrate[$rate]->text  = $rs->shipping_rate_name;
 			$shippingrate[$rate]->value = $shipping_rate_id;
-			$shippingrate[$rate]->rate 	= $rs->shipping_rate_value;
-			$shippingrate[$rate]->vat 	= $shippingVatRate;
+			$shippingrate[$rate]->rate  = $rs->shipping_rate_value;
+			$shippingrate[$rate]->vat   = $shippingVatRate;
 			$rate++;
 		}
+
 		return $shippingrate;
-    }
-}	?>
+	}
+}
+
+?>
--- component/plugins/rs_payment_paypal/rs_payment_paypal.php
+++ component/plugins/rs_payment_paypal/rs_payment_paypal.php
@@ -1,20 +1,12 @@
 <?php
 /**
- * @copyright Copyright (C) 2010 redCOMPONENT.com. All rights reserved.
- * @license GNU/GPL, see license.txt or http://www.gnu.org/copyleft/gpl.html
- * Developed by email@recomponent.com - redCOMPONENT.com
+ * @package     RedSHOP.Plugin
+ * @subpackage  rs_payment_paypal
  *
- * redSHOP can be downloaded from www.redcomponent.com
- * redSHOP is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License 2
- * as published by the Free Software Foundation.
- *
- * You should have received a copy of the GNU General Public License
- * along with redSHOP; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * @copyright   Copyright (C) 2005 - 2013 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
-/** ensure this file is being included by a parent file */
 defined('_JEXEC') or die('Restricted access');
 jimport('joomla.plugin.plugin');
 /*$mainframe =& JFactory::getApplication();
@@ -22,113 +14,124 @@ $mainframe->registerEvent( 'onPrePayment', 'plgRedshoppayment_paypal' );*/
 class plgRedshop_paymentrs_payment_paypal extends JPlugin
 {
 	var $_table_prefix = null;
-   /**
-    * Constructor
-    *
-    * For php4 compatability we must not use the __constructor as a constructor for
-    * plugins because func_get_args ( void ) returns a copy of all passed arguments
-    * NOT references.  This causes problems with cross-referencing necessary for the
-    * observer design pattern.
-    */
-   	function plgRedshop_paymentrs_payment_paypal(&$subject)
-    {
-            // load plugin parameters
-            parent::__construct( $subject );
-            $this->_table_prefix = '#__redshop_';
-            $this->_plugin = JPluginHelper::getPlugin( 'redshop_payment', 'rs_payment_paypal' );
-            $this->_params = new JRegistry( $this->_plugin->params );
-    }
 
-    /**
-    * Plugin method with the same name as the event will be called automatically.
-    */
- 	function onPrePayment($element, $data)
-    {
+	/**
+	 * Constructor
+	 *
+	 * For php4 compatability we must not use the __constructor as a constructor for
+	 * plugins because func_get_args ( void ) returns a copy of all passed arguments
+	 * NOT references.  This causes problems with cross-referencing necessary for the
+	 * observer design pattern.
+	 */
+	function plgRedshop_paymentrs_payment_paypal(&$subject)
+	{
+		// load plugin parameters
+		parent::__construct($subject);
+		$this->_table_prefix = '#__redshop_';
+		$this->_plugin       = JPluginHelper::getPlugin('redshop_payment', 'rs_payment_paypal');
+		$this->_params       = new JRegistry($this->_plugin->params);
+	}
+
+	/**
+	 * Plugin method with the same name as the event will be called automatically.
+	 */
+	function onPrePayment($element, $data)
+	{
 
 
-    	if($element!='rs_payment_paypal'){
-    		return;
-    	}
-    	if (empty($plugin))
-        {
-         	$plugin = $element;
-        }
+		if ($element != 'rs_payment_paypal')
+		{
+			return;
+		}
+		if (empty($plugin))
+		{
+			$plugin = $element;
+		}
 
- 		$mainframe =& JFactory::getApplication();
-		$paymentpath=JPATH_SITE.DS.'plugins'.DS.'redshop_payment'.DS.$plugin.DS.$plugin.DS.'extra_info.php';
+		$mainframe   =& JFactory::getApplication();
+		$paymentpath = JPATH_SITE . DS . 'plugins' . DS . 'redshop_payment' . DS . $plugin . DS . $plugin . DS . 'extra_info.php';
 		include($paymentpath);
-    }
+	}
 
-  function onNotifyPaymentrs_payment_paypal($element, $request){
+	function onNotifyPaymentrs_payment_paypal($element, $request)
+	{
 
-    	if($element!='rs_payment_paypal'){
-    		return;
-    	}
-    	$db = JFactory::getDBO();
-		$request=JRequest::get('request');
-		$Itemid = $request["Itemid"];
+		if ($element != 'rs_payment_paypal')
+		{
+			return;
+		}
+		$db      = JFactory::getDBO();
+		$request = JRequest::get('request');
+		$Itemid  = $request["Itemid"];
 
- 		$quickpay_parameters=$this->getparameters('rs_payment_paypal');
-		$paymentinfo = $quickpay_parameters[0];
-		$paymentparams = new JRegistry( $paymentinfo->params );
+		$quickpay_parameters = $this->getparameters('rs_payment_paypal');
+		$paymentinfo         = $quickpay_parameters[0];
+		$paymentparams       = new JRegistry($paymentinfo->params);
 
-		$is_test = $paymentparams->get('sandbox','');
-		$verify_status = $paymentparams->get('verify_status','');
-		$invalid_status = $paymentparams->get('invalid_status','');
-		$cancel_status = $paymentparams->get('cancel_status','');
+		$is_test        = $paymentparams->get('sandbox', '');
+		$verify_status  = $paymentparams->get('verify_status', '');
+		$invalid_status = $paymentparams->get('invalid_status', '');
+		$cancel_status  = $paymentparams->get('cancel_status', '');
 
 
-
-		$user=JFactory::getUser();
+		$user = JFactory::getUser();
 
 		$order_id = $request["orderid"];
 
 
-		$status=$request['payment_status'];
-	 	$tid = $request['txn_id'];
-		$uri =& JURI::getInstance();
-		$url= JURI::base();
-		$uid=$user->id;
-		$db=JFactory::getDBO();
+		$status = $request['payment_status'];
+		$tid    = $request['txn_id'];
+		$uri    =& JURI::getInstance();
+		$url    = JURI::base();
+		$uid    = $user->id;
+		$db     = JFactory::getDBO();
 
-		if($status=='Completed')
+		if ($status == 'Completed')
 		{
-		 	$values->order_status_code=$verify_status;
-		 	$values->order_payment_status_code='Paid';
-		 	$values->log=JText::_('COM_REDSHOP_ORDER_PLACED');
- 		 	$values->msg=JText::_('COM_REDSHOP_ORDER_PLACED');
+			$values->order_status_code         = $verify_status;
+			$values->order_payment_status_code = 'Paid';
+			$values->log                       = JText::_('COM_REDSHOP_ORDER_PLACED');
+			$values->msg                       = JText::_('COM_REDSHOP_ORDER_PLACED');
 
-		}else{
-			$values->order_status_code=$invalid_status;
-			$values->order_payment_status_code='Unpaid';
-		 	$values->log=JText::_('COM_REDSHOP_ORDER_NOT_PLACED');
- 		 	$values->msg=JText::_('COM_REDSHOP_ORDER_NOT_PLACED');
+		}
+		else
+		{
+			$values->order_status_code         = $invalid_status;
+			$values->order_payment_status_code = 'Unpaid';
+			$values->log                       = JText::_('COM_REDSHOP_ORDER_NOT_PLACED');
+			$values->msg                       = JText::_('COM_REDSHOP_ORDER_NOT_PLACED');
 		}
 
- 		$values->transaction_id=$tid;
-		$values->order_id=$order_id;
-		return $values;
-    }
+		$values->transaction_id = $tid;
+		$values->order_id       = $order_id;
 
-	function getparameters($payment){
-		$db = JFactory::getDBO();
-		$sql="SELECT * FROM #__extensions WHERE `element`='".$payment."'";
+		return $values;
+	}
+
+	function getparameters($payment)
+	{
+		$db  = JFactory::getDBO();
+		$sql = "SELECT * FROM #__extensions WHERE `element`='" . $payment . "'";
 		$db->setQuery($sql);
-		$params=$db->loadObjectList();
+		$params = $db->loadObjectList();
+
 		return $params;
 	}
 
 
-	function orderPaymentNotYetUpdated($dbConn, $order_id, $tid){
+	function orderPaymentNotYetUpdated($dbConn, $order_id, $tid)
+	{
 
-		$db = JFactory::getDBO();
-		$res = false;
-		 $query = "SELECT COUNT(*) `qty` FROM `#__redshop_order_payment` WHERE `order_id` = '" . $db->getEscaped($order_id ) . "' and order_payment_trans_id = '" .$db->getEscaped( $tid) . "'";
+		$db    = JFactory::getDBO();
+		$res   = false;
+		$query = "SELECT COUNT(*) `qty` FROM `#__redshop_order_payment` WHERE `order_id` = '" . $db->getEscaped($order_id) . "' and order_payment_trans_id = '" . $db->getEscaped($tid) . "'";
 		$db->SetQuery($query);
-	 	$order_payment = $db->loadResult();
-		if ($order_payment == 0){
+		$order_payment = $db->loadResult();
+		if ($order_payment == 0)
+		{
 			$res = true;
 		}
+
 		return $res;
 	}
 }
