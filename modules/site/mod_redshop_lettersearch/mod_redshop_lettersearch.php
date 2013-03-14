dc537f90dd03c12741505e75d45a946390d4f6e9
Javier <javier.gomez.corio@gmail.com>
2013-03-14 16:17:38 +0100

    Review code style in mod letter search

--- modules/site/mod_redshop_lettersearch/mod_redshop_lettersearch.php
+++ modules/site/mod_redshop_lettersearch/mod_redshop_lettersearch.php
@@ -1,36 +1,30 @@
 <?php
 /**
- * @copyright Copyright (C) 2010 redCOMPONENT.com. All rights reserved.
- * @license GNU/GPL, see license.txt or http://www.gnu.org/copyleft/gpl.html
- * Developed by email@recomponent.com - redCOMPONENT.com
+ * @package     RedSHOP.Frontend
+ * @subpackage  mod_redshop_lettersearch
  *
- * redSHOP can be downloaded from www.redcomponent.com
- * redSHOP is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License 2
- * as published by the Free Software Foundation.
- *
- * You should have received a copy of the GNU General Public License
- * along with redSHOP; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * @copyright   Copyright (C) 2005 - 2013 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
  */
-// no direct access
+
 defined('_JEXEC') or die('Restricted access');
 
-//get module path
-$mod_dir = dirname( __FILE__ );
+// Get module path
+$mod_dir = dirname(__FILE__);
 
-define('LETTERSEARCH_MODULE_PATH',$mod_dir);
+define('LETTERSEARCH_MODULE_PATH', $mod_dir);
 
-require_once(LETTERSEARCH_MODULE_PATH.DS.'helper.php');
-$lettersearchHelper = new modlettersearchHelper();
+require_once LETTERSEARCH_MODULE_PATH . DS . 'helper.php';
+$lettersearchHelper = new modlettersearchHelper;
 
-$selected_field = trim( $params->get( 'list_of_fields','') );
-$db	=	JFactory::getDBO();
-$getcharacters = $lettersearchHelper->getDefaultModulecharacters($selected_field);
+$selected_field = trim($params->get('list_of_fields', ''));
+$db             = JFactory::getDBO();
+$getcharacters  = $lettersearchHelper->getDefaultModulecharacters($selected_field);
 
+// Get show number of products
+$number_of_items   = trim($params->get('count_products', 5));
 
+// Get show number of Columns
+$number_of_columns = trim($params->get('number_of_columns', 6));
 
-$number_of_items = trim( $params->get( 'count_products',5) );	// get show number of products
-$number_of_columns = trim( $params->get( 'number_of_columns',6) );	// get show number of Columns
-
-require(JModuleHelper::getLayoutPath('mod_redshop_lettersearch'));
+require JModuleHelper::getLayoutPath('mod_redshop_lettersearch');
