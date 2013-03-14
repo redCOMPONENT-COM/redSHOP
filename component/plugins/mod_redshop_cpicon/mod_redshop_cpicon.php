6354056118467c361cfc704c765916711fd0de82
Javier <javier.gomez.corio@gmail.com>
2013-03-14 18:26:44 +0100

    Review Code Style in Plugins

--- component/plugins/mod_redshop_cpicon/mod_redshop_cpicon.php
+++ component/plugins/mod_redshop_cpicon/mod_redshop_cpicon.php
@@ -1,17 +1,24 @@
 <?php
-// no direct access
+/**
+ * @package     RedSHOP.Plugin
+ * @subpackage  Cpicon
+ *
+ * @copyright   Copyright (C) 2005 - 2013 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
 defined('_JEXEC') or die('Restricted access');
 $lang = & JFactory::getLanguage();
-$app = &JFactory::getApplication();
-$document = &JFactory::getDocument();
-$document->addStyleSheet( JURI::root().'administrator/modules/mod_redshop_cpicon/styles.css');
+$app = & JFactory::getApplication();
+$document = & JFactory::getDocument();
+$document->addStyleSheet(JURI::root() . 'administrator/modules/mod_redshop_cpicon/styles.css');
 
 ?>
 <div id="cpanel">
-<div class="icon-wrapper">
-	<div class="icon">
-		<a href="index.php?option=com_redshop">
-			<img src="components/com_redshop/assets/images/redshopcart48.png" alt="redSHOP"><span>redSHOP</span></a>
+	<div class="icon-wrapper">
+		<div class="icon">
+			<a href="index.php?option=com_redshop">
+				<img src="components/com_redshop/assets/images/redshopcart48.png" alt="redSHOP"><span>redSHOP</span></a>
+		</div>
 	</div>
-</div>
 </div>
