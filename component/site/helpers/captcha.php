f2b6f192c6468cc935257f024435fa820a883ad3
Javier <javier.gomez.corio@gmail.com>
2013-03-20 18:25:29 +0100

    review captcha helper

--- component/site/helpers/captcha.php
+++ component/site/helpers/captcha.php
@@ -9,8 +9,7 @@
 
 class CaptchaSecurityImages
 {
-
-	var $font = 'components/com_redshop/assets/captcha/monofont.ttf';
+	public $font = 'components/com_redshop/assets/captcha/monofont.ttf';
 
 	public function generateCode($characters)
 	{
@@ -18,6 +17,7 @@ class CaptchaSecurityImages
 		$possible = '23456789bcdfghjkmnpqrstvwxyz';
 		$code     = '';
 		$i        = 0;
+
 		while ($i < $characters)
 		{
 			$code .= substr($possible, mt_rand(0, strlen($possible) - 1), 1);
@@ -35,21 +35,18 @@ class CaptchaSecurityImages
 
 		if ($captchaname == 'pri_security_code')
 		{
-			//$session->set('pri_security_code','');
-			//	$session->set('pri_security_code',$code);
 			setcookie('pri_security_code', $code);
-			//echo $_SESSION['pri_security_code'] = $code; exit;
 		}
 		else
 		{
-			//$session->set('security_code','');
-			//$session->set('security_code',$code);
 			setcookie('security_code', $code);
 		}
-		/* font size will be 75% of the image height */
+
+		// Font size will be 75% of the image height
 		$font_size = $height * 0.75;
 		$image     = @imagecreate($width, $height) or die('Cannot initialize new GD image stream');
-		/* set the colours */
+
+		// Set the colours
 		$background_color = imagecolorallocate($image, 255, 255, 255);
 		$text_color       = imagecolorallocate($image, 20, 40, 100);
 		$noise_color      = imagecolorallocate($image, 100, 120, 180);
@@ -58,12 +55,14 @@ class CaptchaSecurityImages
 		{
 			imagefilledellipse($image, mt_rand(0, $width), mt_rand(0, $height), 1, 1, $noise_color);
 		}
-		/* generate random lines in background */
+
+		// Generate random lines in background
 		for ($i = 0; $i < ($width * $height) / 150; $i++)
 		{
 			imageline($image, mt_rand(0, $width), mt_rand(0, $height), mt_rand(0, $width), mt_rand(0, $height), $noise_color);
 		}
-		/* create textbox and add text */
+
+		// Create textbox and add text
 		$textbox = imagettfbbox($font_size, 0, $this->font, $code) or die('Error in imagettfbbox function');
 		$x = ($width - $textbox[4]) / 2;
 		$y = ($height - $textbox[5]) / 2;
@@ -74,14 +73,5 @@ class CaptchaSecurityImages
 		imagejpeg($image);
 		imagedestroy($image);
 		exit;
-
-		//$session->clear($captchaname);
-		//$session->clear('pri_security_code');
-		//$session->set( $captchaname, $code );
-		//$_SESSION['security_code'] = $code;
 	}
-
 }
-
-
-?>
