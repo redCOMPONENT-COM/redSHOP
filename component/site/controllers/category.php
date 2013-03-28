4dcd3b6000226cdae81c96e4676cb69aa8b9ae1d
Gunjan Patel <gunjan@redcomponent.com>
2013-03-28 16:53:50 +0530

    change back to JController

--- component/site/controllers/category.php
+++ component/site/controllers/category.php
@@ -18,7 +18,7 @@ JLoader::import('joomla.application.component.controller');
  * @subpackage  Controller
  * @since       1.0
  */
-class CategoryController extends JControllerLegacy
+class CategoryController extends JController
 {
 	/**
 	 *  Method to Export XML file
