b636ea436132076b1eb95fdc74bab28a8e7490a4
vipula <vipula@redweb.dk>
2013-05-27 11:39:19 +0530

    Add alter query into 1.3.sql

---
+++ component/admin/sql/updates/mysql/1.3.sql
@@ -0,0 +1,2 @@
+ALTER IGNORE TABLE #__redshop_product_attribute_property ADD `extra_field` VARCHAR( 250 ) NOT NULL;
+ALTER IGNORE TABLE #__redshop_product_subattribute_color ADD `extra_field` VARCHAR( 250 ) NOT NULL;
