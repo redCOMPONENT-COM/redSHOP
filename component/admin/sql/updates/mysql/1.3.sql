20629436c3bba51eaee94486d99e06b66f927c00
vipula <vipula@redweb.dk>
2013-06-10 15:48:55 +0530

    Language string added and resolve conflicts

--- component/admin/sql/updates/mysql/1.3.sql
+++ component/admin/sql/updates/mysql/1.3.sql
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
+ALTER TABLE `#__redshop_product_subattribute_color` MODIFY `subattribute_color_name` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER IGNORE TABLE #__redshop_product_attribute_property ADD `extra_field` VARCHAR( 250 ) NOT NULL;
 ALTER IGNORE TABLE #__redshop_product_subattribute_color ADD `extra_field` VARCHAR( 250 ) NOT NULL;
-=======
-ALTER TABLE `#__redshop_product_subattribute_color` MODIFY `subattribute_color_name` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci
->>>>>>> 59b2e490d3c621312eca6ce5a49598a16ebb6721
