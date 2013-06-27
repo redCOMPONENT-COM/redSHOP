98fbbf985b05c287caf1023b66f3a51f5a69bf87
Javier <javier.gomez.corio@gmail.com>
2013-06-27 11:14:51 +0200

    Review code style in Table Subscription

--- component/admin/tables/subscription.php
+++ component/admin/tables/subscription.php
@@ -14,41 +14,65 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-defined( '_JEXEC' ) or die( 'Restricted access' );
+defined('_JEXEC') or die;
 
+// @Todo: probably the following import could be removed
 jimport('joomla.application.component.model');
 
-class Tablesubscription extends JTable
+class TableSubscription extends JTable
 {
-	var $subscription_id  = 0;
-	var $product_id = 0;
-	var $subscription_period = 0;
-	var $subscription_period_unit = null;
-	var $subscription_period_lifetime = 0;
-	var $subscription_applicable_products = null;
-	var $subscription_applicable_categories = null;
-	var $joomla_acl_groups = null;
-	var $fallback_joomla_acl_groups = null;
-	var $shoppergroup = null;
-  	var $fallback_shoppergroup = null;
-		
-	function Tablesubscription(& $db) 
+	public $subscription_id  = 0;
+
+	public $product_id = 0;
+
+	public $subscription_period = 0;
+
+	public $subscription_period_unit = null;
+
+	public $subscription_period_lifetime = 0;
+
+	public $subscription_applicable_products = null;
+
+	public $subscription_applicable_categories = null;
+
+	public $joomla_acl_groups = null;
+
+	public $fallback_joomla_acl_groups = null;
+
+	public $shoppergroup = null;
+
+	public $fallback_shoppergroup = null;
+
+	public function Tablesubscription(&$db)
 	{
-	  $this->_table_prefix = '#__redshop_';
-			
-		parent::__construct($this->_table_prefix.'subscription', 'subscription_id', $db);
+		$this->_table_prefix = '#__redshop_';
+
+		parent::__construct($this->_table_prefix . 'subscription', 'subscription_id', $db);
 	}
 
-	function bind($array, $ignore = '')
+	/**
+	 * Method to bind an associative array or object to the JTable instance.This
+	 * method only binds properties that are publicly accessible and optionally
+	 * takes an array of properties to ignore when binding.
+	 *
+	 * @param   mixed  $array   An associative array or object to bind to the JTable instance.
+	 * @param   mixed  $ignore  An optional array or space separated list of properties to ignore while binding.
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @link    http://docs.joomla.org/JTable/bind
+	 * @since   11.1
+	 */
+	public function bind($array, $ignore = '')
 	{
-		if (key_exists( 'params', $array ) && is_array( $array['params'] )) {
-			$registry = new JRegistry();
+		if (key_exists('params', $array) && is_array($array['params']))
+		{
+			$registry = new JRegistry;
 			$registry->loadArray($array['params']);
 			$array['params'] = $registry->toString();
 		}
 
 		return parent::bind($array, $ignore);
 	}
-	
 }
-?>
+
