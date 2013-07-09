77e584a6bc92d87e37ca820a60de1d99bd421595
Gunjan Patel <gunjan.ce2009@gmail.com>
2013-07-09 15:53:07 +0530

    code style

--- component/admin/views/accountgroup_detail/view.html.php
+++ component/admin/views/accountgroup_detail/view.html.php
@@ -35,9 +35,7 @@ class accountgroup_detailVIEWaccountgroup_detail extends JView
 			JToolBarHelper::cancel('cancel', JText::_('JTOOLBAR_CLOSE'));
 		}
 
-		JToolBarHelper::title(JText::_('COM_REDSHOP_ECONOMIC_ACCOUNT_GROUP') . ': <small><small>[ ' . $text . ' ]</small></small>',
-			'redshop_accountgroup48'
-		);
+		JToolBarHelper::title(JText::_('COM_REDSHOP_ECONOMIC_ACCOUNT_GROUP') . ': <small><small>[ ' . $text . ' ]</small></small>', 'redshop_accountgroup48');
 
 		$lists['published'] = JHTML::_('select.booleanlist', 'published', 'class="inputbox"', $detail->published);
 
