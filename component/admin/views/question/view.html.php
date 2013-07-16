36cff948a8eb08bbb7b599b2c10af01ea727d32e
Gunjan Patel <gunjan@redcomponent.com>
2013-07-16 14:29:13 +0530

    [fix] Strict standard errors in question view

--- component/admin/views/question/view.html.php
+++ component/admin/views/question/view.html.php
@@ -21,7 +21,7 @@ class questionViewquestion extends JView
 		$app      = JFactory::getApplication();
 		$document = JFactory::getDocument();
 
-		$document->setTitle(JText::_('COM_REDSHOP_question'));
+		$document->setTitle(JText::_('COM_REDSHOP_QUESTION'));
 		$model = $this->getModel('question');
 
 		JToolBarHelper::title(JText::_('COM_REDSHOP_QUESTION_MANAGEMENT'), 'redshop_question48');
@@ -31,34 +31,35 @@ class questionViewquestion extends JView
 		JToolBarHelper::publishList();
 		JToolBarHelper::unpublishList();
 
-		$filter_order     = $app->getUserStateFromRequest($context . 'filter_order', 'filter_order', 'question_date');
-		$filter_order_Dir = $app->getUserStateFromRequest($context . 'filter_order_Dir', 'filter_order_Dir', 'DESC');
-		$product_id       = $app->getUserStateFromRequest($context . 'product_id', 'product_id', 0);
+		$filter_order       = $app->getUserStateFromRequest($context . 'filter_order', 'filter_order', 'question_date');
+		$filter_order_Dir   = $app->getUserStateFromRequest($context . 'filter_order_Dir', 'filter_order_Dir', 'DESC');
+		$product_id         = $app->getUserStateFromRequest($context . 'product_id', 'product_id', 0);
 
 		$lists['order']     = $filter_order;
 		$lists['order_Dir'] = $filter_order_Dir;
 
-		$question   = $this->get('Data');
-		$total      = $this->get('Total');
-		$pagination = $this->get('Pagination');
+		$question           = $this->get('Data');
+		$total              = $this->get('Total');
+		$pagination         = $this->get('Pagination');
 
-		$option = $model->getProduct();
-		$optionsection = array();
+		$option             = $model->getProduct();
+		$optionsection      = array();
+		$optionsection[0]   = new stdClass;
 		$optionsection[0]->product_id = 0;
 		$optionsection[0]->product_name = JText::_('COM_REDSHOP_SELECT');
 
 		if (count($option) > 0)
 		{
-			$optionsection = @array_merge($optionsection, $option);
+			$optionsection = array_merge($optionsection, $option);
 		}
 
 		$lists['product_id'] = JHTML::_('select.genericlist', $optionsection, 'product_id',
 			'class="inputbox" size="1" onchange="document.adminForm.submit();"', 'product_id', 'product_name', $product_id
 		);
 
-		$this->lists = $lists;
-		$this->question = $question;
-		$this->pagination = $pagination;
+		$this->lists       = $lists;
+		$this->question    = $question;
+		$this->pagination  = $pagination;
 		$this->request_url = $uri->toString();
 
 		parent::display($tpl);
