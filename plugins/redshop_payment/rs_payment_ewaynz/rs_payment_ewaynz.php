3b0a4d6ecfa73585c7ecb5e39eca648492de6005
Gunjan Patel <gunjan@redcomponent.com>
2013-09-05 17:27:37 +0530

    [fix] SetQuery typo

--- plugins/redshop_payment/rs_payment_ewaynz/rs_payment_ewaynz.php
+++ plugins/redshop_payment/rs_payment_ewaynz/rs_payment_ewaynz.php
@@ -76,7 +76,7 @@ class plgRedshop_paymentrs_payment_ewaynz extends JPlugin
 		$db = JFactory::getDBO();
 		$res = false;
 		$query = "SELECT COUNT(*) FROM " . $this->_table_prefix . "order_payment WHERE `order_id` = '" . $db->getEscaped($order_id) . "' and order_payment_trans_id = '" . $db->getEscaped($tid) . "'";
-		$db->SetQuery($query);
+		$db->setQuery($query);
 		$order_payment = $db->loadResult();
 
 		if ($order_payment == 0)
--- plugins/redshop_payment/rs_payment_quickpay/rs_payment_quickpay.php
+++ plugins/redshop_payment/rs_payment_quickpay/rs_payment_quickpay.php
@@ -94,7 +94,7 @@ class plgRedshop_paymentrs_payment_quickpay extends JPlugin
 			// Find the corresponding order in the database
 			$db = JFactory::getDBO();
 			$qv = "SELECT order_id, order_number FROM #__redshop_orders WHERE order_id='" . $order_id . "'";
-			$db->SetQuery($qv);
+			$db->setQuery($qv);
 			$orders = $db->LoadObjectList();
 
 			if ($orders)
@@ -140,7 +140,7 @@ class plgRedshop_paymentrs_payment_quickpay extends JPlugin
 		$db    = JFactory::getDBO();
 		$res   = false;
 		$query = "SELECT COUNT(*), `qty` FROM `#__redshop_order_payment` WHERE `order_id` = '" . $db->getEscaped($order_id) . "' and order_payment_trans_id = '" . $db->getEscaped($tid) . "'";
-		$db->SetQuery($query);
+		$db->setQuery($query);
 		$order_payment = $db->loadResult();
 
 		if ($order_payment == 0)
