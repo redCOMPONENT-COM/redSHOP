758cfffa523009a9117b6f2cf698bae6873a490e
Gunjan Patel <gunjan@redcomponent.com>
2013-09-06 16:07:48 +0530

    [sec] Review security issue

--- modules/site/mod_redshop_lettersearch/helper.php
+++ modules/site/mod_redshop_lettersearch/helper.php
@@ -19,7 +19,7 @@ class modlettersearchHelper
 		$db    = JFactory::getDbo();
 		$query = "SELECT DISTINCT LEFT(fd.data_txt, 1) AS chars FROM #__redshop_fields AS f";
 		$query .= " LEFT JOIN #__redshop_fields_data AS fd ON fd.fieldid = f.field_id";
-		$query .= " WHERE f.field_id = '" . $selected_field . "' AND  fd.section=1 ";
+		$query .= " WHERE f.field_id = " . (int) $selected_field . " AND  fd.section=1 ";
 		$query .= " AND fd.data_txt IS NOT NULL  ORDER BY fd.data_txt";
 		$db->setQuery($query);
 		$characterlist = $db->loadObjectlist();
