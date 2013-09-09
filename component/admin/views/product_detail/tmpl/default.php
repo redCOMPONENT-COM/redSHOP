1eb9a501f2692ddf43e54add080c7070701bf7c7
bnisevic <bojan@redcomponent.com>
2013-09-09 18:01:27 +0200

    Moved meta data layout to the default_product_meta_data.php.

--- component/admin/views/product_detail/tmpl/default.php
+++ component/admin/views/product_detail/tmpl/default.php
@@ -217,7 +217,6 @@ echo $this->loadTemplate('product_accessory');
 echo JHtml::_('tabs.panel', JText::_('COM_REDSHOP_RELATED_PRODUCT'), 'productTab7');
 echo $this->loadTemplate('related');
 ?>
-
 <?php if ($this->CheckRedProductFinder > 0) : ?>
 	<?php
 		// Tab8 - redPRODUCTFINDER tab panel.
@@ -234,7 +233,6 @@ echo $this->loadTemplate('related');
 			$ordering = $this->getassociation->ordering;
 		}
 	?>
-
 	<table class="adminform">
 		<tr>
 			<td>
@@ -248,7 +246,6 @@ echo $this->loadTemplate('related');
 	</table>
 	<input type="hidden" name="association_id" value="<?php echo $accosiation_id; ?>"/>
 	<input type="hidden" name="ordering" value="<?php echo $ordering; ?>"/>
-
 <?php endif; ?>
 
 <?php
@@ -259,219 +256,22 @@ echo JHtml::_('tabs.panel', JText::_('COM_REDSHOP_META_DATA_TAB'), 'productTab9'
 	<legend>
 		<?php echo JText::_('COM_REDSHOP_META_DATA_TAB'); ?>
 	</legend>
-
-	<table class="admintable">
-
-		<tr>
-			<td class="key">
-				<label for="append_to_global_seo">
-					<?php echo JText::_('COM_REDSHOP_APPEND_TO_GLOBAL_SEO_LBL'); ?>
-				</label>
-			</td>
-			<td>
-				<?php echo $this->lists['append_to_global_seo']; ?>
-			</td>
-			<td>
-				<?php
-					echo JHTML::tooltip(
-									JText::_('COM_REDSHOP_TOOLTIP_APPEND_TO_GLOBAL_SEO_LBL'),
-									JText::_('COM_REDSHOP_APPEND_TO_GLOBAL_SEO_LBL'),
-									'tooltip.png',
-									'',
-									'',
-									false
-					);
-				?>
-			</td>
-		</tr>
-
-		<tr>
-			<td class="key">
-				<label for="pagetitle">
-					<?php echo JText::_('COM_REDSHOP_PAGE_TITLE'); ?>
-				</label>
-			</td>
-			<td>
-				<input class="text_area" type="text" name="pagetitle" id="pagetitle" size="75" value="<?php echo htmlspecialchars($this->detail->pagetitle); ?>"/>
-			</td>
-			<td>
-				<?php echo JHTML::tooltip(JText::_('COM_REDSHOP_TOOLTIP_PAGE_TITLE'), JText::_('COM_REDSHOP_PAGE_TITLE'), 'tooltip.png', '', '', false); ?>
-			</td>
-		</tr>
-
-		<tr>
-			<td class="key">
-				<label for="pageheading">
-					<?php echo JText::_('COM_REDSHOP_PAGE_HEADING'); ?>
-				</label>
-			</td>
-			<td>
-				<input class="text_area" type="text" name="pageheading" id="pageheading" size="75" value="<?php echo $this->detail->pageheading; ?>"/>
-			</td>
-			<td>
-				<?php echo JHTML::tooltip(JText::_('COM_REDSHOP_TOOLTIP_PAGE_HEADING'), JText::_('COM_REDSHOP_PAGE_HEADING'), 'tooltip.png', '', '', false); ?>
-			</td>
-		</tr>
-
-		<tr>
-			<td class="key">
-				<label for="sef_url">
-					<?php echo JText::_('COM_REDSHOP_SEF_URL'); ?>
-				</label>
-			</td>
-			<td>
-				<input class="text_area" type="text" name="sef_url" id="sef_url" size="75" value="<?php echo $this->detail->sef_url; ?>"/>
-			</td>
-			<td>
-				<?php echo JHTML::tooltip(JText::_('COM_REDSHOP_TOOLTIP_SEF_URL'), JText::_('COM_REDSHOP_SEF_URL'), 'tooltip.png', '', '', false); ?>
-			</td>
-		</tr>
-
-		<tr>
-			<td class="key">
-				<label for="canonical_url">
-					<?php echo JText::_('COM_REDSHOP_CANONICAL_URL_PRODUCT'); ?>
-				</label>
-			</td>
-			<td>
-				<input class="text_area"
-					   type="text"
-					   name="canonical_url"
-					   id="canonical_url"
-					   size="75"
-					   value="<?php echo isset($this->detail->canonical_url) ? $this->detail->canonical_url : ""; ?>"
-					/>
-			</td>
-			<td>
-				<?php
-					echo JHTML::tooltip(
-									JText::_('COM_REDSHOP_TOOLTIP_CANONICAL_URL_PRODUCT'),
-									JText::_('COM_REDSHOP_CANONICAL_URL_PRODUCT'),
-									'tooltip.png',
-									'',
-									'',
-									false
-				);
-				?>
-			</td>
-		</tr>
-
-		<tr>
-			<td class="key">
-				<label for="cat_in_sefurl">
-					<?php echo JText::_('COM_REDSHOP_SELECT_CATEGORY_TO_USEIN_SEF'); ?>
-				</label>
-			</td>
-			<td>
-				<?php echo $this->lists['cat_in_sefurl']; ?>
-			</td>
-			<td>
-				<?php
-					echo JHTML::tooltip(
-									JText::_('COM_REDSHOP_TOOLTIP_SELECT_CATEGORY_TO_USEIN_SEF'),
-									JText::_('COM_REDSHOP_SELECT_CATEGORY_TO_USEIN_SEF'),
-									'tooltip.png',
-									'',
-									'',
-									false
-					);
-				?>
-			</td>
-		</tr>
-
-		<tr>
-			<td colspan="2">
-				<hr/>
-			</td>
-		</tr>
-
-		<tr>
-			<td class="key">
-				<label for="metakey">
-					<?php echo JText::_('COM_REDSHOP_META_KEYWORDS'); ?>
-				</label>
-			</td>
-			<td>
-				<textarea class="text_area" name="metakey" id="metakey" rows="4" cols="40"><?php echo $this->detail->metakey; ?></textarea>
-			</td>
-			<td>
-				<?php echo JHTML::tooltip(JText::_('COM_REDSHOP_TOOLTIP_META_KEYWORDS'), JText::_('COM_REDSHOP_META_KEYWORDS'), 'tooltip.png', '', '', false); ?>
-			</td>
-		</tr>
-
-		<tr>
-			<td class="key">
-				<label for="metadesc">
-					<?php echo JText::_('COM_REDSHOP_META_DESCRIPTION'); ?>
-				</label>
-			</td>
-			<td>
-				<textarea class="text_area" name="metadesc" id="metadesc" rows="4" cols="40"><?php echo htmlspecialchars($this->detail->metadesc); ?></textarea>
-			</td>
-			<td>
-				<?php
-					echo JHTML::tooltip(JText::_('COM_REDSHOP_TOOLTIP_META_DESCRIPTION'), JText::_('COM_REDSHOP_META_DESCRIPTION'), 'tooltip.png', '', '', false);
-				?>
-			</td>
-		</tr>
-
-		<tr>
-			<td class="key">
-				<label for="metalanguage_setting">
-					<?php echo JText::_('COM_REDSHOP_META_LANG_SETTING'); ?>
-				</label>
-			</td>
-			<td>
-				<textarea class="text_area"
-						  name="metalanguage_setting"
-						  id="metalanguage_setting"
-						  rows="4"
-						  cols="40"><?php echo $this->detail->metalanguage_setting; ?></textarea>
-			</td>
-			<td>
-				<?php
-					echo JHTML::tooltip(JText::_('COM_REDSHOP_TOOLTIP_META_LANG_SETTING'), JText::_('COM_REDSHOP_META_LANG_SETTING'), 'tooltip.png', '', '', false);
-				?>
-			</td>
-		</tr>
-
-		<tr>
-			<td class="key">
-				<label for="metarobot_info">
-					<?php echo JText::_('COM_REDSHOP_META_ROBOT_INFO'); ?>
-				</label>
-			</td>
-			<td>
-				<textarea class="text_area" name="metarobot_info" id="metarobot_info" rows="4" cols="40"><?php echo $this->detail->metarobot_info; ?></textarea>
-			</td>
-			<td>
-				<?php
-					echo JHTML::tooltip(JText::_('COM_REDSHOP_TOOLTIP_META_ROBOT_INFO'), JText::_('COM_REDSHOP_META_ROBOT_INFO'), 'tooltip.png', '', '', false);
-				?>
-			</td>
-		</tr>
-
-	</table>
-
+	<?php echo $this->loadTemplate('product_meta_data'); ?>
 </fieldset>
 
 <?php if (USE_STOCKROOM == 1) : ?>
 	<?php
 	// Tab10 - Stockroom tab panel.
 	echo JHtml::_('tabs.panel', JText::_('COM_REDSHOP_STOCKROOM_TAB'), 'productTab10');
+	echo $this->loadTemplate('productstockroom');
 	?>
-	<div class="col50">
-		<?php echo $this->loadTemplate('productstockroom')?>
-	</div>
 <?php endif; ?>
 
 <?php
 // Tab11 - Discount calculator tab panel.
 echo JHtml::_('tabs.panel', JText::_('COM_REDSHOP_DISCOUNT_CALCULATOR'), 'productTab11');
+echo $this->loadTemplate('calculator');
 ?>
-<div class="col50">
-	<?php echo $this->loadTemplate('calculator')?>
-</div>
 
 <?php
 // Tab12 - Economic tab panel.
