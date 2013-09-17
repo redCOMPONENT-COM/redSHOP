4e5b6888a24c9fb15d77fa42d5f09ede8d9934f5
Gunjan Patel <gunjan@redcomponent.com>
2013-09-17 12:05:14 +0530

    [review] Code style

--- component/site/helpers/product.php
+++ component/site/helpers/product.php
@@ -5292,7 +5292,7 @@ class producthelper
 						{
 							$borderstyle    = ($selectedsubproperty == $subproperty[$i]->value) ? " 1px solid " : "";
 
-							$filePath      = JPATH_SITE . '/components/com_redshop/assets/images/subcolor/' . $subproperty[$i]->subattribute_color_image;
+							$filePath       = JPATH_SITE . '/components/com_redshop/assets/images/subcolor/' . $subproperty[$i]->subattribute_color_image;
 							$physiclePath   = RedShopHelperImages::generateImages($filePath, '', 'thumb', 'subcolor', ATTRIBUTE_SCROLLER_THUMB_WIDTH, ATTRIBUTE_SCROLLER_THUMB_HEIGHT, USE_IMAGE_SIZE_SWAPPING);
 							$thumbUrl       = REDSHOP_FRONT_IMAGES_ABSPATH . 'subcolor/thumb/' . basename($physiclePath);
 
