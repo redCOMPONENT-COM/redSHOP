09130d2735547896dc8daf664814727996fb4797
Lu Nguyen <lu@redweb.dk>
2013-10-07 10:47:47 +0700

    Fix error when checkout final with product user field

--- component/site/helpers/product.php
+++ component/site/helpers/product.php
@@ -3199,7 +3199,7 @@ class producthelper
 
 	public function insertProdcutUserfield($id = 'NULL', $cart = array(), $order_item_id = 0, $section_id = 12)
 	{
-		$db == JFactory::getDbo();
+		$db = JFactory::getDbo();
 
 		$extraField = new extraField;
 		$row_data   = $extraField->getSectionFieldList($section_id, 1);
