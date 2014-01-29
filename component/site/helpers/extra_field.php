8f92d707b030ccb3df9f42fb19ce263a01c4884e
Gunjan Patel <gunjan@redcomponent.com>
2014-01-29 12:04:48 +0530

    [fix] Issue with display document image

--- component/site/helpers/extra_field.php
+++ component/site/helpers/extra_field.php
@@ -684,7 +684,7 @@ class extraField
 						else
 						{
 							// Support for multiple file upload using JSON for better string handling
-							$document_value = json_decode($data_value->data_txt);
+							$document_value = json_decode(stripslashes($data_value->data_txt));
 						}
 
 						if (count($document_value) > 0)
