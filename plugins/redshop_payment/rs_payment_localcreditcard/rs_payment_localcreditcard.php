0b3a8f36cbb76a7c3e8f1cc836ff1e6307689842
Gunjan Patel <gunjan@redcomponent.com>
2014-02-04 18:48:40 +0530

    [fix] Plugins

--- plugins/redshop_payment/rs_payment_localcreditcard/rs_payment_localcreditcard.php
+++ plugins/redshop_payment/rs_payment_localcreditcard/rs_payment_localcreditcard.php
@@ -9,31 +9,10 @@
 
 defined('_JEXEC') or die;
 
-jimport('joomla.plugin.plugin');
-
 require_once JPATH_SITE . '/administrator/components/com_redshop/helpers/order.php';
 
 class plgRedshop_paymentrs_payment_localcreditcard extends JPlugin
 {
-	public $_table_prefix = null;
-
-	/**
-	 * Constructor
-	 *
-	 * For php4 compatability we must not use the __constructor as a constructor for
-	 * plugins because func_get_args ( void ) returns a copy of all passed arguments
-	 * NOT references.  This causes problems with cross-referencing necessary for the
-	 * observer design pattern.
-	 */
-	public function plgRedshop_paymentrs_payment_localcreditcard(&$subject)
-	{
-		// Load plugin parameters
-		parent::__construct($subject);
-		$this->_table_prefix = '#__redshop_';
-		$this->_plugin = JPluginHelper::getPlugin('redshop_payment', 'rs_payment_localcreditcard');
-		$this->_params = new JRegistry($this->_plugin->params);
-	}
-
 	/**
 	 * Plugin method with the same name as the event will be called automatically.
 	 */
