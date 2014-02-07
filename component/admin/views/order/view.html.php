53cb9183aec3f83ea2ffa433ae895265a8ae5b3a
Gunjan Patel <gunjan@redcomponent.com>
2014-02-07 18:21:33 +0530

    AT-12 #comment Adding plugin and Plugin event

--- component/admin/views/order/view.html.php
+++ component/admin/views/order/view.html.php
@@ -44,8 +44,26 @@ class orderVieworder extends JView
 			JToolBarHelper::custom('multiprint_order', 'print_f2.png', 'print_f2.png', 'COM_REDSHOP_MULTI_PRINT_ORDER_LBL', true);
 			JToolBarHelper::title(JText::_('COM_REDSHOP_ORDER_MANAGEMENT'), 'redshop_order48');
 			JToolBarHelper::addNewX();
-			JToolBarHelper::custom('allstatus', 'save.png', 'save_f2.png', 'COM_REDSHOP_CHANGE_STATUS_TO_ALL_LBL', true);
-			JToolBarHelper::custom('allStatusExceptPacsoft', 'save.png', 'print_f2.png', 'COM_REDSHOP_CHANGE_STATUS_TO_ALL_WITHOUT_PACSOFT_LBL', true);
+
+			JToolBarHelper::custom(
+				'allStatusExceptPacsoft',
+				'save.png',
+				'print_f2.png',
+				'COM_REDSHOP_CHANGE_STATUS_TO_ALL_LBL',
+				true
+			);
+
+			if (POSTDK_INTEGRATION)
+			{
+				JToolBarHelper::custom(
+					'allstatus',
+					'save.png',
+					'save_f2.png',
+					'COM_REDSHOP_CHANGE_STATUS_TO_ALL_WITH_PACSOFT_LBL',
+					true
+				);
+			}
+
 			JToolBarHelper::custom('export_data', 'save.png', 'save_f2.png', 'COM_REDSHOP_EXPORT_DATA_LBL', false);
 			JToolBarHelper::custom('export_fullorder_data', 'save.png', 'save_f2.png', 'COM_REDSHOP_EXPORT_FULL_DATA_LBL', false);
 			JToolBarHelper::custom('gls_export', 'save.png', 'save_f2.png', 'COM_REDSHOP_EXPORT_GLS_LBL', false);
