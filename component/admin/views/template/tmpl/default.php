e012dea7f02384871887b3ceeb7dca62bb13c005
Gunjan Patel <gunjan@redcomponent.com>
2014-03-07 16:45:28 +0530

    [fix] Strict standard issue in template

--- component/admin/views/template/tmpl/default.php
+++ component/admin/views/template/tmpl/default.php
@@ -6,16 +6,13 @@
  * @copyright   Copyright (C) 2005 - 2013 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
-$option = JRequest::getVar('option');
-$filter = JRequest::getVar('filter');
-$redtemplate = new Redtemplate();
+$filter      = JRequest::getVar('filter');
+$redtemplate = new Redtemplate;
 ?>
 <script language="javascript" type="text/javascript">
 
-	Joomla.submitbutton = function (pressbutton) {
-		submitbutton(pressbutton);
-	}
-	submitbutton = function (pressbutton) {
+	Joomla.submitbutton = function (pressbutton)
+	{
 		var form = document.adminForm;
 		if (pressbutton) {
 			form.task.value = pressbutton;
@@ -35,7 +32,7 @@ $redtemplate = new Redtemplate();
 	}
 
 </script>
-<form action="<?php echo 'index.php?option=' . $option; ?>" method="post" name="adminForm" id="adminForm">
+<form action="index.php?option=com_redshop" method="post" name="adminForm" id="adminForm">
 	<div id="editcell">
 
 		<table width="100%">
@@ -91,13 +88,14 @@ $redtemplate = new Redtemplate();
 			</thead>
 			<?php
 			$k = 0;
+
 			for ($i = 0, $n = count($this->templates); $i < $n; $i++)
 			{
-				$row = & $this->templates[$i];
-				$row->id = $row->template_id;
-				$link = JRoute::_('index.php?option=' . $option . '&view=template_detail&task=edit&cid[]=' . $row->template_id);
-
-				$published = JHtml::_('jgrid.published', $row->published, $i, '', 1);
+				$row        = $this->templates[$i];
+				$row->id    = $row->template_id;
+				$canCheckin = $this->user->authorise('core.manage',     'com_checkin') || $row->checked_out == $this->user->get('id') || $row->checked_out == 0;
+				$link       = JRoute::_('index.php?option=' . $option . '&view=template_detail&task=edit&cid[]=' . $row->template_id);
+				$published  = JHtml::_('jgrid.published', $row->published, $i, '', 1);
 
 				?>
 				<tr class="<?php echo "row$k"; ?>">
@@ -105,23 +103,19 @@ $redtemplate = new Redtemplate();
 						<?php echo $this->pagination->getRowOffset($i); ?>
 					</td>
 					<td align="center">
-						<?php echo @JHTML::_('grid.checkedout', $row, $i); ?>
+						<?php if ($row->checked_out) : ?>
+							<?php echo JHtml::_('jgrid.checkedout', $i, $row->editor, $row->checked_out_time); ?>
+						<?php else : ?>
+							<?php echo JHtml::_('grid.id', $i, $row->template_id); ?>
+						<?php endif; ?>
 					</td>
 					<td>
-						<?php
-
-						if (JTable::isCheckedOut($this->user->get('id'), $row->checked_out))
-						{
+						<?php if (!$canCheckin) : ?>
 							echo $row->template_name;
-						}
-						else
-						{
-							?>
+						<?php else : ?>
 							<a href="<?php echo $link; ?>"
 							   title="<?php echo JText::_('COM_REDSHOP_EDIT_TEMPLATES'); ?>"><?php echo $row->template_name; ?></a>
-						<?php
-						}
-						?>
+						<?php endif; ?>
 					</td>
 					<td>
 						<?php echo $redtemplate->getTemplateSections($row->template_section); ?>
