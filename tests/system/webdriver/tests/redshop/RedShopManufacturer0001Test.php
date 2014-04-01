9d0262aa9f35813a5ec3849b4622c55f11a61c3b
javier gomez <javier.gomez.corio@gmail.com>
2014-04-01 18:36:24 +0200

    Preparing redSHOP1 for selenium system testing

---
+++ tests/system/webdriver/tests/redshop/RedShopManufacturer0001Test.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package    RedCore
+ * @subpackage Model
+ * @copyright  Copyright (C) 2012 - 2014 redCOMPONENT.com. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+require_once 'JoomlaWebdriverTestCase.php';
+
+/**
+ * This class tests the  Manufacturer Add/Edit.
+ *
+ * @package     RedShop2.Test
+ * @subpackage  Webdriver
+ * @since       2.0
+ */
+class RedShopManufacturer0001Test extends JoomlaWebdriverTestCase
+{
+	// @Todo
+}
