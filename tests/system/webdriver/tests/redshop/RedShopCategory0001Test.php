71f0109a70fe27f1c36634ae8f184af443ab900d
Puneet Kala <puneet.0191@gmail.com>
2014-04-04 00:59:14 +0530

    Test Class File

--- tests/system/webdriver/tests/redshop/RedShopCategory0001Test.php
+++ tests/system/webdriver/tests/redshop/RedShopCategory0001Test.php
@@ -1,7 +1,69 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: punekala
- * Date: 4/4/14
- * Time: 12:02 AM
- */ 
+ * @package     RedCore
+ * @subpackage  Model
+ * @copyright   Copyright (C) 2012 - 2014 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+require_once 'JoomlaWebdriverTestCase.php';
+
+/**
+ * This class tests the  Category Add/Edit.
+ *
+ * @package     RedShop.Test
+ * @subpackage  Webdriver
+ * @since       1.2
+ */
+class RedShopCategory0001Test extends JoomlaWebdriverTestCase
+{
+	/**
+	 * The menu name being tested.
+	 *
+	 * @var     string
+	 * @since   3.0
+	 */
+	protected $appMenuLinkName = 'RedShopCategoriesManagerPage';
+
+	/**
+	 * The menu group name being tested.
+	 *
+	 * @var     string
+	 * @since   3.0
+	 */
+	protected $appMenuGroupName = 'RedSHOP_Category';
+
+	/**
+	 * Function to login to the Application
+	 *
+	 * @return void
+	 */
+	public function setUp()
+	{
+		parent::setUp();
+		$cpPage = $this->doAdminLogin();
+		$this->appTestPage = $cpPage->clickMenu($this->appMenuGroupName, $this->appMenuLinkName);
+	}
+
+	/**
+	 * Logout and close test.
+	 *
+	 * @return void
+	 *
+	 * @since   3.0
+	 */
+	public function tearDown()
+	{
+		$this->doAdminLogout();
+		parent::tearDown();
+	}
+
+	public function createCategory()
+	{
+		$rand = rand();
+		$name = 'Red Category' . $rand;
+		$noOfProducts = rand(10, 99);
+		$this->appTestPage->addCategory($name, $noOfProducts);
+		$this->assertTrue($this->appTestPage->searchCategory($name), 'Category Must be Created');
+	}
+}
