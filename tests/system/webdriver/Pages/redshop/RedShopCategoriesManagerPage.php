d05a58c7d43b76d6b76141ae2cbf2f36c4f8155a
Puneet Kala <puneet.0191@gmail.com>
2014-04-03 22:31:38 +0530

    Function to Search a Category

--- tests/system/webdriver/Pages/redshop/RedShopCategoriesManagerPage.php
+++ tests/system/webdriver/Pages/redshop/RedShopCategoriesManagerPage.php
@@ -122,4 +122,31 @@ class RedShopCategoriesManagerPage extends AdminManagerPage
 		$elementObject->findElement(By::xPath("//input[@id='cb" . $row . "']"))->click();
 		$elementObject->findElement(By::xPath("//li[@id='toolbar-delete']/a"))->click();
 	}
+
+	public function searchCategory($categoryName, $functionName = 'Search')
+	{
+		$elementObject = $this->driver;
+		$searchField = $elementObject->findElement(By::xPath("//input[@id='category_main_filter']"));
+		$searchField->clear();
+		$searchField->sendKeys($categoryName);
+		$elementObject->findElement(By::xPath("//button[@onclick=\"document.adminForm.submit();\"]"))->click();
+		sleep(5);
+		$row = $this->getRowNumber($categoryName) - 1;
+
+		if ($functionName == 'Search')
+		{
+			$elementObject->waitForElementUntilIsPresent(By::xPath("//input[@id='cb" . $row . "']"), 10);
+		}
+
+		$arrayElement = $elementObject->findElements(By::xPath("//tbody/tr/td[3]/a[contains(text(),'" . $categoryName . "')]"));
+
+		if (count($arrayElement))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 }
