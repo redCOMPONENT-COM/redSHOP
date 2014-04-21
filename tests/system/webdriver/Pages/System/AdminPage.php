6794f81aabf7dd667798c918bd37922f7b27d966
Puneet Kala <puneet.0191@gmail.com>
2014-04-21 19:35:14 +0530

    Adding Text Library to avoid Conflicts

--- tests/system/webdriver/Pages/System/AdminPage.php
+++ tests/system/webdriver/Pages/System/AdminPage.php
@@ -116,6 +116,7 @@ abstract class AdminPage
 		'RedSHOP_Country'  => 'administrator/index.php?option=com_redshop&view=country',
 		'RedSHOP_Wrapper'  => 'administrator/index.php?option=com_redshop&view=wrapper',
 		'RedSHOP_GiftCard' => 'administrator/index.php?option=com_redshop&view=giftcard',
+		'RedSHOP_TextLibrary' => 'administrator/index.php?option=com_redshop&view=textlibrary',
 	);
 
 	/**
