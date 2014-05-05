b1dcd19ff0ef93e923ccc1ff63f8a6a1979ef173
Puneet Kala <puneet.0191@gmail.com>
2014-05-05 21:08:18 +0530

    REDSHOP-1325 Test Class File

---
+++ tests/system/webdriver/tests/redshop/RedShopUser0001Test.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @package     RedShop
+ * @subpackage  Test
+ * @copyright   Copyright (C) 2012 - 2014 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+require_once 'JoomlaWebdriverTestCase.php';
+
+/**
+ * This class tests the Users Add/Edit.
+ *
+ * @package     RedShop.Test
+ * @subpackage  Webdriver
+ * @since       2.0
+ */
+class RedShopUser0001Test extends JoomlaWebdriverTestCase
+{
+	/**
+	 * The menu name being tested.
+	 *
+	 * @var     string
+	 * @since   3.0
+	 */
+	protected $appMenuLinkName = 'RedShopUsersManagerPage';
+
+	/**
+	 * The menu group name being tested.
+	 *
+	 * @var     string
+	 * @since   3.0
+	 */
+	protected $appMenuGroupName = 'RedSHOP_User';
+
+	/**
+	 * Function to login to the Application
+	 *
+	 * @return void
+	 */
+	public function setUp()
+	{
+		parent::setUp();
+		$cpPage = $this->doAdminLogin();
+		$this->appTestPage = $cpPage->clickMenu($this->appMenuGroupName, $this->appMenuLinkName);
+	}
+
+	/**
+	 * Logout and close test.
+	 *
+	 * @return void
+	 *
+	 * @since   3.0
+	 */
+	public function tearDown()
+	{
+		$this->doAdminLogout();
+		parent::tearDown();
+	}
+}
