3bef18e694e356e6bcc17054c66e25be95401359
Puneet Kala <puneet.0191@gmail.com>
2014-05-07 20:16:58 +0530

    REDSHOP-1338 Updating file

--- tests/system/webdriver/tests/Report/index.php
+++ tests/system/webdriver/tests/Report/index.php
@@ -5,34 +5,38 @@
  * @copyright   Copyright (C) 2012 - 2014 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
+?>
 
-echo "<html lang=\"en\">
-		<head>
-		<meta charset=\"utf-8\">
-		<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
-		<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
-		<link rel=\"stylesheet\" href=\"//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css\">
-		<link rel=\"stylesheet\" href=\"//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css\">
-		<script src=\"//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js\"></script>
-		<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js\"></script>
-		</html>";
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1\">
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	</head>
+</html>
+<?php
 $attributes = array("name", "tests", "assertions", "failures", "errors");
 
 if (file_exists('../logs/junit.xml'))
 {
 	$xml = simplexml_load_file('../logs/junit.xml');
-	echo "<table class=\"table table-striped\">";
-	echo "<thead>";
-	echo "<tr>";
-	echo "<th>Name</th>";
-	echo "<th>Tests</th>";
-	echo "<th>Assertions</th>";
-	echo "<th>Failures</th>";
-	echo "<th>Errors</th>";
-	echo "</tr>";
-	echo "</thead>";
-	echo "<tbody>";
-
+?>
+	<table class="table table-striped">
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Tests</th>
+				<th>Assertions</th>
+				<th>Failures</th>
+				<th>Errors</th>
+			</tr>
+		</thead>
+		<tbody>
+<?php
 	if (isset($xml->testsuite[0]->testsuite[0]))
 	{
 		$count = $xml->testsuite[0]->children()->count();
@@ -79,13 +83,10 @@ if (file_exists('../logs/junit.xml'))
 }
 else
 {
-	echo "<h1>";
-	echo "Please execute System Test! to view the report";
-	echo "</h1>";
-	echo "<h4>";
-	echo "For more details about how to run system tests please read here";
-	echo "<a href=\"https://redweb.atlassian.net/wiki/display/RSBTB/2013/12/06/Running+System+Tests%2C+Automation+Testing+for+RedshopB2B\">";
-	echo " System Test";
-	echo "</a>";
-	echo "</h2>";
+?>
+	<h1>Please execute System Test! to view the report</h1>
+	<h2>For more details about how to run system tests please read here
+		<a href="https://redweb.atlassian.net/wiki/display/RSBTB/2013/12/06/Running+System+Tests%2C+Automation+Testing+for+RedshopB2B"> System Test</a>
+	</h2>
+<?php
 }
