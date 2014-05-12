511a9e4aa475d9aa80f47c54f1d5ad6f5daaf309
Puneet Kala <puneet.0191@gmail.com>
2014-05-12 22:43:41 +0530

    REDSHOP-1318 Functions to test Currency

--- tests/system/webdriver/tests/redshop/RedShopCurrency0001Test.php
+++ tests/system/webdriver/tests/redshop/RedShopCurrency0001Test.php
@@ -57,4 +57,43 @@ class RedShopCurrency0001Test extends JoomlaWebdriverTestCase
 		$this->doAdminLogout();
 		parent::tearDown();
 	}
+
+	/**
+	 * Function to Add a new Currency, test the CRUD Part
+	 *
+	 * @test
+	 *
+	 * @return void
+	 */
+	public function createCurrency()
+	{
+		$rand = rand();
+		$name = 'RedShopCurrency' . $rand;
+		$code = $rand;
+		$this->appTestPage->addCurrency($name, $code);
+		$this->assertTrue($this->appTestPage->searchCurrency($name), 'Currency Must be Created');
+		$this->appTestPage->deleteCurrency($name);
+		$this->assertFalse($this->appTestPage->searchCurrency($name, 'Delete'), 'Currency Must be Deleted');
+	}
+
+	/**
+	 * Function to test the Edit feature of Currency Add/Edit Screen
+	 *
+	 * @test
+	 *
+	 * @return void
+	 */
+	public function updateCurrency()
+	{
+		$rand = rand();
+		$name = 'RedShopCurrency' . $rand;
+		$newName = 'NewCurrency' . $rand;
+		$code = $rand;
+		$this->appTestPage->addCurrency($name, $code);
+		$this->assertTrue($this->appTestPage->searchCurrency($name), 'Currency Must be Created');
+		$this->appTestPage->editCurrency('Name', $newName, $name);
+		$this->assertTrue($this->appTestPage->searchCurrency($newName), 'Currency Must be Updated');
+		$this->appTestPage->deleteCurrency($newName);
+		$this->assertFalse($this->appTestPage->searchCurrency($newName, 'Delete'), 'Currency Must be Deleted');
+	}
 }
