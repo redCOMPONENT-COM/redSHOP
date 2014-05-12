287c23df63afa778a2e9e4c240adbc5b8c4d7a6f
Puneet Kala <puneet.0191@gmail.com>
2014-05-12 22:27:10 +0530

    REDSHOP-1318 Currency Test Class

---
+++ tests/system/webdriver/tests/redshop/RedShopCurrency0001Test.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @package     RedShop
+ * @subpackage  Test
+ * @copyright   Copyright (C) 2012 - 2014 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+require_once 'JoomlaWebdriverTestCase.php';
+
+/**
+ * This class tests the  Currency Add/Edit.
+ *
+ * @package     RedShop.Test
+ * @subpackage  Webdriver
+ * @since       1.2
+ */
+class RedShopCurrency0001Test extends JoomlaWebdriverTestCase
+{
+	/**
+	 * The menu name being tested.
+	 *
+	 * @var     string
+	 * @since   3.0
+	 */
+	protected $appMenuLinkName = 'RedShopCurrenciesManagerPage';
+
+	/**
+	 * The menu group name being tested.
+	 *
+	 * @var     string
+	 * @since   3.0
+	 */
+	protected $appMenuGroupName = 'RedSHOP_Currency';
+
+	/**
+	 * Function to login to the Application
+	 *
+	 * @return void
+	 */
+	public function setUp()
+	{
+		parent::setUp();
+		$cpPage = $this->doAdminLogin();
+		$this->appTestPage = $cpPage->clickMenu($this->appMenuGroupName, $this->appMenuLinkName);
+	}
+
+	/**
+	 * Logout and close test.
+	 *
+	 * @return void
+	 *
+	 * @since   3.0
+	 */
+	public function tearDown()
+	{
+		$this->doAdminLogout();
+		parent::tearDown();
+	}
+}
