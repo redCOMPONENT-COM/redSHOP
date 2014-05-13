a30a3d2b7e3e07ef494e03ac976ffc8c76644a64
Puneet Kala <puneet.0191@gmail.com>
2014-05-13 20:24:39 +0530

    REDSHOP-1321 States Manager Page

---
+++ tests/system/webdriver/Pages/redshop/RedShopStatesManagerPage.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @package     RedSHOP
+ * @subpackage  Page
+ * @copyright   Copyright (C) 2012 - 2014 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+use SeleniumClient\By;
+use SeleniumClient\SelectElement;
+use SeleniumClient\WebDriver;
+use SeleniumClient\WebDriverWait;
+use SeleniumClient\DesiredCapabilities;
+use SeleniumClient\WebElement;
+
+/**
+ * Page class for the back-end States Redshop.
+ *
+ * @package     RedShop.Test
+ * @subpackage  Webdriver
+ * @since       1.0
+ */
+class RedShopStatesManagerPage extends AdminManagerPage
+{
+	/**
+	 * XPath string used to uniquely identify this page
+	 *
+	 * @var    string
+	 *
+	 * @since    1.0
+	 */
+	protected $waitForXpath = "//h2[contains(text(),'States')]";
+
+	/**
+	 * URL used to uniquely identify this page
+	 *
+	 * @var    string
+	 * @since  3.0
+	 */
+	protected $url = 'administrator/index.php?option=com_redshop&view=state';
+}
