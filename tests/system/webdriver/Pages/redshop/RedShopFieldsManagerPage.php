b66a48625902181da22bc1931982b4234581eab1
Puneet Kala <puneet.0191@gmail.com>
2014-05-19 20:58:40 +0530

    REDSHOP-1327 Adding Fields Manager Page

---
+++ tests/system/webdriver/Pages/redshop/RedShopFieldsManagerPage.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @package     RedSHOP
+ * @subpackage  Page
+ * @copyright   Copyright (C) 2012 - 2014 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+use SeleniumClient\By;
+use SeleniumClient\SelectElement;
+use SeleniumClient\WebDriver;
+use SeleniumClient\WebDriverWait;
+use SeleniumClient\DesiredCapabilities;
+use SeleniumClient\WebElement;
+
+/**
+ * Page class for the back-end Fields Redshop.
+ *
+ * @package     RedShop.Test
+ * @subpackage  Webdriver
+ * @since       1.0
+ */
+class RedShopFieldsManagerPage extends AdminManagerPage
+{
+	/**
+	 * XPath string used to uniquely identify this page
+	 *
+	 * @var    string
+	 *
+	 * @since    1.0
+	 */
+	protected $waitForXpath = "//h2[contains(text(),'Custom Field Management')]";
+
+	/**
+	 * URL used to uniquely identify this page
+	 *
+	 * @var    string
+	 * @since  3.0
+	 */
+	protected $url = 'administrator/index.php?option=com_redshop&view=fields';
+}
