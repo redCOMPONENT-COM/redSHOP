5d7958201e41dfdafceb36cd139392cab7d313d3
javier gomez <javier.gomez.corio@gmail.com>
2014-05-20 09:45:36 +0200

    create addManufacturer function

--- tests/system/webdriver/Pages/redshop/RedShopManufacturersManagerPage.php
+++ tests/system/webdriver/Pages/redshop/RedShopManufacturersManagerPage.php
@@ -21,5 +21,53 @@ use SeleniumClient\WebElement;
  */
 class RedShop2ManufacturersManagerPage extends AdminManagerPage
 {
-	// @Todo
+	/**
+	 * XPath string used to uniquely identify this page
+	 *
+	 * @var    string
+	 *
+	 * @since    1.0
+	 */
+	protected $waitForXpath = "//h2[contains(text(),'Manufacturer Management')]";
+
+	/**
+	 * URL used to uniquely identify this page
+	 *
+	 * @var    string
+	 * @since  3.0
+	 */
+	protected $url = 'administrator/index.php?option=com_redshop&view=manufacturer';
+
+	/**
+	 * Function to Add a Manufacturer
+	 *
+	 * @param   string  $name         Name of the Card
+	 * @param   string  $price        Price of the card
+	 * @param   string  $value        Value of the Card
+	 * @param   string  $validity     Validity of the Card
+	 * @param   string  $description  Description of the Card
+	 *
+	 * @return RedShopManufacturerManagerPage
+	 */
+	public function addManufacturer($name = 'Sample Manufacturer', $price = '100', $value = '10', $validity = '10')
+	{
+		$elementObject = $this->driver;
+		$elementObject->findElement(By::xPath("//a[@onclick=\"Joomla.submitbutton('add')\"]"))->click();
+		$elementObject->waitForElementUntilIsPresent(By::xPath("//input[@id='giftcard_name']"));
+		$nameField = $elementObject->findElement(By::xPath("//input[@id='giftcard_name']"));
+		$nameField->clear();
+		$nameField->sendKeys($name);
+		$priceField = $elementObject->findElement(By::xPath("//input[@id='giftcard_price']"));
+		$priceField->clear();
+		$priceField->sendKeys($price);
+		$valueField = $elementObject->findElement(By::xPath("//input[@id='giftcard_value']"));
+		$valueField->clear();
+		$valueField->sendKeys($value);
+		$validityField = $elementObject->findElement(By::xPath("//input[@id='giftcard_validity']"));
+		$validityField->clear();
+		$validityField->sendKeys($validity);
+		$elementObject->findElement(By::xPath("//a[@onclick=\"Joomla.submitbutton('save')\"]"))->click();
+		$elementObject->waitForElementUntilIsPresent(By::xPath("//li[contains(text(),'Gift Card Saved')]"), 10);
+	}
+
 }
