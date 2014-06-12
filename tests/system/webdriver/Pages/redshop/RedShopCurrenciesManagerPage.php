7394d165ccc62d46fa9e43fc25d5a82dd2a3c3d7
Puneet Kala <puneet.0191@gmail.com>
2014-06-12 23:36:01 +0530

    REDSHOP-1365 Checking Notices in Edit View

--- tests/system/webdriver/Pages/redshop/RedShopCurrenciesManagerPage.php
+++ tests/system/webdriver/Pages/redshop/RedShopCurrenciesManagerPage.php
@@ -50,6 +50,7 @@ class RedShopCurrenciesManagerPage extends AdminManagerPage
 	{
 		$elementObject = $this->driver;
 		$elementObject->findElement(By::xPath("//a[@onclick=\"Joomla.submitbutton('add')\"]"))->click();
+		$this->checkNoticesForEditView(get_class($this));
 		$elementObject->waitForElementUntilIsPresent(By::xPath("//input[@id='currency_name']"));
 		$nameField = $elementObject->findElement(By::xPath("//input[@id='currency_name']"));
 		$nameField->clear();
@@ -79,6 +80,7 @@ class RedShopCurrenciesManagerPage extends AdminManagerPage
 		$elementObject->waitForElementUntilIsPresent(By::xPath("//input[@id='cb" . $row . "']"), 10);
 		$elementObject->findElement(By::xPath("//input[@id='cb" . $row . "']"))->click();
 		$elementObject->findElement(By::xPath("//li[@id='toolbar-edit']/a"))->click();
+		$this->checkNoticesForEditView(get_class($this));
 		$elementObject->waitForElementUntilIsPresent(By::xPath("//input[@id='currency_name']"));
 
 		switch ($field)
