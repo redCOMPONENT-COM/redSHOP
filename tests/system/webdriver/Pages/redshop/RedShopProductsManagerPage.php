682c5072e84e589a9cb4e6824c15914ea19d9f7d
Puneet Kala <puneet.0191@gmail.com>
2014-06-16 19:11:25 +0530

    REDSHOP-1420 Page Class for Products Manager

---
+++ tests/system/webdriver/Pages/redshop/RedShopProductsManagerPage.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @package     RedSHOP
+ * @subpackage  Page
+ * @copyright   Copyright (C) 2012 - 2014 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+use SeleniumClient\By;
+use SeleniumClient\SelectElement;
+use SeleniumClient\WebDriver;
+use SeleniumClient\WebDriverWait;
+use SeleniumClient\DesiredCapabilities;
+use SeleniumClient\WebElement;
+
+/**
+ * Page class for the back-end Products Redshop.
+ *
+ * @package     RedShop.Test
+ * @subpackage  Webdriver
+ * @since       1.0
+ */
+class RedShopProductsManagerPage extends AdminManagerPage
+{
+	/**
+	 * XPath string used to uniquely identify this page
+	 *
+	 * @var    string
+	 *
+	 * @since    1.0
+	 */
+	protected $waitForXpath = "//h2[text() = 'Product Management']";
+
+	/**
+	 * URL used to uniquely identify this page
+	 *
+	 * @var    string
+	 * @since  3.0
+	 */
+	protected $url = 'administrator/index.php?option=com_redshop&view=product';
+
+	public function addProduct($name = 'Sample', $number = '123455', $price = '10', $category = 'redCOMPONENT')
+	{
+		$elementObject = $this->driver;
+		$elementObject->findElement(By::xPath("//a[@onclick=\"Joomla.submitbutton('add')\"]"))->click();
+		$this->checkNoticesForEditView(get_class($this));
+		$elementObject->waitForElementUntilIsPresent(By::xPath("//input[@id='product_name']"));
+		$nameField = $elementObject->findElement(By::xPath("//input[@id='product_name']"));
+		$nameField->clear();
+		$nameField->sendKeys($name);
+		$numberField = $elementObject->findElement(By::xPath("//input[@id='product_number']"));
+		$numberField->clear();
+		$numberField->sendKeys($number);
+		$priceField = $elementObject->findElement(By::xPath("//input[@id='product_price']"));
+		$priceField->clear();
+		$priceField->sendKeys($price);
+		$elementObject->findElement(By::xPath("//select[@id='product_category']//option[contains(text(),'" . $category . "')]"))->click();
+		sleep(2);
+		$elementObject->findElement(By::xPath("//a[@onclick=\"Joomla.submitbutton('save')\"]"))->click();
+		$elementObject->waitForElementUntilIsPresent(By::xPath("//li[text() = 'Product details saved']"), 10);
+	}
+}
