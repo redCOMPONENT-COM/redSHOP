1c6a657687a5aa76784052424a9226c2307d846c
Vipula Patel <vipula@redweb.dk>
2014-07-16 16:15:40 +0530

    AT-182 decimal option for piece added

--- component/admin/sql/updates/mysql/1.4.sql
+++ component/admin/sql/updates/mysql/1.4.sql
@@ -1,3 +1,5 @@
 ALTER TABLE `#__redshop_quotation`
 	ADD `quotation_customer_note` TEXT NOT NULL
 	AFTER `quotation_note`;
+ALTER TABLE `#__redshop_product`
+	ADD `allow_decimal_piece` TEXT NOT NULL;
