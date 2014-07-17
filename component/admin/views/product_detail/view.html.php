aa56ec7e4deabe121ad85b11d1d0d6063a8db062
clinchergt <jacob@redcomponent.com>
2014-07-17 12:56:54 -0600

    REDSHOP-1451 Fixed. The TOP category will be no longer shown

--- component/admin/views/product_detail/view.html.php
+++ component/admin/views/product_detail/view.html.php
@@ -423,7 +423,7 @@ class Product_DetailViewProduct_Detail extends JView
 			'class="inputbox" size="1"  ', 'value', 'text', $detail->product_tax_id
 		);
 
-		$categories = $product_category->list_all("product_category[]", 0, $productcats, 10, true, true);
+		$categories = $product_category->list_all("product_category[]", 0, $productcats, 10, false, true);
 		$lists['categories'] = $categories;
 		$detail->first_selected_category_id = isset($productcats[0]) ? $productcats[0] : null;
 
