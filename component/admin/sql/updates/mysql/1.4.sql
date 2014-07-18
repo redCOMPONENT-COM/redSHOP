9748dc5dbed617cfdbc380be967d8ea86b01acf0
Vipula Patel <vipula@redweb.dk>
2014-07-18 12:34:48 +0530

    Changed field data type

--- component/admin/sql/updates/mysql/1.4.sql
+++ component/admin/sql/updates/mysql/1.4.sql
@@ -2,4 +2,4 @@ ALTER TABLE `#__redshop_quotation`
 	ADD `quotation_customer_note` TEXT NOT NULL
 	AFTER `quotation_note`;
 ALTER TABLE `#__redshop_product`
-	ADD `allow_decimal_piece` TEXT NOT NULL;
+	ADD `allow_decimal_piece` int(4) NOT NULL;
