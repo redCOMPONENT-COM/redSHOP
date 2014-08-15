25123215225aafdcf65f9798b87c94ee3f179704
javier gomez <javier.gomez.corio@gmail.com>
2014-08-15 17:43:17 +0200

    Document Categories test

--- tests/acceptance/Category0001Cept.php
+++ tests/acceptance/Category0001Cept.php
@@ -10,16 +10,24 @@ $I->wantTo('Want to Test Category Manager');
 $I->doAdminLogin("Function to Login to Admin Panel");
 $I = new AcceptanceTester\CategoryManagerSteps($scenario);
 $randomCategoryName = 'Testing Category ' . rand(99, 999);
-$updatedCategoryName = 'New ' . $randomCategoryName;
+
+$I->wantTo('Create a Category');
 $I->addCategory($randomCategoryName);
 $I->see($randomCategoryName);
+
+$I->wantTo('Rename an existing Category');
+$updatedCategoryName = 'New ' . $randomCategoryName;
 $I->searchCategory($randomCategoryName);
 $I->updateCategory($randomCategoryName, $updatedCategoryName);
 $I->searchCategory($updatedCategoryName);
 $I->see($updatedCategoryName);
+
+$I->wantTo('Change the status of an existing Category');
 $I->changeState($updatedCategoryName, 'unpublish');
 $currentState = $I->getState($updatedCategoryName);
 $I->verifyState('unpublished', $currentState);
+
+$I->wantTo('Delete an existing Category');
 $I->deleteCategory($updatedCategoryName);
 $I->searchCategory($updatedCategoryName, 'Delete');
 $I->dontSee($updatedCategoryName);
