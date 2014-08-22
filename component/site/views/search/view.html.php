ee78bd6b745188ff3cde5a134d998eb47209cf35
clinchergt <jacobo@redcomponent.com>
2014-08-22 14:37:32 -0600

    REDSHOP-1482 Fixed Manufacture id fetching. It was fetching the manufacture_id from the first result from the query...

--- component/site/views/search/view.html.php
+++ component/site/views/search/view.html.php
@@ -152,12 +152,12 @@ class RedshopViewSearch extends JView
 			$texts            = new text_library;
 			$stockroomhelper  = new rsstockroomhelper;
 
-			$Itemid      = JRequest::getInt('Itemid');
-			$search_type = JRequest::getCmd('search_type');
-			$cid         = JRequest::getInt('category_id');
+			$Itemid         = JRequest::getInt('Itemid');
+			$search_type    = JRequest::getCmd('search_type');
+			$cid            = JRequest::getInt('category_id');
+			$manufacture_id = JRequest::getInt('manufacture_id');
 
 			$manisrch       = $this->search;
-			$manufacture_id = $manisrch[0]->manufacturer_id;
 			$templateid     = JRequest::getInt('templateid');
 
 			// Cmd removes space between to words
