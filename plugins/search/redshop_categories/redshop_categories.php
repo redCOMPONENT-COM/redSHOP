97e53149f4ecedbd914915aabef61d1b82e6d098
Gunjan Patel <gunjan@redcomponent.com>
2014-08-28 14:35:33 +0530

    Add @shandak changes back to file

--- plugins/search/redshop_categories/redshop_categories.php
+++ plugins/search/redshop_categories/redshop_categories.php
@@ -11,7 +11,9 @@ defined('_JEXEC') or die;
 
 jimport('joomlplugin.plugin');
 
-require_once JPATH_ROOT . '/components/com_redshop/helpers/helper.php';
+JLoader::import('loadhelpers', JPATH_SITE . '/components/com_redshop');
+JLoader::load('RedshopHelperHelper');
+
 
 class plgSearchRedshop_categories extends JPlugin
 {
