4da5b9550315b96d335a93819fed56917d8f563e
Puneet Kala <puneet.0191@gmail.com>
2014-09-09 19:19:44 +0530

    Adding Media Files

---
+++ tests/acceptance/ManageMediasAdministratorCept.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package     RedShop
+ * @subpackage  Cept
+ * @copyright   Copyright (C) 2012 - 2014 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// Load the Step Object Page
+$I = new AcceptanceTester\LoginSteps($scenario);
+
+$I->wantTo('Want to Test Media Manager');
+$I->doAdminLogin();
+$I = new AcceptanceTester\MediaManagerSteps($scenario);
+$I->addMedia();
