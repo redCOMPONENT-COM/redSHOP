2f5c0be2655540b381473ffd9fa769f14724c849
Puneet Kala <puneet.0191@gmail.com>
2014-09-15 21:47:55 +0530

    Updating Language

--- tests/acceptance/ManageAdministratorNoticesCept.php
+++ tests/acceptance/ManageAdministratorNoticesCept.php
@@ -9,7 +9,7 @@
 // Load the Step Object Page
 $I = new AcceptanceTester\LoginSteps($scenario);
 
-$I->wantTo('Want to Test Presence of Notices, Warnings on Administrator Modules');
+$I->wantTo('Test Presence of Notices, Warnings on Administrator Modules');
 $I->doAdminLogin();
 $I = new AcceptanceTester\AdminManagerSteps($scenario);
 $I->CheckAllLinks();
