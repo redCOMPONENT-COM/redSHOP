1e719b745c121ab42316332988f6f43a962e3f65
Puneet Kala <puneet.0191@gmail.com>
2014-09-15 21:23:55 +0530

    Check For Notices in All MVC's

---
+++ tests/_pages/AdminManagerPage.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @package     RedShop
+ * @subpackage  Page Class
+ * @copyright   Copyright (C) 2012 - 2014 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+/**
+ * Class AdminManagerPage
+ *
+ * @link   http://codeception.com/docs/07-AdvancedUsage#PageObjects
+ *
+ * @since  1.4
+ */
+class AdminManagerPage
+{
+	public static $allLinks = array (
+		array('Category Manager', '/administrator/index.php?option=com_redshop&view=category'),
+		array('Country Manager', '/administrator/index.php?option=com_redshop&view=country'),
+		array('Product Manager', '/administrator/index.php?option=com_redshop&view=product'),
+		array('Manufacturer Manager', '/administrator/index.php?option=com_redshop&view=manufacturer'),
+		array('Media Manager', '/administrator/index.php?option=com_redshop&view=media'),
+		array('Order Manager', '/administrator/index.php?option=com_redshop&view=order'),
+		array('Quotation Manager', '/administrator/index.php?option=com_redshop&view=quotation'),
+		array('Discount Manager', '/administrator/index.php?option=com_redshop&view=discount'),
+		array('Gift Card Manager', '/administrator/index.php?option=com_redshop&view=giftcard'),
+		array('Voucher Manager', '/administrator/index.php?option=com_redshop&view=voucher'),
+		array('Coupon Manager', '/administrator/index.php?option=com_redshop&view=coupon'),
+		array('Mail  Manager', '/administrator/index.php?option=com_redshop&view=mail'),
+		array('News Letter Manager', '/administrator/index.php?option=com_redshop&view=newsletter'),
+		array('Shipping Box Manager', '/administrator/index.php?option=com_redshop&view=shipping_box'),
+		array('Wrapping  Manager', '/administrator/index.php?option=com_redshop&view=wrapper'),
+		array('User Manager', '/administrator/index.php?option=com_redshop&view=user'),
+		array('Vat/Tax Group Manager', '/administrator/index.php?option=com_redshop&view=tax_group'),
+		array('Currency Manager', '/administrator/index.php?option=com_redshop&view=currency'),
+		array('Country Manager', '/administrator/index.php?option=com_redshop&view=country'),
+		array('State Manager', '/administrator/index.php?option=com_redshop&view=state'),
+		array('Custom Field Manager', '/administrator/index.php?option=com_redshop&view=fields'),
+		array('Template Manager', '/administrator/index.php?option=com_redshop&view=template'),
+		array('Text Library Manager', '/administrator/index.php?option=com_redshop&view=textlibrary'),
+		array('Question Manager', '/administrator/index.php?option=com_redshop&view=question'),
+		array('Rating Manager', '/administrator/index.php?option=com_redshop&view=rating')
+	);
+}
