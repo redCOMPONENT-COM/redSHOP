35701dfcc63635be1223d9e4e0be8eaac20ec838
Puneet Kala <puneet.0191@gmail.com>
2014-09-16 20:44:22 +0530

    Updating the Message Displayed

--- tests/acceptance/ManageAdministratorNoticesCept.php
+++ tests/acceptance/ManageAdministratorNoticesCept.php
@@ -9,7 +9,7 @@
 // Load the Step Object Page
 $I = new AcceptanceTester\LoginSteps($scenario);
 
-$I->wantTo('Test Presence of Notices, Warnings on Administrator Modules');
+$I->wantTo('Test Presence of Notices, Warnings on Administrator');
 $I->doAdminLogin();
 $I = new AcceptanceTester\AdminManagerSteps($scenario);
 $I->CheckAllLinks();
