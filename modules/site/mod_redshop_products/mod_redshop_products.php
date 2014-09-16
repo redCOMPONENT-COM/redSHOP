bbdcffbb0c9b80207466ec776e91fc3ea8bb8542
Vladimir <vshandak@yahoo.com>
2014-09-16 15:36:47 +0300

    More beauty view

--- modules/site/mod_redshop_products/mod_redshop_products.php
+++ modules/site/mod_redshop_products/mod_redshop_products.php
@@ -9,12 +9,13 @@
 
 defined('_JEXEC') or die('Restricted access');
 
-
 if (!defined('MOD_REDSHOP_PRODUCTS'))
 {
-	// Get all category to set as default
-
-
+	/**
+	 * Get all category to set as default
+	 *
+	 * @return int|string
+	 */
 	function getDefaultModuleCategories()
 	{
 		$db = JFactory::getDbo();
@@ -53,30 +54,23 @@ if (!defined('MOD_REDSHOP_PRODUCTS'))
 $type = trim($params->get('type', 0));
 
 // Set all published category as default
-
-
 $cids     = getDefaultModuleCategories();
 $category = trim($params->get('category', ''));
 
 // Set product output limit
-
-
 $count = trim($params->get('count', 5));
 
 // Get show image yes/no option
-
-
 $image = trim($params->get('image', 0));
 
 // Get Vertical product yes/no option for horizontal and vertical product display
-
 $verticalProduct = trim($params->get('vertical_product', 0));
 
 // Get featured product yes/no option
-
 $featuredProduct = trim($params->get('featured_product', 0));
 
-$whereFeatured   = "";
+$whereFeatured = '';
+
 if ($featuredProduct)
 {
 	$whereFeatured = " AND p.product_special = 1 ";
@@ -85,8 +79,12 @@ if ($featuredProduct)
 // If enable, option will show price
 
 $showPrice  = trim($params->get('show_price', 0));
-$thumbwidth  = trim($params->get('thumbwidth', 100)); // get show image thumbwidth size
-$thumbheight = trim($params->get('thumbheight', 100)); // get show image thumbheight size
+
+// Get show image thumbwidth size
+$thumbwidth  = trim($params->get('thumbwidth', 100));
+
+// Get show image thumbheight size
+$thumbheight = trim($params->get('thumbheight', 100));
 
 $showShortDescription = trim($params->get('show_short_description', 1));
 
@@ -111,9 +109,9 @@ else
 	$mainChild = " AND p.product_parent_id=0";
 }
 
-
 $showStockroomStatus = trim($params->get('show_stockroom_status', 1));
-if ($category == "")
+
+if ($category == '')
 {
 	$category = $cids;
 }
@@ -121,7 +119,6 @@ if ($category == "")
 $db = JFactory::getDbo();
 
 // Getting the configuration
-
 require_once JPATH_ADMINISTRATOR . '/components/com_redshop/helpers/redshop.cfg.php';
 JLoader::import('loadhelpers', JPATH_SITE . '/components/com_redshop');
 JLoader::load('RedshopHelperAdminConfiguration');
@@ -129,13 +126,9 @@ $Redconfiguration = new Redconfiguration;
 $Redconfiguration->defineDynamicVars();
 
 JLoader::load('RedshopHelperProduct');
-
 JLoader::load('RedshopHelperHelper');
-
 JLoader::load('RedshopHelperAdminTemplate');
-
 JLoader::load('RedshopHelperAdminExtra_field');
-
 
 switch ($type)
 {
@@ -171,12 +164,9 @@ switch ($type)
 		$sql = "SELECT DISTINCT(p.product_id),p.*  FROM #__redshop_product p left outer join #__redshop_product_category_xref cx on cx.product_id = p.product_id  WHERE p.published=1 AND p.product_on_sale = 1 " . $whereFeatured . " " . $mainChild . " AND cx.category_id IN ($category) ORDER BY rand() LIMIT 0,$count";
 
 		break;
-
 }
 
 $db->setQuery($sql);
-
 $rows = $db->loadObjectList();
-
 
 require JModuleHelper::getLayoutPath('mod_redshop_products');
