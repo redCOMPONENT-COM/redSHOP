9d337c4fa2516f17fee7d2188b54d2ba0808ed67
Gunjan Patel <gunjan@redcomponent.com>
2014-09-23 15:57:00 +0530

    [add] Shopper Group Selection validation

--- component/admin/views/user_detail/tmpl/default.php
+++ component/admin/views/user_detail/tmpl/default.php
@@ -6,110 +6,119 @@
  * @copyright   Copyright (C) 2005 - 2013 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
-defined('_JEXEC') or die ('Restricted access');
+defined('_JEXEC') or die;
 
 JHTML::_('behavior.tooltip');
 jimport('joomla.html.pane');
 
-$this->producthelper = new producthelper();
-$this->order_functions = new order_functions();
-$this->config = new Redconfiguration();
+$this->producthelper   = new producthelper;
+$this->order_functions = new order_functions;
+$this->config          = new Redconfiguration;
+$this->model           = $this->getModel('user_detail');
+$this->flag            = JRequest::getVar('flag', '', 'request', 'string');
+$this->shipping        = JRequest::getVar('shipping', '', 'request', 'string');
+$cancel                = JRequest::getVar('cancel', '', 'cancel', 'string');
+$this->silerntuser     = ($this->detail->users_info_id) ? true : false;
 
-$this->model = $this->getModel('user_detail');
-$this->flag = JRequest::getVar('flag', '', 'request', 'string');
-$this->shipping = JRequest::getVar('shipping', '', 'request', 'string');
-$cancel = JRequest::getVar('cancel', '', 'cancel', 'string');
-
-$this->silerntuser = ($this->detail->users_info_id) ? true : false;
 if ($this->detail->users_info_id && $this->detail->user_id && $this->detail->username)
 {
 	$this->silerntuser = false;
 }
-# set selected tab index
-# 0 default
-# 2 for shipping
-# 3 for order
+
+/*
+ set selected tab index
+ 0 default
+ 2 for shipping
+ 3 for order
+ */
 $tab = 0;
+
 if ($cancel)
 {
 	$tab = 2;
 }
+
 if ($this->pagination->limitstart > 0)
 {
 	$tab = 3;
-}?>
-
+}
+?>
 <script language="javascript" type="text/javascript">
 	Joomla.submitbutton = function (pressbutton) {
 		var form = document.adminForm;
+
 		if (pressbutton == 'cancel') {
 			submitform(pressbutton);
 			return;
 		}
+
 		if (pressbutton == 'order') {
 			submitform(pressbutton);
 			return;
 		}
-		var r = new RegExp("[\<|\>|\"|\'|\%|\;|\(|\)|\&]", "i");
-		<?php
-		if ($this->shipping)
-		{	?>
-		if ((form.firstname.value) == "") {
-			alert("<?php echo JText::_('COM_REDSHOP_YOUR_MUST_PROVIDE_A_FIRSTNAME', true );?>");
-		} else if (form.lastname.value == "") {
-			alert("<?php echo JText::_('COM_REDSHOP_YOUR_MUST_PROVIDE_A_LASTTNAME', true );?>");
-		} else {
-			submitform(pressbutton);
-		}
-		<?php
-			}
-			else
-			{	?>
-		if ((form.email.value) == "") {
-			alert("<?php echo JText::_('COM_REDSHOP_PROVIDE_EMAIL_ADDRESS', true );?>");
-			return false;
-		}
-		else if (email_valid == 0) {
-			alert("<?php echo JText::_('COM_REDSHOP_EMAIL_NOT_AVAILABLE', true );?>");
-			return false;
-		}
-		<?php	if(!$this->silerntuser)
-				{	?>
-		if (form.username.value == "") {
-			alert("<?php echo JText::_('COM_REDSHOP_YOU_MUST_PROVIDE_LOGIN_NAME', true );?>");
-			return false;
-		} else if (r.exec(form.username.value) || form.username.value.length < 2) {
-			alert("<?php echo JText::_('COM_REDSHOP_WARNLOGININVALID', true );?>");
-			return false;
-		} else if (user_valid == 0) {
-			alert("<?php echo JText::_('COM_REDSHOP_USERNAME_NOT_AVAILABLE', true );?>");
-			return false;
 
-		} else if ((((form.password.value) != "") || (form.password2.value != "")) && (form.password.value != form.password2.value)) {
-			alert("<?php echo JText::_('COM_REDSHOP_PASSWORD_NOT_MATCH', true );?>");
-			return false;
-		}
-		<?php 	}	?>
-		var chks = document.getElementsByName('groups[]');
+		var r = new RegExp("[\<|\>|\"|\'|\%|\;|\(|\)|\&]", "i");
+
+		<?php if ($this->shipping) : ?>
+			if ((form.firstname.value) == "") {
+				alert("<?php echo JText::_('COM_REDSHOP_YOUR_MUST_PROVIDE_A_FIRSTNAME', true );?>");
+			} else if (form.lastname.value == "") {
+				alert("<?php echo JText::_('COM_REDSHOP_YOUR_MUST_PROVIDE_A_LASTTNAME', true );?>");
+			} else {
+				submitform(pressbutton);
+			}
+		<?php else: ?>
+			if ((form.email.value) == "") {
+				alert("<?php echo JText::_('COM_REDSHOP_PROVIDE_EMAIL_ADDRESS', true );?>");
+				return false;
+			}
+			else if (email_valid == 0) {
+				alert("<?php echo JText::_('COM_REDSHOP_EMAIL_NOT_AVAILABLE', true );?>");
+				return false;
+			}
+		<?php endif; ?>
+
+		<?php if(!$this->silerntuser) : ?>
+			if (form.username.value == "") {
+				alert("<?php echo JText::_('COM_REDSHOP_YOU_MUST_PROVIDE_LOGIN_NAME', true );?>");
+				return false;
+			} else if (r.exec(form.username.value) || form.username.value.length < 2) {
+				alert("<?php echo JText::_('COM_REDSHOP_WARNLOGININVALID', true );?>");
+				return false;
+			} else if (user_valid == 0) {
+				alert("<?php echo JText::_('COM_REDSHOP_USERNAME_NOT_AVAILABLE', true );?>");
+				return false;
+
+			} else if ((((form.password.value) != "") || (form.password2.value != "")) && (form.password.value != form.password2.value)) {
+				alert("<?php echo JText::_('COM_REDSHOP_PASSWORD_NOT_MATCH', true );?>");
+				return false;
+			}
+		<?php endif; ?>
+
+		var chks       = document.getElementsByName('groups[]');
 
 		if (chks.length)
 		{
 			var checkCount = 0;
 
-			for (var i = 0; i < chks.length; i++)
-			{
-				if (chks[i].checked)
-				{
+			for (var i = 0; i < chks.length; i++){
+				if (chks[i].checked){
 					checkCount++;
 				}
 			}
 
-			if (checkCount == 0) {
+			if (checkCount == 0){
 				alert("<?php echo JText::_('COM_REDSHOP_SELECT_USER_GROUP', true );?>");
 				return false;
 			}
 		}
 
+		// Added Rule for shopper group
+		if (form.shopper_group_id.value == 0)
+		{
+			alert("<?php echo JText::_('COM_REDSHOP_SELECT_SHOPPER_GROUP', true );?>");
+			return false;
+		}
 
 		if (form.firstname.value == "") {
 			alert("<?php echo JText::_('COM_REDSHOP_PLEASE_ENTER_FIRST_NAME', true );?>");
@@ -119,38 +128,31 @@ if ($this->pagination->limitstart > 0)
 			alert("<?php echo JText::_('COM_REDSHOP_PLEASE_ENTER_LAST_NAME', true );?>");
 			return false;
 		}
+
 		if (document.getElementById('is_company1').checked) {
 			if (form.company_name.value == "") {
 				alert("<?php echo JText::_('COM_REDSHOP_PLEASE_ENTER_COMPANY_NAME', true );?>");
 				return false;
 			}
-			/*if (form.ean_number.value == "")
-			 {
-			 alert( "
-			<?php echo JText::_('COM_REDSHOP_PLEASE_ENTER_EAN_NUMBER', true );?>" );
-			 return false;
-			 }*/
 		}
 
 		submitform(pressbutton);
-		<?php
-			 }	?>
 	}
 </script>
 
 <form action="<?php echo JRoute::_($this->request_url) ?>" method="post" name="adminForm" id="adminForm">
-	<?php
+<?php
 	$myTabs = JPane::getInstance('tabs', array('startOffset' => $tab));
 
-	//Create Pane
+	// Create Pane
 	echo $myTabs->startPane('pane');
+
 	if (!$this->shipping)
 	{
 		// Create 1st Tab
 		echo $myTabs->startPanel(JText::_('COM_REDSHOP_GENERAL_USER_INFO'), 'tab1');
 		echo $this->loadTemplate('user');
 		echo $myTabs->endPanel();
-		//Create 2nd Tab
 	}
 
 	$title = ($this->shipping == 1) ? JText::_('COM_REDSHOP_SHIPPING_INFORMATION') : JText::_('COM_REDSHOP_BILLING_INFORMATION');
@@ -166,6 +168,7 @@ if ($this->pagination->limitstart > 0)
 	}
 
 	$this->userorders = $this->model->userOrders();
+
 	if ($this->detail->user_id && count($this->userorders) > 0)
 	{
 		echo $myTabs->startPanel(JText::_('COM_REDSHOP_ORDER_INFORMATION'), 'tab4');
@@ -197,9 +200,11 @@ if ($this->pagination->limitstart > 0)
 	else
 	{
 		echo '<input type="hidden" name="address_type" id="address_type" value="BT"  />';
-	}    ?>
+	}
+?>
 	<div class="clr"></div>
 	<input type="hidden" name="user_id" value="<?php echo $this->detail->user_id; ?>"/>
 	<input type="hidden" name="users_info_id" value="<?php echo $this->detail->users_info_id; ?>"/>
 	<input type="hidden" name="task" value=""/>
-	<input type="hidden" name="view" value="user_detail"/></form>
+	<input type="hidden" name="view" value="user_detail"/>
+</form>
