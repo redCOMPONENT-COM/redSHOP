8e29f11e11cb112dab234d74205990b8f88e6557
Luis <luis@redweb.dk>
2014-10-22 22:44:07 +0200

    - add old array back :)

--- component/admin/helpers/shipping.php
+++ component/admin/helpers/shipping.php
@@ -1121,7 +1121,7 @@ class shipping
 
 		if (!$shippingrate)
 		{
-			$numbers = array_merge(range('1', '9'), array(0), range('a', 'z'), range('A', 'Z'), array(" "));
+			$numbers = array("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " ");
 
 			$zipCond = "";
 			$zip     = trim($zip);
