282e10e631a7a4d2ee48b2af976435426443fc80
Gunjan Patel <gunjan@redcomponent.com>
2014-11-26 12:49:32 +0530

    REDSHOP-1700 #comment Fix published and favoured button not working issue in review backend

--- component/admin/controllers/rating.php
+++ component/admin/controllers/rating.php
@@ -17,4 +17,92 @@ class RedshopControllerRating extends JController
 	{
 		$this->setRedirect('index.php');
 	}
+
+	public function publish()
+	{
+		$option = JRequest::getVar('option');
+
+		$cid = JRequest::getVar('cid', array(0), 'post', 'array');
+
+		if (!is_array($cid) || count($cid) < 1)
+		{
+			JError::raiseError(500, JText::_('COM_REDSHOP_SELECT_AN_ITEM_TO_PUBLISH'));
+		}
+
+		$model = $this->getModel('rating_detail');
+
+		if (!$model->publish($cid, 1))
+		{
+			echo "<script> alert('" . $model->getError(true) . "'); window.history.go(-1); </script>\n";
+		}
+
+		$msg = JText::_('COM_REDSHOP_RATING_DETAIL_PUBLISHED_SUCCESFULLY');
+		$this->setRedirect('index.php?option=' . $option . '&view=rating', $msg);
+	}
+
+	public function unpublish()
+	{
+		$option = JRequest::getVar('option');
+
+		$cid = JRequest::getVar('cid', array(0), 'post', 'array');
+
+		if (!is_array($cid) || count($cid) < 1)
+		{
+			JError::raiseError(500, JText::_('COM_REDSHOP_SELECT_AN_ITEM_TO_UNPUBLISH'));
+		}
+
+		$model = $this->getModel('rating_detail');
+
+		if (!$model->publish($cid, 0))
+		{
+			echo "<script> alert('" . $model->getError(true) . "'); window.history.go(-1); </script>\n";
+		}
+
+		$msg = JText::_('COM_REDSHOP_RATING_DETAIL_UNPUBLISHED_SUCCESFULLY');
+		$this->setRedirect('index.php?option=' . $option . '&view=rating', $msg);
+	}
+
+	public function fv_publish()
+	{
+		$option = JRequest::getVar('option');
+
+		$cid = JRequest::getVar('cid', array(0), 'post', 'array');
+
+		if (!is_array($cid) || count($cid) < 1)
+		{
+			JError::raiseError(500, JText::_('COM_REDSHOP_SELECT_AN_ITEM_TO_PUBLISH'));
+		}
+
+		$model = $this->getModel('rating_detail');
+
+		if (!$model->favoured($cid, 1))
+		{
+			echo "<script> alert('" . $model->getError(true) . "'); window.history.go(-1); </script>\n";
+		}
+
+		$msg = JText::_('COM_REDSHOP_RATING_DETAIL_PUBLISHED_SUCCESFULLY');
+		$this->setRedirect('index.php?option=' . $option . '&view=rating', $msg);
+	}
+
+	public function fv_unpublish()
+	{
+		$option = JRequest::getVar('option');
+
+		$cid = JRequest::getVar('cid', array(0), 'post', 'array');
+
+		if (!is_array($cid) || count($cid) < 1)
+		{
+			JError::raiseError(500, JText::_('COM_REDSHOP_SELECT_AN_ITEM_TO_UNPUBLISH'));
+		}
+
+		$model = $this->getModel('rating_detail');
+
+		if (!$model->favoured($cid, 0))
+		{
+			echo "<script> alert('" . $model->getError(true) . "'); window.history.go(-1); </script>\n";
+		}
+
+		$msg = JText::_('COM_REDSHOP_RATING_DETAIL_UNPUBLISHED_SUCCESFULLY');
+		$this->setRedirect('index.php?option=' . $option . '&view=rating', $msg);
+	}
 }
