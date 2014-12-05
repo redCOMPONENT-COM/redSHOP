0b23380d73dbd5366ca4c2ee181e1d5377752b19
Vladimir <vshandak@yahoo.com>
2014-12-05 15:42:22 +0200

    Fixed with table

--- component/admin/views/wizard/tmpl/welcome.php
+++ component/admin/views/wizard/tmpl/welcome.php
@@ -26,10 +26,10 @@
 <div>
 	<table cellpadding="4" cellspacing="0" border="0" width="100%" class="adminlist">
 		<tr>
-			<td valign="top">
+			<td valign="top" width="270px">
 				<img src="<?php echo REDSHOP_ADMIN_IMAGES_ABSPATH; ?>261-x-88.png" width="261" height="88" alt="redSHOP Logo" align="left">
 			</td>
-			<td valign="top" width="100%">
+			<td valign="top">
 				<strong>redSHOP</strong><br/>
 				<font class="small">by <a href="http://www.redcomponent.com" target="_blank">redcomponent.com </a><br/></font>
 				<font class="small">
