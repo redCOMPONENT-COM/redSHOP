74a73e8da8bc14196c966ed909b9ff4fd52fe35e
puneet0191 <puneet.0191@gmail.com>
2014-12-18 22:07:54 +0530

    REDSHOP-1516 Moving Steps into Individual folders

---
+++ tests/acceptance/Joomla2Steps/InstallExtensionJ2Steps.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @package     RedShop
+ * @subpackage  Step Class
+ * @copyright   Copyright (C) 2012 - 2014 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace AcceptanceTester;
+
+/**
+ * Class InstallExtensionJ2Steps
+ *
+ * @package  AcceptanceTester
+ *
+ * @since    1.4
+ *
+ * @link     http://codeception.com/docs/07-AdvancedUsage#StepObjects
+ */
+class InstallExtensionJ2Steps extends \AcceptanceTester
+{
+	/**
+	 * Function to Install RedShop1, inside Joomla 2.5
+	 *
+	 * @return void
+	 */
+	public function installExtension()
+	{
+		$I = $this;
+		$this->acceptanceTester = $I;
+		$I->amOnPage(\ExtensionManagerPage::$URL);
+		$config = $I->getConfig();
+		$I->fillField(\ExtensionManagerPage::$extensionDirectoryPath, $config['folder']);
+		$I->click(\ExtensionManagerPage::$installButton);
+		$I->waitForText(\ExtensionManagerPage::$installSuccessMessage, 60);
+	}
+
+	/**
+	 * Function to Install Demo Data for the Extension
+	 *
+	 * @return void
+	 */
+	public function installSampleData()
+	{
+		$I = $this;
+		$config = $I->getConfig();
+
+		if ($config['install_extension_demo_data'] == 'yes')
+		{
+			$I->click(\ExtensionManagerPage::$installDemoContent);
+			$I->waitForText(\ExtensionManagerPage::$demoDataInstallSuccessMessage, 30);
+		}
+	}
+}
