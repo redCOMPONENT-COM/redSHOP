0013e6edb7ba7ca1a4214455131e1da6b10f7c6a
puneet0191 <puneet.0191@gmail.com>
2014-12-22 21:53:13 +0530

    REDSHOP-1809 Adding logic in Cept to load required Step Class

--- tests/acceptance/ManageStatesAdministratorCept.php
+++ tests/acceptance/ManageStatesAdministratorCept.php
@@ -7,12 +7,15 @@
  */
 $scenario->group('Joomla2');
 $scenario->group('Joomla3');
+
 // Load the Step Object Page
 $I = new AcceptanceTester\LoginSteps($scenario);
 
 $I->wantTo('Test State Manager in Administrator');
 $I->doAdminLogin();
-$I = new AcceptanceTester\CountryManagerSteps($scenario);
+$config = $I->getConfig();
+$className = 'AcceptanceTester\CountryManager' . $config['env'] . 'Steps';
+$I = new $className($scenario);
 $randomCountryName = 'Testing Country ' . rand(99, 999);
 $randomStateName = 'Testing State' . rand(99, 999);
 $updatedRandomStateName = 'New ' . $randomStateName;
@@ -21,13 +24,15 @@ $randomThreeCode = rand(99, 999);
 $randomCountry = 'Country ' . rand(99, 999);
 $I->addCountry($randomCountryName, $randomThreeCode, $randomTwoCode, $randomCountry);
 $I->searchCountry($randomCountryName);
-$I = new AcceptanceTester\StateManagerSteps($scenario);
+$className = 'AcceptanceTester\StateManager' . $config['env'] . 'Steps';
+$I = new $className($scenario);
 $I->wantTo('Add a new State');
 $I->addState($randomCountry, $randomStateName, $randomTwoCode, $randomThreeCode);
 $I->wantTo('Update the new State');
 $I->updateState($randomStateName, $updatedRandomStateName);
 $I->wantTo('Delete the New State');
 $I->deleteState($updatedRandomStateName);
-$I = new AcceptanceTester\CountryManagerSteps($scenario);
+$className = 'AcceptanceTester\CountryManager' . $config['env'] . 'Steps';
+$I = new $className($scenario);
 $I->deleteCountry($randomCountryName);
 $I->searchCountry($randomCountryName, 'Delete');
