baa95918d69fa041cc1dfc43661acc113dd292d5
puneet0191 <puneet.0191@gmail.com>
2014-12-23 23:24:46 +0530

    REDSHOP-1808 Updating Paths for Joomla2Steps

---
+++ tests/acceptance/_steps/Joomla2Steps/OrderManagerJoomla2Steps.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @package     RedShop
+ * @subpackage  Step Class
+ * @copyright   Copyright (C) 2012 - 2014 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+namespace AcceptanceTester;
+/**
+ * Class OrderManagerJoomla2Steps
+ *
+ * @package  AcceptanceTester
+ *
+ * @link     http://codeception.com/docs/07-AdvancedUsage#StepObjects
+ *
+ * @since    1.4
+ */
+class OrderManagerJoomla2Steps extends AdminManagerJoomla2Steps
+{
+	/**
+	 * Function to Add a new Order
+	 *
+	 * @return void
+	 */
+	public function addOrder()
+	{
+		$I = $this;
+		$I->amOnPage(\OrderManagerPage::$URL);
+		$I->verifyNotices(false, $this->checkForNotices(), 'Order Manager Page');
+		$I->click('New');
+		$I->verifyNotices(false, $this->checkForNotices(), 'Order Manager New');
+		$I->click('Cancel');
+	}
+}
