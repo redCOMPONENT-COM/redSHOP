ffabf4d3b57a4e7803295b9ada9a50eaa5037422
puneet0191 <puneet.0191@gmail.com>
2014-12-23 23:25:15 +0530

    REDSHOP-1808 Updating Bootstrap for Steps

--- tests/acceptance/_bootstrap.php
+++ tests/acceptance/_bootstrap.php
@@ -3,6 +3,6 @@
 // Here you can initialize variables that will be available to your tests
 
 \Codeception\Util\Autoload::registerSuffix('Page', __DIR__ . DIRECTORY_SEPARATOR . '_pages');
-\Codeception\Util\Autoload::registerSuffix('Steps', __DIR__ . DIRECTORY_SEPARATOR . 'Joomla3Steps');
-\Codeception\Util\Autoload::registerSuffix('Steps', __DIR__ . DIRECTORY_SEPARATOR . 'Joomla2Steps');
+\Codeception\Util\Autoload::registerSuffix('Steps', __DIR__ . DIRECTORY_SEPARATOR . '_steps/Joomla3Steps');
+\Codeception\Util\Autoload::registerSuffix('Steps', __DIR__ . DIRECTORY_SEPARATOR . '_steps/Joomla2Steps');
 
