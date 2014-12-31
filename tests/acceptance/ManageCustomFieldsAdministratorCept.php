67ee5901436c522f3f0787ca526772ee8d9788e0
puneet0191 <puneet.0191@gmail.com>
2014-12-31 14:12:18 +0530

    REDSHOP-1242 Adding Cept file for Custom Fields

---
+++ tests/acceptance/ManageCustomFieldsAdministratorCept.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package     RedShop
+ * @subpackage  Cept
+ * @copyright   Copyright (C) 2012 - 2014 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+$scenario->group('Joomla3');
+
+// Load the Step Object Page
+$I = new AcceptanceTester\LoginSteps($scenario);
+
+$I->wantTo('Test Custom Fields Manager in Administrator');
+$I->doAdminLogin();
+$config = $I->getConfig();
+$className = 'AcceptanceTester\CustomFieldManager' . $config['env'] . 'Steps';
+$I = new $className($scenario);
+
+
+$name = 'Testing Field' . rand(100, 1000);
+$title = 'Test Title' . rand(10, 100);
+$type = 'Text area';
+$section = 'Category';
+$newTitle = 'Updated ' . $title;
+$I->addField($name, $title, $type, $section);
+$I->searchField($title);
+$I->editField($title, $newTitle);
+$I->searchField($newTitle);
+$I->changeState($newTitle);
+$I->verifyState('unpublished', $I->getState($newTitle));
+$I->deleteCustomField($newTitle);
+$I->searchField($newTitle, 'Delete');
