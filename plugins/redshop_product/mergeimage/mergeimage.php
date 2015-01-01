dca204785e5352c37e69d705adf1d96561456f8c
Gunjan Patel <gunjan@redcomponent.com>
2015-01-01 13:07:24 +0530

    Update copyright to 2015 in plugins

--- plugins/redshop_product/mergeimage/mergeimage.php
+++ plugins/redshop_product/mergeimage/mergeimage.php
@@ -2,7 +2,7 @@
 /**
  * @package     RedShop
  * @subpackage  redshop_payment
- * @copyright   Copyright (C) 2013 redCOMPONENT.com. All rights reserved.
+ * @copyright   Copyright (C) 2015 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die;
