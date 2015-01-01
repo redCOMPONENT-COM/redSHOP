564388c872ea82b1f566a869c66fc3c2e11eeca9
Gunjan Patel <gunjan@redcomponent.com>
2015-01-01 12:19:33 +0530

    Update copyright to 2015

--- component/admin/views/quotation/view.csv.php
+++ component/admin/views/quotation/view.csv.php
@@ -3,7 +3,7 @@
  * @package     Redshop
  * @subpackage  Views
  *
- * @copyright   Copyright (C) 2012 - 2014 redCOMPONENT.com. All rights reserved.
+ * @copyright   Copyright (C) 2012 - 2015 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later, see LICENSE.
  */
 
