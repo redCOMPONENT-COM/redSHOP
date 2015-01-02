ad3f14004cc14675a14e3685ff8a7cdc94cd398b
Gunjan Patel <gunjan@redcomponent.com>
2015-01-02 16:23:56 +0530

    REDSHOP-1850 Update copyright to 2008-2015 in plugins

--- install.php
+++ install.php
@@ -2,7 +2,7 @@
 /**
  * @package    RedSHOP.Installer
  *
- * @copyright  Copyright (C) 2005 - 2015 redCOMPONENT.com. All rights reserved.
+ * @copyright  Copyright (C) 2008 - 2015 redCOMPONENT.com. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE
  */
 
