f5c410c272c853d0b118df6990e443d8626bc1f6
puneet0191 <puneet.0191@gmail.com>
2015-01-02 13:01:09 +0530

    REDSHOP-1192 Adding Manufacturer Joomla3 Page Class

---
+++ tests/_pages/Joomla3/Administrator/ManufacturerManagerJoomla3Page.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @package     RedShop
+ * @subpackage  Page Class
+ * @copyright   Copyright (C) 2012 - 2014 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+/**
+ * Class ManufacturerManagerJoomla3Page
+ *
+ * @link   http://codeception.com/docs/07-AdvancedUsage#PageObjects
+ *
+ * @since  1.4
+ */
+class ManufacturerManagerJoomla3Page
+{
+	public static $mailName = "//input[@id='mail_name']";
+
+	public static $mailSubject = "//input[@id='mail_subject']";
+
+	public static $mailBcc = "//input[@id='mail_bcc']";
+
+	public static $mailSectionDropDown = "//div[@id='mail_section_chzn']/a";
+
+	public static $mailSuccessMessage = 'Mail template saved';
+
+	public static $firstResultRow = "//div[@id='editcell']//table[2]//tbody/tr[1]";
+
+	public static $selectFirst = "//input[@id='cb0']";
+
+	public static $mailTemplateStatePath = "//div[@id='editcell']//table[2]//tbody/tr[1]/td[6]/a";
+
+	/**
+	 * Function to get the path for Section
+	 *
+	 * @param   String  $section  Section for the mail Template
+	 *
+	 * @return string
+	 */
+	public function mailSection($section)
+	{
+		$path = "//div[@id='mail_section_chzn']/div/ul/li[contains(text(), '" . $section . "')]";
+
+		return $path;
+	}
+}
