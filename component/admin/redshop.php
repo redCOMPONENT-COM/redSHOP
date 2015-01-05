9fe739af8286d4123fe2bfe87f7bcbbae8655411
Vladimir <vshandak@yahoo.com>
2015-01-05 14:04:20 +0200

    Fixed bug in check task value for redshop admin side

--- component/admin/redshop.php
+++ component/admin/redshop.php
@@ -97,14 +97,7 @@
 		return true;
 	}
 
-	$controller = JRequest::getVar('view', 'redshop');
-
-	// Set the controller page
-	if (!file_exists(JPATH_COMPONENT . '/controllers/' . $controller . '.php'))
-	{
-		$controller = 'redshop';
-		JRequest::setVar('view', 'redshop');
-	}
+	$view = $app->input->get('view', 'redshop');
 
 	JHtml::_('behavior.framework');
 	JHtml::_('redshopjquery.framework');
@@ -117,10 +110,42 @@
 	}
 
 	$user        = JFactory::getUser();
-	$task        = JRequest::getVar('task');
+	$task        = $app->input->get('task', '');
 	$layout      = JRequest::getVar('layout', '');
 	$showbuttons = JRequest::getVar('showbuttons', '0');
 	$showall     = JRequest::getVar('showall', '0');
+
+	// Check for array format.
+	$filter = JFilterInput::getInstance();
+
+	if (is_array($task))
+	{
+		$command = $filter->clean(array_pop(array_keys($task)), 'cmd');
+	}
+	else
+	{
+		$command = $filter->clean($task, 'cmd');
+	}
+
+	// Check for a not controller.task command.
+	if ($command != '' && strpos($command, '.') === false)
+	{
+		JRequest::setVar('task', $view . '.' . $command);
+		$task = $command;
+	}
+	elseif ($command != '' && strpos($command, '.') !== false)
+	{
+		$commands = explode('.', $command);
+		$view = $commands[0];
+		$task = $commands[1];
+	}
+
+	// Set the controller page
+	if (!file_exists(JPATH_COMPONENT . '/controllers/' . $view . '.php'))
+	{
+		$view = 'redshop';
+		JRequest::setVar('view', $view);
+	}
 
 	$document->addStyleDeclaration('fieldset.adminform textarea {margin: 0px 0px 10px 0px !important;width: 100% !important;}');
 
@@ -138,12 +163,12 @@
 	$document->addStyleSheet(JURI::root() . 'administrator/components/com_redshop/assets/css/redshop.css');
 	$format = $app->input->get('format', 'html');
 
-	if ($controller != "search" && $controller != "order_detail" && $controller != "wizard" && $task != "getcurrencylist"
-		&& $layout != "thumbs" && $controller != "catalog_detail" && $task != "clearsef" && $task != "removesubpropertyImage"
-		&& $task != "removepropertyImage" && $controller != "product_price" && $task != "template" && $json_var == ''
+	if ($view != "search" && $view != "order_detail" && $view != "wizard" && $task != "getcurrencylist"
+		&& $layout != "thumbs" && $view != "catalog_detail" && $task != "clearsef" && $task != "removesubpropertyImage"
+		&& $task != "removepropertyImage" && $view != "product_price" && $task != "template" && $json_var == ''
 		&& $task != 'gbasedownload' && $task != "export_data" && $showbuttons != "1" && $showall != 1
-		&& $controller != "product_attribute_price" && $task != "ins_product" && $controller != "shipping_rate_detail"
-		&& $controller != "accountgroup_detail" && $layout != "labellisting" && $task != "checkVirtualNumber" && $controller != "update"
+		&& $view != "product_attribute_price" && $task != "ins_product" && $view != "shipping_rate_detail"
+		&& $view != "accountgroup_detail" && $layout != "labellisting" && $task != "checkVirtualNumber" && $view != "update"
 		&& $format == 'html')
 	{
 		// Container CSS class definition
@@ -156,25 +181,25 @@
 			$redSHOPCSSContainerClass = ' isJ30';
 		}
 
-		echo '<div id="redSHOPAdminContainer" class="redSHOPAdminView' . ucfirst($controller) . $redSHOPCSSContainerClass . '">';
+		echo '<div id="redSHOPAdminContainer" class="redSHOPAdminView' . ucfirst($view) . $redSHOPCSSContainerClass . '">';
 
-		if ($controller != "redshop" && $controller != "configuration" && $controller != "product_detail"
-			&& $controller != "country_detail" && $controller != "state_detail" && $controller != "category_detail"
-			&& $controller != "fields_detail" && $controller != "stockroom_detail"
-			&& $controller != "shipping_detail" && $controller != "user_detail" && $controller != "template_detail"
-			&& $controller != "voucher_detail" && $controller != "textlibrary_detail" && $controller != "manufacturer_detail"
-			&& $controller != "rating_detail" && $controller != "newslettersubscr_detail" && $controller != "discount_detail"
-			&& $controller != "mail_detail" && $controller != "newsletter_detail" && $controller != "media_detail"
-			&& $controller != "shopper_group_detail" && $controller != "sample_detail" && $controller != "attributeprices"
-			&& $controller != "attributeprices_detail" && $controller != "prices_detail" && $controller != "wrapper_detail"
-			&& $controller != "tax_group_detail" && $controller != "addorder_detail" && $controller != "tax_detail"
-			&& $controller != "coupon_detail" && $controller != "giftcard_detail" && $controller != "attribute_set_detail"
-			&& $controller != 'shipping_box_detail' && $controller != 'quotation_detail'
-			&& $controller != 'question_detail' && $controller != 'answer_detail'
-			&& $controller != 'xmlimport_detail' && $controller != 'addquotation_detail'
-			&& $controller != 'xmlexport_detail' && $task != 'element'  && $controller != 'stockimage_detail'
-			&& $controller != 'mass_discount_detail' && $controller != 'supplier_detail'
-			&& $controller != 'orderstatus_detail')
+		if ($view != "redshop" && $view != "configuration" && $view != "product_detail"
+			&& $view != "country_detail" && $view != "state_detail" && $view != "category_detail"
+			&& $view != "fields_detail" && $view != "stockroom_detail"
+			&& $view != "shipping_detail" && $view != "user_detail" && $view != "template_detail"
+			&& $view != "voucher_detail" && $view != "textlibrary_detail" && $view != "manufacturer_detail"
+			&& $view != "rating_detail" && $view != "newslettersubscr_detail" && $view != "discount_detail"
+			&& $view != "mail_detail" && $view != "newsletter_detail" && $view != "media_detail"
+			&& $view != "shopper_group_detail" && $view != "sample_detail" && $view != "attributeprices"
+			&& $view != "attributeprices_detail" && $view != "prices_detail" && $view != "wrapper_detail"
+			&& $view != "tax_group_detail" && $view != "addorder_detail" && $view != "tax_detail"
+			&& $view != "coupon_detail" && $view != "giftcard_detail" && $view != "attribute_set_detail"
+			&& $view != 'shipping_box_detail' && $view != 'quotation_detail'
+			&& $view != 'question_detail' && $view != 'answer_detail'
+			&& $view != 'xmlimport_detail' && $view != 'addquotation_detail'
+			&& $view != 'xmlexport_detail' && $task != 'element'  && $view != 'stockimage_detail'
+			&& $view != 'mass_discount_detail' && $view != 'supplier_detail'
+			&& $view != 'orderstatus_detail')
 		{
 			echo '<div style="float:left;width:19%; margin-right:1%;">';
 			JLoader::load('RedshopHelperAdminMenu');
@@ -186,26 +211,8 @@
 		}
 	}
 
-	// Check for array format.
-	$filter = JFilterInput::getInstance();
-
-	if (is_array($task))
-	{
-		$command = $filter->clean(array_pop(array_keys($task)), 'cmd');
-	}
-	else
-	{
-		$command = $filter->clean($task, 'cmd');
-	}
-
-	// Check for a not controller.task command.
-	if ($command != '' && strpos($command, '.') === false)
-	{
-		JRequest::setVar('task', $controller . '.' . $command);
-	}
-
 	// Execute the task.
-	$controller	= JControllerLegacy::getInstance('Redshop');
+	$controller = JControllerLegacy::getInstance('Redshop');
 
 	if (version_compare(JVERSION, '3.0', '<'))
 	{
