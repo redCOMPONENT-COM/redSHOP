54a9908148ca129ef06733b2fb599e27e7acd788
Vladimir <vshandak@yahoo.com>
2015-01-26 11:22:39 +0200

    Fixed newslettersubscr

--- component/admin/controllers/newslettersubscr.php
+++ component/admin/controllers/newslettersubscr.php
@@ -79,4 +79,44 @@ class RedshopControllerNewslettersubscr extends RedshopController
 			$this->setRedirect('index.php?option=com_redshop&view=newslettersubscr&task=import_data', $msg);
 		}
 	}
+
+	public function publish()
+	{
+		$cid = JRequest::getVar('cid', array(0), 'post', 'array');
+
+		if (!is_array($cid) || count($cid) < 1)
+		{
+			JError::raiseError(500, JText::_('COM_REDSHOP_SELECT_AN_ITEM_TO_PUBLISH'));
+		}
+
+		$model = $this->getModel('newslettersubscr_detail');
+
+		if (!$model->publish($cid, 1))
+		{
+			echo "<script> alert('" . $model->getError(true) . "'); window.history.go(-1); </script>\n";
+		}
+
+		$msg = JText::_('COM_REDSHOP_NEWSLETTER_SUBSCR_DETAIL_PUBLISHED_SUCCESFULLY');
+		$this->setRedirect('index.php?option=com_redshop&view=newslettersubscr', $msg);
+	}
+
+	public function unpublish()
+	{
+		$cid = JRequest::getVar('cid', array(0), 'post', 'array');
+
+		if (!is_array($cid) || count($cid) < 1)
+		{
+			JError::raiseError(500, JText::_('COM_REDSHOP_SELECT_AN_ITEM_TO_UNPUBLISH'));
+		}
+
+		$model = $this->getModel('newslettersubscr_detail');
+
+		if (!$model->publish($cid, 0))
+		{
+			echo "<script> alert('" . $model->getError(true) . "'); window.history.go(-1); </script>\n";
+		}
+
+		$msg = JText::_('COM_REDSHOP_NEWSLETTER_SUBSCR_DETAIL_UNPUBLISHED_SUCCESFULLY');
+		$this->setRedirect('index.php?option=com_redshop&view=newslettersubscr', $msg);
+	}
 }
