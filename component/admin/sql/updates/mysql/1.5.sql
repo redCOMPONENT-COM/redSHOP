ab4cd872e7f21ca9d27f31ba0ec77cd155252d66
javier gomez <javier.gomez.corio@gmail.com>
2015-01-28 10:39:21 +0100

    REDSHOP-928 update mexican peso ISO on old install

--- component/admin/sql/updates/mysql/1.5.sql
+++ component/admin/sql/updates/mysql/1.5.sql
@@ -1,3 +1,6 @@
 ALTER TABLE `#__redshop_product_download` CHANGE `download_id` `download_id` VARCHAR(255)
 CHARACTER SET utf8
 COLLATE utf8_general_ci NOT NULL DEFAULT '';
+
+REPLACE INTO `#__redshop_currency` VALUES
+	(99, 'Mexican Peso', 'MXN');
