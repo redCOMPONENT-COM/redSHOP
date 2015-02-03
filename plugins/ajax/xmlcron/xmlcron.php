b938c0aff8c09cfb77f6dca72804513a03a8e63e
Gunjan Patel <gunjan@redcomponent.com>
2015-02-03 17:06:09 +0530

    REDSHOP-1981 #comment Updating file header xmlcron plugin

--- plugins/ajax/xmlcron/xmlcron.php
+++ plugins/ajax/xmlcron/xmlcron.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     RedSHOP.Plugin
- * @subpackage  User.cart
+ * @subpackage  Ajax.xmlcron
  *
  * @copyright   Copyright (C) 2008 - 2015 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
