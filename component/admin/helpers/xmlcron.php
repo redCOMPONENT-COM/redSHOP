ad6108e982e181fd58298be7f5d25d1068cef5ba
Gunjan Patel <gunjan@redcomponent.com>
2015-02-03 17:02:45 +0530

    REDSHOP-1981 #comment Adding deprecated warning in xmlcron file

--- component/admin/helpers/xmlcron.php
+++ component/admin/helpers/xmlcron.php
@@ -5,6 +5,9 @@
  *
  * @copyright   Copyright (C) 2008 - 2015 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
+ *
+ * @since       1.0
+ * @deprecated  This file will be removed in redSHOP 1.6
  */
 $my_path = dirname(__FILE__);
 if (file_exists($my_path . "/../../../../configuration.php"))
