aa9460ae7fe024a3bae9c90664acf5761241c66a
Gunjan Patel <gunjan@redcomponent.com>
2015-02-06 12:48:44 +0530

    REDSHOP-2022 #comment Change package RedSHOP.Platform to RedSHOP.Library

--- libraries/redshop/html/redshopselect.php
+++ libraries/redshop/html/redshopselect.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package     Joomla.Platform
+ * @package     RedSHOP.Library
  * @subpackage  HTML
  *
  * @copyright   Copyright (C) 2008 - 2015 Open Source Matters, Inc. All rights reserved.
@@ -12,7 +12,7 @@ defined('JPATH_PLATFORM') or die;
 /**
  * Utility class for creating HTML Grids
  *
- * @package     RedSHOP.Platform
+ * @package     RedSHOP.Library
  * @subpackage  HTML
  * @since       1.5
  */
