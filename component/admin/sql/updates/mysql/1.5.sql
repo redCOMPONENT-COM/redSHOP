65418fa73a927d5c3578fff75f8989cf4978c7fc
Vladimir <vshandak@yahoo.com>
2015-02-09 14:03:21 +0200

    Change replace to update for not add any new fields

--- component/admin/sql/updates/mysql/1.5.sql
+++ component/admin/sql/updates/mysql/1.5.sql
@@ -2,5 +2,6 @@ ALTER TABLE `#__redshop_product_download` CHANGE `download_id` `download_id` VAR
 CHARACTER SET utf8
 COLLATE utf8_general_ci NOT NULL DEFAULT '';
 
-REPLACE INTO `#__redshop_currency` (`currency_id`, `currency_name`, `currency_code`) VALUES
-	(99, 'Mexican Peso', 'MXN');
+UPDATE `#__redshop_currency`
+SET `currency_name` = 'Mexican Peso', `currency_code` = 'MXN'
+WHERE `currency_id` = 99;
