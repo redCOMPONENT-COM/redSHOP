46c4e8adb6ec806c99e3b1d10863472a59fc1572
Vladimir <vshandak@yahoo.com>
2015-02-09 13:25:21 +0200

    Add name fields in query

--- component/admin/sql/updates/mysql/1.5.sql
+++ component/admin/sql/updates/mysql/1.5.sql
@@ -2,5 +2,5 @@ ALTER TABLE `#__redshop_product_download` CHANGE `download_id` `download_id` VAR
 CHARACTER SET utf8
 COLLATE utf8_general_ci NOT NULL DEFAULT '';
 
-REPLACE INTO `#__redshop_currency` VALUES
+REPLACE INTO `#__redshop_currency` (`currency_id`, `currency_name`, `currency_code`) VALUES
 	(99, 'Mexican Peso', 'MXN');
