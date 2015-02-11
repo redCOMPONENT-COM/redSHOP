93805db372fe662e77103f9d66a1c1bf908f15b0
Gunjan Patel <gunjan@redcomponent.com>
2015-02-11 18:38:42 +0530

    [imp] Backend Dashboard quick icons code shrink

--- component/admin/views/redshop/tmpl/default_quickpanel.php
+++ component/admin/views/redshop/tmpl/default_quickpanel.php
@@ -48,47 +48,66 @@ defined('_JEXEC') or die;
 			$user->usertype = $usertype[0];
 			$user->gid      = $user->groups[$user->usertype];
 			$quicklink_icon = explode(",", QUICKLINK_ICON);
-			$new_arr        = RedShopHelperImages::geticonarray();
+			$icons        = RedShopHelperImages::geticonarray();
+			$sections = array(
+				'products'       => 'prod',
+				'discounts'      => 'discount',
+				'communications' => 'comm',
+				'shippings'      => 'shipping',
+				'vats'           => 'vat',
+				'importexport'   => 'import',
+				'altration'      => 'altration',
+				'customerinput'  => 'customerinput',
+				'accountings'    => 'acc'
+			);
 		?>
 		<div class="row-fluid">
-			<!-- Products -->
+		<?php foreach ($sections as $sectionName => $sectionAssets) : ?>
 			<div id="cpanel">
-			<?php for ($i = 0, $n = count($new_arr['products']); $i < $n; $i++) : ?>
-				<?php
-					$isQuickLink = in_array($new_arr['products'][$i], $quicklink_icon);
-
-					$isBackendAccess = (
-										$user->gid != 8
-										&& ENABLE_BACKENDACCESS != 0
-										&& in_array($new_arr['products'][$i], $this->access_rslt)
-										&& $isQuickLink
-									);
-					$defaultAccess = (ENABLE_BACKENDACCESS == 0 && $isQuickLink);
-				?>
-				<?php if ($defaultAccess || $isBackendAccess) : ?>
+				<?php for ($i = 0, $n = count($icons[$sectionName]); $i < $n; $i++) : ?>
 					<?php
-						redshopViewredshop::quickiconButton(
-							'index.php?option=com_redshop&amp;view=' . $new_arr['products'][$i],
-							$new_arr['prodimages'][$i],
-							JText::_("COM_REDSHOP_" . $new_arr['prodtxt'][$i])
-						);
-					?>
-				<?php endif; ?>
-			<?php endfor; ?>
-			</div>
-
-			<!-- Orders -->
-			<div id="cpanel">
-				<?php for ($i = 0; $i < count($new_arr['orders']); $i++) : ?>
-					<?php if (('stockroom' == $new_arr['orders'][$i] && USE_STOCKROOM)
-									|| 'stockroom' != $new_arr['orders'][$i]) : ?>
-					<?php
-						$isQuickLink = in_array($new_arr['orders'][$i], $quicklink_icon);
+						$isQuickLink = in_array($icons[$sectionName][$i], $quicklink_icon);
 
 						$isBackendAccess = (
 											$user->gid != 8
 											&& ENABLE_BACKENDACCESS != 0
-											&& in_array($new_arr['orders'][$i], $this->access_rslt)
+											&& in_array($icons[$sectionName][$i], $this->access_rslt)
+											&& $isQuickLink
+										);
+						$defaultAccess = (ENABLE_BACKENDACCESS == 0 && $isQuickLink);
+					?>
+					<?php if ($defaultAccess || $isBackendAccess) : ?>
+						<!-- Default shipping link -->
+						<?php $link = 'index.php?option=com_redshop&amp;view=' . $icons[$sectionName][$i]; ?>
+
+						<?php if ($icons[$sectionName][$i] == 'shipping_detail') : ?>
+							<?php $link = 'index.php?option=com_installer'; ?>
+						<?php endif; ?>
+
+						<?php
+							redshopViewredshop::quickiconButton(
+								$link,
+								$icons[$sectionAssets . 'images'][$i],
+								JText::_("COM_REDSHOP_" . $icons[$sectionAssets . 'txt'][$i])
+							);
+						?>
+					<?php endif; ?>
+				<?php endfor; ?>
+			</div>
+		<?php endforeach; ?>
+
+			<!-- Orders -->
+			<div id="cpanel">
+				<?php for ($i = 0, $n = count($icons['orders']); $i < $n; $i++) : ?>
+					<?php if (('stockroom' == $icons['orders'][$i] && USE_STOCKROOM)
+									|| 'stockroom' != $icons['orders'][$i]) : ?>
+					<?php
+						$isQuickLink = in_array($icons['orders'][$i], $quicklink_icon);
+
+						$isBackendAccess = (
+											$user->gid != 8
+											&& ENABLE_BACKENDACCESS != 0
+											&& in_array($icons['orders'][$i], $this->access_rslt)
 											&& $isQuickLink
 										);
 						$defaultAccess = (ENABLE_BACKENDACCESS == 0 && $isQuickLink);
@@ -96,113 +115,28 @@ defined('_JEXEC') or die;
 						<?php if ($defaultAccess || $isBackendAccess) : ?>
 							<?php
 								redshopViewredshop::quickiconButton(
-									'index.php?option=com_redshop&amp;view=' . $new_arr['orders'][$i],
-									$new_arr['orderimages'][$i],
-									JText::_("COM_REDSHOP_" . $new_arr['ordertxt'][$i])
+									'index.php?option=com_redshop&amp;view=' . $icons['orders'][$i],
+									$icons['orderimages'][$i],
+									JText::_("COM_REDSHOP_" . $icons['ordertxt'][$i])
 								);
 							?>
 						<?php endif; ?>
 					<?php endif; ?>
 				<?php endfor; ?>
-			</div>
-
-			<!-- Discounts -->
-			<div id="cpanel">
-			<?php for ($i = 0, $n = count($new_arr['discounts']); $i < $n; $i++) : ?>
-				<?php
-					$isQuickLink = in_array($new_arr['discounts'][$i], $quicklink_icon);
-
-					$isBackendAccess = (
-										$user->gid != 8
-										&& ENABLE_BACKENDACCESS != 0
-										&& in_array($new_arr['discounts'][$i], $this->access_rslt)
-										&& $isQuickLink
-									);
-					$defaultAccess = (ENABLE_BACKENDACCESS == 0 && $isQuickLink);
-				?>
-				<?php if ($defaultAccess || $isBackendAccess) : ?>
-					<?php
-						redshopViewredshop::quickiconButton(
-							'index.php?option=com_redshop&amp;view=' . $new_arr['discounts'][$i],
-							$new_arr['discountimages'][$i],
-							JText::_("COM_REDSHOP_" . $new_arr['discounttxt'][$i])
-						);
-					?>
-				<?php endif; ?>
-			<?php endfor; ?>
-			</div>
-
-			<!-- Communications -->
-			<div id="cpanel">
-			<?php for ($i = 0, $n = count($new_arr['communications']); $i < $n; $i++) : ?>
-				<?php
-					$isQuickLink = in_array($new_arr['communications'][$i], $quicklink_icon);
-
-					$isBackendAccess = (
-										$user->gid != 8
-										&& ENABLE_BACKENDACCESS != 0
-										&& in_array($new_arr['communications'][$i], $this->access_rslt)
-										&& $isQuickLink
-									);
-					$defaultAccess = (ENABLE_BACKENDACCESS == 0 && $isQuickLink);
-				?>
-				<?php if ($defaultAccess || $isBackendAccess) : ?>
-					<?php
-						redshopViewredshop::quickiconButton(
-							'index.php?option=com_redshop&amp;view=' . $new_arr['communications'][$i],
-							$new_arr['commimages'][$i],
-							JText::_("COM_REDSHOP_" . $new_arr['commtxt'][$i])
-						);
-					?>
-				<?php endif; ?>
-			<?php endfor; ?>
-			</div>
-
-			<!-- shippings -->
-			<div id="cpanel">
-			<?php for ($i = 0, $n = count($new_arr['shippings']); $i < $n; $i++) : ?>
-				<?php
-					$isQuickLink = in_array($new_arr['shippings'][$i], $quicklink_icon);
-
-					$isBackendAccess = (
-										$user->gid != 8
-										&& ENABLE_BACKENDACCESS != 0
-										&& in_array($new_arr['shippings'][$i], $this->access_rslt)
-										&& $isQuickLink
-									);
-					$defaultAccess = (ENABLE_BACKENDACCESS == 0 && $isQuickLink);
-				?>
-				<?php if ($defaultAccess || $isBackendAccess) : ?>
-					<!-- Default shipping link -->
-					<?php $link = 'index.php?option=com_redshop&amp;view=' . $new_arr['shippings'][$i]; ?>
-
-					<?php if ($new_arr['shippings'][$i] == 'shipping_detail') : ?>
-						<?php $link = 'index.php?option=com_installer'; ?>
-					<?php endif; ?>
-
-					<?php
-						redshopViewredshop::quickiconButton(
-							$link,
-							$new_arr['shippingimages'][$i],
-							JText::_("COM_REDSHOP_" . $new_arr['shippingtxt'][$i])
-						);
-					?>
-				<?php endif; ?>
-			<?php endfor; ?>
 			</div>
 
 			<!-- users -->
 			<div id="cpanel">
-				<?php for ($i = 0; $i < count($new_arr['users']); $i++) : ?>
-					<?php if (('accessmanager' == $new_arr['users'][$i] && ENABLE_BACKENDACCESS)
-									|| 'accessmanager' != $new_arr['users'][$i]) : ?>
+				<?php for ($i = 0, $n = count($icons['users']); $i < $n; $i++) : ?>
+					<?php if (('accessmanager' == $icons['users'][$i] && ENABLE_BACKENDACCESS)
+									|| 'accessmanager' != $icons['users'][$i]) : ?>
 					<?php
-						$isQuickLink = in_array($new_arr['users'][$i], $quicklink_icon);
+						$isQuickLink = in_array($icons['users'][$i], $quicklink_icon);
 
 						$isBackendAccess = (
 											$user->gid != 8
 											&& ENABLE_BACKENDACCESS != 0
-											&& in_array($new_arr['users'][$i], $this->access_rslt)
+											&& in_array($icons['users'][$i], $this->access_rslt)
 											&& $isQuickLink
 										);
 						$defaultAccess = (ENABLE_BACKENDACCESS == 0 && $isQuickLink);
@@ -210,144 +144,14 @@ defined('_JEXEC') or die;
 						<?php if ($defaultAccess || $isBackendAccess) : ?>
 							<?php
 								redshopViewredshop::quickiconButton(
-									'index.php?option=com_redshop&amp;view=' . $new_arr['users'][$i],
-									$new_arr['userimages'][$i],
-									JText::_("COM_REDSHOP_" . $new_arr['usertxt'][$i])
+									'index.php?option=com_redshop&amp;view=' . $icons['users'][$i],
+									$icons['userimages'][$i],
+									JText::_("COM_REDSHOP_" . $icons['usertxt'][$i])
 								);
 							?>
 						<?php endif; ?>
 					<?php endif; ?>
 				<?php endfor; ?>
-			</div>
-
-			<!-- vats -->
-			<div id="cpanel">
-			<?php for ($i = 0, $n = count($new_arr['vats']); $i < $n; $i++) : ?>
-				<?php
-					$isQuickLink = in_array($new_arr['vats'][$i], $quicklink_icon);
-
-					$isBackendAccess = (
-										$user->gid != 8
-										&& ENABLE_BACKENDACCESS != 0
-										&& in_array($new_arr['vats'][$i], $this->access_rslt)
-										&& $isQuickLink
-									);
-					$defaultAccess = (ENABLE_BACKENDACCESS == 0 && $isQuickLink);
-				?>
-				<?php if ($defaultAccess || $isBackendAccess) : ?>
-					<?php
-						redshopViewredshop::quickiconButton(
-							'index.php?option=com_redshop&amp;view=' . $new_arr['vats'][$i],
-							$new_arr['vatimages'][$i],
-							JText::_("COM_REDSHOP_" . $new_arr['vattxt'][$i])
-						);
-					?>
-				<?php endif; ?>
-			<?php endfor; ?>
-			</div>
-
-			<!-- importexport -->
-			<div id="cpanel">
-			<?php for ($i = 0, $n = count($new_arr['importexport']); $i < $n; $i++) : ?>
-				<?php
-					$isQuickLink = in_array($new_arr['importexport'][$i], $quicklink_icon);
-
-					$isBackendAccess = (
-										$user->gid != 8
-										&& ENABLE_BACKENDACCESS != 0
-										&& in_array($new_arr['importexport'][$i], $this->access_rslt)
-										&& $isQuickLink
-									);
-					$defaultAccess = (ENABLE_BACKENDACCESS == 0 && $isQuickLink);
-				?>
-				<?php if ($defaultAccess || $isBackendAccess) : ?>
-					<?php
-						redshopViewredshop::quickiconButton(
-							'index.php?option=com_redshop&amp;view=' . $new_arr['importexport'][$i],
-							$new_arr['importimages'][$i],
-							JText::_("COM_REDSHOP_" . $new_arr['importtxt'][$i])
-						);
-					?>
-				<?php endif; ?>
-			<?php endfor; ?>
-			</div>
-
-			<!-- altration -->
-			<div id="cpanel">
-			<?php for ($i = 0, $n = count($new_arr['altration']); $i < $n; $i++) : ?>
-				<?php
-					$isQuickLink = in_array($new_arr['altration'][$i], $quicklink_icon);
-
-					$isBackendAccess = (
-										$user->gid != 8
-										&& ENABLE_BACKENDACCESS != 0
-										&& in_array($new_arr['altration'][$i], $this->access_rslt)
-										&& $isQuickLink
-									);
-					$defaultAccess = (ENABLE_BACKENDACCESS == 0 && $isQuickLink);
-				?>
-				<?php if ($defaultAccess || $isBackendAccess) : ?>
-					<?php
-						redshopViewredshop::quickiconButton(
-							'index.php?option=com_redshop&amp;view=' . $new_arr['altration'][$i],
-							$new_arr['altrationimages'][$i],
-							JText::_("COM_REDSHOP_" . $new_arr['altrationtxt'][$i])
-						);
-					?>
-				<?php endif; ?>
-			<?php endfor; ?>
-			</div>
-
-			<!-- customerinput -->
-			<div id="cpanel">
-			<?php for ($i = 0, $n = count($new_arr['customerinput']); $i < $n; $i++) : ?>
-				<?php
-					$isQuickLink = in_array($new_arr['customerinput'][$i], $quicklink_icon);
-
-					$isBackendAccess = (
-										$user->gid != 8
-										&& ENABLE_BACKENDACCESS != 0
-										&& in_array($new_arr['customerinput'][$i], $this->access_rslt)
-										&& $isQuickLink
-									);
-					$defaultAccess = (ENABLE_BACKENDACCESS == 0 && $isQuickLink);
-				?>
-				<?php if ($defaultAccess || $isBackendAccess) : ?>
-					<?php
-						redshopViewredshop::quickiconButton(
-							'index.php?option=com_redshop&amp;view=' . $new_arr['customerinput'][$i],
-							$new_arr['customerinputimages'][$i],
-							JText::_("COM_REDSHOP_" . $new_arr['customerinputtxt'][$i])
-						);
-					?>
-				<?php endif; ?>
-			<?php endfor; ?>
-			</div>
-
-			<!-- accountings -->
-			<div id="cpanel">
-			<?php for ($i = 0, $n = count($new_arr['accountings']); $i < $n; $i++) : ?>
-				<?php
-					$isQuickLink = in_array($new_arr['accountings'][$i], $quicklink_icon);
-
-					$isBackendAccess = (
-										$user->gid != 8
-										&& ENABLE_BACKENDACCESS != 0
-										&& in_array($new_arr['accountings'][$i], $this->access_rslt)
-										&& $isQuickLink
-									);
-					$defaultAccess = (ENABLE_BACKENDACCESS == 0 && $isQuickLink);
-				?>
-				<?php if ($defaultAccess || $isBackendAccess) : ?>
-					<?php
-						redshopViewredshop::quickiconButton(
-							'index.php?option=com_redshop&amp;view=' . $new_arr['accountings'][$i],
-							$new_arr['accimages'][$i],
-							JText::_("COM_REDSHOP_" . $new_arr['acctxt'][$i])
-						);
-					?>
-				<?php endif; ?>
-			<?php endfor; ?>
 			</div>
 		</div>
 	</div>
