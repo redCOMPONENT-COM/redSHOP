05c5a7b5beb24dfd9a9021b0025cade4c0940bf8
Gunjan Patel <gunjan@redcomponent.com>
2015-02-12 16:47:15 +0530

    REDSHOP-1858 #comment Rewrite 2checkout plugin and added language strings

--- plugins/redshop_payment/rs_payment_2checkout/rs_payment_2checkout.php
+++ plugins/redshop_payment/rs_payment_2checkout/rs_payment_2checkout.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package     RedSHOP
+ * @package     RedSHOP.Plugin
  * @subpackage  Plugin
  *
  * @copyright   Copyright (C) 2008 - 2015 redCOMPONENT.com. All rights reserved.
@@ -12,10 +12,23 @@ defined('_JEXEC') or die;
 JLoader::import('redshop.library');
 JLoader::load('RedshopHelperAdminOrder');
 
-class plgRedshop_paymentrs_payment_2checkout extends JPlugin
+/**
+ * 2Checkout Payment plugin for redSHOP and other joomla ecommerce component
+ *
+ * @package     RedSHOP.Payment
+ * @subpackage  Plugin
+ *
+ * @since       1.0
+ */
+class PlgRedshop_PaymentRs_Payment_2checkout extends JPlugin
 {
 	/**
 	 * Plugin method with the same name as the event will be called automatically.
+	 *
+	 * @param   string  $element  Plugin Name
+	 * @param   array   $data     Order Information
+	 *
+	 * @return  void
 	 */
 	public function onPrePayment($element, $data)
 	{
@@ -29,11 +42,72 @@ class plgRedshop_paymentrs_payment_2checkout extends JPlugin
 			$plugin = $element;
 		}
 
-		$app = JFactory::getApplication();
-		$paymentpath = JPATH_SITE . '/plugins/redshop_payment/' . $plugin . '/' . $plugin . '/extra_info.php';
-		include $paymentpath;
+		$app         = JFactory::getApplication();
+		$orderHelper = new order_functions;
+		$rs          = $orderHelper->getOrderItemDetail($data['order_id']);
+		$Itemid      = $app->input->getInt('Itemid');
+
+		// Authnet vars to send
+		$formdata = array(
+			'sid'                => $this->params->get('vendor_id'),
+			'cart_order_id'      => 'Order Id:' . $data['order_id'],
+			'merchant_order_id'  => $data['order_id'],
+
+			// Customer Name and Billing Address
+			'card_holder_name'   => $data['billinginfo']->firstname . ' ' . $data['billinginfo']->lastname,
+			'street_address'     => $data['billinginfo']->address,
+			'city'               => $data['billinginfo']->city,
+			'state'              => $data['billinginfo']->state_code,
+			'zip'                => $data['billinginfo']->zipcode,
+			'country'            => $data['billinginfo']->country_code,
+			'email'              => $data['billinginfo']->user_email,
+			'phone'              => $data['billinginfo']->phone,
+
+			// Customer Shipping Address
+			'ship_name'          => $data['shippinginfo']->firstname . ' ' . $data['shippinginfo']->lastname,
+			'ship_steet_address' => $data['shippinginfo']->address,
+			'ship_city'          => $data['shippinginfo']->city,
+			'ship_state'         => $data['shippinginfo']->state_code,
+			'ship_zip'           => $data['shippinginfo']->zipcode,
+			'ship_country'       => $data['shippinginfo']->country_code,
+			'return_url'         => JURI::base() . "index.php?option=com_redshop&view=order_detail&controller=order_detail&task=notify_payment&payment_plugin=rs_payment_2checkout&Itemid=$Itemid&orderid=" . $data['order_id'],
+			'x_receipt_link_url' => JURI::base() . "index.php?option=com_redshop&view=order_detail&controller=order_detail&task=notify_payment&payment_plugin=rs_payment_2checkout&Itemid=$Itemid&orderid=" . $data['order_id'],
+			'id_type'            => 1,
+			'c_tangible_1'       => 'Y',
+			'total'              => number_format($data['carttotal'], 2, '.', '')
+		);
+
+		for ($p = 0, $nrs = count($rs); $p < $nrs; $p++)
+		{
+			$formdata['c_prod_' . ($p + 1)]        = '1,' . $rs[$p]->product_quantity;
+			$formdata['c_name_' . ($p + 1)]        = $rs[$p]->order_item_name;
+			$formdata['c_price_' . ($p + 1)]       = $rs[$p]->product_item_price;
+			$formdata['c_description_' . ($p + 1)] = '';
+		}
+
+		// Live 2checkout api url
+		$checkoutUrl = 'https://www.2checkout.com/checkout/purchase';
+
+		if ((bool) $this->params->get('is_test'))
+		{
+			$formdata['demo'] = 'Y';
+
+			// Test mode 2checkout api url
+			$checkoutUrl = 'https://sandbox.2checkout.com/checkout/purchase';
+		}
+
+		$app->redirect($checkoutUrl . '/?' . JUri::buildQuery($formdata));
+		$app->close();
 	}
 
+	/**
+	 * This method will trigger when notify payment task will execute
+	 *
+	 * @param   string  $element  Plugin Name
+	 * @param   array   $request  Request data from payment gateway
+	 *
+	 * @return  array   Return array of information about payment status and codes.
+	 */
 	public function onNotifyPaymentrs_payment_2checkout($element, $request)
 	{
 		if ($element != 'rs_payment_2checkout')
@@ -41,70 +115,51 @@ class plgRedshop_paymentrs_payment_2checkout extends JPlugin
 			return;
 		}
 
-		$db             = JFactory::getDbo();
-		$request        = JRequest::get('request');
-		$order_id       = $request['merchant_order_id'];
-		$invoice_id     = $request['invoice_id'];
-		$is_test        = $this->params->get("is_test");
-		$vendor_id      = $this->params->get("vendor_id");
-		$verify_status  = $this->params->get("verify_status");
-		$invalid_status = $this->params->get("invalid_status");
-		$secret_words   = $this->params->get("secret_words");
-		$order_amount   = $request["total"];
-		$order_ekey     = $request["key"];
-		$accept         = $_REQUEST["sid"];
-		$Itemid         = $request["Itemid"];
+		$app = JFactory::getApplication();
 
 		// Load plugin language
 		JPlugin::loadLanguage('com_redshop');
 
-		// Get value from xml file
-		$string_to_hash = $secret_words;
+		// Get value from plugin params file
+		$stringForHash = $this->params->get('secret_words');
 
 		// This should be YOUR vendor number
-		$string_to_hash .= $request['sid'];
+		$stringForHash .= $app->input->getString('sid');
 
-		// Append the order number, in this script it will always be 1
-		if ($is_test)
+		// Append the order number for live mode
+		if ($this->params->get('is_test'))
 		{
-			$string_to_hash .= 1;
+			$stringForHash .= 1;
 		}
 		else
 		{
-			$string_to_hash .= $request['order_number'];
+			$stringForHash .= $app->input->getString('order_number');
 		}
 
 		// Append the sale total
-		$string_to_hash .= $request["total"];
+		$stringForHash .= $app->input->getString('total');
 
 		// Get a md5 hash of the string, uppercase it to match the returned key
-		$hash_to_check = strtoupper(md5($string_to_hash));
+		$hashCode = strtoupper(md5($stringForHash));
 
 		// Now validate on the MD5 stamping. If the MD5 key is valid or if MD5 is disabled
-		if ($order_ekey === $hash_to_check)
+		if ($app->input->getString('key') === $hashCode)
 		{
-			$transaction_id                    = $invoice_id;
-			$values->order_status_code         = $verify_status;
+			$values->order_status_code         = $this->params->get('verify_status');
 			$values->order_payment_status_code = 'Paid';
+			$values->transaction_id            = $app->input->getString('invoice_id');
+			$values->order_id                  = $app->input->getInt('merchant_order_id', 0);
 			$values->log                       = JText::_('COM_REDSHOP_ORDER_PLACED');
 			$values->msg                       = JText::_('COM_REDSHOP_ORDER_PLACED');
-			$values->transaction_id            = $transaction_id;
-			$values->order_id                  = $order_id;
 		}
 		else
 		{
-			$values->order_status_code         = $invalid_status;
+			$values->order_status_code         = $this->params->get('invalid_status');
 			$values->order_payment_status_code = 'Unpaid';
 			$values->log                       = JText::_('COM_REDSHOP_ORDER_NOT_PLACED');
 			$values->msg                       = JText::_('COM_REDSHOP_ORDER_NOT_PLACED');
-			$msg                               = JText::_('COM_REDSHOP_PHPSHOP_PAYMENT_ERROR');
 		}
 
 		return $values;
-	}
-
-	public function onCapture_Paymentrs_payment_2checkout($element, $data)
-	{
-		return;
 	}
 }
