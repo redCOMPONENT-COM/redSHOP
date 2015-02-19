4613d1cc24062780bda9defe164ebbfd6e77fe22
clinchergt <jacobo@redcomponent.com>
2015-02-19 11:47:55 -0600

    REDSHOP-2062 REDSHOP-2064 Payment plugin language file now gets loaded on receipt

--- component/site/views/order_detail/view.html.php
+++ component/site/views/order_detail/view.html.php
@@ -45,6 +45,13 @@ class RedshopViewOrder_detail extends RedshopView
 		$encr   = JRequest::getString('encr', null);
 		$layout = JRequest::getCmd('layout');
 
+		// Load payment language file
+		$language          = JFactory::getLanguage();
+		$base_dir          = JPATH_ADMINISTRATOR;
+		$language_tag      = $language->getTag();
+		$extension = 'plg_redshop_payment_' . $order_functions->getOrderPaymentDetail($order_id)[0]->payment_method_class;
+		$language->load($extension, $base_dir, $language_tag, true);
+
 		$model = $this->getModel('order_detail');
 
 		$OrdersDetail = $order_functions->getOrderDetails($oid);
