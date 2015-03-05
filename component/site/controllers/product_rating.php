edf59a89ddcc57051facde1dd62d2d2cb20bb4a4
Vladimir <vshandak@yahoo.com>
2015-03-05 18:07:19 +0200

    First steep for change rating

--- component/site/controllers/product_rating.php
+++ component/site/controllers/product_rating.php
@@ -17,18 +17,117 @@ defined('_JEXEC') or die;
  * @subpackage  Controller
  * @since       1.0
  */
-class RedshopControllerProduct_rating extends RedshopController
+class RedshopControllerProduct_Rating extends RedshopControllerForm
 {
+	/**
+	 * Method to send Ask Question Mail.
+	 *
+	 * @return bool
+	 */
+	public function submit()
+	{
+		// Check for request forgeries.
+		JSession::checkToken() or jexit(JText::_('JINVALID_TOKEN'));
+
+		$app = JFactory::getApplication();
+		$data = $app->input->post->get('jform', array(), 'array');
+		$model = $this->getModel('product_rating');
+
+		$productId   = $app->input->getInt('pid', 0);
+		$Itemid      = $app->input->getInt('Itemid', 0);
+		$rate         = $app->input->getInt('rate', 0);
+		$category_id = $app->input->getInt('category_id', 0);
+		$userHelper  = new rsUserhelper;
+
+		if ($rate)
+		{
+			$link = 'index.php?option=com_redshop&view=product&pid=' . $productId . '&cid=' . $category_id . '&Itemid=' . $Itemid;
+		}
+		else
+		{
+			$link = 'index.php?option=com_redshop&view=ask_question&pid=' . $productId . '&tmpl=component&Itemid=' . $Itemid;
+		}
+
+		// Validate the posted data.
+		$form = $model->getForm();
+
+		if (!$form)
+		{
+			JError::raiseError(500, $model->getError());
+			$this->setRedirect($link);
+
+			return false;
+		}
+
+		// Save the data in the session.
+		$app->setUserState('com_redshop.product_rating.data', $data);
+
+		// Check captcha only for guests
+		if (SHOW_CAPTCHA && JFactory::getUser()->guest)
+		{
+			if (!$userHelper->checkCaptcha($data, false))
+			{
+				$app->enqueueMessage(JText::_('COM_REDSHOP_INVALID_SECURITY'), 'warning');
+				$this->setRedirect($link);
+
+				return false;
+			}
+		}
+
+		$validate = $model->validate($form, $data);
+
+		if ($validate === false)
+		{
+			// Get the validation messages.
+			$errors = $model->getErrors();
+
+			// Push up to three validation messages out to the user.
+			for ($i = 0, $n = count($errors); $i < $n && $i < 3; $i++)
+			{
+				if ($errors[$i] instanceof Exception)
+				{
+					$app->enqueueMessage($errors[$i]->getMessage(), 'warning');
+				}
+				else
+				{
+					$app->enqueueMessage($errors[$i], 'warning');
+				}
+			}
+		}
+		else
+		{
+			$data['product_id'] = $productId;
+			$data['Itemid'] = $Itemid;
+
+			if ($model->sendMailForReview($data))
+			{
+				// Flush the data from the session
+				$app->setUserState('com_redshop.ask_question.data', null);
+				$app->enqueueMessage(JText::_('COM_REDSHOP_EMAIL_HAS_BEEN_SENT_SUCCESSFULLY'));
+
+				if (!$rate)
+				{
+					$link .= '&questionSend=1';
+				}
+			}
+			else
+			{
+				$app->enqueueMessage($model->getError(), 'warning');
+			}
+		}
+
+		$this->setRedirect($link);
+	}
+
 	/**
 	 * save function
 	 *
 	 * @access public
 	 * @return void
 	 */
-	function save()
+	public function saveOld()
 	{
 		$post        = JRequest::get('post');
-		$option      = JRequest::getVar('option');
 		$Itemid      = JRequest::getVar('Itemid');
 		$product_id  = JRequest::getInt('product_id');
 		$category_id = JRequest::getInt('category_id');
