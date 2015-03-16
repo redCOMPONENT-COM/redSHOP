e5683aa78e7946194444ae6ec7c42f304d3aeb4b
clinchergt <jacobo@redcomponent.com>
2015-03-16 11:58:22 -0600

    REDSHOP-2218 Allow related product template to include custom fields

--- component/site/helpers/product.php
+++ component/site/helpers/product.php
@@ -10379,6 +10379,10 @@ class producthelper
 					$related_template_data = $this->getProductOnSaleComment($related_product[$r], $related_template_data);
 					$related_template_data = $this->getSpecialProductComment($related_product[$r], $related_template_data);
 
+					//  Extra field display
+					$extraFieldName = $extra_field->getSectionFieldNameArray(1, 1, 1);
+					$related_template_data = $this->getExtraSectionTag($extraFieldName, $related_product[$r]->product_id, "1", $related_template_data, 1);
+
 					// Related product attribute price list
 					$related_template_data = $this->replaceAttributePriceList($related_product[$r]->product_id, $related_template_data);
 				}
