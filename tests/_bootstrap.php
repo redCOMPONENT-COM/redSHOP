f5bffa357336bb50c45282e7feda3aba4ed5b796
javier gomez <javier.gomez.corio@gmail.com>
2015-04-01 14:20:16 +0200

    REDSHOP-2262 create a test performance checker

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 // This is global bootstrap for autoloading 
+include_once __DIR__.DIRECTORY_SEPARATOR . '_extensions/testsperformance.php';
 
 \Codeception\Util\Autoload::registerSuffix('Page', __DIR__.DIRECTORY_SEPARATOR.'_pages/Joomla2/Administrator');
 \Codeception\Util\Autoload::registerSuffix('Page', __DIR__.DIRECTORY_SEPARATOR.'_pages/Joomla2/Installation');
@@ -8,3 +9,4 @@
 \Codeception\Util\Autoload::registerSuffix('Page', __DIR__.DIRECTORY_SEPARATOR.'_pages/Joomla3/Installation');
 \Codeception\Util\Autoload::registerSuffix('Page', __DIR__.DIRECTORY_SEPARATOR.'_pages/Joomla3/System');
 \Codeception\Util\Autoload::registerSuffix('Group', __DIR__.DIRECTORY_SEPARATOR.'_groups');
+
