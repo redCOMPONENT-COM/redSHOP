76db50bd2f9acf6656b5e685dfaa73a053f5540a
Vladimir <vshandak@yahoo.com>
2015-04-14 12:05:40 +0700

    Fixed shoppergrouplogo module

--- modules/site/mod_redshop_shoppergrouplogo/mod_redshop_shoppergrouplogo.php
+++ modules/site/mod_redshop_shoppergrouplogo/mod_redshop_shoppergrouplogo.php
@@ -10,26 +10,13 @@
 defined('_JEXEC') or die;
 
 JLoader::import('redshop.library');
-$thumbwidth  = trim($params->get('thumbwidth', 100));
-$thumbheight = trim($params->get('thumbheight', 100));
+$thumbwidth  = (int) $params->get('thumbwidth', 100);
+$thumbheight = (int) $params->get('thumbheight', 100);
 
-$db = JFactory::getDbo();
 // Getting the configuration
 require_once JPATH_ADMINISTRATOR . '/components/com_redshop/helpers/redshop.cfg.php';
 JLoader::load('RedshopHelperAdminConfiguration');
 $Redconfiguration = new Redconfiguration;
 $Redconfiguration->defineDynamicVars();
 
-JLoader::load('RedshopHelperProduct');
-$user = JFactory::getUser();
-
-$sql = "SELECT s.*,u.user_id "
-	. "FROM #__redshop_users_info AS u "
-	. ", #__redshop_shopper_group AS s "
-	. "WHERE u.shopper_group_id = s.shopper_group_id "
-	. "AND s.published=1 "
-	. "AND u.user_id=" . (int) $user->id . " ";
-$db->setQuery($sql);
-$rows = $db->loadObject();
-
-require JModuleHelper::getLayoutPath('mod_redshop_shoppergrouplogo');?>
+require JModuleHelper::getLayoutPath('mod_redshop_shoppergrouplogo');
