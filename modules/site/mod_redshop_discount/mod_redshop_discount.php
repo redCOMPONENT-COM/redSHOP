be8f8a81fae4d69c8c8a226a0e7f39ac28489bc0
Vladimir <vshandak@yahoo.com>
2015-04-16 16:12:35 +0700

    Revert "Fixed redshop_discount module"
    This reverts commit 22e4026e33d4686d880ab7aa8b3464f33afb8279.

--- modules/site/mod_redshop_discount/mod_redshop_discount.php
+++ modules/site/mod_redshop_discount/mod_redshop_discount.php
@@ -10,21 +10,13 @@
 defined('_JEXEC') or die;
 
 JLoader::import('redshop.library');
+$document = JFactory::getDocument();
+$document->addStyleSheet("modules/mod_redshop_discount/css/discount.css");
 
-$time = time();
 $db = JFactory::getDbo();
-$query = $db->getQuery(true)
-	->select('d.*')
-	->from($db->qn('#__redshop_discount', 'd'))
-	->leftJoin($db->qn('#__redshop_discount_shoppers', 'ds') . ' ON ds.discount_id = d.discount_id')
-	->where('d.published = 1')
-	->where('d.start_date <= ' . $db->q($time))
-	->where('d.end_date >= ' . $db->q($time))
-	->where('ds.shopper_group_id = ' . (int) RedshopHelperUser::getShopperGroup(JFactory::getUser()->id))
-	->order('d.amount ASC');
-$data = $db->setQuery($query)->LoadObjectList();
 
-if ($data)
-{
-	require JModuleHelper::getLayoutPath('mod_redshop_discount', $params->get('layout', 'default'));
-}
+$query = "SELECT * FROM #__redshop_discount where published = 1 order by amount desc";
+$db->setQuery($query);
+$data = $db->LoadObjectList();
+
+require JModuleHelper::getLayoutPath('mod_redshop_discount');
