38ce63db2a16b75466f0f835d47f281a09986b4d
Vladimir <vshandak@yahoo.com>
2015-04-19 13:43:56 +0700

    Fixed vies plugin

--- plugins/redshop_vies_registration/rs_vies_registration/rs_vies_registration.php
+++ plugins/redshop_vies_registration/rs_vies_registration/rs_vies_registration.php
@@ -33,20 +33,11 @@ class PlgRedshop_Vies_Registrationrs_Vies_Registration extends JPlugin
 		JHtml::_('redshopjquery.framework');
 		$lang = JFactory::getLanguage();
 		$lang->load('plg_redshop_vies_registration_rs_vies_registration', JPATH_ADMINISTRATOR);
-		JText::script('PLG_REDSHOP_VIES_REGISTRATION_BROWSER_NOT_SUPPORT_XMLHTTP');
+		JText::script('PLG_REDSHOP_VIES_REGISTRATION_VALIDATION_STATUS2');
 		JText::script('PLG_REDSHOP_VIES_REGISTRATION_VERYFIES_VAT_NUMBER');
 		JHtml::script('plugins/redshop_vies_registration/rs_vies_registration/js/vies.js');
 
 		parent::__construct($subject, $config);
-	}
-
-	public function onChangeRegistrationForm(&$template)
-	{
-		$template = str_replace(
-			"{vat_number}",
-			'{vat_number}<input type="hidden" name="vies_wait_input" value="" id="vies_wait_input">',
-			$template
-		);
 	}
 
 	/**
@@ -69,6 +60,7 @@ class PlgRedshop_Vies_Registrationrs_Vies_Registration extends JPlugin
 		$member_requester_country_code = $this->params->get("member_state");
 
 		$client = new SoapClient("http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl");
+		$result = '';
 
 		if ($client)
 		{
@@ -82,87 +74,50 @@ class PlgRedshop_Vies_Registrationrs_Vies_Registration extends JPlugin
 			try
 			{
 				$r = $client->checkVatApprox($params);
-				$requestIdentifier_num = $r->requestIdentifier;
 
 				if ($r->valid == true)
 				{
-					?>
-					<td>
-						<?php echo JText::_('PLG_REDSHOP_VIES_REGISTRATION_VALIDATION')
-							. ": " . JText::sprintf('PLG_REDSHOP_VIES_REGISTRATION_VALID_VAT_NUMBER', $requestIdentifier_num); ?>
-						<input type="hidden" name="vies_vat_number" value="<?php echo $requestIdentifier_num; ?>">
-						<input type="hidden" name="vies_status" value="valid">
-					</td>
-					<?php
+					$result = json_encode(true);
 				}
 				else
 				{
-					?>
-					<td>
-						<?php echo JText::_('PLG_REDSHOP_VIES_REGISTRATION_VALIDATION')
-							. ": " . JText::_('PLG_REDSHOP_VIES_REGISTRATION_INVALID_VAT_NUMBER'); ?><br>
-						<label class="radio"><input type="radio" name="vies_status_invalid" id="vies_status_invalid1" value="0" >&nbsp;
-							<?php echo JText::_('PLG_REDSHOP_VIES_REGISTRATION_VALIDATION_STATUS1'); ?>
-						</label>
-						<label class="radio"><input type="radio" name="vies_status_invalid" id="vies_status_invalid2" value="1" checked="checked">&nbsp;
-							<?php echo JText::_('PLG_REDSHOP_VIES_REGISTRATION_VALIDATION_STATUS2'); ?>
-						</label>
-						<input type="hidden" name="vies_vat_number" value="" id="vies_vat_number">
-						<input type="hidden" name="vies_status" value="Invalid" id="vies_status">
-					</td>
-					<?php
+					$result = json_encode(
+						JText::_('PLG_REDSHOP_VIES_REGISTRATION_VALIDATION')
+						. ": " . JText::_('PLG_REDSHOP_VIES_REGISTRATION_INVALID_VAT_NUMBER')
+					);
 				}
 			}
 			catch (SoapFault $e)
 			{
 				$ret = $e->faultstring;
 
-				if (strtoupper($ret) == "INVALID_INPUT")
+				switch (strtoupper($ret))
 				{
-					$main_msg = JText::_('PLG_REDSHOP_VIES_REGISTRATION_COUNTRYCODE_IS_INVALID');
-				}
-				elseif (strtoupper($ret) == "SERVICE_UNAVAILABLE")
-				{
-					$main_msg = JText::_('PLG_REDSHOP_VIES_REGISTRATION_SOAP_UNAVAILABLE');
-				}
-				elseif (strtoupper($ret) == "MS_UNAVAILABLE")
-				{
-					$main_msg = JText::_('PLG_REDSHOP_VIES_REGISTRATION_MEMBER_STATE_UNAVAILABLE');
-				}
-				elseif (strtoupper($ret) == "TIMEOUT")
-				{
-					$main_msg = JText::_('PLG_REDSHOP_VIES_REGISTRATION_MEMBER_STATE_NOT_BE_REACHED');
-				}
-				elseif (strtoupper($ret) == "SERVER_BUSY")
-				{
-					$main_msg = JText::_('PLG_REDSHOP_VIES_REGISTRATION_SERVICE_CANNOT_PROCESS');
-				}
-				else
-				{
-					$main_msg = JText::_('PLG_REDSHOP_VIES_REGISTRATION_INVALID');
+					case 'INVALID_INPUT':
+						$main_msg = JText::_('PLG_REDSHOP_VIES_REGISTRATION_COUNTRYCODE_IS_INVALID');
+						break;
+					case 'SERVICE_UNAVAILABLE':
+						$main_msg = JText::_('PLG_REDSHOP_VIES_REGISTRATION_SOAP_UNAVAILABLE');
+						break;
+					case 'MS_UNAVAILABLE':
+						$main_msg = JText::_('PLG_REDSHOP_VIES_REGISTRATION_MEMBER_STATE_UNAVAILABLE');
+						break;
+					case 'TIMEOUT':
+						$main_msg = JText::_('PLG_REDSHOP_VIES_REGISTRATION_MEMBER_STATE_NOT_BE_REACHED');
+						break;
+					case 'SERVER_BUSY':
+						$main_msg = JText::_('PLG_REDSHOP_VIES_REGISTRATION_SERVICE_CANNOT_PROCESS');
+						break;
+					default:
+						$main_msg = JText::_('PLG_REDSHOP_VIES_REGISTRATION_INVALID');
+						break;
 				}
 
-				?>
-				<td>
-					<?php echo JText::_('PLG_REDSHOP_VIES_REGISTRATION_VALIDATION') . ": "; ?>
-					<?php
-					if ($main_msg != "")
-					{
-						echo $main_msg . "<br>";
-					}
-					?>
-					<label class="radio"><input type="radio" name="vies_status_invalid" id="vies_status_invalid1" value="0" >&nbsp;
-						<?php echo JText::_('PLG_REDSHOP_VIES_REGISTRATION_VALIDATION_STATUS1'); ?>
-					</label>
-					<label class="radio"><input type="radio" name="vies_status_invalid" id="vies_status_invalid2" value="1" checked="checked">&nbsp;
-						<?php echo JText::_('PLG_REDSHOP_VIES_REGISTRATION_VALIDATION_STATUS2'); ?>
-					</label>
-					<input type="hidden" name="vies_vat_number" value="" id="vies_vat_number">
-					<input type="hidden" name="vies_status" value="Invalid" id="vies_status">
-				</td>
-				<?php
+				$result = JText::_('PLG_REDSHOP_VIES_REGISTRATION_VALIDATION') . ": " . $main_msg;
 			}
 		}
+
+		echo $result;
 
 		exit;
 	}
