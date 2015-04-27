8b8b163a6beef29e4084d98b3d61d090c5fde586
Gunjan Patel <gunjan@redcomponent.com>
2015-04-27 16:19:25 +0530

    REDSHOP-1891 Adding Capture and Refund functionality

--- plugins/redshop_payment/klarna/klarna.php
+++ plugins/redshop_payment/klarna/klarna.php
@@ -23,6 +23,29 @@ class plgRedshop_PaymentKlarna extends JPlugin
 	protected $autoloadLanguage = true;
 
 	/**
+	 * Constructor
+	 *
+	 * @param   object  &$subject  The object to observe
+	 * @param   array   $config    An optional associative array of configuration settings.
+	 *                             Recognized key values include 'name', 'group', 'params', 'language'
+	 *                             (this list is not meant to be comprehensive).
+	 *
+	 * @since   11.1
+	 */
+	public function __construct(&$subject, $config = array())
+	{
+		define('JPATH_PLUGIN_KLARNA_LIBRARY', JPATH_SITE . '/plugins/redshop_payment/klarna/library/klarna/');
+
+		require_once JPATH_PLUGIN_KLARNA_LIBRARY . 'Klarna.php';
+
+		// Dependencies from http://phpxmlrpc.sourceforge.net/
+		require_once JPATH_PLUGIN_KLARNA_LIBRARY . '/transport/xmlrpc-3.0.0.beta/lib/xmlrpc.inc';
+		require_once JPATH_PLUGIN_KLARNA_LIBRARY . '/transport/xmlrpc-3.0.0.beta/lib/xmlrpc_wrappers.inc';
+
+		parent::__construct($subject, $config);
+	}
+
+	/**
 	 * This method will be triggered on before placing order to reserve amount in klarna.
 	 *
 	 * @param   string  $element  Name of the payment plugin
@@ -37,17 +60,9 @@ class plgRedshop_PaymentKlarna extends JPlugin
 			return;
 		}
 
-		$extraField = new extraField;
-
-		define('JPATH_PLUGIN_KLARNA_LIBRARY', JPATH_SITE . '/plugins/redshop_payment/klarna/library/klarna/');
-
-		require_once JPATH_PLUGIN_KLARNA_LIBRARY . 'Klarna.php';
-
-		// Dependencies from http://phpxmlrpc.sourceforge.net/
-		require_once JPATH_PLUGIN_KLARNA_LIBRARY . '/transport/xmlrpc-3.0.0.beta/lib/xmlrpc.inc';
-		require_once JPATH_PLUGIN_KLARNA_LIBRARY . '/transport/xmlrpc-3.0.0.beta/lib/xmlrpc_wrappers.inc';
-
-		$k = new Klarna();
+		$orderHelper = new order_functions;
+		$extraField  = new extraField;
+		$k           = new Klarna;
 
 		$k->config(
 			$this->params->get('merchantId'),
@@ -60,7 +75,6 @@ class plgRedshop_PaymentKlarna extends JPlugin
 			'./pclasses.json'     // PClass storage URI path
 		);
 
-		$orderHelper = new order_functions;
 		$orderItems = $orderHelper->getOrderItemDetail($data['order_id']);
 
 		foreach ($orderItems as $orderItem)
@@ -159,6 +173,18 @@ class plgRedshop_PaymentKlarna extends JPlugin
 				$data['billinginfo']->users_info_id
 			);
 
+			$dateOfBirth = $extraField->getSectionFieldDataList(
+				$this->params->get('privateDOB'),
+				extraField::SECTION_PRIVATE_BILLING_ADDRESS,
+				$data['billinginfo']->users_info_id
+			);
+
+			$gender = $extraField->getSectionFieldDataList(
+				$this->params->get('privateGender'),
+				extraField::SECTION_PRIVATE_BILLING_ADDRESS,
+				$data['billinginfo']->users_info_id
+			);
+
 			if ((int) $data['billinginfo']->is_company)
 			{
 				$pnoInfo = $extraField->getSectionFieldDataList(
@@ -166,48 +192,90 @@ class plgRedshop_PaymentKlarna extends JPlugin
 					extraField::SECTION_COMPANY_BILLING_ADDRESS,
 					$data['billinginfo']->users_info_id
 				);
+
+				$dateOfBirth = $extraField->getSectionFieldDataList(
+					$this->params->get('companyDOB'),
+					extraField::SECTION_COMPANY_BILLING_ADDRESS,
+					$data['billinginfo']->users_info_id
+				);
+
+				$gender = $extraField->getSectionFieldDataList(
+					$this->params->get('companyGender'),
+					extraField::SECTION_COMPANY_BILLING_ADDRESS,
+					$data['billinginfo']->users_info_id
+				);
 			}
 
-			$data['billinginfo']->pno = (count($pnoInfo) > 0) ? $pnoInfo->data_txt : '';
+			$data['billinginfo']->pno    = (count($pnoInfo) > 0) ? $pnoInfo->data_txt : '';
+			$data['billinginfo']->gender = '';
 
-			$result = $k->reserveAmount(
-				$data['billinginfo']->pno,
-				null, // KlarnaFlags::MALE, KlarnaFlags::FEMALE (AT/DE/NL only) @todo
-				-1,   // Automatically calculate and reserve the cart total amount
-				KlarnaFlags::NO_FLAG,
-				KlarnaPClass::INVOICE
-			);
-
-			$reservation = $result[0];
-			$status      = $result[1];
-
-			$values                 = new stdClass;
-			$values->order_id       = $data['order_id'];
-			$values->transaction_id = $reservation;
-
-			if ($status == KlarnaFlags::ACCEPTED)
+			if (in_array($data['billinginfo']->country_2_code, array('AT', 'DE', 'NL')))
 			{
-				$values->order_status_code         = $this->params->get('verify_status', '');
-				$values->order_payment_status_code = 'Paid';
+				$data['billinginfo']->pno    = (count($dateOfBirth) > 0) ? str_replace('-', '', $dateOfBirth->data_txt) : '';
 
-				$values->log = JText::_('PLG_KLARNA_ORDER_PLACED');
-				$values->msg = JText::_('PLG_KLARNA_ORDER_PLACED');
+				if (count($gender) > 0)
+				{
+					$data['billinginfo']->gender = KlarnaFlags::FEMALE;
+
+					if ('m' == $dateOfBirth->data_txt)
+					{
+						$data['billinginfo']->gender = KlarnaFlags::MALE;
+					}
+				}
 			}
+
+			$paymentInfo = $orderHelper->getOrderPaymentDetail($data['order_id']);
+
+			// Reserve amount only for new orders.
+			if ('' == trim($paymentInfo[0]->order_payment_trans_id))
+			{
+				$result = $k->reserveAmount(
+					$data['billinginfo']->pno,
+					$data['billinginfo']->gender,
+					-1,   // Automatically calculate and reserve the cart total amount
+					KlarnaFlags::NO_FLAG,
+					KlarnaPClass::INVOICE
+				);
+
+				$reservation = $result[0];
+				$status      = $result[1];
+
+				$values                 = new stdClass;
+				$values->order_id       = $data['order_id'];
+				$values->transaction_id = $reservation;
+
+				if ($status == KlarnaFlags::ACCEPTED || $status == KlarnaFlags::PENDING)
+				{
+					$values->order_status_code         = $this->params->get('verify_status', '');
+					$values->order_payment_status_code = 'Paid';
+					$values->log                       = JText::_('PLG_KLARNA_ORDER_PLACED');
+					$values->msg                       = JText::_('PLG_KLARNA_ORDER_PLACED');
+
+					if ($status == KlarnaFlags::PENDING)
+					{
+						$values->order_payment_status_code = 'Unpaid';
+						$values->log                       = JText::_('PLG_KLARNA_ORDER_PENDING_STATUS_APPROVED');
+						$values->msg                       = JText::_('PLG_KLARNA_ORDER_PENDING_STATUS_APPROVED');
+					}
+				}
+				else
+				{
+					$values->order_status_code         = $this->params->get('invalid_status', '');
+					$values->order_payment_status_code = 'Unpaid';
+
+					$values->log = JText::_('PLG_KLARNA_NOT_PLACED');
+					$values->msg = JText::_('PLG_KLARNA_NOT_PLACED');
+				}
+
+				// Change order status based on Klarna status response
+				$this->klarnaOrderReservationUpdate($values);
+			}
+
+			// Update order if transaction placed already.
 			else
 			{
-				$values->order_status_code         = $this->params->get('invalid_status', '');
-				$values->order_payment_status_code = 'Unpaid';
-
-				$values->log = JText::_('PLG_KLARNA_NOT_PLACED');
-				$values->msg = JText::_('PLG_KLARNA_NOT_PLACED');
+				$k->update($paymentInfo[0]->order_payment_trans_id);
 			}
-
-			// Change order status based on Klarna status response
-			$this->klarnaOrderReservationUpdate($values);
-
-			/*// $status is KlarnaFlags::PENDING or KlarnaFlags::ACCEPTED.
-			echo "OK: reservation {$reservation} - order status {$status}\n";
-			die;*/
 		}
 		catch(Exception $e)
 		{
@@ -248,6 +316,8 @@ class plgRedshop_PaymentKlarna extends JPlugin
 
 	/**
 	 * Handle Payment notification from Epay
+	 * This evet is not needed to verify the status, we are doing in different way for Klarna.
+	 * This method is only to ignore.
 	 *
 	 * @param   string  $element  Plugin Name
 	 * @param   array   $request  Request data sent from Epay
@@ -260,8 +330,148 @@ class plgRedshop_PaymentKlarna extends JPlugin
 		{
 			return false;
 		}
-
-		// This evet is not needed to verify the status, we are doing in different way for Klarna.
-		// This method is only to ignore.
 	}
+
+	/**
+	 * This method will be trigger on order status change to capture order ammount.
+	 *
+	 * @param   string  $element  Name of plugin
+	 * @param   array   $data     Order Information array
+	 *
+	 * @return  object  Success or failed message
+	 */
+	public function onCapture_PaymentKlarna($element, $data)
+	{
+		$transactionId = $data['order_transactionid'];
+
+		if ('' == $transactionId)
+		{
+			return;
+		}
+
+		$db  = JFactory::getDbo();
+		$app = JFactory::getApplication();
+		$k   = new Klarna;
+
+		$k->config(
+			$this->params->get('merchantId'),
+			$this->params->get('sharedSecret'),
+			KlarnaCountry::fromCode($this->params->get('purchaseCountry')),
+			KlarnaLanguage::fromCode($this->params->get('purchaseLanguage')),
+			KlarnaCurrency::fromCode(strtolower($this->params->get('purchaseCurrency'))),
+			Klarna::BETA,         // Server
+			'json',               // PClass storage
+			'./pclasses.json'     // PClass storage URI path
+		);
+
+		$return = new stdClass;
+
+		try
+		{
+			$result = $k->activate(
+				$transactionId,
+				null,    // OCR Number
+				KlarnaFlags::RSRV_SEND_BY_EMAIL
+			);
+
+			// For optional arguments, flags, partial activations and so on, refer to the documentation.
+			// See Klarna::setActivateInfo
+
+			// "ok" or "no_risk"
+			$risk = $result[0];
+			$invNo = $result[1];
+
+			if ('ok' == $risk || 'no_risk' == $risk)
+			{
+				// Update transaction string
+				$query = $db->getQuery(true)
+						->update($db->qn('#__redshop_order_payment'))
+						->set($db->qn('order_payment_trans_id') . ' = ' . $db->q($invNo))
+						->where($db->qn('order_id') . ' = ' . $db->q($data['order_id']));
+
+				// Set the query and execute the update.
+				$db->setQuery($query)->execute();
+
+				$return->responsestatus = 'Success';
+				$return->message        = JText::_('PLG_REDSHOP_PAYMENT_KLARNA_PAYMENT_CAPTURE_SUCCESS');
+				$return->type           = 'message';
+			}
+			else
+			{
+				$return->responsestatus = 'Fail';
+				$return->message        = JText::_('PLG_REDSHOP_PAYMENT_KLARNA_PAYMENT_CAPTURE_FAIL');
+				$return->type           = 'error';
+			}
+		}
+		catch(Exception $e)
+		{
+			$return->responsestatus = 'Fail';
+			$return->message        = $e->getMessage() . ' #' . $e->getCode();
+			$return->type           = 'error';
+		}
+
+		$app->enqueueMessage($return->message, $return->type);
+
+		return $return;
+	}
+
+	/**
+	 * Refund amount on cancel order
+	 *
+	 * @param   string  $element  Plugin Name
+	 * @param   array   $data     Order Transaction information
+	 *
+	 * @return  object  Return status information
+	 */
+	public function onStatus_PaymentKlarna($element, $data)
+	{
+		if ($element != 'klarna')
+		{
+			return;
+		}
+
+		$transactionId = $data['order_transactionid'];
+
+		if ('' == $transactionId)
+		{
+			return;
+		}
+
+		$db  = JFactory::getDbo();
+		$app = JFactory::getApplication();
+		$k   = new Klarna;
+
+		$k->config(
+			$this->params->get('merchantId'),
+			$this->params->get('sharedSecret'),
+			KlarnaCountry::fromCode($this->params->get('purchaseCountry')),
+			KlarnaLanguage::fromCode($this->params->get('purchaseLanguage')),
+			KlarnaCurrency::fromCode(strtolower($this->params->get('purchaseCurrency'))),
+			Klarna::BETA, // Server
+			'json',
+			'./pclasses.json'
+		);
+
+		$return = new stdClass;
+
+		try
+		{
+			$k->creditInvoice($transactionId);
+
+			$return->responsestatus = 'Success';
+			$return->type           = 'message';
+			$return->message = JText::_('PLG_REDSHOP_PAYMENT_KLARNA_PAYMENT_REFUND_SUCCESS');
+		}
+		catch(Exception $e)
+		{
+			$return->responsestatus = 'Fail';
+			$return->message        = $e->getMessage() . ' #' . $e->getCode();
+			$return->type           = 'error';
+		}
+
+		$app->enqueueMessage($return->message, $return->type);
+
+		return $return;
+	}
+
 }
