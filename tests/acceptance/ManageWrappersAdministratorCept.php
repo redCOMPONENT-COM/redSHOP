f11bb13d5917d113823dc56d75cf56a9de9f4cde
puneet0191 <puneet.0191@gmail.com>
2015-05-11 22:08:53 +0530

    REDSHOP-2409 Updating Cept files Accordingly

--- tests/acceptance/ManageWrappersAdministratorCept.php
+++ tests/acceptance/ManageWrappersAdministratorCept.php
@@ -5,18 +5,12 @@
  * @copyright   Copyright (C) 2008 - 2015 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
-$scenario->group('Joomla3');
 
 // Load the Step Object Page
 $I = new AcceptanceTester($scenario);
-$config = $I->getConfig();
-$className = 'AcceptanceTester\Login' . $config['env'] . 'Steps';
-$I = new $className($scenario);
-
 $I->wantTo('Test Wrapper Manager in Administrator');
-$I->doAdminLogin();
-$config = $I->getConfig();
-$className = 'AcceptanceTester\WrapperManager' . $config['env'] . 'Steps';
+$I->doAdministratorLogin();
+$className = 'AcceptanceTester\WrapperManagerJoomla3Steps';
 $I = new $className($scenario);
 $name = 'Sample Wrapper ' . rand(10, 100);
 $price = rand(100, 1000);
@@ -30,5 +24,3 @@ $I->changeWrapperState($newName);
 $I->verifyState('unpublished', $I->getWrapperState($newName));
 $I->deleteWrapper($newName);
 $I->searchWrapper($newName, 'Delete');
-
-
