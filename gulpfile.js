e90ebd6f6ab2135c59f976099311d13105013d6d
Gunjan Patel <gunjan@redcomponent.com>
2015-05-21 15:04:06 +0530

    Adding Gulp - New build system update

---
+++ gulpfile.js
@@ -0,0 +1,119 @@
+var gulp = require('gulp'),
+
+// ZIP compress files
+zip = require('gulp-zip'),
+
+// Utility functions for gulp plugins
+gutil = require('gulp-util')
+//notify = require("gulp-notify"),
+
+// File systems
+fs    = require('fs'),
+path  = require('path'),
+merge = require('merge-stream'),
+
+// Gulp Configuration
+config = require('./gulp-config.json')
+
+// Extension Configuration file
+extensionConfig = require('./package.json');
+
+function getFolders(dir) {
+    return fs.readdirSync(dir)
+      .filter(function(file) {
+        return fs.statSync(path.join(dir, file)).isDirectory();
+      });
+}
+
+function showHelp(){
+	gutil.log(
+		gutil.colors.white.bold.bgMagenta(
+			'\n\n\nFollowing tasks and switches are available:\n\n\t 1. gulp release:component \n\t\t Use this command to release component. Version and other information can be set in gulp-config.json file. \n\n\t 2. gulp release:extensions \n\t\t This command is to release the extensions.\n\t\t This command will read the base directory and create zip files for each of the folder. \n\t\t === Switches === \n\t --folder {source direcory}  Default: "./plugins" \n\t --suffix {text of suffix}   Default: "plg_"\n\n\t Example Usage: \n\t\t gulp release:extensions --folder ./modules --suffix ext_ \n\n\n'
+		)
+	);
+}
+
+// Creating zip files for  Extensions
+gulp.task('release:extensions', function() {
+
+	// Source directory for read and prepare for zip
+	var srcFolder = gutil.env.folder || './plugins',
+
+	// Read all the folders in given source directory
+	folders = getFolders(srcFolder),
+
+	// Extension package name suffix
+	extSuffix = gutil.env.suffix || 'plg_';
+
+	// Display log
+	gutil.log(gutil.colors.white.bgBlue(folders.length) + gutil.colors.blue.bold(' extensions are ready for release'));
+
+	// Loop through the folders and create zip files for each of them.
+	var tasks;
+
+	folders.map(function(folder) {
+
+		var plugins = getFolders(path.join(srcFolder, folder));
+
+		// Display name of the folder
+		gutil.log(gutil.colors.blue.bold.italic(folder + ' (' + plugins.length + ')'));
+
+		tasks = plugins.map(function(plugin) {
+
+			gutil.log(gutil.colors.red.bold.italic('-' + plugin));
+
+			return gulp.src(
+					path.join(srcFolder, folder, plugin, '**')
+				)
+			    .pipe(
+			    	zip(
+			    		extSuffix + config.releasePkgName + plugin + '_' + 'v' + config.version + '.zip'
+			    	)
+			    )
+				.pipe(
+					gulp.dest(
+						path.join(config.packageDir, srcFolder.split(path.sep)[1], folder)
+					)
+				);
+		});
+	});
+
+	return merge(tasks);
+});
+
+// Creating zip files for Component
+gulp.task('release:component', function() {
+
+	if (!config.packageFiles || (config.packageFiles && config.packageFiles.length <= 0))
+	{
+		gutil.log(
+			gutil.colors.white.bgRed(
+				'ERROR: Please specify `packageFiles` in gulp-config.json or make sure you have added files list'
+			)
+		);
+
+		return false;
+	}
+
+	gutil.log(gutil.colors.white.bgGreen('Preparing release for component'));
+
+	gulp.src(config.packageFiles, {base: '.'})
+		.pipe(zip(config.releasePkgName + 'v' + config.version + '_' + config.joomlaVersion + '.zip'))
+		.pipe(gulp.dest(config.packageDir));
+
+	gutil.log(gutil.colors.white.bgGreen('Component packages are ready at ' + config.packageDir))
+});
+
+gulp.task(
+	'release',
+	[
+		'release:component',
+		'release:extensions'
+	],
+	function() {
+
+});
+
+gulp.task('default', function() {
+	showHelp();
+});
