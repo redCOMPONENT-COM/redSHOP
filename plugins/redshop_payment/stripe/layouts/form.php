77d6fe5e5f3a6e216307b989ff5f76523f92d41a
Gunjan Patel <gunjan@redcomponent.com>
2015-05-27 12:47:21 +0530

    REDSHOP-2495 Adding notify and after notify functions in stripe

--- plugins/redshop_payment/stripe/layouts/form.php
+++ plugins/redshop_payment/stripe/layouts/form.php
@@ -13,16 +13,18 @@ $data    = $displayData['data'];
 $params  = $displayData['params'];
 $price   = $data['order']->order_total;
 
+$name        = $params->get('dataName', SHOP_NAME);
+$description = JText::sprintf('PLG_REDSHOP_PAYMENT_STRIPE_PAYMENT_DESCRIPTION', $data['order_id']);
 ?>
-<h3><?php echo SHOP_NAME; ?></h3>
+<h3><?php echo $name; ?></h3>
 <form action="<?php echo $action; ?>" method="POST">
 	<script
 		src="https://checkout.stripe.com/checkout.js" class="stripe-button"
 		data-key="<?php echo $params->get('publishableKey'); ?>"
 		data-amount="<?php echo round($price * 100); ?>"
 		data-currency="<?php echo CURRENCY_CODE; ?>"
-		data-name="<?php echo SHOP_NAME; ?>"
-		data-description="<?php echo SHOP_NAME; ?>"
-		data-image="<?php echo $params->get('imagePath', 'plugins/redshop_payment/stripe/library/128.png'); ?>">
+		data-name="<?php echo $name; ?>"
+		data-description="<?php echo $description; ?>"
+		data-image="<?php echo $params->get('logo', 'plugins/redshop_payment/stripe/library/128.png'); ?>">
 	</script>
 </form>
