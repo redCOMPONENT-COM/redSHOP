635e3bfe2e632e37a066c6e9d96de1ae585e63eb
puneet0191 <puneet.0191@gmail.com>
2015-12-04 21:38:36 +0530

    REDSHOP-2694 Removing stuff thats not needed.

--- tests/acceptance/administrator/ManageMediaAdministratorCest.php
+++ tests/acceptance/administrator/ManageMediaAdministratorCest.php
@@ -17,18 +17,13 @@ use \AcceptanceTester;
  */
 class ManageMediaAdministratorCest
 {
-	public function __construct()
-	{
-		$this->faker = Faker\Factory::create();
-	}
-
 	/**
 	 * Function to Test Media Creation in Backend
 	 *
 	 */
 	public function createMedia(AcceptanceTester $I, $scenario)
 	{
-		$I->wantTo('Test Manufacture creation in Administrator');
+		$I->wantTo('Test Media creation in Administrator');
 		$I->doAdministratorLogin();
 		$I = new AcceptanceTester\MediaManagerJoomla3Steps($scenario);
 		$I->addMedia();
