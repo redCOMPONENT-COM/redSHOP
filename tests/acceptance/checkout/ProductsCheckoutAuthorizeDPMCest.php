4105713097aca8dd08f28e880219f25cd170b4a2
javier gomez <javier.gomez.corio@gmail.com>
2016-01-18 13:37:27 +0100

    Purchase random product

--- tests/acceptance/checkout/ProductsCheckoutAuthorizeDPMCest.php
+++ tests/acceptance/checkout/ProductsCheckoutAuthorizeDPMCest.php
@@ -62,8 +62,39 @@ class ProductsCheckoutAuthorizeDPMCest
 			"state" => "Karnataka",
 			"phone" => "8787878787"
 		);
-		$productName = 'redCOOKIE';
-		$categoryName = 'Events and Forms';
+
+		$randomNumber = rand(10, 1000);
+		if (($randomNumber % 2) == 1)
+		{
+			$productRandomizer = rand(10, 1000);
+
+			if (($productRandomizer % 2) == 1)
+			{
+				$productName = 'redSLIDER';
+			}
+			else
+			{
+				$productName = 'redCOOKIE';
+			}
+
+			$categoryName = 'Events and Forms';
+		}
+		else
+		{
+			$productRandomizer = rand(10, 1000);
+
+			if (($productRandomizer % 2) == 1)
+			{
+				$productName = 'redSHOP';
+			}
+			else
+			{
+				$productName = 'redITEM';
+			}
+
+			$categoryName = 'CCK and e-Commerce';
+		}
+
 		$this->checkoutProductWithAuthorizeDPMPayment($I, $scenario, $customerInformation, $customerInformation, $checkoutAccountInformation, $productName, $categoryName);
 	}
 
