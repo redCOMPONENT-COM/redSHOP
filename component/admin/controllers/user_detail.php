d3e7af6b381de88a32e2951edaf79175216ed703
ducredweb <duc@redweb.dk>
2016-01-22 12:59:29 +0700

    Update user_detail.php

--- component/admin/controllers/user_detail.php
+++ component/admin/controllers/user_detail.php
@@ -87,7 +87,7 @@ class RedshopControllerUser_detail extends RedshopController
 
 		$model = $this->getModel('user_detail');
 
-		if (!$model->delete($delete_joomla_users, $cid)
+		if (!$model->delete($delete_joomla_users, $cid))
 		{
 			echo "<script> alert('" . $model->getError(true) . "'); window.history.go(-1); </script>\n";
 		}
