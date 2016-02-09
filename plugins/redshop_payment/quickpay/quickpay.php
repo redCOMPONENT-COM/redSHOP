3cf9358f1e4c470b54f34b91262b2011504c4037
Gunjan Patel <gunjan@redcomponent.com>
2016-02-09 17:39:11 +0530

    REDSHOP-2780 #comment Updating quickpay payment api v10

--- plugins/redshop_payment/quickpay/quickpay.php
+++ plugins/redshop_payment/quickpay/quickpay.php
@@ -52,8 +52,6 @@ class PlgRedshop_PaymentQuickpay extends RedshopPayment
 			$orderId = $orderInfo['order_id'];
 		}
 
-
-
 		$params = array(
 			'version'         => "v10",
 			'merchant_id'     => $this->params->get("merchantId"),
@@ -65,10 +63,7 @@ class PlgRedshop_PaymentQuickpay extends RedshopPayment
 			'cancelurl'       => $this->getNotifyUrl($orderInfo['order_id']),
 			'callbackurl'     => $this->getNotifyUrl($orderInfo['order_id']),
 			'language'        => $this->getLang(),
-			'autocapture'     => $this->params->get("autoCapture"),
-			/*'variables'       => array(
-				"abc" => $orderInfo['order_id']
-			)*/
+			'autocapture'     => $this->params->get("autoCapture")
 		);
 
 		$paymentMethods = $this->params->get("paymentMethods");
@@ -79,38 +74,6 @@ class PlgRedshop_PaymentQuickpay extends RedshopPayment
 		}
 
 		$params["checksum"] = $this->sign($params, $this->params->get("apiKey"));
-
-		/*$params['invoice_address'] = array(
-			'name'            => $orderInfo['billinginfo']->firstname . ' ' . $orderInfo['billinginfo']->lastname,
-			'att'             => '',
-			'street'          => $orderInfo['billinginfo']->address,
-			'house_number'    => '',
-			'house_extension' => '',
-			'zip_code'        => $orderInfo['billinginfo']->zipcode,
-			'city'            => $orderInfo['billinginfo']->city,
-			'region'          => '',
-			'country_code'    => $orderInfo['billinginfo']->country_code,
-			'vat_no'          => '',
-			'phone_number'    => $orderInfo['billinginfo']->phone,
-			'mobile_number'   => '',
-			'email'           => $orderInfo['billinginfo']->user_email
-		);
-
-		$params['shipping_address'] = array(
-			'name'            => $orderInfo['shippinginfo']->firstname . ' ' . $orderInfo['shippinginfo']->lastname,
-			'att'             => '',
-			'street'          => $orderInfo['shippinginfo']->address,
-			'house_number'    => '',
-			'house_extension' => '',
-			'zip_code'        => $orderInfo['shippinginfo']->zipcode,
-			'city'            => $orderInfo['shippinginfo']->city,
-			'region'          => '',
-			'country_code'    => $orderInfo['shippinginfo']->country_code,
-			'vat_no'          => '',
-			'phone_number'    => $orderInfo['shippinginfo']->phone,
-			'mobile_number'   => '',
-			'email'           => $orderInfo['shippinginfo']->user_email
-		);*/
 
 		return $params;
 	}
@@ -132,10 +95,17 @@ class PlgRedshop_PaymentQuickpay extends RedshopPayment
 
 		$input = JFactory::getApplication()->input;
 
-		$accountId = $input->post->get('id');
+		$orderId = $input->getInt('orderid');
+
+		$requestBody = file_get_contents("php://input");
+
+		$response = json_decode($requestBody);
+
+		$merchantId    = $response->merchant_id;
+		$transactionId = $response->id;
 
 		// Invalid request
-		if ($accountId !== $this->params->get("customerId"))
+		if ($response->merchant_id != $this->params->get("merchantId"))
 		{
 			JLog::add('Invalid Request', JLog::ERROR, 'jerror');
 
@@ -143,49 +113,273 @@ class PlgRedshop_PaymentQuickpay extends RedshopPayment
 		}
 
 		// Invalid Signature
-		if (!$this->isSignatureValidated($input->post))
+		if (!$this->isSignatureValidated())
 		{
 			JLog::add('Invalid Signature', JLog::ERROR, 'jerror');
 
 			return $this->setStatus(
 				$orderId,
 				$transactionId,
-				$this->params->get('invalid_status', ''),
+				$this->params->get('cancel_status', ''),
 				'Unpaid',
 				JText::_('PLG_REDSHOP_PAYMENT_QUICKPAY_PAYMENT_INVALID'),
 				JText::_('PLG_REDSHOP_PAYMENT_QUICKPAY_PAYMENT_INVALID_LOG')
 			);
 		}
 
-		$transactionId = $input->post->getString('operation_number');
-		$orderId       = $input->post->getInt('control');
+		$operations = $response->operations;
+		$operation = $operations[count($operations) - 1];
+
+		if (!$response->accepted)
+		{
+			return $this->setStatus(
+				$orderId,
+				$transactionId,
+				$this->params->get('invalid_status', ''),
+				'Unpaid',
+				$operation->qp_status_msg . '<br />' . $operation->aq_status_msg,
+				$operation->qp_status_msg . '<br />' . $operation->aq_status_msg
+			);
+		}
+
+		if ($operation->pending
+				|| $operation->qp_status_code != 20000
+				|| $operation->aq_status_code != 20000)
+		{
+			return $this->setStatus(
+				$orderId,
+				$transactionId,
+				$this->params->get('cancel_status', ''),
+				'Unpaid',
+				$operation->qp_status_msg . '<br />' . $operation->aq_status_msg,
+				JText::_('PLG_REDSHOP_PAYMENT_QUICKPAY_PAYMENT_REJECTED_LOG')
+			);
+		}
 
 		// Set order status based QuickPay post notification.
-		switch($input->post->get('operation_status'))
+		switch($operation->type)
 		{
-			case 'completed':
-				return $this->setStatus(
-					$orderId,
-					$transactionId,
-					$this->params->get('verify_status', ''),
-					'Paid',
-					JText::_('PLG_REDSHOP_PAYMENT_QUICKPAY_PAYMENT_SUCCESS'),
-					JText::_('PLG_REDSHOP_PAYMENT_QUICKPAY_PAYMENT_SUCCESS_LOG')
-				);
+			case 'authorize':
+
+				if ('pending' == $response->state || 'new' == $response->state)
+				{
+					return $this->setStatus(
+						$orderId,
+						$transactionId,
+						$this->params->get('verify_status', ''),
+						'Unpaid',
+						$operation->qp_status_msg . '<br />' . $operation->aq_status_msg,
+						JText::_('PLG_REDSHOP_PAYMENT_QUICKPAY_PAYMENT_SUCCESS_LOG')
+					);
+				}
+
 			break;
-			case 'rejected':
+			case 'capture':
 				return $this->setStatus(
 					$orderId,
 					$transactionId,
-					$this->params->get('cancel_status', ''),
-					'Unpaid',
-					JText::_('PLG_REDSHOP_PAYMENT_QUICKPAY_PAYMENT_REJECTED'),
-					JText::_('PLG_REDSHOP_PAYMENT_QUICKPAY_PAYMENT_REJECTED_LOG')
+					$this->params->get('capture_status', ''),
+					'Paid',
+					$operation->qp_status_msg . '<br />' . $operation->aq_status_msg,
+					JText::_('PLG_REDSHOP_PAYMENT_QUICKPAY_PAYMENT_SUCCESS_LOG')
 				);
 			break;
 		}
 
 		return;
+	}
+
+	/**
+	 * Send Information on QuickPay using CURL
+	 *
+	 * @param   string  $method  Method to send data
+	 * @param   array   $data    Array of information or null
+	 * @param   string  $type    API type to get information
+	 *
+	 * @return  object           Response in Object
+	 */
+	private function sendQuickpayRequest($method, $data, $type)
+	{
+		// Set up Curl Headers
+		$headers = array(
+			'Accept-Version' => 'v10',
+			'Authorization' => 'Basic ' . base64_encode(':' . $this->params->get('apiUserKey'))
+		);
+
+		// Set Account Creation Url
+		$url = JUri::getInstance('https://api.quickpay.net/' . $type);
+
+		$curl     = new JHttpTransportCurl(new JRegistry);
+		$response = $curl->request($method, $url, $data, $headers);
+
+		return $response;
+	}
+
+	/**
+	 * Method will be trigger on capturing payment.
+	 *
+	 * @param   string  $element  Element Name
+	 * @param   array   $data     Order information in array
+	 *
+	 * @return  object            Return information Object
+	 */
+	public function onCapture_PaymentQuickpay($element, $data)
+	{
+		if ($element != 'quickpay')
+		{
+			return;
+		}
+
+		$sendData = array(
+			'amount' => ($data['order_amount'] * 100)
+		);
+
+		$type     = 'payments/' . $data['order_transactionid'] . '/capture';
+		$response = $this->sendQuickpayRequest('POST', $sendData, $type);
+
+		$body = json_decode($response->body);
+
+		if (202 == $response->code && $body->accepted)
+		{
+			$operations = $body->operations;
+			$operation = $operations[count($operations) - 1];
+
+			if ($operation->pending
+				|| $operation->qp_status_code != 20000
+				|| $operation->aq_status_code != 20000)
+			{
+				$message                = $operation->qp_status_msg . '<br />' . $operation->aq_status_msg;
+				$values->responsestatus = 'Fail';
+				JLog::add($message, JLog::ERROR, 'jerror');
+			}
+			else
+			{
+				$values->responsestatus = 'Success';
+				$message                = $operation->qp_status_msg . '<br />' . $operation->aq_status_msg;
+			}
+		}
+		else
+		{
+			$message                = $body->message . ' Error: Amount ' . implode('<br />', $body->errors->amount);
+			$values->responsestatus = 'Fail';
+			JLog::add($message, JLog::ERROR, 'jerror');
+		}
+
+		$values->message = $message;
+
+		return $values;
+	}
+
+	/**
+	 * Method will be trigger on cancelling order
+	 *
+	 * @param   string  $element  Element name
+	 * @param   array   $data     Order info
+	 *
+	 * @return  object            Response to update order status
+	 */
+	public function onStatus_PaymentQuickpay($element, $data)
+	{
+		if ($element != 'quickpay')
+		{
+			return;
+		}
+
+		$type = 'payments/' . $data['order_transactionid'];
+
+		$response  = $this->sendQuickpayRequest('GET', null, $type);
+
+		$body = json_decode($response->body);
+
+		if (200 != $response->code)
+		{
+			$values->message        = $body->message;
+			$values->responsestatus = 'Fail';
+			JLog::add($values->message, JLog::ERROR, 'jerror');
+
+			return $values;
+		}
+
+		if ('processed' == $body->state)
+		{
+			return $this->doRefund($data);
+		}
+
+		return $this->doCancel($data);
+	}
+
+	/**
+	 * Method to do refund
+	 *
+	 * @param   array  $data  Order Information
+	 *
+	 * @return  object         Response object to update order status
+	 */
+	protected function doRefund($data)
+	{
+		// Only allow refund if enabled.
+		if (!(boolean) $this->params->get('refund'))
+		{
+			return;
+		}
+
+		$sendData = array(
+			'amount' => ($data['order_amount'] * 100)
+		);
+
+		$type = 'payments/' . $data['order_transactionid'] . '/refund';
+
+		$response  = $this->sendQuickpayRequest('POST', $sendData, $type);
+
+		$body = json_decode($response->body);
+
+		if (202 == $response->code && $body->accepted)
+		{
+			$values->responsestatus = 'Success';
+			$message                = JText::_('PLG_REDSHOP_PAYMENT_QUICKPAY_PAYMENT_CAPTURED');
+		}
+		else
+		{
+			$message                = $body->message . ' Error: Amount ' . implode('<br />', $body->errors->amount);
+			$values->responsestatus = 'Fail';
+			JLog::add($message, JLog::ERROR, 'jerror');
+		}
+
+		$values->message = $message;
+
+		return $values;
+	}
+
+	/**
+	 * Cancel the payment
+	 *
+	 * @param   array  $data  Order Information
+	 *
+	 * @return  object         Return object to update order status log
+	 */
+	protected function doCancel($data)
+	{
+		$type = 'payments/' . $data['order_transactionid'] . '/cancel';
+
+		$response  = $this->sendQuickpayRequest('POST', null, $type);
+
+		$body = json_decode($response->body);
+
+		if (202 == $response->code && $body->accepted)
+		{
+			$values->responsestatus = 'Success';
+			$message                = JText::_('PLG_REDSHOP_PAYMENT_QUICKPAY_PAYMENT_CAPTURED');
+		}
+		else
+		{
+			$message                = $body->message;
+			$values->responsestatus = 'Fail';
+			JLog::add($message, JLog::ERROR, 'jerror');
+		}
+
+		$values->message = $message;
+
+		return $values;
 	}
 
 	/**
@@ -195,36 +389,27 @@ class PlgRedshop_PaymentQuickpay extends RedshopPayment
 	 *
 	 * @return  boolean         True on validate signature
 	 */
-	private function isSignatureValidated($post)
+	private function isSignatureValidated()
 	{
-		$string = $this->params->get('pin') .
-			$post->get('id', '', 'STRING') .
-			$post->get('operation_number', '', 'STRING') .
-			$post->get('operation_type', '', 'STRING') .
-			$post->get('operation_status', '', 'STRING') .
-			$post->get('operation_amount', '', 'STRING') .
-			$post->get('operation_currency', '', 'STRING') .
-			$post->get('operation_withdrawal_amount', '', 'STRING') .
-			$post->get('operation_commission_amount', '', 'STRING') .
-			$post->get('operation_original_amount', '', 'STRING') .
-			$post->get('operation_original_currency', '', 'STRING') .
-			$post->get('operation_datetime', '','STRING') .
-			$post->get('operation_related_number', '', 'STRING') .
-			$post->get('control', '', 'STRING') .
-			$post->get('description', '','STRING') .
-			$post->get('email', '', 'STRING') .
-			$post->get('p_info', '', 'STRING') .
-			$post->get('p_email', '', 'STRING') .
-			$post->get('channel', '', 'STRING') .
-			$post->get('channel_country', '', 'STRING') .
-			$post->get('geoip_country','', 'STRING');
+		$requestBody = file_get_contents("php://input");
+		$checksum    = hash_hmac("sha256", $requestBody, $this->params->get('privateKey'));
 
-		if ($post->get('signature') == hash('sha256', $string))
+		if ($checksum == $_SERVER["HTTP_QUICKPAY_CHECKSUM_SHA256"])
 		{
 			return true;
 		}
+
+		return false;
 	}
 
+	/**
+	 * Hash sign to create checksum to send quickpay
+	 *
+	 * @param   array   $params  Payment Information
+	 * @param   string  $apiKey  Payment Window API Key given from quickpay
+	 *
+	 * @return  string  Prepared hash sign key
+	 */
 	protected function sign($params, $apiKey)
 	{
 		$flattenedParams = $this->flattenParams($params);
@@ -234,6 +419,15 @@ class PlgRedshop_PaymentQuickpay extends RedshopPayment
 		return hash_hmac("sha256", $base, $apiKey);
 	}
 
+	/**
+	 * Flatten the paramters of the array.
+	 *
+	 * @param   object  $obj     Base object to flatten
+	 * @param   array   $result  Final result of array
+	 * @param   array   $path    Path of array depth
+	 *
+	 * @return  array   Final flatten array
+	 */
 	protected function flattenParams($obj, $result = array(), $path = array())
 	{
 		if (is_array($obj))
