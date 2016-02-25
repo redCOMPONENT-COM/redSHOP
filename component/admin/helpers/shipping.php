21326405704382b674caa86ed9229dcfcc8c454b
Gunjan Patel <gunjan@redcomponent.com>
2016-02-25 16:49:21 +0530

    [imp] Rewrite shipping encrypt and decrypt functions

--- component/admin/helpers/shipping.php
+++ component/admin/helpers/shipping.php
@@ -661,7 +661,15 @@ class shipping
 		return $rate . "`" . $total;
 	}
 
-	/******************New Function used in redshop 1.1**********************/
+	/**
+	 * Encrypt Shipping
+	 *
+	 * @param   string  $Str_Message  String to encrypt
+	 *
+	 * @deprecated 1.6  Use RedshopShippingRate::encrypt(array);
+	 *
+	 * @return  string  Encrypt shipping rate
+	 */
 	public function encryptShipping($Str_Message)
 	{
 		$Len_Str_Message       = strlen($Str_Message);
@@ -685,6 +693,15 @@ class shipping
 		return $result;
 	}
 
+	/**
+	 * Decrypt Shipping
+	 *
+	 * @param   string  $Str_Message  String to decrypt
+	 *
+	 * @deprecated 1.6  Use RedshopShippingRate::decrypt(string);
+	 *
+	 * @return  string  Encrypt shipping rate
+	 */
 	public function decryptShipping($Str_Message)
 	{
 		$Str_Message           = base64_decode($Str_Message);
