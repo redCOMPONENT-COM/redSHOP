17ef8fe552b09c7e8bc618c0312a4e058e921a87
ducredweb <duc@redweb.dk>
2016-03-23 14:59:41 +0700

    Update attribute-uncompressed.js

--- media/com_redshop/js/attribute-uncompressed.js
+++ media/com_redshop/js/attribute-uncompressed.js
@@ -1830,10 +1830,11 @@ function discountCalculation(proid) {
 				if (document.getElementById('main_price' + proid))
 				{
 					var product_main_price = document.getElementById('main_price' + proid).value;
+					calculateTotalPrice(proid, 0);
 
 					if (redSHOP.RSConfig._('SHOW_PRICE') == '1' && ( redSHOP.RSConfig._('DEFAULT_QUOTATION_MODE') != '1' || (redSHOP.RSConfig._('DEFAULT_QUOTATION_MODE') && redSHOP.RSConfig._('SHOW_QUOTATION_PRICE'))))
 					{
-						var product_total = parseFloat(product_main_price) + parseFloat(price_total);
+						var product_total = final_price_f - parseFloat(product_main_price) + parseFloat(price_total);
 
 						if (areaPrice[8] == 1) {
 							var product_price_excl_vat = price_total + price_excl_vat * qty;
@@ -1858,8 +1859,6 @@ function discountCalculation(proid) {
 						// set product main price as price total for dynamic price change
 						document.getElementById('main_price' + proid).value = product_price_excl_vat;
 					}
-
-					calculateTotalPrice(proid, 0);
 				}
 			}
 		}
