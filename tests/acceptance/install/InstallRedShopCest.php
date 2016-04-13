e9019ab26fed09fd10449e3bef44bf7b36e74de3
javier gomez <javier.gomez.corio@gmail.com>
2016-04-13 12:17:51 +0200

    [tests] remove floating toolbars to avoid selenium issues

--- tests/acceptance/install/InstallRedShopCest.php
+++ tests/acceptance/install/InstallRedShopCest.php
@@ -52,4 +52,26 @@ class InstallRedShopCest
 			$I->waitForText('Data Installed Successfully', 10, '#system-message-container');
 		}
 	}
+
+	public function disableTemplateFloatingToolbars(AcceptanceTester $I)
+	{
+		$I->am('administrator');
+		$I->wantTo('disable the floating template toolbars');
+		$I->doAdministratorLogin();
+		$I->waitForText('Control Panel', 60, ['css' => 'h1']);
+		$I->click(['link' => 'Extensions']);
+		$I->waitForElement(['link' => 'Templates'],60);
+		$I->click(['link' => 'Templates']);
+		$I->waitForText('Templates: Styles', 60, ['css' => 'h1']);
+		$I->click(['link' => 'isis - Default']);
+		$I->waitForText('Templates: Edit Style', 60, ['css' => 'h1']);
+		$I->click(['link' => 'Advanced']);
+		$I->waitForElement(['css' => "label[data-original-title='<strong>Status Module Position</strong><br />Choose the location of the status module.']"], 60);
+		$I->executeJS("window.scrollTo(0, document.body.scrollHeight);");
+		$I->selectOptionInChosen('Status Module Position', 'Top');
+		$I->selectOptionInRadioField('Pinned Toolbar', 'No');
+		$I->click('Save & Close');
+		$I->waitForText('Style successfully saved.', 60, ['id' => 'system-message-container']);
+		$I->see('Style successfully saved.', ['id' => 'system-message-container']);
+	}
 }
