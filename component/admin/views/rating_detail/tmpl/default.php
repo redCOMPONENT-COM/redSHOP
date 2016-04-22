8fd52c88a04f5cb7b0b9ff273b5ccfb46c462e04
ducredweb <duc@redweb.dk>
2016-04-22 13:19:05 +0700

    REDSHOP-2838 No name in product review by guest
    remove commented code

--- component/admin/views/rating_detail/tmpl/default.php
+++ component/admin/views/rating_detail/tmpl/default.php
@@ -30,15 +30,13 @@ $productHelper = new producthelper;
 			alert("<?php echo JText::_('COM_REDSHOP_RATING_COMMENT_MUST_BE_FILLED', true ); ?>");
 			return false;
 		}
-		// if (form.userid.value == "") {
-		// 	alert("<?php echo JText::_('COM_REDSHOP_RATING_MUST_SELECT_USER', true ); ?>");
-		// 	return false;
-		// }
+		
 		if (form.product_id.value == "") {
 			alert("<?php echo JText::_('COM_REDSHOP_RATING_MUST_SELECT_PRODUCT', true ); ?>");
 			return false;
 		}
-		else {
+		else 
+		{
 			submitform(pressbutton);
 		}
 	}
