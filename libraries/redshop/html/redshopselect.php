2e36d59ef4078d840a168072714a18033e08040e
ducredweb <duc@redweb.dk>
2016-05-13 17:58:22 +0700

    REDSHOP-2473 Issue adding Medias

--- libraries/redshop/html/redshopselect.php
+++ libraries/redshop/html/redshopselect.php
@@ -207,7 +207,7 @@ abstract class JHtmlRedshopSelect extends JHtmlSelect
 
 		$options['select2.options'] = array(
 			'placeholder' => '',
-			'minimumInputLength' => 2,
+			'minimumInputLength' => 0,
 			'width' => 'resolve',
 			'multiple' => 'false',
 			'allowClear' => 'true',
