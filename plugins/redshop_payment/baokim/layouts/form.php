bd715ea6aed5206c0ca32bb412eae740f44bde56
Tu Vuong <tu@redweb.dk>
2016-06-02 10:24:56 +0700

    update code and add language

--- plugins/redshop_payment/baokim/layouts/form.php
+++ plugins/redshop_payment/baokim/layouts/form.php
@@ -25,7 +25,7 @@ $name      = $params->get('dataName', SHOP_NAME);
 <div id="wrapper">
 	<!-- nav -->
 	<div class="nav">
-		<div class="nav_title">Phương thức thanh toán</div>
+		<div class="nav_title"><?php echo JText::_('PLG_REDSHOP_PAYMENT_BAOKIM_PAYMENT'); ?></div>
 	</div>
 	<!--/ end nav -->
 
@@ -36,8 +36,7 @@ $name      = $params->get('dataName', SHOP_NAME);
 				<div class="method row-fluid" id="2">
 					<div class="icon"><img src="<?php echo JURI::root() . 'plugins/redshop_payment/baokim/media/images/creditcard.png'; ?>" border="0"/></div>
 					<div class="info">
-						<span class="title">Thanh toán trực tuyến bằng thẻ quốc tế <!--<img src="images/safe.png" border="0" style="vertical-align: bottom; margin-left: 5px;" />--></span>
-
+						<span class="title"><?php echo JText::_('PLG_REDSHOP_PAYMENT_BAOKIM_ONLINE_VISA'); ?></span>
 						<div class="bank_list"
 							<ul id="b_l">
 								<?php echo $baokim->generateBankImage($banks, PAYMENT_METHOD_TYPE_CREDIT_CARD); ?>
@@ -47,12 +46,11 @@ $name      = $params->get('dataName', SHOP_NAME);
 					</div>
 					<div class="check_box"></div>
 				</div>
-
 				<div class="row-fluid method" id="3">
 					<div class="icon"><img src="<?php echo JURI::root() . 'plugins/redshop_payment/baokim/media/images/transfer.png'; ?>" border="0"/></div>
 					<div class="info">
-						<span class="title">Chuyển khoản InternetBanking</span>
-						<span class="desc">Chọn ngân hàng thanh toán</span>
+						<span class="title"><?php echo JText::_('PLG_REDSHOP_PAYMENT_BAOKIM_INTERNET_BANKING'); ?></span>
+						<span class="desc"><?php echo JText::_('PLG_REDSHOP_PAYMENT_BAOKIM_CHOOSE_BANK'); ?></span>
 
 						<div class="bank_list">
 							<ul id="b_l">
@@ -65,8 +63,8 @@ $name      = $params->get('dataName', SHOP_NAME);
 				<div class="row-fluid method" id="1">
 					<div class="icon"><img src="<?php echo JURI::root() . 'plugins/redshop_payment/baokim/media/images/atm.png'; ?>" border="0"/></div>
 					<div class="info">
-						<span class="title">Thanh toán trực tuyến bằng thẻ ATM nội địa</span>
-						<span class="desc">Chọn ngân hàng thanh toán</span>
+						<span class="title"><?php echo JText::_('PLG_REDSHOP_PAYMENT_BAOKIM_ATM'); ?></span>
+						<span class="desc"><?php echo JText::_('PLG_REDSHOP_PAYMENT_BAOKIM_CHOOSE_BANK'); ?></span>
 
 						<div class="bank_list">
 							<ul id="b_l">
@@ -77,50 +75,46 @@ $name      = $params->get('dataName', SHOP_NAME);
 					</div>
 					<div class="check_box"></div>
 				</div>
-
 				<div class="row-fluid method" id="0">
 					<div class="icon"><img src="<?php echo JURI::root() . 'plugins/redshop_payment/baokim/media/images/sercurity.png'; ?>" border="0"/></div>
 					<div class="info">
 						<div class="bk_logo"><a href="http://baokim.vn" target="_blank"><img
 									src="<?php echo JURI::root() . 'plugins/redshop_payment/baokim/media/images/bk_logo.png'; ?>" border="0"/></a></div>
-						<span class="title">Thanh toán Bảo Kim</span>
-						<span class="desc">Thanh toán bằng tài khoản Bảo Kim (Baokim.vn)</span>
+						<span class="title"><?php echo JText::_('PLG_REDSHOP_PAYMENT_BAOKIM_WALLET'); ?></span>
+						<span class="desc"><?php echo JText::_('PLG_REDSHOP_PAYMENT_BAOKIM_ACCOUNT'); ?></span>
 					</div>
 					<div class="check_box"></div>
 				</div>
 				<li class="mode">
 					<div class="info1">
-						<span class="title">Hình thức thanh toán</span>
+						<span class="title"><?php echo JText::_('PLG_REDSHOP_PAYMENT_BAOKIM_TYPE'); ?></span>
 
 						<div class="payment-mode">
-							<input type="radio" checked="true" class="input-mode" name="payment_mode" value="1"><span class="desc-mode">Thanh toán trực tiếp</span>
+							<input type="radio" checked="true" class="input-mode" name="payment_mode" value="1"><span class="desc-mode"><?php echo JText::_('PLG_REDSHOP_PAYMENT_BAOKIM_TYPE_DIRECT'); ?></span>
 						</div>
 
 						<div class="payment-mode">
-							<input type="radio" class="input-mode" name="payment_mode" value="2" ><span class="desc-mode">Thanh toán an toàn</span>
+							<input type="radio" class="input-mode" name="payment_mode" value="2" ><span class="desc-mode"><?php echo JText::_('PLG_REDSHOP_PAYMENT_BAOKIM_TYPE_SAFE'); ?></span>
 						</div>
 						<div id="daykeep" >
-							<span class="desc-mode" style="margin-right:5px;">Số ngày tạm giữ</span>
+							<span class="desc-mode" style="margin-right:5px;"><?php echo JText::_('PLG_REDSHOP_PAYMENT_BAOKIM_DAY_KEEP'); ?></span>
 							<select name="escrow_timeout" class="daykeep">
-								<option value=3>3 ngày</option>
-								<option value=5>5 ngày</option>
-								<option value=7>7 ngày</option>
+								<option value=3><?php echo JText::sprintf('PLG_REDSHOP_PAYMENT_BAOKIM_DAY', 3); ?></option>
+								<option value=5><?php echo JText::sprintf('PLG_REDSHOP_PAYMENT_BAOKIM_DAY', 5); ?></option>
+								<option value=7><?php echo JText::sprintf('PLG_REDSHOP_PAYMENT_BAOKIM_DAY', 7); ?></option>
 							<select>
 						</div>
 					</div>
 
 				</li>
-
-
 				<input type="hidden" name="active_submit" value="submit"/>
 				<input type="hidden" name="bank_payment_method_id" id="bank_payment_method_id" value=""/>
 				<input type="hidden" name="payer_name" value="<?php echo $formInput['firstname'] . ' ' . $formInput['lastname']; ?>">
 				<input type="hidden" name="payer_phone_no" value="<?php echo $formInput['phone']; ?>">
 				<input type="hidden" name="payer_email" value="<?php echo $formInput['email']; ?>">
 				<input type="hidden" name="address" value="<?php echo $formInput['street']; ?>">
-
 				<div class="submit">
-					<input type="submit" class="btn btn-success pm_submit" name="submit" value="Hoàn thành"/>
+					<input type="submit" class="btn btn-success pm_submit" name="submit" value="<?php echo JText::_('PLG_REDSHOP_PAYMENT_BAOKIM_FINISH'); ?>"/>
 				</div>
 			</form>
 		</div>
@@ -137,24 +131,20 @@ $name      = $params->get('dataName', SHOP_NAME);
 			jQuery('.selected .check_box').addClass('checked_box');
 			var method = jQuery(this).attr('id');
 			if (method != 0) {
-				//jQuery('.mode').css('display','block');
 				jQuery('.info1').slideDown();
 				jQuery('.selected img').click(function () {
 					jQuery('.method img').removeClass('img-active');
 					jQuery(this).addClass('img-active');
 					var id = jQuery(this).attr('id');
 					jQuery('#bank_payment_method_id').val(id);
-
 				});
 			}
 			else {
-				//jQuery('.mode').css('display','none');
 				jQuery('.info1').slideUp('slow');
 				jQuery('.method img').removeClass('img-active');
 			}
 			jQuery('#form-action').attr('action', '<?php echo $action; ?>');
 		});
-
 		jQuery('.input-mode').click(function () {
 			var a = jQuery(this).val();
 			if (a == 2) {
@@ -163,7 +153,6 @@ $name      = $params->get('dataName', SHOP_NAME);
 			if (a == 1) {
 				jQuery('#daykeep').css('display', 'none');
 			}
-
 		});
 	});
 </script>
