6ea187cd88074982e431e4b6a1d56adbae69fc80
javier gomez <javier.gomez.corio@gmail.com>
2016-06-09 12:33:51 +0200

    [tests] run tests in Joomla 3.6.beta

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,8 +1,3 @@
 <?php
-// This is global bootstrap for autoloading 
-include_once __DIR__.DIRECTORY_SEPARATOR . '_extensions/testsperformance.php';
 
-\Codeception\Util\Autoload::registerSuffix('Page', __DIR__.DIRECTORY_SEPARATOR.'_pages/Joomla3/Administrator');
-\Codeception\Util\Autoload::registerSuffix('Page', __DIR__.DIRECTORY_SEPARATOR.'_pages/Joomla3/System');
-\Codeception\Util\Autoload::registerSuffix('Group', __DIR__.DIRECTORY_SEPARATOR.'_groups');
-
+date_default_timezone_set('Europe/Copenhagen');
