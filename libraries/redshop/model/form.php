6ab5f2a37bd55d31c095686e6a43c7285b45be92
ducredweb <duc@redweb.dk>
2016-06-28 13:31:19 +0700

    revert doc comments in form.php

--- libraries/redshop/model/form.php
+++ libraries/redshop/model/form.php
@@ -59,6 +59,7 @@ class RedshopModelForm extends JModelForm
 	/**
 	 * Method for getting the form from the model.
 	 *
+	 * @param   array    $data      Data for the form.
 	 * @param   boolean  $loadData  True if the form is to load its own data (default case), false if not.
 	 *
 	 * @return  mixed  A JForm object on success, false on failure
