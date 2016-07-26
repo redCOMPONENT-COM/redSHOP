e4eefa4600eb309a10150e2bf626b62e7401bf02
Lu Nguyen <lu@redweb.dk>
2016-07-26 17:53:22 +0700

    Using meaningful name instead of query_temp

--- component/admin/models/statistic.php
+++ component/admin/models/statistic.php
@@ -387,28 +387,28 @@ class RedshopModelStatistic extends RedshopModel
 		$query->where('FROM_UNIXTIME(' . $db->qn('cdate') . ') BETWEEN NOW() - INTERVAL 30 DAY AND NOW()');
 
 		// Today
-		$query_temp = clone $query_default;
-		$query_temp->select($db->q(JText::_('COM_REDSHOP_STATISTIC_TODAY')));
-		$query_temp->where('DATE(FROM_UNIXTIME(' . $db->qn('cdate') . ')) = CURDATE()');
-		$query->union($query_temp);
+		$union = clone $query_default;
+		$union->select($db->q(JText::_('COM_REDSHOP_STATISTIC_TODAY')));
+		$union->where('DATE(FROM_UNIXTIME(' . $db->qn('cdate') . ')) = CURDATE()');
+		$query->union($union);
 
 		// Yesterday
-		$query_temp = clone $query_default;
-		$query_temp->select($db->q(JText::_('COM_REDSHOP_STATISTIC_YESTERDAY')));
-		$query_temp->where('DATE(FROM_UNIXTIME(' . $db->qn('cdate') . ')) = SUBDATE(CURDATE(),1)');
-		$query->union($query_temp);
+		$union = clone $query_default;
+		$union->select($db->q(JText::_('COM_REDSHOP_STATISTIC_YESTERDAY')));
+		$union->where('DATE(FROM_UNIXTIME(' . $db->qn('cdate') . ')) = SUBDATE(CURDATE(),1)');
+		$query->union($union);
 
 		// 7 days
-		$query_temp = clone $query_default;
-		$query_temp->select($db->q(JText::sprintf('COM_REDSHOP_STATISTIC_LAST_DAYS', '7')));
-		$query_temp->where('FROM_UNIXTIME(' . $db->qn('cdate') . ') BETWEEN NOW() - INTERVAL 7 DAY AND NOW()');
-		$query->union($query_temp);
+		$union = clone $query_default;
+		$union->select($db->q(JText::sprintf('COM_REDSHOP_STATISTIC_LAST_DAYS', '7')));
+		$union->where('FROM_UNIXTIME(' . $db->qn('cdate') . ') BETWEEN NOW() - INTERVAL 7 DAY AND NOW()');
+		$query->union($union);
 
 		// 90 days
-		$query_temp = clone $query_default;
-		$query_temp->select($db->q(JText::sprintf('COM_REDSHOP_STATISTIC_LAST_DAYS', '90')));
-		$query_temp->where('FROM_UNIXTIME(' . $db->qn('cdate') . ') BETWEEN NOW() - INTERVAL 90 DAY AND NOW()');
-		$query->union($query_temp);
+		$union = clone $query_default;
+		$union->select($db->q(JText::sprintf('COM_REDSHOP_STATISTIC_LAST_DAYS', '90')));
+		$union->where('FROM_UNIXTIME(' . $db->qn('cdate') . ') BETWEEN NOW() - INTERVAL 90 DAY AND NOW()');
+		$query->union($union);
 
 		$db->setQuery($query);
 
