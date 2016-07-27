f3608b9eddb9d31ae5afcf423ebdd943bd7baebd
Lu Nguyen Nhat <lu@redweb.dk>
2016-07-27 13:39:28 +0700

    REDSHOP-3027 Changing Chart in Dashboard layout (#2487)
    * REDSHOP-3027 # Changing Chart in Dashboard layout
    
    * Using meaningful name instead of query1
    
    * Formate code in phpcs
    
    * Using meaningful name instead of query_temp
    
    * Using meaningful name

--- component/admin/views/redshop/tmpl/default_sales_piechart.php
+++ component/admin/views/redshop/tmpl/default_sales_piechart.php
@@ -8,21 +8,22 @@
  */
 defined('_JEXEC') or die;
 
-JFactory::getDocument()->addScript('//www.google.com/jsapi');
+JFactory::getDocument()->addScript('//www.gstatic.com/charts/loader.js');
 
-$turnover = RedshopModel::getInstance('Statistic', 'RedshopModel')->getTotalTurnoverCpanel();
+$turnover = RedshopModel::getInstance('Statistic', 'RedshopModel')->getTotalTurnOverCpanel();
+
+$sales = RedshopModel::getInstance('Statistic', 'RedshopModel')->getTotalSalesCpanel();
 
 $data = json_encode($turnover, JSON_NUMERIC_CHECK);
 
-$title = addslashes(JText::_('COM_REDSHOP_PIE_CHART_FOR_LASTMONTH_SALES'));
 ?>
 
 <script language="javascript" type="text/javascript">
 	//Load the Visualization API and the piechart package.
-	google.load('visualization', '1.1', {'packages': ['corechart']});
+	google.charts.load("current", {packages:['corechart']});
 
 	//Set a callback to run when the Google Visualization API is loaded.
-	google.setOnLoadCallback(drawChart);
+	google.charts.setOnLoadCallback(drawChart);
 
 	//Callback that creates and populates a data table,
 	//instantiates the pie chart, passes in the data and
@@ -32,13 +33,12 @@ $title = addslashes(JText::_('COM_REDSHOP_PIE_CHART_FOR_LASTMONTH_SALES'));
 		var data = new google.visualization.DataTable();
 		data.addColumn('string', '<?php echo JText::_('COM_REDSHOP_STATISTIC_DURATION');?>');
 		data.addColumn('number', '<?php echo JText::_('COM_REDSHOP_SALES_AMOUNT');?>');
-		data.addColumn({type: 'number', role: 'annotation'});
+		data.addColumn({type: 'string', role: 'annotation'});
 		data.addRows(<?php echo $data;?>);
 
 		var options = {
 			height: 500,
-			colors: ['#ebebeb'],
-			vAxis: {title: '<?php echo JText::_('COM_REDSHOP_SALES_AMOUNT');?>'}
+			colors: ['#bed78e']
 		};
 
 		//Instantiate and draw our chart, passing in some options.
@@ -46,28 +46,52 @@ $title = addslashes(JText::_('COM_REDSHOP_PIE_CHART_FOR_LASTMONTH_SALES'));
 		chart.draw(data, options);
 	}
 </script>
-<form action="index.php?option=com_redshop" method="post" name="chartform" id="chartForm">
-	<div id="editcell">
-		<?php
-			echo JText::_('COM_REDSHOP_FILTER') . ": ";
-			$options = array();
-			$options[] = JHTML::_('select.option', '1', JText::_('COM_REDSHOP_DAILY'));
-			$options[] = JHTML::_('select.option', '2', JText::_('COM_REDSHOP_WEEKLY'));
-			$options[] = JHTML::_('select.option', '3', JText::_('COM_REDSHOP_MONTHLY'));
-			$options[] = JHTML::_('select.option', '4', JText::_('COM_REDSHOP_YEARLY'));
 
-			echo JHTML::_(
-				'select.genericlist',
-				$options,
-				'filteroption',
-				'class="inputbox" size="1" onchange="document.chartform.submit();"',
-				'value',
-				'text',
-				JFactory::getApplication()->input->getInt('filteroption', 1)
-			);
+<div class="row">
+	<div class="col-sm-8">
+		<form action="index.php?option=com_redshop" method="post" name="chartform" id="chartForm">
+			<div id="editcell">
+				<?php
+					echo JText::_('COM_REDSHOP_FILTER') . ": ";
+					$options = array();
+					$options[] = JHTML::_('select.option', '1', JText::_('COM_REDSHOP_DAILY'));
+					$options[] = JHTML::_('select.option', '2', JText::_('COM_REDSHOP_WEEKLY'));
+					$options[] = JHTML::_('select.option', '3', JText::_('COM_REDSHOP_MONTHLY'));
+					$options[] = JHTML::_('select.option', '4', JText::_('COM_REDSHOP_YEARLY'));
 
-		?>
-		<div style="float:left;" id="lastmonthsales_statistics_pie"></div>
+					echo JHTML::_(
+						'select.genericlist',
+						$options,
+						'filteroption',
+						'class="inputbox" size="1" onchange="document.chartform.submit();"',
+						'value',
+						'text',
+						JFactory::getApplication()->input->getInt('filteroption', 1)
+					);
+
+				?>
+				<div style="float:left;" id="lastmonthsales_statistics_pie"></div>
+			</div>
+			<input type="hidden" name="view" value="redshop"/>
+		</form>
 	</div>
-	<input type="hidden" name="view" value="redshop"/>
-</form>
+
+	<div class="col-sm-4">
+		<table class="adminlist table table-striped" width="100%">
+			<thead>
+				<tr>
+					<th></th>
+					<th><?php echo JText::_('COM_REDSHOP_STATISTIC_TOTAL_SALES') ?></th>
+					<th><?php echo JText::_('COM_REDSHOP_STATISTIC_ORDER_COUNT') ?></th>
+				</tr>
+			</thead>
+			<?php foreach($sales as $sale) : ?>
+				<tr>
+					<td><?php echo $sale[2];?></td>
+					<td><?php echo $sale[0];?></td>
+					<td><?php echo $sale[1];?></td>
+				</tr>
+			<?php endforeach; ?>
+		</table>
+	</div>
+</div>
