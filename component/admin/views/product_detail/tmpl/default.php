cff28c3b9702e35e45fb968f9655c136f00afb9f
ducredweb <duc@redweb.dk>
2016-07-29 14:30:53 +0700

    parseInt for quantity

--- component/admin/views/product_detail/tmpl/default.php
+++ component/admin/views/product_detail/tmpl/default.php
@@ -76,7 +76,7 @@ JHTMLBehavior::modal();
 		} else if (form.product_template.value == "0") {
 			alert("<?php echo JText::_('COM_REDSHOP_TEMPLATE_MUST_SELECTED', true); ?>");
 			return;
-		} else if (form.min_order_product_quantity.value > form.max_order_product_quantity.value) {
+		} else if (parseInt(form.min_order_product_quantity.value) > parseInt(form.max_order_product_quantity.value)) {
 			alert("<?php echo JText::_('COM_REDSHOP_MINIMUM_QUANTITY_PER_ORDER_MUST_BE_LESS_THAN_MAXIMUM_QUANTITY_PER_ORDER', true); ?>");
 			return;
 		} else if (form.copy_attribute.length) {
