8400108f639a40ff8a3deb89373138d95ad2af53
Tito Alvarez <augustoalvarez@gmail.com>
2016-08-01 15:11:59 -0600

    Compatibility of tests with Joomla 3.6

--- tests/acceptance/administrator/ManageCategoryAdministratorCest.php
+++ tests/acceptance/administrator/ManageCategoryAdministratorCest.php
@@ -66,7 +66,7 @@ class ManageCategoryAdministratorCest
 		$I->doAdministratorLogin();
 		$I = new AcceptanceTester\CategoryManagerJoomla3Steps($scenario);
 		$I->wantTo('Update Existing Category');
-		$I->changeState($this->newCategoryName, 'unpublish');
+		$I->changeCategoryState($this->newCategoryName, 'unpublish');
 		$currentState = $I->getCategoryState($this->newCategoryName);
 		$I->verifyState('unpublished', $currentState);
 	}
