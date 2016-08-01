d6550047dab1e03af3e9618a465d5a28f096447b
Lu Nguyen Nhat <lu@redweb.dk>
2016-08-01 16:56:05 +0700

    REDSHOP-3030 Move product detail tabs into left panel same as configuration tabs (#2500)
    * REDSHOP-3030 # Move product detail tabs into left panel same as configuration tabs
    
    * HTML tab generic
    
    * Use display data directly
    
    * Changing the Tab layout path will be more readable
    
    * Code style - Add blank line

---
+++ component/admin/layouts/component/full/tab/main.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package     RedSHOP.Backend
+ * @subpackage  Template
+ *
+ * @copyright   Copyright (C) 2008 - 2016 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+defined('_JEXEC') or die;
+
+extract($displayData);
+
+?>
+
+<div class="row">
+	<div class="col-sm-2">
+		<div class="box">
+			<div class="box-body no-padding">
+				<ul class="tabconfig nav nav-pills nav-stacked" role="tablist">
+					<?php foreach ($tabMenu as $row) : ?>
+						<?php echo RedshopLayoutHelper::render('component.full.tab.link', $row); ?>
+					<?php endforeach; ?>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-sm-10">
+		<div class="tab-content">
+			<?php foreach ($tabMenu as $row) : ?>
+				<?php echo RedshopLayoutHelper::render('component.full.tab.layout', array('row' => $row, 'view' => $view)); ?>
+			<?php endforeach; ?>
+		</div>
+	</div>
+</div>
