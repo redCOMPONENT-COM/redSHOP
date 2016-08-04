a371564a50b1bb4086729098b27c417e87795c62
Lu Nguyen Nhat <lu@redweb.dk>
2016-08-04 16:43:01 +0700

    REDSHOP-3045 Improvement Configuration (#2524)

--- component/admin/views/configuration/tmpl/default.php
+++ component/admin/views/configuration/tmpl/default.php
@@ -108,158 +108,16 @@ if ($app->input->getInt('dashboard', 0))
 </script>
 <form action="<?php echo 'index.php?option=com_redshop'; ?>" method="post" name="adminForm" id="adminForm"
 	  enctype="multipart/form-data">
-	<div class="row">
-		<div class="col-sm-2">
-			<div class="box">
-				<div class="box-body no-padding">
-				  <ul class="tabconfig nav nav-pills nav-stacked" role="tablist">
-					<li role="presentation" class="<?php echo ($selectedTabPosition == 'general') ? 'active' : '' ?>">
-						<a href="#general" aria-controls="general" role="tab" data-toggle="tab">
-							<?php echo JText::_('COM_REDSHOP_GENERAL_CONFIGURATION', true) ?>
-						</a>
-					</li>
 
-					<li role="presentation" class="<?php echo ($selectedTabPosition == 'user') ? 'active' : '' ?>">
-						<a href="#user" aria-controls="user" role="tab" data-toggle="tab">
-							<?php echo JText::_('COM_REDSHOP_USER', true) ?>
-						</a>
-					</li>
-
-					<li role="presentation" class="<?php echo ($selectedTabPosition == 'cattab') ? 'active' : '' ?>">
-						<a href="#cattab" aria-controls="cattab" role="tab" data-toggle="tab">
-							<?php echo JText::_('COM_REDSHOP_CATEGORY_TAB', true) ?>
-						</a>
-					</li>
-
-					<li role="presentation" class="<?php echo ($selectedTabPosition == 'manufacturertab') ? 'active' : '' ?>">
-						<a href="#manufacturertab" aria-controls="manufacturertab" role="tab" data-toggle="tab">
-							<?php echo JText::_('COM_REDSHOP_REDMANUFACTURER_TAB', true) ?>
-						</a>
-					</li>
-
-					<li role="presentation" class="<?php echo ($selectedTabPosition == 'producttab') ? 'active' : '' ?>">
-						<a href="#producttab" aria-controls="producttab" role="tab" data-toggle="tab">
-							<?php echo JText::_('COM_REDSHOP_PRODUCT_TAB', true) ?>
-						</a>
-					</li>
-
-					<li role="presentation" class="<?php echo ($selectedTabPosition == 'featuretab') ? 'active' : '' ?>">
-						<a href="#featuretab" aria-controls="featuretab" role="tab" data-toggle="tab">
-							<?php echo JText::_('COM_REDSHOP_FEATURE_TAB', true) ?>
-						</a>
-					</li>
-
-					<li role="presentation" class="<?php echo ($selectedTabPosition == 'pricetab') ? 'active' : '' ?>">
-						<a href="#pricetab" aria-controls="pricetab" role="tab" data-toggle="tab">
-							<?php echo JText::_('COM_REDSHOP_PRICE_TAB', true) ?>
-						</a>
-					</li>
-
-					<li role="presentation" class="<?php echo ($selectedTabPosition == 'carttab') ? 'active' : '' ?>">
-						<a href="#carttab" aria-controls="carttab" role="tab" data-toggle="tab">
-							<?php echo JText::_('COM_REDSHOP_CART_TAB', true) ?>
-						</a>
-					</li>
-
-					<li role="presentation" class="<?php echo ($selectedTabPosition == 'ordertab') ? 'active' : '' ?>">
-						<a href="#ordertab" aria-controls="ordertab" role="tab" data-toggle="tab">
-							<?php echo JText::_('COM_REDSHOP_ORDER_TAB', true) ?>
-						</a>
-					</li>
-
-					<li role="presentation" class="<?php echo ($selectedTabPosition == 'newslettertab') ? 'active' : '' ?>">
-						<a href="#newslettertab" aria-controls="newslettertab" role="tab" data-toggle="tab">
-							<?php echo JText::_('COM_REDSHOP_NEWSLETTER_TAB', true) ?>
-						</a>
-					</li>
-
-					<li role="presentation" class="<?php echo ($selectedTabPosition == 'integration') ? 'active' : '' ?>">
-						<a href="#integration" aria-controls="integration" role="tab" data-toggle="tab">
-							<?php echo JText::_('COM_REDSHOP_INTEGRATION', true) ?>
-						</a>
-					</li>
-
-					<li role="presentation" class="<?php echo ($selectedTabPosition == 'seo') ? 'active' : '' ?>">
-						<a href="#seo" aria-controls="seo" role="tab" data-toggle="tab">
-							<?php echo JText::_('COM_REDSHOP_SEO', true) ?>
-						</a>
-					</li>
-
-					<li role="presentation" class="<?php echo ($selectedTabPosition == 'dashboard') ? 'active' : '' ?>">
-						<a href="#dashboard" aria-controls="dashboard" role="tab" data-toggle="tab">
-							<?php echo JText::_('COM_REDSHOP_DASHBOARD', true) ?>
-						</a>
-					</li>
-
-					<li role="presentation" class="<?php echo ($selectedTabPosition == 'redshopabout') ? 'active' : '' ?>">
-						<a href="#redshopabout" aria-controls="redshopabout" role="tab" data-toggle="tab">
-							<?php echo JText::_('COM_REDSHOP_ABOUT', true) ?>
-						</a>
-					</li>
-				  </ul>
-				</div>
-			</div>
-		</div>
-		<div class="col-sm-10">
-			<div class="tab-content">
-				<div role="tabpanel" class="tab-pane <?php echo ($selectedTabPosition == 'general') ? 'active' : '' ?>" id="general">
-					<?php echo $this->loadTemplate('general'); ?>
-				</div>
-
-				<div role="tabpanel" class="tab-pane <?php echo ($selectedTabPosition == 'user') ? 'active' : '' ?>" id="user">
-					<?php echo $this->loadTemplate('user'); ?>
-				</div>
-
-				<div role="tabpanel" class="tab-pane <?php echo ($selectedTabPosition == 'cattab') ? 'active' : '' ?>" id="cattab">
-					<?php echo $this->loadTemplate('cattab'); ?>
-				</div>
-
-				<div role="tabpanel" class="tab-pane <?php echo ($selectedTabPosition == 'manufacturertab') ? 'active' : '' ?>" id="manufacturertab">
-					<?php echo $this->loadTemplate('manufacturertab'); ?>
-				</div>
-
-				<div role="tabpanel" class="tab-pane <?php echo ($selectedTabPosition == 'producttab') ? 'active' : '' ?>" id="producttab">
-					<?php echo $this->loadTemplate('producttab'); ?>
-				</div>
-
-				<div role="tabpanel" class="tab-pane <?php echo ($selectedTabPosition == 'featuretab') ? 'active' : '' ?>" id="featuretab">
-					<?php echo $this->loadTemplate('featuretab'); ?>
-				</div>
-
-				<div role="tabpanel" class="tab-pane <?php echo ($selectedTabPosition == 'pricetab') ? 'active' : '' ?>" id="pricetab">
-					<?php echo $this->loadTemplate('pricetab'); ?>
-				</div>
-
-				<div role="tabpanel" class="tab-pane <?php echo ($selectedTabPosition == 'carttab') ? 'active' : '' ?>" id="carttab">
-					<?php echo $this->loadTemplate('carttab'); ?>
-				</div>
-
-				<div role="tabpanel" class="tab-pane <?php echo ($selectedTabPosition == 'ordertab') ? 'active' : '' ?>" id="ordertab">
-					<?php echo $this->loadTemplate('ordertab'); ?>
-				</div>
-
-				<div role="tabpanel" class="tab-pane <?php echo ($selectedTabPosition == 'newslettertab') ? 'active' : '' ?>" id="newslettertab">
-					<?php echo $this->loadTemplate('newslettertab'); ?>
-				</div>
-
-				<div role="tabpanel" class="tab-pane <?php echo ($selectedTabPosition == 'integration') ? 'active' : '' ?>" id="integration">
-					<?php echo $this->loadTemplate('integration'); ?>
-				</div>
-
-				<div role="tabpanel" class="tab-pane <?php echo ($selectedTabPosition == 'seo') ? 'active' : '' ?>" id="seo">
-					<?php echo $this->loadTemplate('seo'); ?>
-				</div>
-
-				<div role="tabpanel" class="tab-pane <?php echo ($selectedTabPosition == 'dashboard') ? 'active' : '' ?>" id="dashboard">
-					<?php echo $this->loadTemplate('dashboard'); ?>
-				</div>
-
-				<div role="tabpanel" class="tab-pane <?php echo ($selectedTabPosition == 'redshopabout') ? 'active' : '' ?>" id="redshopabout">
-					<?php echo $this->loadTemplate('redshopabout'); ?>
-				</div>
-			</div>
-		</div>
-	</div>
+	<?php
+		echo RedshopLayoutHelper::render(
+			'component.full.tab.main',
+			array(
+				'view'    => $this,
+				'tabMenu' => $this->tabmenu->getData('tab')->items,
+			)
+		);
+	?>
 
 	<input type="hidden" name="view" value="configuration"/>
 	<input type="hidden" name="task" value=""/>
