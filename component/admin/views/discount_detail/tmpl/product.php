41c996b96c84aea96aa7b36b986016cb46d0e530
ducredweb <duc@redweb.dk>
2016-09-05 12:49:06 +0700

    REDSHOP-3096 Cannot create new Discount (#2587)

--- component/admin/views/discount_detail/tmpl/product.php
+++ component/admin/views/discount_detail/tmpl/product.php
@@ -25,11 +25,13 @@ $now = JFactory::getDate();
 		   return new Date(parts[2], parts[1] - 1, parts[0]);
 		}
 
-		if (form.amount.value == "") {
+		if (form.amount.value == "" || form.amount.value == 0) {
 			alert("<?php echo JText::_('COM_REDSHOP_DISCOUNT_AMOUNT_MUST_FILLED', true ); ?>");
 		} else if (parseDate(form.start_date.value) > parseDate(form.end_date.value)) {
  			alert("<?php echo JText::_('COM_REDSHOP_DISCOUNT_START_DATE_END_DATE_CONDITION', true); ?>");
-  		} else {
+  		} else if (form.shopper_group_id.value == "") {
+			alert("<?php echo JText::_('COM_REDSHOP_SHOPPER_GROUP_MUST_BE_SELECTED', true ); ?>");
+		} else {
 			submitform(pressbutton);
 		}
 	}
