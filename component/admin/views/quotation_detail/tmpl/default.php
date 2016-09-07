7ad22ed3e1131d4e966a763372d61f28bc860843
ducredweb <duc@redweb.dk>
2016-09-07 17:45:25 +0700

    REDSHOP-3110 Product Quantity in Quote (#2610)
    * REDSHOP-3110 Product Quantity in Quote
    
    * REDSHOP-3110 Product Quantity in Quote
    
    * REDSHOP-3110 fixed syntax error on in component/admin/views/addquotation_detail/tmpl/default.php

--- component/admin/views/quotation_detail/tmpl/default.php
+++ component/admin/views/quotation_detail/tmpl/default.php
@@ -29,7 +29,6 @@ $url             = $uri->root();
 $quotation_item = $quotationHelper->getQuotationProduct($quotation->quotation_id);    ?>
 <script type="text/javascript">
 	var rowCount = 1;
-	var qrowCount = <?php echo count($quotation_item);?>;
 
 	Joomla.submitbutton = submitbutton = function (pressbutton)
 	{
@@ -43,14 +42,27 @@ $quotation_item = $quotationHelper->getQuotationProduct($quotation->quotation_id
 				alert("<?php echo JText::_('COM_REDSHOP_SELECT_PRODUCT');?>");
 				return;
 			}
+
+			if (form.quantityproduct1.value == "" || parseInt(form.quantityproduct1.value) <= 0) {
+				alert("<?php echo JText::_('COM_REDSHOP_PLEASE_ENTER_VALID_QUANTITY'); ?>");
+				form.quantityproduct1.focus();
+				return false;
+ 			}
+
 			submitform('newQuotationItem');
 			return;
 		}
 		if ((pressbutton == 'save') || (pressbutton == 'send') || (pressbutton == 'aplly')) {
 			if (form.user_id.value == 0 && form.quotation_email.value == "") {
-				alert('<?php echo JText::_('COM_REDSHOP_CREATE_ACCOUNT_FOR_QUOTATION');?>');
+				alert("<?php echo JText::_('COM_REDSHOP_CREATE_ACCOUNT_FOR_QUOTATION');?>");
 				return false;
 			}
+
+			if (validateProductQuantity() == false)
+ 			{
+  				return false;
+  			}
+
 			if (form.quotation_total.value == 0) {
 				if (!confirm("<?php echo JText::_('COM_REDSHOP_CONFIRM_WITH_QUOTATION_TOTAL_ZERO');?>")) {
 					return false;
@@ -58,6 +70,24 @@ $quotation_item = $quotationHelper->getQuotationProduct($quotation->quotation_id
 			}
 		}
 		submitform(pressbutton);
+	}
+
+	function validateProductQuantity()
+ 	{
+ 		var valid = true;
+ 		var quantity = document.querySelectorAll("input[name*='quantityp']");
+
+ 		for (i = 0; i < quantity.length; i++)
+		{
+			if (parseInt(quantity[i].value) <= 0)
+			{
+				alert("<?php echo JText::_('COM_REDSHOP_PLEASE_ENTER_VALID_QUANTITY');?>");
+				quantity[i].focus();
+				valid = false;
+				break;
+			}
+		}
+		return valid;
 	}
 </script>
 <form action="<?php echo JRoute::_($this->request_url) ?>" method="post" name="adminForm" id="adminForm">
