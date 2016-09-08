836337b9f990f9b65541461dba0267a098829f45
Viet Vu <jooservices@gmail.com>
2016-09-08 10:20:41 +0700

    REDSHOP-3150 #Comment Update language string

--- component/site/layouts/cart/cart.php
+++ component/site/layouts/cart/cart.php
@@ -20,7 +20,11 @@ if (isset($cart) && isset($cart['idx']) && $cart['idx'] > 0)
 if ($displayData['cartOutput'] == 'simple'): ?>
 	<div class="mod_cart_extend_total_pro_value" id="mod_cart_total_txt_product" >
 	<?php if ($displayData['totalQuantity']): ?>
-		<?php echo JText::_('MOD_REDSHOP_CART_TOTAL_PRODUCT') . ' ' . $displayData['totalQuantity'] . ' ' . JText::_('MOD_REDSHOP_CART_PRODUCTS_IN_CART'); ?>
+		<?php if ($displayData['totalQuantity'] == 1) : ?>
+			<?php echo JText::_('MOD_REDSHOP_CART_TOTAL_PRODUCT') . ' ' . $displayData['totalQuantity'] . ' ' . JText::_('MOD_REDSHOP_CART_PRODUCT_IN_CART'); ?>
+		<?php else: ?>
+			<?php echo JText::_('MOD_REDSHOP_CART_TOTAL_PRODUCT') . ' ' . $displayData['totalQuantity'] . ' ' . JText::_('MOD_REDSHOP_CART_PRODUCTS_IN_CART'); ?>
+		<?php endif; ?>
 	<?php endif; ?>
 	</div>
 <?php else: ?>
