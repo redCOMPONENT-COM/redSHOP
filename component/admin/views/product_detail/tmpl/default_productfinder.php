209a993253a6aff01b2d26a6397d49ba800a0658
Lu Nguyen Nhat <lu@redweb.dk>
2016-09-15 11:20:08 +0700

    REDSHOP-3209 Admin > Edit product is broken when redPRODUCTFINDER is available (#2669)
    * REDSHOP-3209 Admin > Edit product is broken when redPRODUCTFINDER is availale
    
    * Version bump
    
    * Change version to 2.0.0.1
    
    * Let's make the code more readable

---
+++ component/admin/views/product_detail/tmpl/default_productfinder.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package     RedSHOP.Backend
+ * @subpackage  Template
+ *
+ * @copyright   Copyright (C) 2008 - 2016 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('_JEXEC') or die;
+
+$accosiation_id = 0;
+$ordering       = 1;
+
+if (!empty($this->getassociation))
+{
+    $accosiation_id = $this->getassociation->id;
+    $ordering       = $this->getassociation->ordering;
+}
+
+?>
+  <div class="col50">
+    <table class="adminform">
+      <tr>
+        <td><?php echo JHTML::tooltip(JText::_('COM_REDSHOP_TAG_NAME_TIP'), JText::_('COM_REDSHOP_TAG_NAME'), 'tooltip.png', '', '', false); ?> <?php echo JText::_('COM_REDSHOP_TAG_NAME'); ?> </td>
+        <td><?php echo $this->lists['tags']; ?> </td>
+      </tr>
+    </table>
+    <input type="hidden" name="association_id" value="<?php echo $accosiation_id; ?>" />
+    <input type="hidden" name="ordering" value="<?php echo $ordering; ?>" />
+  </div>
