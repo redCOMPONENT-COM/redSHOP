aa07c15e417de08b024d153d99f316fdd4ea3f34
Viet Vu <jooservices@gmail.com>
2016-09-23 18:57:44 +0700

    REDSHOP-3154 Update Using old classname (#2696)

--- component/site/models/manufacturers.php
+++ component/site/models/manufacturers.php
@@ -249,7 +249,7 @@ class RedshopModelManufacturers extends RedshopModel
 		$and       = '';
 
 		// Shopper group - choose from manufactures Start
-		$rsUserhelper               = RedshopSiteUser::getInstance();
+		$rsUserhelper               = rsUserHelper::getInstance();
 		$shopper_group_manufactures = $rsUserhelper->getShopperGroupManufacturers();
 
 		if ($shopper_group_manufactures != "")
