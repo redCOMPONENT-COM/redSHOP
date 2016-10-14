d71aeb64bdb3447d5d283d1e4e7f2f8eb763ebcd
Tu Vuong <tu@redweb.dk>
2016-10-14 11:32:41 +0700

    REDSHOP-3254 Create attribute management View (#2756)
    * REDSHOP-3254 Create attribute management View
    
    * improve code and Create attribute detail View
    
    * remove duplicate pagination

---
+++ libraries/redshop/form/fields/rpublished.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @package     Redshop
+ * @subpackage  Fields
+ *
+ * @copyright   Copyright (C) 2008 - 2016 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later, see LICENSE.
+ */
+
+defined('JPATH_BASE') or die;
+
+JFormHelper::loadFieldClass('predefinedlist');
+
+/**
+ * jQuery UI datepicker field for redbooking.
+ *
+ * @package     Redshop
+ * @subpackage  Fields
+ * @since       1.0
+ */
+class JFormFieldRpublished extends JFormFieldPredefinedList
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var  string
+	 */
+	protected $type = 'Rpublished';
+
+	/**
+	 * Cached array of the category items.
+	 *
+	 * @var    array
+	 * @since  1.0
+	 */
+	protected static $options = array();
+
+	/**
+	 * The array of values
+	 *
+	 * @var  string
+	 */
+	protected $predefinedOptions = array(
+		1   => 'JPUBLISHED',
+		0   => 'JUNPUBLISHED',
+		2   => 'JARCHIVED',
+		-2  => 'JTRASHED',
+		'*' => 'JALL'
+	);
+
+	/**
+	 * Method to get the options to populate list
+	 *
+	 * @return  array  The field option objects.
+	 *
+	 * @since   1.0
+	 */
+	protected function getOptions()
+	{
+		// Hash for caching
+		$hash = md5($this->element);
+		$type = strtolower($this->type);
+
+		if (!isset(static::$options[$type][$hash]) && !empty($this->predefinedOptions))
+		{
+			// B/C with statuses options
+			if (!isset($this->element['filter']) && isset($this->element['statuses']))
+			{
+				$this->element['filter'] = (string) $this->element['statuses'];
+			}
+
+			static::$options[$type][$hash] = parent::getOptions();
+		}
+
+		return static::$options[$type][$hash];
+	}
+}
