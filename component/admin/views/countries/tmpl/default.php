6c37339fa97bddf098696eb8150e725fc2385b33
Nguyen Hoang Anh <anh.redweb@gmail.com>
2016-10-17 15:23:41 +0700

    Fix bug

--- component/admin/views/countries/tmpl/default.php
+++ component/admin/views/countries/tmpl/default.php
@@ -102,8 +102,6 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 	<input type="hidden" name="view" value="countries"/>
 	<input type="hidden" name="task" value=""/>
 	<input type="hidden" name="boxchecked" value="0"/>
-	<input type="hidden" name="filter_order" value="<?php echo $this->lists['order']; ?>"/>
-	<input type="hidden" name="filter_order_Dir" value="<?php echo $this->lists['order_Dir']; ?>"/>
 </form>
 
 
