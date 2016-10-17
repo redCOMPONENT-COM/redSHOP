d9a065ce1b3245585c45a1578e7777ea79d95b03
Viet Vu <jooservices@gmail.com>
2016-10-17 14:17:04 +0700

    REDSHOP-3353 Update accessmanagers controller

--- component/admin/controllers/accessmanagers.php
+++ component/admin/controllers/accessmanagers.php
@@ -9,13 +9,34 @@
 
 defined('_JEXEC') or die;
 
+/**
+ * Access managers controller
+ *
+ * @package     RedSHOP.Backend
+ * @subpackage  Controller.Accessmanagers
+ * @since       2.0
+ */
 class RedshopControllerAccessmanagers extends RedshopControllerAdmin
 {
+	/**
+	 * Cancel task
+	 *
+	 * @return  void
+	 */
 	public function cancel()
 	{
 		$this->setRedirect('index.php');
 	}
 
+	/**
+	 * Proxy to get RedshopModelAccessmanagers
+	 *
+	 * @param   string  $name    Model name
+	 * @param   string  $prefix  Model prefix
+	 * @param   array   $config  Configuration
+	 *
+	 * @return  object
+	 */
 	public function getModel($name = 'Accessmanagers', $prefix = 'RedshopModel', $config = array())
 	{
 		return parent::getModel($name, $prefix, $config);
