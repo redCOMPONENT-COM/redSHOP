71e7349e723a4b3964f90d56408acd9caa14aa5e
Viet Vu <jooservices@gmail.com>
2016-10-17 15:44:10 +0700

    REDSHOP-3353 Update accessmanager view layout

--- component/admin/views/accessmanager/tmpl/default.php
+++ component/admin/views/accessmanager/tmpl/default.php
@@ -6,7 +6,6 @@
  * @copyright   Copyright (C) 2008 - 2016 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
-$producthelper = productHelper::getInstance();
 
 $input   = JFactory::getApplication()->input;
 $config  = Redconfiguration::getInstance();
@@ -25,10 +24,6 @@ $view    = $input->get('view');
 	}
 </script>
 <form name="adminForm" id="adminForm" method="post" action="index.php">
-	<input type="hidden" name="option" value="com_redshop"/>
-	<input type="hidden" name="view" value="<?php echo $view ?>"/>
-	<input type="hidden" name="section" value="<?php echo $section ?>"/>
-	<input type="hidden" name="task" value=""/>
 	<table cellspacing="0" cellpadding="0" border="0" width="100%" class="adminlist table table-striped table-hover">
 		<thead>
 		<tr>
@@ -136,6 +131,10 @@ $view    = $input->get('view');
 
 	</table>
 	<field>
-		<?php echo JHtml::_( 'form.token' ); ?>
+		<input type="hidden" name="option" value="com_redshop"/>
+		<input type="hidden" name="view" value="<?php echo $view ?>"/>
+		<input type="hidden" name="section" value="<?php echo $section ?>"/>
+		<input type="hidden" name="task" value=""/>
+		<?php echo JHtml::_('form.token'); ?>
 	</field>
 </form>
