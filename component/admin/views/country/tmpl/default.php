ee7e783cf7d480c481771e9ef7b029b26c921b4a
Anh Nguyen Hoang <anh@redweb.dk>
2016-10-19 13:31:24 +0700

    REDSHOP-3352 Country: Re-structure Table. (#2761)
    * REDSHOP-3352 refactor redshop_country table
    
    * REDSHOP-3352 refactory country
    
    * REDSHOP-3352 refactor country
    
    * REDSHOP-3352 refactor country
    
    * REDSHOP-3352 fix bug
    
    * Fix bug
    
    * Fix confict
    
    * Fix filter search
    
    * Change test generate 3code from $this->randomThreeCode = $this->faker->numberBetween(99, 999); to $this->randomThreeCode = $this->faker->numberBetween(100, 999);
    
    * Fix tests
    
    * Fix notice
    
    * Fix Tests
    
    * Change message
    
    * Fix Tests, change filter to xml
    
    * Fix bug
    
    * Fix tests!!
    
    * Fix bug
    
    * Fix id filter_search in currency
    
    * Fix id filter_search in currency
    
    * Fix filter currency
    
    * Fix filter currency
    
    * Fix currency
    
    * Fix comments

--- component/admin/views/country/tmpl/default.php
+++ component/admin/views/country/tmpl/default.php
@@ -7,101 +7,43 @@
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
 defined('_JEXEC') or die;
+JHtml::_('behavior.formvalidator');
+$uri = JURI::getInstance();
+$url = $uri->root();
 
-
-$filter = JRequest::getVar('filter');
-?>
-<script language="javascript" type="text/javascript">
-	Joomla.submitbutton = function (pressbutton) {
-		var form = document.adminForm;
-		if (pressbutton) {
-			form.task.value = pressbutton;
-		}
-
-		if ((pressbutton == 'add') || (pressbutton == 'edit') || (pressbutton == 'publish') || (pressbutton == 'unpublish')
-			|| (pressbutton == 'remove') || (pressbutton == 'saveorder') || (pressbutton == 'orderup') || (pressbutton == 'orderdown')) {
-			form.view.value = "country_detail";
-		}
-		try {
-			form.onsubmit();
-		}
-		catch (e) {
-		}
-
-		form.submit();
-	}
-</script>
-
-<form action="index.php?option=com_redshop" class="admin" id="adminForm" method="post" name="adminForm">
-	<table width="100%">
-		<tr>
-			<td valign="top" class="key">
-				<div class="btn-wrapper input-append">
-					<input type="text" name="filter" id="filter" value="<?php echo $this->filter; ?>"
-						   placeholder="<?php echo JText::_('COM_REDSHOP_COUNTRY_FILTER'); ?>">
-					<input type="submit" class="btn" value="<?php echo JText::_("COM_REDSHOP_SEARCH") ?>">
-				</div>
-			</td>
-		</tr>
-	</table>
-	<table class="adminlist table table-striped">
-		<thead>
-		<tr>
-			<th width="5"><?php echo JText::_('COM_REDSHOP_NUM'); ?></th>
-			<th width="10">
-				<?php echo JHtml::_('redshopgrid.checkall'); ?>
-			</th>
-			<th><?php echo JHTML::_('grid.sort', JText::_('COM_REDSHOP_COUNTRY_NAME'), 'country_name', $this->lists['order_Dir'], $this->lists['order']);?></th>
-			<th><?php echo JHTML::_('grid.sort', JText::_('COM_REDSHOP_COUNTRY_3_CODE'), 'country_3_code', $this->lists['order_Dir'], $this->lists['order']);?></th>
-			<th><?php echo JHTML::_('grid.sort', JText::_('COM_REDSHOP_COUNTRY_2_CODE'), 'country_2_code', $this->lists['order_Dir'], $this->lists['order']);?></th>
-			<!-- <th><?php echo JHTML::_('grid.sort', JText::_('COM_REDSHOP_COUNTRY_JTEXT'), 'country_jtext', $this->lists['order_Dir'], $this->lists['order']);?></th> -->
-			<th><?php echo JHTML::_('grid.sort', JText::_('COM_REDSHOP_ID'), 'country_id', $this->lists['order_Dir'], $this->lists['order']); ?>    </th>
-		</tr>
-		</thead>
-		<?php
-		$k = 0;
-		for ($i = 0, $n = count($this->fields); $i < $n; $i++)
+JFactory::getDocument()->addScriptDeclaration('
+	Joomla.submitbutton = function(task)
+	{
+		if (task == "country.cancel" || document.formvalidator.isValid(document.getElementById("adminForm")))
 		{
-			$row = $this->fields[$i];
-			$row->id = $row->country_id;
-			$link = JRoute::_('index.php?option=com_redshop&view=country_detail&task=edit&cid[]=' . $row->country_id);
-
-			?>
-			<tr class="<?php echo "row$k"; ?>">
-				<td><?php echo $this->pagination->getRowOffset($i); ?></td>
-				<td><?php echo JHTML::_('grid.id', $i, $row->id); ?></td>
-				<td><a href="<?php echo $link; ?>"
-				       title="<?php echo JText::_('COM_REDSHOP_EDIT_COUNTRY'); ?>"><?php echo $row->country_name ?></a>
-				</td>
-				<td align="center" width="10%"><?php echo $row->country_3_code; ?></td>
-				<td align="center" width="10%"><?php echo $row->country_2_code; ?></td>
-				<!-- <td align="center" width="10%"><?php echo $row->country_jtext; ?></td> -->
-				<td align="center" width="10%"><?php echo $row->country_id;?></td>
-
-			</tr>
-			<?php
-			$k = 1 - $k;
-
+			Joomla.submitform(task);
 		}
-		?>
+	};
+');
+?>
 
-
-		<tfoot>
-		<td colspan="9">
-			<?php if (version_compare(JVERSION, '3.0', '>=')): ?>
-				<div class="redShopLimitBox">
-					<?php echo $this->pagination->getLimitBox(); ?>
-				</div>
-			<?php endif; ?>
-			<?php echo $this->pagination->getListFooter(); ?>
-		</td>
-		</tfoot>
-	</table>
-	<input type="hidden" name="view" value="country"/>
-	<input type="hidden" name="task" value=""/>
-	<input type="hidden" name="boxchecked" value="0"/>
-	<input type="hidden" name="filter_order" value="<?php echo $this->lists['order']; ?>"/>
-	<input type="hidden" name="filter_order_Dir" value="<?php echo $this->lists['order_Dir']; ?>"/>
+<form action="index.php?option=com_redshop&task=country.edit&id=<?php echo $this->item->id; ?>"
+	method="post"
+	id="adminForm"
+	name="adminForm"
+	class="form-validate form-horizontal"
+	enctype="multipart/form-data">
+	<fieldset class="adminform">
+		<table class="admintable table">
+			<?php foreach ($this->form->getFieldset('details') as $field) : ?>
+				<?php if ($field->hidden) : ?>
+					<?php echo $field->input;?>
+				<?php endif; ?>
+				<tr>
+					<td class="key"><?php echo $field->label; ?></td>
+					<td><?php echo $field->input; ?></td>
+				</tr>
+			<?php endforeach; ?>
+		</table>
+		<input type="hidden" name="id" value="<?php echo $this->item->id; ?>" />
+		<input type="hidden" name="task" value="" />
+		<?php echo JHtml::_('form.token'); ?>
+	</fieldset>
 </form>
 
 
