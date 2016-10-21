b5ff2cbec8b503efe239b2d6de549b628c601bac
thuyREDWEB <thuy@redweb.dk>
2016-10-21 11:59:56 +0700

    REDSHOP-3381: Refactor method: generateParcel()

--- libraries/redshop/helper/order.php
+++ libraries/redshop/helper/order.php
@@ -739,4 +739,257 @@ class RedshopHelperOrder
 			}
 		}
 	}
+
+	/**
+	 * Generate parcel
+	 *
+	 * @param   integer  $orderId  Order ID to generate
+	 *
+	 * @return  string   'success' or error message
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function generateParcel($orderId)
+	{
+		$db                        = JFactory::getDbo();
+		$orderDetail               = self::getOrderDetails($orderId);
+		$productHelper             = productHelper::getInstance();
+		$orderProducts             = self::getOrderItemDetail($orderId);
+		$billingInfo               = self::getOrderBillingUserInfo($orderId);
+		$shippingInfo              = self::getOrderShippingUserInfo($orderId);
+		$shippingRateDecryptDetail = RedshopShippingRate::decrypt($orderDetail->ship_method_id);
+
+		// Get Shipping Delivery Type
+		$shippingDeliveryType = 1;
+
+		if (isset($shippingRateDecryptDetail[8]) === true)
+		{
+			$shippingDeliveryType = (int) $shippingRateDecryptDetail[8];
+		}
+
+		$query = $db->getQuery(true)
+					->select($db->qn('country_2_code'))
+					->from($db->qn('#__redshop_country'))
+					->where($db->qn('country_3_code') . ' = ' . $db->quote(Redshop::getConfig()->get('SHOP_COUNTRY')));
+		$db->setQuery($query);
+		$billingInfo->country_code = $db->loadResult();
+
+		$query = $db->getQuery(true)
+					->select($db->qn('country_2_code'))
+					->from($db->qn('#__redshop_country'))
+					->where($db->qn('country_3_code') . ' = ' . $db->quote($shippingInfo->country_code));
+		$db->setQuery($query);
+		$shippingInfo->country_code = $db->loadResult();
+
+		// For product content
+		$totalWeight     = 0;
+		$contentProducts = array();
+		$qty             = 0;
+
+		for ($c = 0, $cn = count($orderProducts); $c < $cn; $c++)
+		{
+			$qty += $orderProducts [$c]->product_quantity;
+			$contentProducts[] = $orderProducts[$c]->order_item_name;
+
+			// Product Weight
+			$query = $db->getQuery(true)
+						->select($db->qn('weight'))
+						->from($db->qn('#__redshop_product'))
+						->where($db->qn('product_id') . ' = ' . (int) $orderProducts [$c]->product_id);
+			$db->setQuery($query);
+			$weight = $db->loadResult();
+
+			// Accessory Weight
+			$orderAccItemData = self::getOrderItemAccessoryDetail($orderProducts[$c]->order_item_id);
+			$accWeight = 0;
+
+			if (count($orderAccItemData) > 0)
+			{
+				for ($a = 0, $an = count($orderAccItemData); $a < $an; $a++)
+				{
+					$accessoryQuantity = $orderAccItemData[$a]->product_quantity;
+					$query = $db->getQuery(true)
+								->select($db->qn('weight'))
+								->from($db->qn('#__redshop_product'))
+								->where($db->qn('product_id') . ' = ' . (int) $orderAccItemData[$a]->product_id);
+					$db->setQuery($query);
+					$accessoryWeight = $db->loadResult();
+					$accWeight += ($accessoryWeight * $accessoryQuantity);
+				}
+			}
+
+			// Total weight
+			$totalWeight += (($weight * $orderProducts [$c]->product_quantity) + $accWeight);
+		}
+
+		$unitRatio = $productHelper->getUnitConversation('kg', Redshop::getConfig()->get('DEFAULT_WEIGHT_UNIT'));
+
+		if ($unitRatio != 0)
+		{
+			// Converting weight in pounds
+			$totalWeight = $totalWeight * $unitRatio;
+		}
+
+		if (Redshop::getConfig()->get('SHOW_PRODUCT_DETAIL'))
+		{
+			$contentProducts       = array_unique($contentProducts);
+			$contentProducts       = implode(",", $contentProducts);
+			$contentProducts       = mb_convert_encoding($contentProducts, "ISO-8859-1", "UTF-8");
+			$contentProductsRemark = substr(mb_convert_encoding($contentProducts, "ISO-8859-1", "UTF-8"), 0, 29);
+		}
+		else
+		{
+			$contentProducts       = " ";
+			$contentProductsRemark = " ";
+		}
+
+		$filter    = JFilterInput::getInstance();
+
+		// Filter name to remove special characters
+		$firstName = $filter->clean(
+			mb_convert_encoding($shippingInfo->firstname, "ISO-8859-1", "UTF-8"),
+			'username'
+		);
+		$lastName  = $filter->clean(
+			mb_convert_encoding($shippingInfo->lastname, "ISO-8859-1", "UTF-8"),
+			'username'
+		);
+		$fullName  = $firstName . " " . $lastName;
+		$address   = mb_convert_encoding($shippingInfo->address, "ISO-8859-1", "UTF-8");
+		$city      = mb_convert_encoding($shippingInfo->city, "ISO-8859-1", "UTF-8");
+
+		if ($billingInfo->is_company)
+		{
+			$companyName   = mb_convert_encoding($shippingInfo->company_name, "ISO-8859-1", "UTF-8");
+			$fProductCode  = "PDKEP";
+			$addon         = "<addon adnid='POD'></addon>";
+			$finalAddress1 = $companyName;
+			$finalAddress2 = $address;
+		}
+		else
+		{
+			// Post Danmark MyPack Home
+			$fProductCode  = "PDK17";
+			$addon         = "<addon adnid='DLV'></addon>";
+			$finalAddress1 = $address;
+			$finalAddress2 = "";
+		}
+
+		// When shipping delivery set to post office don't need to send DLV or POD addon.
+		if ($shippingDeliveryType == 0)
+		{
+			// Post Danmark MyPack Collect
+			$fProductCode = "P19DK";
+			$addon        = "";
+		}
+
+		if (Redshop::getConfig()->get('WEBPACK_ENABLE_EMAIL_TRACK'))
+		{
+			$addon .= '<addon adnid="NOTEMAIL"></addon>';
+		}
+
+		if (Redshop::getConfig()->get('WEBPACK_ENABLE_SMS'))
+		{
+			$addon .= '<addon adnid="NOTSMS"></addon>';
+		}
+
+		// No pickup agent by default
+		$agentEle = '';
+
+		// Only when we have store to send parcel - i.e Pickup Location
+		if ('' != trim($order_details->shop_id))
+		{
+			// Get shop location stored using postdanmark plugin or other similar plugin.
+			$shopLocation = explode('|', $order_details->shop_id);
+
+			// Sending shop location id as an agent code.
+			$agentEle     = '<val n="agentto">' . $shopLocation[0] . '</val>';
+
+			// PUPOPT is stands for "Optional Service Point".
+			$addon        .= '<addon adnid="PUPOPT"></addon>';
+		}
+
+		$xmlnew = '<?xml version="1.0" encoding="ISO-8859-1"?>
+				<unifaunonline>
+				<meta>
+				<val n="doorcode">"' . date('Y-m-d H:i') . '"</val>
+				</meta>
+				<receiver rcvid="' . $shippingInfo->users_info_id . '">
+				<val n="name"><![CDATA[' . $fullName . ']]></val>
+				<val n="address1"><![CDATA[' . $finalAddress1 . ']]></val>
+				<val n="address2"><![CDATA[' . $finalAddress2 . ']]></val>
+				<val n="zipcode">' . $shippingInfo->zipcode . '</val>
+				<val n="city">' . $city . '</val>
+				<val n="country">' . $shippingInfo->country_code . '</val>
+				<val n="contact"><![CDATA[' . $firstName . ']]></val>
+				<val n="phone">' . $shippingInfo->phone . '</val>
+				<val n="doorcode"/>
+				<val n="email">' . $shippingInfo->user_email . '</val>
+				<val n="sms">' . $shippingInfo->phone . '</val>
+				</receiver>
+				<shipment orderno="' . $shippingInfo->order_id . '">
+				<val n="from">1</val>
+				<val n="to">' . $shippingInfo->users_info_id . '</val>
+				<val n="reference">' . $orderDetail->order_number . '</val>
+				' . $agentEle . '
+				<service srvid="' . $fProductCode . '">
+				' . $addon . '
+				</service>
+				<container type="parcel">
+				<val n="copies">1</val>
+				<val n="weight">' . $totalWeight . '</val>
+				<val n="contents">' . $contentProducts . '</val>
+				<val n="packagecode">PC</val>
+				</container>
+				</shipment>
+				</unifaunonline>';
+
+		$postURL = "https://www.pacsoftonline.com/ufoweb/order?session=po_DK"
+				. "&user=" . Redshop::getConfig()->get('POSTDK_CUSTOMER_NO')
+				. "&pin=" . Redshop::getConfig()->get('POSTDK_CUSTOMER_PASSWORD')
+				. "&developerid=000000075"
+				. "&type=xml";
+
+		try
+		{
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $postURL);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: text/xml'));
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch, CURLOPT_VERBOSE, true);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $xmlnew);
+			$response = curl_exec($ch);
+			curl_close($ch);
+
+			$xmlResponse = JFactory::getXML($response, false)->val;
+
+			if ('201' == (string) $xmlResponse[1] && 'Created' == (string) $xmlResponse[2])
+			{
+				// Update current order success entry.
+				$query = $db->getQuery(true)
+							->update($db->qn('#__redshop_orders'))
+							->set($db->qn('order_label_create') . ' = 1')
+							->where($db->qn('order_id') . ' = ' . (int) $orderId);
+
+				// Set the query and execute the update.
+				$db->setQuery($query);
+				$db->execute();
+
+				return "success";
+			}
+			else
+			{
+				JError::raiseWarning(
+					21,
+					(string) $xmlResponse[1] . "-" . (string) $xmlResponse[2] . "-" . (string) $xmlResponse[0]
+				);
+			}
+		}
+		catch (Exception $e)
+		{
+			JFactory::getApplication()->enqueueMessage($e->getMessage(), 'error');
+		}
+	}
 }
