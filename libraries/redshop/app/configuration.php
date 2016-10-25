8fb39d55efbbcb2218ad62afd5eeb45603ab4484
thuyREDWEB <thuy@redweb.dk>
2016-10-25 14:22:34 +0700

    REDSHOP-3384 Refactor method substrws() to RedshopAppConfiguration::truncate()

--- libraries/redshop/app/configuration.php
+++ libraries/redshop/app/configuration.php
@@ -1033,4 +1033,145 @@ class RedshopAppConfiguration
 			$config->set('DEFAULT_QUOTATION_MODE', (int) self::cfgQuotationMode());
 		}
 	}
+
+	/**
+	 * Truncate string or HTML and return a preview text with '...'
+	 *
+	 * @param   string   $text          Text or HTML input
+	 * @param   integer  $length        Length of character to truncate
+	 * @param   string   $ending        Ending '...'
+	 * @param   boolean  $exact         If exact, string will be truncated exactly by length
+	 * @param   boolean  $considerHtml  Pre-define $text is HTML or not, if not, tag <> will also be truncated
+	 *
+	 * @return  string
+	 *
+	 * @since  2.0.0.3
+	 */
+	public static function truncate($text, $length = 50, $ending = '...', $exact = false, $considerHtml = true)
+	{
+		if ($considerHtml)
+		{
+			if (strlen(preg_replace('/<.*?>/', '', $text)) <= $length)
+			{
+				return $text;
+			}
+
+			$totalLength = strlen(strip_tags($ending));
+			$openTags    = array();
+			$truncate    = '';
+
+			preg_match_all('/(<\/?([\w+]+)[^>]*>)?([^<>]*)/', $text, $tags, PREG_SET_ORDER);
+
+			foreach ($tags as $tag)
+			{
+				if (!preg_match('/img|br|input|hr|area|base|basefont|col|frame|isindex|link|meta|param/s', $tag[2]))
+				{
+					if (preg_match('/<[\w]+[^>]*>/s', $tag[0]))
+					{
+						array_unshift($openTags, $tag[2]);
+					}
+
+					elseif (preg_match('/<\/([\w]+)[^>]*>/s', $tag[0], $closeTag))
+					{
+						$pos = array_search($closeTag[1], $openTags);
+
+						if ($pos !== false)
+						{
+							array_splice($openTags, $pos, 1);
+						}
+					}
+				}
+
+				$truncate .= $tag[1];
+
+				$contentLength = strlen(preg_replace('/&[0-9a-z]{2,8};|&#[0-9]{1,7};|&#x[0-9a-f]{1,6};/i', ' ', $tag[3]));
+
+				if ($contentLength + $totalLength > $length)
+				{
+					$left           = $length - $totalLength;
+					$entitiesLength = 0;
+
+					if (preg_match_all('/&[0-9a-z]{2,8};|&#[0-9]{1,7};|&#x[0-9a-f]{1,6};/i', $tag[3], $entities, PREG_OFFSET_CAPTURE))
+					{
+						foreach ($entities[0] as $entity)
+						{
+							if ($entity[1] + 1 - $entitiesLength <= $left)
+							{
+								$left--;
+								$entitiesLength += strlen($entity[0]);
+							}
+							else
+							{
+								break;
+							}
+						}
+					}
+
+					$truncate .= substr($tag[3], 0, $left + $entitiesLength);
+					break;
+				}
+				else
+				{
+					$truncate .= $tag[3];
+					$totalLength = $contentLength;
+				}
+
+				if ($totalLength >= $length)
+				{
+					break;
+				}
+			}
+		}
+
+		else
+		{
+			if (strlen($text) <= $length)
+			{
+				return $text;
+			}
+			else
+			{
+				$truncate = substr($text, 0, $length - strlen($ending));
+			}
+		}
+
+		if (!$exact)
+		{
+			$spacepos = strrpos($truncate, ' ');
+
+			if (isset($spacepos))
+			{
+				if ($considerHtml)
+				{
+					$bits = substr($truncate, $spacepos);
+					preg_match_all('/<\/([a-z])>/', $bits, $droppedTags, PREG_SET_ORDER);
+
+					if (!empty($droppedTags))
+					{
+						foreach ($droppedTags as $closingTag)
+						{
+							if (!in_array($closingTag[1], $openTags))
+							{
+								array_unshift($openTags, $closingTag[1]);
+							}
+						}
+					}
+				}
+
+				$truncate = substr($truncate, 0, $spacepos);
+			}
+		}
+
+		$truncate .= $ending;
+
+		if ($considerHtml)
+		{
+			foreach ($openTags as $tag)
+			{
+				$truncate .= '</' . $tag . '>';
+			}
+		}
+
+		return $truncate;
+	}
 }
