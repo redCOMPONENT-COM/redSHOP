8427888a715122074cca8081559b5605e5bfb7b3
anhredweb <anh@redweb.dk>
2016-11-03 16:41:45 +0700

    Add raccessgroups.php

---
+++ libraries/redshop/form/fields/raccessgroups.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_categories
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('JPATH_BASE') or die;
+
+JFormHelper::loadFieldClass('list');
+
+/**
+ * Form Field class for the Joomla Framework.
+ *
+ * @since  1.6
+ */
+class JFormFieldRAccessGroups extends JFormFieldList
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var    string
+	 * @since  1.6
+	 */
+	protected $type = 'RAccessGroups';
+
+	/**
+	 * Method to get the field input markup for a generic list.
+	 *
+	 * @return  string  The field input markup.
+	 */
+	public function getInput()
+	{
+		return JHtml::_('access.usergroups', $this->name, $this->value, true);
+	}
+}
