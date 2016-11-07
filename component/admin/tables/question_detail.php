42a29fe3ecc646d9e41c2be135c20193ab538ffc
anhredweb <anh@redweb.dk>
2016-11-07 18:05:23 +0700

    Not done yet

--- component/admin/tables/question_detail.php
+++ component/admin/tables/question_detail.php
@@ -9,46 +9,26 @@
 
 defined('_JEXEC') or die;
 
-class Tablequestion_detail extends JTable
+/**
+ * Table Question Detail
+ *
+ * @package     RedSHOP.Backend
+ * @subpackage  Table
+ * @since       2.0.0.2.1
+ */
+class RedshopTablequestion_detail extends RedshopTable
 {
-	public $question_id = null;
+	/**
+	 * The table key column. Usually: id
+	 *
+	 * @var  string
+	 */
+	protected $_tableKey = 'question_id';
 
-	public $parent_id = 0;
-
-	public $product_id = null;
-
-	public $user_id = null;
-
-	public $user_name = null;
-
-	public $user_email = null;
-
-	public $question = null;
-
-	public $question_date = null;
-
-	public $telephone = null;
-
-	public $address = null;
-
-	public $published = 1;
-
-	public $ordering = null;
-
-	public function __construct(&$db)
-	{
-		parent::__construct('#__redshop_customer_question', 'question_id', $db);
-	}
-
-	public function bind($array, $ignore = '')
-	{
-		if (array_key_exists('params', $array) && is_array($array['params']))
-		{
-			$registry = new JRegistry;
-			$registry->loadArray($array['params']);
-			$array['params'] = $registry->toString();
-		}
-
-		return parent::bind($array, $ignore);
-	}
+	/**
+	 * The table name without the prefix. Ex: cursos_courses
+	 *
+	 * @var  string
+	 */
+	protected $_tableName = 'redshop_customer_question';
 }
