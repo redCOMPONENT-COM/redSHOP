c664df57da3b5f7fdf63f62a24c45493e697afb7
anhredweb <anh@redweb.dk>
2016-11-09 13:08:21 +0700

    Update sql

--- component/admin/sql/updates/mysql/2.0.0.5.sql
+++ component/admin/sql/updates/mysql/2.0.0.5.sql
@@ -1,7 +1,7 @@
 SET
   FOREIGN_KEY_CHECKS = 0;
 ALTER TABLE `#__redshop_customer_question` CHANGE `question_id` `id` INT(11) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `#__rs_customer_question` ADD CONSTRAINT `#__rs_customer_question_fk1`
+ALTER TABLE `#__redshop_customer_question` ADD CONSTRAINT `#__rs_customer_question_fk1`
     FOREIGN KEY (`product_id`)
     REFERENCES `#__redshop_product` (`product_id`)
     ON DELETE RESTRICT
