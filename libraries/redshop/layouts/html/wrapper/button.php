462786f8c7ef6a814c67069b488d1785343fe36c
anhredweb <anh@redweb.dk>
2016-11-14 13:36:57 +0700

    Change to use Modal Bootsrap

--- libraries/redshop/layouts/html/wrapper/button.php
+++ libraries/redshop/layouts/html/wrapper/button.php
@@ -9,9 +9,27 @@
 defined('JPATH_BASE') or die;
 
 extract($displayData);
+$url = JRoute::_(JURI::base() . 'index.php?option=com_redshop&showall=1&view=wrapper&product_id=' . $productId . '&tmpl=component');
+
+JFactory::getDocument()->addScriptDeclaration('
+(function($){
+	jQuery(document).ready(function(){
+		jQuery("#btnWrapper_' . $productId . '").on("click", function(){
+			html = \'<iframe src="' . $url . '" frameborder="0" width="' . $width . '" height="' . $height . '"></iframe>\';
+
+			$("#wrapperModal_' . $productId . '").html(html);
+			$("#wrapperModal_' . $productId . '").modal("show");
+		});
+	});
+})(jQuery);
+');
 ?>
-<a class="<?php echo $class ?>"
-	href="index.php?option=com_redshop&showall=1&view=wrapper&product_id=<?php echo $productId; ?>&tmpl=component"
-	rel="{handler: '<?php echo $handler ?>', size: {x: <?php echo $width ?>, y: <?php echo $height ?>}}">
-	<img src="<?php echo REDSHOP_ADMIN_IMAGES_ABSPATH; ?>wrapper16.png" align="absmiddle"
-	alt="<?php echo JText::_('COM_REDSHOP_WRAPPER'); ?>"> <?php echo "(" . $count . ")";?></a>
+<a style="cursor: pointer" id="btnWrapper_<?php echo $productId ?>"><img
+	src="<?php echo REDSHOP_ADMIN_IMAGES_ABSPATH; ?>media16.png" align="absmiddle"
+	alt="media"> (<?php  echo $count;?>)</a>
+
+<div class="<?php echo $class ?> fade shadow" 
+	id="wrapperModal_<?php echo $productId?>"
+	style="overflow-y: hidden; width: <?php echo $width ?>px; padding: 5px;">
+</div>
+
