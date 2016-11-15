592fc34cb44a1e2bc16e7f17fabb0011f2b66910
anhredweb <anh@redweb.dk>
2016-11-15 13:43:08 +0700

    Change to PHP inline

--- modules/site/mod_redmasscart/tmpl/default.php
+++ modules/site/mod_redmasscart/tmpl/default.php
@@ -17,30 +17,23 @@ $u = JFactory::getURI();
 	<form action="" method="post" name="masscart" id="masscart">
 		<div id="redcart">
 			<label for="numbercart">
-				<?php if ($params->get('title') != "")
-				{
-					echo $params->get('title');
-				}
-				else
-				{
-					echo JText::_('MOD_REDMASSCART_PRODUCT_NUMBER');
-				}
-				?>
+				<?php if ($params->get('title') != ""): ?>
+					<?php echo $params->get('title'); ?>
+				<?php else: ?>
+					<?php echo JText::_('MOD_REDMASSCART_PRODUCT_NUMBER'); ?>
+				<?php endif; ?>
 			<?php echo JHTML::_('tooltip', $params->get('info')); ?></label>
 			<textarea id="numbercart" name="numbercart"
 			          style="width:<?php echo $params->get('textwidth'); ?>px; height:<?php echo $params->get('textheight'); ?>px;"
 			          class="inputbox_numbercart"></textarea>
 
 		</div>
-		<?php
-		if ($params->get('chk_quantity') == "1")
-		{
-			?>
+		<?php if ($params->get('chk_quantity') == "1"): ?>
 			<div id="productQuantity">
 				<label for="mod_quantity"><?php  echo JText::_('MOD_REDMASSCART_PRODUCT_QUANTITY');  ?></label>
 				<input type="text" id="mod_quantity" class="span12" name="mod_quantity" size="2"/>
 			</div>
-		<?php } ?>
+		<?php endif; ?>
 		<br/>
 		<input type="submit" name="Submit" class="button_cart btn" value="<?php echo $params->get('cartbtntitle'); ?>"/>
 		<input type="hidden" name="option" value="com_redshop"/>
