ca0aa5006e95ca97f2756e50459671bc8f34d357
Viet Vu <jooservices@gmail.com>
2016-11-18 15:02:45 +0700

    ONKELP-161 Remove check product_on_sale for mass discount

--- component/admin/models/mass_discount_detail.php
+++ component/admin/models/mass_discount_detail.php
@@ -200,23 +200,21 @@ class RedshopModelMass_discount_detail extends RedshopModel
 				{
 					$productData = Redshop::product((int) $arr_diff[$i]);
 
-					if ($productData->product_on_sale != 1)
+					// Ticket ONKELP-161: Temporary comment these code to by pass product_on_sale check before apply another mass discount
+					$p_price = ($data['discount_type'] == 1) ?
+						($productData->product_price - ($productData->product_price * $data['discount_amount'] / 100)) :
+						$productData->product_price - ($data['discount_amount']);
+					$p_price = $producthelper->productPriceRound($p_price);
+					$query = 'UPDATE ' . $this->_table_prefix . 'product SET product_on_sale="1" , discount_price="'
+						. $p_price . '" , discount_stratdate="' . $data['discount_startdate'] . '" , discount_enddate="'
+						. $data['discount_enddate'] . '" WHERE product_id="' . $arr_diff[$i] . '" ';
+					$this->_db->setQuery($query);
+
+					if (!$this->_db->execute())
 					{
-						$p_price = ($data['discount_type'] == 1) ?
-							($productData->product_price - ($productData->product_price * $data['discount_amount'] / 100)) :
-							$productData->product_price - ($data['discount_amount']);
-						$p_price = $producthelper->productPriceRound($p_price);
-						$query = 'UPDATE ' . $this->_table_prefix . 'product SET product_on_sale="1" , discount_price="'
-							. $p_price . '" , discount_stratdate="' . $data['discount_startdate'] . '" , discount_enddate="'
-							. $data['discount_enddate'] . '" WHERE product_id="' . $arr_diff[$i] . '" ';
-						$this->_db->setQuery($query);
+						$this->setError($this->_db->getErrorMsg());
 
-						if (!$this->_db->execute())
-						{
-							$this->setError($this->_db->getErrorMsg());
-
-							return false;
-						}
+						return false;
 					}
 				}
 				catch (Exception $e)
