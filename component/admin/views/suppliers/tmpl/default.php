972b2880cc8009dfbc6d5a1ee5edfaa921c05643
Thong Tran <thong@redweb.dk>
2016-11-22 18:19:45 +0700

    Update code

--- component/admin/views/suppliers/tmpl/default.php
+++ component/admin/views/suppliers/tmpl/default.php
@@ -10,15 +10,12 @@ defined('_JEXEC') or die;
 
 JHtml::_('behavior.modal');
 
+$user = JFactory::getUser();
+
 $listOrder = $this->escape($this->state->get('list.ordering'));
 $listDirn  = $this->escape($this->state->get('list.direction'));
 ?>
-<form 
-	action="index.php?option=com_redshop&view=suppliers" 
-	class="admin" 
-	id="adminForm" 
-	method="post" 
-	name="adminForm">
+<form action="index.php?option=com_redshop&view=suppliers" class="admin" id="adminForm" method="post" name="adminForm">
 	<div class="filterTool">
 		<?php
 		echo RedshopLayoutHelper::render(
@@ -41,116 +38,81 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 		?>
 	</div>
 	<?php if (empty($this->items)) : ?>
-		<div class="alert alert-no-items">
+		<div class="alert alert-no-items alert-info">
 			<?php echo JText::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
 		</div>
 	<?php else : ?>
-	<div id="editcell">
 		<table class="adminlist table table-striped">
 			<thead>
-			<tr>
-				<th width="5%">
-					<?php
-					echo JText::_('COM_REDSHOP_NUM');
-					?>
-				</th>
-				<th width="5%"><?php echo JHtml::_('redshopgrid.checkall'); ?></th>
-				<th class="title">
-					<?php
-					echo JHTML::_('grid.sort', 'COM_REDSHOP_SUPPLIER_NAME', 'supplier_name', $listOrder, $listDirn);
-					?>
-
-				</th>
-				<th width="20%">
-					<?php
-					echo JHTML::_('grid.sort', 'COM_REDSHOP_SUPPLIER_EMAIL', 'supplier_email', $listOrder, $listDirn);
-					?>
-				</th>
-				<th width="5%" nowrap="nowrap">
-					<?php echo JHtml::_('grid.sort', 'COM_REDSHOP_PUBLISHED', 'published', $listDirn, $listOrder); ?>
-				</th>
-				<th width="5%" nowrap="nowrap">
-					<?php
-					echo JHTML::_('grid.sort', 'COM_REDSHOP_ID', 'id', $listOrder, $listDirn);
-					?>
-				</th>
-
-			</tr>
+				<tr>
+					<th width="1">#</th>
+					<th width="5%">
+						<?php echo JHtml::_('redshopgrid.checkall'); ?>
+					</th>
+					<th width="5%" nowrap="nowrap">
+						<?php echo JHtml::_('grid.sort', 'COM_REDSHOP_PUBLISHED', 'published', $listDirn, $listOrder); ?>
+					</th>
+					<th class="title">
+						<?php echo JHTML::_('grid.sort', 'COM_REDSHOP_SUPPLIER_NAME', 's.name', $listDirn, $listOrder); ?>
+					</th>
+					<th width="20%">
+						<?php echo JHTML::_('grid.sort', 'COM_REDSHOP_SUPPLIER_EMAIL', 'email', $listDirn, $listOrder); ?>
+					</th>
+					<th width="5%" nowrap="nowrap">
+						<?php echo JHTML::_('grid.sort', 'COM_REDSHOP_ID', 'id', $listDirn, $listOrder); ?>
+					</th>
+				</tr>
 			</thead>
 			<tbody>
-			<?php
-			$k = 0;
-
-			for ($i = 0, $n = count($this->items); $i < $n; $i++)
-			{
-				$row = $this->items[$i];
-
-				$row->id = $row->id;
-
-				$link = JRoute::_('index.php?option=com_redshop&task=supplier.edit&id=' . $row->id);
-
-				$published = JHTML::_('grid.published', $row, $i);
-
-				?>
-				<tr class="<?php echo "row$k"; ?>">
+			<?php foreach ($this->items as $i => $row): ?>
+				<tr>
 					<td align="center">
-						<?php
-						echo $this->pagination->getRowOffset($i);
-						?>
+						<?php echo $this->pagination->getRowOffset($i) ?>
 					</td>
 					<td align="center">
-						<?php
-						echo JHTML::_('grid.id', $i, $row->id);
-						?>
+						<?php echo JHTML::_('grid.id', $i, $row->id) ?>
 					</td>
-					<td><a href="<?php
-						echo $link;
-						?>"
-					       title="<?php
-					       echo JText::_('COM_REDSHOP_EDIT_SUPPLIER');
-					       ?>"><?php
-							echo $row->supplier_name;
-							?></a></td>
+					<td align="center">
+						<?php echo JHTML::_('grid.published', $row, $i) ?>
+					</td>
 					<td>
-						<?php
-						echo $row->supplier_email;
-						?>
+						<?php if ($row->checked_out && $user->id != $row->checked_out): ?>
+							<?php
+							$author = JFactory::getUser($row->checked_out);
+							$canCheckin = $user->authorise('core.manage', 'com_checkin') || $row->checked_out == $user->id || $row->checked_out == 0;
+							echo JHtml::_('jgrid.checkedout', $i, $row->checked_out, $row->checked_out_time, 'suppliers.', $canCheckin);
+							?>
+						<?php endif; ?>
+
+						<?php if ($row->checked_out && $user->id != $row->checked_out): ?>
+							<?php echo JHtml::_('string.truncate', $row->name, 50, true, false) ?>
+						<?php else: ?>
+							<a href="<?php echo JRoute::_('index.php?option=com_redshop&task=supplier.edit&id=' . $row->id) ?>"
+							   title="<?php echo JText::_('COM_REDSHOP_EDIT_SUPPLIER') ?>">
+								<?php echo JHtml::_('string.truncate', $row->name, 50, true, false) ?>
+							</a>
+						<?php endif; ?>
+					</td>
+					<td>
+						<?php echo $row->email; ?>
 					</td>
 					<td align="center">
-						<?php
-						echo $published;
-						?>
-					</td>
-					<td align="center">
-						<?php
-						echo $row->id;
-						?>
+						<?php echo $row->id; ?>
 					</td>
 				</tr>
-				<?php
-				$k = 1 - $k;
-			}
-			?>
+			<?php endforeach; ?>
 			</tbody>
 			<tfoot>
 			<td colspan="9">
-				<?php if (version_compare(JVERSION, '3.0', '>=')): ?>
-					<div class="redShopLimitBox">
-						<?php echo $this->pagination->getLimitBox(); ?>
-					</div>
-				<?php endif; ?>
-				<?php
-				echo $this->pagination->getListFooter();
-				?>
+				<?php echo $this->pagination->getListFooter(); ?>
 			</td>
 			</tfoot>
 		</table>
-	</div>
 	<?php endif; ?>
-	<?php echo JHtml::_('form.token'); ?>
-	<input type="hidden" name="view" value="suppliers"/>
+	<input type="hidden" name="view" value="suppliers">
 	<input type="hidden" name="task" value=""/>
 	<input type="hidden" name="boxchecked" value="0"/>
 	<input type="hidden" name="filter_order" value="<?php echo $listOrder ?>" />
 	<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn ?>" />
+	<?php echo JHtml::_('form.token'); ?>
 </form>
