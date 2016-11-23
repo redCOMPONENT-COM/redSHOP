6f55a2c24954ab4cbadf49e2a38d37853ec0f9ad
anhredweb <anh@redweb.dk>
2016-11-23 15:15:29 +0700

    refactor

--- modules/site/mod_redshop_promote_free_shipping/mod_redshop_promote_free_shipping.php
+++ modules/site/mod_redshop_promote_free_shipping/mod_redshop_promote_free_shipping.php
@@ -3,7 +3,7 @@
  * @package     RedSHOP.Frontend
  * @subpackage  mod_redshop_promote_free_shipping
  *
- * @copyright   Copyright (C) 2008 - 2015 redCOMPONENT.com. All rights reserved.
+ * @copyright   Copyright (C) 2008 - 2016 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
@@ -11,9 +11,9 @@ defined('_JEXEC') or die;
 
 JLoader::import('redshop.library');
 
-$shippinghelper = shipping::getInstance();
-$shipping_rate_id = $params->get("shipping_rate_id");
-$text = $shippinghelper->getfreeshippingRate($shipping_rate_id);
+$shippingHelper = shipping::getInstance();
+$shippingRateId = $params->get("shipping_rate_id");
+$text 			= $shippingHelper->getfreeshippingRate($shippingRateId);
+$conditionText  = $params->get("condition_text");
 
 require JModuleHelper::getLayoutPath('mod_redshop_promote_free_shipping');
-
--- modules/site/mod_redshop_promote_free_shipping/tmpl/default.php
+++ modules/site/mod_redshop_promote_free_shipping/tmpl/default.php
@@ -3,12 +3,10 @@
  * @package     RedSHOP.Frontend
  * @subpackage  mod_redshop_promote_free_shipping
  *
- * @copyright   Copyright (C) 2008 - 2015 redCOMPONENT.com. All rights reserved.
+ * @copyright   Copyright (C) 2008 - 2016 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
 defined('_JEXEC') or die;
-
-$condition_text = $params->get("condition_text");
-
-echo "<div id='rs_promote_free_shipping_div'>" . $text . "</div><br><div id='redconditionDiv'>" . $condition_text . "</div>";
+?>
+<div id='rs_promote_free_shipping_div'><?php echo $text ?></div><br><div id='redconditionDiv'><?php echo $conditionText ?></div>
