db34337e2e813d97e4bbc24334436778f804505b
Viet Vu <jooservices@gmail.com>
2016-11-24 10:08:37 +0700

    REDSHOP-3567 Update docblocks

--- plugins/logman/redshop/install.php
+++ plugins/logman/redshop/install.php
@@ -1,74 +1,77 @@
 <?php
 /**
- * @package     LOGman
- * @copyright   Copyright (C) 2011 - 2016 Timble CVBA. (http://www.timble.net)
- * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link        http://www.joomlatools.com
+ * @package    LOGman
+ * @copyright  Copyright (C) 2008 - 2015 redCOMPONENT.com. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
  */
 
 /**
- * LOGman - AdvancedModules installer
+ * redSHOP LOGman plugin installer.
  *
- * @author  Arunas Mazeika <https://github.com/amazeika>
- * @package Joomlatools\Component\LOGman
+ * @package  Joomlatools\Plugin\LOGman
+ *
+ * @since    1.0.0
  */
 class plgLogmanAdvancedmodulesInstallerScript
 {
-    /**
-     * @var string The current installed LOGman version.
-     */
-    protected $_logman_ver = null;
+	/**
+	 * @var string The current installed LOGman version.
+	 */
+	protected $logman_ver = null;
 
-    public function preflight($type, $installer)
-    {
-        $return = true;
-        $errors = array();
+	public function preflight($type, $installer)
+	{
+		$return = true;
+		$errors = array();
 
-        if (version_compare($this->getLogmanVersion(), '3.0.0', '<'))
-        {
-            $errors[] = JText::_('This plugin requires a newer LOGman version. Please download the latest version from <a href=http://joomlatools.com target=_blank>joomlatools.com</a> and upgrade.');
-            $return   = false;
-        }
+		if (version_compare($this->getLogmanVersion(), '3.0.0', '<'))
+		{
+			$errors[] = JText::_('This plugin requires a newer LOGman version. Please download the latest version from <a href=http://joomlatools.com target=_blank>joomlatools.com</a> and upgrade.');
+			$return   = false;
+		}
 
-        if ($return == false && $errors)
-        {
-            $error = implode('<br />', $errors);
-            $installer->getParent()->abort($error);
-        }
+		if ($return == false && $errors)
+		{
+			$error = implode('<br />', $errors);
+			$installer->getParent()->abort($error);
+		}
 
-        return $return;
-    }
+		return $return;
+	}
 
-    /**
-     * Returns the current version (if any) of LOGman.
-     *
-     * @return string|null The LOGman version if present, null otherwise.
-     */
-    public function getLogmanVersion()
-    {
-        if (!$this->_logman_ver) {
-            $this->_logman_ver = $this->_getExtensionVersion('com_logman');
-        }
+	/**
+	 * Returns the current version (if any) of LOGman.
+	 *
+	 * @return string|null The LOGman version if present, null otherwise.
+	 */
+	public function getLogmanVersion()
+	{
+		if (!$this->logman_ver)
+		{
+			$this->logman_ver = $this->_getExtensionVersion('com_logman');
+		}
 
-        return $this->_logman_ver;
-    }
+		return $this->_logman_ver;
+	}
 
-    /**
-     * Extension version getter.
-     *
-     * @param string $element The element name, e.g. com_extman, com_logman, etc.
-     * @return mixed|null|string The extension version, null if couldn't be determined.
-     */
-    protected function _getExtensionVersion($element)
-    {
-        $version = null;
+	/**
+	 * Extension version getter.
+	 *
+	 * @param   string  $element  The element name, e.g. com_extman, com_logman, etc.
+	 *
+	 * @return mixed|null|string The extension version, null if couldn't be determined.
+	 */
+	protected function _getExtensionVersion($element)
+	{
+		$version = null;
 
-        $query    = "SELECT manifest_cache FROM #__extensions WHERE element = '{$element}'";
-        if ($result = JFactory::getDBO()->setQuery($query)->loadResult()) {
-            $manifest = new JRegistry($result);
-            $version  = $manifest->get('version', null);
-        }
+		$query = "SELECT manifest_cache FROM #__extensions WHERE element = '{$element}'";
+		if ($result = JFactory::getDBO()->setQuery($query)->loadResult())
+		{
+			$manifest = new JRegistry($result);
+			$version  = $manifest->get('version', null);
+		}
 
-        return $version;
-    }
+		return $version;
+	}
 }
