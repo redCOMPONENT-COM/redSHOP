9c6113468ac677960c30c0d82a2f4f1ecd5bb124
Nguyen Hoang Anh <anh@redweb.dk>
2016-11-28 09:57:36 +0700

    refactor

--- plugins/ajax/xmlcron/xmlcron.php
+++ plugins/ajax/xmlcron/xmlcron.php
@@ -37,12 +37,26 @@ class PlgAjaxXmlCron extends JPlugin
 	 */
 	public function xmlExportFileUpdate()
 	{
-		$currenttime = time();
+		$currentTime = time();
 		$xmlHelper   = new xmlHelper;
 
 		$db = JFactory::getDbo();
 		$query = $db->getQuery(true)
-					->select('*')
+					->select(
+						$db->qn(
+							[
+								'xmlexport_id', 'filename', 'display_filename',
+								'parent_name', 'section_type', 'auto_sync',
+								'sync_on_request', 'auto_sync_interval', 'xmlexport_date',
+								'xmlexport_filetag', 'element_name', 'published',
+								'use_to_all_users', 'xmlexport_billingtag', 'billing_element_name',
+								'xmlexport_shippingtag', 'shipping_element_name', 'xmlexport_orderitemtag',
+								'orderitem_element_name', 'xmlexport_stocktag', 'stock_element_name',
+								'xmlexport_prdextrafieldtag', 'prdextrafield_element_name',
+								'xmlexport_on_category'
+							]
+						)
+					)
 					->from($db->qn('#__redshop_xml_export'))
 					->where($db->qn('published') . ' = 1')
 					->where($db->qn('auto_sync') . ' = 1')
@@ -51,27 +65,33 @@ class PlgAjaxXmlCron extends JPlugin
 
 		// Set the query and load the result.
 		$db->setQuery($query);
-		$exportlist = $db->loadObjectlist();
+		$exportList = $db->loadObjectlist();
 
-		for ($i = 0, $n = count($exportlist); $i < $n; $i++)
+		for ($i = 0, $n = count($exportList); $i < $n; $i++)
 		{
 			$query = $db->getQuery(true)
-						->select('*')
+						->select(
+							$db->qn(
+								[
+									'xmlexport_log_id', 'xmlexport_id', 'xmlexport_filename', 'xmlexport_date'
+								]
+							)
+						)
 						->from($db->qn('#__redshop_xml_export_log'))
-						->where($db->qn('xmlexport_id') . ' = ' . (int) $exportlist[$i]->xmlexport_id)
+						->where($db->qn('xmlexport_id') . ' = ' . (int) $exportList[$i]->xmlexport_id)
 						->order($db->qn('xmlexport_date') . ' DESC');
 
 			$db->setQuery($query);
-			$lastrs = $db->loadObject();
+			$lastResult = $db->loadObject();
 
-			if (count($lastrs) > 0)
+			if (count($lastResult) > 0)
 			{
-				$difftime = $currenttime - $lastrs->xmlexport_date;
+				$difftime = $currentTime - $lastResult->xmlexport_date;
 				$hours    = $difftime / (60 * 60);
 
-				if ($exportlist[$i]->auto_sync_interval < $hours)
+				if ($exportList[$i]->auto_sync_interval < $hours)
 				{
-					$xmlHelper->writeXMLExportFile($lastrs->xmlexport_id);
+					$xmlHelper->writeXMLExportFile($lastResult->xmlexport_id);
 				}
 			}
 		}
@@ -84,38 +104,58 @@ class PlgAjaxXmlCron extends JPlugin
 	 */
 	public function xmlImportFileUpdate()
 	{
-		$currenttime = time();
+		$currentTime = time();
 		$xmlHelper   = new xmlHelper;
 
 		$db = JFactory::getDbo();
 		$query = $db->getQuery(true)
-					->select('*')
+					->select(
+						$db->qn(
+							[
+								'xmlimport_id', 'filename', 'display_filename',
+								'xmlimport_url', 'section_type', 'auto_sync',
+								'sync_on_request', 'auto_sync_interval', ' 	override_existing',
+								'add_prefix_for_existing', 'xmlimport_date',
+								'xmlimport_filetag', 'xmlimport_billingtag', 'xmlimport_shippingtag',
+								'xmlimport_orderitemtag', 'xmlimport_stocktag', 'xmlimport_prdextrafieldtag',
+								'published', 'element_name', 'billing_element_name', 'xmlexport_orderitemtag',
+								'shipping_element_name', 'orderitem_element_name', 'stock_element_name',
+								'prdextrafield_element_name', 'xmlexport_billingtag', 'xmlexport_shippingtag'
+							]
+						)
+					)
 					->from($db->qn('#__redshop_xml_import'))
 					->where($db->qn('published') . ' = 1')
 					->where($db->qn('auto_sync') . ' = 1')
 					->where($db->qn('sync_on_request') . ' = 0')
 					->where($db->qn('auto_sync_interval') . ' != 0');
 		$db->setQuery($query);
-		$importlist = $db->loadObjectlist();
+		$importList = $db->loadObjectlist();
 
-		for ($i = 0, $n = count($importlist); $i < $n; $i++)
+		for ($i = 0, $n = count($importList); $i < $n; $i++)
 		{
 			$query = $db->getQuery(true)
-						->select('*')
+						->select(
+							$db->qn(
+								[
+									'xmlimport_log_id', 'xmlimport_id', 'xmlimport_filename', 'xmlimport_date'
+								]
+							)
+						)
 						->from($db->qn('#__redshop_xml_import_log'))
-						->where($db->qn('xmlimport_id') . ' = ' . (int) $importlist[$i]->xmlimport_id)
+						->where($db->qn('xmlimport_id') . ' = ' . (int) $importList[$i]->xmlimport_id)
 						->order($db->qn('xmlimport_date') . ' DESC');
 			$db->setQuery($query);
-			$lastrs = $db->loadObject();
+			$lastResult = $db->loadObject();
 
-			if (count($lastrs) > 0)
+			if (count($lastResult) > 0)
 			{
-				$difftime = $currenttime - $lastrs->xmlimport_date;
+				$difftime = $currentTime - $lastResult->xmlimport_date;
 				$hours = $difftime / (60 * 60);
 
-				if ($importlist[$i]->auto_sync_interval < $hours)
+				if ($importList[$i]->auto_sync_interval < $hours)
 				{
-					$xmlHelper->importXMLFile($lastrs->xmlimport_id);
+					$xmlHelper->importXMLFile($lastResult->xmlimport_id);
 				}
 			}
 		}
