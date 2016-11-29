00e18de6b67a48fa2f867f8727e3a9e745bb63b1
Thong Tran <thong@redweb.dk>
2016-11-29 13:59:51 +0700

    REDSHOP-3602 Refactor Mass Discount view

--- component/admin/models/product_detail.php
+++ component/admin/models/product_detail.php
@@ -598,7 +598,7 @@ class RedshopModelProduct_Detail extends RedshopModel
 			$category_array = array_diff($oldcategory, $data['product_category']);
 		}
 
-		$sel = "SELECT * FROM " . $this->table_prefix . "mass_discount WHERE " . $where_cat_discount . " ORDER BY mass_discount_id desc limit 0,1";
+		$sel = "SELECT * FROM " . $this->table_prefix . "mass_discount WHERE " . $where_cat_discount . " ORDER BY id desc limit 0,1";
 		$this->_db->setQuery($sel);
 		$mass_discount = $this->_db->loadObject();
 
@@ -608,7 +608,7 @@ class RedshopModelProduct_Detail extends RedshopModel
 		}
 
 		$sel = "SELECT * FROM " . $this->table_prefix . "mass_discount WHERE FIND_IN_SET('" . $row->manufacturer_id .
-			"',manufacturer_id) ORDER BY mass_discount_id desc limit 0,1";
+			"',manufacturer_id) ORDER BY id desc limit 0,1";
 		$this->_db->setQuery($sel);
 		$mass_discount = $this->_db->loadObject();
 
