d9cc72b349992897c4590f373f54398c369e6164
Thong Tran <thong@redweb.dk>
2016-12-01 15:32:09 +0700

    Optimize Order Statistic and Products Statistic

--- component/admin/views/statistic_order/tmpl/default.php
+++ component/admin/views/statistic_order/tmpl/default.php
@@ -7,8 +7,11 @@
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
 defined('_JEXEC') or die;
+
 JFactory::getDocument()->addScript('//www.gstatic.com/charts/loader.js');
-$producthelper = productHelper::getInstance();
+
+$productHelper = productHelper::getInstance();
+
 $listOrder = $this->escape($this->state->get('list.ordering'));
 $listDirn  = $this->escape($this->state->get('list.direction'));
 ?>
@@ -24,49 +27,29 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 	//draws it.
 	function drawChart() {
 		var data = google.visualization.arrayToDataTable([
-			['<?php echo JText::_('COM_REDSHOP_STATISTIC_DURATION');?>', '<?php echo JText::_('COM_REDSHOP_SALES_AMOUNT');?>', {role: 'style'}, {role: 'annotation'}],
+			['', '<?php echo JText::_('COM_REDSHOP_SALES_AMOUNT') ?>', {role: 'annotation'}],
 			<?php if (count($this->orders) > 0) :?>
 				<?php foreach ($this->orders as $row) : ?>
-	         		['<?php echo $row->viewdate ?>', <?php echo $row->order_total; ?>, '#0099ff', '<?php echo $producthelper->getProductFormattedPrice($row->order_total); ?>'],
-	       	 	<?php endforeach; ?>
-	       	 <?php else: ?>
-	       	 	[0, 0, '#0099ff', 0],
-	       	 <?php endif; ?>
-	      ]);
+					[
+						'<?php echo $row->viewdate ?>',
+						<?php echo $row->order_total ?>,
+						"<?php echo $productHelper->getProductFormattedPrice($row->order_total) ?>"
+					],
+				<?php endforeach; ?>
+			 <?php else: ?>
+				[0, 0, 0],
+			 <?php endif; ?>
+		  ]);
 
 		var options = {
-			  chart: {
-	            title: '<?php echo JText::_("COM_REDSHOP_STATISTIC_ORDER"); ?>',
-	            subtitle: '<?php echo JText::_("COM_REDSHOP_STATISTIC_ORDER"); ?>',
-	          },
-		  annotations: {
-				boxStyle: {
-					// Color of the box outline.
-					stroke: '#888',
-					// Thickness of the box outline.
-					strokeWidth: 1,
-					// x-radius of the corner curvature.
-					rx: 10,
-					// y-radius of the corner curvature.
-					ry: 10,
-					// Attributes for linear gradient fill.
-					gradient: {
-						// Start color for gradient.
-						color1: '#fbf6a7',
-						// Finish color for gradient.
-						color2: '#33b679',
-						// Where on the boundary to start and
-						// end the color1/color2 gradient,
-						// relative to the upper left corner
-						// of the boundary.
-						x1: '0%', y1: '0%',
-						x2: '100%', y2: '100%',
-						// If true, the boundary for x1,
-						// y1, x2, and y2 is the box. If
-						// false, it's the entire chart.
-						useObjectBoundingBoxUnits: true
-					}
-				}
+			title: "<?php echo JText::_('COM_REDSHOP_STATISTIC_ORDER') ?>",
+			bars: 'vertical',
+			height: 500,
+			vAxis: {
+				format: 'decimal'
+			},
+			legend: {
+				position: 'top'
 			}
 		};
 
@@ -76,13 +59,20 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 	}
 </script>
 <form action="index.php?option=com_redshop&view=statistic_order" method="post" name="adminForm" id="adminForm">
-	<div class="filterTool">
-		<div class="filterItem">
+	<div class="filterTool row-fluid">
+		<div class="filterItem col-md-3">
 			<div class="js-stools clearfix">
 				<?php echo $this->filterForm->getInput('date_range', 'filter') ?>
 			</div>
+		</div>
+		<div class="filterItem col-md-3">
 			<div class="js-stools clearfix">
 				<?php echo $this->filterForm->getInput('date_group', 'filter') ?>
+			</div>
+		</div>
+		<div class="filterItem col-md-1 pull-right">
+			<div class="js-stools clearfix">
+				<?php echo $this->filterForm->getInput('limit', 'list') ?>
 			</div>
 		</div>
 	</div>
@@ -92,27 +82,43 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 		<p><?php echo JText::_('COM_REDSHOP_NO_DATA') ?></p>
 	</div>
 	<?php else: ?>
+		<hr />
 	<div id="order_statistic_chart"></div>
 	<hr />
 	<table class="adminlist table table-striped" width="100%">
 		<thead>
-		<tr>
-			<th align="center"><?php echo JHTML::_('grid.sort', JText::_('COM_REDSHOP_DATE'), 'cdate', $listDirn, $listOrder) ?></th>
-			<th align="center"><?php echo JHTML::_('grid.sort', JText::_('COM_REDSHOP_ORDER_COUNT'), 'count', $listDirn, $listOrder) ?></th>
-			<th align="center"><?php echo JHTML::_('grid.sort', JText::_('COM_REDSHOP_TOTAL_LBL'), 'order_total', $listDirn, $listOrder) ?></th>
-		</tr>
+			<tr>
+				<th align="center">
+					<?php echo JHTML::_('grid.sort', JText::_('COM_REDSHOP_DATE'), 'orderdate', $listDirn, $listOrder) ?>
+				</th>
+				<th align="center" width="10%">
+					<?php echo JHTML::_('grid.sort', JText::_('COM_REDSHOP_ORDER_COUNT'), 'count', $listDirn, $listOrder) ?>
+				</th>
+				<th  style="text-align: right;" width="20%">
+					<?php echo JHTML::_('grid.sort', JText::_('COM_REDSHOP_TOTAL_LBL'), 'order_total', $listDirn, $listOrder) ?>
+				</th>
+			</tr>
 		</thead>
+		<tbody>
 		<?php foreach ($this->orders as $i => $row) : ?>
 			<tr>
 				<td align="center"><?php echo $row->viewdate; ?></td>
 				<td align="center"><?php echo $row->count; ?></td>
-				<td align="center"><?php  echo $producthelper->getProductFormattedPrice($row->order_total);?></td>
+				<td style="text-align: right;"><?php echo $productHelper->getProductFormattedPrice($row->order_total) ?></td>
 			</tr>
 		<?php endforeach; ?>
+		</tbody>
+		<tfoot>
+			<td colspan="3">
+				<?php echo $this->pagination->getListFooter(); ?>
+			</td>
+		</tfoot>
 	</table>
 	<?php endif; ?>
 	<input type="hidden" name="filter_order" value="<?php echo $listOrder ?>" />
 	<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn ?>" />
 	<input type="hidden" name="task" value=""/>
-	<input type="hidden" name="boxchecked" value="0"/>
+	<input type="hidden" name="boxchecked" value="0" />
+	<input type="hidden" name="view" value="statistic_order" />
+	<?php echo JHtml::_('form.token'); ?>
 </form>
