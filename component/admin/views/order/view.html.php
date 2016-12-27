346abe6af8c69a0793e0e1365a5da1c3e51828d4
ducredweb <duc@redweb.dk>
2016-12-27 17:44:28 +0700

    REDSHOP-3862 Change status & mass DDmail sending on multi orders in order backend

--- component/admin/views/order/view.html.php
+++ component/admin/views/order/view.html.php
@@ -85,25 +85,6 @@ class RedshopViewOrder extends RedshopViewAdmin
 				true
 			);
 
-			RedshopToolbarHelper::custom(
-				'allStatusExceptPacsoft',
-				'save.png',
-				'print_f2.png',
-				'COM_REDSHOP_CHANGE_STATUS_TO_ALL_LBL',
-				true
-			);
-
-			if (Redshop::getConfig()->get('POSTDK_INTEGRATION'))
-			{
-				RedshopToolbarHelper::custom(
-					'allstatus',
-					'save.png',
-					'save_f2.png',
-					'COM_REDSHOP_CHANGE_STATUS_TO_ALL_WITH_PACSOFT_LBL',
-					true
-				);
-			}
-
 			$group = RedshopToolbarHelper::createGroup('export', 'COM_REDSHOP_EXPORT_DATA_LBL');
 
 			$group->appendButton('Standard',
@@ -165,8 +146,30 @@ class RedshopViewOrder extends RedshopViewAdmin
 			'class="inputbox" size="1" onchange="document.adminForm.submit();" '
 		);
 
+		$massChangeOrderStatus[]   = JHtml::_('select.option', '', JText::_('COM_REDSHOP_CHANGE_ORDER_STATUS_TO'));
+		$massChangeOrderStatus     = array_merge($massChangeOrderStatus, RedshopHelperOrder::getOrderStatusList());
+		$massChangeOrderStatus 	 =  JHtml::_('select.genericlist', $massChangeOrderStatus, 'mass_change_order_status', ' class="inputbox" size="1" ', 'value', 'text', '');
+
+		$massChangePaymentStatus[] = JHtml::_('select.option', '', JText::_('COM_REDSHOP_CHANGE_PAYMENT_STATUS_TO'));
+		$massChangePaymentStatus[] = JHtml::_('select.option', 'Paid', JText::_('COM_REDSHOP_PAYMENT_STA_PAID'));
+		$massChangePaymentStatus[] = JHtml::_('select.option', 'Unpaid', JText::_('COM_REDSHOP_PAYMENT_STA_UNPAID'));
+		$massChangePaymentStatus[] = JHtml::_('select.option', 'Partial Paid', JText::_('COM_REDSHOP_PAYMENT_STA_PARTIAL_PAID'));
+		$massChangePaymentStatus   = JHtml::_('select.genericlist', $massChangePaymentStatus, 'mass_change_payment_status', ' class="inputbox" size="1" ', 'value', 'text', '');
+
+
+		$massMailSending[] = JHtml::_('select.option', '', '- ' . JText::_('COM_REDSHOP_SEND_ORDER_MAIL') . ' ? -');
+		$massMailSending[] = JHtml::_('select.option', 'No', 'No');
+		$massMailSending[] = JHtml::_('select.option', 'Yes', 'Yes');
+		$massMailSending   = JHtml::_('select.genericlist', $massMailSending, 'mass_mail_sending', ' class="inputbox" size="1" ', 'value', 'text', '');
+
+
+		$massChangeStatus['mass_change_order_status'] =  $massChangeOrderStatus;
+		$massChangeStatus['mass_change_payment_status'] =  $massChangePaymentStatus;
+		$massChangeStatus['mass_mail_sending'] =  $massMailSending;
+
 		$this->user        = JFactory::getUser();
 		$this->lists       = $lists;
+		$this->massChangeStatus       = $massChangeStatus;
 		$this->orders      = $this->get('Data');
 		$this->pagination  = $this->get('Pagination');
 		$this->request_url = JUri::getInstance()->toString();
