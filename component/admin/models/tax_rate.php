1b4688d25478bdb303d69671355f673bded66174
ducredweb <duc@redweb.dk>
2017-02-09 13:33:54 +0700

    REDSHOP-4160 can not save State right after click on "Save" or "Save & Close" button

--- component/admin/models/tax_rate.php
+++ component/admin/models/tax_rate.php
@@ -18,4 +18,24 @@ defined('_JEXEC') or die;
  */
 class RedshopModelTax_Rate extends RedshopModelForm
 {
+	/**
+	 * Method to save the form data.
+	 *
+	 * @param   array  $data  The form data.
+	 *
+	 * @return  boolean  True on success, False on error.
+	 *
+	 * @since   2.0.0.4
+	 */
+	public function save($data)
+	{
+		$post = JFactory::getApplication()->input->post->getArray();
+
+		if (!empty($post['jform_state_code']))
+		{
+			$data['tax_state'] = $post['jform_state_code'];
+		}
+
+		return parent::save($data);
+	}
 }
