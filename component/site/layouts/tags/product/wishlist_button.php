f356ce31fe8084ab9cf116865d9eb334a2e805d9
tuvuong <tu@redweb.dk>
2017-02-10 13:19:55 +0700

    REDSHOP-4136 Remove wishlist button is duplicated

--- component/site/layouts/tags/product/wishlist_button.php
+++ component/site/layouts/tags/product/wishlist_button.php
@@ -21,9 +21,6 @@ extract($displayData);
 $user = JFactory::getUser();
 ?>
 
-<input type="button" class="redshop-wishlist-button" data-productid="<?php echo $productId ?>" data-href="<?php echo $link ?>"
-	data-formid="<?php echo $formId ?>" value="<?php echo JText::_("COM_REDSHOP_ADD_TO_WISHLIST") ?>" />
-
 <?php if (!$user->guest) :?>
 	<input
 		type="button"
