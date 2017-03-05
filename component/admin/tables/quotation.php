2416643d08d7cc31a62ecb2d88e96b4744dbeb4a
thongredweb <thong@redweb.dk>
2017-03-05 18:05:27 +0700

    REDSHOP-3896 Refactor Quotation

---
+++ component/admin/tables/quotation.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package     RedSHOP.Backend
+ * @subpackage  Table
+ *
+ * @copyright   Copyright (C) 2008 - 2016 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Table Quotation
+ *
+ * @package     RedSHOP.Backend
+ * @subpackage  Table
+ * @since       __DEPLOY_VERSION__
+ */
+class RedshopTableQuotation extends RedshopTable
+{
+	/**
+	 * The table name without the prefix. Ex: cursos_courses
+	 *
+	 * @var  string
+	 */
+
+	protected $_tableName = 'redshop_quotation';
+}
