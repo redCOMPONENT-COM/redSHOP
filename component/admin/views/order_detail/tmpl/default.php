ac7df65e45d0574236700a2fd9ad857da6d991ca
Nguyen Hoang Anh <anh@redweb.dk>
2017-03-15 10:46:11 +0700

    REDSHOPphing

--- component/admin/views/order_detail/tmpl/default.php
+++ component/admin/views/order_detail/tmpl/default.php
@@ -1157,7 +1157,14 @@ for ($t = 0; $t < $totalDownloadProduct; $t++)
         console.log(form);
         var subTotal = <?php echo $subtotal_excl_vat ?>;
         var discount = parseFloat(jQuery('#update_discount').val());
+
         var specialDiscount = parseFloat(jQuery('#special_discount').val());
+
+        if ((discount < 0) || (specialDiscount < 0))
+        {
+            alert('<?php echo JText::_("COM_REDSHOP_ORDER_DISCOUNT_NOT_LESS_THAN_ZERO") ?>');
+            return false;
+        }
 
         var totalDiscount = discount + specialDiscount;
 
@@ -1169,4 +1176,36 @@ for ($t = 0; $t < $totalDownloadProduct; $t++)
 
         jQuery(form).submit();
     }
+
+    function validateInputFloat(e)
+    {
+        // Allow: backspace, delete, tab, escape, enter and .
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+            // Allow: Ctrl/cmd+A
+            (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Allow: Ctrl/cmd+C
+            (e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Allow: Ctrl/cmd+X
+            (e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Allow: home, end, left, right
+            (e.keyCode >= 35 && e.keyCode <= 39)) {
+            // let it happen, don't do anything
+            return;
+        }
+        // Ensure that it is a number and stop the keypress
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+    }
+
+    jQuery(document).ready(function() {
+
+        jQuery("#update_discount").keydown(function (e) {
+            validateInputFloat(e);
+        });
+
+        jQuery("#special_discount").keydown(function (e) {
+            validateInputFloat(e);
+        });
+    });
 </script>
