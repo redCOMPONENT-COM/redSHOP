a666bf31cf182c6e1ad62aad344daae43dabddbb
ducredweb <duc@redweb.dk>
2017-04-03 11:29:02 +0700

    fix conflict

--- component/site/helpers/rscarthelper.php
+++ component/site/helpers/rscarthelper.php
@@ -6051,7 +6051,6 @@ class rsCarthelper
 							}
 
 							$accPropertyCart[$ip]['property_id']     = $acc_property_data[$ip];
-							$accPropertyCart[$ip]['attribute_id']    = $property[0]->attribute_id;
 							$accPropertyCart[$ip]['property_name']   = $property[0]->text;
 							$accPropertyCart[$ip]['property_oprand'] = $property[0]->oprand;
 							$accPropertyCart[$ip]['property_price']  = $property_price;
@@ -6097,10 +6096,7 @@ class rsCarthelper
 					}
 				}
 
-				if (!empty($accPropertyCart))
-				{
-					$generateAttributeCart[array_search($accPropertyCart[0]['attribute_id'], $attribute_data)]['attribute_childs'] = $accPropertyCart;
-				}
+				$generateAttributeCart[$ia]['attribute_childs'] = $accPropertyCart;
 			}
 		}
 
