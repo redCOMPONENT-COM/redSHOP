658d0081feb052c39415e5e4b3c81df276603b2f
ducredweb <duc@redweb.dk>
2017-04-21 14:18:04 +0700

    Field detail CRUD view

---
+++ component/admin/models/field.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @package     RedSHOP.Backend
+ * @subpackage  Model
+ *
+ * @copyright   Copyright (C) 2008 - 2017 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Model Field
+ *
+ * @package     RedSHOP.Backend
+ * @subpackage  Model
+ * @since       2.0.4
+ */
+class RedshopModelField extends RedshopModelForm
+{
+	/**
+	 * Method to get the data that should be injected in the form.
+	 *
+	 * @return  mixed  The data for the form.
+	 *
+	 * @since   1.6
+	 */
+	protected function loadFormData()
+	{
+		// Check the session for previously entered form data.
+		$app = JFactory::getApplication();
+		$data = $app->getUserState('com_redshop.edit.field.data', array());
+
+		if (empty($data))
+		{
+			$data = $this->getItem();
+		}
+
+		$this->preprocessData('com_redshop.field', $data);
+
+		return $data;
+	}
+}
