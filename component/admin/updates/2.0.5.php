75f47cbb801ebe0b4ee699b34a5682dbeb6d648b
thongredweb <thong@redweb.dk>
2017-04-27 16:57:02 +0700

    [Install] Optimize in version.class

---
+++ component/admin/updates/2.0.5.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package     RedSHOP.Backend
+ * @subpackage  Updates
+ *
+ * @copyright   Copyright (C) 2008 - 2017 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Abstract update class
+ *
+ * @package     Redshob.Update
+ *
+ * @since       2.0.5
+ */
+class RedshopUpdate205 extends RedshopInstallUpdate
+{
+	/**
+	 * Method for update menu.
+	 *
+	 * @return  void
+	 *
+	 * @since   2.0.5
+	 */
+	public function updateCategory()
+	{
+
+	}
+}
