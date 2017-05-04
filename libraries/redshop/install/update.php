285c77a04191d6989938ba896fea4644a85fd92c
thongredweb <thong@redweb.dk>
2017-05-04 09:46:25 +0700

    [Optimize] Installation separate version in specific class.

--- libraries/redshop/install/update.php
+++ libraries/redshop/install/update.php
@@ -18,4 +18,40 @@ use Redshop\Update\AbstractUpdate;
  */
 class RedshopInstallUpdate extends AbstractUpdate
 {
+	/**
+	 * Return list of old files for clean
+	 *
+	 * @return  array
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	protected function getOldFiles()
+	{
+		return array();
+	}
+
+	/**
+	 * Return list of old folders for clean
+	 *
+	 * @return  array
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	protected function getOldFolders()
+	{
+		return array();
+	}
+
+	/**
+	 * Method clean old files
+	 *
+	 * @return  void
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function cleanOldFiles()
+	{
+		$this->deleteFolders($this->getOldFolders());
+		$this->deleteFiles($this->getOldFiles());
+	}
 }
