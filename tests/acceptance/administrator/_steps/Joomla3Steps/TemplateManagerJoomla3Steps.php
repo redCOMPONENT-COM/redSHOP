0cbed8aa966ae6d97e0e4eccb7772479b2395940
thongredweb <thong@redweb.dk>
2017-05-15 23:22:36 +0700

    REDSHOP-4392 Update acceptance test

--- tests/acceptance/administrator/_steps/Joomla3Steps/TemplateManagerJoomla3Steps.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/TemplateManagerJoomla3Steps.php
@@ -128,7 +128,7 @@ class TemplateManagerJoomla3Steps extends AdminManagerJoomla3Steps
 		$I->amOnPage(\TemplateManagerJoomla3Page::$URL);
 		$I->checkForPhpNoticesOrWarnings();
 		$I->searchTemplate($templateName);
-		$I->checkAllResults();
+		$I->click(\TemplateManagerJoomla3Page::$firstResultRow);
 		$I->click("Delete");
 		$I->acceptPopup();
 		$I->waitForText("1 item successfully deleted", 60, '.alert-success');
