9ced8eef07247f95e71e6896b10a09f83f68514f
Tu Vuong <tu@redweb.dk>
2017-05-18 10:27:14 +0700

    Create Product Gift Plugin

--- plugins/redshop_product/gift/layouts/cart.php
+++ plugins/redshop_product/gift/layouts/cart.php
@@ -12,7 +12,10 @@ extract($displayData);
 
 ?>
 <div class="gift_title"><?php echo JText::_('PLG_REDSHOP_PRODUCT_GIFT_TITLE'); ?></div>
-<div class="gift_row">
-	<div class="product_name"><?php echo $data['product']->product_name; ?></div>
-	<div class="quantity"><?php echo $data['quantity']; ?></div>
-</div>
+<?php foreach ($data as $key => $gift) : ?>
+	<div class="gift_row">
+		<?php $product = RedshopHelperProduct::getProductById($gift['product_id']); ?>
+		<div class="product_name"><?php echo $product->product_name; ?></div>
+		<div class="quantity"><?php echo $gift['quantity']; ?></div>
+	</div>
+<?php endforeach; ?>
