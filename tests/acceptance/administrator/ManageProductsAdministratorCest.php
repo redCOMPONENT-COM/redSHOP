f74a01c1cf747fd4997359b4315b712668bcf53e
nhung@redweb.dk <nhung@redweb.dk>
2017-05-23 13:43:51 +0700

    code for product

--- tests/acceptance/administrator/ManageProductsAdministratorCest.php
+++ tests/acceptance/administrator/ManageProductsAdministratorCest.php
@@ -18,7 +18,7 @@
 class ManageProductsAdministratorCest
 {
 
-    public function _construc()
+    public function __construct()
     {
         $this->randomCategoryName = 'TestingCategory' . rand(99, 999);
         $this->randomProductName = 'Testing Products' . rand(99, 999);
@@ -36,28 +36,168 @@ class ManageProductsAdministratorCest
      */
     public function testProductAdministrator(AcceptanceTester $I, $scenario)
     {
-        $I->wantTo('Test Product Manager in Administrator');
+        $I->wantTo('Create Category in Administrator');
         $I->doAdministratorLogin();
         $I = new AcceptanceTester\CategoryManagerJoomla3Steps($scenario);
         $I->wantTo('Create a Category');
         $I->addCategory($this->randomCategoryName);
-        $I->wantTo('I Want to add product inside the category');
-        $this->createProduct($I, $this->randomProductName, $this->randomCategoryName, $this->randomProductNumber, $this->randomProductPrice);
-        $this->searchProduct($I, $this->randomProductName);
-        $I->wantTo('Delete the product which was created');
-        $this->deleteProduct($I, $this->randomProductName);
-        $I->wantTo('Delete Category');
-        $I->deleteCategory($this->randomCategoryName);
+//        $I->see($this->randomCategoryName);
+
+//        $I->wantTo('I Want to add product inside the category');
+//        $I->createProduct($this->randomProductName, $this->randomCategoryName, $this->randomProductNumber, $this->randomProductPrice);
+//        $this->searchProduct($I, $this->randomProductName);
+//        $I->wantTo('Delete the product which was created');
+//        $this->deleteProduct($I, $this->randomProductName);
+//        $I->wantTo('Delete Category');
+//        $I->deleteCategory($this->randomCategoryName);
     }
 
+    /**
+     * @param AcceptanceTester $I
+     * @param $scenario
+     */
+    public function checkEditButton(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test Edit Button Product Manager in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\ProductManagerJoomla3Steps($scenario);
+        $I->wantTo('I Want to add product inside the category');
+        $I->checkEditButton();
+    }
+
+
+    /**
+     * @param AcceptanceTester $I
+     * @param $scenario
+     */
+    public function checkCopyButton(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test Copy Button Product Manager in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\ProductManagerJoomla3Steps($scenario);
+        $I->wantTo('I Want to add product inside the category');
+        $I->checkCopyButton();
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     * @param $scenario
+     */
+    public function checkDeleteButton(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test Delete  Button Product Manager in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\ProductManagerJoomla3Steps($scenario);
+        $I->wantTo('I Want to add product inside the category');
+        $I->checkDeleteButton();
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     * @param $scenario
+     */
+    public function checkPublishButton(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test Publish Button Product Manager in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\ProductManagerJoomla3Steps($scenario);
+        $I->wantTo('I Want to add product inside the category');
+        $I->checkPublishButton();
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     * @param $scenario
+     */
+    public function checkUnpublishButton(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test Unpublish Button Product Manager in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\ProductManagerJoomla3Steps($scenario);
+        $I->wantTo('I Want to add product inside the category');
+        $I->checkUnpublishButton();
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     * @param $scenario
+     */
+    public function checkAssignNewCategoryButton(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test Assign New Category Button Product Manager in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\ProductManagerJoomla3Steps($scenario);
+        $I->wantTo('I Want to add product inside the category');
+        $I->checkAssignNewCategoryButton();
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     * @param $scenario
+     */
+    public function checkRemoveCategoryButton(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test Assign New Category Button Product Manager in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\ProductManagerJoomla3Steps($scenario);
+        $I->wantTo('I Want to add product inside the category');
+        $I->checkRemoveCategoryButton();
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     * @param $scenario
+     *
+     */
     public function createProductSave(AcceptanceTester $I, $scenario)
     {
-        $I->wantTo('Test Product Manager in Administrator');
+        $I->wantTo('Test Product Save Manager in Administrator');
         $I->doAdministratorLogin();
-        $I = new AcceptanceTester\CategoryManagerJoomla3Steps($scenario);
+        $I = new AcceptanceTester\ProductManagerJoomla3Steps($scenario);
+        $I->wantTo('I Want to add product inside the category');
+        $I->createProductSave($this->randomProductName, $this->randomCategoryName, $this->randomProductNumber, $this->randomProductPrice);
+    }
+
+    public function deleteProduct(AcceptanceTester $I,  $scenario)
+    {
+        $I->wantTo('Delete Product Manager in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\ProductManagerJoomla3Steps($scenario);
+        $I->wantTo('I Want to delete product inside the category');
+        $I->deleteProduct($this->randomProductName);
     }
 
 
+    /**
+     * @param AcceptanceTester $I
+     * @param $scenario
+     */
+
+    public function createProductSaveClose(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test Product Save Manager in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\ProductManagerJoomla3Steps($scenario);
+        $I->wantTo('I Want to add product inside the category');
+        $I->createProductSaveClose($this->randomProductName, $this->randomCategoryName, $this->randomProductNumber, $this->randomProductPrice);
+    }
+
+
+    public function createProductSaveNew(AcceptanceTester $I, $scenario){
+        $I->wantTo('Test Product Save Manager in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\ProductManagerJoomla3Steps($scenario);
+        $I->wantTo('I Want to add product inside the category');
+        $I->createProductSaveNew($this->randomProductName, $this->randomCategoryName, $this->randomProductNumber, $this->randomProductPrice);
+    }
+
+    public function createProductCancel(AcceptanceTester $I, $scenario){
+        $I->wantTo('Test Product Save Manager in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\ProductManagerJoomla3Steps($scenario);
+        $I->wantTo('I Want to add product inside the category');
+        $I->createProductCancel();
+    }
     /**
      * Function to create a Product
      *
@@ -69,25 +209,25 @@ class ManageProductsAdministratorCest
      *
      * @return void
      */
-    private function createProduct(AcceptanceTester $I, $productName, $productCategory, $productNumber, $price)
-    {
-        $I->amOnPage(\ProductManagerPage::$URL);
-        $I->checkForPhpNoticesOrWarnings();
-        $I->waitForText('Product Management', 30, ['xpath' => "//h1"]);
-        $I->click("New");
-        $I->waitForElement(['id' => "product_name"], 30);
-        $I->checkForPhpNoticesOrWarnings();
-        $I->fillField(['id' => "product_name"], $productName);
-        $I->fillField(['id' => "product_number"], $productNumber);
-        $I->fillField(['id' => "product_price"], $price);
-        $I->click(['xpath' => "//div[@id='s2id_product_category']//ul/li"]);
-        $I->fillField(['xpath' => "//div[@id='s2id_product_category']//ul/li//input"], $productCategory);
-        $I->waitForElement(['xpath' => "//span[contains(text(), '" . $productCategory . "')]"]);
-        $I->click(['xpath' => "//span[contains(text(), '" . $productCategory . "')]"]);
-        $I->click("Save & Close");
-        $I->waitForText('Product details saved', 30, ['class' => 'alert-success']);
-        $I->see('Product details saved', ['class' => 'alert-success']);
-    }
+//    private function createProduct(AcceptanceTester $I, $productName, $productCategory, $productNumber, $price)
+//    {
+//        $I->amOnPage(\ProductManagerPage::$URL);
+//        $I->checkForPhpNoticesOrWarnings();
+//        $I->waitForText('Product Management', 30, ['xpath' => "//h1"]);
+//        $I->click("New");
+//        $I->waitForElement(['id' => "product_name"], 30);
+//        $I->checkForPhpNoticesOrWarnings();
+//        $I->fillField(['id' => "product_name"], $productName);
+//        $I->fillField(['id' => "product_number"], $productNumber);
+//        $I->fillField(['id' => "product_price"], $price);
+//        $I->click(['xpath' => "//div[@id='s2id_product_category']//ul/li"]);
+//        $I->fillField(['xpath' => "//div[@id='s2id_product_category']//ul/li//input"], $productCategory);
+//        $I->waitForElement(['xpath' => "//span[contains(text(), '" . $productCategory . "')]"]);
+//        $I->click(['xpath' => "//span[contains(text(), '" . $productCategory . "')]"]);
+//        $I->click("Save & Close");
+//        $I->waitForText('Product details saved', 30, ['class' => 'alert-success']);
+//        $I->see('Product details saved', ['class' => 'alert-success']);
+//    }
 
 
     /**
@@ -98,21 +238,21 @@ class ManageProductsAdministratorCest
      *
      * @return void
      */
-    private function deleteProduct(AcceptanceTester $I, $productName)
-    {
-        $I->wantTo('Delete an existing Product');
-        $I->amOnPage('administrator/index.php?option=com_redshop&view=product');
-        $I->waitForText('Product Management', 30, ['xpath' => "//h1"]);
-        $I->fillField(['xpath' => "//div[@class='filterItem']//div//input[@name='keyword']"], $productName);
-        $I->click(['xpath' => "//div[@class='filterItem']//div//input[@value='Search']"]);
-        $I->see($productName, ['xpath' => "//table[contains(@class, 'adminlist')]//tbody//tr[1]"]);
-        $I->checkAllResults();
-        $I->click("Delete");
-        $I->acceptPopup();
-        $I->waitForText('Product deleted successfully', 30, ['class' => 'alert-success']);
-        $this->searchProduct($I, $productName, 'Delete');
-        $I->dontSee($productName);
-    }
+//    private function deleteProduct(AcceptanceTester $I, $productName)
+//    {
+//        $I->wantTo('Delete an existing Product');
+//        $I->amOnPage('administrator/index.php?option=com_redshop&view=product');
+//        $I->waitForText('Product Management', 30, ['xpath' => "//h1"]);
+//        $I->fillField(['xpath' => "//div[@class='filterItem']//div//input[@name='keyword']"], $productName);
+//        $I->click(['xpath' => "//div[@class='filterItem']//div//input[@value='Search']"]);
+//        $I->see($productName, ['xpath' => "//table[contains(@class, 'adminlist')]//tbody//tr[1]"]);
+//        $I->checkAllResults();
+//        $I->click("Delete");
+//        $I->acceptPopup();
+//        $I->waitForText('Product deleted successfully', 30, ['class' => 'alert-success']);
+//        $this->searchProduct($I, $productName, 'Delete');
+//        $I->dontSee($productName);
+//    }
 
     /**
      * Function to Search for a Product
