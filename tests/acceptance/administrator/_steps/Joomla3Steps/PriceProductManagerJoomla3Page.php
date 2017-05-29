1a3bceab830ef82b8c079c5926dacf83f0fe456e
nhung@redweb.dk <nhung@redweb.dk>
2017-05-29 14:31:36 +0700

    create file for price product

---
+++ tests/acceptance/administrator/_steps/Joomla3Steps/PriceProductManagerJoomla3Page.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nhung nguyen
+ * Date: 5/29/2017
+ * Time: 2:03 PM
+ */
+
+namespace AcceptanceTester;
+
+
+class PriceProductManagerJoomla3Page extends AdminManagerJoomla3Steps
+{
+
+}
