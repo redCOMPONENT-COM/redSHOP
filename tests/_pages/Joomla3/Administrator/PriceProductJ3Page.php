eec07d2ad975e6b19db69ebad8ee00f881e567e2
nhung@redweb.dk <nhung@redweb.dk>
2017-05-30 10:52:37 +0700

    codeception for product price

--- tests/_pages/Joomla3/Administrator/PriceProductJ3Page.php
+++ tests/_pages/Joomla3/Administrator/PriceProductJ3Page.php
@@ -9,5 +9,9 @@
 class PriceProductJ3Page
 {
     public static $URL = '/administrator/index.php?option=com_redshop&view=product&layout=listing';
-
+    public static $discount = ['name' => "discount_price[]"];
+    public static $priceProduct = ['id' => "product_price_44"];
+    public static $quantityStart = ['id' => "price_quantity_start"];
+    public static $quantityEnd = ['id' => "price_quantity_end"];
+    public static $priceDefault =['name'=>"price[]"];
 }
--- tests/acceptance/administrator/PriceProductAdministratorCest.php
+++ tests/acceptance/administrator/PriceProductAdministratorCest.php
@@ -9,15 +9,74 @@
 class PriceProductAdministratorCest
 {
 
-    public function createCategorySave(AcceptanceTester $I, $scenario)
+    public function __construct()
+    {
+        $this->randomCategoryName = 'TestingCategory' . rand(99, 999);
+        $this->randomProductName = 'Testing Products' . rand(99, 999);
+        $this->randomProductNumber = rand(999, 9999);
+        $this->randomProductPrice = rand(11, 100);
+        $this->randomPriceDiscount = rand(5, 10);
+        $this->randomPriceLess = rand(1, 4);
+        $this->randomPriceDiscounctMoreThan = rand(101, 200);
+        $this->quantityStart=10;
+        $this->quantityEnd=100;
+        $this->priceDefault=100;
+    }
+
+
+    public function testProductAdministrator(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Create Category in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\CategoryManagerJoomla3Steps($scenario);
+        $I->wantTo('Create a Category');
+        $I->addCategory($this->randomCategoryName);
+    }
+
+    public function createProductSaveClose(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test Product Save Close Manager in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\ProductManagerJoomla3Steps($scenario);
+        $I->wantTo('I Want to add product inside the category');
+        $I->createProductSaveClose($this->randomProductName, $this->randomCategoryName, $this->randomProductNumber, $this->randomProductPrice);
+    }
+
+    //search product then insert discount price
+    public function addDiscountPrice(AcceptanceTester $I, $scenario)
     {
         $I->wantTo('Test Change Price of Product in Administrator');
         $I->doAdministratorLogin();
         $I = new AcceptanceTester\PriceProductManagerJoomla3Page($scenario);
         $I->wantTo('Create a Category Save button');
-        $I->addCategorySave($this->categoryName);
-        $I->see("item successfully saved", '.alert-success');
+        $I->addDiscountPrice($this->randomProductName, $this->randomPriceDiscount);
+        $I->see("Product Management", '.page-title');
     }
+
+    //this case can not run at firefox because have something wrong at code .
+    // When develop fixed the issues please open code
+//    public function addDiscountPriceMoreThan(AcceptanceTester $I, $scenario)
+//    {
+//        $I->wantTo('Test Change Price of Product in Administrator');
+//        $I->doAdministratorLogin();
+//        $I = new AcceptanceTester\PriceProductManagerJoomla3Page($scenario);
+//        $I->wantTo('Create a Category Save button');
+//        $I->addDiscountPriceMoreThan($this->randomProductName, $this->randomPriceDiscountMoreThan);
+//        $I->see("Product Management", '.page-title');
+//    }
+//
+//
+//    public function addPriceLessDiscount(AcceptanceTester $I, $scenario)
+//    {
+//        $I->wantTo('Test Change Price of Product in Administrator');
+//        $I->doAdministratorLogin();
+//        $I = new AcceptanceTester\PriceProductManagerJoomla3Page($scenario);
+//        $I->wantTo('Create a Category Save button');
+//        $I->addPriceLessDiscount($this->randomProductName, $this->randomPriceLess);
+//        $I->see("Product Management", '.page-title');
+//    }
+
+
 
 
 }
