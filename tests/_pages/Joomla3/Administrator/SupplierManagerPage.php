f48989653fcc2c20d5898bb9f3119a42ccbac109
nhung@redweb.dk <nhung@redweb.dk>
2017-06-05 19:42:24 +0700

    push code for manage supplier

--- tests/_pages/Joomla3/Administrator/SupplierManagerPage.php
+++ tests/_pages/Joomla3/Administrator/SupplierManagerPage.php
@@ -15,5 +15,28 @@
  */
 class SupplierManagerPage
 {
-	public static $URL = '/administrator/index.php?option=com_redshop&view=suppliers';
+    public static $URL = '/administrator/index.php?option=com_redshop&view=suppliers';
+
+    public static $checkAllSupplier = "//input[@onclick='Joomla.checkAll(this)']";
+
+    public static $supplierNameField = ['id' => "jform_name"];
+
+    public static $supplierEmailId = ['id' => "jform_email"];
+
+    public static $supplierSuccessMessage = "Item successfully saved.";
+
+
+    public static $supplierResultRow = "//table[contains(@class, 'adminlist')]/tbody/tr[1]";
+
+    public static  $searchField = ['id' => 'filter_search'];
+
+    public static $fieldMissing="Field required: Name";
+
+    public static $fieldEmailInvalid="Invalid field:  Supplier Email ";
+
+    public static $supplierResultName="//div[@class='table-responsive']/table/tbody/tr/td[2]";
+
+    public static $supplierStatePath = "//div[@class='table-responsive']/table/tbody/tr/td[4]/a";
+
+
 }
