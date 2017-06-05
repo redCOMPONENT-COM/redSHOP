31e74d988efb4adbb71593354f1776fca32b9ecf
lu.redweb <lu@redweb.dk>
2017-06-05 14:13:20 +0700

    PALMARINE-717 [Fix] Girocheckout payment plugin

--- plugins/redshop_payment/rs_payment_giropay/rs_payment_giropay/extra_info.php
+++ plugins/redshop_payment/rs_payment_giropay/rs_payment_giropay/extra_info.php
@@ -9,36 +9,31 @@
 
 JLoader::import('redshop.library');
 
-$uri       = JURI::getInstance();
-$url       = $uri->root();
-$user      = JFactory::getUser();
-$sessionid = session_id();
-$amount    = RedshopHelperCurrency::convert($data['carttotal'], '', "EUR");
+$app    = JFactory::getApplication();
+$itemId = $app->input->getInt('Itemid', 0);
 
-$parameter['sourceId']      = $this->params->get("source_id");
-$parameter['merchantId']    = $this->params->get("merchant_id");
-$parameter['projectId']     = $this->params->get("project_id");
+$amount = RedshopHelperCurrency::convert($data['carttotal'], '', 'EUR');
+
+$parameter['sourceId']      = $this->params->get('source_id');
+$parameter['merchantId']    = $this->params->get('merchant_id');
+$parameter['projectId']     = $this->params->get('project_id');
 $parameter['transactionId'] = $data['order_id'];
 $parameter['amount']        = number_format($amount, 2, '.', '');
-$parameter['vwz']           = "Order";
+$parameter['vwz']           = 'Order';
 $parameter['bankcode']      = '';
-$parameter['urlRedirect']   = JURI::base() . "index.php?option=com_redshop&view=order_detail&oid=" . $data['order_id'];
-$parameter['urlNotify']     = JURI::base() . "index.php?option=com_redshop&view=order_detail&tmpl=component&controller=order_detail&task=notify_payment&payment_plugin=rs_payment_giropay&orderid=" . $data['order_id'];
+$parameter['urlRedirect']   = JURI::base() . 'index.php?option=com_redshop&view=order_detail&tmpl=component&controller=order_detail&task=notify_payment&payment_plugin=rs_payment_giropay&orderid=' . $data['order_id'] . '&Itemid=' . $itemId;
+$parameter['urlNotify']     = JURI::base() . 'index.php?option=com_redshop&view=order_detail&oid=' . $data['order_id'] . '&Itemid=' . $itemId;
 
-$secret_password = $this->params->get("secret_password");
-$hash            = $gsGiropay->generateHash(implode('', $parameter), $secret_password);
+$secret_password = $this->params->get('secret_password', '');
+$parameter['hash'] = $gsGiropay->generateHash(implode('', $parameter), $secret_password);
 ?>
 <form action="https://payment.girosolution.de/payment/start" method="post" name="giropayfrm" id="giropayfrm">
-    <input type="hidden" name="sourceId" value="<?php echo $parameter['sourceId']; ?>">
-    <input type="hidden" name="merchantId" value="<?php echo $parameter['merchantId']; ?>">
-    <input type="hidden" name="projectId" value="<?php echo $parameter['projectId']; ?>">
-    <input type="hidden" name="transactionId" value="<?php echo $data['order_id']; ?>">
-    <input type="hidden" name="amount" value="<?php echo $parameter['amount']; ?>"/>
-    <input type="hidden" name="vwz" value="<?php echo $parameter['vwz']; ?>">
-    <input type="hidden" name="bankcode" value="<?php echo $parameter['bankcode']; ?>">
-    <input type="hidden" name="urlNotify" value="<?php echo $parameter['urlNotify']; ?>">
-    <input type="hidden" name="urlRedirect" value="<?php echo $parameter['urlRedirect']; ?>">
-    <input type="hidden" name="hash" value="<?php echo $hash; ?>"/>
+    <?php
+    foreach ($parameter as $name => $value)
+    {
+        echo '<input type="hidden" name="' . $name . '" value="' . $value . '" />';
+    }
+    ?>
 </form>
 <script type='text/javascript'>
     window.onload = function () {
