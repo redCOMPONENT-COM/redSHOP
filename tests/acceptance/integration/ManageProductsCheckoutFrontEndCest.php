845275e5da379ad2b848355f62627afbf707f82e
nhung@redweb.dk <nhung@redweb.dk>
2017-06-08 17:26:45 +0700

    code for product in fontend

--- tests/acceptance/integration/ManageProductsCheckoutFrontEndCest.php
+++ tests/acceptance/integration/ManageProductsCheckoutFrontEndCest.php
@@ -25,7 +25,56 @@ class ManageProductsCheckoutFrontEndCest
 	 *
 	 * @return void
 	 */
-	public function testProductsCheckoutFrontEnd(AcceptanceTester $I, $scenario)
+    public function __construct()
+    {
+        $this->categoryName = 'TestingCategory' . rand(99, 999);
+        $this->ramdoCategoryNameAssign = 'CategoryAssign' . rand(99, 999);
+        $this->productName = 'Testing Products' . rand(99, 999);
+        $this->minimumPerProduct = 2;
+        $this->minimumQuantity = 3;
+        $this->maximumQuantity = 5;
+        $this->discountStart = "12-12-2016";
+        $this->discountEnd = "23-05-2017";
+        $this->randomProductNumber = rand(999, 9999);
+        $this->randomProductNumberNew = rand(999, 9999);
+        $this->randomProductAttributeNumber = rand(999, 9999);
+        $this->randomProductNameAttribute = 'Testing Attribute' . rand(99, 999);
+        $this->randomProductPrice = rand(99, 199);
+        $this->discountPriceThanPrice =100;
+        $this->statusProducts = 'Product on sale';
+        $this->searchCategory = 'Category';
+        $this->newProductName = 'New-Test Product' . rand(99, 999);
+        $this->nameAttribute = 'Size';
+        $this->valueAttribute = "Z";
+        $this->priceAttribute = 12;
+        $this->nameProductAccessories = "redFORM";
+        $this->nameRelatedProduct = "redITEM";
+        $this->quantityStock = 4;
+        $this->PreorderStock = 2;
+        $this->priceProductForThan=10;
+
+    }
+
+
+    public function testProductAdministrator(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Create Category in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\CategoryManagerJoomla3Steps($scenario);
+        $I->wantTo('Create a Category');
+        $I->addCategorySave($this->categoryName);
+    }
+    public function createProductSave(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test Product Save Manager in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\ProductManagerJoomla3Steps($scenario);
+        $I->wantTo('I Want to add product inside the category');
+        $I->createProductSave($this->productName, $this->categoryName, $this->randomProductNumber, $this->randomProductPrice, $this->minimumPerProduct, $this->minimumQuantity, $this->maximumQuantity, $this->discountStart, $this->discountEnd);
+    }
+
+
+    public function testProductsCheckoutFrontEnd(AcceptanceTester $I, $scenario)
 	{
 		$I = new AcceptanceTester($scenario);
 		$I->wantTo('Test Product Checkout on Front End with Bank Transfer');
@@ -76,10 +125,8 @@ class ManageProductsCheckoutFrontEndCest
 
 		}*/
 
-		$productName = 'redITEM';
-		$categoryName = 'CCK and e-Commerce';
 
-		$this->checkOutProductWithBankTransfer($I, $scenario, $customerInformation, $customerInformation, $productName, $categoryName);
+		$this->checkOutProductWithBankTransfer($I, $scenario, $customerInformation, $customerInformation, $this->productName, $this->categoryName);
 	}
 
 	/**
@@ -94,7 +141,7 @@ class ManageProductsCheckoutFrontEndCest
 	 *
 	 * @return void
 	 */
-	private function checkOutProductWithBankTransfer(AcceptanceTester $I, $scenario, $addressDetail, $shipmentDetail, $productName = 'redCOOKIE', $categoryName = 'Events and Forms')
+	private function checkOutProductWithBankTransfer(AcceptanceTester $I, $scenario, $addressDetail, $shipmentDetail, $productName, $categoryName )
 	{
 		$I->amOnPage(\FrontEndProductManagerJoomla3Page::$URL);
 		$I->waitForElement(\FrontEndProductManagerJoomla3Page::$categoryDiv,30);
