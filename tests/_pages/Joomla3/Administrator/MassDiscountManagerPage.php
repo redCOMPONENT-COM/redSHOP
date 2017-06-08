9cb2b9af0e496ff8756763217521fdf4ad3e88c6
nhung@redweb.dk <nhung@redweb.dk>
2017-06-08 14:15:41 +0700

    add mass discount

---
+++ tests/_pages/Joomla3/Administrator/MassDiscountManagerPage.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: nhung nguyen
+ * Date: 6/8/2017
+ * Time: 1:59 PM
+ */
+class MassDiscountManagerPage
+{
+    public static $URL = '/administrator/index.php?option=com_redshop&view=mass_discounts';
+    public static $name = "#jform_name";
+    public static $valueAmount = "#jform_amount";
+    public static $pathNameProduct = "#s2id_autogen3";
+}
---
+++ tests/acceptance/administrator/_steps/Joomla3Steps/MassDiscountManagerJoomla3Steps.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nhung nguyen
+ * Date: 6/8/2017
+ * Time: 2:00 PM
+ */
+
+namespace AcceptanceTester;
+
+
+class MassDiscountManagerJoomla3Steps extends AdminManagerJoomla3Steps
+{
+
+    public function addMassDiscount($massDiscountName , $amountValue, $nameProduct ){
+        $I=$this;
+        $I->amOnPage(\MassDiscountManagerPage::$URL);
+        $I->click('New');
+        $I->verifyNotices(false,$this->checkForNotices(),'product mass discount new');
+        $I->checkForPhpNoticesOrWarnings();
+        $I->fillField(\MassDiscountManagerPage::$name,$massDiscountName);
+        $I->fillField(\MassDiscountManagerPage::$valueAmount,$amountValue);
+        $I->click(\MassDiscountManagerPage::$pathNameProduct);
+//        $I->waitForElement()
+
+        $I->click('Save');
+
+    }
+}
