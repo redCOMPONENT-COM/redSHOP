dc1796c8acec7bc230e6139507557b67e0c4e028
Tu Vuong <tu@redweb.dk>
2017-06-09 14:53:44 +0700

    KON-100 Move custom fields to layout

---
+++ component/site/layouts/fields/editor.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package     RedSHOP.Frontend
+ * @subpackage  Template
+ *
+ * @copyright   Copyright (C) 2008 - 2017 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * $displayData extract
+ *
+ * @param   object  $rowData          Extra field data
+ * @param   string  $extraFieldLabel  Extra field label
+ * @param   string  $required         Extra field required
+ * @param   string  $requiredLabel    Extra field required label
+ * @param   string  $errorMsg         Extra field error message
+ * @param   string  $textValue        Extra field value
+ */
+extract($displayData);
+?>
+
+<td valign="top" width="100" align="right" class="key">
+	<?php echo $extraFieldLabel; ?>
+</td>
+<td>
+	<?php echo $editor->display($rowData[$i]->name, $textValue, '200', '50', '100', '20', false); ?>
