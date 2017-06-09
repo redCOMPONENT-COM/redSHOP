b967762999a7c8dd43ed1301e43b34c989e0eaed
nhung@redweb.dk <nhung@redweb.dk>
2017-06-09 14:11:54 +0700

    codeception for Mass Discount

--- tests/_pages/Joomla3/Administrator/MassDiscountManagerPage.php
+++ tests/_pages/Joomla3/Administrator/MassDiscountManagerPage.php
@@ -12,4 +12,10 @@ class MassDiscountManagerPage
     public static $name = "#jform_name";
     public static $valueAmount = "#jform_amount";
     public static $pathNameProduct = "#s2id_autogen3";
+    public static $categoryField = "#s2id_jform_category_id";
+    public static $dayStart = "#jform_start_date";
+    public static $dayEnd = "#jform_end_date";
+    public static $checkFirstItems = "//input[@id='cb0']";
+    public static $MassDiscountFilter = "#filter_search";
+
 }
