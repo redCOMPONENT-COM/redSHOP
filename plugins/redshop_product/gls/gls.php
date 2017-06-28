98f453468c801a332314e3aec4a43ec815744d05
Viet Vu <jooservices@gmail.com>
2017-06-28 13:44:01 +0700

    RSC-98 Code updated

--- plugins/redshop_product/gls/gls.php
+++ plugins/redshop_product/gls/gls.php
@@ -3,7 +3,7 @@
  * @package     RedSHOP
  * @subpackage  Plugin
  *
- * @copyright   Copyright (C) 2008 - 2015 redCOMPONENT.com. All rights reserved.
+ * @copyright   Copyright (C) 2008 - 2017 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
@@ -12,7 +12,7 @@ defined('_JEXEC') or die;
 /**
  * Update Shipping information for GLS Shipping Plugin
  *
- * @since  1.3.3.1
+ * @since  1.0
  */
 class PlgRedshop_ProductGls extends JPlugin
 {
