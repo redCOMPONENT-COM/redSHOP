869048fefbdd6c6dcb83f1e91f6780c799a5d070
nhung@redweb.dk <nhung@redweb.dk>
2017-07-07 17:19:42 +0700

    change code inside page

--- tests/acceptance/administrator/PriceProductAdministratorCest.php
+++ tests/acceptance/administrator/PriceProductAdministratorCest.php
@@ -1,10 +1,13 @@
 <?php
 
 /**
- * Created by PhpStorm.
- * User: nhung nguyen
- * Date: 5/29/2017
- * Time: 2:06 PM
+ * Class PriceProductAdministratorCest
+ *
+ * @package  AcceptanceTester
+ *
+ * @link     http://codeception.com/docs/07-AdvancedUsage
+ *
+ * @since    1.4
  */
 class PriceProductAdministratorCest
 {
@@ -18,12 +21,18 @@ class PriceProductAdministratorCest
         $this->randomPriceDiscount = rand(5, 10);
         $this->randomPriceLess = rand(1, 4);
         $this->randomPriceDiscounctMoreThan = rand(101, 200);
-        $this->quantityStart=10;
-        $this->quantityEnd=100;
-        $this->priceDefault=100;
+        $this->quantityStart = 10;
+        $this->quantityEnd = 100;
+        $this->priceDefault = 100;
     }
 
-
+    /**
+     *
+     * Function create category
+     *
+     * @param AcceptanceTester $I
+     * @param $scenario
+     */
     public function testProductAdministrator(AcceptanceTester $I, $scenario)
     {
         $I->wantTo('Create Category in Administrator');
@@ -33,6 +42,13 @@ class PriceProductAdministratorCest
         $I->addCategory($this->randomCategoryName);
     }
 
+    /**
+     *
+     * Function create product
+     *
+     * @param AcceptanceTester $I
+     * @param $scenario
+     */
     public function createProductSaveClose(AcceptanceTester $I, $scenario)
     {
         $I->wantTo('Test Product Save Close Manager in Administrator');
@@ -41,7 +57,13 @@ class PriceProductAdministratorCest
         $I->wantTo('I Want to add product inside the category');
         $I->createProductSaveClose($this->randomProductName, $this->randomCategoryName, $this->randomProductNumber, $this->randomProductPrice);
     }
-
+    /**
+     *
+     * add Discount Price for product
+     *
+     * @param AcceptanceTester $I
+     * @param $scenario
+     */
     //search product then insert discount price
     public function addDiscountPrice(AcceptanceTester $I, $scenario)
     {
@@ -50,11 +72,11 @@ class PriceProductAdministratorCest
         $I = new AcceptanceTester\PriceProductManagerJoomla3Steps($scenario);
         $I->wantTo('Create a Category Save button');
         $I->addDiscountPrice($this->randomProductName, $this->randomPriceDiscount);
-        $I->see("Product Management", '.page-title');
+        $I->see(\PriceProductJoomla3Page::$namePage, \PriceProductJoomla3Page::$selectorPage);
     }
 
-    //this case can not run at firefox because have something wrong at code .
-    // When develop fixed the issues please open code
+//    //this case can not run at firefox because have something wrong at code .
+//    // When develop fixed the issues please open code
 //    public function addDiscountPriceMoreThan(AcceptanceTester $I, $scenario)
 //    {
 //        $I->wantTo('Test Change Price of Product in Administrator');
@@ -64,8 +86,7 @@ class PriceProductAdministratorCest
 //        $I->addDiscountPriceMoreThan($this->randomProductName, $this->randomPriceDiscountMoreThan);
 //        $I->see("Product Management", '.page-title');
 //    }
-//
-//
+
 //    public function addPriceLessDiscount(AcceptanceTester $I, $scenario)
 //    {
 //        $I->wantTo('Test Change Price of Product in Administrator');
@@ -75,6 +96,4 @@ class PriceProductAdministratorCest
 //        $I->addPriceLessDiscount($this->randomProductName, $this->randomPriceLess);
 //        $I->see("Product Management", '.page-title');
 //    }
-
-
 }
