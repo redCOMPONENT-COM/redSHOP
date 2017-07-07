116d493edaddae8f584fb1672658de372a4ff97a
nhung@redweb.dk <nhung@redweb.dk>
2017-07-07 17:19:42 +0700

    change code inside page

--- tests/acceptance/administrator/_steps/Joomla3Steps/PriceProductManagerJoomla3Steps.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/PriceProductManagerJoomla3Steps.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: nhung nguyen
- * Date: 5/29/2017
- * Time: 2:03 PM
+ * Class PriceProductManagerJoomla3Steps
+ *
+ * @package  AcceptanceTester
+ *
+ * @since    1.4
  */
 
 namespace AcceptanceTester;
@@ -21,39 +22,37 @@ class PriceProductManagerJoomla3Steps extends AdminManagerJoomla3Steps
         $I->searchProduct($productName);
         $I->wait(5);
         $I->fillField(\PriceProductJoomla3Page::$discount, $priceDiscount);
-        $I->click(['xpath' => "//a[contains(@href,'savediscountprice')]"]);
+        $I->click(\PriceProductJoomla3Page::$saveButton);
     }
 
+//    public function addDiscountPriceMoreThan($productName, $priceDiscountThan)
+//    {
+//        $I = $this;
+//        $I->amOnPage(\PriceProductJoomla3Page::$URL);
+//        $I->searchProduct($productName);
+//        $I->wait(3);
+//        $I->fillField(\PriceProductJoomla3Page::$discount, $priceDiscountThan);
+//        $I->click(['xpath' => "//a[contains(@href,'savediscountprice')]"]);
+//
+//    }
+//
+//    public function addPriceLessDiscount($productName, $randomPriceLess)
+//    {
+//        $I = $this;
+//        $I->amOnPage(\PriceProductJoomla3Page::$URL);
+//        $I->searchProduct($productName);
+//        $I->wait(3);
+//        $I->fillField(\PriceProductJoomla3Page::$priceProduct, $randomPriceLess);
+//        $I->click(['xpath' => "//a[contains(@href,'saveprice')]"]);
+//
+//    }
 
-    public function addDiscountPriceMoreThan($productName, $priceDiscountThan)
-    {
-        $I = $this;
-        $I->amOnPage(\PriceProductJoomla3Page::$URL);
-        $I->searchProduct($productName);
-        $I->wait(3);
-        $I->fillField(\PriceProductJoomla3Page::$discount, $priceDiscountThan);
-        $I->click(['xpath' => "//a[contains(@href,'savediscountprice')]"]);
-
-    }
-
-    public function addPriceLessDiscount($productName, $randomPriceLess)
-    {
-        $I = $this;
-        $I->amOnPage(\PriceProductJoomla3Page::$URL);
-        $I->searchProduct($productName);
-        $I->wait(3);
-        $I->fillField(\PriceProductJoomla3Page::$priceProduct, $randomPriceLess);
-        $I->click(['xpath' => "//a[contains(@href,'saveprice')]"]);
-
-    }
     public function searchProduct($productName)
     {
         $I = $this;
         $I->wantTo('Search the Product');
         $I->amOnPage(\PriceProductJoomla3Page::$URL);
-        $I->waitForText('Product Management', 30, ['xpath' => "//h1"]);
+        $I->see(\PriceProductJoomla3Page::$namePage, \PriceProductJoomla3Page::$selectorPage);
         $I->filterListPriceProductSearch($productName);
     }
-
-
 }
