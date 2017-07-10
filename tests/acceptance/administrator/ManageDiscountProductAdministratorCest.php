cf3264f9cb88ed69c28bfdee0c9f04d44e014829
nhung@redweb.dk <nhung@redweb.dk>
2017-07-10 17:54:54 +0700

    change brower

--- tests/acceptance/administrator/ManageDiscountProductAdministratorCest.php
+++ tests/acceptance/administrator/ManageDiscountProductAdministratorCest.php
@@ -10,200 +10,202 @@ class ManageDiscountProductAdministratorCest
         $this->faker = Faker\Factory::create();
         $this->categoryName = 'Events and Forms';
         $this->noPage = $this->faker->randomNumber();
-        $this->productPrice = 120;
+        $this->productPrice = $this->faker->numberBetween(100,999);
         $this->condition = "Higher";
         $this->type = "Percentage";
         $this->startDate = "19-06-2017";
         $this->endDate = "23-06-2017";
         $this->GroupName = "Product Name";
-        $this->discountAmount = 12;
+        $this->discountAmount = $this->faker->numberBetween(1,10);
+//        $this->fakeDat=$this->faker->dateTimeBetween();
         $this->groupName = "Default Private";
     }
 
-//    /**
-//     *
-//     * Function create discount on product with save button
-//     *
-//     * @param AcceptanceTester $I
-//     * @param $scenario
-//     */
-//    public function createCategorySave(AcceptanceTester $I, $scenario)
-//    {
-//        $I->wantTo('Test Category Save creation in Administrator');
-//        $I->doAdministratorLogin();
-//        $I = new AcceptanceTester\DiscountProductJoomla3Steps($scenario);
-//        $I->wantTo('Create a Category Save button');
-//        $I->addDiscountProductSave($this->productPrice, $this->condition, $this->type, $this->discountAmount, $this->startDate, $this->endDate, $this->categoryName, $this->groupName);
-//        $I->see("Discount Detail Saved", '.alert-success');
-//    }
-//
-//    /**
-//     *
-//     * Function create discount on product with save and close button
-//     *
-//     * @param AcceptanceTester $I
-//     * @param $scenario
-//     *
-//     */
-//    public function addDiscountProductSaveClose(AcceptanceTester $I, $scenario)
-//    {
-//        $I->wantTo('Test Discount Product  Save Close creation in Administrator');
-//        $I->doAdministratorLogin();
-//        $I = new AcceptanceTester\DiscountProductJoomla3Steps($scenario);
-//        $I->wantTo('Create a Category Save button');
-//        $I->addDiscountProductSaveClose($this->productPrice, $this->condition, $this->type, $this->discountAmount, $this->startDate, $this->endDate, $this->categoryName, $this->groupName);
-//        $I->see("Product Discount Management", '.page-title');
-//    }
-//
-//    /**
-//     *
-//     * Function check cancel button
-//     *
-//     * @param AcceptanceTester $I
-//     * @param $scenario
-//     *
-//     */
-//    public function addDiscountProductCancelButton(AcceptanceTester $I, $scenario)
-//    {
-//        $I->wantTo('Test Check Cancel button in Administrator');
-//        $I->doAdministratorLogin();
-//        $I = new AcceptanceTester\DiscountProductJoomla3Steps($scenario);
-//        $I->wantTo('Create a Category Save button');
-//        $I->addDiscountProductCancelButton();
-//        $I->see("Product Discount Management", '.page-title');
-//    }
-//
-//    /**
-//     *
-//     * Function add discount product missing amount
-//     *
-//     * @param AcceptanceTester $I
-//     * @param $scenario
-//     *
-//     */
-//    public function addDiscountProductMissingAmountSaveClose(AcceptanceTester $I, $scenario)
-//    {
-//        $I->wantTo('Test add discount missing amount in Administrator');
-//        $I->doAdministratorLogin();
-//        $I = new AcceptanceTester\DiscountProductJoomla3Steps($scenario);
-//        $I->wantTo('Create a Category Save button');
-//        $I->addDiscountProductMissingAmountSaveClose($this->productPrice, $this->condition, $this->type, $this->startDate, $this->endDate, $this->categoryName, $this->groupName);
-//        $I->see("Discount", '.page-title');
-//    }
-//
-//    /**
-//     *
-//     * Function add discount missing shopper groups
-//     *
-//     * @param AcceptanceTester $I
-//     * @param $scenario
-//     *
-//     */
-//    public function addDiscountProductMissingShopperGroupSaveClose(AcceptanceTester $I, $scenario)
-//    {
-//        $I->wantTo('Test add discount missing shopper groups in Administrator');
-//        $I->doAdministratorLogin();
-//        $I = new AcceptanceTester\DiscountProductJoomla3Steps($scenario);
-//        $I->wantTo('Create a Category Save button');
-//        $I->addDiscountProductMissingShopperGroupSaveClose($this->productPrice, $this->condition, $this->type, $this->discountAmount, $this->startDate, $this->endDate, $this->categoryName);
-//        $I->see("Discount", '.page-title');
-//    }
-//
-//    /**
-//     * Function add discount product have start day more than end day
-//     *
-//     * @param AcceptanceTester $I
-//     * @param $scenario
-//     *
-//     */
-//    public function addDiscountProductStartMoreThanEnd(AcceptanceTester $I, $scenario)
-//    {
-//        $I->wantTo('Test add discount product start more than end  in Administrator');
-//        $I->doAdministratorLogin();
-//        $I = new AcceptanceTester\DiscountProductJoomla3Steps($scenario);
-//        $I->wantTo('Create a Category Save button');
-//        $I->addDiscountProductStartMoreThanEnd($this->productPrice, $this->condition, $this->type, $this->discountAmount, $this->startDate, $this->endDate, $this->categoryName, $this->groupName);
-//        $I->see("Discount", '.page-title');
-//    }
-//
-//    /**
-//     *
-//     * Function check edit button
-//     *
-//     * @param AcceptanceTester $I
-//     * @param $scenario
-//     */
-//    public function checkEditButton(AcceptanceTester $I, $scenario)
-//    {
-//        $I->wantTo('check Edit button  in Administrator');
-//        $I->doAdministratorLogin();
-//        $I = new AcceptanceTester\DiscountProductJoomla3Steps($scenario);
-//        $I->wantTo('Create a Category Save button');
-//        $I->checkEditButton();
-//        $I->see("Product Discount Management", '.page-title');
-//    }
-//
-//    /**
-//     *
-//     * Function check delete button
-//     *
-//     * @param AcceptanceTester $I
-//     * @param $scenario
-//     */
-//    public function checkDeleteButton(AcceptanceTester $I, $scenario)
-//    {
-//        $I->wantTo('heck Delete button   in Administrator');
-//        $I->doAdministratorLogin();
-//        $I = new AcceptanceTester\DiscountProductJoomla3Steps($scenario);
-//        $I->wantTo('Create a Category Save button');
-//        $I->checkDeleteButton();
-//        $I->see("Product Discount Management", '.page-title');
-//    }
-//
-//    /**
-//     *
-//     * Function check publish button
-//     *
-//     * @param AcceptanceTester $I
-//     * @param $scenario
-//     */
-//    public function checkPublishButton(AcceptanceTester $I, $scenario)
-//    {
-//        $I->wantTo('check Publish button  in Administrator');
-//        $I->doAdministratorLogin();
-//        $I = new AcceptanceTester\DiscountProductJoomla3Steps($scenario);
-//        $I->wantTo('Create a Category Save button');
-//        $I->checkPublishButton();
-//        $I->see("Product Discount Management", '.page-title');
-//    }
-//
-//    /**
-//     *
-//     * Function check unpublish button
-//     *
-//     * @param AcceptanceTester $I
-//     * @param $scenario
-//     *
-//     */
-//    public function checkUnpublishButton(AcceptanceTester $I, $scenario)
-//    {
-//        $I->wantTo('check Unpublish button  in Administrator');
-//        $I->doAdministratorLogin();
-//        $I = new AcceptanceTester\DiscountProductJoomla3Steps($scenario);
-//        $I->wantTo('Create a Category Save button');
-//        $I->checkUnpublishButton();
-//        $I->see("Product Discount Management", '.page-title');
-//    }
-//
-//    /**
-//     * Function check unpublish all discount
-//     *
-//     * @param AcceptanceTester $I
-//     * @param $scenario
-//     *
-//     * @depends createCategorySave
-//     *
-//     */
+    /**
+     *
+     * Function create discount on product with save button
+     *
+     * @param AcceptanceTester $I
+     * @param $scenario
+     */
+    public function createDiscountSave(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test Category Save creation in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\DiscountProductJoomla3Steps($scenario);
+        $I->wantTo('Create a Category Save button');
+
+        $I->addDiscountProductSave($this->productPrice, $this->condition, $this->type, $this->discountAmount, $this->startDate, $this->endDate, $this->categoryName, $this->groupName);
+        $I->see(\DiscountProductJ3Page::$messageSaveDiscountSuccess, \DiscountProductJ3Page::$selectorSuccess);
+    }
+
+    /**
+     *
+     * Function create discount on product with save and close button
+     *
+     * @param AcceptanceTester $I
+     * @param $scenario
+     *
+     */
+    public function addDiscountProductSaveClose(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test Discount Product  Save Close creation in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\DiscountProductJoomla3Steps($scenario);
+        $I->wantTo('Create a Category Save button');
+        $I->addDiscountProductSaveClose($this->productPrice, $this->condition, $this->type, $this->discountAmount, $this->startDate, $this->endDate, $this->categoryName, $this->groupName);
+        $I->see(\DiscountProductJ3Page::$namePage, \DiscountProductJ3Page::$pageTitle);
+    }
+
+    /**
+     *
+     * Function check cancel button
+     *
+     * @param AcceptanceTester $I
+     * @param $scenario
+     *
+     */
+    public function addDiscountProductCancelButton(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test Check Cancel button in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\DiscountProductJoomla3Steps($scenario);
+        $I->wantTo('Create a Category Save button');
+        $I->addDiscountProductCancelButton();
+        $I->see(\DiscountProductJ3Page::$namePage, \DiscountProductJ3Page::$pageTitle);
+    }
+
+    /**
+     *
+     * Function add discount product missing amount
+     *
+     * @param AcceptanceTester $I
+     * @param $scenario
+     *
+     */
+    public function addDiscountProductMissingAmountSaveClose(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test add discount missing amount in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\DiscountProductJoomla3Steps($scenario);
+        $I->wantTo('Create a Category Save button');
+        $I->addDiscountProductMissingAmountSaveClose($this->productPrice, $this->condition, $this->type, $this->startDate, $this->endDate, $this->categoryName, $this->groupName);
+        $I->see(\DiscountProductJ3Page::$namePageDetail, \DiscountProductJ3Page::$pageTitle);
+    }
+
+    /**
+     *
+     * Function add discount missing shopper groups
+     *
+     * @param AcceptanceTester $I
+     * @param $scenario
+     *
+     */
+    public function addDiscountProductMissingShopperGroupSaveClose(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test add discount missing shopper groups in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\DiscountProductJoomla3Steps($scenario);
+        $I->wantTo('Create a Category Save button');
+        $I->addDiscountProductMissingShopperGroupSaveClose($this->productPrice, $this->condition, $this->type, $this->discountAmount, $this->startDate, $this->endDate, $this->categoryName);
+        $I->see(\DiscountProductJ3Page::$namePageDetail, \DiscountProductJ3Page::$pageTitle);
+    }
+
+    /**
+     * Function add discount product have start day more than end day
+     *
+     * @param AcceptanceTester $I
+     * @param $scenario
+     *
+     */
+    public function addDiscountProductStartMoreThanEnd(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test add discount product start more than end  in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\DiscountProductJoomla3Steps($scenario);
+        $I->wantTo('Create a Category Save button');
+        $I->addDiscountProductStartMoreThanEnd($this->productPrice, $this->condition, $this->type, $this->discountAmount, $this->startDate, $this->endDate, $this->categoryName, $this->groupName);
+        $I->see(\DiscountProductJ3Page::$namePageDetail, \DiscountProductJ3Page::$pageTitle);
+    }
+
+    /**
+     *
+     * Function check edit button
+     *
+     * @param AcceptanceTester $I
+     * @param $scenario
+     */
+    public function checkEditButton(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('check Edit button  in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\DiscountProductJoomla3Steps($scenario);
+        $I->wantTo('Create a Category Save button');
+        $I->checkEditButton();
+        $I->see(\DiscountProductJ3Page::$namePage, \DiscountProductJ3Page::$pageTitle);
+    }
+
+    /**
+     *
+     * Function check delete button
+     *
+     * @param AcceptanceTester $I
+     * @param $scenario
+     */
+    public function checkDeleteButton(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('heck Delete button   in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\DiscountProductJoomla3Steps($scenario);
+        $I->wantTo('Create a Category Save button');
+        $I->checkDeleteButton();
+        $I->see(\DiscountProductJ3Page::$namePage, \DiscountProductJ3Page::$pageTitle);
+    }
+
+    /**
+     *
+     * Function check publish button
+     *
+     * @param AcceptanceTester $I
+     * @param $scenario
+     */
+    public function checkPublishButton(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('check Publish button  in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\DiscountProductJoomla3Steps($scenario);
+        $I->wantTo('Create a Category Save button');
+        $I->checkPublishButton();
+        $I->see(\DiscountProductJ3Page::$namePage, \DiscountProductJ3Page::$pageTitle);
+    }
+
+    /**
+     *
+     * Function check unpublish button
+     *
+     * @param AcceptanceTester $I
+     * @param $scenario
+     *
+     */
+    public function checkUnpublishButton(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('check Unpublish button  in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\DiscountProductJoomla3Steps($scenario);
+        $I->wantTo('Create a Category Save button');
+        $I->checkUnpublishButton();
+        $I->see(\DiscountProductJ3Page::$namePage, \DiscountProductJ3Page::$pageTitle);
+    }
+
+    /**
+     * Function check unpublish all discount
+     *
+     * @param AcceptanceTester $I
+     * @param $scenario
+     *
+     * @depends createDiscountSave
+     *
+     */
     public function checkUnpublishAll(AcceptanceTester $I, $scenario)
     {
         $I->wantTo('Unpublish all discount Administrator');
@@ -211,7 +213,7 @@ class ManageDiscountProductAdministratorCest
         $I = new AcceptanceTester\DiscountProductJoomla3Steps($scenario);
         $I->wantTo('Create a Category Save button');
         $I->checkUnpublishAll();
-        $I->see("Product Discount Management", '.page-title');
+        $I->see(\DiscountProductJ3Page::$namePage, \DiscountProductJ3Page::$pageTitle);
     }
 
     /**
@@ -230,7 +232,7 @@ class ManageDiscountProductAdministratorCest
         $I = new AcceptanceTester\DiscountProductJoomla3Steps($scenario);
         $I->wantTo('Create a Category Save button');
         $I->checkPublishAll();
-        $I->see("Product Discount Management", '.page-title');
+        $I->see(\DiscountProductJ3Page::$namePage, \DiscountProductJ3Page::$pageTitle);
     }
 
     /**
@@ -250,7 +252,7 @@ class ManageDiscountProductAdministratorCest
         $I = new AcceptanceTester\DiscountProductJoomla3Steps($scenario);
         $I->wantTo('Create a Category Save button');
         $I->checkDeleteAll();
-        $I->see("Product Discount Management", '.page-title');
+        $I->see(\DiscountProductJ3Page::$namePage, \DiscountProductJ3Page::$pageTitle);
     }
 
 }
