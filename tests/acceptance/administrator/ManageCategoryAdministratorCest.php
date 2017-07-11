892e25ed54417786ba51abd778ad2ca377760b1a
puneet0191 <puneet.0191@gmail.com>
2017-07-11 20:54:02 +0530

    Revert the test commit

--- tests/acceptance/administrator/ManageCategoryAdministratorCest.php
+++ tests/acceptance/administrator/ManageCategoryAdministratorCest.php
@@ -69,7 +69,7 @@ class ManageCategoryAdministratorCest
 		$I->wantTo('Update Existing Category');
 		$I->changeCategoryState($this->newCategoryName, 'unpublish');
 		$currentState = $I->getCategoryState($this->newCategoryName);
-		$I->verifyState('published', $currentState);
+		$I->verifyState('unpublished', $currentState);
 	}
 
 	/**
