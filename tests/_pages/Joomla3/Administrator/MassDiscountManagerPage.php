ac543b3258c0ff1cc65b8eda5a28cd2434a429a0
nhung@redweb.dk <nhung@redweb.dk>
2017-07-11 12:00:55 +0700

    change style codeception

--- tests/_pages/Joomla3/Administrator/MassDiscountManagerPage.php
+++ tests/_pages/Joomla3/Administrator/MassDiscountManagerPage.php
@@ -1,25 +1,33 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: nhung nguyen
- * Date: 6/8/2017
- * Time: 1:59 PM
- */
+
 class MassDiscountManagerPage
 {
     public static $URL = '/administrator/index.php?option=com_redshop&view=mass_discounts';
-    public static $name = "#jform_name";
-    public static $valueAmount = "#jform_amount";
-    public static $pathNameProduct = "#s2id_autogen3";
-    public static $categoryField = "#s2id_jform_category_id";
-    public static $dayStart = "#jform_start_date";
-    public static $dayEnd = "#jform_end_date";
-    public static $checkFirstItems = "//input[@id='cb0']";
-    public static $MassDiscountFilter = "#filter_search";
-    public static $choiceAll="//input[@onclick='Joomla.checkAll(this)']";
 
-public static $MassDicountResultRow="//div[@class='table-responsive']/table/tbody/tr/td[3]/a";
+    public static $name = "#jform_name";
+
+    public static $valueAmount = "#jform_amount";
+
+    public static $pathNameProduct = "#s2id_autogen3";
+
+    public static $categoryField = "#s2id_jform_category_id";
+
+    public static $dayStart = "#jform_start_date";
+
+    public static $dayEnd = "#jform_end_date";
+
+    public static $checkFirstItems = "//input[@id='cb0']";
+
+    public static $MassDiscountFilter = "#filter_search";
+
+    public static $MassDicountResultRow="//div[@class='table-responsive']/table/tbody/tr/td[3]/a";
+
+    //page name
+
+    public static $pageEdit="Mass Discount  Edit";
+
+    public static $pageNew="product mass discount new";
 
 
 }
