49b7843b8c43e4c4cd3700877e6fc37cf0478fd1
thongredweb <thong@redweb.dk>
2017-07-12 12:09:13 +0700

    [Scrutinizer] RsUserHelper::createJoomlaUser()

--- component/site/helpers/rsuserhelper.php
+++ component/site/helpers/rsuserhelper.php
@@ -299,156 +299,19 @@ class RsUserHelper
 		return $user;
 	}
 
-	public function createJoomlaUser($data, $createuser = 0)
+	/**
+	 * Method for create Joomla user.
+	 *
+	 * @param   array  $data        User data.
+	 * @param   int    $createUser  Create user
+	 *
+	 * @return  boolean|JUser       JUser if success. False otherwise.
+	 *
+	 * @deprecated    __DEPLOY_VERSION__
+	 */
+	public function createJoomlaUser($data, $createUser = 0)
 	{
-		$app = JFactory::getApplication();
-		$input = $app->input;
-		$createaccount = (isset($data['createaccount']) && $data['createaccount'] == 1) ? 1 : 0;
-
-		// Registration is without account creation REGISTER_METHOD = 1
-		// Or Optional account creation
-		if (Redshop::getConfig()->get('REGISTER_METHOD') == 1 || (Redshop::getConfig()->get('REGISTER_METHOD') == 2 && $createaccount == 0))
-		{
-			$user     = new stdClass;
-			$user->id = 0;
-
-			return $user;
-		}
-
-		$data['password']  = $input->post->getRaw('password1', '');
-		$data['password2'] = $input->post->getRaw('password2', '');
-		$data['email']     = $data['email1'];
-		$data['name']      = $name = $data['firstname'];
-
-		// Prevent front-end user to change user group in the form and then being able to register on any Joomla! user group.
-		if ($app->isSite())
-		{
-			$params         = JComponentHelper::getParams('com_users');
-			$data['groups'] = array($params->get('new_usertype', 2));
-		}
-
-		// Do a password safety check
-		if (Redshop::getConfig()->get('REGISTER_METHOD') == 3)
-		{
-			// Silent registration
-			$better_token      = substr(uniqid(md5(rand()), true), 0, 10);
-			$data['username']  = $data['email'];
-			$data['password']  = $better_token;
-			$data['password2'] = $better_token;
-			$input->post->set('password1', $better_token);
-		}
-
-		if (trim($data['email']) == "")
-		{
-			JError::raiseWarning('', JText::_('COM_REDSHOP_EMPTY_EMAIL'));
-
-			return false;
-		}
-
-		if (trim($data['username']) == "")
-		{
-			JError::raiseWarning('', JText::_('COM_REDSHOP_EMPTY_USERNAME'));
-
-			return false;
-		}
-
-		$countusername = $this->validate_user($data['username']);
-
-		if ($countusername > 0)
-		{
-			JError::raiseWarning('', JText::_('COM_REDSHOP_USERNAME_ALREADY_EXISTS'));
-
-			return false;
-		}
-
-		$countemail = $this->validate_email($data['email']);
-
-		if ($countemail > 0)
-		{
-			JError::raiseWarning('', JText::_('COM_REDSHOP_EMAIL_ALREADY_EXISTS'));
-
-			return false;
-		}
-
-		if (trim($data['password']) == "")
-		{
-			JError::raiseWarning('', JText::_('COM_REDSHOP_EMPTY_PASSWORD'));
-
-			return false;
-		}
-
-		if (strlen($data['password']) || strlen($data['password2']))
-		{
-			if ($data['password'] != $data['password2'])
-			{
-				JError::raiseWarning('', JText::_('COM_REDSHOP_PASSWORDS_DO_NOT_MATCH'));
-
-				return false;
-			}
-		}
-
-		JPluginHelper::importPlugin('redshop_user');
-		RedshopHelperUtility::getDispatcher()->trigger('onBeforeCreateJoomlaUser', array(&$data));
-
-		// Get required system objects
-		$user = clone JFactory::getUser();
-
-		// If user registration is not allowed, show 403 not authorized.
-		$usersConfig = JComponentHelper::getParams('com_users');
-
-		if (!$user->bind($data))
-		{
-			JError::raiseError(500, $user->getError());
-
-			return false;
-		}
-
-		$date = JFactory::getDate();
-		$user->set('id', 0);
-		$user->set('registerDate', $date->toSql());
-
-		// If user activation is turned on, we need to set the activation information
-		$useractivation = $usersConfig->get('useractivation');
-
-		if ($useractivation == '1')
-		{
-			if (version_compare(JVERSION, '3.0', '<'))
-			{
-				$hash = JApplication::getHash(JUserHelper::genRandomPassword());
-			}
-			else
-			{
-				$hash = JApplicationHelper::getHash(JUserHelper::genRandomPassword());
-			}
-
-			$user->set('activation', $hash);
-			$user->set('block', '0');
-		}
-
-		$user->set('name', $name);
-		$user->name = $name;
-
-		// If there was an error with registration, set the message and display form
-		if (!$user->save())
-		{
-			JError::raiseWarning('', JText::_($user->getError()));
-
-			return false;
-		}
-
-		$credentials             = array();
-		$credentials['username'] = $data['username'];
-		$credentials['password'] = $data['password2'];
-
-		// Perform the login action
-		if (!JFactory::getUser()->id)
-		{
-			$app->login($credentials);
-		}
-
-		RedshopHelperUtility::getDispatcher()->trigger('onAfterCreateJoomlaUser', array(&$user));
-
-		return $user;
+		return RedshopHelperJoomla::createJoomlaUser($data, (boolean) $createUser);
 	}
 
 	/**
--- libraries/redshop/helper/joomla.php
+++ libraries/redshop/helper/joomla.php
@@ -19,8 +19,8 @@ class RedshopHelperJoomla
 	/**
 	 * Get redSHOP manifest value
 	 *
-	 * @param   string  $name     Name param
-	 * @param   mixed   $default  Default return value if value is not exists
+	 * @param   string $name    Name param
+	 * @param   mixed  $default Default return value if value is not exists
 	 *
 	 * @return  mixed
 	 */
@@ -30,8 +30,8 @@ class RedshopHelperJoomla
 
 		if (!isset($oldManifest))
 		{
-			$db = JFactory::getDbo();
-			$query = $db->getQuery(true)
+			$db          = JFactory::getDbo();
+			$query       = $db->getQuery(true)
 				->select('manifest_cache')
 				->from($db->qn('#__extensions'))
 				->where('type = ' . $db->q('component'))
@@ -47,5 +47,155 @@ class RedshopHelperJoomla
 		{
 			return $default;
 		}
+	}
+
+	/**
+	 * Method for create Joomla user.
+	 *
+	 * @param   array   $data       User data.
+	 * @param   boolean $createUser Create user
+	 *
+	 * @return  boolean|JUser|stdClass       JUser if success. False otherwise.
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public static function createJoomlaUser($data, $createUser = false)
+	{
+		$app   = JFactory::getApplication();
+		$input = $app->input;
+
+		$createUser = isset($data['createaccount']) ? (boolean) $data['createaccount'] : $createUser;
+
+		// Registration is without account creation REGISTER_METHOD = 1
+		// Or Optional account creation
+		if (Redshop::getConfig()->get('REGISTER_METHOD') == 1 || (Redshop::getConfig()->get('REGISTER_METHOD') == 2 && !$createUser))
+		{
+			$user     = new stdClass;
+			$user->id = 0;
+
+			return $user;
+		}
+
+		$data['password']  = $input->post->get('password1', '', 'RAW');
+		$data['password2'] = $input->post->get('password2', '', 'RAW');
+		$data['email']     = $data['email1'];
+		$data['name']      = $name = $data['firstname'];
+
+		$userParams = JComponentHelper::getParams('com_users');
+
+		// Prevent front-end user to change user group in the form and then being able to register on any Joomla! user group.
+		if ($app->isSite())
+		{
+			$data['groups'] = array($userParams->get('new_usertype', 2));
+		}
+
+		// Do a password safety check
+		if (Redshop::getConfig()->get('REGISTER_METHOD') == 3)
+		{
+			// Silent registration
+			$betterToken = substr(uniqid(md5(rand()), true), 0, 10);
+
+			$data['username']  = $data['email'];
+			$data['password']  = $betterToken;
+			$data['password2'] = $betterToken;
+
+			$input->post->set('password1', $betterToken);
+		}
+
+		if (trim($data['email']) == "")
+		{
+			JError::raiseWarning('', JText::_('COM_REDSHOP_EMPTY_EMAIL'));
+
+			return false;
+		}
+
+		if (trim($data['username']) == "")
+		{
+			JError::raiseWarning('', JText::_('COM_REDSHOP_EMPTY_USERNAME'));
+
+			return false;
+		}
+
+		if (RedshopHelperUser::validateUser($data['username']) > 0)
+		{
+			JError::raiseWarning('', JText::_('COM_REDSHOP_USERNAME_ALREADY_EXISTS'));
+
+			return false;
+		}
+
+		if (RedshopHelperUser::validateEmail($data['email']) > 0)
+		{
+			JError::raiseWarning('', JText::_('COM_REDSHOP_EMAIL_ALREADY_EXISTS'));
+
+			return false;
+		}
+
+		// Check: Password is empty
+		if (trim($data['password']) == "")
+		{
+			JError::raiseWarning('', JText::_('COM_REDSHOP_EMPTY_PASSWORD'));
+
+			return false;
+		}
+
+		// Check: Password not match
+		if ($data['password'] != $data['password2'])
+		{
+			JError::raiseWarning('', JText::_('COM_REDSHOP_PASSWORDS_DO_NOT_MATCH'));
+
+			return false;
+		}
+
+		JPluginHelper::importPlugin('redshop_user');
+		RedshopHelperUtility::getDispatcher()->trigger('onBeforeCreateJoomlaUser', array(&$data));
+
+		// Get required system objects
+		$user = clone JFactory::getUser();
+
+		// If user registration is not allowed, show 403 not authorized.
+		if (!$user->bind($data))
+		{
+			JError::raiseError(500, $user->getError());
+
+			return false;
+		}
+
+		$date = JFactory::getDate();
+		$user->set('id', 0);
+		$user->set('registerDate', $date->toSql());
+
+		// If user activation is turned on, we need to set the activation information
+		$activationMethod = $userParams->get('useractivation');
+
+		if ($activationMethod == '1')
+		{
+			$user->set('activation', JApplication::getHash(JUserHelper::genRandomPassword()));
+			$user->set('block', '0');
+		}
+
+		$user->set('name', $name);
+		$user->name = $name;
+
+		// If there was an error with registration, set the message and display form
+		if (!$user->save())
+		{
+			JError::raiseWarning('', JText::_($user->getError()));
+
+			return false;
+		}
+
+		$credentials             = array();
+		$credentials['username'] = $data['username'];
+		$credentials['password'] = $data['password2'];
+
+		// Perform the login action
+		if (!JFactory::getUser()->id)
+		{
+			$app->login($credentials);
+		}
+
+		RedshopHelperUtility::getDispatcher()->trigger('onAfterCreateJoomlaUser', array(&$user));
+
+		return $user;
 	}
 }
