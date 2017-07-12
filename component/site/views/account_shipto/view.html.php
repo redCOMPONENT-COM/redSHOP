1ddfcb0ee107391f52f1d0c35cceb586c41b1caf
thongredweb <thong@redweb.dk>
2017-07-12 10:48:14 +0700

    [Fix] Fatal error: Account shipto error

--- component/site/views/account_shipto/view.html.php
+++ component/site/views/account_shipto/view.html.php
@@ -116,8 +116,8 @@ class RedshopViewAccount_Shipto extends RedshopView
 		}
 
 		$this->lists             = $lists;
-		$this->shippingAddresses = $shippingAddresses;
-		$this->billingAddresses  = $billingAddress;
+		$this->shippingAddresses = !is_array($shippingAddresses) ? array($shippingAddresses) : $shippingAddresses;
+		$this->billingAddresses  = !is_array($billingAddress) ? array($billingAddress) : $billingAddress;
 		$this->request_url       = JUri::getInstance()->toString();
 		JFilterOutput::cleanText($this->request_url);
 		$this->params = $app->getMenu()->getActive()->getParams();
