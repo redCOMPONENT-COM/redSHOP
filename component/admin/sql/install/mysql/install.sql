78f9b17efe4f34ddac129f999a676212c579b2b3
thongredweb <thong@redweb.dk>
2017-07-14 16:58:10 +0700

    [Refactor] Vouchers system

--- component/admin/sql/install/mysql/install.sql
+++ component/admin/sql/install/mysql/install.sql
@@ -1,6 +1,7 @@
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 
+
 -- -----------------------------------------------------
 -- Table `#__redshop_attribute_set`
 -- -----------------------------------------------------
