e9397951b7a047d342aab36ec40d30d1863e6664
Nhung Nguyen <nhung@redweb.dk>
2017-07-18 15:30:03 +0700

    Remove tests file

--- tests/_groups/InstallationJ3Group.php
+++ tests/_groups/InstallationJ3Group.php
@@ -17,21 +17,21 @@ class InstallationJ3Group extends \Codeception\Platform\Group
     public static $group = 'installationJ3';
 
     /**
-	 * Function to delete the Configuration File before Doing the Installation
-	 *
-	 * @param   TestEvent  $e  Name of the Test Event
-	 *
-	 * @return void
-	 */
-	public function _before(TestEvent $e)
-	{
-		// Remove Joomla 3 CMS old configuration.php file before do a clean joomla installation
-		$joomla3ConfigurationFile = 'tests/system/joomla-cms3/configuration.php';
-//tests/joomla-cms3/con
-		if (file_exists($joomla3ConfigurationFile))
-		{
-			chmod($joomla3ConfigurationFile, 0777);
-			unlink($joomla3ConfigurationFile);
-		}
-	}
+     * Function to delete the Configuration File before Doing the Installation
+     *
+     * @param   TestEvent  $e  Name of the Test Event
+     *
+     * @return void
+     */
+    public function _before(TestEvent $e)
+    {
+        // Remove Joomla 3 CMS old configuration.php file before do a clean joomla installation
+        $joomla3ConfigurationFile = 'tests/system/joomla-cms3/configuration.php';
+
+        if (file_exists($joomla3ConfigurationFile))
+        {
+            chmod($joomla3ConfigurationFile, 0777);
+            unlink($joomla3ConfigurationFile);
+        }
+    }
 }
