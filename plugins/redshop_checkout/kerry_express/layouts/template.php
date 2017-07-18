10d63ee32609bb73f8f1ab2a28e1aeb0353d5b08
Tu Vuong <tu@redweb.dk>
2017-07-18 10:16:53 +0700

    update billing

--- plugins/redshop_checkout/kerry_express/layouts/template.php
+++ plugins/redshop_checkout/kerry_express/layouts/template.php
@@ -7,23 +7,12 @@
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
 defined('_JEXEC') or die;
-JHTML::_('behavior.tooltip');
 extract($displayData);
 ?>
 <script type="text/javascript">
-	jQuery(document).ready(function(){
-		var infoId = <?php echo $id; ?>;
-		jQuery.ajax({
-	        type: "POST",
-	        url: "<?php echo JUri::root() . 'index.php?option=com_ajax&plugin=GetKerryCity&group=redshop_checkout&format=raw'; ?>",
-	        data: {id: infoId},
-	        success: function(data) {
-	        	jQuery('select#rs_kerry_city').append(data);
-	        	jQuery("select#rs_kerry_city").trigger("change");
-	        }
-	    });
-
-	    jQuery('select#rs_kerry_city').on('change', function(){
+	function getShippingDistrict(infoId)
+	{
+		jQuery('select#rs_kerry_city').on('change', function(){
 	    	var id = jQuery(this).val();
 	    	jQuery.ajax({
 		        type: "POST",
@@ -33,12 +22,15 @@ extract($displayData);
 		        	jQuery('select#rs_kerry_district').html('');
 		        	jQuery('#s2id_rs_kerry_district .select2-chosen').html('Select District');
 		        	jQuery('select#rs_kerry_district').append(data);
-		        	jQuery("select#rs_kerry_district").trigger("change");
+		        	jQuery('select#rs_kerry_district').trigger("change");
 		        }
 		    });
-	    })
+	    });
+	}
 
-	    jQuery('select#rs_kerry_district').on('change', function(){
+	function getShippingWard(infoId)
+	{
+		jQuery('select#rs_kerry_district').on('change', function(){
 	    	var id = jQuery(this).val();
 	    	jQuery.ajax({
 		        type: "POST",
@@ -48,9 +40,63 @@ extract($displayData);
 		        	jQuery('select#rs_kerry_ward').html('');
 		        	jQuery('#s2id_rs_kerry_ward .select2-chosen').html('Select Ward');
 		        	jQuery('select#rs_kerry_ward').append(data);
-		        	jQuery("select#rs_kerry_ward").trigger("change");
+		        	jQuery('select#rs_kerry_ward').trigger("change");
 		        }
 		    });
-	    })
+	    });
+	}
+
+	function getBillingDistrict(infoId)
+	{
+		jQuery('select#rs_kerry_billing_city').on('change', function(){
+	    	var id = jQuery(this).val();
+	    	jQuery.ajax({
+		        type: "POST",
+		        data: {city: id, id: infoId},
+		        url: "<?php echo JUri::root() . 'index.php?option=com_ajax&plugin=GetKerryDistrict&group=redshop_checkout&format=raw'; ?>",
+		        success: function(data) {
+		        	jQuery('select#rs_kerry_billing_district').html('');
+		        	jQuery('#s2id_rs_kerry_billing_district .select2-chosen').html('Select District');
+		        	jQuery('select#rs_kerry_billing_district').append(data);
+		        	jQuery('select#rs_kerry_billing_district').trigger("change");
+		        }
+		    });
+	    });
+	}
+
+	function getBillingWard(infoId)
+	{
+		jQuery('select#rs_kerry_billing_district').on('change', function(){
+	    	var id = jQuery(this).val();
+	    	jQuery.ajax({
+		        type: "POST",
+		        data: {district: id, id: infoId},
+		        url: "<?php echo JUri::root() . 'index.php?option=com_ajax&plugin=GetKerryWard&group=redshop_checkout&format=raw'; ?>",
+		        success: function(data) {
+		        	jQuery('select#rs_kerry_billing_ward').html('');
+		        	jQuery('#s2id_rs_kerry_billing_ward .select2-chosen').html('Select Ward');
+		        	jQuery('select#rs_kerry_billing_ward').append(data);
+		        	jQuery('select#rs_kerry_billing_ward').trigger("change");
+		        }
+		    });
+	    });
+	}
+
+	jQuery(document).ready(function(){
+		var infoId = <?php echo $id; ?>;
+	    getShippingDistrict(infoId);
+	    getShippingWard(infoId);
+	    jQuery.ajax({
+	        type: "POST",
+	        url: "<?php echo JUri::root() . 'index.php?option=com_ajax&plugin=GetKerryCity&group=redshop_checkout&format=raw'; ?>",
+	        data: {id: infoId},
+	        success: function(data) {
+	        	jQuery('select#rs_kerry_city').append(data).trigger("change");
+	        	jQuery('select#rs_kerry_billing_city').append(data);
+	        	getBillingDistrict(infoId);
+	    		getBillingWard(infoId);
+	    		jQuery('select#rs_kerry_billing_city').append(data).trigger('change');
+	        }
+	    });
 	});
 </script>
