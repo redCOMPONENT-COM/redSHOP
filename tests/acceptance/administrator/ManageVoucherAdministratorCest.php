bad1bf5b5330c1994d41ef2c6b7c04f192dfa473
nhung@redweb.dk <nhung@redweb.dk>
2017-07-21 09:46:46 +0700

    fix conflict

--- tests/acceptance/administrator/ManageVoucherAdministratorCest.php
+++ tests/acceptance/administrator/ManageVoucherAdministratorCest.php
@@ -54,209 +54,209 @@ class ManageVoucherAdministratorCest
     }
 
 
-//    public function createCategory(AcceptanceTester $I, $scenario){
-//        $I->wantTo('Test Voucher creation in Administrator');
-//        $I->doAdministratorLogin();
-//        $I = new AcceptanceTester\CategoryManagerJoomla3Steps($scenario);
-//        $I->addCategorySave($this->randomCategoryName);
-//    }
-//
-//    public function createProduct(AcceptanceTester $I, $scenario){
-//        $I->wantTo('Test Voucher creation in Administrator');
-//        $I->doAdministratorLogin();
-//        $I = new AcceptanceTester\ProductManagerJoomla3Steps($scenario);
-//        $I->createProductSave($this->productName,$this->randomCategoryName, $this->randomProductNumber, $this->randomProductPrice, $this->minimumPerProduct, $this->minimumQuantity, $this->maximumQuantity, $this->discountStart, $this->discountEnd);
-//    }
-//    /**
-//     * Function to Test Voucher with Save and Close  Creation in Backend
-//     *
-//     */
-//    public function createVoucher(AcceptanceTester $I, $scenario)
+    public function createCategory(AcceptanceTester $I, $scenario){
+        $I->wantTo('Test Voucher creation in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\CategoryManagerJoomla3Steps($scenario);
+        $I->addCategorySave($this->randomCategoryName);
+    }
+
+    public function createProduct(AcceptanceTester $I, $scenario){
+        $I->wantTo('Test Voucher creation in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\ProductManagerJoomla3Steps($scenario);
+        $I->createProductSave($this->productName,$this->randomCategoryName, $this->randomProductNumber, $this->randomProductPrice, $this->minimumPerProduct, $this->minimumQuantity, $this->maximumQuantity, $this->discountStart, $this->discountEnd);
+    }
+    /**
+     * Function to Test Voucher with Save and Close  Creation in Backend
+     *
+     */
+    public function createVoucher(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test Voucher creation in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\VoucherManagerJoomla3Steps($scenario);
+        $I->addVoucher($this->randomVoucherCode, $this->voucherAmount, $this->voucherCount,$this->productName);
+    }
+
+    /**
+     * Function check cancel button when goes on new page
+     *
+     * @param AcceptanceTester $I
+     * @param $scenario
+     */
+    public function cancelButton(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test Voucher creation in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\VoucherManagerJoomla3Steps($scenario);
+        $I->cancelButton();
+        $I->see('Voucher Management', '.page-title');
+    }
+
+//     When backend fix this bugs , Please open code
+//    public function addStartMoreThanEnd(AcceptanceTester $I, $scenario)
 //    {
 //        $I->wantTo('Test Voucher creation in Administrator');
 //        $I->doAdministratorLogin();
 //        $I = new AcceptanceTester\VoucherManagerJoomla3Steps($scenario);
-//        $I->addVoucher($this->randomVoucherCode, $this->voucherAmount, $this->voucherCount,$this->productName);
+//        $I->addVoucher($this->randomVoucherCode, $this->voucherAmount,$this->startDate,$this->endDate, $this->voucherCount);
 //    }
-//
-//    /**
-//     * Function check cancel button when goes on new page
-//     *
-//     * @param AcceptanceTester $I
-//     * @param $scenario
-//     */
-//    public function cancelButton(AcceptanceTester $I, $scenario)
-//    {
-//        $I->wantTo('Test Voucher creation in Administrator');
-//        $I->doAdministratorLogin();
-//        $I = new AcceptanceTester\VoucherManagerJoomla3Steps($scenario);
-//        $I->cancelButton();
-//        $I->see('Voucher Management', '.page-title');
-//    }
-//
-////     When backend fix this bugs , Please open code
-////    public function addStartMoreThanEnd(AcceptanceTester $I, $scenario)
-////    {
-////        $I->wantTo('Test Voucher creation in Administrator');
-////        $I->doAdministratorLogin();
-////        $I = new AcceptanceTester\VoucherManagerJoomla3Steps($scenario);
-////        $I->addVoucher($this->randomVoucherCode, $this->voucherAmount,$this->startDate,$this->endDate, $this->voucherCount);
-////    }
-//
-//    /**
-//     *
-//     * Function add voucher missing code
-//     *
-//     * @param AcceptanceTester $I
-//     * @param $scenario
-//     */
-//    public function addVoucherMissingCode(AcceptanceTester $I, $scenario)
-//    {
-//        $I->wantTo('Test Voucher creation in Administrator');
-//        $I->doAdministratorLogin();
-//        $I = new AcceptanceTester\VoucherManagerJoomla3Steps($scenario);
-//        $I->addVoucherMissingCode($this->voucherAmount, $this->startDate, $this->endDate, $this->voucherCount);
-//    }
-//
-//    /**
-//     *
-//     * Fucntion add voucher missing products
-//     *
-//     * @param AcceptanceTester $I
-//     * @param $scenario
-//     */
-//    public function addVoucherMissingProducts(AcceptanceTester $I, $scenario)
-//    {
-//        $I->wantTo('Test Voucher creation in Administrator');
-//        $I->doAdministratorLogin();
-//        $I = new AcceptanceTester\VoucherManagerJoomla3Steps($scenario);
-//        $I->addVoucherMissingProducts($this->randomVoucherCode, $this->voucherAmount, $this->startDate, $this->endDate, $this->voucherCount);
-//    }
-//
-//    /**
-//     *
-//     * Function Edit voucher missing code
-//     *
-//     * @param AcceptanceTester $I
-//     * @param $scenario
-//     */
-//    public function editVoucherMissingNameCode(AcceptanceTester $I, $scenario)
-//    {
-//        $I->wantTo('Test if Voucher gets updated in Administrator');
-//        $I->doAdministratorLogin();
-//        $I = new AcceptanceTester\VoucherManagerJoomla3Steps($scenario);
-//        $I->editVoucherMissingName($this->randomVoucherCode);
-//    }
-//
-//    /**
-//     *
-//     * Function check cancel button
-//     *
-//     * @param AcceptanceTester $I
-//     * @param $scenario
-//     */
-//    public function checkCloseButton(AcceptanceTester $I, $scenario)
-//    {
-//        $I->wantTo('Test if Voucher gets updated in Administrator');
-//        $I->doAdministratorLogin();
-//        $I = new AcceptanceTester\VoucherManagerJoomla3Steps($scenario);
-//        $I->checkCloseButton($this->randomVoucherCode);
-//    }
-//
-//    /**
-//     *
-//     * Function check edit button without choice any voucher
-//     *
-//     * @param AcceptanceTester $I
-//     * @param $scenario
-//     */
-//    public function checkEditButton(AcceptanceTester $I, $scenario)
-//    {
-//        $I->wantTo('Test if Voucher gets updated in Administrator');
-//        $I->doAdministratorLogin();
-//        $I = new AcceptanceTester\VoucherManagerJoomla3Steps($scenario);
-//        $I->checkEditButton();
-//    }
-//
-//    /**
-//     *
-//     * Function check delete button without choice any voucher
-//     *
-//     * @param AcceptanceTester $I
-//     * @param $scenario
-//     */
-//    public function checkDeleteButton(AcceptanceTester $I, $scenario)
-//    {
-//        $I->wantTo('Test if Voucher gets updated in Administrator');
-//        $I->doAdministratorLogin();
-//        $I = new AcceptanceTester\VoucherManagerJoomla3Steps($scenario);
-//        $I->checkDeleteButton();
-//    }
-//
-//    /**
-//     *
-//     * Function check publish button without choice any voucher
-//     *
-//     * @param AcceptanceTester $I
-//     * @param $scenario
-//     */
-//    public function checkPublishButton(AcceptanceTester $I, $scenario)
-//    {
-//        $I->wantTo('Test if Voucher gets updated in Administrator');
-//        $I->doAdministratorLogin();
-//        $I = new AcceptanceTester\VoucherManagerJoomla3Steps($scenario);
-//        $I->checkPublishButton();
-//    }
-//
-//    /**
-//     *
-//     * Function check unpublish button without choice any voucher
-//     *
-//     * @param AcceptanceTester $I
-//     * @param $scenario
-//     */
-//    public function checkUnpublishButton(AcceptanceTester $I, $scenario)
-//    {
-//        $I->wantTo('Test if Voucher gets updated in Administrator');
-//        $I->doAdministratorLogin();
-//        $I = new AcceptanceTester\VoucherManagerJoomla3Steps($scenario);
-//        $I->checkUnpublishButton();
-//    }
-//
-//    /**
-//     * Function to Test Voucher Deletion
-//     *
-//     * @depends createVoucher
-//     */
-//    public function deleteVoucher(AcceptanceTester $I, $scenario)
-//    {
-//        $I->wantTo('Deletion of Voucher in Administrator');
-//        $I->doAdministratorLogin();
-//        $I = new AcceptanceTester\VoucherManagerJoomla3Steps($scenario);
-//        $I->deleteVoucher($this->randomVoucherCode);
-//    }
-//
-//    /**
-//     * Function to Test Voucher Creation in Backend
-//     *
-//     */
-//    public function addVoucherSave(AcceptanceTester $I, $scenario)
-//    {
-//        $I->wantTo('Test Voucher creation in Administrator');
-//        $I->doAdministratorLogin();
-//        $I = new AcceptanceTester\VoucherManagerJoomla3Steps($scenario);
-//        $I->addVoucherSave($this->randomVoucherCode, $this->voucherAmount, $this->startDate, $this->endDate, $this->voucherCount,$this->productName);
-//    }
-//
-//    /**
-//     *
-//     * Function change  sates unpublish voucher state inline
-//     *
-//     * @param AcceptanceTester $I
-//     * @param $scenario
-//     *
-//     * @depends addVoucherSave
-//     */
+
+    /**
+     *
+     * Function add voucher missing code
+     *
+     * @param AcceptanceTester $I
+     * @param $scenario
+     */
+    public function addVoucherMissingCode(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test Voucher creation in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\VoucherManagerJoomla3Steps($scenario);
+        $I->addVoucherMissingCode($this->voucherAmount, $this->startDate, $this->endDate, $this->voucherCount,$this->productName);
+    }
+
+    /**
+     *
+     * Fucntion add voucher missing products
+     *
+     * @param AcceptanceTester $I
+     * @param $scenario
+     */
+    public function addVoucherMissingProducts(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test Voucher creation in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\VoucherManagerJoomla3Steps($scenario);
+        $I->addVoucherMissingProducts($this->randomVoucherCode, $this->voucherAmount, $this->startDate, $this->endDate, $this->voucherCount);
+    }
+
+    /**
+     *
+     * Function Edit voucher missing code
+     *
+     * @param AcceptanceTester $I
+     * @param $scenario
+     */
+    public function editVoucherMissingNameCode(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test if Voucher gets updated in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\VoucherManagerJoomla3Steps($scenario);
+        $I->editVoucherMissingName($this->randomVoucherCode);
+    }
+
+    /**
+     *
+     * Function check cancel button
+     *
+     * @param AcceptanceTester $I
+     * @param $scenario
+     */
+    public function checkCloseButton(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test if Voucher gets updated in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\VoucherManagerJoomla3Steps($scenario);
+        $I->checkCloseButton($this->randomVoucherCode);
+    }
+
+    /**
+     *
+     * Function check edit button without choice any voucher
+     *
+     * @param AcceptanceTester $I
+     * @param $scenario
+     */
+    public function checkEditButton(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test if Voucher gets updated in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\VoucherManagerJoomla3Steps($scenario);
+        $I->checkEditButton();
+    }
+
+    /**
+     *
+     * Function check delete button without choice any voucher
+     *
+     * @param AcceptanceTester $I
+     * @param $scenario
+     */
+    public function checkDeleteButton(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test if Voucher gets updated in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\VoucherManagerJoomla3Steps($scenario);
+        $I->checkDeleteButton();
+    }
+
+    /**
+     *
+     * Function check publish button without choice any voucher
+     *
+     * @param AcceptanceTester $I
+     * @param $scenario
+     */
+    public function checkPublishButton(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test if Voucher gets updated in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\VoucherManagerJoomla3Steps($scenario);
+        $I->checkPublishButton();
+    }
+
+    /**
+     *
+     * Function check unpublish button without choice any voucher
+     *
+     * @param AcceptanceTester $I
+     * @param $scenario
+     */
+    public function checkUnpublishButton(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test if Voucher gets updated in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\VoucherManagerJoomla3Steps($scenario);
+        $I->checkUnpublishButton();
+    }
+
+    /**
+     * Function to Test Voucher Deletion
+     *
+     * @depends createVoucher
+     */
+    public function deleteVoucher(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Deletion of Voucher in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\VoucherManagerJoomla3Steps($scenario);
+        $I->deleteVoucher($this->randomVoucherCode);
+    }
+
+    /**
+     * Function to Test Voucher Creation in Backend
+     *
+     */
+    public function addVoucherSave(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test Voucher creation in Administrator');
+        $I->doAdministratorLogin();
+        $I = new AcceptanceTester\VoucherManagerJoomla3Steps($scenario);
+        $I->addVoucherSave($this->randomVoucherCode, $this->voucherAmount, $this->startDate, $this->endDate, $this->voucherCount,$this->productName);
+    }
+
+    /**
+     *
+     * Function change  sates unpublish voucher state inline
+     *
+     * @param AcceptanceTester $I
+     * @param $scenario
+     *
+     * @depends addVoucherSave
+     */
     public function changeVoucherStateUnpublish(AcceptanceTester $I, $scenario)
     {
         $I->wantTo('Test if State Unpublish of a Voucher gets Updated in Administrator');
