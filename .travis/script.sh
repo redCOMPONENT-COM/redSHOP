8a382e20a1e4c993be909ce3f3ef15d9c220fab9
thongredweb <thong@redweb.dk>
2017-07-22 18:19:23 +0700

    [Travis] update

--- .travis/script.sh
+++ .travis/script.sh
@@ -2,12 +2,12 @@
 set -ev
 
 # Following line uses a bot account to authenticate in github and make composer stable and faster, see https://redweb.atlassian.net/wiki/pages/viewpage.action?pageId=46694753
-# composer config -g github-oauth.github.com "${GITHUB_TOKEN}"
-# composer update
-# composer install --working-dir ./libraries/redshop --ansi
-#- php vendor/bin/robo check:for-missed-debug-code
-#- php vendor/bin/robo check:for-php-parse
-# - php vendor/bin/robo check:codestyle
+composer config -g github-oauth.github.com "${GITHUB_TOKEN}"
+composer update
+composer install --working-dir ./libraries/redshop --ansi
+#php vendor/bin/robo check:for-missed-debug-code
+#php vendor/bin/robo check:for-php-parse
+#php vendor/bin/robo check:codestyle
 mv tests/acceptance.suite.dist.yml tests/acceptance.suite.yml # Create travis system tests config file
 # Download Joomla for testing
 php vendor/bin/robo prepare:site-for-system-tests
