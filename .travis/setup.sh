3e2c5fe77c798cebfa2f790fdc0d59967426abea
thongredweb <thong@redweb.dk>
2017-07-24 20:22:57 +0700

    [Travis] Update code

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -30,8 +30,8 @@ else
 	sudo sed -e "s,;listen.owner = nobody,listen.owner = $USER,g" --in-place $file
 	sudo sed -e "s,;listen.group = nobody,listen.group = $USER,g" --in-place $file
 	sudo sed -e "s,;listen.mode = 0660,listen.mode = 0666,g" --in-place $file
-	sudo sed -e "s,user = nobody,;user = $USER,g" --in-place $file
-	sudo sed -e "s,group = nobody,;group = $USER,g" --in-place $file
+	sudo sed -e "s,user = nobody,user = $USER,g" --in-place $file
+	sudo sed -e "s,group = nobody,group = $USER,g" --in-place $file
 	sudo a2enmod rewrite actions fastcgi alias
 	echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 	~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
@@ -42,7 +42,7 @@ else
 	sudo a2ensite 000-default.conf
 	sudo service apache2 restart
 
-	ls -la /tmp/php${TRAVIS_PHP_VERSION:0:1}-fpm.sock
+	cat /etc/apache2/sites-available/000-default.conf
 
 	# XVFB
 	sh -e /etc/init.d/xvfb start
