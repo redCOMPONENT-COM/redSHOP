417d13f0694d281512ddcca901eef230d6b156bc
thongredweb <thong@redweb.dk>
2017-07-24 19:55:54 +0700

    [Travis] Update code

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -19,21 +19,21 @@ else
   	# sudo apt-get install --yes --force-yes apache2 libapache2-mod-fastcgi
 
 	sudo mkdir $(pwd)/.run
-	phpversionname="$(phpenv version-name)"
-	file="/home/$USER/.phpenv/versions/$phpversionname/etc/php-fpm.conf"
-	sudo cp /home/$USER/.phpenv/versions/$phpversionname/etc/php-fpm.conf.default /home/$USER/.phpenv/versions/$phpversionname/etc/php-fpm.conf
-	if [ -f /home/$USER/.phpenv/versions/$phpversionname/etc/php-fpm.d/www.conf.default ]; then
-		sudo cp /home/$USER/.phpenv/versions/$phpversionname/etc/php-fpm.d/www.conf.default /home/$USER/.phpenv/versions/$phpversionname/etc/php-fpm.d/www.conf
-		file=/home/$USER/.phpenv/versions/$phpversionname/etc/php-fpm.d/www.conf
+
+	file="~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf"
+	sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
+	if [ -f ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/www.conf.default ]; then
+		sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/www.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/www.conf
+		file=~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/www.conf
 	fi;
-	sudo sed -e "s,listen = 127.0.0.1:9000,listen = /tmp/php${phpversionname:0:1}-fpm.sock,g" --in-place $file
+	sudo sed -e "s,listen = 127.0.0.1:9000,listen = /tmp/php${TRAVIS_PHP_VERSION:0:1}-fpm.sock,g" --in-place $file
 	sudo sed -e "s,;listen.owner = nobody,listen.owner = $USER,g" --in-place $file
 	sudo sed -e "s,;listen.group = nobody,listen.group = $USER,g" --in-place $file
 	sudo sed -e "s,;listen.mode = 0660,listen.mode = 0666,g" --in-place $file
 	sudo sed -e "s,user = nobody,;user = $USER,g" --in-place $file
 	sudo sed -e "s,group = nobody,;group = $USER,g" --in-place $file
 	sudo sudo a2enmod rewrite actions fastcgi alias
-	echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$phpversionname/etc/php.ini
+	echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 	~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
 	sudo a2dissite 000-default.conf
 	sudo cp -f ./tests/travis-ci-apache.conf /etc/apache2/sites-available/000-default.conf
@@ -46,9 +46,9 @@ else
 	sh -e /etc/init.d/xvfb start
 	sleep 3
 	# Window manager
-	sudo apt-get install fluxbox -y --force-yes
-	fluxbox &
-	sleep 3 # give fluxbox some time to start
+	# sudo apt-get install fluxbox -y --force-yes
+	# fluxbox &
+	# sleep 3 # give fluxbox some time to start
 
 	# Install redSHOP Library composer
 	# composer config -g github-oauth.github.com "${GITHUB_TOKEN}"
