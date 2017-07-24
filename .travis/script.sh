989179c80c769e04487046622ea931e5ca4d1bbb
thongredweb <thong@redweb.dk>
2017-07-24 17:25:57 +0700

    [Travis] Update code

--- .travis/script.sh
+++ .travis/script.sh
@@ -8,8 +8,6 @@ if [ "${ACCEPTANCE}" = "false" ]; then
 elif [ "${ACCEPTANCE}" = "true" ]; then
 	mv tests/RoboFile.ini.dist tests/RoboFile.ini # Create Robo Config file.
 	mv tests/acceptance.suite.dist.yml tests/acceptance.suite.yml # Create travis system tests config file
-	# php vendor/bin/robo prepare:site-for-system-tests # Download Joomla for testing
-	# php vendor/bin/robo check:travis-webserver # Test apache
 	php vendor/bin/robo run:tests 1 # Run Acceptance test
 	#- php vendor/bin/robo send:codeception-output-to-slack C02L0SE5E xoxp-2309442657-4789197868-4789233706-68cec7 # Send output to Slack.
 else
