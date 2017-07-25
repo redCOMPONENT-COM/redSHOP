b5f2e4d917ba7b7f0eb191c356df6a3ab42cee95
Nhung Nguyen <nhung@redweb.dk>
2017-07-25 17:27:26 +0700

    change runTests for test runTravis

--- RoboFile.php
+++ RoboFile.php
@@ -341,92 +341,92 @@ class RoboFile extends \Robo\Tasks
         }
     }
 
-    /**
-     * Function to Run tests in a Group
-     *
-     * @return void
-     */
-    public function runTests($use_htaccess = 0)
-    {
-        $this->prepareSiteForSystemTests($use_htaccess);
+//     /**
+//      * Function to Run tests in a Group
+//      *
+//      * @return void
+//      */
+//     public function runTests($use_htaccess = 0)
+//     {
+//         $this->prepareSiteForSystemTests($use_htaccess);
 
-        $this->getComposer();
+//         $this->getComposer();
 
-        $this->taskComposerInstall()->run();
+//         $this->taskComposerInstall()->run();
 
-//		$this->runSelenium();
+// //		$this->runSelenium();
 
-        $this->taskSeleniumStandaloneServer()
-            ->setURL("http://localhost:4444")
-            ->runSelenium()
-            ->waitForSelenium()
-            ->run()
-            ->stopOnFail();
+//         $this->taskSeleniumStandaloneServer()
+//             ->setURL("http://localhost:4444")
+//             ->runSelenium()
+//             ->waitForSelenium()
+//             ->run()
+//             ->stopOnFail();
 
-        // Make sure to Run the B uild Command to Generate AcceptanceTester
-        $this->_exec("vendor/bin/codecept build");
+//         // Make sure to Run the B uild Command to Generate AcceptanceTester
+//         $this->_exec("vendor/bin/codecept build");
 
-        $this->taskCodecept()
-            ->arg('--steps')
-            ->arg('--debug')
-            ->arg('--tap')
-            ->arg('--fail-fast')
-            ->arg('tests/acceptance/install/')
-            ->run()
-            ->stopOnFail();
+//         $this->taskCodecept()
+//             ->arg('--steps')
+//             ->arg('--debug')
+//             ->arg('--tap')
+//             ->arg('--fail-fast')
+//             ->arg('tests/acceptance/install/')
+//             ->run()
+//             ->stopOnFail();
 
-        $this->taskCodecept()
-            ->arg('--steps')
-            ->arg('--debug')
-            ->arg('--tap')
-            ->arg('--fail-fast')
-            ->arg('tests/acceptance/administrator/')
-            ->run()
-            ->stopOnFail();
+//         $this->taskCodecept()
+//             ->arg('--steps')
+//             ->arg('--debug')
+//             ->arg('--tap')
+//             ->arg('--fail-fast')
+//             ->arg('tests/acceptance/administrator/')
+//             ->run()
+//             ->stopOnFail();
 
-        $this->taskCodecept()
-            ->arg('--steps')
-            //  ->arg('--debug')
-            ->arg('--tap')
-            ->arg('--fail-fast')
-            ->arg('tests/acceptance/integration/ManageProductsCheckoutFrontEndCest.php')
-            ->run()
-            ->stopOnFail();
+//         $this->taskCodecept()
+//             ->arg('--steps')
+//             //  ->arg('--debug')
+//             ->arg('--tap')
+//             ->arg('--fail-fast')
+//             ->arg('tests/acceptance/integration/ManageProductsCheckoutFrontEndCest.php')
+//             ->run()
+//             ->stopOnFail();
 
-        /*
-        $this->taskCodecept()
-            ->arg('--steps')
-            ->arg('--debug')
-            ->arg('--tap')
-            ->arg('--fail-fast')
-            ->arg('tests/acceptance/checkout/')
-            ->run();
-            // ->stopOnFail();
-        */
+//         /*
+//         $this->taskCodecept()
+//             ->arg('--steps')
+//             ->arg('--debug')
+//             ->arg('--tap')
+//             ->arg('--fail-fast')
+//             ->arg('tests/acceptance/checkout/')
+//             ->run();
+//             // ->stopOnFail();
+//         */
 
-        $this->taskCodecept()
-            //  ->arg('--steps')
-            //  ->arg('--debug')
-            ->arg('--tap')
-            ->arg('--fail-fast')
-            ->arg('tests/acceptance/uninstall/')
-            ->run()
-            ->stopOnFail();
+//         $this->taskCodecept()
+//             //  ->arg('--steps')
+//             //  ->arg('--debug')
+//             ->arg('--tap')
+//             ->arg('--fail-fast')
+//             ->arg('tests/acceptance/uninstall/')
+//             ->run()
+//             ->stopOnFail();
 
-        /* @todo: REDSHOP-2884
-         * $this->say('preparing for update test');
-         * $this->getDevelop();
-         * $this->taskCodecept()
-         * ->arg('--steps')
-         * ->arg('--debug')
-         * ->arg('--fail-fast')
-         * ->arg('tests/acceptance/update/')
-         * ->run()
-         * ->stopOnFail();
-         */
+//         /* @todo: REDSHOP-2884
+//          * $this->say('preparing for update test');
+//          * $this->getDevelop();
+//          * $this->taskCodecept()
+//          * ->arg('--steps')
+//          * ->arg('--debug')
+//          * ->arg('--fail-fast')
+//          * ->arg('tests/acceptance/update/')
+//          * ->run()
+//          * ->stopOnFail();
+//          */
 
-        $this->killSelenium();
-    }
+//         $this->killSelenium();
+//     }
 
 
     public function runTravis($folder)
