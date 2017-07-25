64f1c15aaeab19977e216dcc6eff3141c090e92e
thongredweb <thong@redweb.dk>
2017-07-25 16:47:00 +0700

    [Travis] Update code

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -17,12 +17,10 @@ else
 
 	sudo apt-get update -qq
   	sudo apt-get install --yes --force-yes apache2 libapache2-mod-fastcgi
-
 	sudo mkdir $(pwd)/.run
-
 	sudo a2enmod rewrite actions fastcgi alias
 
-	if [[ ${TRAVIS_PHP_VERSION:0:1} == "7" ]]; then
+	if [[ ${TRAVIS_PHP_VERSION:0:3} == "7.0" ]]; then
 		sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/www.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/www.conf
 		sudo sed -e "s,listen = 127.0.0.1:9000,listen = /tmp/php7-fpm.sock,g" --in-place ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/www.conf
 		sudo sed -e "s,;listen.owner = nobody,listen.owner = $USER,g" --in-place ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/www.conf
@@ -49,36 +47,33 @@ else
 
 		echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 		~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
-		sudo cp -f ./tests/travis-ci-apache.conf /etc/apache2/sites-available/default
-		sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/default
-		sudo sed -e "s?%PHPVERSION%?5?g" --in-place /etc/apache2/sites-available/default
+		sudo cp -f ./tests/travis-ci-apache.conf /etc/apache2/sites-available/000-default.conf
+		sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/000-default.conf
+		sudo sed -e "s?%PHPVERSION%?5?g" --in-place /etc/apache2/sites-available/000-default.conf
 	fi
 
 	sudo service apache2 restart
 
-	# XVFB
-	sh -e /etc/init.d/xvfb start
-	sleep 3
-	# Window manager
-	sudo apt-get install fluxbox -y --force-yes
-	fluxbox &
-	sleep 3 # give fluxbox some time to start
+	# sh -e /etc/init.d/xvfb start
+	# sleep 3
+	#sudo apt-get install fluxbox -y --force-yes
+	#fluxbox &
+	#sleep 3 # give fluxbox some time to start
 
-	# Install redSHOP Library composer
-	composer config -g github-oauth.github.com "${GITHUB_TOKEN}"
-	composer global require hirak/prestissimo
+	#composer config -g github-oauth.github.com "${GITHUB_TOKEN}"
+	#composer global require hirak/prestissimo
 
-	cd libraries/redshop
-	composer install --prefer-dist
+	#cd libraries/redshop
+	#composer install --prefer-dist
 
-	cd ../../plugins/redshop_pdf/tcpdf/helper
-	composer install --prefer-dist
+	#cd ../../plugins/redshop_pdf/tcpdf/helper
+	#composer install --prefer-dist
 
-	cd ../../../..
-	composer install --prefer-dist
+	#cd ../../../..
+	#composer install --prefer-dist
 
 	# Gulp packages
-	npm install
-	mv gulp-config.sample.json gulp-config.json
-	gulp release --skip-version
+	#npm install
+	#mv gulp-config.sample.json gulp-config.json
+	#gulp release --skip-version
 fi
