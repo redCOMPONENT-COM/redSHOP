8c86e14563263e9583b4dfbee8f1d04c06d28120
nhung@redweb.dk <nhung@redweb.dk>
2017-07-25 18:00:04 +0700

    change style code and fix delete

--- tests/acceptance/administrator/ManageShopperGroupAdministratorCest.php
+++ tests/acceptance/administrator/ManageShopperGroupAdministratorCest.php
@@ -7,7 +7,8 @@ class ManageShopperGroupAdministratorCest
     public function __construct()
     {
         $this->faker = Faker\Factory::create();
-        $this->shopperName = 'Testing shopper ' . $this->faker->randomNumber();
+        $this->shopperName = $this->faker->bothify(' Testing shopper ##??');
+        $this->shopperNameSaveClose = $this->faker->bothify(' Name Close ##??');
         $this->shopperType = 'Default Private';
         $this->categoryName = 'Testing Category ' . $this->faker->randomNumber();
         $this->customerType = 'Company customer';
@@ -19,11 +20,13 @@ class ManageShopperGroupAdministratorCest
         $this->idShopperChange = '1';
 
     }
-
+    public function _before(AcceptanceTester $I)
+    {
+        $I->doAdministratorLogin();
+    }
     public function createCategory(AcceptanceTester $I, $scenario)
     {
         $I->wantTo('Test Category Save creation in Administrator');
-        $I->doAdministratorLogin();
         $I = new AcceptanceTester\CategoryManagerJoomla3Steps($scenario);
         $I->wantTo('Create a Category Save button');
         $I->addCategorySave($this->categoryName);
@@ -43,10 +46,11 @@ class ManageShopperGroupAdministratorCest
     public function creatShopperGroup(AcceptanceTester $I, $scenario)
     {
         $I->wantTo('Test Category Save creation in Administrator');
-        $I->doAdministratorLogin();
         $I = new AcceptanceTester\ShopperGroupManagerJoomla3Steps($scenario);
         $I->wantTo('Create a Category Save button');
-        $I->addShopperGroupsSave($this->shopperName, $this->shopperType, $this->customerType, "Templates", $this->shippingRate, $this->shippingCheckout, $this->catalog, $this->showPrice);
+        $I->addShopperGroups($this->shopperName, $this->shopperType, $this->customerType, $this->categoryName, $this->shippingRate, $this->shippingCheckout, $this->catalog, $this->showPrice, 'save');
+        $I->addShopperGroups($this->shopperNameSaveClose, $this->shopperType, $this->customerType, $this->categoryName, $this->shippingRate, $this->shippingCheckout, $this->catalog, $this->showPrice, 'saveclose');
+
     }
 
     /**
@@ -61,7 +65,6 @@ class ManageShopperGroupAdministratorCest
     public function deleteShopperGroupsYes(AcceptanceTester $I, $scenario)
     {
         $I->wantTo('Check delete Shopper groups');
-        $I->doAdministratorLogin();
         $I = new AcceptanceTester\ShopperGroupManagerJoomla3Steps($scenario);
         $I->wantTo('Edit Name of Shopper groups');
         $I->deleteShopperGroupsYes();
@@ -75,49 +78,13 @@ class ManageShopperGroupAdministratorCest
      * @param $scenario
      *
      */
-    public function changeStatusUnpublish(AcceptanceTester $I, $scenario)
+    public function changeStateShopperGroup(AcceptanceTester $I, $scenario)
     {
         $I->wantTo('Test Unpublish Shopper groups');
-        $I->doAdministratorLogin();
         $I = new AcceptanceTester\ShopperGroupManagerJoomla3Steps($scenario);
         $I->wantTo('Test Unpublish Shopper groups');
-        $I->changeStateShopperGroups();
-        $currentState = $I->getShopperGroupsStates();
-        $I->verifyState('unpublished', $currentState);
-    }
-
-    /**
-     * Function change status os shopper groups is publish
-     * Change Shopper groups have name is Default Private
-     *
-     * @param AcceptanceTester $I
-     * @param $scenario
-     *
-     */
-    public function changeStatusPublish(AcceptanceTester $I, $scenario)
-    {
-        $I->wantTo('Test Publish Shopper groups');
-        $I->doAdministratorLogin();
-        $I = new AcceptanceTester\ShopperGroupManagerJoomla3Steps($scenario);
-        $I->wantTo('Test Publish Shopper groups');
-        $I->changeStateShopperGroups();
-        $currentState = $I->getShopperGroupsStates();
-        $I->verifyState('published', $currentState);
-    }
-
-    /**
-     * Functionc check Close button inside page detail
-     *
-     * @param AcceptanceTester $I
-     * @param $scenario
-     */
-    public function checkCloseButton(AcceptanceTester $I, $scenario)
-    {
-        $I->wantTo('Check Close button Shopper groups');
-        $I->doAdministratorLogin();
-        $I = new AcceptanceTester\ShopperGroupManagerJoomla3Steps($scenario);
-        $I->wantTo('Edit Name of Shopper groups');
-        $I->checkCloseButton($this->idShopperChange);
+        $I->changeStateShopperGroup('unpublished');
+        $I->changeStateShopperGroup('published');
     }
 
     /**
@@ -126,15 +93,14 @@ class ManageShopperGroupAdministratorCest
      * @param AcceptanceTester $I
      * @param $scenario
      *
-     * @depends changeStatusPublish
+     * @depends changeStateShopperGroup
      */
     public function editNameShopper(AcceptanceTester $I, $scenario)
     {
         $I->wantTo('Edit Name of Shopper groups');
-        $I->doAdministratorLogin();
         $I = new AcceptanceTester\ShopperGroupManagerJoomla3Steps($scenario);
         $I->wantTo('Edit Name of Shopper groups');
-        $I->editShopperGroups($this->shopperType,$this->idShopperChange,$this->nameShopperEdit);
+        $I->editShopperGroups($this->shopperType, $this->idShopperChange, $this->nameShopperEdit);
     }
 
     /**
@@ -144,90 +110,36 @@ class ManageShopperGroupAdministratorCest
     public function deleteShopperGroupsNo(AcceptanceTester $I, $scenario)
     {
         $I->wantTo('Check delete Shopper groups');
-        $I->doAdministratorLogin();
         $I = new AcceptanceTester\ShopperGroupManagerJoomla3Steps($scenario);
         $I->wantTo('Edit Name of Shopper groups');
         $I->deleteShopperGroupsNo();
     }
 
-    /**
-     * Function check cancel button
-     *
-     * @param AcceptanceTester $I
-     * @param $scenario
-     */
-    public function addShopperGroupsCancel(AcceptanceTester $I, $scenario)
+    public function changeStatusAllShopperGroups(AcceptanceTester $I, $scenario)
     {
-        $I->wantTo('Check delete Shopper groups');
-        $I->doAdministratorLogin();
+        $I->wantTo('Publish all  Shopper groups');
         $I = new AcceptanceTester\ShopperGroupManagerJoomla3Steps($scenario);
-        $I->wantTo('Edit Name of Shopper groups');
-        $I->addShopperGroupsCancel();
+        $I->changStatusAllShopperGroups('unpublished');
+        $I->changStatusAllShopperGroups('published');
+    }
+
+    public function checkButtons(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Test to validate different buttons on Gift Card Views');
+        $I = new AcceptanceTester\ShopperGroupManagerJoomla3Steps($scenario);
+        $I->checkButtons('edit');
+        $I->checkButtons('cancel');
+        $I->checkButtons('publish');
+        $I->checkButtons('unpublish');
+        $I->checkButtons('Delete');
     }
 
     public function addShopperGroupsMissingName(AcceptanceTester $I, $scenario)
     {
         $I->wantTo('Check delete Shopper groups');
-        $I->doAdministratorLogin();
         $I = new AcceptanceTester\ShopperGroupManagerJoomla3Steps($scenario);
         $I->wantTo('Edit Name of Shopper groups');
         $I->addShopperGroupsMissingName();
-    }
-
-    public function publishAllShopperGroups(AcceptanceTester $I, $scenario)
-    {
-        $I->wantTo('Publish all  Shopper groups');
-        $I->doAdministratorLogin();
-        $I = new AcceptanceTester\ShopperGroupManagerJoomla3Steps($scenario);
-        $I->wantTo('Publish all  Shopper groups');
-        $I->publishAllShopperGroups();
-    }
-
-    public function unpublishAllShopperGroups(AcceptanceTester $I, $scenario)
-    {
-        $I->wantTo('Unpublish all  Shopper groups');
-        $I->doAdministratorLogin();
-        $I = new AcceptanceTester\ShopperGroupManagerJoomla3Steps($scenario);
-        $I->wantTo('unpublish all  Shopper groups');
-        $I->unpublishAllShopperGroups();
-    }
-
-
-    public function checkEditButton(AcceptanceTester $I, $scenario)
-    {
-        $I->wantTo('check edit button Shopper groups');
-        $I->doAdministratorLogin();
-        $I = new AcceptanceTester\ShopperGroupManagerJoomla3Steps($scenario);
-        $I->checkEditButton();
-    }
-    public function checkDeleteButton(AcceptanceTester $I, $scenario)
-    {
-        $I->wantTo('check delete button Shopper groups');
-        $I->doAdministratorLogin();
-        $I = new AcceptanceTester\ShopperGroupManagerJoomla3Steps($scenario);
-        $I->checkDeleteButton();
-    }
-    public function checkPublishButton(AcceptanceTester $I, $scenario)
-    {
-        $I->wantTo('check publish button Shopper groups');
-        $I->doAdministratorLogin();
-        $I = new AcceptanceTester\ShopperGroupManagerJoomla3Steps($scenario);
-        $I->checkPublishButton();
-    }
-    public function checkUnPublishButton(AcceptanceTester $I, $scenario)
-    {
-        $I->wantTo('check unpublish button Shopper groups');
-        $I->doAdministratorLogin();
-        $I = new AcceptanceTester\ShopperGroupManagerJoomla3Steps($scenario);
-        $I->checkUnPublishButton();
-    }
-
-    public function deleteAllShopperGroups(AcceptanceTester $I, $scenario)
-    {
-        $I->wantTo('delete all  Shopper groups');
-        $I->doAdministratorLogin();
-        $I = new AcceptanceTester\ShopperGroupManagerJoomla3Steps($scenario);
-        $I->deleteAllShopperGroups();
     }
 
 }
--- tests/acceptance/administrator/_steps/Joomla3Steps/ShopperGroupManagerJoomla3Steps.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/ShopperGroupManagerJoomla3Steps.php
@@ -8,15 +8,13 @@ namespace AcceptanceTester;
 
 class ShopperGroupManagerJoomla3Steps extends AdminManagerJoomla3Steps
 {
-    public function addShopperGroupsSave($shopperName, $shopperType, $shopperCustomer, $category, $shippingRate, $shippingCheckout, $catalog, $showPrice)
+
+    public function addShopperGroups($shopperName, $shopperType, $shopperCustomer, $category, $shippingRate, $shippingCheckout, $catalog, $showPrice, $function)
     {
         $I = $this;
         $I->amOnPage(\ShopperGroupJ3Page::$URL);
         $I->click(\ShopperGroupJ3Page::$newButton);
-        $I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URLNew);
-
         $I->fillField(\ShopperGroupJ3Page::$shopperName, $shopperName);
-
         $I->click(\ShopperGroupJ3Page::$shopperGroupType);
         $I->waitForElement(\ShopperGroupJ3Page::$shopperType);
         $I->fillField(\ShopperGroupJ3Page::$shopperType, $shopperType);
@@ -52,60 +50,117 @@ class ShopperGroupManagerJoomla3Steps extends AdminManagerJoomla3Steps
 
         $I->click(\ShopperGroupJ3Page::$quotationYes);
         $I->click(\ShopperGroupJ3Page::$publishYes);
+        switch ($function){
+            case 'save':
+                $I->click(\ShopperGroupJ3Page::$saveButton);
+                $I->see(\ShopperGroupJ3Page::$saveSuccess, \ShopperGroupJ3Page::$selectorSuccess);
+                break;
+            case 'saveclose':
+                $I->click(\ShopperGroupJ3Page::$saveCloseButton);
+                $I->see(\ShopperGroupJ3Page::$saveSuccess, \ShopperGroupJ3Page::$selectorSuccess);
+                $I->see(\ShopperGroupJ3Page::$namePageManagement, \ShopperGroupJ3Page::$selectorNamePage);
+                break;
+        }
 
-        $I->click(\ShopperGroupJ3Page::$saveButton);
-        $I->see(\ShopperGroupJ3Page::$saveSuccess, \ShopperGroupJ3Page::$selectorSuccess);
     }
 
-    public function addShopperGroupsSaveClose($shopperName, $shopperType, $shopperCustomer, $category, $shippingRate, $shippingCheckout, $catalog, $showPrice)
-    {
-        $I = $this;
-        $I->amOnPage(\ShopperGroupJ3Page::$URL);
-        $I->click(\ShopperGroupJ3Page::$newButton);
-        $I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URLNew);
-
-        $I->fillField(\ShopperGroupJ3Page::$shopperName, $shopperName);
-
-        $I->click(\ShopperGroupJ3Page::$shopperGroupType);
-        $I->waitForElement(\ShopperGroupJ3Page::$shopperType);
-        $I->fillField(\ShopperGroupJ3Page::$shopperType, $shopperType);
-        $userShopperPage = new \ShopperGroupJ3Page();
-        $I->waitForElement($userShopperPage->returnSearch($shopperType), 60);
-        $I->click($userShopperPage->returnSearch($shopperType));
-
-        $I->click(\ShopperGroupJ3Page::$customerType);
-        $I->waitForElement(\ShopperGroupJ3Page::$customerTypeSearch);
-        $I->fillField(\ShopperGroupJ3Page::$customerTypeSearch, $shopperCustomer);
-        $I->pressKey(\ShopperGroupJ3Page::$customerTypeSearch, \Facebook\WebDriver\WebDriverKeys::ARROW_DOWN, \Facebook\WebDriver\WebDriverKeys::ENTER);
-
-        $I->click(\ShopperGroupJ3Page::$shopperGroupPortalYes);
-
-        $I->click(\ShopperGroupJ3Page::$categoryFiled);
-        $I->fillField(\ShopperGroupJ3Page::$categoryFill, $category);
-        $I->pressKey(\ShopperGroupJ3Page::$categoryFill, \Facebook\WebDriver\WebDriverKeys::ARROW_DOWN, \Facebook\WebDriver\WebDriverKeys::ENTER);
-
-        $I->click(\ShopperGroupJ3Page::$shippingNo);
-        $I->fillField(\ShopperGroupJ3Page::$shippingRate, $shippingRate);
-        $I->fillField(\ShopperGroupJ3Page::$shippingCheckout, $shippingCheckout);
-        $I->click(\ShopperGroupJ3Page::$vatNo);
-
-        $I->click(\ShopperGroupJ3Page::$showPrice);
-        $I->waitForElement(\ShopperGroupJ3Page::$showPriceSearch);
-        $I->fillField(\ShopperGroupJ3Page::$showPriceSearch, $showPrice);
-        $I->pressKey(\ShopperGroupJ3Page::$showPriceSearch, \Facebook\WebDriver\WebDriverKeys::ARROW_DOWN, \Facebook\WebDriver\WebDriverKeys::ENTER);
-
-        $I->click(\ShopperGroupJ3Page::$catalogId);
-        $I->waitForElement(\ShopperGroupJ3Page::$catalogSearch);
-        $I->fillField(\ShopperGroupJ3Page::$catalogSearch, $catalog);
-        $I->pressKey(\ShopperGroupJ3Page::$catalogSearch, \Facebook\WebDriver\WebDriverKeys::ARROW_DOWN, \Facebook\WebDriver\WebDriverKeys::ENTER);
-
-        $I->click(\ShopperGroupJ3Page::$quotationYes);
-        $I->click(\ShopperGroupJ3Page::$publishYes);
-
-        $I->click(\ShopperGroupJ3Page::$saveCloseButton);
-        $I->see(\ShopperGroupJ3Page::$saveSuccess, \ShopperGroupJ3Page::$selectorSuccess);
-        $I->see(\ShopperGroupJ3Page::$namePageManagement, \ShopperGroupJ3Page::$selectorNamePage);
-    }
+//    public function addShopperGroupsSave($shopperName, $shopperType, $shopperCustomer, $category, $shippingRate, $shippingCheckout, $catalog, $showPrice)
+//    {
+//        $I = $this;
+//        $I->amOnPage(\ShopperGroupJ3Page::$URL);
+//        $I->click(\ShopperGroupJ3Page::$newButton);
+//        $I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URLNew);
+//
+//        $I->fillField(\ShopperGroupJ3Page::$shopperName, $shopperName);
+//        $I->click(\ShopperGroupJ3Page::$shopperGroupType);
+//        $I->waitForElement(\ShopperGroupJ3Page::$shopperType);
+//        $I->fillField(\ShopperGroupJ3Page::$shopperType, $shopperType);
+//        $userShopperPage = new \ShopperGroupJ3Page();
+//        $I->waitForElement($userShopperPage->returnSearch($shopperType), 60);
+//        $I->click($userShopperPage->returnSearch($shopperType));
+//
+//        $I->click(\ShopperGroupJ3Page::$customerType);
+//        $I->waitForElement(\ShopperGroupJ3Page::$customerTypeSearch);
+//        $I->fillField(\ShopperGroupJ3Page::$customerTypeSearch, $shopperCustomer);
+//        $I->pressKey(\ShopperGroupJ3Page::$customerTypeSearch, \Facebook\WebDriver\WebDriverKeys::ARROW_DOWN, \Facebook\WebDriver\WebDriverKeys::ENTER);
+//
+//        $I->click(\ShopperGroupJ3Page::$shopperGroupPortalYes);
+//
+//        $I->click(\ShopperGroupJ3Page::$categoryFiled);
+//        $I->fillField(\ShopperGroupJ3Page::$categoryFill, $category);
+//        $I->pressKey(\ShopperGroupJ3Page::$categoryFill, \Facebook\WebDriver\WebDriverKeys::ARROW_DOWN, \Facebook\WebDriver\WebDriverKeys::ENTER);
+//
+//        $I->click(\ShopperGroupJ3Page::$shippingNo);
+//        $I->fillField(\ShopperGroupJ3Page::$shippingRate, $shippingRate);
+//        $I->fillField(\ShopperGroupJ3Page::$shippingCheckout, $shippingCheckout);
+//        $I->click(\ShopperGroupJ3Page::$vatNo);
+//
+//        $I->click(\ShopperGroupJ3Page::$showPrice);
+//        $I->waitForElement(\ShopperGroupJ3Page::$showPriceSearch);
+//        $I->fillField(\ShopperGroupJ3Page::$showPriceSearch, $showPrice);
+//        $I->pressKey(\ShopperGroupJ3Page::$showPriceSearch, \Facebook\WebDriver\WebDriverKeys::ARROW_DOWN, \Facebook\WebDriver\WebDriverKeys::ENTER);
+//
+//        $I->click(\ShopperGroupJ3Page::$catalogId);
+//        $I->waitForElement(\ShopperGroupJ3Page::$catalogSearch);
+//        $I->fillField(\ShopperGroupJ3Page::$catalogSearch, $catalog);
+//        $I->pressKey(\ShopperGroupJ3Page::$catalogSearch, \Facebook\WebDriver\WebDriverKeys::ARROW_DOWN, \Facebook\WebDriver\WebDriverKeys::ENTER);
+//
+//        $I->click(\ShopperGroupJ3Page::$quotationYes);
+//        $I->click(\ShopperGroupJ3Page::$publishYes);
+//
+//        $I->click(\ShopperGroupJ3Page::$saveButton);
+//        $I->see(\ShopperGroupJ3Page::$saveSuccess, \ShopperGroupJ3Page::$selectorSuccess);
+//    }
+//
+//    public function addShopperGroupsSaveClose($shopperName, $shopperType, $shopperCustomer, $category, $shippingRate, $shippingCheckout, $catalog, $showPrice)
+//    {
+//        $I = $this;
+//        $I->amOnPage(\ShopperGroupJ3Page::$URL);
+//        $I->click(\ShopperGroupJ3Page::$newButton);
+//        $I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URLNew);
+//
+//        $I->fillField(\ShopperGroupJ3Page::$shopperName, $shopperName);
+//
+//        $I->click(\ShopperGroupJ3Page::$shopperGroupType);
+//        $I->waitForElement(\ShopperGroupJ3Page::$shopperType);
+//        $I->fillField(\ShopperGroupJ3Page::$shopperType, $shopperType);
+//        $userShopperPage = new \ShopperGroupJ3Page();
+//        $I->waitForElement($userShopperPage->returnSearch($shopperType), 60);
+//        $I->click($userShopperPage->returnSearch($shopperType));
+//
+//        $I->click(\ShopperGroupJ3Page::$customerType);
+//        $I->waitForElement(\ShopperGroupJ3Page::$customerTypeSearch);
+//        $I->fillField(\ShopperGroupJ3Page::$customerTypeSearch, $shopperCustomer);
+//        $I->pressKey(\ShopperGroupJ3Page::$customerTypeSearch, \Facebook\WebDriver\WebDriverKeys::ARROW_DOWN, \Facebook\WebDriver\WebDriverKeys::ENTER);
+//
+//        $I->click(\ShopperGroupJ3Page::$shopperGroupPortalYes);
+//
+//        $I->click(\ShopperGroupJ3Page::$categoryFiled);
+//        $I->fillField(\ShopperGroupJ3Page::$categoryFill, $category);
+//        $I->pressKey(\ShopperGroupJ3Page::$categoryFill, \Facebook\WebDriver\WebDriverKeys::ARROW_DOWN, \Facebook\WebDriver\WebDriverKeys::ENTER);
+//
+//        $I->click(\ShopperGroupJ3Page::$shippingNo);
+//        $I->fillField(\ShopperGroupJ3Page::$shippingRate, $shippingRate);
+//        $I->fillField(\ShopperGroupJ3Page::$shippingCheckout, $shippingCheckout);
+//        $I->click(\ShopperGroupJ3Page::$vatNo);
+//
+//        $I->click(\ShopperGroupJ3Page::$showPrice);
+//        $I->waitForElement(\ShopperGroupJ3Page::$showPriceSearch);
+//        $I->fillField(\ShopperGroupJ3Page::$showPriceSearch, $showPrice);
+//        $I->pressKey(\ShopperGroupJ3Page::$showPriceSearch, \Facebook\WebDriver\WebDriverKeys::ARROW_DOWN, \Facebook\WebDriver\WebDriverKeys::ENTER);
+//
+//        $I->click(\ShopperGroupJ3Page::$catalogId);
+//        $I->waitForElement(\ShopperGroupJ3Page::$catalogSearch);
+//        $I->fillField(\ShopperGroupJ3Page::$catalogSearch, $catalog);
+//        $I->pressKey(\ShopperGroupJ3Page::$catalogSearch, \Facebook\WebDriver\WebDriverKeys::ARROW_DOWN, \Facebook\WebDriver\WebDriverKeys::ENTER);
+//
+//        $I->click(\ShopperGroupJ3Page::$quotationYes);
+//        $I->click(\ShopperGroupJ3Page::$publishYes);
+//
+//        $I->click(\ShopperGroupJ3Page::$saveCloseButton);
+//        $I->see(\ShopperGroupJ3Page::$saveSuccess, \ShopperGroupJ3Page::$selectorSuccess);
+//        $I->see(\ShopperGroupJ3Page::$namePageManagement, \ShopperGroupJ3Page::$selectorNamePage);
+//    }
 
 
     public function deleteShopperGroupsYes()
@@ -128,6 +183,23 @@ class ShopperGroupManagerJoomla3Steps extends AdminManagerJoomla3Steps
         $I->wait(3);
     }
 
+    public function changeStateShopperGroup($status){
+        $I = $this;
+        $I->amOnPage(\ShopperGroupJ3Page::$URL);
+        $I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URL);
+        $I->click(\ShopperGroupJ3Page::$shopperFirstStatus);
+        $I->wait(3);
+        switch ($status){
+            case 'unpublished':
+                $currentState = $I->getShopperGroupsStates();
+                $I->verifyState('unpublished', $currentState);
+                break;
+            case 'published':
+                $currentState = $I->getShopperGroupsStates();
+                $I->verifyState('published', $currentState);
+                break;
+        }
+    }
     public function checkCloseButton($idShopperGroups)
     {
         $I = $this;
@@ -165,18 +237,6 @@ class ShopperGroupManagerJoomla3Steps extends AdminManagerJoomla3Steps
         $I->cancelPopup();
     }
 
-
-    public function addShopperGroupsCancel()
-    {
-        $I = $this;
-        $I->amOnPage(\ShopperGroupJ3Page::$URL);
-        $I->click(\ShopperGroupJ3Page::$newButton);
-        $I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URLNew);
-        $I->click(\ShopperGroupJ3Page::$cancelButton);
-        $I->see(\ShopperGroupJ3Page::$namePageManagement, \ShopperGroupJ3Page::$selectorNamePage);
-    }
-
-
     public function addShopperGroupsMissingName()
     {
         $I = $this;
@@ -187,61 +247,53 @@ class ShopperGroupManagerJoomla3Steps extends AdminManagerJoomla3Steps
         $I->acceptPopup();
     }
 
-    public function publishAllShopperGroups()
-    {
+
+    public function changStatusAllShopperGroups($status){
         $I = $this;
         $I->amOnPage(\ShopperGroupJ3Page::$URL);
         $I->checkAllResults();
-        $I->click(\ShopperGroupJ3Page::$publishButton);
-        $I->see(\ShopperGroupJ3Page::$publishSuccess, \ShopperGroupJ3Page::$xpathMessageSuccess);
+        switch ($status){
+            case 'publish':
+                $I->click(\ShopperGroupJ3Page::$publishButton);
+                $I->see(\ShopperGroupJ3Page::$publishSuccess, \ShopperGroupJ3Page::$xpathMessageSuccess);
+                break ;
+
+            case 'unpublish':
+                $I->click(\ShopperGroupJ3Page::$unpublishButton);
+                $I->see(\ShopperGroupJ3Page::$unpublishSuccess, \ShopperGroupJ3Page::$xpathMessageSuccess);
+                break;
+        }
         $I->see(\ShopperGroupJ3Page::$namePageManagement, \ShopperGroupJ3Page::$selectorNamePage);
     }
 
-    public function unpublishAllShopperGroups()
-    {
+
+    public function checkButtons($buttonName){
         $I = $this;
         $I->amOnPage(\ShopperGroupJ3Page::$URL);
-        $I->checkAllResults();
-        $I->click(\ShopperGroupJ3Page::$unpublishButton);
-        $I->see(\ShopperGroupJ3Page::$unpublishSuccess, \ShopperGroupJ3Page::$xpathMessageSuccess);
-        $I->see(\ShopperGroupJ3Page::$namePageManagement, \ShopperGroupJ3Page::$selectorNamePage);
-    }
-
-    public function checkEditButton()
-    {
-        $I = $this;
-        $I->amOnPage(\ShopperGroupJ3Page::$URL);
-        $I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URL);
-        $I->click(\ShopperGroupJ3Page::$editButton);
-        $I->acceptPopup();
-    }
-
-
-    public function checkDeleteButton()
-    {
-        $I = $this;
-        $I->amOnPage(\ShopperGroupJ3Page::$URL);
-        $I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URL);
-        $I->click(\ShopperGroupJ3Page::$deleteButton);
-        $I->acceptPopup();
-    }
-
-    public function checkPublishButton()
-    {
-        $I = $this;
-        $I->amOnPage(\ShopperGroupJ3Page::$URL);
-        $I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URL);
-        $I->click(\ShopperGroupJ3Page::$publishButton);
-        $I->acceptPopup();
-    }
-
-    public function checkUnPublishButton()
-    {
-        $I = $this;
-        $I->amOnPage(\ShopperGroupJ3Page::$URL);
-        $I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URL);
-        $I->click(\ShopperGroupJ3Page::$unpublishButton);
-        $I->acceptPopup();
+        switch ($buttonName){
+            case 'edit':
+                $I->click(\ShopperGroupJ3Page::$editButton);
+                $I->acceptPopup();
+                break;
+            case 'delete':
+                $I->click(\ShopperGroupJ3Page::$deleteButton);
+                $I->acceptPopup();
+                break;
+            case 'unpublish':
+                $I->click(\ShopperGroupJ3Page::$unpublishButton);
+                $I->acceptPopup();
+                break;
+            case 'publish':
+                $I->click(\ShopperGroupJ3Page::$publishButton);
+                $I->acceptPopup();
+                break;
+            case 'cancel':
+                $I->click(\ShopperGroupJ3Page::$newButton);
+                $I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URLNew);
+                $I->click(\ShopperGroupJ3Page::$cancelButton);
+                break;
+        }
+        $I->see(\ShopperGroupJ3Page::$namePageManagement, \ShopperGroupJ3Page::$pageHead);
     }
 
     public function deleteAllShopperGroups()
@@ -252,7 +304,8 @@ class ShopperGroupManagerJoomla3Steps extends AdminManagerJoomla3Steps
         $I->checkAllResults();
         $I->click(\ShopperGroupJ3Page::$deleteButton);
         $I->acceptPopup();
-        $I->see(\ShopperGroupJ3Page::$cannotDelete, \ShopperGroupJ3Page::$xpathMessageSuccess);
+
+        $I->waitForElement(\ShopperGroupJ3Page::$xpathMessageSuccess);
     }
 
     public function getShopperGroupsStates()
