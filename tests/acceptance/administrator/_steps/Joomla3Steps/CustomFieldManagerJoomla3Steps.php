387ec64cd49981df2649b6d7f27dd7b21991e100
nhung@redweb.dk <nhung@redweb.dk>
2017-07-27 19:23:11 +0700

    fix conflict

--- tests/acceptance/administrator/_steps/Joomla3Steps/CustomFieldManagerJoomla3Steps.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/CustomFieldManagerJoomla3Steps.php
@@ -5,7 +5,6 @@
  * @copyright   Copyright (C) 2008 - 2015 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 namespace AcceptanceTester;
 /**
  * Class CustomFieldManagerJoomla3Steps
@@ -21,47 +20,17 @@ class CustomFieldManagerJoomla3Steps extends AdminManagerJoomla3Steps
     /**
      * Function to add a new Custom Field
      *
-<<<<<<< HEAD
-     * @param   string $name Name of the new Field
-     * @param   string $title Title of the new Field
-     * @param   string $type Type of the new Field
-     * @param   string $section Section of the new Field
-     * @param   String $option Optional Field Value
-=======
      * @param   string  $name     Name of the new Field
      * @param   string  $title    Title of the new Field
      * @param   string  $type     Type of the new Field
      * @param   string  $section  Section of the new Field
      * @param   String  $option   Optional Field Value
->>>>>>> 8f4be64df0909c812eab0b76006c5b76716e5886
      *
      * @return void
      */
     public function addField($name = 'SampleField', $title = 'Field Title', $type = 'Text area', $section = 'Category', $option = 'Testing Options')
     {
         $I = $this;
-<<<<<<< HEAD
-        $I->amOnPage(\CustomFieldManagerJoomla3Page::$URL);
-        $I->checkForPhpNoticesOrWarnings(\CustomFieldManagerJoomla3Page::$URL);
-        $customFieldsManagerPage = new \CustomFieldManagerJoomla3Page;
-        $I->click('New');
-        $I->waitForElement(\CustomFieldManagerJoomla3Page::$fieldName, 30);
-        $I->fillField(\CustomFieldManagerJoomla3Page::$fieldName, $name);
-        $I->click(\CustomFieldManagerJoomla3Page::$fieldTypeDropDown);
-        $I->waitForElement(\CustomFieldManagerJoomla3Page::$fieldTypeSearch, 3);
-        $I->fillField(\CustomFieldManagerJoomla3Page::$fieldTypeSearch, $type);
-
-        $I->click($customFieldsManagerPage->xPathChoice($type));
-        $I->fillField(\CustomFieldManagerJoomla3Page::$fieldTitle, $title);
-
-        $I->click(\CustomFieldManagerJoomla3Page::$fieldSectionDropDown);
-        $I->waitForElement(\CustomFieldManagerJoomla3Page::$fieldSectionSearch, 3);
-        $I->fillField(\CustomFieldManagerJoomla3Page::$fieldSectionSearch, $section);
-        $I->click($customFieldsManagerPage->xPathChoice($section));
-        //        $I->click('Save');
-//        $I->wait(4);
-//        $I->see(\CustomFieldManagerJoomla3Page::$messageSaveSuccess, \CustomFieldManagerJoomla3Page::$selectorSuccess);
-=======
         $fieldsForOptions = array("Check box", "Image", "Image with link", "Multiple select box", );
         $I->amOnPage(\CustomFieldManagerJoomla3Page::$URL);
         $customFieldsManagerPage = new \CustomFieldManagerJoomla3Page;
@@ -93,19 +62,13 @@ class CustomFieldManagerJoomla3Steps extends AdminManagerJoomla3Steps
         $I->see($title, \CustomFieldManagerJoomla3Page::$firstResultRow);
         $I->executeJS('window.scrollTo(0,0)');
         $I->click(['link' => 'ID']);
->>>>>>> 8f4be64df0909c812eab0b76006c5b76716e5886
     }
 
     /**
      * Function to Edit a Field
      *
-<<<<<<< HEAD
-     * @param   string $title Current Title of the Field
-     * @param   string $updatedTitle New Title for the Field
-=======
      * @param   string  $title         Current Title of the Field
      * @param   string  $updatedTitle  New Title for the Field
->>>>>>> 8f4be64df0909c812eab0b76006c5b76716e5886
      *
      * @return void
      */
@@ -117,17 +80,10 @@ class CustomFieldManagerJoomla3Steps extends AdminManagerJoomla3Steps
         $I->see($title, \CustomFieldManagerJoomla3Page::$firstResultRow);
         $I->click(\CustomFieldManagerJoomla3Page::$selectFirst);
         $I->click('Edit');
-<<<<<<< HEAD
-        $I->waitForElement(\CustomFieldManagerJoomla3Page::$fieldName, 30);
-        $I->fillField(\CustomFieldManagerJoomla3Page::$fieldTitle, $updatedTitle);
-        $I->click('Save & Close');
-        $I->waitForText(\CustomFieldManagerJoomla3Page::$fieldSuccessMessage, 10, \CustomFieldManagerJoomla3Page::$fieldMessagesLocation);
-=======
         $I->waitForElement(\CustomFieldManagerJoomla3Page::$fieldName,30);
         $I->fillField(\CustomFieldManagerJoomla3Page::$fieldTitle, $updatedTitle);
         $I->click('Save & Close');
         $I->waitForText(\CustomFieldManagerJoomla3Page::$fieldSuccessMessage,10,\CustomFieldManagerJoomla3Page::$fieldMessagesLocation);
->>>>>>> 8f4be64df0909c812eab0b76006c5b76716e5886
         $I->see($updatedTitle, \CustomFieldManagerJoomla3Page::$firstResultRow);
         $I->executeJS('window.scrollTo(0,0)');
         $I->click(['link' => 'ID']);
@@ -136,13 +92,8 @@ class CustomFieldManagerJoomla3Steps extends AdminManagerJoomla3Steps
     /**
      * Function to change State of a Custom Field
      *
-<<<<<<< HEAD
-     * @param   string $title Title of the Custom Field
-     * @param   string $state State of the Mail Template
-=======
      * @param   string  $title  Title of the Custom Field
      * @param   string  $state  State of the Mail Template
->>>>>>> 8f4be64df0909c812eab0b76006c5b76716e5886
      *
      * @return void
      */
@@ -154,13 +105,8 @@ class CustomFieldManagerJoomla3Steps extends AdminManagerJoomla3Steps
     /**
      * Function to Search for a Custom Field
      *
-<<<<<<< HEAD
-     * @param   string $title Title of the Field
-     * @param   string $functionName Name of the function After Which search is being Called
-=======
      * @param   string  $title         Title of the Field
      * @param   string  $functionName  Name of the function After Which search is being Called
->>>>>>> 8f4be64df0909c812eab0b76006c5b76716e5886
      *
      * @return void
      */
@@ -172,11 +118,7 @@ class CustomFieldManagerJoomla3Steps extends AdminManagerJoomla3Steps
     /**
      * Function to get State of the Custom Field
      *
-<<<<<<< HEAD
-     * @param   String $title Title of the Custom Field
-=======
      * @param   String  $title  Title of the Custom Field
->>>>>>> 8f4be64df0909c812eab0b76006c5b76716e5886
      *
      * @return string
      */
@@ -190,11 +132,7 @@ class CustomFieldManagerJoomla3Steps extends AdminManagerJoomla3Steps
     /**
      * Function to Delete Custom Field
      *
-<<<<<<< HEAD
-     * @param   String $title Title of the Field which is to be Deleted
-=======
      * @param   String  $title  Title of the Field which is to be Deleted
->>>>>>> 8f4be64df0909c812eab0b76006c5b76716e5886
      *
      * @return void
      */
