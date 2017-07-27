a35be94abe1ee33e3e1d40fd1c91c329b912e5ac
nhung@redweb.dk <nhung@redweb.dk>
2017-07-27 19:11:18 +0700

    update Manufactue redsHOP 1.6

--- tests/_pages/Joomla3/Administrator/ManufacturerManagerJoomla3Page.php
+++ tests/_pages/Joomla3/Administrator/ManufacturerManagerJoomla3Page.php
@@ -27,5 +27,41 @@ class ManufacturerManagerJoomla3Page
 
 	public static $selectFirst = "//input[@id='cb0']";
 
-	public static $manufacturerStatePath = "//div[@id='editcell']//table[2]//tbody/tr[1]/td[7]/a";
+	public static $manufacturerStatePath = "//div[@id='editcell']/div[2]/table/tbody/tr/td[5]/a";
+
+	public static $xpathName="//div[@id='editcell']/div[2]/table/tbody/tr/td[2]/a";
+
+
+	public static $productPerPage="//input[@id='product_per_page']";
+
+    //button
+
+    public static $newButton = "New";
+
+    public static $saveButton = "Save";
+
+    public static $unpublishButton = "Unpublish";
+
+    public static $publishButton = "Publish";
+
+    public static $saveCloseButton = "Save & Close";
+
+    public static $deleteButton = "Delete";
+
+    public static $editButton = "Edit";
+
+    public static $saveNewButton = "Save & New";
+
+    public static $cancelButton = "Cancel";
+
+    public static $checkInButton = "Check-in";
+
+    //selector
+
+    public static $selectorSuccess = '.alert-success';
+
+    public static $selectorError = '.alert-danger';
+
+    public static $selectorNamePage = '.page-title';
+
 }
