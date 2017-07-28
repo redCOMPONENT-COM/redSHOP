0a4bcc989b666342b6a2f832f7831859b05f46b8
nhung@redweb.dk <nhung@redweb.dk>
2017-07-28 11:06:19 +0700

    customer field

--- tests/_pages/Joomla3/Administrator/CustomFieldManagerJoomla3Page.php
+++ tests/_pages/Joomla3/Administrator/CustomFieldManagerJoomla3Page.php
@@ -14,22 +14,31 @@
  * @since  1.4
  */
 class CustomFieldManagerJoomla3Page
-{
-    public static $URL = '/administrator/index.php?option=com_redshop&view=fields';
+{ public static $URL = '/administrator/index.php?option=com_redshop&view=fields';
+
+    public static $URLNew = '';
 
     public static $fieldName = "//input[@id='jform_name']";
 
     public static $fieldTitle = "//input[@id='jform_title']";
 
-    public static $fieldTypeDropDown = "//div[@id='jform_type_chzn']/a";
+
+    public static $fieldTypeDropDown = ['id' => 's2id_jform_type'];
+
+    public static $fieldTypeSearch = ['id' => 's2id_autogen1_search'];
+
+    public static $fieldSectionDropDown = ['id' => 's2id_jform_section'];
+
+    public static $fieldSectionSearch = ['id' => 's2id_autogen2_search'];
+
+    public static $filterSearch = ['id' => 'filter_search'];
+
 
     public static $fieldTypeSearchField = "//div[@id='jform_type_chzn']/div/div/input";
 
-    public static $fieldSectionDropDown = "//div[@id='jform_section_chzn']/a";
-
     public static $fieldSectionSearchField = "//div[@id='jform_section_chzn']/div/div/input";
 
-    public static $fieldSuccessMessage = 'Field details saved';
+    public static $fieldSuccessMessage = 'Item saved.';
 
     public static $fieldMessagesLocation = "//div[@id='system-message-container']/div/p";
 
@@ -41,10 +50,24 @@ class CustomFieldManagerJoomla3Page
 
     public static $optionValueField = "//input[@name='extra_value[]']";
 
+    //message
+    public static $messageSaveSuccess='Item saved.';
+
+
+    //selector
+
+    public static $selectorSuccess = '.alert-success';
+
+    public static $selectorError = '.alert-danger';
+
+    public static $selectorNamePage = '.page-title';
+
+
+
     /**
      * Function to get the path for Field Type
      *
-     * @param   String  $type  Type of Field to be Added
+     * @param   String $type Type of Field to be Added
      *
      * @return string
      */
@@ -58,7 +81,7 @@ class CustomFieldManagerJoomla3Page
     /**
      * Function to get the path for Field Section
      *
-     * @param   String  $section  Section of Field to be Added
+     * @param   String $section Section of Field to be Added
      *
      * @return string
      */
@@ -68,4 +91,34 @@ class CustomFieldManagerJoomla3Page
 
         return $path;
     }
+
+    public function xPathChoice($typeChoice)
+    {
+        $path = ['xpath' => "//span[contains(text(), '" . $typeChoice . "')]"];
+        return $path;
+    }
+
+
+    //button
+
+    public static $newButton = "New";
+
+    public static $saveButton = "Save";
+
+    public static $unpublishButton = "Unpublish";
+
+    public static $publishButton = "Publish";
+
+    public static $saveCloseButton = "Save & Close";
+
+    public static $deleteButton = "Delete";
+
+    public static $editButton = "Edit";
+
+    public static $saveNewButton = "Save & New";
+
+    public static $cancelButton = "Cancel";
+
+    public static $checkInButton = "Check-in";
+
 }
