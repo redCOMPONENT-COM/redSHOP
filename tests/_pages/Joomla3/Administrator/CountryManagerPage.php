12d0b0b9d8e167af2140ca68e7cda5820e7dfa17
thongredweb <thong@redweb.dk>
2017-07-29 23:55:30 +0700

    [Improve] Country test case.

--- tests/_pages/Joomla3/Administrator/CountryManagerPage.php
+++ tests/_pages/Joomla3/Administrator/CountryManagerPage.php
@@ -18,14 +18,13 @@ class CountryManagerPage
 	public static $namePage = 'Country Management';
 	public static $headPage = ['xpath' => "//h1"];
 
+	public static $searchField = ['id' => 'filter_search'];
+
 	public static $URL = '/administrator/index.php?option=com_redshop&view=countries';
 
 	public static $countryName = "#jform_country_name";
-
 	public static $countryTwoCode = "#jform_country_2_code";
-
 	public static $countryThreeCode = "#jform_country_3_code";
-
 	public static $country = "#jform_country_jtext";
 
 	public static $countryCheck = "//input[@id='cb0']";
@@ -33,10 +32,11 @@ class CountryManagerPage
 	public static $countryResultRow = "//table[contains(@class, 'adminlist')]/tbody/tr[1]";
 
 	public static $newButton = "New";
-
 	public static $saveCloseButton = "Save & Close";
+	public static $deleteButton = "Delete";
 
 	public static $itemSaveSuccessMessage = "Item saved.";
+	public static $messageDeletedOneSuccess = "1 item successfully deleted";
 
 	public static $selectorSuccess = ".alert-success";
 }
