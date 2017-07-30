06a1527b18b00dee4df3a473bdceef9a160bf444
thongredweb <thong@redweb.dk>
2017-07-30 14:14:40 +0700

    [Improve] Voucher test case.

--- tests/_pages/Joomla3/Administrator/VoucherManagerPage.php
+++ tests/_pages/Joomla3/Administrator/VoucherManagerPage.php
@@ -39,7 +39,7 @@ class VoucherManagerPage
 
     public static $voucherStatePath = "//div[@class='table-responsive']/table/tbody/tr/td[10]/a";
 
-    public static $voucherId = "//div[@class='table-responsive']/tbody/tr/td[11]/a";
+    public static $voucherId = "//div[@class='table-responsive']/tbody/tr/td[11]";
 
     public static $voucherSearchField = "#filter_search";
 
