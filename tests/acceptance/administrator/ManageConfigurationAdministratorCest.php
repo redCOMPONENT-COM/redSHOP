cc33037c5640391e100fa7a17dab80d1f4dbb59b
nhung@redweb.dk <nhung@redweb.dk>
2017-08-02 18:28:02 +0700

    fix conflict

--- tests/acceptance/administrator/ManageConfigurationAdministratorCest.php
+++ tests/acceptance/administrator/ManageConfigurationAdministratorCest.php
@@ -8,88 +8,86 @@
  */
 class ManageConfigurationAdministratorCest
 {
-    public function featureUsedStockRoom(AcceptanceTester $I, $scenario)
-    {
-        $I->wantTo('Test used Stockroom  in Administrator');
-        $I->doAdministratorLogin();
-        $I = new AcceptanceTester\ConfigurationManageJoomla3Steps($scenario);
-        $I->wantTo('Start stook room ');
-        $I->featureUsedStockRoom();
-        $I->see("Configuration", '.page-title');
-    }
+	public function featureUsedStockRoom(AcceptanceTester $I, $scenario)
+	{
+		$I->wantTo('Test used Stockroom  in Administrator');
+		$I->doAdministratorLogin();
+		$I = new AcceptanceTester\ConfigurationManageJoomla3Steps($scenario);
+		$I->wantTo('Start stook room ');
+		$I->featureUsedStockRoom();
+		$I->see("Configuration", '.page-title');
+	}
 
-    public function featureStockRoomNo(AcceptanceTester $I, $scenario)
-    {
-        $I->wantTo('Test used Stockroom  in Administrator');
-        $I->doAdministratorLogin();
-        $I = new AcceptanceTester\ConfigurationManageJoomla3Steps($scenario);
-        $I->wantTo('Off stook room ');
-        $I->featureOffStockRoom();
-        $I->see("Configuration", '.page-title');
-    }
-
-
-
-    public function featureEditInLineYes(AcceptanceTester $I, $scenario)
-    {
-        $I->wantTo('Test Edit inline is yes  in Administrator');
-        $I->doAdministratorLogin();
-        $I = new AcceptanceTester\ConfigurationManageJoomla3Steps($scenario);
-        $I->wantTo(' Edit inline is yes ');
-        $I->featureEditInLineYes();
-        $I->see("Configuration", '.page-title');
-    }
-
-    public function featureEditInLineNo(AcceptanceTester $I, $scenario)
-    {
-        $I->wantTo('Test Edit inline is yes  in Administrator');
-        $I->doAdministratorLogin();
-        $I = new AcceptanceTester\ConfigurationManageJoomla3Steps($scenario);
-        $I->wantTo(' Edit inline is yes ');
-        $I->featureEditInLineNo();
-        $I->see("Configuration", '.page-title');
-    }
-
-    public function featureComparisonYes(AcceptanceTester $I, $scenario)
-    {
-        $I->wantTo('Test Comparison is yes  in Administrator');
-        $I->doAdministratorLogin();
-        $I = new AcceptanceTester\ConfigurationManageJoomla3Steps($scenario);
-        $I->wantTo(' Edit inline is yes ');
-        $I->featureComparisonYes();
-        $I->see("Configuration", '.page-title');
-    }
-
-    public function featureComparisonNo(AcceptanceTester $I, $scenario)
-    {
-        $I->wantTo('Test Comparison is No  in Administrator');
-        $I->doAdministratorLogin();
-        $I = new AcceptanceTester\ConfigurationManageJoomla3Steps($scenario);
-        $I->wantTo(' Edit inline is No ');
-        $I->featureComparisonNo();
-        $I->see("Configuration", '.page-title');
-    }
+	public function featureStockRoomNo(AcceptanceTester $I, $scenario)
+	{
+		$I->wantTo('Test used Stockroom  in Administrator');
+		$I->doAdministratorLogin();
+		$I = new AcceptanceTester\ConfigurationManageJoomla3Steps($scenario);
+		$I->wantTo('Off stook room ');
+		$I->featureOffStockRoom();
+		$I->see("Configuration", '.page-title');
+	}
 
 
-    public function featurePriceNo(AcceptanceTester $I, $scenario)
-    {
-        $I->wantTo('Show Price is No  in Administrator');
-        $I->doAdministratorLogin();
-        $I = new AcceptanceTester\ConfigurationManageJoomla3Steps($scenario);
-        $I->wantTo(' Edit inline is No ');
-        $I->featurePriceNo();
-        $I->see("Configuration", '.page-title');
-    }
+	public function featureEditInLineYes(AcceptanceTester $I, $scenario)
+	{
+		$I->wantTo('Test Edit inline is yes  in Administrator');
+		$I->doAdministratorLogin();
+		$I = new AcceptanceTester\ConfigurationManageJoomla3Steps($scenario);
+		$I->wantTo(' Edit inline is yes ');
+		$I->featureEditInLineYes();
+		$I->see("Configuration", '.page-title');
+	}
+
+	public function featureEditInLineNo(AcceptanceTester $I, $scenario)
+	{
+		$I->wantTo('Test Edit inline is yes  in Administrator');
+		$I->doAdministratorLogin();
+		$I = new AcceptanceTester\ConfigurationManageJoomla3Steps($scenario);
+		$I->wantTo(' Edit inline is yes ');
+		$I->featureEditInLineNo();
+		$I->see("Configuration", '.page-title');
+	}
+
+	public function featureComparisonYes(AcceptanceTester $I, $scenario)
+	{
+		$I->wantTo('Test Comparison is yes  in Administrator');
+		$I->doAdministratorLogin();
+		$I = new AcceptanceTester\ConfigurationManageJoomla3Steps($scenario);
+		$I->wantTo(' Edit inline is yes ');
+		$I->featureComparisonYes();
+		$I->see("Configuration", '.page-title');
+	}
+
+	public function featureComparisonNo(AcceptanceTester $I, $scenario)
+	{
+		$I->wantTo('Test Comparison is No  in Administrator');
+		$I->doAdministratorLogin();
+		$I = new AcceptanceTester\ConfigurationManageJoomla3Steps($scenario);
+		$I->wantTo(' Edit inline is No ');
+		$I->featureComparisonNo();
+		$I->see("Configuration", '.page-title');
+	}
 
 
-    public function featurePriceYes(AcceptanceTester $I, $scenario)
-    {
-        $I->wantTo('Show Price is Yes  in Administrator');
-        $I->doAdministratorLogin();
-        $I = new AcceptanceTester\ConfigurationManageJoomla3Steps($scenario);
-        $I->wantTo(' Edit inline is yes ');
-        $I->featurePriceYes();
-        $I->see("Configuration", '.page-title');
-    }
+	public function featurePriceNo(AcceptanceTester $I, $scenario)
+	{
+		$I->wantTo('Show Price is No  in Administrator');
+		$I->doAdministratorLogin();
+		$I = new AcceptanceTester\ConfigurationManageJoomla3Steps($scenario);
+		$I->wantTo(' Edit inline is No ');
+		$I->featurePriceNo();
+		$I->see("Configuration", '.page-title');
+	}
 
+
+	public function featurePriceYes(AcceptanceTester $I, $scenario)
+	{
+		$I->wantTo('Show Price is Yes  in Administrator');
+		$I->doAdministratorLogin();
+		$I = new AcceptanceTester\ConfigurationManageJoomla3Steps($scenario);
+		$I->wantTo(' Edit inline is yes ');
+		$I->featurePriceYes();
+		$I->see("Configuration", '.page-title');
+	}
 }
--- tests/acceptance/administrator/_steps/Joomla3Steps/ShopperGroupManagerJoomla3Steps.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/ShopperGroupManagerJoomla3Steps.php
@@ -9,212 +9,212 @@ namespace AcceptanceTester;
 class ShopperGroupManagerJoomla3Steps extends AdminManagerJoomla3Steps
 {
 
-    public function addShopperGroups($shopperName, $shopperType, $shopperCustomer, $category, $shippingRate, $shippingCheckout, $catalog, $showPrice, $function)
-    {
-        $I = $this;
-        $I->amOnPage(\ShopperGroupJ3Page::$URL);
-        $I->click(\ShopperGroupJ3Page::$newButton);
-        $I->fillField(\ShopperGroupJ3Page::$shopperName, $shopperName);
-        $I->click(\ShopperGroupJ3Page::$shopperGroupType);
-        $I->waitForElement(\ShopperGroupJ3Page::$shopperType);
-        $I->fillField(\ShopperGroupJ3Page::$shopperType, $shopperType);
-        $userShopperPage = new \ShopperGroupJ3Page();
-        $I->waitForElement($userShopperPage->returnSearch($shopperType), 60);
-        $I->click($userShopperPage->returnSearch($shopperType));
+	public function addShopperGroups($shopperName, $shopperType, $shopperCustomer, $category, $shippingRate, $shippingCheckout, $catalog, $showPrice, $function)
+	{
+		$I = $this;
+		$I->amOnPage(\ShopperGroupJ3Page::$URL);
+		$I->click(\ShopperGroupJ3Page::$newButton);
+		$I->fillField(\ShopperGroupJ3Page::$shopperName, $shopperName);
+		$I->click(\ShopperGroupJ3Page::$shopperGroupType);
+		$I->waitForElement(\ShopperGroupJ3Page::$shopperType);
+		$I->fillField(\ShopperGroupJ3Page::$shopperType, $shopperType);
+		$userShopperPage = new \ShopperGroupJ3Page();
+		$I->waitForElement($userShopperPage->returnSearch($shopperType), 60);
+		$I->click($userShopperPage->returnSearch($shopperType));
 
-        $I->click(\ShopperGroupJ3Page::$customerType);
-        $I->waitForElement(\ShopperGroupJ3Page::$customerTypeSearch);
-        $I->fillField(\ShopperGroupJ3Page::$customerTypeSearch, $shopperCustomer);
-        $I->pressKey(\ShopperGroupJ3Page::$customerTypeSearch, \Facebook\WebDriver\WebDriverKeys::ARROW_DOWN, \Facebook\WebDriver\WebDriverKeys::ENTER);
+		$I->click(\ShopperGroupJ3Page::$customerType);
+		$I->waitForElement(\ShopperGroupJ3Page::$customerTypeSearch);
+		$I->fillField(\ShopperGroupJ3Page::$customerTypeSearch, $shopperCustomer);
+		$I->pressKey(\ShopperGroupJ3Page::$customerTypeSearch, \Facebook\WebDriver\WebDriverKeys::ARROW_DOWN, \Facebook\WebDriver\WebDriverKeys::ENTER);
 
-        $I->click(\ShopperGroupJ3Page::$shopperGroupPortalYes);
+		$I->click(\ShopperGroupJ3Page::$shopperGroupPortalYes);
 
-        $I->click(\ShopperGroupJ3Page::$categoryFiled);
-        $I->fillField(\ShopperGroupJ3Page::$categoryFill, $category);
-        $I->pressKey(\ShopperGroupJ3Page::$categoryFill, \Facebook\WebDriver\WebDriverKeys::ARROW_DOWN, \Facebook\WebDriver\WebDriverKeys::ENTER);
+		$I->click(\ShopperGroupJ3Page::$categoryFiled);
+		$I->fillField(\ShopperGroupJ3Page::$categoryFill, $category);
+		$I->pressKey(\ShopperGroupJ3Page::$categoryFill, \Facebook\WebDriver\WebDriverKeys::ARROW_DOWN, \Facebook\WebDriver\WebDriverKeys::ENTER);
 
-        $I->click(\ShopperGroupJ3Page::$shippingNo);
-        $I->fillField(\ShopperGroupJ3Page::$shippingRate, $shippingRate);
-        $I->fillField(\ShopperGroupJ3Page::$shippingCheckout, $shippingCheckout);
-        $I->click(\ShopperGroupJ3Page::$vatNo);
+		$I->click(\ShopperGroupJ3Page::$shippingNo);
+		$I->fillField(\ShopperGroupJ3Page::$shippingRate, $shippingRate);
+		$I->fillField(\ShopperGroupJ3Page::$shippingCheckout, $shippingCheckout);
+		$I->click(\ShopperGroupJ3Page::$vatNo);
 
-        $I->click(\ShopperGroupJ3Page::$showPrice);
-        $I->waitForElement(\ShopperGroupJ3Page::$showPriceSearch);
-        $I->fillField(\ShopperGroupJ3Page::$showPriceSearch, $showPrice);
-        $I->pressKey(\ShopperGroupJ3Page::$showPriceSearch, \Facebook\WebDriver\WebDriverKeys::ARROW_DOWN, \Facebook\WebDriver\WebDriverKeys::ENTER);
+		$I->click(\ShopperGroupJ3Page::$showPrice);
+		$I->waitForElement(\ShopperGroupJ3Page::$showPriceSearch);
+		$I->fillField(\ShopperGroupJ3Page::$showPriceSearch, $showPrice);
+		$I->pressKey(\ShopperGroupJ3Page::$showPriceSearch, \Facebook\WebDriver\WebDriverKeys::ARROW_DOWN, \Facebook\WebDriver\WebDriverKeys::ENTER);
 
-        $I->click(\ShopperGroupJ3Page::$catalogId);
-        $I->waitForElement(\ShopperGroupJ3Page::$catalogSearch);
-        $I->fillField(\ShopperGroupJ3Page::$catalogSearch, $catalog);
-        $I->pressKey(\ShopperGroupJ3Page::$catalogSearch, \Facebook\WebDriver\WebDriverKeys::ARROW_DOWN, \Facebook\WebDriver\WebDriverKeys::ENTER);
+		$I->click(\ShopperGroupJ3Page::$catalogId);
+		$I->waitForElement(\ShopperGroupJ3Page::$catalogSearch);
+		$I->fillField(\ShopperGroupJ3Page::$catalogSearch, $catalog);
+		$I->pressKey(\ShopperGroupJ3Page::$catalogSearch, \Facebook\WebDriver\WebDriverKeys::ARROW_DOWN, \Facebook\WebDriver\WebDriverKeys::ENTER);
 
-        $I->click(\ShopperGroupJ3Page::$quotationYes);
-        $I->click(\ShopperGroupJ3Page::$publishYes);
-        switch ($function) {
-            case 'save':
-                $I->click(\ShopperGroupJ3Page::$saveButton);
-                $I->see(\ShopperGroupJ3Page::$saveSuccess, \ShopperGroupJ3Page::$selectorSuccess);
-                break;
-            case 'saveclose':
-                $I->click(\ShopperGroupJ3Page::$saveCloseButton);
-                $I->see(\ShopperGroupJ3Page::$saveSuccess, \ShopperGroupJ3Page::$selectorSuccess);
-                $I->see(\ShopperGroupJ3Page::$namePageManagement, \ShopperGroupJ3Page::$selectorNamePage);
-                break;
-        }
+		$I->click(\ShopperGroupJ3Page::$quotationYes);
+		$I->click(\ShopperGroupJ3Page::$publishYes);
+		switch ($function) {
+			case 'save':
+				$I->click(\ShopperGroupJ3Page::$saveButton);
+				$I->see(\ShopperGroupJ3Page::$saveSuccess, \ShopperGroupJ3Page::$selectorSuccess);
+				break;
+			case 'saveclose':
+				$I->click(\ShopperGroupJ3Page::$saveCloseButton);
+				$I->see(\ShopperGroupJ3Page::$saveSuccess, \ShopperGroupJ3Page::$selectorSuccess);
+				$I->see(\ShopperGroupJ3Page::$namePageManagement, \ShopperGroupJ3Page::$selectorNamePage);
+				break;
+		}
 
-    }
+	}
 
-    public function changeStateShopperGroups()
-    {
-        $I = $this;
-        $I->amOnPage(\ShopperGroupJ3Page::$URL);
-        $I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URL);
-        $I->click(\ShopperGroupJ3Page::$shopperFirstStatus);
-        $I->wait(3);
-    }
+	public function changeStateShopperGroups()
+	{
+		$I = $this;
+		$I->amOnPage(\ShopperGroupJ3Page::$URL);
+		$I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URL);
+		$I->click(\ShopperGroupJ3Page::$shopperFirstStatus);
+		$I->wait(3);
+	}
 
-    public function changeStateShopperGroup($status)
-    {
-        $I = $this;
-        $I->amOnPage(\ShopperGroupJ3Page::$URL);
-        $I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URL);
-        $I->click(\ShopperGroupJ3Page::$shopperFirstStatus);
-        $I->wait(3);
-        switch ($status) {
-            case 'unpublished':
-                $currentState = $I->getShopperGroupsStates();
-                $I->verifyState('unpublished', $currentState);
-                break;
-            case 'published':
-                $currentState = $I->getShopperGroupsStates();
-                $I->verifyState('published', $currentState);
-                break;
-        }
-    }
+	public function changeStateShopperGroup($status)
+	{
+		$I = $this;
+		$I->amOnPage(\ShopperGroupJ3Page::$URL);
+		$I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URL);
+		$I->click(\ShopperGroupJ3Page::$shopperFirstStatus);
+		$I->wait(3);
+		switch ($status) {
+			case 'unpublished':
+				$currentState = $I->getShopperGroupsStates();
+				$I->verifyState('unpublished', $currentState);
+				break;
+			case 'published':
+				$currentState = $I->getShopperGroupsStates();
+				$I->verifyState('published', $currentState);
+				break;
+		}
+	}
 
-    public function checkCloseButton($idShopperGroups)
-    {
-        $I = $this;
-        $I->amOnPage(\ShopperGroupJ3Page::$URL);
-        $I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URL);
-        $I->click(\ShopperGroupJ3Page::$nameShopperGroupsFirst);
-        $URLEdit = \ShopperGroupJ3Page::$URLEdit . $idShopperGroups;
-        $I->amOnPage($URLEdit);
-        $I->checkForPhpNoticesOrWarnings($URLEdit);
-        $I->click(\ShopperGroupJ3Page::$closeButton);
-        $I->see(\ShopperGroupJ3Page::$namePageManagement, \ShopperGroupJ3Page::$selectorNamePage);
-    }
+	public function checkCloseButton($idShopperGroups)
+	{
+		$I = $this;
+		$I->amOnPage(\ShopperGroupJ3Page::$URL);
+		$I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URL);
+		$I->click(\ShopperGroupJ3Page::$nameShopperGroupsFirst);
+		$URLEdit = \ShopperGroupJ3Page::$URLEdit . $idShopperGroups;
+		$I->amOnPage($URLEdit);
+		$I->checkForPhpNoticesOrWarnings($URLEdit);
+		$I->click(\ShopperGroupJ3Page::$closeButton);
+		$I->see(\ShopperGroupJ3Page::$namePageManagement, \ShopperGroupJ3Page::$selectorNamePage);
+	}
 
-    public function editShopperGroups($nameShopperGroups, $idShopperGroups, $nameEdit)
-    {
-        $I = $this;
-        $I->amOnPage(\ShopperGroupJ3Page::$URL);
-        $I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URL);
-        $I->click(\ShopperGroupJ3Page::$nameShopperGroupsFirst);
-        $URLEdit = \ShopperGroupJ3Page::$URLEdit . $idShopperGroups;
-        $I->amOnPage($URLEdit);
-        $I->checkForPhpNoticesOrWarnings($URLEdit);
-        $I->fillField(\ShopperGroupJ3Page::$shopperName, $nameEdit);
-        $I->click(\ShopperGroupJ3Page::$saveButton);
-        $I->see(\ShopperGroupJ3Page::$saveSuccess, \ShopperGroupJ3Page::$selectorSuccess);
-    }
+	public function editShopperGroups($nameShopperGroups, $idShopperGroups, $nameEdit)
+	{
+		$I = $this;
+		$I->amOnPage(\ShopperGroupJ3Page::$URL);
+		$I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URL);
+		$I->click(\ShopperGroupJ3Page::$nameShopperGroupsFirst);
+		$URLEdit = \ShopperGroupJ3Page::$URLEdit . $idShopperGroups;
+		$I->amOnPage($URLEdit);
+		$I->checkForPhpNoticesOrWarnings($URLEdit);
+		$I->fillField(\ShopperGroupJ3Page::$shopperName, $nameEdit);
+		$I->click(\ShopperGroupJ3Page::$saveButton);
+		$I->see(\ShopperGroupJ3Page::$saveSuccess, \ShopperGroupJ3Page::$selectorSuccess);
+	}
 
-    public function deleteShopperGroupsNo()
-    {
-        $I = $this;
-        $I->amOnPage(\ShopperGroupJ3Page::$URL);
-        $I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URL);
-        $I->click(\ShopperGroupJ3Page::$shopperFirst);
-        $I->click(\ShopperGroupJ3Page::$deleteButton);
-        $I->cancelPopup();
-    }
+	public function deleteShopperGroupsNo()
+	{
+		$I = $this;
+		$I->amOnPage(\ShopperGroupJ3Page::$URL);
+		$I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URL);
+		$I->click(\ShopperGroupJ3Page::$shopperFirst);
+		$I->click(\ShopperGroupJ3Page::$deleteButton);
+		$I->cancelPopup();
+	}
 
-    public function addShopperGroupsMissingName()
-    {
-        $I = $this;
-        $I->amOnPage(\ShopperGroupJ3Page::$URL);
-        $I->click(\ShopperGroupJ3Page::$newButton);
-        $I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URLNew);
-        $I->click(\ShopperGroupJ3Page::$saveCloseButton);
-        $I->acceptPopup();
-    }
-
-
-    public function changStatusAllShopperGroups($status)
-    {
-        $I = $this;
-        $I->amOnPage(\ShopperGroupJ3Page::$URL);
-        $I->checkAllResults();
-        switch ($status) {
-            case 'publish':
-                $I->click(\ShopperGroupJ3Page::$publishButton);
-                $I->see(\ShopperGroupJ3Page::$publishSuccess, \ShopperGroupJ3Page::$xpathMessageSuccess);
-                break;
-
-            case 'unpublish':
-                $I->click(\ShopperGroupJ3Page::$unpublishButton);
-                $I->see(\ShopperGroupJ3Page::$unpublishSuccess, \ShopperGroupJ3Page::$xpathMessageSuccess);
-                break;
-        }
-        $I->see(\ShopperGroupJ3Page::$namePageManagement, \ShopperGroupJ3Page::$selectorNamePage);
-    }
+	public function addShopperGroupsMissingName()
+	{
+		$I = $this;
+		$I->amOnPage(\ShopperGroupJ3Page::$URL);
+		$I->click(\ShopperGroupJ3Page::$newButton);
+		$I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URLNew);
+		$I->click(\ShopperGroupJ3Page::$saveCloseButton);
+		$I->acceptPopup();
+	}
 
 
-    public function checkButtons($buttonName)
-    {
-        $I = $this;
-        $I->amOnPage(\ShopperGroupJ3Page::$URL);
-        switch ($buttonName) {
-            case 'edit':
-                $I->click(\ShopperGroupJ3Page::$editButton);
-                $I->acceptPopup();
-                break;
-            case 'delete':
-                $I->click(\ShopperGroupJ3Page::$deleteButton);
-                $I->acceptPopup();
-                break;
-            case 'unpublish':
-                $I->click(\ShopperGroupJ3Page::$unpublishButton);
-                $I->acceptPopup();
-                break;
-            case 'publish':
-                $I->click(\ShopperGroupJ3Page::$publishButton);
-                $I->acceptPopup();
-                break;
-            case 'cancel':
-                $I->click(\ShopperGroupJ3Page::$newButton);
-                $I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URLNew);
-                $I->click(\ShopperGroupJ3Page::$cancelButton);
-                break;
-        }
-        $I->see(\ShopperGroupJ3Page::$namePageManagement, \ShopperGroupJ3Page::$pageHead);
-    }
+	public function changStatusAllShopperGroups($status)
+	{
+		$I = $this;
+		$I->amOnPage(\ShopperGroupJ3Page::$URL);
+		$I->checkAllResults();
+		switch ($status) {
+			case 'publish':
+				$I->click(\ShopperGroupJ3Page::$publishButton);
+				$I->see(\ShopperGroupJ3Page::$publishSuccess, \ShopperGroupJ3Page::$xpathMessageSuccess);
+				break;
 
-    public function deleteAllShopperGroups()
-    {
-        $I = $this;
-        $I->amOnPage(\ShopperGroupJ3Page::$URL);
-        $I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URL);
-        $I->checkAllResults();
-        $I->click(\ShopperGroupJ3Page::$deleteButton);
-        $I->acceptPopup();
+			case 'unpublish':
+				$I->click(\ShopperGroupJ3Page::$unpublishButton);
+				$I->see(\ShopperGroupJ3Page::$unpublishSuccess, \ShopperGroupJ3Page::$xpathMessageSuccess);
+				break;
+		}
+		$I->see(\ShopperGroupJ3Page::$namePageManagement, \ShopperGroupJ3Page::$selectorNamePage);
+	}
 
-        $I->waitForElement(\ShopperGroupJ3Page::$xpathMessageSuccess);
-    }
 
-    public function getShopperGroupsStates()
-    {
-        $I = $this;
-        $I->amOnPage(\ShopperGroupJ3Page::$URL);
-        $I->wait(3);
-        $text = $I->grabAttributeFrom(\ShopperGroupJ3Page::$shopperFirstStatus, 'onclick');
+	public function checkButtons($buttonName)
+	{
+		$I = $this;
+		$I->amOnPage(\ShopperGroupJ3Page::$URL);
+		switch ($buttonName) {
+			case 'edit':
+				$I->click(\ShopperGroupJ3Page::$editButton);
+				$I->acceptPopup();
+				break;
+			case 'delete':
+				$I->click(\ShopperGroupJ3Page::$deleteButton);
+				$I->acceptPopup();
+				break;
+			case 'unpublish':
+				$I->click(\ShopperGroupJ3Page::$unpublishButton);
+				$I->acceptPopup();
+				break;
+			case 'publish':
+				$I->click(\ShopperGroupJ3Page::$publishButton);
+				$I->acceptPopup();
+				break;
+			case 'cancel':
+				$I->click(\ShopperGroupJ3Page::$newButton);
+				$I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URLNew);
+				$I->click(\ShopperGroupJ3Page::$cancelButton);
+				break;
+		}
+		$I->see(\ShopperGroupJ3Page::$namePageManagement, \ShopperGroupJ3Page::$pageHead);
+	}
 
-        if (strpos($text, 'unpublish') > 0) {
-            $result = 'published';
-        } else {
-            $result = 'unpublished';
-        }
-        return $result;
-    }
+	public function deleteAllShopperGroups()
+	{
+		$I = $this;
+		$I->amOnPage(\ShopperGroupJ3Page::$URL);
+		$I->checkForPhpNoticesOrWarnings(\ShopperGroupJ3Page::$URL);
+		$I->checkAllResults();
+		$I->click(\ShopperGroupJ3Page::$deleteButton);
+		$I->acceptPopup();
+
+		$I->waitForElement(\ShopperGroupJ3Page::$xpathMessageSuccess);
+	}
+
+	public function getShopperGroupsStates()
+	{
+		$I = $this;
+		$I->amOnPage(\ShopperGroupJ3Page::$URL);
+		$I->wait(3);
+		$text = $I->grabAttributeFrom(\ShopperGroupJ3Page::$shopperFirstStatus, 'onclick');
+
+		if (strpos($text, 'unpublish') > 0) {
+			$result = 'published';
+		} else {
+			$result = 'unpublished';
+		}
+		return $result;
+	}
 
 }
