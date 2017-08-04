25602aaee80cc17c41b4fd7bcf8268f7a2bc018d
Tu Vuong <tu@redweb.dk>
2017-08-04 17:46:44 +0700

    REDSHOP-4611 Update nganluong payment plugin with the lastest version

--- plugins/redshop_payment/nganluong/nganluong.php
+++ plugins/redshop_payment/nganluong/nganluong.php
@@ -11,7 +11,7 @@ defined('_JEXEC') or die;
 // Load nganluong library
 require_once dirname(__DIR__) . '/nganluong/library/init.php';
 
-class plgRedshop_PaymentNganluong extends RedshopPayment
+class plgRedshop_PaymentNganluong extends JPlugin
 {
 	/**
 	 * Load the language file on instantiation.
@@ -22,12 +22,41 @@ class plgRedshop_PaymentNganluong extends RedshopPayment
 	protected $autoloadLanguage = true;
 
 	/**
+	 * Get notify url for payment status update.
+	 *
+	 * @param   integer  $orderId  Order Id
+	 *
+	 * @return  string             Notify url
+	 */
+	protected function getNotifyUrl($orderId)
+	{
+		return JUri::base()
+				. 'index.php?option=com_redshop&view=order_detail&task=notify_payment&payment_plugin=' . $this->_name
+				. '&orderid=' . $orderId
+				. '&Itemid=' . JFactory::getApplication()->input->getInt('Itemid');
+	}
+
+	/**
+	 * Get return url of for the payment.
+	 *
+	 * @param   integer  $orderId  Order Id
+	 *
+	 * @return  string   Return Url
+	 */
+	protected function getReturnUrl($orderId)
+	{
+		return JUri::base()
+				. 'index.php?option=com_redshop&view=order_detail&layout=receipt&oid=' . $orderId
+				. '&Itemid=' . JFactory::getApplication()->input->getInt('Itemid');
+	}
+
+	/**
 	 * This method will be triggered on before placing order to authorize or charge credit card
 	 *
 	 * @param   string  $element  Name of the payment plugin
 	 * @param   array   $data     Cart Information
 	 *
-	 * @return  object  Authorize or Charge success or failed message and transaction id
+	 * @return  mixed
 	 */
 	public function onPrePayment($element, $data)
 	{
@@ -36,108 +65,62 @@ class plgRedshop_PaymentNganluong extends RedshopPayment
 			return;
 		}
 
-		echo $this->renderPaymentForm($data);
-	}
+		$app = JFactory::getApplication();
+		$url = 'https://www.nganluong.vn/checkout.php';
 
-	/**
-	 * Prepare Payment Input
-	 *
-	 * @param   array  $orderInfo  Order Information
-	 *
-	 * @return  array  Payment Gateway for parameters
-	 */
-	protected function preparePaymentInput($orderInfo)
-	{
-		$inputs = array(
-				'action' 	  => JURI::base() . "index.php?tmpl=component&option=com_redshop&view=order_detail&controller=order_detail&task=process_payment&payment_method_id=nganluong&order_id=" . $orderInfo['order_id'],
-				'firstname'   => $orderInfo['billinginfo']->firstname,
-				'lastname'    => $orderInfo['billinginfo']->lastname,
-				'email'       => $orderInfo['billinginfo']->user_email,
-				'url'         => $this->getReturnUrl($orderInfo['order_id']),
-				'urlc'        => $this->getNotifyUrl($orderInfo['order_id']),
-				'phone'       => $orderInfo['billinginfo']->phone,
-				'street'      => $orderInfo['billinginfo']->address,
-			);
-
-		return $inputs;
-	}
-
-	/**
-	 * Notify payment
-	 *
-	 * @param   string  $element  Name of plugin
-	 * @param   array   $request  HTTP request data
-	 *
-	 * @return  object  Contains the information of order success of falier in object
-	 */
-	public function onPrePayment_Nganluong($element, $request)
-	{
-		if ($element != 'nganluong')
+		if ($this->params->get('sandbox') == 1)
 		{
-			return;
+			$url = 'http://sandbox.nganluong.vn:8088/nl30/checkout.php';
 		}
-
-		$app         = JFactory::getApplication();
-		$input       = $app->input;
-		$orderHelper = order_functions::getInstance();
-		$orderId     = $input->getInt('order_id');
-		$order       = $orderHelper->getOrderDetails($orderId);
-		$price       = $order->order_total;
 
 		$merchantId = $this->params->get('nganluong_merchant_id');
 		$merchantPass = $this->params->get('nganluong_merchant_password');
 		$email = $this->params->get('nganluong_email');
-		$url = $this->params->get('nganluong_url_api');
 
-		$nlCheckout = new NL_CheckOutV3($merchantId, $merchantPass, $email, $url);
-		$totalAmount = $price;
-		$items = array();
-		$paymentMethod = $input->post->get('option_payment');
-		$bankCode = $input->post->get('bankcode');
-		$orderCode = $orderId;
-		$paymentType = '';
-		$discountAmount = $order->order_discount;
-		$orderDescription = '';
-		$taxAmount = $order->order_tax;
-		$feeshipping = $order->order_shipping;
-		$returnUrl = urlencode($this->getNotifyUrl($orderId));
-		$cancelUrl = urlencode($this->getReturnUrl($orderId));
+		$nlCheckout                   = new NL_CheckOut;
+		$nlCheckout->nganluongUrl     = $url;
+		$nlCheckout->merchantSiteCode = $merchantId;
+		$nlCheckout->securePass       = $merchantPass;
 
-		$buyerFullname = $input->post->get('fullname');
-		$buyerEmail = $input->post->get('email');
-		$buyerMobile = $input->post->get('phone');
-		$buyerAddress = $input->post->get('address');
+		$orderId          = $data['order']->order_id;
+		$totalAmount      = $data['order']->order_total;
+		$items            = array();
+		$orderCode        = $orderId;
+		$orderQuantity    = $data['order_quantity'];
+		$paymentType      = '';
+		$discountAmount   = $data['order']->order_discount;
+		$orderDescription = $data['order']->customer_message;
+		$taxAmount        = $data['order']->order_tax;
+		$feeShipping      = $data['order']->order_shipping;
+		$returnUrl        = urlencode($this->getNotifyUrl($orderId));
+		$cancelUrl        = urlencode($this->getReturnUrl($orderId));
+		$buyerFullname    = $data['billinginfo']->firstname . ' ' . $data['billinginfo']->lastname;
+		$buyerEmail       = $data['billinginfo']->email;
+		$buyerMobile      = $data['billinginfo']->phone;
+		$buyerInfo        = $buyerFullname . "*|*" . $buyerEmail . "*|*" . $buyerMobile;
 
-		if (!empty($paymentMethod) && !empty($buyerEmail) && !empty($buyerMobile) && !empty($buyerFullname))
+		$nlResult = $nlCheckout->buildCheckoutUrlExpand(
+			$returnUrl,
+			$email,
+			'',
+			$orderCode,
+			$totalAmount,
+			Redshop::getConfig()->get('CURRENCY_CODE'),
+			$orderQuantity,
+			$taxAmount,
+			$discountAmount,
+			0,
+			$feeShipping,
+			$orderDescription,
+			$buyerInfo,
+			''
+		);
+
+		if (!empty($orderId))
 		{
-			if ($paymentMethod == "VISA")
-			{
-				$nlResult = $nlCheckout->VisaCheckout($orderCode, $totalAmount, $paymentType, $orderDescription, $taxAmount, $feeshipping, $discountAmount, $returnUrl, $cancelUrl, $buyerFullname, $buyerEmail, $buyerMobile, $buyerAddress, $items, $bankCode);
-			}
-			elseif ($paymentMethod == "NL")
-			{
-				$nlResult = $nlCheckout->NLCheckout($orderCode, $totalAmount, $paymentType, $orderDescription, $taxAmount, $feeshipping, $discountAmount, $returnUrl, $cancelUrl, $buyerFullname, $buyerEmail, $buyerMobile, $buyerAddress, $items);
-			}
-			elseif ($paymentMethod == "ATM_ONLINE" && !empty($bankCode))
-			{
-				$nlResult = $nlCheckout->BankCheckout($orderCode, $totalAmount, $bankCode, $paymentType, $orderDescription, $taxAmount, $feeshipping, $discountAmount, $returnUrl, $cancelUrl, $buyerFullname, $buyerEmail, $buyerMobile, $buyerAddress, $items);
-			}
-			elseif ($paymentMethod == "NH_OFFLINE")
-			{
-				$nlResult = $nlCheckout->officeBankCheckout($orderCode, $totalAmount, $bankCode, $paymentType, $orderDescription, $taxAmount, $feeshipping, $discountAmount, $returnUrl, $cancelUrl, $buyerFullname, $buyerEmail, $buyerMobile, $buyerAddress, $items);
-			}
-			elseif ($paymentMethod == "ATM_OFFLINE")
-			{
-				$nlResult = $nlCheckout->BankOfflineCheckout($orderCode, $totalAmount, $bankCode, $paymentType, $orderDescription, $taxAmount, $feeshipping, $discountAmount, $returnUrl, $cancelUrl, $buyerFullname, $buyerEmail, $buyerMobile, $buyerAddress, $items);
-			}
-			elseif ($paymentMethod == "IB_ONLINE")
-			{
-				$nlResult = $nlCheckout->IBCheckout($orderCode, $totalAmount, $bankCode, $paymentType, $orderDescription, $taxAmount, $feeshipping, $discountAmount, $returnUrl, $cancelUrl, $buyerFullname, $buyerEmail, $buyerMobile, $buyerAddress, $items);
-			}
+			$nlResult .= '&cancel_url=' . $cancelUrl;
+			$app->redirect($nlResult);
 		}
-
-		$redirect   = (string) $nlResult->checkout_url;
-		$app->redirect($redirect);
 	}
 
 	/**
