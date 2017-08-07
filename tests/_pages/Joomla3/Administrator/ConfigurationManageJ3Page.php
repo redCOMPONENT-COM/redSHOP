2f63c12fe249dcdc8ace6dd9faba47307d58741c
nhung@redweb.dk <nhung@redweb.dk>
2017-08-07 11:03:18 +0700

    function for setup discount at configuration

--- tests/_pages/Joomla3/Administrator/ConfigurationManageJ3Page.php
+++ tests/_pages/Joomla3/Administrator/ConfigurationManageJ3Page.php
@@ -5,73 +5,114 @@
  */
 class ConfigurationManageJ3Page extends AdminJ3Page
 {
-    //nam page
-    public static $namePage = "Configuration";
+	//nam page
+	public static $namePage = "Configuration";
 
-    public static $URL = '/administrator/index.php?option=com_redshop&view=configuration';
+	public static $URL = '/administrator/index.php?option=com_redshop&view=configuration';
 
-    public static $stockRoomYes = "#use_stockroom1-lbl";
+	public static $stockRoomYes = "#use_stockroom1-lbl";
 
-    public static $stockRoomNo = "#use_stockroom0-lbl";
+	public static $stockRoomNo = "#use_stockroom0-lbl";
 
-    public static $eidtInLineYes = "#inline_editing1-lbl";
+	public static $eidtInLineYes = "#inline_editing1-lbl";
 
-    public static $editInLineNo = "#inline_editing0-lbl";
+	public static $editInLineNo = "#inline_editing0-lbl";
 
-    public static $comparisonNo = "#compare_products0-lbl";
+	public static $comparisonNo = "#compare_products0-lbl";
 
-    public static $comparisonYes = "#compare_products1-lbl";
+	public static $comparisonYes = "#compare_products1-lbl";
 
-    //Price
-    public static $showPriceYes = "#show_price1-lbl";
+	//Price
+	public static $showPriceYes = "#show_price1-lbl";
 
-    public static $showPriceNo = "#show_price0-lbl";
+	public static $showPriceNo = "#show_price0-lbl";
 
-    public static $countryPrice = ['id' => 's2id_default_vat_country'];
+	public static $countryPrice = ['id' => 's2id_default_vat_country'];
 
-    public static $countrySearchPrice = ['id' => 's2id_autogen35_search'];
+	public static $countrySearchPrice = ['id' => 's2id_autogen35_search'];
 
-    public static $statePrice = ['id' => 's2id_default_vat_state'];
+	public static $statePrice = ['id' => 's2id_default_vat_state'];
 
-    public static $stateSearchPrice = ['id' => 's2id_autogen36_search'];
+	public static $stateSearchPrice = ['id' => 's2id_autogen36_search'];
 
-    public static $vatGroup=['id' => 's2id_default_vat_group'];
+	public static $vatGroup = ['id' => 's2id_default_vat_group'];
 
-    public static $vatSearchGroup=['id' => 's2id_autogen37_search'];
+	public static $vatSearchGroup = ['id' => 's2id_autogen37_search'];
 
-    public static $vatDefaultBase = ['id' => 's2id_vat_based_on'];
+	public static $vatDefaultBase = ['id' => 's2id_vat_based_on'];
 
-    public static $vatSearchDefaultBase = ['id' => 's2id_autogen38_search'];
+	public static $vatSearchDefaultBase = ['id' => 's2id_autogen38_search'];
 
-    public static $applyDiscountAfter = ['id' => 'apply_vat_on_discount0-lbl'];
+	public static $applyDiscountAfter = ['id' => 'apply_vat_on_discount0-lbl'];
 
-    public static $applyDiscountBefore = ['id' => 'apply_vat_on_discount1-lbl'];
+	public static $applyDiscountBefore = ['id' => 'apply_vat_on_discount1-lbl'];
 
-    public static $vatAfterDiscount = ['id' => 'vat_rate_after_discount'];
+	public static $vatAfterDiscount = ['id' => 'vat_rate_after_discount'];
 
-    public static $calculationBaseBilling = ['id' => 'calculate_vat_onBT-lbl'];
+	public static $calculationBaseBilling = ['id' => 'calculate_vat_onBT-lbl'];
 
-    public static $calculationBaseShipping = ['id' => 'calculate_vat_onST-lbl'];
+	public static $calculationBaseShipping = ['id' => 'calculate_vat_onST-lbl'];
 
-    public static $vatNumberNo = ['id' => 'required_vat_number0-lbl'];
+	public static $vatNumberNo = ['id' => 'required_vat_number0-lbl'];
 
-    public static $vatNumberYes = ['id' => 'required_vat_number1-lbl'];
+	public static $vatNumberYes = ['id' => 'required_vat_number1-lbl'];
 
-    //xPath feature
-    public static $priceTab = ['xpath' => "//h3[text()='Main Price Settings']"];
+	//price Discount
 
-    public static $comparisonTab = ['xpath' => "//h3[text()='Comparison']"];
+	public static $enableDiscountYes = ['id' => 'discount_enable1-lbl'];
 
-    public static $stockRoomTab = ['xpath' => "//h3[text()='Stockroom']"];
+	public static $enableDiscountNo = ['id' => 'discount_enable0-lbl'];
 
-    public static $editInline = ['xpath' => "//h3[text()='Inline Edit']"];
+	public static $discountType = ['id' => 's2id_discount_type'];
 
-    public static $ratingTab = ['xpath' => "//h3[text()='Rating']"];
+	public static $discountSearch = ['id' => 's2id_autogen39_search'];
 
-    //button
-    public static $featureSetting = "Feature Settings";
+	public static $enableCouponNo = ['id' => 'coupons_enable0-lbl'];
 
-    public static $price = "Price";
+	public static $enableCouponYes = ['id' => 'coupons_enable1-lbl'];
+
+	public static $couponInforYes = ['id' => 'couponinfo1-lbl'];
+
+	public static $couponInforNo = ['id' => 'couponinfo0-lbl'];
+
+	public static $enableVoucherNo = ['id' => 'vouchers_enable0-lbl'];
+
+	public static $enableVoucherYes = ['id' => 'vouchers_enable1-lbl'];
+
+	public static $enableDiscountMailNo = ['id' => 'special_discount_mail_send0-lbl'];
+
+	public static $enableDiscountMailYes = ['id' => 'special_discount_mail_send1-lbl'];
+
+	public static $applyDiscountNo = ['id' => 'apply_voucher_coupon_already_discount0-lbl'];
+
+	public static $applyDiscountYes = ['id' => 'apply_voucher_coupon_already_discount1-lbl'];
+
+	public static $shippingBaseOnTotal = ['id' => 'shipping_aftertotal-lbl'];
+
+	public static $shippingBaseOnSubtotal = ['id' => 'shipping_aftersubtotal-lbl'];
+
+	public static $valueDiscountCouponTotal = ['id' => 's2id_discoupon_percent_or_total'];
+
+	public static $valueDiscountCouponPercentager = ['id' => 's2id_autogen40_search'];
+
+	public static $discountAmount = ['id' => 'discoupon_value'];
+
+
+	//xPath feature
+	public static $priceTab = ['xpath' => "//h3[text()='Main Price Settings']"];
+
+	public static $comparisonTab = ['xpath' => "//h3[text()='Comparison']"];
+
+	public static $stockRoomTab = ['xpath' => "//h3[text()='Stockroom']"];
+
+	public static $editInline = ['xpath' => "//h3[text()='Inline Edit']"];
+
+	public static $ratingTab = ['xpath' => "//h3[text()='Rating']"];
+
+	//button
+	public static $featureSetting = "Feature Settings";
+
+	public static $price = "Price";
 
 
 }
--- tests/acceptance/administrator/_steps/Joomla3Steps/ConfigurationManageJoomla3Steps.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/ConfigurationManageJoomla3Steps.php
@@ -9,178 +9,269 @@
 namespace AcceptanceTester;
 
 
+use Prophecy\Doubler\LazyDouble;
+
 class ConfigurationManageJoomla3Steps extends AdminManagerJoomla3Steps
 {
-    public function featureUsedStockRoom()
-    {
-        $I = $this;
-        $I->amOnPage(\ConfigurationManageJ3Page::$URL);
-        $I->click(\ConfigurationManageJ3Page::$featureSetting);
-        $I->waitForElement(\ConfigurationManageJ3Page::$ratingTab, 60);
-        $I->waitForElement(\ConfigurationManageJ3Page::$stockRoomTab, 60);
-        $I->click(\ConfigurationManageJ3Page::$stockRoomYes);
-        $I->click(\ConfigurationManageJ3Page::$buttonSave);
-    }
+	public function featureUsedStockRoom()
+	{
+		$I = $this;
+		$I->amOnPage(\ConfigurationManageJ3Page::$URL);
+		$I->click(\ConfigurationManageJ3Page::$featureSetting);
+		$I->waitForElement(\ConfigurationManageJ3Page::$ratingTab, 60);
+		$I->waitForElement(\ConfigurationManageJ3Page::$stockRoomTab, 60);
+		$I->click(\ConfigurationManageJ3Page::$stockRoomYes);
+		$I->click(\ConfigurationManageJ3Page::$buttonSave);
+	}
 
-    public function featureOffStockRoom()
-    {
-        $I = $this;
-        $I->amOnPage(\ConfigurationManageJ3Page::$URL);
-        $I->click(\ConfigurationManageJ3Page::$featureSetting);
-        $I->waitForElement(\ConfigurationManageJ3Page::$ratingTab, 60);
-        $I->waitForElement(\ConfigurationManageJ3Page::$stockRoomTab, 60);
-        $I->click(\ConfigurationManageJ3Page::$stockRoomNo);
-        $I->click(\ConfigurationManageJ3Page::$buttonSave);
-    }
-
-
-    public function featureEditInLineYes()
-    {
-        $I = $this;
-        $I->amOnPage(\ConfigurationManageJ3Page::$URL);
-        $I->click(\ConfigurationManageJ3Page::$featureSetting);
-        $I->waitForElement(\ConfigurationManageJ3Page::$editInline, 60);
-        $I->waitForElement(\ConfigurationManageJ3Page::$stockRoomTab, 60);
-        $I->click(\ConfigurationManageJ3Page::$eidtInLineYes);
-        $I->click(\ConfigurationManageJ3Page::$buttonSave);
-    }
-
-    public function featureEditInLineNo()
-    {
-        $I = $this;
-        $I->amOnPage(\ConfigurationManageJ3Page::$URL);
-        $I->click(\ConfigurationManageJ3Page::$featureSetting);
-        $I->waitForElement(\ConfigurationManageJ3Page::$editInline, 60);
-        $I->waitForElement(\ConfigurationManageJ3Page::$stockRoomTab, 60);
-        $I->click(\ConfigurationManageJ3Page::$editInLineNo);
-        $I->click(\ConfigurationManageJ3Page::$buttonSave);
-    }
-
-    public function featureComparisonNo()
-    {
-        $I = $this;
-        $I->amOnPage(\ConfigurationManageJ3Page::$URL);
-        $I->click(\ConfigurationManageJ3Page::$featureSetting);
-        $I->waitForElement(\ConfigurationManageJ3Page::$comparisonTab, 60);
-        $I->waitForElement(\ConfigurationManageJ3Page::$stockRoomTab, 60);
-        $I->click(\ConfigurationManageJ3Page::$comparisonNo);
-        $I->click(\ConfigurationManageJ3Page::$buttonSave);
-    }
-
-    public function featureComparisonYes()
-    {
-        $I = $this;
-        $I->amOnPage(\ConfigurationManageJ3Page::$URL);
-        $I->click(\ConfigurationManageJ3Page::$featureSetting);
-        $I->waitForElement(\ConfigurationManageJ3Page::$comparisonTab, 60);
-        $I->waitForElement(\ConfigurationManageJ3Page::$stockRoomTab, 60);
-        $I->click(\ConfigurationManageJ3Page::$comparisonYes);
-        $I->click(\ConfigurationManageJ3Page::$buttonSave);
-    }
+	public function featureOffStockRoom()
+	{
+		$I = $this;
+		$I->amOnPage(\ConfigurationManageJ3Page::$URL);
+		$I->click(\ConfigurationManageJ3Page::$featureSetting);
+		$I->waitForElement(\ConfigurationManageJ3Page::$ratingTab, 60);
+		$I->waitForElement(\ConfigurationManageJ3Page::$stockRoomTab, 60);
+		$I->click(\ConfigurationManageJ3Page::$stockRoomNo);
+		$I->click(\ConfigurationManageJ3Page::$buttonSave);
+	}
 
 
-    //Price
+	public function featureEditInLineYes()
+	{
+		$I = $this;
+		$I->amOnPage(\ConfigurationManageJ3Page::$URL);
+		$I->click(\ConfigurationManageJ3Page::$featureSetting);
+		$I->waitForElement(\ConfigurationManageJ3Page::$editInline, 60);
+		$I->waitForElement(\ConfigurationManageJ3Page::$stockRoomTab, 60);
+		$I->click(\ConfigurationManageJ3Page::$eidtInLineYes);
+		$I->click(\ConfigurationManageJ3Page::$buttonSave);
+	}
 
-    public function featurePriceNo()
-    {
-        $I = $this;
-        $I->amOnPage(\ConfigurationManageJ3Page::$URL);
-        $I->click(\ConfigurationManageJ3Page::$price);
-        $I->waitForElement(\ConfigurationManageJ3Page::$priceTab, 60);
-        $I->click(\ConfigurationManageJ3Page::$showPriceNo);
-        $I->click(\ConfigurationManageJ3Page::$buttonSave);
-    }
+	public function featureEditInLineNo()
+	{
+		$I = $this;
+		$I->amOnPage(\ConfigurationManageJ3Page::$URL);
+		$I->click(\ConfigurationManageJ3Page::$featureSetting);
+		$I->waitForElement(\ConfigurationManageJ3Page::$editInline, 60);
+		$I->waitForElement(\ConfigurationManageJ3Page::$stockRoomTab, 60);
+		$I->click(\ConfigurationManageJ3Page::$editInLineNo);
+		$I->click(\ConfigurationManageJ3Page::$buttonSave);
+	}
 
-    public function featurePriceYes()
-    {
-        $I = $this;
-        $I->amOnPage(\ConfigurationManageJ3Page::$URL);
-        $I->click(\ConfigurationManageJ3Page::$price);
-        $I->waitForElement(\ConfigurationManageJ3Page::$priceTab, 60);
-        $I->click(\ConfigurationManageJ3Page::$showPriceYes);
-        $I->click(\ConfigurationManageJ3Page::$buttonSave);
-    }
+	public function featureComparisonNo()
+	{
+		$I = $this;
+		$I->amOnPage(\ConfigurationManageJ3Page::$URL);
+		$I->click(\ConfigurationManageJ3Page::$featureSetting);
+		$I->waitForElement(\ConfigurationManageJ3Page::$comparisonTab, 60);
+		$I->waitForElement(\ConfigurationManageJ3Page::$stockRoomTab, 60);
+		$I->click(\ConfigurationManageJ3Page::$comparisonNo);
+		$I->click(\ConfigurationManageJ3Page::$buttonSave);
+	}
 
-    /**
-     * @param $country
-     * @param $state
-     * @param $vatDefault
-     * @param $vatCalculation
-     * @param $vatAfter
-     * @param $calculationBase
-     * @param $vatNumber
-     */
-    public function setupVAT($country, $state, $vatDefault, $vatCalculation, $vatAfter, $vatNumber, $calculationBase, $requiVAT)
-    {
-        $I = $this;
-        $I->amOnPage(\ConfigurationManageJ3Page::$URL);
-        $I->click(\ConfigurationManageJ3Page::$price);
+	public function featureComparisonYes()
+	{
+		$I = $this;
+		$I->amOnPage(\ConfigurationManageJ3Page::$URL);
+		$I->click(\ConfigurationManageJ3Page::$featureSetting);
+		$I->waitForElement(\ConfigurationManageJ3Page::$comparisonTab, 60);
+		$I->waitForElement(\ConfigurationManageJ3Page::$stockRoomTab, 60);
+		$I->click(\ConfigurationManageJ3Page::$comparisonYes);
+		$I->click(\ConfigurationManageJ3Page::$buttonSave);
+	}
 
-        $I->click(\ConfigurationManageJ3Page::$countryPrice);
-        $I->waitForElement(\ConfigurationManageJ3Page::$countrySearchPrice, 5);
-        $I->fillField(\ConfigurationManageJ3Page::$countrySearchPrice, $country);
-        $userConfigurationPage = new \ConfigurationManageJ3Page();
-        $I->waitForElement($userConfigurationPage->returnChoice($country));
-        $I->click($userConfigurationPage->returnChoice($country));
 
-        //get state
-        $I->click(\ConfigurationManageJ3Page::$statePrice);
-        $I->waitForElement(\ConfigurationManageJ3Page::$stateSearchPrice, 5);
-        $I->fillField(\ConfigurationManageJ3Page::$stateSearchPrice, $state);
-        $I->waitForElement($userConfigurationPage->returnChoice($state));
-        $I->click($userConfigurationPage->returnChoice($state));
+	//Price
 
-        //get default vat
-        $I->click(\ConfigurationManageJ3Page::$vatGroup);
-        $I->waitForElement(\ConfigurationManageJ3Page::$vatSearchGroup, 5);
-        $I->fillField(\ConfigurationManageJ3Page::$vatSearchGroup, $vatDefault);
-        $I->waitForElement($userConfigurationPage->returnChoice($vatDefault));
-        $I->pressKey(\ConfigurationManageJ3Page::$vatGroup, \Facebook\WebDriver\WebDriverKeys::ARROW_DOWN, \Facebook\WebDriver\WebDriverKeys::ENTER);
+	public function featurePriceNo()
+	{
+		$I = $this;
+		$I->amOnPage(\ConfigurationManageJ3Page::$URL);
+		$I->click(\ConfigurationManageJ3Page::$price);
+		$I->waitForElement(\ConfigurationManageJ3Page::$priceTab, 60);
+		$I->click(\ConfigurationManageJ3Page::$showPriceNo);
+		$I->click(\ConfigurationManageJ3Page::$buttonSave);
+	}
 
-        //get vat base on
-        $I->click(\ConfigurationManageJ3Page::$vatDefaultBase);
-        $I->waitForElement(\ConfigurationManageJ3Page::$vatSearchDefaultBase, 5);
-        $I->fillField(\ConfigurationManageJ3Page::$vatSearchDefaultBase, $vatCalculation);
-        $I->waitForElement($userConfigurationPage->returnChoice($vatCalculation));
-        $I->pressKey(\ConfigurationManageJ3Page::$vatDefaultBase, \Facebook\WebDriver\WebDriverKeys::ARROW_DOWN, \Facebook\WebDriver\WebDriverKeys::ENTER);
+	public function featurePriceYes()
+	{
+		$I = $this;
+		$I->amOnPage(\ConfigurationManageJ3Page::$URL);
+		$I->click(\ConfigurationManageJ3Page::$price);
+		$I->waitForElement(\ConfigurationManageJ3Page::$priceTab, 60);
+		$I->click(\ConfigurationManageJ3Page::$showPriceYes);
+		$I->click(\ConfigurationManageJ3Page::$buttonSave);
+	}
 
-        //apply vat on discount
-        switch ($vatAfter) {
-            case 'after':
-                $I->click(\ConfigurationManageJ3Page::$applyDiscountAfter);
-                break;
-            case 'before':
-                $I->click(\ConfigurationManageJ3Page::$applyDiscountBefore);
-                break;
-        }
+	/**
+	 * @param $country
+	 * @param $state
+	 * @param $vatDefault
+	 * @param $vatCalculation
+	 * @param $vatAfter
+	 * @param $calculationBase
+	 * @param $vatNumber
+	 */
+	public function setupVAT($country, $state, $vatDefault, $vatCalculation, $vatAfter, $vatNumber, $calculationBase, $requiVAT)
+	{
+		$I = $this;
+		$I->amOnPage(\ConfigurationManageJ3Page::$URL);
+		$I->click(\ConfigurationManageJ3Page::$price);
 
-        // value after discount
-        $I->fillField(\ConfigurationManageJ3Page::$vatAfterDiscount, $vatNumber);
+		$I->click(\ConfigurationManageJ3Page::$countryPrice);
+		$I->waitForElement(\ConfigurationManageJ3Page::$countrySearchPrice, 5);
+		$I->fillField(\ConfigurationManageJ3Page::$countrySearchPrice, $country);
+		$userConfigurationPage = new \ConfigurationManageJ3Page();
+		$I->waitForElement($userConfigurationPage->returnChoice($country));
+		$I->click($userConfigurationPage->returnChoice($country));
 
-        //get value calculation based on
-        switch ($calculationBase) {
-            case 'billing':
-                $I->click(\ConfigurationManageJ3Page::$calculationBaseBilling);
-                break;
-            case 'shipping':
-                $I->click(\ConfigurationManageJ3Page::$calculationBaseShipping);
-                break;
-        }
+		//get state
+		$I->click(\ConfigurationManageJ3Page::$statePrice);
+		$I->waitForElement(\ConfigurationManageJ3Page::$stateSearchPrice, 5);
+		$I->fillField(\ConfigurationManageJ3Page::$stateSearchPrice, $state);
+		$I->waitForElement($userConfigurationPage->returnChoice($state));
+		$I->click($userConfigurationPage->returnChoice($state));
 
-        //get requi vat yesno
+		//get default vat
+		$I->click(\ConfigurationManageJ3Page::$vatGroup);
+		$I->waitForElement(\ConfigurationManageJ3Page::$vatSearchGroup, 5);
+		$I->fillField(\ConfigurationManageJ3Page::$vatSearchGroup, $vatDefault);
+		$I->waitForElement($userConfigurationPage->returnChoice($vatDefault));
+		$I->pressKey(\ConfigurationManageJ3Page::$vatGroup, \Facebook\WebDriver\WebDriverKeys::ARROW_DOWN, \Facebook\WebDriver\WebDriverKeys::ENTER);
 
-        switch ($requiVAT) {
-            case 'yes':
-                $I->click(\ConfigurationManageJ3Page::$vatNumberYes);
-                break;
-            case 'no':
-                $I->click(\ConfigurationManageJ3Page::$vatNumberNo);
-                break;
-        }
+		//get vat base on
+		$I->click(\ConfigurationManageJ3Page::$vatDefaultBase);
+		$I->waitForElement(\ConfigurationManageJ3Page::$vatSearchDefaultBase, 5);
+		$I->fillField(\ConfigurationManageJ3Page::$vatSearchDefaultBase, $vatCalculation);
+		$I->waitForElement($userConfigurationPage->returnChoice($vatCalculation));
+		$I->pressKey(\ConfigurationManageJ3Page::$vatDefaultBase, \Facebook\WebDriver\WebDriverKeys::ARROW_DOWN, \Facebook\WebDriver\WebDriverKeys::ENTER);
 
-        $I->click(\ConfigurationManageJ3Page::$buttonSave);
-        $I->see(\ConfigurationManageJ3Page::$namePage, \ConfigurationManageJ3Page::$selectorPageTitle);
+		//apply vat on discount
+		switch ($vatAfter) {
+			case 'after':
+				$I->click(\ConfigurationManageJ3Page::$applyDiscountAfter);
+				break;
+			case 'before':
+				$I->click(\ConfigurationManageJ3Page::$applyDiscountBefore);
+				break;
+		}
 
-    }
+		// value after discount
+		$I->fillField(\ConfigurationManageJ3Page::$vatAfterDiscount, $vatNumber);
+
+		//get value calculation based on
+		switch ($calculationBase) {
+			case 'billing':
+				$I->click(\ConfigurationManageJ3Page::$calculationBaseBilling);
+				break;
+			case 'shipping':
+				$I->click(\ConfigurationManageJ3Page::$calculationBaseShipping);
+				break;
+		}
+
+		//get requi vat yesno
+
+		switch ($requiVAT) {
+			case 'yes':
+				$I->click(\ConfigurationManageJ3Page::$vatNumberYes);
+				break;
+			case 'no':
+				$I->click(\ConfigurationManageJ3Page::$vatNumberNo);
+				break;
+		}
+
+		$I->click(\ConfigurationManageJ3Page::$buttonSave);
+		$I->see(\ConfigurationManageJ3Page::$namePage, \ConfigurationManageJ3Page::$selectorPageTitle);
+
+	}
+
+	public function setupDiscount($enableDiscount, $allowedDiscount, $coupon, $couponInfor, $voucher, $sendEmail, $apply, $calculate, $value, $amount)
+	{
+		$I = $this;
+		$I->amOnPage(\ConfigurationManageJ3Page::$URL);
+		$I->click(\ConfigurationManageJ3Page::$price);
+		switch ($enableDiscount) {
+			case 'yes': {
+				$I->click(\ConfigurationManageJ3Page::$enableDiscountYes);
+
+				//discount type apply
+				$I->click(\ConfigurationManageJ3Page::$discountType);
+				$I->waitForElement(\ConfigurationManageJ3Page::$discountSearch, 5);
+				$I->fillField(\ConfigurationManageJ3Page::$discountSearch, $allowedDiscount);
+				$userPage = new \ConfigurationManageJ3Page();
+				$I->waitForElement($userPage->returnChoice($allowedDiscount));
+				$I->pressKey(\ConfigurationManageJ3Page::$discountSearch, \Facebook\WebDriver\WebDriverKeys::ARROW_DOWN, \Facebook\WebDriver\WebDriverKeys::ENTER);
+
+				//enable discount
+				switch ($coupon) {
+					case 'yes':
+						$I->click(\ConfigurationManageJ3Page::$enableCouponYes);
+						break;
+					case 'no':
+						$I->click(\ConfigurationManageJ3Page::$enableCouponNo);
+						break;
+				}
+				switch ($couponInfor) {
+					case 'yes':
+						$I->click(\ConfigurationManageJ3Page::$couponInforYes);
+						break;
+					case 'no':
+						$I->click(\ConfigurationManageJ3Page::$couponInforNo);
+						break;
+				}
+
+				switch ($voucher) {
+					case 'yes':
+						$I->click(\ConfigurationManageJ3Page::$enableVoucherYes);
+						break;
+					case 'no':
+						$I->click(\ConfigurationManageJ3Page::$enableVoucherNo);
+						break;
+				}
+				switch ($sendEmail) {
+					case 'yes':
+						$I->click(\ConfigurationManageJ3Page::$enableDiscountMailYes);
+						break;
+					case 'no':
+						$I->click(\ConfigurationManageJ3Page::$enableDiscountMailNo);
+						break;
+				}
+				switch ($apply) {
+					case 'yes':
+						$I->click(\ConfigurationManageJ3Page::$applyDiscountYes);
+						break;
+					case 'no':
+						$I->click(\ConfigurationManageJ3Page::$applyDiscountNo);
+						break;
+				}
+				switch ($calculate) {
+					case 'total':
+						$I->click(\ConfigurationManageJ3Page::$shippingBaseOnTotal);
+						break;
+					case 'subtotal':
+						$I->click(\ConfigurationManageJ3Page::$shippingBaseOnSubtotal);
+						break;
+				}
+
+				//value discount total or percentage
+
+				$I->click(\ConfigurationManageJ3Page::$valueDiscountCouponTotal);
+				$I->waitForElement(\ConfigurationManageJ3Page::$valueDiscountCouponPercentager, 5);
+				$I->fillField(\ConfigurationManageJ3Page::$valueDiscountCouponPercentager, $value);
+				$I->waitForElement($userPage->returnChoice($value));
+				$I->pressKey(\ConfigurationManageJ3Page::$valueDiscountCouponPercentager, \Facebook\WebDriver\WebDriverKeys::ARROW_DOWN, \Facebook\WebDriver\WebDriverKeys::ENTER);
+
+				$I->fillField(\ConfigurationManageJ3Page::$discountAmount, $amount);
+			}
+				break;
+			case 'no':
+				$I->click(\ConfigurationManageJ3Page::$enableDiscountNo);
+				break;
+
+		}
+		$I->click(\ConfigurationManageJ3Page::$buttonSave);
+		$I->see(\ConfigurationManageJ3Page::$namePage, \ConfigurationManageJ3Page::$selectorPageTitle);
+
+	}
 
 }
