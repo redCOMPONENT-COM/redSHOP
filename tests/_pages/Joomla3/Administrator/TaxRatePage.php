de207d2811a0713793aa8c6feab17b49335169ee
thongredweb <thong@redweb.dk>
2017-08-08 22:41:40 +0700

    Update test class

---
+++ tests/_pages/Joomla3/Administrator/TaxRatePage.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @package     RedShop
+ * @subpackage  Page Class
+ * @copyright   Copyright (C) 2008 - 2015 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+/**
+ * Class TaxRatePage
+ *
+ * @link   http://codeception.com/docs/07-AdvancedUsage#PageObjects
+ *
+ * @since  1.4
+ */
+class TaxRatePage extends AdminJ3Page
+{
+	/**
+	 * @var string
+	 */
+	public static $namePage = 'VAT Rates';
+
+	/**
+	 * @var string
+	 */
+	public static $url = '/administrator/index.php?option=com_redshop&view=tax_rates';
+
+	/**
+	 * @var string
+	 */
+	public static $nameEditPage = 'VAT Rates: [ Edit ]';
+
+	/**
+	 * @var array
+	 */
+	public static $fieldName = ['id' => 'jform_name'];
+
+	/**
+	 * @var array
+	 */
+	public static $fieldValue = ['id' => 'jform_tax_rate'];
+
+	/**
+	 * @var array
+	 */
+	public static $fieldState = ['id' => 'rs_state_jformtax_state'];
+
+	/**
+	 * @var array
+	 */
+	public static $fieldCountry = ['id' => 'jform_tax_country'];
+
+	/**
+	 * @var array
+	 */
+	public static $fieldGroup = ['id' => 'jform_tax_group_id'];
+
+	/**
+	 * @var array
+	 */
+	public static $headPage = ['xpath' => "//h1"];
+
+	/**
+	 * @var string
+	 */
+	public static $messageError = "Error";
+
+	/**
+	 * @var string
+	 */
+	public static $messageSuccess = "Message";
+
+	/**
+	 * @var string
+	 */
+	public static $selectorError = '.alert-danger';
+}
