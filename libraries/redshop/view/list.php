d0cec73dae95f6ecbc7ace6bb0fc23f55dce05ca
thongredweb <thong@redweb.dk>
2017-08-09 00:48:51 +0700

    [Refactor] Order Statuses

--- libraries/redshop/view/list.php
+++ libraries/redshop/view/list.php
@@ -23,41 +23,77 @@ jimport('joomla.application.component.viewlegacy');
 class RedshopViewList extends AbstractView
 {
 	/**
+	 * @var  RedshopModel
+	 */
+	public $model;
+	/**
+	 * @var  array
+	 */
+	public $items;
+	/**
+	 * @var  JPagination
+	 */
+	public $pagination;
+	/**
+	 * @var  array
+	 */
+	public $state;
+	/**
+	 * @var  array
+	 */
+	public $activeFilters;
+	/**
+	 * @var  JForm
+	 */
+	public $filterForm;
+	/**
+	 * @var  boolean
+	 *
+	 * @since  2.0.6
+	 */
+	public $hasOrdering = false;
+	/**
+	 * @var  boolean
+	 *
+	 * @since  2.0.6
+	 */
+	public $isNested = false;
+	/**
+	 * @var    array
+	 *
+	 * @since  2.0.6
+	 */
+	public $nestedOrdering;
+	/**
 	 * Layout used to render the component
 	 *
 	 * @var  string
 	 */
 	protected $componentLayout = 'component.admin';
-
 	/**
 	 * Do we have to display a sidebar ?
 	 *
 	 * @var  boolean
 	 */
 	protected $displaySidebar = true;
-
 	/**
 	 * Do we have to disable a sidebar ?
 	 *
 	 * @var  boolean
 	 */
 	protected $disableSidebar = false;
-
 	/**
 	 * @var  string
 	 */
 	protected $instancesName;
-
 	/**
 	 * @var  string
 	 */
 	protected $instanceName;
-
 	/**
 	 * @var array
 	 */
 	protected $columns = array();
-
 	/**
 	 * Column for render published state.
 	 *
@@ -67,60 +103,9 @@ class RedshopViewList extends AbstractView
 	protected $stateColumns = array('published', 'state');
 
 	/**
-	 * @var  RedshopModel
-	 */
-	public $model;
-
-	/**
-	 * @var  array
-	 */
-	public $items;
-
-	/**
-	 * @var  JPagination
-	 */
-	public $pagination;
-
-	/**
-	 * @var  array
-	 */
-	public $state;
-
-	/**
-	 * @var  array
-	 */
-	public $activeFilters;
-
-	/**
-	 * @var  JForm
-	 */
-	public $filterForm;
-
-	/**
-	 * @var  boolean
-	 *
-	 * @since  2.0.6
-	 */
-	public $hasOrdering = false;
-
-	/**
-	 * @var  boolean
-	 *
-	 * @since  2.0.6
-	 */
-	public $isNested = false;
-
-	/**
-	 * @var    array
-	 *
-	 * @since  2.0.6
-	 */
-	public $nestedOrdering;
-
-	/**
 	 * Method for run before display to initial variables.
 	 *
-	 * @param   string  $tpl  Template name
+	 * @param   string $tpl Template name
 	 *
 	 * @return  void
 	 *
@@ -169,77 +154,6 @@ class RedshopViewList extends AbstractView
 			$app->enqueueMessage(JText::_('COM_REDSHOP_ACCESS_ERROR_NOT_HAVE_PERMISSION'), 'error');
 			$app->redirect('index.php?option=com_redshop');
 		}
-	}
-
-	/**
-	 * Add the page title and toolbar.
-	 *
-	 * @return  void
-	 *
-	 * @since   1.6
-	 */
-	public function addTitle()
-	{
-		$title = $this->getTitle();
-
-		if ($this->pagination->total)
-		{
-			$title .= "<span style='font-size: 0.5em; vertical-align: middle;'> (" . $this->pagination->total . ")</span>";
-		}
-
-		JToolbarHelper::title($title);
-	}
-
-	/**
-	 * Method for get page title.
-	 *
-	 * @return  string
-	 *
-	 * @since   2.0.6
-	 */
-	public function getTitle()
-	{
-		return JText::_('COM_REDSHOP_' . strtoupper($this->getInstanceName()) . '_MANAGEMENT');
-	}
-
-	/**
-	 * Method for add toolbar.
-	 *
-	 * @return  void
-	 *
-	 * @since   2.0.6
-	 */
-	protected function addToolbar()
-	{
-		// Add common button
-		if ($this->canCreate)
-		{
-			JToolbarHelper::addNew($this->getInstanceName() . '.add');
-		}
-
-		if ($this->canDelete)
-		{
-			JToolbarHelper::deleteList('', $this->getInstancesName() . '.delete');
-		}
-
-		if ($this->canEdit)
-		{
-			JToolbarHelper::publish($this->getInstancesName() . '.publish', 'JTOOLBAR_PUBLISH', true);
-			JToolbarHelper::unpublish($this->getInstancesName() . '.unpublish', 'JTOOLBAR_UNPUBLISH', true);
-			JToolbarHelper::checkin($this->getInstancesName() . '.checkin', 'JTOOLBAR_CHECKIN', true);
-		}
-	}
-
-	/**
-	 * Method for get columns
-	 *
-	 * @return  array
-	 *
-	 * @since   2.0.6
-	 */
-	public function getColumns()
-	{
-		return $this->columns;
 	}
 
 	/**
@@ -302,6 +216,49 @@ class RedshopViewList extends AbstractView
 	}
 
 	/**
+	 * Add the page title and toolbar.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.6
+	 */
+	public function addTitle()
+	{
+		$title = $this->getTitle();
+
+		if ($this->pagination->total)
+		{
+			$title .= '<span style="font - size: 0.5em; vertical - align: middle;"> (' . $this->pagination->total . ')</span>';
+		}
+
+		JToolbarHelper::title($title);
+	}
+
+	/**
+	 * Method for get page title.
+	 *
+	 * @return  string
+	 *
+	 * @since   2.0.6
+	 */
+	public function getTitle()
+	{
+		return JText::_('COM_REDSHOP_' . strtoupper($this->getInstanceName()) . '_MANAGEMENT');
+	}
+
+	/**
+	 * Method for get columns
+	 *
+	 * @return  array
+	 *
+	 * @since   2.0.6
+	 */
+	public function getColumns()
+	{
+		return $this->columns;
+	}
+
+	/**
 	 * Method for render 'Published' column
 	 *
 	 * @param   array  $config Row config.
@@ -317,7 +274,9 @@ class RedshopViewList extends AbstractView
 		$user             = JFactory::getUser();
 		$isCheckedOut     = $row->checked_out && $user->id != $row->checked_out;
 		$inlineEditEnable = Redshop::getConfig()->getBool('INLINE_EDITING');
-		$value = $row->{$config['dataCol']};
+		$value            = $row->{$config['dataCol']};
+		$primaryKey       = $this->getPrimaryKey();
+		$itemId           = $row->{$primaryKey};
 
 		if (in_array($config['dataCol'], $this->stateColumns))
 		{
@@ -337,16 +296,46 @@ class RedshopViewList extends AbstractView
 
 			if ($config['edit_link'])
 			{
-				$display = '<a href="index.php?option=com_redshop&task=' . $this->getInstanceName() . '.edit&id=' . $row->id . '">' . $value . '</a>';
+				$display = '<a href="index.php?option=com_redshop&task=' . $this->getInstanceName() . '.edit&' . $primaryKey . '=' . $itemId . '">'
+					. $value . '</a>';
 			}
 
-			return JHtml::_('redshopgrid.inline', $config['dataCol'], $value, $display, $row->id, $config['type']);
+			return JHtml::_('redshopgrid.inline', $config['dataCol'], $value, $display, $itemId, $config['type']);
 		}
 		elseif ($config['edit_link'] === true)
 		{
-			return '<a href="index.php?option=com_redshop&task=' . $this->getInstanceName() . '.edit&id=' . $row->id . '">' . $value . '</a>';
+			return '<a href="index.php?option=com_redshop&task=' . $this->getInstanceName() . '.edit&' . $primaryKey . '=' . $itemId . '">'
+				. $value . '</a>';
 		}
 
 		return '<div class="normal-data">' . $value . '</div>';
+	}
+
+	/**
+	 * Method for add toolbar.
+	 *
+	 * @return  void
+	 *
+	 * @since   2.0.6
+	 */
+	protected function addToolbar()
+	{
+		// Add common button
+		if ($this->canCreate)
+		{
+			JToolbarHelper::addNew($this->getInstanceName() . '.add');
+		}
+
+		if ($this->canDelete)
+		{
+			JToolbarHelper::deleteList('', $this->getInstancesName() . '.delete');
+		}
+
+		if ($this->canEdit)
+		{
+			JToolbarHelper::publish($this->getInstancesName() . '.publish', 'JTOOLBAR_PUBLISH', true);
+			JToolbarHelper::unpublish($this->getInstancesName() . '.unpublish', 'JTOOLBAR_UNPUBLISH', true);
+			JToolbarHelper::checkin($this->getInstancesName() . '.checkin', 'JTOOLBAR_CHECKIN', true);
+		}
 	}
 }
