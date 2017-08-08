e110c6cc35b6a1c3a363a903735eefe1df56615d
thongredweb <thong@redweb.dk>
2017-08-08 15:49:22 +0700

    [Travis] Update VAT Group test

--- tests/_pages/Joomla3/Administrator/VATGroupManagerJoomla3Page.php
+++ tests/_pages/Joomla3/Administrator/VATGroupManagerJoomla3Page.php
@@ -8,12 +8,12 @@
  */
 class VATGroupManagerJoomla3Page
 {
-    public static $URL = '/administrator/index.php?option=com_redshop&view=tax_groups';
-    public static $VATGroupName = "#jform_name";
-    public static $CheckAllVATGroup="//input[@onclick='Joomla.checkAll(this)']";
-    public static $VATGroupsNamePath="//div[@class='table-responsive']/table/tbody/tr/td[4]/a";
-    public static $VATGroupNameStatus="//div[@class='table-responsive']/table/tbody/tr/td[3]/a";
-    public static $VATGroupManagementSearch = "#filter_search";
+	public static $URL = '/administrator/index.php?option=com_redshop&view=tax_groups';
+	public static $VATGroupName = "#jform_name";
+	public static $CheckAllVATGroup = "//input[@onclick='Joomla.checkAll(this)']";
+	public static $VATGroupsNamePath = "//div[@class='table-responsive']/table/tbody/tr/td[4]/a";
+	public static $VATGroupNameStatus = "//div[@class='table-responsive']/table/tbody/tr/td[3]/a";
+	public static $VATGroupManagementSearch = "#filter_search";
 
-
+	public static $fieldMissing = "Field required: VAT / Tax Group Name";
 }
