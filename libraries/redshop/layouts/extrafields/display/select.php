8b20b1425929070a0fe430ad1b597f905be82aeb
Tu Vuong <tu@redweb.dk>
2017-08-10 13:46:20 +0700

    REDSHOP-4621 Move extra fields display to layouts

---
+++ libraries/redshop/layouts/extrafields/display/select.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package     RedSHOP.Frontend
+ * @subpackage  Template
+ *
+ * @copyright   Copyright (C) 2008 - 2017 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * $displayData extract
+ *
+ * @param   string  $data  Extra field data
+ */
+extract($displayData);
+
+echo urldecode(implode('<br>', $htmlData));
