727ce27c3f256aa2fc83ad6987e07678f7673ebc
Viet Vu <jooservices@gmail.com>
2017-08-10 17:09:16 +0700

    [Entity] Add product entity methods

--- libraries/redshop/entity/product.php
+++ libraries/redshop/entity/product.php
@@ -21,9 +21,9 @@ class RedshopEntityProduct extends RedshopEntity
 	/**
 	 * Get the associated table
 	 *
-	 * @param   string  $name  Main name of the Table. Example: Article for ContentTableArticle
+	 * @param   string $name Main name of the Table. Example: Article for ContentTableArticle
 	 *
-	 * @return  RedshopTable
+	 * @return  TableProduct_Detail|boolean
 	 */
 	public function getTable($name = null)
 	{
@@ -33,8 +33,8 @@ class RedshopEntityProduct extends RedshopEntity
 	/**
 	 * Default loading is trying to use the associated table
 	 *
-	 * @param   string  $key       Field name used as key
-	 * @param   string  $keyValue  Value used if it's not the $this->id property of the instance
+	 * @param   string $key      Field name used as key
+	 * @param   string $keyValue Value used if it's not the $this->id property of the instance
 	 *
 	 * @return  self
 	 */
@@ -51,5 +51,63 @@ class RedshopEntityProduct extends RedshopEntity
 		}
 
 		return $this;
+	}
+
+	/**
+	 *
+	 * @return  array<object>
+	 *
+	 * @since   2.0.7
+	 */
+	public function getMediaDetail()
+	{
+		$db    = JFactory::getDbo();
+		$query = $db->getQuery(true);
+		$query->select('*')
+			->from($db->quoteName('#__redshop_media'))
+			->where($db->quoteName('section_id') . ' = ' . (int) $this->getId())
+			->where($db->quoteName('media_section') . ' = ' . $db->quote('product'));
+
+		return $db->setQuery($query)->loadObjectlist();
+	}
+
+	/**
+	 *
+	 * @return  array<string>
+	 *
+	 * @since   2.0.7
+	 */
+	public function getCategories()
+	{
+		$db    = $this->getDbo();
+		$query = $db->getQuery(true)
+			->select($db->qn('name'))
+			->from($db->qn('#__redshop_product_category_xref', 'pcx'))
+			->leftjoin($db->qn('#__redshop_category', 'c') . ' ON ' . $db->qn('c.id') . ' = ' . $db->qn('pcx.category_id'))
+			->where($db->qn('pcx.product_id') . ' = ' . $db->q((int) $this->getId()))
+			->order($db->qn('c.name'));
+
+		return $db->setQuery($query)->loadObjectlist();
+	}
+
+	/**
+	 * Get array object of custom fields' data
+	 *
+	 * @return  array<object>|null
+	 *
+	 * @TODO    Use section const instead fixed value.
+	 * @since   2.0.7
+	 */
+	public function getCustomfieldsData()
+	{
+		$db    = $this->getDbo();
+		$query = $db->getQuery(true)
+			->select('*')
+			->from($db->quoteName('#__redshop_fields_data', 'fd'))
+			->innerJoin($db->quoteName('#__redshop_fields', 'f') . ' ON ' . $db->quoteName('fd.fieldid') . ' = ' . $db->quoteName('f.field_id'))
+			->where($db->quoteName('fd.section') . ' = 1')
+			->where($db->quoteName('fd.itemid') . ' = ' . (int) $this->getId());
+
+		return $db->setQuery($query)->loadObjectlist();
 	}
 }
