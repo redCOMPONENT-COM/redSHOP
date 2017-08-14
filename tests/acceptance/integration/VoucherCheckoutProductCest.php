a2f94c6c9fdb52401c85fae047e8937c01368bdc
nhung@redweb.dk <nhung@redweb.dk>
2017-08-14 10:34:46 +0700

    fix voucher discount

--- tests/acceptance/integration/VoucherCheckoutProductCest.php
+++ tests/acceptance/integration/VoucherCheckoutProductCest.php
@@ -17,29 +17,104 @@
  */
 class VoucherCheckoutProductCest
 {
+	/**
+	 * @var \Faker\Generator
+	 */
+	public $faker;
+
+	/**
+	 * @var string
+	 */
+	public $randomVoucherCode;
+
+	/**
+	 * @var int
+	 */
+	public $voucherAmount;
+
+	/**
+	 * @var int
+	 */
+	public $voucherCount;
+
+	/**
+	 * @var string
+	 */
+	public $startDate;
+
+	/**
+	 * @var string
+	 */
+	public $endDate;
+
+	/**
+	 * @var string
+	 */
+	public $randomCategoryName;
+
+	/**
+	 * @var string
+	 */
+	public $productName;
+
+	/**
+	 * @var int
+	 */
+	public $minimumPerProduct;
+
+	/**
+	 * @var int
+	 */
+	public $minimumQuantity;
+
+	/**
+	 * @var string
+	 */
+	public $discountStart;
+
+	/**
+	 * @var string
+	 */
+	public $discountEnd;
+
+	/**
+	 * @var int
+	 */
+	public $maximumQuantity;
+
+	/**
+	 * @var int
+	 */
+	public $randomProductNumber;
+
+	/**
+	 * @var int
+	 */
+	public $randomProductPrice;
+
+
 	public function __construct()
 	{
 		$this->faker = Faker\Factory::create();
 		//create voucher
 		$this->randomVoucherCode = $this->faker->bothify('VoucherCheckoutProductCest ?##?');
-		$this->voucherAmount = 10;
-		$this->voucherCount = $this->faker->numberBetween(99, 999);
-		$this->startDate = "21-06-2017";
-		$this->endDate = "07-07-2017";
+		$this->voucherAmount     = 10;
+		$this->voucherCount      = $this->faker->numberBetween(99, 999);
+		$this->startDate         = "21-06-2017";
+		$this->endDate           = "07-07-2017";
 
 		//create category
 		$this->randomCategoryName = $this->faker->bothify('TestingCategory ?##');
 
 		//create product
-		$this->productName = 'Testing Products' . rand(99, 999);
-		$this->minimumPerProduct = 1;
-		$this->minimumQuantity = 1;
-		$this->maximumQuantity = 5;
-		$this->discountStart = "12-12-2016";
-		$this->discountEnd = "23-05-2017";
+		$this->productName         = 'Testing Products' . rand(99, 999);
+		$this->minimumPerProduct   = 1;
+		$this->minimumQuantity     = 1;
+		$this->maximumQuantity     = 5;
+		$this->discountStart       = "12-12-2016";
+		$this->discountEnd         = "23-05-2017";
 		$this->randomProductNumber = rand(999, 9999);
-		$this->randomProductPrice = 24;
-
+		$this->randomProductPrice  = 24;
 	}
 
 	public function _before(AcceptanceTester $I)
@@ -50,8 +125,9 @@ class VoucherCheckoutProductCest
 	/**
 	 *
 	 * Function create category
+	 *
 	 * @param AcceptanceTester $I
-	 * @param $scenario
+	 * @param                  $scenario
 	 *
 	 */
 	public function createCategory(AcceptanceTester $I, $scenario)
@@ -66,7 +142,7 @@ class VoucherCheckoutProductCest
 	 * Function create product
 	 *
 	 * @param AcceptanceTester $I
-	 * @param $scenario
+	 * @param                  $scenario
 	 *
 	 * @depends createCategory
 	 *
@@ -82,7 +158,8 @@ class VoucherCheckoutProductCest
 	 * Function create voucher and checkout
 	 *
 	 * @param AcceptanceTester $I
-	 * @param $scenario
+	 *
+	 * @param                  $scenario
 	 *
 	 * @depends createProduct
 	 */
@@ -97,17 +174,16 @@ class VoucherCheckoutProductCest
 	/**
 	 * Function to Test Checkout Process of a Product using the Voucher Code
 	 *
-	 * @param   AcceptanceTester $I Actor Class Object
-	 * @param   String $scenario Scenario Variable
-	 * @param   Array $addressDetail Address Detail
-	 * @param   Array $shipmentDetail Shipping Address Detail
-	 * @param   string $productName Name of the Product
-	 * @param   string $categoryName Name of the Category
-	 * @param   string $voucherCode Code for the Coupon
+	 * @param   AcceptanceTester $I              Actor Class Object
+	 * @param   String           $scenario       Scenario Variable
+	 * @param   Array            $addressDetail  Address Detail
+	 * @param   Array            $shipmentDetail Shipping Address Detail
+	 * @param   string           $productName    Name of the Product
+	 * @param   string           $categoryName   Name of the Category
+	 * @param   string           $voucherCode    Code for the Coupon
 	 *
 	 * @return void
 	 */
-//	public function checkoutProductWithVoucherCode(AcceptanceTester $I, $scenario, $addressDetail, $shipmentDetail, $productName = 'redCOOKIE', $categoryName = 'Events and Forms', $voucherCode)
 	private function checkoutProductWithVoucherCode(AcceptanceTester $I, $productName, $categoryName, $voucherCode)
 	{
 		$I->amOnPage(\FrontEndProductManagerJoomla3Page::$URL);
@@ -119,16 +195,14 @@ class VoucherCheckoutProductCest
 		$I->click(\FrontEndProductManagerJoomla3Page::$addToCart);
 		$I->waitForText(\FrontEndProductManagerJoomla3Page::$alertSuccessMessage, 10, \FrontEndProductManagerJoomla3Page::$selectorSuccess);
 		$I->see(\FrontEndProductManagerJoomla3Page::$alertSuccessMessage, \FrontEndProductManagerJoomla3Page::$selectorSuccess);
-		$I->amOnPage('index.php?option=com_redshop&view=cart');
-//		$I->checkForPhpNoticesOrWarnings();
+		$I->amOnPage(\GiftCardCheckoutPage::$cartPageUrL);
 		$I->seeElement(['link' => $productName]);
-		$I->fillField(['id' => 'coupon_input'], $voucherCode);
-		$I->click(['id' => 'coupon_button']);
-//		$I->waitForText("The discount code is valid", 10, '.alert-success');
-//		$I->see("The discount code is valid", '.alert-success');
-		$I->see("$ 24,00", ['class' => "lc-subtotal"]);
-		$I->see("$ 10,00", ['class' => "lc-discount"]);
-		$I->see("$ 14,00", ['class' => "lc-total"]);
+		$I->fillField(\GiftCardCheckoutPage::$couponInput, $voucherCode);
+		$I->click(\GiftCardCheckoutPage::$couponButton);
+		$I->see(\GiftCardCheckoutPage::$messageInvalid, \AdminJ3Page::$selectorSuccess);
+		$I->see("DKK 24,00", \GiftCardCheckoutPage::$priceTotal);
+		$I->see("DKK 10,00", \GiftCardCheckoutPage::$priceDiscount);
+		$I->see("DKK 14,00", \GiftCardCheckoutPage::$priceEnd);
 	}
 
 
