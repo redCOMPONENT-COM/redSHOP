33a727210e453c940b4ebe8f5915fdc448544f78
lu.redweb <lu@redweb.dk>
2017-08-22 13:41:31 +0700

    Update code

--- component/site/helpers/rscarthelper.php
+++ component/site/helpers/rscarthelper.php
@@ -3121,6 +3121,8 @@ class rsCarthelper
 								}
 
 								$dispatcher->trigger('onReplaceShippingTemplate', array($d, &$data, $classname, $checked));
+
+								$data = str_replace("{gls_shipping_location}", "", $data);
 							}
 
 							$rate_data = str_replace("{shipping_rate_loop_start}", "", $rate_data);
