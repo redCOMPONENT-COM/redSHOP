54af9295fa4a2d1dcbdcd4cd1fce2acac833d87e
Nhung <nhung@redweb.dk>
2017-08-24 12:22:26 +0700

    resolve conflict

--- modules/site/mod_fb_albums/tmpl/default.php
+++ modules/site/mod_fb_albums/tmpl/default.php
@@ -19,7 +19,6 @@ $count = count($albums);
             <div class="tab-gallery">
                 <!-- Nav tabs -->
                 <ul class="nav nav-tabs" role="tablist">
-<<<<<<< HEAD
                     <?php foreach ($albums as $album): ?>
                         <li role="presentation"><a href="#<?php echo isset($album->id) ? $album->id : '' ?>" role="tab"
                                                    data-toggle="tab"><?php echo isset($album->name) ? $album->name : ''
@@ -43,39 +42,10 @@ $count = count($albums);
                             <?php endif; ?>
                         </div>
                     <?php endforeach; ?>
-=======
-					<?php foreach ($albums as $album): ?>
-                        <li role="presentation"><a href="#<?php echo isset($album->id) ? $album->id : '' ?>" role="tab"
-                                                   data-toggle="tab"><?php echo isset($album->name) ? $album->name : ''
-								?></a></li>
-					<?php endforeach; ?>
-                </ul>
-                <!-- Tab panes -->
-                <div class="tab-content">
-					<?php foreach ($albums as $album): ?>
-                        <div role="tabpanel" class="tab-pane" id="<?php echo isset($album->id) ? $album->id : '' ?>">
-							<?php if ($count > 0): ?>
-								<?php foreach ($album->photos->data as $photo): ?>
-                                    <a class="gallery_item cboxElement" href="<?php echo $photo->picture ?>"
-                                       title="<?php echo isset($album->name) ? $album->name : ''
-									   ?>" style="background: url('<?php echo $photo->picture ?>');">
-                                        <img src="<?php echo $photo->picture ?>" alt="<?php echo $album->name ?>"/>
-                                    </a>
-								<?php endforeach; ?>
-							<?php else: ?>
-								<?php echo JText::_('MOD_FB_ALBUMS_NO_PHOTO_IN_ALBUM'); ?>
-							<?php endif; ?>
-                        </div>
-					<?php endforeach; ?>
->>>>>>> upstream/develop
                 </div>
             </div>
         </div>
     </div>
 <?php else: ?>
-<<<<<<< HEAD
     <?php echo JText::_('MOD_FB_ALBUMS_NO_ALBUM'); ?>
-=======
-	<?php echo JText::_('MOD_FB_ALBUMS_NO_ALBUM'); ?>
->>>>>>> upstream/develop
 <?php endif; ?>
