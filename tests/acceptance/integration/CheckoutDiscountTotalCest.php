1ed4f660371268d49d0b4ce6804ba7f2fc61a6e6
Nhung <nhung@redweb.dk>
2017-08-25 14:01:18 +0700

    checkout with discount total

--- tests/acceptance/integration/CheckoutDiscountTotalCest.php
+++ tests/acceptance/integration/CheckoutDiscountTotalCest.php
@@ -5,7 +5,8 @@
 
 use AcceptanceTester\CategoryManagerJoomla3Steps;
 use AcceptanceTester\ProductManagerJoomla3Steps;
-use AcceptanceTester\MassDiscountManagerJoomla3Steps;
+use AcceptanceTester\ProductCheckoutManagerJoomla3Steps;
+use AcceptanceTester\DiscountManagerJoomla3Steps;
 class CheckoutDiscountTotalCest
 {
 
@@ -14,13 +15,7 @@ class CheckoutDiscountTotalCest
 
 		$this->faker = Faker\Factory::create();
 		$this->ProductName = 'ProductName' . rand(100, 999);
-		$this->MassDiscountName = 'MassDiscount' . rand(10, 100);
-		$this->MassDiscountNameSave = 'MassDiscountSave' . rand(10, 1000);
-		$this->MassDiscountNameEdit = 'Edit' . $this->MassDiscountName;
 		$this->CategoryName = "CategoryName" . rand(1, 100);
-		$this->ManufactureName = "ManufactureName" . rand(1, 10);
-		$this->MassDiscountAmoutTotal = 90;
-		$this->MassDiscountPercent = 0.3;
 		$this->minimumPerProduct = 1;
 		$this->minimumQuantity = 1;
 		$this->maximumQuantity = $this->faker->numberBetween(100, 1000);
@@ -29,18 +24,19 @@ class CheckoutDiscountTotalCest
 		$this->randomProductNumber = $this->faker->numberBetween(999, 9999);
 		$this->randomProductPrice = 100;
 
-		$this->subtotal="DKK 10,00";
+		$this->subtotal="DKK 100,00";
 		$this->Discount ="DKK 0,00";
-		$this->Total="DKK 10,00";
+		$this->Total="DKK 50,00";
 
 		$this->discountName = 'Discount' . rand(1, 100);
-		$this->amount = $this->faker->numberBetween(100, 999);
-		$this->discountAmount = $this->faker->numberBetween(10, 100);
-		$this->newAmount = $this->faker->numberBetween(100, 999);
+		$this->amount = 150;
+		$this->discountAmount = 50;
 		$this->startDate = '13-06-2017';
 		$this->endDate = '13-08-2017';
 		$this->shopperGroup = 'Default Private';
 		$this->discountType = 'Total';
+		$this->discountCondition='Lower';
+
 	}
 
 
@@ -51,10 +47,10 @@ class CheckoutDiscountTotalCest
 
 	/**
 	 * Step1 : create category
-	 * Step2 : create product
-	 * Step3 : Create Mass Discount
+	 * Step2 : create product have price is 100
+	 * Step3 : Create Mass  and create discount lower 150 and have discount is 50
 	 * Step4 : Goes on frontend and checkout with this product
-	 * Step5 : Delete all data
+	 * Step5 : Delete data
 	 *
 	 * @param AcceptanceTester $I
 	 * @param                  $scenario
@@ -70,25 +66,30 @@ class CheckoutDiscountTotalCest
 		$I->createProductSave($this->ProductName, $this->CategoryName, $this->randomProductNumber, $this->randomProductPrice, $this->minimumPerProduct, $this->minimumQuantity, $this->maximumQuantity, $this->discountStart, $this->discountEnd);
 
 		$I->wantTo('Test Discount creation with save and close button in Administrator');
-		$I = new AcceptanceTester\DiscountManagerJoomla3Steps($scenario);
+		$I = new DiscountManagerJoomla3Steps($scenario);
 		$I->wantTo('Create a Discount');
-		$I->addDiscount($this->discountName, $this->amount, $this->discountAmount, $this->shopperGroup, $this->discountType);
+		$I->addDiscount($this->discountName, $this->amount, $this->discountAmount, $this->shopperGroup, $this->discountType,$this->discountCondition);
+
+		$I->wantTo('Checkout with discount at total');
+		$I = new ProductCheckoutManagerJoomla3Steps($scenario);
+		$I->checkoutWithDiscount($this->ProductName,$this->CategoryName,$this->subtotal,$this->Discount,$this->Total);
+
 	}
 
 
-//	public function clearUp(AcceptanceTester $I, $scenario)
-//	{
-//		$I->wantTo('Delete product');
-//		$I = new ProductManagerJoomla3Steps($scenario);
-//		$I->deleteProduct($this->ProductName);
-//
-//		$I->wantTo('Delete Category');
-//		$I = new AcceptanceTester\CategoryManagerJoomla3Steps($scenario);
-//		$I->deleteCategory($this->CategoryName);
-//
-//		$I = new MassDiscountManagerJoomla3Steps($scenario);
-//		$I->wantTo('Test check add Mass discount ');
-//		$I->deleteMassDiscountOK($this->MassDiscountName);
-//	}
+	public function clearUp(AcceptanceTester $I, $scenario)
+	{
+		$I->wantTo('Delete product');
+		$I = new ProductManagerJoomla3Steps($scenario);
+		$I->deleteProduct($this->ProductName);
+
+		$I->wantTo('Delete Category');
+		$I = new CategoryManagerJoomla3Steps($scenario);
+		$I->deleteCategory($this->CategoryName);
+
+		$I->wantTo('Delete discount total');
+		$I=new DiscountManagerJoomla3Steps($scenario);
+		$I->deleteDiscount($this->discountName,$this->amount);
+	}
 
 }
