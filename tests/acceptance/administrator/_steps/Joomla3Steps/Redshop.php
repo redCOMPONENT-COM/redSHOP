b4c3a48ce97b4399a384036951e8ef8159bd54cc
Nhung <nhung@redweb.dk>
2017-08-28 14:14:28 +0700

    change code

--- tests/acceptance/administrator/_steps/Joomla3Steps/Redshop.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/Redshop.php
@@ -6,10 +6,9 @@
  * @copyright   Copyright (C) 2008 - 2017 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
-namespace AcceptanceTester;
 
 
-class Redshop extends \AcceptanceTester
+class Redshop
 {
 	/**
 	 * Clear all tables.
