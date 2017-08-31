2e0acccdd642166f58ec43cffa907fd8bc7960b5
Nhung <nhung@redweb.dk>
2017-08-31 10:12:37 +0700

    pull code for testing frontend

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 // This is global bootstrap for autoloading 
 include_once __DIR__.DIRECTORY_SEPARATOR . '_extensions/testsperformance.php';
+include_once dirname(__DIR__) . '/tests/_bootstrap_joomla.php';
 
 \Codeception\Util\Autoload::registerSuffix('Page', __DIR__.DIRECTORY_SEPARATOR.'_pages/Joomla3/Administrator');
 \Codeception\Util\Autoload::registerSuffix('Page', __DIR__.DIRECTORY_SEPARATOR.'_pages/Joomla3/System');
