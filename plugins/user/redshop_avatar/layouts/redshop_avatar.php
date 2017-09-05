9df44264e599a8f221ad94b3899c4c5b75df14c4
thongredweb <thong@redweb.dk>
2017-09-05 11:45:17 +0200

    [Optimize] redSHOP Avatar - plugin

--- plugins/user/redshop_avatar/layouts/redshop_avatar.php
+++ plugins/user/redshop_avatar/layouts/redshop_avatar.php
@@ -14,46 +14,55 @@ JHtml::_('bootstrap.tooltip');
 /**
  * $displayData extract
  *
- * @param   array  $displayData Extra field data
- * @param   string $image       Image file name
- * @param   string $key         Image file name
+ * @var   array  $displayData  Extra field data
+ * @var   string $image        Image file name
+ * @var   string $key          Image file name
  */
 extract($displayData);
 
 $user = JFactory::getUser();
-RedshopHelperMediaImage::requireDependencies();
 ?>
 <?php if (!empty($image)): ?>
-    <img src="<?php echo REDSHOP_FRONT_IMAGES_ABSPATH . $key . '/' . $user->id . '/' . $image ?>"/>
+    <img id="plg_user_redshop_avatar_img" src="<?php echo REDSHOP_FRONT_IMAGES_ABSPATH . $key . '/' . $user->id . '/thumb/' . $image ?>" />
 <?php endif; ?>
 <?php if (!$user->guest): ?>
-    <!-- Button to trigger modal -->
-    <a href="#myModal" role="button" class="btn" data-toggle="modal">Launch demo modal</a>
+    <script type="text/javascript">
+        (function($){
+            $(document).ready(function(){
+                $("#plg_redshop_avatar_upload").change(function(){
+                    $(this).closest('form').submit();
+                });
 
-    <!-- Modal -->
-    <div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
-         aria-hidden="true">
-        <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-            <h3 id="myModalLabel">Modal header</h3>
-        </div>
-        <div class="modal-body">
-			<?php echo RedshopLayoutHelper::render(
-				'dropzone',
-				array(
-					'id'           => $key,
-					'type'         => $key,
-					'sectionId'    => $user->id,
-					'mediaSection' => $key,
-					'file'         => null
-				),
-				__DIR__
-			);
-			?>
-        </div>
-        <div class="modal-footer">
-            <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-            <button class="btn btn-primary">Save changes</button>
-        </div>
-    </div>
+                $("#plg_redshop_avatar_form").submit(function(event){
+                    event.preventDefault();
+
+                    var formData = new FormData($(this)[0]);
+                    formData.append("avatar", $("#plg_redshop_avatar_upload")[0].files[0]);
+
+                    $.ajax({
+                        url: $(this).attr("action"),
+                        type: "POST",
+                        data: formData,
+                        async: false,
+                        cache: false,
+                        contentType: false,
+                        enctype: "multipart/form-data",
+                        processData: false,
+                        success: function(response) {
+                           if (response.length > 0) {
+                               $("img#plg_user_redshop_avatar_img").attr("src", response);
+                           }
+                        }
+                    });
+
+                    return false;
+                });
+            });
+        })(jQuery);
+    </script>
+    <form action="<?php echo JUri::root() ?>index.php?option=com_ajax&plugin=uploadAvatar&format=raw&method=post&group=user" method="post"
+          id="plg_redshop_avatar_form" enctype="multipart/form-data">
+        <input type="file" name="avatar" id="plg_redshop_avatar_upload"/>
+        <?php echo JHtml::_('form.token') ?>
+    </form>
 <?php endif;
