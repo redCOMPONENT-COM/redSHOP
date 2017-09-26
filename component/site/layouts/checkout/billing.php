548010d72ccbeb686882c78b19a1eb431faa8438
thongredweb <thong@redweb.dk>
2017-09-26 13:56:11 +0700

    Merge branch 'develop' of https://github.com/redCOMPONENT-COM/redSHOP into REDSHOP-improve
    # Conflicts:
    #	component/site/helpers/rscarthelper.php
    #	plugins/redshop_shipping/default_shipping_gls/default_shipping_gls.php

--- component/site/layouts/checkout/billing.php
+++ component/site/layouts/checkout/billing.php
@@ -9,15 +9,15 @@
 
 defined('_JEXEC') or die;
 
-$dispatcher = JDispatcher::getInstance();
+$dispatcher = RedshopHelperUtility::getDispatcher();
 JPluginHelper::importPlugin('redshop_checkout');
 $openToStretcher = 0;
-$company = "hidden";
-$customer = "hidden";
+$company         = "hidden";
+$customer        = "hidden";
 
 if (Redshop::getConfig()->get('ALLOW_CUSTOMER_REGISTER_TYPE') == 0)
 {
-	$company = "";
+	$company  = "";
 	$customer = "";
 }
 elseif (Redshop::getConfig()->get('ALLOW_CUSTOMER_REGISTER_TYPE') == 1)
@@ -31,30 +31,32 @@ elseif (Redshop::getConfig()->get('ALLOW_CUSTOMER_REGISTER_TYPE') == 2)
 
 $isCompany = $openToStretcher == 1 ? 1 : 0;
 
-$lists['shipping_customer_field'] = RedshopHelperExtrafields::listAllField(14);
-$lists['shipping_company_field']  = RedshopHelperExtrafields::listAllField(15);
+$lists['shipping_customer_field'] = RedshopHelperExtrafields::listAllField(RedshopHelperExtrafields::SECTION_PRIVATE_SHIPPING_ADDRESS);
+$lists['shipping_company_field']  = RedshopHelperExtrafields::listAllField(RedshopHelperExtrafields::SECTION_COMPANY_SHIPPING_ADDRESS);
 ?>
 
 <div class="form-group">
-	<label class="radio-inline <?php echo $customer;?>">
-		<input type="radio" name="togglerchecker" class="toggler" onclick="getBillingTemplate(this);" value="0" <?php echo ($isCompany == 0) ? 'checked="checked"' : '' ?> billing_type="private"/>
-		<?php echo JText::_('COM_REDSHOP_USER_REGISTRATION');?>
-	</label>
-	<label class="radio-inline <?php echo $company;?>">
-		<input type="radio" name="togglerchecker" class="toggler" onclick="getBillingTemplate(this);" value="1"  <?php echo ($isCompany == 1) ? 'checked="checked"' : '' ?> billing_type="company"/>
-		<?php echo JText::_('COM_REDSHOP_COMPANY_REGISTRATION');?>
-	</label>
+    <label class="radio-inline <?php echo $customer; ?>">
+        <input type="radio" name="togglerchecker" class="toggler" onclick="getBillingTemplate(this);"
+               value="0" <?php echo ($isCompany == 0) ? 'checked="checked"' : '' ?> billing_type="private" />
+		<?php echo JText::_('COM_REDSHOP_USER_REGISTRATION'); ?>
+    </label>
+    <label class="radio-inline <?php echo $company; ?>">
+        <input type="radio" name="togglerchecker" class="toggler" onclick="getBillingTemplate(this);"
+               value="1" <?php echo ($isCompany == 1) ? 'checked="checked"' : '' ?> billing_type="company" />
+		<?php echo JText::_('COM_REDSHOP_COMPANY_REGISTRATION'); ?>
+    </label>
 	<?php $dispatcher->trigger('onRenderOnstepCheckout'); ?>
 </div>
 <div id="wrapper-billing"></div>
 <?php if (!JFactory::getUser()->id && Redshop::getConfig()->get('SHIPPING_METHOD_ENABLE')) : ?>
-<div class="form-group">
-	<label for="billisship">
-		<input class="toggler" type="checkbox" id="billisship" name="billisship" value="1" onclick="billingIsShipping(this);" checked="" />
-		<?php echo JText::_('COM_REDSHOP_SHIPPING_SAME_AS_BILLING'); ?>
-	</label>
-</div>
-<div id="divShipping" style="display: none">
-	<?php echo rsUserHelper::getInstance()->getShippingTable(array(), $isCompany, $lists); ?>
-</div>
+    <div class="form-group">
+        <label for="billisship">
+            <input class="toggler" type="checkbox" id="billisship" name="billisship" value="1" onclick="billingIsShipping(this);" checked="" />
+			<?php echo JText::_('COM_REDSHOP_SHIPPING_SAME_AS_BILLING'); ?>
+        </label>
+    </div>
+    <div id="divShipping" style="display: none">
+		<?php echo RedshopHelperShipping::getShippingTable(array(), $isCompany, $lists); ?>
+    </div>
 <?php endif; ?>
