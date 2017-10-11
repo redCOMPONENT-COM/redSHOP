2438196a92fc67af28690e11927817ba6547ecd3
Nhung <nhung@redweb.dk>
2017-10-11 11:24:09 +0700

    fix bad locator

--- tests/_pages/Joomla3/Administrator/CategoryManagerJ3Page.php
+++ tests/_pages/Joomla3/Administrator/CategoryManagerJ3Page.php
@@ -15,142 +15,134 @@
  */
 class CategoryManagerJ3Page extends AdminJ3Page
 {
-    // Include url of current page
-    public static $URL = '/administrator/index.php?option=com_redshop&view=categories';
+	// Include url of current page
+	public static $URL = '/administrator/index.php?option=com_redshop&view=categories';
 
-    public static $URLNew = 'administrator/index.php?option=com_redshop&view=category&layout=edit';
+	public static $URLNew = 'administrator/index.php?option=com_redshop&view=category&layout=edit';
 
-    public static $URLEdit = '/administrator/index.php?option=com_redshop&view=category&layout=edit&id=';
-
-
-    //page name
-    public static $pageManageName = "Category Management";
+	public static $URLEdit = '/administrator/index.php?option=com_redshop&view=category&layout=edit&id=';
 
 
-    public static $headPageName = ['xpath' => "//h1"];
-
-    public static $categoryName = "#jform_name";
-
-//	public static $categoryPage="#"
-
-    public static $categoryFilter = ['id' => 'filter_search'];
-
-    public static $categoryTemplateDropDown = "//div[@id='filter_category_template']/a";
-
-    public static $categorySearch = "//button[@onclick=\"document.adminForm.submit();\"]";
-
-    public static $categoryResultRow = "//div[@class='table-responsive']/table/tbody/tr/td[5]";
-
-    public static $categoryId = "//div[@class='table-responsive']/table/tbody/tr/td[9]";
-
-    public static $categoryCheckInRow = "//div[@class='table-responsive']/table/tbody/tr/td[3]";
-
-    public static $categoryStatePath = "//tbody/tr/td[7]/a";
-
-    public static $checkAll = "//input[@id='cb0']";
-
-    public static $checkAllCategory = "//input[@onclick='Joomla.checkAll(this)']";
-
-    public static $categoryTemplateIDDropDown = "//div[@id='s2id_filter_category_template']/a";
-
-    public static $categoryNoPage = "#jform_products_per_page";
-
-    public static $parentCategory = "//div[@id='s2id_jform_parent_id']/a";
-
-    public static $choiceCategoryParent = "//div[@id='select2-result-label-13']/a";
-
-    public static $accessories = "//div[@id='s2id_category_accessory_search']/a";
-
-    public static $accessoriesFill = "#s2id_autogen1";
-
-    public static $categoryManagement = "/html/body/div/div/div/section[1]/div[1]/h1";
-
-    public static $tabAccessory = ['link' => "Accessories"];
-
-    public static $accessorySearch = ['xpath' => '//div[@id="s2id_category_accessory_search"]//a'];
-
-    public static $searchFirst = ['id' => "s2id_autogen1_search"];
-
-    public static $getAccessory = ['xpath' => "//h3[text()='Accessories']"];
+	//page name
+	public static $pageManageName = "Category Management";
 
 
-    //templatep
+	public static $headPageName = ['xpath' => "//h1"];
 
-    public static $template = "//div/div/div[@id='s2id_jform_more_template']/ul";
+	public static $categoryName = "#jform_name";
 
-    public static $choiceTemplate = '//ul[@class="select2-results"]/li[2]/div[@class="select2-result-label"]';
+	//	public static $categoryPage="#"
 
-    //selector
+	public static $categoryFilter = ['id' => 'filter_search'];
 
-    public static $selectorSuccess = '.alert-success';
+	public static $categoryTemplateDropDown = "//div[@id='filter_category_template']/a";
 
-    public static $selectorError = '.alert-danger';
+	public static $categorySearch = "//button[@onclick=\"document.adminForm.submit();\"]";
 
-    public static $selectorNamePage = '.page-title';
+	public static $categoryResultRow = ['xpath'=>'//table[@id=\'table-categories\']/tbody/tr[1]'];
+
+	public static $categoryId = "//table[@id='table-categories']//tr[1]//td[9]";
+
+	public static $categoryStatePath = ['xpath'=>'//table[@id=\'table-categories\']/tbody/tr/td[7]/a'];
+
+	public static $categoryTemplateIDDropDown = "//div[@id='s2id_filter_category_template']/a";
+
+	public static $categoryNoPage = "#jform_products_per_page";
+
+	public static $parentCategory = "//div[@id='s2id_jform_parent_id']/a";
+
+	public static $choiceCategoryParent = "//div[@id='select2-result-label-13']/a";
+
+	public static $accessories = "//div[@id='s2id_category_accessory_search']/a";
+
+	public static $accessoriesFill = "#s2id_autogen1";
+
+	public static $tabAccessory = ['link' => "Accessories"];
+
+	public static $accessorySearch = ['xpath' => '//div[@id="s2id_category_accessory_search"]//a'];
+
+	public static $searchFirst = ['id' => "s2id_autogen1_search"];
+
+	public static $getAccessory = ['xpath' => "//h3[text()='Accessories']"];
 
 
-    //message
-    public static $messageSaveSuccess = "Item saved.";
+	//templatep
 
-    public static $messageError = "Error";
+	public static $template = "//div/div/div[@id='s2id_jform_more_template']/ul";
 
-    public static $messageSuccess = "Message";
+	public static $choiceTemplate = '//ul[@class="select2-results"]/li[2]/div[@class="select2-result-label"]';
 
-    public static $messageDeleteSuccess = "1 item successfully deleted";
+	//selector
 
-    //button
+	public static $selectorSuccess = '.alert-success';
 
-    public static $newButton = "New";
+	public static $selectorError = '.alert-danger';
 
-    public static $saveButton = "Save";
+	public static $selectorNamePage = '.page-title';
 
-    public static $unpublishButton = "Unpublish";
 
-    public static $publishButton = "Publish";
+	//message
+	public static $messageSaveSuccess = "Item saved.";
 
-    public static $saveCloseButton = "Save & Close";
+	public static $messageError = "Error";
 
-    public static $deleteButton = "Delete";
+	public static $messageSuccess = "Message";
 
-    public static $editButton = "Edit";
+	public static $messageDeleteSuccess = "1 item successfully deleted";
 
-    public static $saveNewButton = "Save & New";
+	//button
 
-    public static $cancelButton = "Cancel";
+	public static $newButton = "New";
 
-    public static $checkInButton = "Check-in";
+	public static $saveButton = "Save";
 
-    /**
-     * Function to get the Path for Template ID
-     *
-     * @param   String $templateIDName Name of the Template
-     *
-     * @return string
-     */
-    public function categoryTemplateID($templateIDName)
-    {
-        $path = "//div[@id='s2id_filter_category_template']/div/ul/li[contains(text(), '" . $templateIDName . "')]";
+	public static $unpublishButton = "Unpublish";
 
-        return $path;
-    }
+	public static $publishButton = "Publish";
 
-    /**
-     * Function to get the Path for Template
-     *
-     * @param   String $templateName Name of the Template
-     *
-     * @return string
-     */
-    public function categoryTemplate($templateName)
-    {
-        $path = "//div[@id='filter_category_template']/div/ul/li[contains(text(), '" . $templateName . "')]";
+	public static $saveCloseButton = "Save & Close";
 
-        return $path;
-    }
+	public static $deleteButton = "Delete";
 
-    public function xPathAccessory($accessoryName)
-    {
-        $path = ['xpath' => "//span[contains(text(), '" . $accessoryName . "')]"];
-        return $path;
-    }
+	public static $editButton = "Edit";
+
+	public static $saveNewButton = "Save & New";
+
+	public static $cancelButton = "Cancel";
+
+	public static $checkInButton = "Check-in";
+
+	/**
+	 * Function to get the Path for Template ID
+	 *
+	 * @param   String $templateIDName Name of the Template
+	 *
+	 * @return string
+	 */
+	public function categoryTemplateID($templateIDName)
+	{
+		$path = "//div[@id='s2id_filter_category_template']/div/ul/li[contains(text(), '" . $templateIDName . "')]";
+
+		return $path;
+	}
+
+	/**
+	 * Function to get the Path for Template
+	 *
+	 * @param   String $templateName Name of the Template
+	 *
+	 * @return string
+	 */
+	public function categoryTemplate($templateName)
+	{
+		$path = "//div[@id='filter_category_template']/div/ul/li[contains(text(), '" . $templateName . "')]";
+
+		return $path;
+	}
+
+	public function xPathAccessory($accessoryName)
+	{
+		$path = ['xpath' => "//span[contains(text(), '" . $accessoryName . "')]"];
+		return $path;
+	}
 }
