584ee37d69ca0fbc7e4e4cc9e9a068cfe29057bb
lu.redweb <lu@redweb.dk>
2017-10-12 16:12:27 +0700

    Update code

--- plugins/redshop_payment/rs_payment_payoo/rs_payment_payoo/extra_info.php
+++ plugins/redshop_payment/rs_payment_payoo/rs_payment_payoo/extra_info.php
@@ -16,44 +16,20 @@ $amount = RedshopHelperCurrency::convert($data['order']->order_total, '', 'VND')
 
 $name = $data['billinginfo']->lastname . ' ' . $data['billinginfo']->firstname;
 
-$urlNotify = urlencode(JURI::base() . "index.php?option=com_redshop&view=order_detail&tmpl=component&controller=order_detail&task=notify_payment&payment_plugin=rs_payment_giropay&orderid=" . $data['order_id']);
+$urlNotify = urlencode(JURI::base() . "index.php?option=com_redshop&view=order_detail&tmpl=component&controller=order_detail&task=notify_payment&payment_plugin=rs_payment_payoo&orderid=" . $data['order_id']);
 
-$xml = "<shops><shop><shop_id>" . $this->params->get("shopid") . "</shop_id><username>" . $this->params->get("username") . "</username><session>" . md5($data['order_id']) . "</session><shop_title>" . $this->params->get("shoptitle") . "</shop_title><shop_domain>" . JURI::base() . "</shop_domain><shop_back_url>" . $urlNotify . "</shop_back_url><notify_url>" . $urlNotify . "</notify_url><order_no>" . $data['order_id'] . "</order_no><order_cash_amount>" . $amount . "</order_cash_amount><order_ship_date>" . $today . "</order_ship_date><order_ship_days>2</order_ship_days><order_description>Payoo</order_description><validity_time>" . $validity . "</validity_time><customer><name>" . $name . "</name><phone>" . $data['billinginfo']->phone . "</phone><address>" . $data['billinginfo']->address . "</address><email>" . $data['billinginfo']->email . "</email></customer></shop></shops>";
+$xml = "<shops><shop><shop_id>" . $this->params->get("shopid") . "</shop_id><username>" . $this->params->get("username") . "</username><session>" . md5($data['order_id']) . "</session><shop_title>" . $this->params->get("shoptitle") . "</shop_title><shop_domain>" . JURI::base() . "</shop_domain><shop_back_url>" . $urlNotify . "</shop_back_url><order_no>" . $data['order_id'] . "</order_no><order_cash_amount>" . $amount . "</order_cash_amount><order_ship_date>" . $today . "</order_ship_date><order_ship_days>2</order_ship_days><order_description>Payoo</order_description><validity_time>" . $validity . "</validity_time><customer><name>" . $name . "</name><phone>" . $data['billinginfo']->phone . "</phone><address>" . $data['billinginfo']->address . "</address><email>" . $data['billinginfo']->email . "</email></customer></shop></shops>";
 
 $checkSum = hash('sha512', $this->params->get("checksumkey") . $xml);
 
 ?>
-<script src="https://newsandbox.payoo.com.vn/v2/merchants/methods.js"></script>
 
-<form id="frmPayByPayoo" class="test-form" name="frmPayByPayoo" action="https://newsandbox.payoo.com.vn/v2/paynow/" method="POST">
-	<div id="payoo-methods" style="width: 710px;"></div>
+<form id="frmPayByPayoo" name="frmPayByPayoo" action="https://newsandbox.payoo.com.vn/v2/paynow/" method="POST">
 		<input type="hidden" name="cmd" value="_cart" />
 		<input type="hidden" name="OrdersForPayoo" value="<?php echo $xml ?>" />
 		<input type="hidden" name="CheckSum" value="<?php echo $checkSum ?>" />
 </form>
-<script type="text/javascript">
-	function validate()
-	{
-		return confirm("Are your sure?");
-	};
 
-	Payoo.init({
-		id: '<?php echo $this->params->get("shopid") ?>',
-		url: '<?php echo JURI::base() ?>',
-		wrapper: 'Ecommerce',
-		selector: '#payoo-methods',
-		type: 'full',
-		autohide: false,
-		order: {
-			seller: '<?php echo $this->params->get("username") ?>',
-			amount: '<?php echo $amount ?>'
-		},
-
-		form: {
-			selector: '.test-form',
-			submit: function(form) {
-				return validate(form);
-			}
-		}
-	});
+<script type='text/javascript'>
+    document.getElementById('frmPayByPayoo').submit();
 </script>
