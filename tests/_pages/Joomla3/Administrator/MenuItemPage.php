b60f0a485fb0a47586d2991cfc1c9f62e1aca16e
Nhung <nhung@redweb.dk>
2017-10-20 16:47:27 +0700

    udpate joomlabrowser

---
+++ tests/_pages/Joomla3/Administrator/MenuItemPage.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nhung
+ * Date: 10/20/17
+ * Time: 2:53 PM
+ */
+
+class MenuItemPage
+{
+
+}
---
+++ tests/acceptance/administrator/_steps/Joomla3Steps/SetupMenuItemSteps.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nhung
+ * Date: 10/20/17
+ * Time: 2:28 PM
+ */
+
+namespace AcceptanceTester;
+
+
+class SetupMenuItemSteps
+{
+
+}
