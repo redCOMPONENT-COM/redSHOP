9fc2d550af69b9e29e4b603c52d8975d45a4d300
lu.redweb <lu@redweb.dk>
2017-11-01 11:11:08 +0700

    Update VN

--- plugins/redshop_payment/rs_payment_payoo/rs_payment_payoo/extra_info.php
+++ plugins/redshop_payment/rs_payment_payoo/rs_payment_payoo/extra_info.php
@@ -16,9 +16,19 @@ $amount = RedshopHelperCurrency::convert($data['order']->order_total, '', 'VND')
 
 $name = $data['billinginfo']->lastname . ' ' . $data['billinginfo']->firstname;
 
+$items = RedshopHelperOrder::getItems($data['order_id']);
+$description = "";
+
+foreach ($items as $item)
+{
+	$description .= $item->order_item_name . ' - SL: ' . $item->product_quantity . ' - T: ' . $item->product_final_price . '<br>';
+}
+
+$description = urlencode($description);
+
 $urlNotify = urlencode(JURI::base() . "index.php?option=com_redshop&view=order_detail&tmpl=component&controller=order_detail&task=notify_payment&payment_plugin=rs_payment_payoo&orderid=" . $data['order_id']);
 
-$xml = "<shops><shop><shop_id>" . $this->params->get("shopid") . "</shop_id><username>" . $this->params->get("username") . "</username><session>" . md5($data['order_id']) . "</session><shop_title>" . $this->params->get("shoptitle") . "</shop_title><shop_domain>" . JURI::base() . "</shop_domain><shop_back_url>" . $urlNotify . "</shop_back_url><order_no>" . $data['order_id'] . "</order_no><order_cash_amount>" . $amount . "</order_cash_amount><order_ship_date>" . $today . "</order_ship_date><order_ship_days>2</order_ship_days><order_description>Payoo</order_description><validity_time>" . $validity . "</validity_time><customer><name>" . $name . "</name><phone>" . $data['billinginfo']->phone . "</phone><address>" . $data['billinginfo']->address . "</address><email>" . $data['billinginfo']->email . "</email></customer></shop></shops>";
+$xml = "<shops><shop><shop_id>" . $this->params->get("shopid") . "</shop_id><username>" . $this->params->get("username") . "</username><session>" . md5($data['order_id']) . "</session><shop_title>" . $this->params->get("shoptitle") . "</shop_title><shop_domain>" . JURI::base() . "</shop_domain><shop_back_url>" . $urlNotify . "</shop_back_url><order_no>" . $data['order_id'] . "</order_no><order_cash_amount>" . $amount . "</order_cash_amount><order_ship_date>" . $today . "</order_ship_date><order_ship_days>2</order_ship_days><order_description>" . $description . "</order_description><validity_time>" . $validity . "</validity_time><customer><name>" . $name . "</name><phone>" . $data['billinginfo']->phone . "</phone><address>" . $data['billinginfo']->address . "</address><email>" . $data['billinginfo']->email . "</email></customer></shop></shops>";
 
 $checkSum = hash('sha512', $this->params->get("checksumkey") . $xml);
 
