62d9a8039204cefb8a76773e4d56a0b6545ffea0
Viet Vu <jooservices@gmail.com>
2017-11-15 19:06:42 +0700

    REDSHOP-4717 - Update RedshopEntityProduct: get & set categories

--- libraries/redshop/entity/product.php
+++ libraries/redshop/entity/product.php
@@ -19,14 +19,115 @@ defined('_JEXEC') or die;
 class RedshopEntityProduct extends RedshopEntity
 {
 	/**
+	 * @var   RedshopEntitiesCollection  Collections of categories
+	 */
+	protected $categories = null;
+
+	/**
 	 * Get the associated table
 	 *
-	 * @param   string  $name  Main name of the Table. Example: Article for ContentTableArticle
+	 * @param   string $name Main name of the Table. Example: Article for ContentTableArticle
 	 *
-	 * @return  RedshopTable
+	 * @return  JTable
 	 */
 	public function getTable($name = null)
 	{
 		return JTable::getInstance('Product_Detail', 'Table');
+	}
+
+	/**
+	 * @param   boolean  $reload  Force reload even it's cached
+	 *
+	 * @return  RedshopEntitiesCollection
+	 *
+	 * @since   2.1.0
+	 */
+	public function getCategories($reload = false)
+	{
+		if (null === $this->categories || $reload === true)
+		{
+			$this->loadCategories();
+		}
+
+		return $this->categories;
+	}
+
+	/**
+	 * @param   array    $ids             Array of categories' ids
+	 * @param   boolean  $removeAssigned  Remove all assigned categories
+	 *
+	 * @return  mixed                     A database cursor resource on success, boolean false on failure.
+	 */
+	public function setCategories($ids, $removeAssigned = false)
+	{
+		// Merge with assigned categories
+		if ($removeAssigned === false)
+		{
+			$this->categories = array_merge(is_array($this->getCategories()) ? $this->getCategories() : array(), $ids);
+		}
+		else
+		{
+			// Or just reset it with new ids
+			$this->categories = $ids;
+		}
+
+		$this->categories = array_unique($this->categories);
+
+		$db = JFactory::getDbo();
+
+		// Delete old assigned categories
+		$query = $db->getQuery(true)
+			->delete($db->qn('#__redshop_product_category_xref'))
+			->where($db->qn('product_id') . ' = ' . (int) $this->get('product_id'));
+		$db->setQuery($query)->execute();
+
+		// Assign new category
+		$query->clear()
+			->insert($db->qn('#__redshop_product_category_xref'))
+			->columns($db->qn(array('category_id', 'product_id')));
+
+		foreach ($this->categories as $id)
+		{
+			$query->values((int) $id . ' , ' . (int) $this->get('product_id'));
+		}
+
+		return $db->setQuery($query)->execute();
+	}
+
+	/**
+	 * Method for load child categories
+	 *
+	 * @return  self
+	 *
+	 * @since   2.1.0
+	 */
+	protected function loadCategories()
+	{
+		if (!$this->hasId())
+		{
+			return $this;
+		}
+
+		$this->categories = new RedshopEntitiesCollection;
+
+		$db    = JFactory::getDbo();
+		$query = $db->getQuery(true)
+			->select($db->qn('category_id'))
+			->from($db->qn('#__redshop_product_category_xref'))
+			->where($db->qn('product_id') . ' = ' . (int) $this->get('product_id'));
+
+		$results = $db->setQuery($query)->loadColumn();
+
+		if (empty($results))
+		{
+			return $this;
+		}
+
+		foreach ($results as $categoryId)
+		{
+			$this->categories->add(RedshopEntityCategory::getInstance($categoryId));
+		}
+
+		return $this;
 	}
 }
