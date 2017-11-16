45802aa460456ba4da2881a047cde2df1a3d7a66
thongredweb <thong@redweb.dk>
2017-11-16 16:17:50 +0700

    [Travis] Move gitsubmodule to travis only

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -2,6 +2,8 @@
 set -ev
 
 if [ "${ACCEPTANCE}" = "false" ]; then
+	mv .travis.gitmodules .gitsubmodules
+
 	git submodule update --init --recursive
 
 	# Following line uses a bot account to authenticate in github and make composer stable and faster, see https://redweb.atlassian.net/wiki/pages/viewpage.action?pageId=46694753
