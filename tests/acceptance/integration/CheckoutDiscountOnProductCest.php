ae599798dcbce190a0368a50bd4def0d961fe27f
Nhung Nguyen <nhungnth19951903@gmail.com>
2017-12-25 11:46:12 +0700

    update code

--- tests/acceptance/integration/CheckoutDiscountOnProductCest.php
+++ tests/acceptance/integration/CheckoutDiscountOnProductCest.php
@@ -131,19 +131,19 @@ class CheckoutDiscountOnProductCest
 		$this->discountAmount      = 50;
 		$this->groupName           = "Default Private";
 	}
-//
-//	/**
-//	 * Method for clean data.
-//	 *
-//	 * @param   mixed $scenario Scenario
-//	 *
-//	 * @return  void
-//	 */
-//	public function deleteData($scenario)
-//	{
-//		$I = new RedshopSteps($scenario);
-//		$I->clearAllData();
-//	}
+
+	/**
+	 * Method for clean data.
+	 *
+	 * @param   mixed $scenario Scenario
+	 *
+	 * @return  void
+	 */
+	public function deleteData($scenario)
+	{
+		$I = new RedshopSteps($scenario);
+		$I->clearAllData();
+	}
 
 	/**
 	 * Method run before test.
