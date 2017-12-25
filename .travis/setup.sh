8218f8e471a033b566007005ed480bcb2ce2687c
Nhung Nguyen <nhungnth19951903@gmail.com>
2017-12-25 14:48:52 +0700

    oconver code

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -11,27 +11,28 @@ if [ "${ACCEPTANCE}" = "false" ]; then
 	composer install --prefer-dist
 	composer install --working-dir ./libraries/redshop --ansi
 else
-    sudo apt-get update
-	sudo apt-get install apache2 libapache2-mod-fastcgi
-	# enable php-fpm
-	sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
-	sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/www.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/www.conf
-	sudo a2enmod rewrite actions fastcgi alias
-	echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-	sudo sed -i -e "s,www-data,travis,g" /etc/apache2/envvars
-	sudo chown -R travis:travis /var/lib/apache2/fastcgi
-	~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
-	# configure apache virtual hosts
-	sudo cp -f ./tests/travis-ci-apache.conf /etc/apache2/sites-available/000-default.conf
-	sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/000-default.conf
-	sudo service apache2 restart
+ 	sudo apt-get update
+ 	sudo apt-get install apache2 libapache2-mod-fastcgi
+ 	# enable php-fpm
+ 	sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
+ 	sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/www.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/www.conf
+ 	sudo a2enmod rewrite actions fastcgi alias
+ 	echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+ 	sudo sed -i -e "s,www-data,travis,g" /etc/apache2/envvars
+ 	sudo chown -R travis:travis /var/lib/apache2/fastcgi
+ 	~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
+ 	# configure apache virtual hosts
+ 	sudo cp -f ./tests/travis-ci-apache.conf /etc/apache2/sites-available/000-default.conf
+ 	sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/000-default.conf
+ 	sudo service apache2 restart
+
 
     # Get ChromeDriver for headless mode
-    	wget "https://chromedriver.storage.googleapis.com/2.33/chromedriver_linux64.zip"
-    	unzip "chromedriver_linux64.zip"
-    	mkdir -p /usr/local/bin
-    	sudo cp -a chromedriver /usr/local/bin
-    	sudo chmod +x /usr/local/bin/chromedriver
+    wget "https://chromedriver.storage.googleapis.com/2.33/chromedriver_linux64.zip"
+    unzip "chromedriver_linux64.zip"
+    mkdir -p /usr/local/bin
+    sudo cp -a chromedriver /usr/local/bin
+    sudo chmod +x /usr/local/bin/chromedriver
 
 	sh -e /etc/init.d/xvfb start
 	sleep 3
