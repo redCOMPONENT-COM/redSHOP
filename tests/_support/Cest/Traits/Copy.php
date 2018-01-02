08a0c99fe0c3d9f1f4f217c1fbe69a165d29b8da
Nhung Nguyen <nhungnth19951903@gmail.com>
2018-01-02 13:46:46 +0700

    update

--- tests/_support/Cest/Traits/Copy.php
+++ tests/_support/Cest/Traits/Copy.php
@@ -1,22 +1,37 @@
 <?php
 /**
- * Check Copy button without choice
+ * @package     RedShop
+ * @subpackage  Cest
+ * @copyright   Copyright (C) 2008 - 2017 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 namespace Cest\Traits;
 
 use Codeception\Scenario;
 use Step\AbstractStep;
-class Copy
+/**
+ * Trait class for test with copy feature
+ *
+ * @package  AcceptanceTester
+ *
+ * @link     http://codeception.com/docs/07-AdvancedUsage
+ *
+ * @since    2.0
+ */
+trait Copy
 {
     /**
-     * Method for click button "Copy" without choice
+     * Method for test button "Copy"
+     *
+     * @param   \AcceptanceTester  $tester    Tester
+     * @param   Scenario           $scenario  Scenario
      *
      * @return  void
      */
     public function copyWithoutChoice(\AcceptanceTester $tester, Scenario $scenario)
     {
-        $tester->wantTo('Administrator -> Button -> Check-in without choice.');
+        $tester->wantTo('Administrator -> Button -> Copy without choice.');
 
         $stepClass = $this->stepClass;
 
--- tests/_support/Cest/Traits/Edit.php
+++ tests/_support/Cest/Traits/Edit.php
@@ -1,13 +1,25 @@
 <?php
 /**
- * Check "Edit" button
+ * @package     RedShop
+ * @subpackage  Cest
+ * @copyright   Copyright (C) 2008 - 2015 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 namespace Cest\Traits;
 
 use Codeception\Scenario;
 use Step\AbstractStep;
-class Edit
+/**
+ * Trait class for test with edit feature
+ *
+ * @package  AcceptanceTester
+ *
+ * @link     http://codeception.com/docs/07-AdvancedUsage
+ *
+ * @since    2.0
+ */
+trait Edit
 {
     /**
      * Method for click button "Edit" without choice
