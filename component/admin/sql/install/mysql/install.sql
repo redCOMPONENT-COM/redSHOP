aad6b7e3158dfc8c7461e7339e203fc9370b3741
Viet Vu <jooservices@gmail.com>
2018-01-03 18:28:02 +0700

    ^ code update

--- component/admin/sql/install/mysql/install.sql
+++ component/admin/sql/install/mysql/install.sql
@@ -1,7 +1,6 @@
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 
--- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
 
