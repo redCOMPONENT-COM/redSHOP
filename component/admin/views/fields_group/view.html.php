040ac068e72b3cc12028cbbb792440a005dae788
Viet Vu <jooservices@gmail.com>
2018-01-03 19:10:26 +0700

    ^ code update

--- component/admin/views/fields_group/view.html.php
+++ component/admin/views/fields_group/view.html.php
@@ -5,7 +5,7 @@
  *
  * @copyright   Copyright (C) 2008 - 2017 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
- * @since       2.0.0.2.1
+ * @since       __DEPLOY_VERSION__
  */
 
 defined('_JEXEC') or die;
