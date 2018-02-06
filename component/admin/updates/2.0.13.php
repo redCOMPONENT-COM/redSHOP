ec64baa4b97e70e7900fb2c77e35eb4476901e94
Thong <thong@redweb.dk>
2018-02-06 13:59:00 +0700

    REDSHOP-4118 Implement new structure for store image

--- component/admin/updates/2.0.13.php
+++ component/admin/updates/2.0.13.php
@@ -12,34 +12,28 @@ defined('_JEXEC') or die;
 /**
  * Update class
  *
- * @package     Redshob.Update
+ * @package   Redshob.Update
  *
- * @since       2.0.13
+ * @since     2.0.13
  */
 class RedshopUpdate2013 extends RedshopInstallUpdate
 {
 	/**
-	 * Method for migrate category images.
+	 * Method for migrate manufacturer images.
 	 *
 	 * @return  void
 	 *
 	 * @since   2.0.13
 	 */
-	public function migrateCategoryImages()
+	public function migrateManufacturerImages()
 	{
 		$db = JFactory::getDbo();
 
 		$query = $db->getQuery(true)
-			->select($db->qn(array('id', 'name', 'category_full_image', 'category_back_full_image')))
-			->from($db->qn('#__redshop_category'))
-			->where(
-				'('
-				. $db->qn('category_full_image') . ' IS NOT NULL OR ' . $db->qn('category_full_image') . ' <> ' . $db->quote('')
-				. ') OR ('
-				. $db->qn('category_back_full_image') . ' IS NOT NULL OR ' . $db->qn('category_back_full_image') . ' <> ' . $db->quote('')
-				. ')'
-			)
-			->order($db->qn('lft'));
+			->select('*')
+			->from($db->qn('#__redshop_media'))
+			->where($db->qn('media_section') . ' = ' . $db->quote('manufacturer'))
+			->order($db->qn('section_id'));
 
 		$medias = $db->setQuery($query)->loadObjectList();
 
@@ -48,13 +42,26 @@ class RedshopUpdate2013 extends RedshopInstallUpdate
 			return;
 		}
 
-		$newBasePath = REDSHOP_MEDIA_IMAGE_RELPATH . 'category';
-		$oldBasePath = REDSHOP_FRONT_IMAGES_RELPATH . 'category';
+		$newBasePath = REDSHOP_MEDIA_IMAGE_RELPATH . 'manufacturer';
+		$oldBasePath = REDSHOP_FRONT_IMAGES_RELPATH . 'manufacturer';
 
 		foreach ($medias as $media)
 		{
+			/** @var RedshopTableMedia $table */
+			$table = RedshopTable::getAdminInstance('Media', array('ignore_request' => true), 'com_redshop');
+
+			$table->bind((array) $media);
+
+			// In case this media don't have media file. Delete this.
+			if (empty($table->media_name))
+			{
+				$table->delete();
+
+				continue;
+			}
+
 			// Prepare target folder.
-			$path = $newBasePath . '/' . $media->id;
+			$path = $newBasePath . '/' . $table->section_id;
 
 			if (!JFolder::exists($path))
 			{
@@ -67,76 +74,28 @@ class RedshopUpdate2013 extends RedshopInstallUpdate
 				JFile::copy(REDSHOP_MEDIA_IMAGE_RELPATH . 'index.html', $path . '/index.html');
 			}
 
-			if (!empty($media->category_full_image))
+			// Check old image exist.
+			$oldImagePath = $oldBasePath . '/' . $table->media_name;
+
+			if (!JFile::exists($oldImagePath))
 			{
-				$this->storeMedia($media, $media->category_full_image, 'full', $oldBasePath, $newBasePath);
+				continue;
 			}
 
-			if (!empty($media->category_full_image))
+			// Generate new image using MD5
+			$newFileName = md5(basename($table->media_name)) . '.' . JFile::getExt($table->media_name);
+
+			if (!JFile::copy($oldImagePath, $path . '/' . $newFileName))
 			{
-				$this->storeMedia($media, $media->category_back_full_image, 'back', $oldBasePath, $newBasePath);
+				continue;
 			}
+
+			// Update media data with new file name.
+			$table->media_name = $newFileName;
+			$table->store();
 		}
 
 		// Remove old folders
 		JFolder::delete($oldBasePath);
-	}
-
-	/**
-	 * Method for store media
-	 *
-	 * @param   object  $category     Category data
-	 * @param   string  $fileName     File name
-	 * @param   string  $scope        Scope of media.
-	 * @param   string  $oldBasePath  Scope of media.
-	 * @param   string  $newBasePath  Scope of media.
-	 *
-	 * @return  void
-	 *
-	 * @since   2.0.13
-	 */
-	private function storeMedia($category, $fileName, $scope, $oldBasePath, $newBasePath)
-	{
-		/** @var RedshopTableMedia $table */
-		$table = RedshopTable::getAdminInstance('Media', array('ignore_request' => true), 'com_redshop');
-
-		// Generate new image using MD5
-		$newFileName = md5(basename($fileName)) . '.' . JFile::getExt($fileName);
-
-		if (!$table->load(
-			array(
-				'media_name'    => $fileName,
-				'media_section' => 'category',
-				'section_id'    => $category->id,
-				'media_type'    => 'images'
-			)
-		))
-		{
-			$table->section_id    = $category->id;
-			$table->media_section = 'category';
-			$table->media_type    = 'images';
-		}
-
-		$table->media_alternate_text = $category->name;
-		$table->published            = 1;
-		$table->ordering             = 0;
-		$table->scope                = $scope;
-
-		// Check old image exist.
-		$oldImagePath = $oldBasePath . '/' . $fileName;
-
-		if (!JFile::exists($oldImagePath))
-		{
-			return;
-		}
-
-		if (!JFile::copy($oldImagePath, $newBasePath . '/' . $category->id . '/' . $newFileName))
-		{
-			return;
-		}
-
-		// Update media data with new file name.
-		$table->media_name = $newFileName;
-		$table->store();
 	}
 }
