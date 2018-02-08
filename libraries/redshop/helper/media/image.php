8e4687b1832112abc7d0e2cdf8b4cddb1cbf5c81
Thong <thong@redweb.dk>
2018-02-08 14:46:23 +0700

    Update jQuery UI 1.12.1 and media path

--- libraries/redshop/helper/media/image.php
+++ libraries/redshop/helper/media/image.php
@@ -70,14 +70,14 @@ class RedshopHelperMediaImage
 	 */
 	public static function requireDependencies()
 	{
-		JHtml::stylesheet('com_redshop/dropzone/dropzone.min.css', array(), true);
-		JHtml::stylesheet('com_redshop/cropper/cropper.min.css', array(), true);
+		JHtml::stylesheet('com_redshop/dropzone.min.css', array(), true);
+		JHtml::stylesheet('com_redshop/cropper.min.css', array(), true);
 		JHtml::stylesheet('com_redshop/lightbox2/css/lightbox.min.css', array(), true);
 		JHtml::stylesheet('com_redshop/redshop.media.min.css', array(), true);
 
-		JHtml::script('com_redshop/dropzone/dropzone.min.js', false, true);
-		JHtml::script('com_redshop/cropper/cropper.min.js', false, true);
-		JHtml::script('com_redshop/lightbox2/lightbox.min.js', false, true);
+		JHtml::script('com_redshop/dropzone.min.js', false, true);
+		JHtml::script('com_redshop/cropper.min.js', false, true);
+		JHtml::script('com_redshop/lightbox2.min.js', false, true);
 		JHtml::script('com_redshop/redshop.media.min.js', false, true);
 
 		return true;
--- libraries/redshop/library.php
+++ libraries/redshop/library.php
@@ -20,7 +20,7 @@ define('JPATH_REDSHOP_LIBRARY', __DIR__);
 
 // Define redSHOP Constant
 define('JPATH_REDSHOP_MEDIA', JPATH_SITE . '/media/com_redshop');
-define('REDSHOP_MEDIA_IMAGES_ABSPATH', JPATH_SITE . '/media/com_redshop/images/');
+define('REDSHOP_MEDIA_IMAGES_ABSPATH', JUri::root() . '/media/com_redshop/images/');
 define('JPATH_REDSHOP_TEMPLATE', JPATH_REDSHOP_MEDIA . '/templates');
 define('JSYSTEM_IMAGES_PATH', JUri::root() . 'media/system/images/');
 define('REDSHOP_FRONT_IMAGES_ABSPATH', JUri::root() . 'components/com_redshop/assets/images/');
