2dec7d9e0eda7104196ec1e4c029bc52daf8c185
Viet Vu <jooservices@gmail.com>
2018-03-16 17:33:58 +0700

    ^ Redirect to redSHOP Dasboard if invalid request in site.cart

--- component/site/controllers/cart.php
+++ component/site/controllers/cart.php
@@ -48,6 +48,13 @@ class RedshopControllerCart extends RedshopController
 		$post                     = $app->input->post->getArray();
 		$parentAccessoryProductId = $post['product_id'];
 
+		// Invalid request then redirect to dashboard
+		if (empty($parentAccessoryProductId))
+		{
+			$app->enqueueMessage(JText::_('COM_REDSHOP_CART_INVALID_REQUEST'), 'error');
+			$this->setRedirect(JRoute::_('index.php?option=com_redshop'));
+		}
+
 		$productHelper = productHelper::getInstance();
 		$itemId        = RedshopHelperRouter::getCartItemId();
 
