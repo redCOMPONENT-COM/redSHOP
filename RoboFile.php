1d36ae500fbddef72bf0e3b562725ed6d2d7e61b
Thong <thong@redweb.dk>
2018-03-27 17:16:25 +0700

    [Travis] Use Docker-Compose 1.20.1

--- RoboFile.php
+++ RoboFile.php
@@ -693,7 +693,6 @@ class RoboFile extends \Robo\Tasks
 	public function checkTravisWebserver()
 	{
 		$this->_exec('php tests/checkers/traviswebserverckecker.php http://localhost:8070/installation/index.php');
-		$this->_exec('php tests/checkers/traviswebserverckecker.php http://localhost:8070/releases/index.html');
 	}
 
 	/**
