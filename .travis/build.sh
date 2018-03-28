0622a8e1a94152d4ab9090116e47e38d1ba13e84
thongredweb <thong@redweb.dk>
2018-03-29 01:07:45 +0700

    Debug

--- .travis/build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-set -ev
-
-echo "$HOME/$TRAVIS_PULL_REQUEST-$TRAVIS_COMMIT.zip"
--- .travis/script.sh
+++ .travis/script.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
 set -ev
 
-if [ "${ACCEPTANCE}" = "false" ]; then
-	# Only run PHPCS on PHP 7.0
-	if [ "$(phpenv version-name)" = "7.0" ]; then
-		php tests/checkers/debugcode.php # Check missed debug code
-		php tests/checkers/phppec.php # Check PHP Parse
-		php tests/checkers/phpcs.php # Check PHP Codestyle.
-	fi
-elif [ "${ACCEPTANCE}" = "true" ]; then
-	mv tests/RoboFile.ini.dist tests/RoboFile.ini # Create Robo Config file.
-	mv tests/acceptance.suite.dist.yml tests/acceptance.suite.yml # Create travis system tests config file
-	php vendor/bin/robo run:tests 1 # Run Acceptance test
-	#- php vendor/bin/robo send:codeception-output-to-slack C02L0SE5E xoxp-2309442657-4789197868-4789233706-68cec7 # Send output to Slack.
-else
-	mv tests/RoboFile.ini.dist tests/RoboFile.ini # Create Robo Config file.
-	mv tests/acceptance.suite.dist.yml tests/acceptance.suite.yml # Create travis system tests config file
-	php vendor/bin/robo run:travis "${ACCEPTANCE}" # Run Acceptance test
-	#- php vendor/bin/robo send:codeception-output-to-slack C02L0SE5E xoxp-2309442657-4789197868-4789233706-68cec7 # Send output to Slack.
-fi
+echo "$HOME/$TRAVIS_PULL_REQUEST-$TRAVIS_COMMIT.zip"
