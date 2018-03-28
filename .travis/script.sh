0e0252c2da3cd840a876d5b1f55954c079232225
thongredweb <thong@redweb.dk>
2018-03-29 02:12:26 +0700

    Test docker build file

--- .travis/script.sh
+++ .travis/script.sh
@@ -4,7 +4,6 @@ composer config -g github-oauth.github.com "${GITHUB_TOKEN}"
 composer global require hirak/prestissimo
 composer install --working-dir ./libraries/redshop --ansi
 composer install --working-dir ./plugins/redshop_pdf/tcpdf/helper --ansi
-mkdir -p ./tests/joomla-cms/releases
 npm install
 mv gulp-config.sample.json gulp-config.json
 node_modules/.bin/gulp release --skip-version
