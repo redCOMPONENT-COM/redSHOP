4d22b4f7066cb582fa721b24a54583fd77d5207a
Thong <thong@redweb.dk>
2018-03-29 15:00:02 +0700

    Update test 1

--- RoboFile.php
+++ RoboFile.php
@@ -729,8 +729,8 @@ class RoboFile extends \Robo\Tasks
 
 		// Install Joomla + redSHOP
 		$this->taskCodecept()
-			// ->arg('--steps')
-			// ->arg('--debug')
+			->arg('--steps')
+			->arg('--debug')
 			->arg('--tap')
 			->arg('--fail-fast')
 			->arg('tests/acceptance/install/')
