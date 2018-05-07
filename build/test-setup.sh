7c6f00964e62c3474374e82b3367b02176c0ae9a
puneet0191 <puneet.0191@gmail.com>
2018-05-07 18:42:48 +0530

    Another update

--- build/test-setup.sh
+++ build/test-setup.sh
@@ -84,7 +84,7 @@ then
 else
   echo "Tests Runs Failed" >&2
   #send screenshot of failed test to Slack
-  vendor/bin/robo send:build-report-error-slack $CLOUD_NAME $API_KEY $API_SECRET $REPO $DRONE_PULL_REQUEST $SLACK_WEBHOOK $SLACK_CHANNEL $DRONE_BUILD_LINK
+  vendor/bin/robo send:build-report-error-slack $CLOUD_NAME $API_KEY $API_SECRET $GITHUB_REPO $DRONE_PULL_REQUEST $SLACK_WEBHOOK $SLACK_CHANNEL $DRONE_BUILD_LINK
   rm -r tests/_output/
   cd ../
   exit 1
