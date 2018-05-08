9370d0dd5df3fd12bc98f8f276c50046576566e1
VanTrieuThanh96 <thanhthanhvan96qn@gmail.com>
2018-05-08 16:05:54 +0700

    update format code

--- tests/_pages/Joomla3/Administrator/StockImagePage.php
+++ tests/_pages/Joomla3/Administrator/StockImagePage.php
@@ -15,62 +15,62 @@
 
 class StockImagePage extends AdminJ3Page
 {
-    //create new
-    public static $URL = '/administrator/index.php?option=com_redshop&view=stockimage';
+	//create new
+	public static $URL = '/administrator/index.php?option=com_redshop&view=stockimage';
 
-    /**
-     * @var string
-     */
-    public static $namePage = 'Stock Image Management';
+	/**
+	 * @var string
+	 */
+	public static $namePage = 'Stock Image Management';
 
-    /**
-     * @var string
-     */
-    public static $titleCreatePage = 'Stock Amount Image';
+	/**
+	 * @var string
+	 */
+	public static $titleCreatePage = 'Stock Amount Image';
 
-    /**
-     * @var array
-     */
-    public static $fieldStockName = ['id' => 'stock_amount_image_tooltip'];
+	/**
+	 * @var array
+	 */
+	public static $fieldStockName = ['id' => 'stock_amount_image_tooltip'];
 
-    /**
-     * @var array
-     */
-    public static $fieldSearchStock = ['id' => 's2id_autogen1_search'];
+	/**
+	 * @var array
+	 */
+	public static $fieldSearchStock = ['id' => 's2id_autogen1_search'];
 
-    /**
-     * @var array
-     */
-    public static $chooseStock = ['class' => 'select2-result-label'];
+	/**
+	 * @var array
+	 */
+	public static $chooseStock = ['class' => 'select2-result-label'];
 
-    /**
-     * @var array
-     */
-    public static $fieldDropStock = ['id' => 's2id_stockroom_id'];
+	/**
+	 * @var array
+	 */
+	public static $fieldDropStock = ['id' => 's2id_stockroom_id'];
 
-    /**
-     * @var array
-     */
-    public static $fieldSearchAmount = ['id' => 's2id_autogen2_search'];
+	/**
+	 * @var array
+	 */
+	public static $fieldSearchAmount = ['id' => 's2id_autogen2_search'];
 
-    /**
-     * @var array
-     */
-    public static $chooseAmount = ['class' => 'select2-result-label'];
+	/**
+	 * @var array
+	 */
+	public static $chooseAmount = ['class' => 'select2-result-label'];
 
-    /**
-     * @var array
-     */
-    public static $fieldDropAmount = ['id' => 's2id_stock_option'];
+	/**
+	 * @var array
+	 */
+	public static $fieldDropAmount = ['id' => 's2id_stock_option'];
 
-    /**
-     * @var array
-     */
-    public static $fieldQuantity = ['id' => 'stock_quantity'];
+	/**
+	 * @var array
+	 */
+	public static $fieldQuantity = ['id' => 'stock_quantity'];
 
-    /**
-     * @var array
-     */
-    public static $search = ['id' => 'filter'];
+	/**
+	 * @var array
+	 */
+	public static $search = ['id' => 'filter'];
 
 }
--- tests/acceptance/administrator/_steps/Joomla3Steps/StockImageSteps.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/StockImageSteps.php
@@ -13,87 +13,87 @@ namespace AcceptanceTester;
 
 class StockImageSteps extends AdminManagerJoomla3Steps
 {
-    /**
-     * Function to Create a Stock
-     *
-     * @param   string $name Name of the Stock which is to be Create
-     *
-     * @return void
-     */
-    public function addStockImage($name = 'Name Stock Image ?##?', $nameStockItem = 'default', $amountStock = 'Higher than', $quantity = '50')
-    {
-        $I = $this;
-        $I->amOnPage(\StockImagePage::$URL);
-        $I->click(\StockImagePage::$buttonNew);
-        $I->waitForText(\StockImagePage::$titleCreatePage, 5);
-        $I->fillField(\StockImagePage::$fieldStockName, $name);
-        $I->fillField(\StockImagePage::$fieldQuantity, $quantity);
-        $I->click(\StockImagePage::$fieldDropStock);
-        $I->fillField(\StockImagePage::$fieldSearchStock, $nameStockItem);
-        $I->click(\StockImagePage::$chooseStock);
-        $I->click(\StockImagePage::$fieldDropAmount);
-        $I->fillField(\StockImagePage::$fieldSearchAmount, $amountStock);
-        $I->click(\StockImagePage::$chooseAmount);
-        $I->click(\StockImagePage::$buttonSaveClose);
-        $I->waitForText(\StockImagePage::$namePage, 5);
-        $I->seeElement(['link' => $name]);
-    }
+	/**
+	 * Function to Create a Stock
+	 *
+	 * @param   string $name Name of the Stock which is to be Create
+	 *
+	 * @return void
+	 */
+	public function addStockImage($name = 'Name Stock Image ?##?', $nameStockItem = 'default', $amountStock = 'Higher than', $quantity = '50')
+	{
+		$I = $this;
+		$I->amOnPage(\StockImagePage::$URL);
+		$I->click(\StockImagePage::$buttonNew);
+		$I->waitForText(\StockImagePage::$titleCreatePage, 5);
+		$I->fillField(\StockImagePage::$fieldStockName, $name);
+		$I->fillField(\StockImagePage::$fieldQuantity, $quantity);
+		$I->click(\StockImagePage::$fieldDropStock);
+		$I->fillField(\StockImagePage::$fieldSearchStock, $nameStockItem);
+		$I->click(\StockImagePage::$chooseStock);
+		$I->click(\StockImagePage::$fieldDropAmount);
+		$I->fillField(\StockImagePage::$fieldSearchAmount, $amountStock);
+		$I->click(\StockImagePage::$chooseAmount);
+		$I->click(\StockImagePage::$buttonSaveClose);
+		$I->waitForText(\StockImagePage::$namePage, 5);
+		$I->seeElement(['link' => $name]);
+	}
 
-    /**
-     * Function to Edit a Stock
-     *
-     * @param   string $name Name of the Stock which is to be Edit
-     *
-     * @return void
-     */
-    public function updateStockImage($name = 'Name Stock Image ?##?', $newQuantity = '40')
-    {
-        $I = $this;
-        $I->amOnPage(\StockImagePage::$URL);
-        $I->searchStockImage($name);
-        $I->click($name);
-        $I->waitForText(\StockImagePage::$titleCreatePage, 30);
-        $I->checkForPhpNoticesOrWarnings();
-        $I->waitForElement(\StockImagePage::$fieldQuantity, 30);
-        $I->fillField(\StockImagePage::$fieldQuantity, $newQuantity);
-        $I->click(\StockImagePage::$buttonSaveClose);
-        $I->waitForText(\StockImagePage::$namePage, 30);
-        $I->seeElement(['link' => $name]);
-    }
+	/**
+	 * Function to Edit a Stock
+	 *
+	 * @param   string $name Name of the Stock which is to be Edit
+	 *
+	 * @return void
+	 */
+	public function updateStockImage($name = 'Name Stock Image ?##?', $newQuantity = '40')
+	{
+		$I = $this;
+		$I->amOnPage(\StockImagePage::$URL);
+		$I->searchStockImage($name);
+		$I->click($name);
+		$I->waitForText(\StockImagePage::$titleCreatePage, 30);
+		$I->checkForPhpNoticesOrWarnings();
+		$I->waitForElement(\StockImagePage::$fieldQuantity, 30);
+		$I->fillField(\StockImagePage::$fieldQuantity, $newQuantity);
+		$I->click(\StockImagePage::$buttonSaveClose);
+		$I->waitForText(\StockImagePage::$namePage, 30);
+		$I->seeElement(['link' => $name]);
+	}
 
-    /**
-     * Function to Delete a Stock
-     *
-     * @param   string $name Name of the Stock which is to be Delete
-     *
-     * @return void
-     */
-    public function deleteStockImage($name = 'Name Stock Image ?##?')
-    {
-        $I = $this;
-        $I->amOnPage(\StockImagePage::$URL);
-        $I->checkForPhpNoticesOrWarnings();
-        $I->waitForElement(\StockImagePage::$search, 30);
-        $I->searchStockImage($name);
-        $I->checkAllResults();
-        $I->waitForText(\StockImagePage::$buttonDelete, 30);
-        $I->click(\StockImagePage::$buttonDelete);
-        $I->dontSeeElement(['link' => $name]);
-    }
+	/**
+	 * Function to Delete a Stock
+	 *
+	 * @param   string $name Name of the Stock which is to be Delete
+	 *
+	 * @return void
+	 */
+	public function deleteStockImage($name = 'Name Stock Image ?##?')
+	{
+		$I = $this;
+		$I->amOnPage(\StockImagePage::$URL);
+		$I->checkForPhpNoticesOrWarnings();
+		$I->waitForElement(\StockImagePage::$search, 30);
+		$I->searchStockImage($name);
+		$I->checkAllResults();
+		$I->waitForText(\StockImagePage::$buttonDelete, 30);
+		$I->click(\StockImagePage::$buttonDelete);
+		$I->dontSeeElement(['link' => $name]);
+	}
 
-    /**
-     * Function to Search a Stock
-     *
-     * @param   string $name Name of the Stock which is to be Search
-     *
-     * @return void
-     */
-    public function searchStockImage($name)
-    {
-        $I = $this;
-        $I->wantTo('Search the Stock Image');
-        $I->waitForText(\StockImagePage::$namePage, 30);
-        $I->waitForElement(\StockImagePage::$search);
-        $I->filterListBySearching($name, \StockImagePage::$search);
-    }
+	/**
+	 * Function to Search a Stock
+	 *
+	 * @param   string $name Name of the Stock which is to be Search
+	 *
+	 * @return void
+	 */
+	public function searchStockImage($name)
+	{
+		$I = $this;
+		$I->wantTo('Search the Stock Image');
+		$I->waitForText(\StockImagePage::$namePage, 30);
+		$I->waitForElement(\StockImagePage::$search);
+		$I->filterListBySearching($name, \StockImagePage::$search);
+	}
 }
