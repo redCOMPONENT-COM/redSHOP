c7331f975e8320aba59bd494c86db52ea1a6e105
nhungnguyen123 <nhungnth19951903@gmail.com>
2018-05-17 14:11:29 +0700

    remove sudo for zip plugins

--- build/test-setup.sh
+++ build/test-setup.sh
@@ -44,13 +44,13 @@ cd $WORKSPACE
 mv gulp-config.json.jenkins.dist gulp-config.json
 gulp release --skip-version
 echo $CHANGE_ID
+cd tests
 ls
-cp /tests/www/tests/releases/redshop.zip .
-cd /tests/ww/tests/releases
 apt-get install zip
-sudo zip -r plugins.zip plugins
+zip -r plugins.zip plugins
+cd ..
+cp /tests/www/tests/releases/redshop.zip .
 cp /tests/www/tests/releases/plugins.zip .
-cd ../ ../ ../ ../
 
 #vendor/bin/robo upload:patch-from-jenkins-to-test-server $GITHUB_TOKEN $GITHUB_REPO_OWNER $REPO $CHANGE_ID
 
