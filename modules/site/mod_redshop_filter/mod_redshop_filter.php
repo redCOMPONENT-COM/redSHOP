e65a3b1000933c3e2b8497e7d92b45619e095acd
thongredweb <thong@redweb.dk>
2018-05-28 16:35:27 +0700

    REDSHOP-5067 Module Product filter not show Category, Manufacturer when user config

--- modules/site/mod_redshop_filter/mod_redshop_filter.php
+++ modules/site/mod_redshop_filter/mod_redshop_filter.php
@@ -9,14 +9,14 @@
 
 defined('_JEXEC') or die;
 
-require_once dirname(__FILE__) . '/helper.php';
+require_once __DIR__ . '/helper.php';
 JLoader::import('redshop.library');
 
 $productHelper      = productHelper::getInstance();
 $input              = JFactory::getApplication()->input;
 $cid                = $input->getInt('cid', 0);
 $mid                = $input->getInt('manufacturer_id', 0);
-$moduleClassSfx     = $params->get("moduleclass_sfx");
+$moduleClassSfx     = $params->get('moduleclass_sfx');
 $rootCategory       = $params->get('root_category', 0);
 $enableCategory     = $params->get('category');
 $enableManufacturer = $params->get('manufacturer');
@@ -32,11 +32,12 @@ $view               = $input->getCmd('view', '');
 $layout             = $input->getCmd('layout', '');
 $itemId             = $input->getInt('Itemid', 0);
 $keyword            = $input->getString('keyword', '');
-$action             = JRoute::_("index.php?option=com_redshop&view=search");
+$action             = JRoute::_('index.php?option=com_redshop&view=search');
 $getData            = $input->getArray();
 
 if (!empty($cid))
 {
+	/** @var RedshopModelCategory $categoryModel */
 	$categoryModel = JModelLegacy::getInstance('Category', 'RedshopModel');
 	$categoryModel->setId($cid);
 	$categoryModel->setState('include_sub_categories_products', true);
@@ -103,10 +104,11 @@ elseif ($view == 'search')
 }
 
 $customFields = ModRedshopFilter::getCustomFields($pids, $productFields);
-$rangeMin     = isset($getData['filterprice']['min']) ? $getData['filterprice']['min'] : $rangePrice['min'];
-$rangeMax     = isset($getData['filterprice']['max']) ? $getData['filterprice']['max'] : $rangePrice['max'];
-
-$pids = array_filter(array_unique($pids));
+$currentMin   = !empty($getData['filterprice']['min']) ? $getData['filterprice']['min'] : $rangePrice['min'];
+$currentMax   = !empty($getData['filterprice']['max']) ? $getData['filterprice']['max'] : $rangePrice['max'];
+$rangeMin     = $rangePrice['min'];
+$rangeMax     = $rangePrice['max'];
+$pids         = array_filter(array_unique($pids));
 
 if ($enablePrice)
 {
