3b5312906a9dfeee4a8861051c28d75144ee95df
VanTrieuThanh96 <thanhthanhvan96qn@gmail.com>
2018-05-31 16:11:41 +0700

    Term & condition

--- tests/_pages/Joomla3/Administrator/CheckoutProductChangeQuantityPage.php
+++ tests/_pages/Joomla3/Administrator/CheckoutProductChangeQuantityPage.php
@@ -15,38 +15,38 @@
  */
 class CheckoutProductChangeQuantityPage extends AdminJ3Page
 {
-    /**
-     * @var string
-     */
-    public static $URL = "/administrator/index.php?option=com_redshop&view=configuration";
+	/**
+	 * @var string
+	 */
+	public static $URL = "/administrator/index.php?option=com_redshop&view=configuration";
 
-    /**
-     * @var string
-     */
-    public static $Cart = 'Cart / Checkout';
+	/**
+	 * @var string
+	 */
+	public static $Cart = "Cart / Checkout";
 
-    /**
-     * @var array
-     */
-    public static $disableQuantity = ['id' => 'quantity_text_display0-lbl'];
+	/**
+	 * @var array
+	 */
+	public static $disableQuantity = ['id' => 'quantity_text_display0-lbl'];
 
-    /**
-     * @var array
-     */
-    public static $enableQuantity = ['id' => 'quantity_text_display1-lbl'];
+	/**
+	 * @var array
+	 */
+	public static $enableQuantity = ['id' => 'quantity_text_display1-lbl'];
 
-    /**
-     * @var array
-     */
-    public static $categoryTitle = ['class' => 'category_front_inside'];
+	/**
+	 * @var array
+	 */
+	public static $categoryTitle = ['class' => 'category_front_inside'];
 
-    /**
-     * @var array
-     */
-    public static $quantityField = ['id' => 'quantitybox0'];
+	/**
+	 * @var array
+	 */
+	public static $quantityField = ['id' => 'quantitybox0'];
 
-    /**
-     * @var array
-     */
-    public static $updateCartButton = ['xpath' => "//img[@onclick=\"document.update_cart0.task.value='update';document.update_cart0.submit();\"]"];
+	/**
+	 * @var array
+	 */
+	public static $updateCartButton = ['xpath' => "//img[@onclick=\"document.update_cart0.task.value='update';document.update_cart0.submit();\"]"];
 }
--- tests/acceptance/administrator/_steps/Joomla3Steps/CheckoutProductQuantityChangeSteps.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/CheckoutProductQuantityChangeSteps.php
@@ -19,47 +19,49 @@ namespace AcceptanceTester;
  */
 class CheckoutProductQuantityChangeSteps extends AdminManagerJoomla3Steps
 {
-    public function goOnFrontEnd($category)
-    {
-        $I = $this;
-        $I->amOnPage(\CheckoutProductChangeQuantityPage::$url);
-        $I->waitForElement(\CheckoutProductChangeQuantityPage::$categoryTitle, 30);
-        $I->click($category);
-        $I->click(\CheckoutProductChangeQuantityPage::$addToCart);
-        $I->amOnPage(\CheckoutProductChangeQuantityPage::$cartPageUrL);
-        $I->click(\CheckoutProductChangeQuantityPage::$quantityField);
-        $I->pressKey(\CheckoutProductChangeQuantityPage::$quantityField, \Facebook\WebDriver\WebDriverKeys::BACKSPACE);
-        $quantities = 10;
-        $quantity = str_split($quantities);
-        foreach ($quantity as $char) {
-            $I->pressKey(\CheckoutProductChangeQuantityPage::$quantityField, $char);
-        }
-        $I->waitForElement(\CheckoutProductChangeQuantityPage::$updateCartButton, 30);
-        $I->click(\CheckoutProductChangeQuantityPage::$updateCartButton);
-        $I->click(\CheckoutProductChangeQuantityPage::$checkoutButton);
-        $I->click(\CheckoutProductChangeQuantityPage::$bankTransfer);
-        $I->click(\CheckoutProductChangeQuantityPage::$checkoutButton);
-        $I->click(\CheckoutProductChangeQuantityPage::$termAndConditions);
-        $I->click(\CheckoutProductChangeQuantityPage::$checkoutFinalStep);
-    }
 
-    public function configChangeQuantityProduct()
-    {
-        $I = $this;
-        $I->amOnPage(\CheckoutProductChangeQuantityPage::$URL);
-        $I->click(\CheckoutProductChangeQuantityPage::$Cart);
-        $I->waitForElement(\CheckoutProductChangeQuantityPage::$enableQuantity, 30);
-        $I->click(\CheckoutProductChangeQuantityPage::$enableQuantity);
-        $I->click(\CheckoutProductChangeQuantityPage::$buttonSave);
-    }
+	public function configChangeQuantityProduct()
+	{
+		$I = $this;
+		$I->amOnPage(\CheckoutProductChangeQuantityPage::$URL);
+		$I->click(\CheckoutProductChangeQuantityPage::$Cart);
+		$I->waitForElement(\CheckoutProductChangeQuantityPage::$enableQuantity, 30);
+		$I->click(\CheckoutProductChangeQuantityPage::$enableQuantity);
+		$I->click(\CheckoutProductChangeQuantityPage::$buttonSave);
+	}
 
-    public function returnConfig()
-    {
-        $I = $this;
-        $I->amOnPage(\CheckoutProductChangeQuantityPage::$URL);
-        $I->click(\CheckoutProductChangeQuantityPage::$Cart);
-        $I->waitForElement(\CheckoutProductChangeQuantityPage::$disableQuantity, 30);
-        $I->click(\CheckoutProductChangeQuantityPage::$disableQuantity);
-        $I->click(\CheckoutProductChangeQuantityPage::$buttonSave);
-    }
+	public function goOnFrontEnd($category)
+	{
+		$I = $this;
+		$I->amOnPage(\CheckoutProductChangeQuantityPage::$url);
+		$I->waitForElement(\CheckoutProductChangeQuantityPage::$categoryTitle, 30);
+		$I->click($category);
+		$I->click(\CheckoutProductChangeQuantityPage::$addToCart);
+		$I->amOnPage(\CheckoutProductChangeQuantityPage::$cartPageUrL);
+		$I->click(\CheckoutProductChangeQuantityPage::$quantityField);
+		$I->pressKey(\CheckoutProductChangeQuantityPage::$quantityField, \Facebook\WebDriver\WebDriverKeys::BACKSPACE);
+		$quantities = 10;
+		$quantity = str_split($quantities);
+		foreach ($quantity as $char) {
+			$I->pressKey(\CheckoutProductChangeQuantityPage::$quantityField, $char);
+		}
+		$I->waitForElement(\CheckoutProductChangeQuantityPage::$updateCartButton, 30);
+		$I->click(\CheckoutProductChangeQuantityPage::$updateCartButton);
+		$I->click(\CheckoutProductChangeQuantityPage::$checkoutButton);
+		$I->click(\CheckoutProductChangeQuantityPage::$bankTransfer);
+		$I->click(\CheckoutProductChangeQuantityPage::$checkoutButton);
+		$I->waitForElement(\CheckoutProductChangeQuantityPage::$termAndConditions,30 );
+		$I->click(\CheckoutProductChangeQuantityPage::$termAndConditions);
+		$I->click(\CheckoutProductChangeQuantityPage::$checkoutFinalStep);
+	}
+
+	public function returnConfig()
+	{
+		$I = $this;
+		$I->amOnPage(\CheckoutProductChangeQuantityPage::$URL);
+		$I->click(\CheckoutProductChangeQuantityPage::$Cart);
+		$I->waitForElement(\CheckoutProductChangeQuantityPage::$disableQuantity, 30);
+		$I->click(\CheckoutProductChangeQuantityPage::$disableQuantity);
+		$I->click(\CheckoutProductChangeQuantityPage::$buttonSave);
+	}
 }
