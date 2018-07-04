fb88b5fabe497bf29eab09cf52a6873465a4dc2b
Nhung <nhung@redweb.dk>
2018-07-04 11:06:21 +0700

    Revert " add for checking"
    This reverts commit 3af7e8438036c811badefd3ffbbf5c4ad338fd99.

--- tests/acceptance/install/InstallRedShopCest.php
+++ tests/acceptance/install/InstallRedShopCest.php
@@ -28,8 +28,6 @@ class InstallRedShopCest
 	public function testInstallJoomla(AcceptanceTester $I)
 	{
 		$I->wantTo('Execute Joomla Installation');
-		$I->amOnPage('/');
-		$I->see('Main Configuration');
 		$I->installJoomlaRemovingInstallationFolder();
 		$I->doAdministratorLogin();
 		$I->setErrorReportingtoDevelopment();
