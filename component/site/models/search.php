bed569358137c1d43052b48b67586b4ee25277b7
hungnguyen <hungnguyen.ckc@gmail.com>
2018-07-16 18:26:38 +0700

    REDSHOP-5087 Fix module search not working

--- component/site/models/search.php
+++ component/site/models/search.php
@@ -184,7 +184,7 @@ class RedshopModelSearch extends RedshopModel
 
 		$orderBy = $app->input->getString('order_by', '');
 		$this->setState('order_by', $orderBy);
-		$this->setState('template_id', $filter['template_id']);
+		$this->setState('template_id', !empty($filter['template_id']) ? $filter['template_id'] : $templateid);
 
 		$this->setState('productperpage', $perpageproduct);
 		$this->setState('list.limit', $limit);
