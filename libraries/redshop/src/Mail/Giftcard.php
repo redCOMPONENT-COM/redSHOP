8fd28d18fbf83de6503f65c476113243f2e1e91a
Viet Vu <jooservices@gmail.com>
2018-07-19 10:29:10 +0700

    ^ Move send gift card to src/Mail/Giftcard

---
+++ libraries/redshop/src/Mail/Giftcard.php
@@ -0,0 +1,183 @@
+<?php
+/**
+ * @package     RedShop
+ * @subpackage  Order
+ *
+ * @copyright   Copyright (C) 2008 - 2017 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Redshop\Mail;
+
+/**
+ * @package     Redshop\Mail
+ *
+ * @since       2.1.0
+ */
+class Giftcard
+{
+	/**
+	 * @param   integer $orderId Order id
+	 *
+	 * @return  boolean
+	 *
+	 * @since   2.1.0
+	 * @throws  \Exception
+	 */
+	public static function sendMail($orderId)
+	{
+		$giftCardTemplate = self::getTemplate();
+		$productHelper    = \productHelper::getInstance();
+		$giftCards        = \RedshopHelperOrder::giftCardItems((int) $orderId);
+
+		if (empty($giftCards))
+		{
+			return false;
+		}
+
+		foreach ($giftCards as $eachOrders)
+		{
+			$giftcardData  = \RedshopEntityGiftcard::getInstance($eachOrders->product_id)->getItem();
+			$giftCardValue = $productHelper->getProductFormattedPrice($giftcardData->giftcard_value, true);
+			$giftCardPrice = $eachOrders->product_final_price;
+			$giftCode      = \Redshop\Crypto\Helper\Encrypt::generateCustomRandomEncryptKey(12);
+			$userFields    = $productHelper->GetProdcutUserfield($eachOrders->order_item_id, 13);
+
+			/** @var \RedshopTableCoupon $couponItems */
+			$couponItems = \RedshopTable::getAdminInstance('Coupon');
+
+			if ($giftcardData->customer_amount)
+			{
+				$giftcardData->giftcard_value = $eachOrders->product_final_price;
+			}
+
+			$couponEndDate = mktime(0, 0, 0, date('m'), date('d') + $giftcardData->giftcard_validity, date('Y'));
+
+			$couponItems->code          = $giftCode;
+			$couponItems->type          = 0;
+			$couponItems->value         = $giftcardData->giftcard_value;
+			$couponItems->start_date    = \JFactory::getDate()->toSql();
+			$couponItems->end_date      = $couponEndDate === false ? JFactory::getDbo()->getNullDate() : JFactory::getDate($couponEndDate)->toSql();
+			$couponItems->effect        = 0;
+			$couponItems->userid        = 0;
+			$couponItems->amount_left   = 1;
+			$couponItems->published     = 1;
+			$couponItems->free_shipping = $giftcardData->free_shipping;
+
+			try
+			{
+				if ($couponItems->store())
+				{
+					return false;
+				}
+			}
+			catch (\Exception $exception)
+			{
+				throw new \Exception($exception->getMessage());
+			}
+
+			$mailSubject = $giftCardTemplate->mail_subject;
+			$mailSubject = str_replace('{giftcard_name}', $giftcardData->giftcard_name, $mailSubject);
+			$mailSubject = str_replace('{giftcard_price}', $productHelper->getProductFormattedPrice($giftCardPrice), $mailSubject);
+			$mailSubject = str_replace('{giftcard_value}', $giftCardValue, $mailSubject);
+			$mailSubject = str_replace('{giftcard_validity}', $giftcardData->giftcard_validity, $mailSubject);
+
+			$mailBody = $giftCardTemplate->mail_body;
+			$mailBody = str_replace('{giftcard_name}', $giftcardData->giftcard_name, $mailBody);
+			$mailBody = str_replace("{product_userfields}", $userFields, $mailBody);
+			$mailBody = str_replace("{giftcard_price_lbl}", \JText::_('LIB_REDSHOP_GIFTCARD_PRICE_LBL'), $mailBody);
+			$mailBody = str_replace("{giftcard_price}", $productHelper->getProductFormattedPrice($giftCardPrice), $mailBody);
+			$mailBody = str_replace("{giftcard_reciver_name_lbl}", \JText::_('LIB_REDSHOP_GIFTCARD_RECIVER_NAME_LBL'), $mailBody);
+			$mailBody = str_replace("{giftcard_reciver_email_lbl}", \JText::_('LIB_REDSHOP_GIFTCARD_RECIVER_EMAIL_LBL'), $mailBody);
+			$mailBody = str_replace("{giftcard_reciver_email}", $eachOrders->giftcard_user_email, $mailBody);
+			$mailBody = str_replace("{giftcard_reciver_name}", $eachOrders->giftcard_user_name, $mailBody);
+			$mailBody = $productHelper->getValidityDate($giftcardData->giftcard_validity, $mailBody);
+			$mailBody = str_replace("{giftcard_value}", $giftCardValue, $mailBody);
+			$mailBody = str_replace("{giftcard_value_lbl}", \JText::_('LIB_REDSHOP_GIFTCARD_VALUE_LBL'), $mailBody);
+			$mailBody = str_replace("{giftcard_desc}", $giftcardData->giftcard_desc, $mailBody);
+			$mailBody = str_replace("{giftcard_validity}", $giftcardData->giftcard_validity, $mailBody);
+			$mailBody = str_replace("{giftcard_code_lbl}", \JText::_('LIB_REDSHOP_GIFTCARD_CODE_LBL'), $mailBody);
+			$mailBody = str_replace("{giftcard_code}", $giftCode, $mailBody);
+
+			ob_flush();
+			ob_clean();
+
+			echo "<div id='redshopcomponent' class='redshop'>";
+
+			$giftCardAttachment = null;
+			$pdfImage           = '';
+			$mailImage          = '';
+
+			if ($giftcardData->giftcard_image && file_exists(REDSHOP_FRONT_IMAGES_RELPATH . 'giftcard/' . $giftcardData->giftcard_image))
+			{
+				$pdfImage  = '<img src="' . REDSHOP_FRONT_IMAGES_RELPATH . 'giftcard/' . $giftcardData->giftcard_image . '" alt="test alt attribute" width="150px" height="150px" border="0" />';
+				$mailImage = '<img src="components/com_redshop/assets/images/giftcard/' . $giftcardData->giftcard_image . '" alt="test alt attribute" width="150px" height="150px" border="0" />';
+			}
+
+			if (\RedshopHelperPdf::isAvailablePdfPlugins())
+			{
+				$pdfMailBody = $mailBody;
+				$pdfMailBody = str_replace("{giftcard_image}", $pdfImage, $pdfMailBody);
+
+				\JPluginHelper::importPlugin('redshop_pdf');
+				$backgroundImage = '';
+
+				$pdfFile = \RedshopHelperUtility::getDispatcher()->trigger(
+					'onRedshopCreateGiftCardPdf',
+					array($giftcardData, $pdfMailBody, $backgroundImage)
+				);
+
+				if (!empty($pdfFile))
+				{
+					$giftCardAttachment = JPATH_SITE . '/components/com_redshop/assets/orders/' . $pdfFile[0] . ".pdf";
+				}
+			}
+
+			$mailBody = str_replace("{giftcard_image}", $mailImage, $mailBody);
+			Helper::imgInMail($mailBody);
+
+			self::executeSendMail($eachOrders->giftcard_user_email, $mailSubject, $mailBody, $giftCardAttachment);
+		}
+
+		return true;
+	}
+
+	/**
+	 *
+	 * @return array
+	 *
+	 * @since  2.1.0
+	 */
+	protected static function getTemplate()
+	{
+		$giftCardTemplate = Helper::getTemplate(0, "giftcard_mail");
+
+		if (!empty($giftCardTemplate))
+		{
+			$giftCardTemplate = $giftCardTemplate[0];
+		}
+
+		return $giftCardTemplate;
+	}
+
+	/**
+	 * @param   string $receipt     Receipt email
+	 * @param   string $mailSubject Subject
+	 * @param   string $mailBody    Body
+	 * @param   string $attachment  File attachment
+	 *
+	 * @return boolean
+	 *
+	 * @since  2.1.0
+	 */
+	protected static function executeSendMail($receipt, $mailSubject, $mailBody, $attachment)
+	{
+		$config   = JFactory::getConfig();
+		$from     = $config->get('mailfrom');
+		$fromname = $config->get('fromname');
+
+		return \JFactory::getMailer()->sendMail(
+			$from, $fromname, $receipt, $mailSubject, $mailBody, 1, null, null, $attachment
+		);
+	}
+}
