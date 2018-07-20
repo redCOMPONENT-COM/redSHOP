d31d81dc23e58d322a05aa407d1aeec32a465c76
ducredweb <duc@redweb.dk>
2018-07-20 10:40:22 +0700

    KON-591 shorten function "createUserInEconomic" to fix Scrutinizer

--- libraries/redshop/src/Economic/RedshopEconomic.php
+++ libraries/redshop/src/Economic/RedshopEconomic.php
@@ -396,43 +396,60 @@ class RedshopEconomic
 
 		if (!empty($debtorHandle[0]))
 		{
-			$debtorEmailHandle = \RedshopHelperUtility::getDispatcher()->trigger('Debtor_FindByEmail', array($eco));
-
-			if (!empty($debtorEmailHandle[0]))
-			{
-				$emailarray = $debtorEmailHandle[0]->DebtorHandle;
-
-				if (count($emailarray) > 1)
-				{
-					for ($i = 0, $in = count($emailarray); $i < $in; $i++)
-					{
-						if ($debtorHandle[0]->Number == $emailarray[$i]->Number)
-						{
-							$eco['eco_user_number'] = $debtorHandle[0]->Number;
-						}
-					}
-				}
-				elseif (count($emailarray) > 0)
-				{
-					$eco['eco_user_number'] = $emailarray->Number;
-				}
-			}
-			else
-			{
-				$debtorVatHandle = \RedshopHelperUtility::getDispatcher()->trigger('Debtor_FindByVAT', array($eco));
-
-				if (!empty($debtorVatHandle[0]))
-				{
-					$eco['eco_user_number'] = $debtorVatHandle[0]->Number;
-				}
-				else
-				{
-					$eco['newuserFlag'] = true;
-				}
-			}
+			self::handleExistingDebtorOnEmailVat($debtorHandle, $eco);
 		}
 
 		return \RedshopHelperUtility::getDispatcher()->trigger('storeDebtor', array($eco));
+	}
+
+	/**
+	 * Method to check existing Debtor base on duplication of Email/Vat
+	 *
+	 * @param   array $debtor   existing Debtor data
+	 * @param   array $eco      output economic user data
+	 *
+	 * @return  void
+	 *
+	 * @since   2.1.0
+	 */
+	public static function handleExistingDebtorOnEmailVat($debtor, &$eco)
+	{
+		self::importEconomic();
+
+		$debtorEmailHandle = \RedshopHelperUtility::getDispatcher()->trigger('Debtor_FindByEmail', array($eco));
+
+		if (!empty($debtorEmailHandle[0]))
+		{
+			$emailArray = $debtorEmailHandle[0]->DebtorHandle;
+
+			if (count($emailArray) > 1)
+			{
+				for ($i = 0, $in = count($emailArray); $i < $in; $i++)
+				{
+					if ($debtor[0]->Number == $emailArray[$i]->Number)
+					{
+						$eco['eco_user_number'] = $debtor[0]->Number;
+					}
+				}
+			}
+			elseif (count($emailArray) > 0)
+			{
+				$eco['eco_user_number'] = $emailArray->Number;
+			}
+		}
+		else
+		{
+			$debtorVatHandle = \RedshopHelperUtility::getDispatcher()->trigger('Debtor_FindByVAT', array($eco));
+
+			if (!empty($debtorVatHandle[0]))
+			{
+				$eco['eco_user_number'] = $debtorVatHandle[0]->Number;
+			}
+			else
+			{
+				$eco['newuserFlag'] = true;
+			}
+		}
 	}
 
 	/**
