33a732f6716fcb64f6b1a38226d349d05ac72ca9
VanTrieuThanh96 <thanhthanhvan96qn@gmail.com>
2018-07-23 16:09:21 +0700

    update codeception 2.4.x

--- tests/_pages/Joomla3/Administrator/CheckoutProductChangeQuantityPage.php
+++ tests/_pages/Joomla3/Administrator/CheckoutProductChangeQuantityPage.php
@@ -18,21 +18,21 @@ class CheckoutProductChangeQuantityPage extends AdminJ3Page
 	/**
 	 * @var array
 	 */
-	public static $categoryTitle = ['class' => 'category_front_inside'];
+	public static $categoryTitle = ".category_front_inside";
 
-	public static $fillUserName = ['id' => 'modlgn-username'];
+	public static $fillUserName = "#modlgn-username";
 
-	public static $fillPassWord = ['id' => 'modlgn-passwd'];
+	public static $fillPassWord = "#modlgn-passwd";
 
 	public static $submitButton = "Log in";
 
 	/**
 	 * @var array
 	 */
-	public static $quantityField = ['id' => 'quantitybox0'];
+	public static $quantityField = "#quantitybox0";
 
 	/**
 	 * @var array
 	 */
-	public static $updateCartButton = ['xpath' => "//img[@onclick=\"document.update_cart0.task.value='update';document.update_cart0.submit();\"]"];
+	public static $updateCartButton = "//img[@onclick=\"document.update_cart0.task.value='update';document.update_cart0.submit();\"]";
 }
