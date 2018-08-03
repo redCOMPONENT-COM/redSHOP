147c1face0da6fc2553e0d3f268c3fb7881bc9df
VanTrieuThanh96 <thanhthanhvan96qn@gmail.com>
2018-08-03 15:10:54 +0700

    update code

--- tests/acceptance/administrator/Stock_Image/StockImageCest.php
+++ tests/acceptance/administrator/Stock_Image/StockImageCest.php
@@ -18,58 +18,59 @@ use AcceptanceTester\ConfigurationSteps;
  */
 class StockImageCest
 {
-	public function __construct()
-	{
-		$this->faker = Faker\Factory::create();
-		$this->name = $this->faker->bothify('Name Stock Image ?##?');
-		$this->newImageTooltip = 'Updated ' . $this->name;
-		$this->quantity = '100';
-		$this->randomStockName = $this->faker->bothify('Name Stock Image ?##?');
-		$this->updatedRandomStockName = 'New ' . $this->randomStockName;
-	}
+    public function __construct()
+    {
+        $this->faker = Faker\Factory::create();
+        $this->name = $this->faker->bothify('Name Stock Image ?##?');
+        $this->newImageTooltip = 'Updated ' . $this->name;
+        $this->quantity = '100';
+        $this->randomStockName = $this->faker->bothify('Name Stock Image ?##?');
+        $this->updatedRandomStockName = 'New ' . $this->randomStockName;
+    }
 
-	public function _before(AcceptanceTester $I)
-	{
-		$I->doAdministratorLogin();
-	}
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function _before(AcceptanceTester $I)
+    {
+        $I->doAdministratorLogin();
+    }
 
-	public function enableStock(AcceptanceTester $I, $scenario)
-	{
-		$I->wantTo('Enable StockRoom in Administrator page');
-		$I = new ConfigurationSteps($scenario);
-		$I -> featureUsedStockRoom();
+    public function enableStock(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Enable StockRoom in Administrator page');
+        $I = new ConfigurationSteps($scenario);
+        $I -> featureUsedStockRoom();
 
-		/**
-		* Function to Create a Stock Image List
-		*/
-		$I->wantTo('Create Stock Image in Administrator page');
-		$I = new StockImageSteps($scenario);
-		$I->addStockImage($this->name);
+        /**
+         * Function to Create a Stock Image List
+         */
+        $I->wantTo('Create Stock Image in Administrator page');
+        $I = new StockImageSteps($scenario);
+        $I->addStockImage($this->name);
 
-		/**
-		 * Function to Update a Stock Image List
-		 */
-		$I->wantTo('Update Stock Image in Administrator page');
-		$I = new StockImageSteps($scenario);
-		$I->updateStockImage($this->name);
-	}
+        /**
+         * Function to Update a Stock Image List
+         */
+        $I->wantTo('Update Stock Image in Administrator page');
+        $I = new StockImageSteps($scenario);
+        $I->updateStockImage($this->name);
+    }
 
-	public function disableStock(AcceptanceTester $I, $scenario)
-	{
+    public function disableStock(AcceptanceTester $I, $scenario)
+    {
+        /**
+         * Function to Delete a Stock Image List
+         */
+        $I->wantTo('Delete Stock Image in Administrator page');
+        $I = new StockImageSteps($scenario);
+        $I->deleteStockImage($this->name);
 
-		/**
-		 * Function to Delete a Stock Image List
-		 */
-		$I->wantTo('Delete Stock Image in Administrator page');
-		$I = new StockImageSteps($scenario);
-		$I->deleteStockImage($this->name);
-
-
-		/**
-		 * Function to Disable a Stock Image List
-		 */
-		$I->wantTo('Disable StockRoom in Administrator page');
-		$I = new ConfigurationSteps($scenario);
-		$I->featureOffStockRoom();
-	}
+        /**
+         * Function to Disable a Stock Image List
+         */
+        $I->wantTo('Disable StockRoom in Administrator page');
+        $I = new ConfigurationSteps($scenario);
+        $I->featureOffStockRoom();
+    }
 }
