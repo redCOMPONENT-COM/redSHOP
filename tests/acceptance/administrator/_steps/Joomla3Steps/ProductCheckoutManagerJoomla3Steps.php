db92ca8402b776a89d6f9b915d316b8325c718a5
VanTrieuThanh96 <thanhthanhvan96qn@gmail.com>
2018-08-06 14:30:19 +0700

    Change I->click for 853 line

--- tests/acceptance/administrator/_steps/Joomla3Steps/ProductCheckoutManagerJoomla3Steps.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/ProductCheckoutManagerJoomla3Steps.php
@@ -850,7 +850,7 @@ class ProductCheckoutManagerJoomla3Steps extends AdminManagerJoomla3Steps
 		$I->waitForElement(\FrontEndProductManagerJoomla3Page::$billingFinal, 30);
 		$I->scrollTo(\FrontEndProductManagerJoomla3Page::$bankTransfer);
 		$I->waitForElement(\FrontEndProductManagerJoomla3Page::$bankTransfer, 30);
-		$I->executeJS($productFrontEndManagerPage->radioCheckID(\FrontEndProductManagerJoomla3Page::$bankTransferId));
+		$I->click(\FrontEndProductManagerJoomla3Page::$bankTransferId);
 		$I->click(\FrontEndProductManagerJoomla3Page::$checkoutButton);
 		$I->waitForElementVisible(\FrontEndProductManagerJoomla3Page::$addressEmail);
 		$I->fillField(\FrontEndProductManagerJoomla3Page::$addressAddress, 'address');
