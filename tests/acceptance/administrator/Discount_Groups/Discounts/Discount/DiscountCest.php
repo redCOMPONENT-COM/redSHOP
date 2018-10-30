da83c1c001e9528198a148b9df99c3bf43aa4d70
VanTrieuThanh96 <thanhthanhvan96qn@gmail.com>
2018-10-30 10:59:44 +0700

    change time

--- tests/acceptance/administrator/Discount_Groups/Discounts/Discount/DiscountCest.php
+++ tests/acceptance/administrator/Discount_Groups/Discounts/Discount/DiscountCest.php
@@ -79,8 +79,8 @@ class DiscountCest
 		$this->amount            = $this->faker->numberBetween(100, 999);
 		$this->discountAmount    = $this->faker->numberBetween(10, 100);
 		$this->newAmount         = $this->faker->numberBetween(100, 999);
-		$this->startDate         = '13-06-2017';
-		$this->endDate           = '13-08-2017';
+		$this->startDate         = '13-06-2019';
+		$this->endDate           = '13-08-2019';
 		$this->shopperGroup      = 'Default Private';
 		$this->discountType      = 0;
 		$this->discountCondition = 1;
