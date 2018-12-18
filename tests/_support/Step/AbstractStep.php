07b4883e32dfb0ebf2b7059be392744492638893
nhungredweb <nhung@redweb.dk>
2018-12-18 15:38:42 +0700

    add checking

--- tests/_support/Step/AbstractStep.php
+++ tests/_support/Step/AbstractStep.php
@@ -81,13 +81,14 @@ class AbstractStep extends AdminManagerJoomla3Steps
 			case 'save&close':
 				$tester->wait(0.5);
 				$tester->click($pageClass::$buttonSaveClose);
-				$tester-$this->wait(0.5);
+				$tester->wait(0.5);
                 $tester->assertSystemMessageContains($pageClass::$messageItemSaveSuccess);
 				break;
 			case 'save&new':
 				$tester->wait(0.5);
 				$tester->click($pageClass::$buttonSaveNew);
                 $tester->assertSystemMessageContains($pageClass::$messageItemSaveSuccess);
+                $tester->wait(0.5);
                 $tester->click($pageClass::$buttonCancel);
 				break;
 			default:
@@ -126,7 +127,7 @@ class AbstractStep extends AdminManagerJoomla3Steps
                 $tester->click($pageClass::$buttonClose);
 				break;
 			case 'save&close':
-                $tester-$this->wait(0.5);
+                $tester->wait(0.5);
 				$tester->click($pageClass::$buttonSaveClose);
 				$tester->wait(0.5);
                 $tester->assertSystemMessageContains($pageClass::$messageItemSaveSuccess);
@@ -260,6 +261,7 @@ class AbstractStep extends AdminManagerJoomla3Steps
 
 				default:
 					$this->fillField($field['xpath'], $data[$index]);
+					$this->wait(0.5);
 					break;
 			}
 		}
