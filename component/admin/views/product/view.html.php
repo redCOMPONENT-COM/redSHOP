4ed0f200037d02699f4bac72d0783da23bb57cdd
hoang.tran <hoang.tran@redweb.dk>
2018-12-19 11:00:52 +0700

    fix function addToolbar on product page

--- component/admin/views/product/view.html.php
+++ component/admin/views/product/view.html.php
@@ -12,127 +12,126 @@ defined('_JEXEC') or die;
 
 class RedshopViewProduct extends RedshopViewAdmin
 {
-	/**
-	 * The pagination object.
-	 *
-	 * @var  JPagination
-	 */
-	public $pagination;
+    /**
+     * The pagination object.
+     *
+     * @var  JPagination
+     */
+    public $pagination;
 
-	/**
-	 * The request url.
-	 *
-	 * @var  string
-	 */
-	public $request_url;
+    /**
+     * The request url.
+     *
+     * @var  string
+     */
+    public $request_url;
 
-	/**
-	 * The current user.
-	 *
-	 * @var  JUser
-	 */
-	public $user;
+    /**
+     * The current user.
+     *
+     * @var  JUser
+     */
+    public $user;
 
-	public function display($tpl = null)
-	{
-		global $context;
+    public function display($tpl = null)
+    {
+        global $context;
 
-		$context = 'product_id';
+        $context = 'product_id';
 
-		$GLOBALS['productlist'] = array();
+        $GLOBALS['productlist'] = array();
 
-		$list_in_products = RedshopHelperExtrafields::listAllFieldInProduct();
+        $list_in_products = RedshopHelperExtrafields::listAllFieldInProduct();
 
-		$uri = JUri::getInstance();
+        $uri = JUri::getInstance();
 
-		$layout = JFactory::getApplication()->input->getCmd('layout', '');
+        $layout = JFactory::getApplication()->input->getCmd('layout', '');
 
-		// We don't need toolbar in the modal window.
-		if ($layout !== 'element')
-		{
-			$this->addToolbar();
-		}
+        // We don't need toolbar in the modal window.
+        if ($layout !== 'element')
+        {
+            $this->addToolbar();
+        }
 
         if ($layout == 'listing')
         {
-            JToolBarHelper::title(JText::_('Product Price Managment'), 'stack redshop_products48');
+            JToolBarHelper::title(JText::_('Product Price Managment'));
         }
 
-		$state       = $this->get('State');
-		$category_id = $state->get('category_id');
+        $state       = $this->get('State');
+        $category_id = $state->get('category_id');
 
-		if ($category_id)
-		{
-			$filter_order = $state->get('list.ordering', 'x.ordering');
-		}
-		else
-		{
-			$filter_order = $state->get('list.ordering', 'p.product_id');
-		}
+        if ($category_id)
+        {
+            $filter_order = $state->get('list.ordering', 'x.ordering');
+        }
+        else
+        {
+            $filter_order = $state->get('list.ordering', 'p.product_id');
+        }
 
-		$filter_order_Dir = $state->get('list.direction');
+        $filter_order_Dir = $state->get('list.direction');
 
-		$search_field = $state->get('search_field');
-		$keyword      = $state->get('keyword');
+        $search_field = $state->get('search_field');
+        $keyword      = $state->get('keyword');
 
-		$categories  = $this->get('CategoryList');
-		$categories1 = array();
+        $categories  = $this->get('CategoryList');
+        $categories1 = array();
 
-		foreach ($categories as $key => $value)
-		{
-			$categories1[$key]            = new stdClass;
-			$categories1[$key]->id        = $categories[$key]->id;
-			$categories1[$key]->parent_id = $categories[$key]->parent_id;
-			$categories1[$key]->title     = $categories[$key]->title;
-			$treename                     = str_replace("&#160;&#160;&#160;&#160;&#160;&#160;", " ", $categories[$key]->treename);
-			$treename                     = str_replace("<sup>", " ", $treename);
-			$treename                     = str_replace("</sup>&#160;", " ", $treename);
-			$categories1[$key]->treename  = $treename;
-			$categories1[$key]->children  = $categories[$key]->children;
-		}
+        foreach ($categories as $key => $value)
+        {
+            $categories1[$key]            = new stdClass;
+            $categories1[$key]->id        = $categories[$key]->id;
+            $categories1[$key]->parent_id = $categories[$key]->parent_id;
+            $categories1[$key]->title     = $categories[$key]->title;
+            $treename                     = str_replace("&#160;&#160;&#160;&#160;&#160;&#160;", " ", $categories[$key]->treename);
+            $treename                     = str_replace("<sup>", " ", $treename);
+            $treename                     = str_replace("</sup>&#160;", " ", $treename);
+            $categories1[$key]->treename  = $treename;
+            $categories1[$key]->children  = $categories[$key]->children;
+        }
 
-		$temps              = array();
-		$temps[0]           = new stdClass;
-		$temps[0]->id       = "0";
-		$temps[0]->treename = JText::_('COM_REDSHOP_SELECT_CATEGORY');
-		$categories1        = @array_merge($temps, $categories1);
-		$lists['category']  = JHTML::_('select.genericlist', $categories1, 'category_id',
-			'class="inputbox" onchange="document.adminForm.submit();" ', 'id', 'treename', $category_id
-		);
+        $temps              = array();
+        $temps[0]           = new stdClass;
+        $temps[0]->id       = "0";
+        $temps[0]->treename = JText::_('COM_REDSHOP_SELECT_CATEGORY');
+        $categories1        = @array_merge($temps, $categories1);
+        $lists['category']  = JHTML::_('select.genericlist', $categories1, 'category_id',
+            'class="inputbox" onchange="document.adminForm.submit();" ', 'id', 'treename', $category_id
+        );
 
-		$product_sort          = RedshopHelperProduct::getProductsSortByList();
-		$lists['product_sort'] = JHTML::_('select.genericlist', $product_sort, 'product_sort',
-			'class="inputbox"  onchange="document.adminForm.submit();" ', 'value', 'text', $state->get('product_sort')
-		);
+        $product_sort          = RedshopHelperProduct::getProductsSortByList();
+        $lists['product_sort'] = JHTML::_('select.genericlist', $product_sort, 'product_sort',
+            'class="inputbox"  onchange="document.adminForm.submit();" ', 'value', 'text', $state->get('product_sort')
+        );
 
-		$lists['order']     = $filter_order;
-		$lists['order_Dir'] = $filter_order_Dir;
-		$products           = $this->get('Data');
+        $lists['order']     = $filter_order;
+        $lists['order_Dir'] = $filter_order_Dir;
+        $products           = $this->get('Data');
+        $pagination = $this->get('Pagination');
 
-		$pagination = $this->get('Pagination');
+        // Assign template
+        $templates      = RedshopHelperTemplate::getTemplate('product');
+        $temps          = array();
+        $temps[0]       = new stdClass;
+        $temps[0]->id   = "0";
+        $temps[0]->name = JText::_('COM_REDSHOP_ASSIGN_TEMPLATE');
+        $templates      = @array_merge($temps, $templates);
 
-		// Assign template
-		$templates      = RedshopHelperTemplate::getTemplate('product');
-		$temps          = array();
-		$temps[0]       = new stdClass;
-		$temps[0]->id   = "0";
-		$temps[0]->name = JText::_('COM_REDSHOP_ASSIGN_TEMPLATE');
-		$templates      = @array_merge($temps, $templates);
+        $lists['product_template'] = JHtml::_('select.genericlist', $templates, 'product_template',
+            'class="inputbox" size="1"  onchange="return AssignTemplate()" ', 'id', 'name', 0
+        );
 
-		$lists['product_template'] = JHtml::_('select.genericlist', $templates, 'product_template',
-			'class="inputbox" size="1"  onchange="return AssignTemplate()" ', 'id', 'name', 0
-		);
+        $this->state            = $state;
+        $this->list_in_products = $list_in_products;
+        $this->keyword          = $keyword;
+        $this->search_field     = $search_field;
+        $this->user             = JFactory::getUser();
+        $this->lists            = $lists;
+        $this->products         = $products;
+        $this->pagination       = $pagination;
+        $this->request_url      = $uri->toString();
 
-		$this->state            = $state;
-		$this->list_in_products = $list_in_products;
-		$this->keyword          = $keyword;
-		$this->search_field     = $search_field;
-		$this->user             = JFactory::getUser();
-		$this->lists            = $lists;
-		$this->products         = $products;
-		$this->pagination       = $pagination;
-		$this->request_url      = $uri->toString();
-
-		parent::display($tpl);
-	}
+        parent::display($tpl);
+    }
 }
