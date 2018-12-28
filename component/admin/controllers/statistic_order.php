54d78c9f4dbb9033e9a30645c5f867fa70ba56b4
hoang.tran <hoang.tran@redweb.dk>
2018-12-28 10:17:15 +0700

    fix issue pull request

--- component/admin/controllers/statistic_order.php
+++ component/admin/controllers/statistic_order.php
@@ -100,9 +100,9 @@ class RedshopControllerStatistic_Order extends RedshopControllerAdmin
             $noItems = RedshopHelperOrder::getOrderItemDetail($data[$i]->order_id);
             for ($it = 0, $itn = count($noItems); $it < $itn; $it++)
             {
-                $orderItemName = str_replace("\"", " ", $noItems[$it]->order_item_name);
-                if (!empty($orderItemName))
+                if (!empty($noItems[$it]->order_item_name))
                 {
+                    $orderItemName = str_replace("\"", " ", $noItems[$it]->order_item_name);
                     echo str_replace(",", " ", utf8_decode($orderItemName)) . " ,";
                 }
                 else
