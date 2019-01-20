6b5653c144c7e545d4d6e66c52114ca507f12a97
Tito Alvarez <augustoalvarez@gmail.com>
2019-01-20 01:54:22 -0600

    Splitting in more groups and optimizing resources while cloning stages

---
+++ tests/drone/setup-base-install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+tests_db=$1
+tests_suite=$2
+php_versions=$3
+
+for php_version in ${php_versions//,/ }
+do
+	# Waiting for setup to be done
+	while [[ ! -f tests/.setup$php_version.tmp ]]
+	do
+		sleep 1
+	done
+
+	# Final DB dump with full Joomla/extension setup
+	rsync -a --delete tests/$tests_suite$php_version/joomla-cms/ tests/joomla-cms$php_version
+	sed -i "s/db = '$tests_db$php_version'/db = 'tests_db'/g" tests/joomla-cms$php_version/configuration.php
+	sed -i "s,$tests_suite$php_version/joomla-cms/,joomla-cms/,g" tests/joomla-cms$php_version/configuration.php
+	mysqldump -u root -proot -h db $tests_db$php_version > tests/dbdump$php_version.sql.tmp
+done
--- tests/drone/setup-stages.sh
+++ tests/drone/setup-stages.sh
@@ -2,15 +2,18 @@
 
 tests_db=$1
 tests_suite=$2
-php_version=$3
+php_versions=$3
 
-# Prepares and restores DB
-mysql -u root -proot -h db -e "CREATE DATABASE $tests_db$php_version"
-mysql -u root -proot -h db -U $tests_db$php_version < tests/dbdump$php_version.sql.tmp
+for php_version in ${php_versions//,/ }
+do
+	# Prepares and restores DB
+	mysql -u root -proot -h db -e "CREATE DATABASE $tests_db$php_version"
+	mysql -u root -proot -h db -U $tests_db$php_version < tests/dbdump$php_version.sql.tmp
 
-# Creating clone of Joomla site
-mkdir -p tests/$tests_suite$php_version/joomla-cms
-rsync -a tests/joomla-cms$php_version/ tests/$tests_suite$php_version/joomla-cms
-sed -i "s/db = 'tests_db'/db = '$tests_db$php_version'/g" tests/$tests_suite$php_version/joomla-cms/configuration.php
-sed -i "s,joomla-cms$php_version/,$tests_suite$php_version/joomla-cms/,g" tests/$tests_suite$php_version/joomla-cms/configuration.php
-touch tests/.cache.setup.$tests_suite$php_version.tmp
+	# Creating clone of Joomla site
+	mkdir -p tests/$tests_suite$php_version/joomla-cms
+	rsync -a tests/joomla-cms$php_version/ tests/$tests_suite$php_version/joomla-cms
+	sed -i "s/db = 'tests_db'/db = '$tests_db$php_version'/g" tests/$tests_suite$php_version/joomla-cms/configuration.php
+	sed -i "s,joomla-cms$php_version/,$tests_suite$php_version/joomla-cms/,g" tests/$tests_suite$php_version/joomla-cms/configuration.php
+	touch tests/.cache.setup.$tests_suite$php_version.tmp
+done
