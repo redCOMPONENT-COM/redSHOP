a288be2c514b61357366f90f1e7b0f38a5061b72
hungnguyen <hungnguyen.ckc@gmail.com>
2019-01-22 14:07:49 +0700

    update code

--- plugins/redshop_import/shipping_address/shipping_address.php
+++ plugins/redshop_import/shipping_address/shipping_address.php
@@ -128,13 +128,17 @@ class PlgRedshop_ImportShipping_Address extends AbstractImportPlugin
 		}
 
 		$data['address_type'] = 'ST';
-		$table->set('users_info_id', null);
 
-		if (!$table->bind($data))
+		if (array_key_exists($this->primaryKey, $data) && $data[$this->primaryKey])
+		{
+			$table->load($data[$this->primaryKey]);
+		}
+
+		if (!$table->bind($data) || !$table->check() || !$table->store())
 		{
 			return false;
 		}
 
-		return $db->insertObject('#__redshop_users_info', $table, $this->primaryKey);
+		return true;
 	}
 }
