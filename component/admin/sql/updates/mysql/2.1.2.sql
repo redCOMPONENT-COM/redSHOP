3e1db977e855ebe086e26229822a2a69c8680101
ducredweb <duc@redweb.dk>
2019-01-23 20:25:43 +0700

    REDSHOP-5322 update code

--- component/admin/sql/updates/mysql/2.1.2.sql
+++ component/admin/sql/updates/mysql/2.1.2.sql
@@ -3,6 +3,6 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- -----------------------------------------------------
 -- Table `#__redshop_category`
 -- -----------------------------------------------------
-CALL redSHOP_Column_Update('#__redshop_category', 'product_filter_params', 'product_filter_params', "MEDIUMTEXT NOT NULL DEFAULT ''");
+CALL redSHOP_Column_Update('#__redshop_category', 'product_filter_params', 'product_filter_params', "MEDIUMTEXT NOT NULL DEFAULT '' AFTER `publish_down`");
 
 SET FOREIGN_KEY_CHECKS = 1;
