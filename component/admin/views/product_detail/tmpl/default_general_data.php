f9e052935300206b25d75f6a878f08ab3eb752c6
Khoa Vo <khoa.vo@redweb.dk>
2019-01-24 18:53:35 +0700

    REDSHOP-5319 update code

--- component/admin/views/product_detail/tmpl/default_general_data.php
+++ component/admin/views/product_detail/tmpl/default_general_data.php
@@ -284,7 +284,9 @@ foreach ($media->getAll() as $mediaItem)
                                 'discount_stratdate',
                                 'discount_stratdate',
                                 $calendarFormat,
-                                array('class' => 'form-control', 'size' => '15', 'maxlength' => '19')
+                                array('class' => 'form-control', 'size' => '15', 'maxlength' => '19'),
+                                null,
+								$config->get('offset')
                             );
                             ?>
                         </div>
@@ -307,7 +309,9 @@ foreach ($media->getAll() as $mediaItem)
                                 'discount_enddate',
                                 'discount_enddate',
                                 $calendarFormat,
-                                array('class' => 'form-control', 'size' => '15', 'maxlength' => '19')
+                                array('class' => 'form-control', 'size' => '15', 'maxlength' => '19'),
+								null,
+								$config->get('offset')
                             );
                             ?>
                         </div>
