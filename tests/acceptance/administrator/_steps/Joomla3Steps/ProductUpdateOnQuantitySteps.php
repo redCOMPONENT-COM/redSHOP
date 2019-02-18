ea9157a10985a457f10a788fef9f193bb5321840
unknown <trang.nguyen@redweb.dk>
2019-02-18 11:46:09 +0700

    edit style

--- tests/acceptance/administrator/_steps/Joomla3Steps/ProductUpdateOnQuantitySteps.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/ProductUpdateOnQuantitySteps.php
@@ -103,7 +103,7 @@ class ProductUpdateOnQuantitySteps extends AdminManagerJoomla3Steps
 		$I->fillField(\FrontEndProductManagerJoomla3Page::$addressPostalCode, $customerInformation['postalCode']);
 		$I->fillField(\FrontEndProductManagerJoomla3Page::$addressCity, $customerInformation['city']);
 		$I->fillField(\FrontEndProductManagerJoomla3Page::$addressPhone, $customerInformation['phone']);
-        $I->waitForElement(\FrontEndProductManagerJoomla3Page::$bankTransfer, 30);
+		$I->waitForElement(\FrontEndProductManagerJoomla3Page::$bankTransfer, 30);
 		$I->scrollTo(\FrontEndProductManagerJoomla3Page::$bankTransfer);
 		$I->click(\FrontEndProductManagerJoomla3Page::$bankTransfer);
 		$I->waitForElement(\FrontEndProductManagerJoomla3Page::$acceptTerms, 30);
