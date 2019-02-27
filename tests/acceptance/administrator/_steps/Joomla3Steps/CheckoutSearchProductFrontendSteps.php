573306ab92cd0de1985950e70ae23459656fa832
unknown <trang.nguyen@redweb.dk>
2019-02-27 09:48:29 +0700

    fix fail checkout

--- tests/acceptance/administrator/_steps/Joomla3Steps/CheckoutSearchProductFrontendSteps.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/CheckoutSearchProductFrontendSteps.php
@@ -142,18 +142,10 @@ class CheckoutSearchProductFrontendSteps  extends AdminManagerJoomla3Steps
 		$I->fillField(FrontEndProductManagerJoomla3Page::$addressCity, $customerInformation['city']);
 		$I->fillField(FrontEndProductManagerJoomla3Page::$addressPhone, $customerInformation['phone']);
 		$I->waitForElement(FrontEndProductManagerJoomla3Page::$bankTransfer, 30);
+		$I->scrollTo(FrontEndProductManagerJoomla3Page::$bankTransfer);
+		$I->wait(0.5);
 		$I->executeJS(FrontEndProductManagerJoomla3Page::$jqueryBankTransfer);
-		try
-		{
-			$I->seeCheckboxIsChecked(\FrontEndProductManagerJoomla3Page::$bankTransfer);
-		}catch (\Exception $e)
-		{
-			$I->click(\FrontEndProductManagerJoomla3Page::$bankTransfer);
-		}
-		$I->wait(2);
-		$I->waitForElement(FrontEndProductManagerJoomla3Page::$acceptTerms, 30);
-		$I->executeJS($productFrontEndManagerPage->radioCheckID(FrontEndProductManagerJoomla3Page::$termAndConditionsId));
-
+		$I->wait(1);
 		try
 		{
 			$I->seeCheckboxIsChecked(FrontEndProductManagerJoomla3Page::$termAndConditions);
