f7a34e630fb7bf4c090cedce985619e2c725f7b4
hoang.tran <hoang.tran@redweb.dk>
2019-03-22 14:57:49 +0700

    not show button comparison in product detail page when configuration disable comparision

--- component/site/views/product/tmpl/default.php
+++ component/site/views/product/tmpl/default.php
@@ -105,6 +105,12 @@ else
 	$template_desc = str_replace('{discount_calculator}', '', $template_desc);
 }
 
+if (Redshop::getConfig()->get('COMPARE_PRODUCTS') == 0)
+{
+	$template_desc = str_replace('{compare_products_button}', '', $template_desc);
+	$template_desc = str_replace('{compare_product_div}', '', $template_desc);
+}
+
 $template_desc = str_replace('{component_heading}', $this->escape($this->data->product_name), $template_desc);
 
 if (strstr($template_desc, '{back_link}'))
