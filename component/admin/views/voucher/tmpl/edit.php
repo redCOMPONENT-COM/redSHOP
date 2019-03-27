9b293ea20ce6c19efac9818a639efd29f6312c97
hungnguyen <hung@redweb.dk>
2019-03-27 15:56:05 +0700

    REDSHOP-4498 Voucher - New Voucher - Suggest amount of voucher left more than 0

--- component/admin/views/voucher/tmpl/edit.php
+++ component/admin/views/voucher/tmpl/edit.php
@@ -10,3 +10,24 @@
 defined('_JEXEC') or die;
 
 echo RedshopLayoutHelper::render('view.edit.' . $this->formLayout, array('data' => $this));
+?>
+
+<script>
+	Joomla.submitbutton = function (pressbutton) {
+		var form = document.adminForm;
+
+		if (pressbutton === 'voucher.apply' || pressbutton === 'voucher.save' || pressbutton ==='voucher.save2new') {
+			if (form.jform_amount.value <= 0) {
+				alert("<?php echo JText::_('COM_REDSHOP_VOUCHER_ERROR_DISCOUNT_AMOUNT_ZERO', true ); ?>");
+				return false;
+			}
+
+			if (form.jform_voucher_left.value <= 0) {
+				alert("<?php echo JText::_('COM_REDSHOP_VOUCHER_ERROR_DISCOUNT_AMOUNT_LEFT_ZERO', true ); ?>");
+				return false;
+			}
+		}
+
+		submitform(pressbutton);
+	}
+</script>
