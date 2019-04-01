f1256e6f1f294b652c6f618e87285ee474884f99
hungnguyen <hung@redweb.dk>
2019-04-01 11:24:00 +0700

    REDSHOP-4530 Gift Card Management - Edit form - Show confuse message when attach txt, doc,.. instead of image

--- component/admin/controllers/giftcard.php
+++ component/admin/controllers/giftcard.php
@@ -35,4 +35,69 @@ class RedshopControllerGiftcard extends RedshopControllerForm
 
 		return $model;
 	}
+
+	/**
+	 * Method to save a record.
+	 *
+	 * @param   string  $key     The name of the primary key of the URL variable.
+	 * @param   string  $urlVar  The name of the URL variable if different from the primary key (sometimes required to avoid router collisions).
+	 *
+	 * @return  boolean          True if successful, false otherwise.
+	 * @throws  Exception
+	 */
+	public function save($key = null, $urlVar = null)
+	{
+		$app       = JFactory::getApplication();
+		$data      = $this->input->post->get('jform', array(), 'array');
+		$file      = $app->input->files->get('jform');
+		$model     = $this->getModel();
+		$context   = "$this->option.edit.$this->context";
+		$recordId  = $this->input->getInt($urlVar);
+		$form      = $model->getForm($data, false);
+		$validData = $model->validate($form, $data);
+
+		// Save the data in the session.
+		$app->setUserState($context . '.data', $validData);
+
+		if (!$this->isImage($file['giftcard_bgimage_file']['name'], 'background image', $recordId, $urlVar))
+		{
+			return false;
+		}
+
+		if (!$this->isImage($file['giftcard_image_file']['name'], 'image', $recordId, $urlVar))
+		{
+			return false;
+		}
+
+		parent::save($key = null, $urlVar = null);
+	}
+
+
+	/**
+	 * Method check image.
+	 *
+	 * @return  boolean
+	 *
+	 * @since __DEPLOY_VERSION__
+	 */
+	public function isImage($imageName, $nameInput, $recordId, $urlVar)
+	{
+		$supportedImage = array('gif', 'jpg', 'jpeg', 'png');
+		$ext            = strtolower(pathinfo($imageName, PATHINFO_EXTENSION));
+
+		if (in_array($ext, $supportedImage))
+		{
+			return true;
+		}
+
+		// Redirect back to the edit screen.
+		$this->setError(JText::sprintf('COM_REDSHOP_GIFTCARD_ERROR_NOT_IMAGE', $nameInput));
+		$this->setMessage($this->getError(), 'error');
+
+		$this->setRedirect(
+			$this->getRedirectToItemRoute($this->getRedirectToItemAppend($recordId, $urlVar))
+		);
+
+		return false;
+	}
 }
