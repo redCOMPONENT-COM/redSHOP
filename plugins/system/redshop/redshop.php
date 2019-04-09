2a2f6b4e031d8ca6af7b4dba23f356b48f1a475b
hungnguyen <hung@redweb.dk>
2019-04-09 14:54:59 +0700

    REDSHOP-5376 Plugin Giao hang nhanh - Show inform City, District, Ward empty on Checkout page

--- plugins/system/redshop/redshop.php
+++ plugins/system/redshop/redshop.php
@@ -79,4 +79,109 @@ class PlgSystemRedSHOP extends JPlugin
 			$session->set('product_currency', $newCurrencyId);
 		}
 	}
+
+	/**
+	 * Change the state in core_content if the state in a table is changed
+	 *
+	 * @param   string   $context  The context for the content passed to the plugin.
+	 * @param   array    $pks      A list of primary key ids of the content that has changed state.
+	 * @param   integer  $value    The value of the state that the content has been changed to.
+	 *
+	 * @return  boolean
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function onContentChangeState($context, $pks, $value)
+	{
+		if ($context != 'com_plugins.plugin')
+		{
+			return false;
+		}
+
+		$this->checkDisableExtrafield($pks, $value);
+
+		return true;
+	}
+
+	/**
+	 * On Saving extensions logging method
+	 * Method is called when an extension is being saved
+	 *
+	 * @param   string   $context  The extension
+	 * @param   JTable   $table    DataBase Table object
+	 * @param   boolean  $isNew    If the extension is new or not
+	 * @param   array    $data     array data extension
+	 *
+	 * @return  boolean
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function onExtensionAfterSave($context, $table, $isNew, $data)
+	{
+		if ($context != 'com_plugins.plugin')
+		{
+			return false;
+		}
+
+		$this->checkDisableExtrafield($data['extension_id'], $data['enabled']);
+
+		return true;
+	}
+
+	/**
+	 * Method check disable extrafield
+	 *
+	 * @param   integer   $pks      extension id
+	 * @param   boolean   $status   status extension
+	 *
+	 * @return  void
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function checkDisableExtrafield($pks, $status)
+	{
+		if (is_array($pks))
+		{
+			$pks = implode(',', $pks);
+		}
+
+		$db = JFactory::getDbo();
+		$query = $db->getQuery(true)
+			->select($db->qn('element'))
+			->from($db->quoteName('#__extensions'))
+			->where($db->quoteName('extension_id') . ' IN (' . $pks . ')');
+		$db->setQuery($query);
+
+		$plugins = $db->loadColumn();
+
+		foreach ($plugins as $plugin)
+		{
+			if ($plugin == 'giaohangnhanh')
+			{
+				if ($status == false)
+				{
+					$fields = array(
+						$db->qn('published') . ' = ' . $db->q(0)
+					);
+				}
+				else
+				{
+					$fields = array(
+						$db->qn('published') . ' = ' . $db->q(1)
+					);
+				}
+
+				$conditions = array(
+					$db->qn('name') . ' = ' . $db->q('rs_ghn_city') . ' OR ' .
+					$db->qn('name') . ' = ' . $db->q('rs_ghn_district') . ' OR ' .
+					$db->qn('name') . ' = ' . $db->q('rs_ghn_billing_city') . ' OR ' .
+					$db->qn('name') . ' = ' . $db->q('rs_ghn_billing_district')
+				);
+
+				$query->clear();
+				$query->update($db->quoteName('#__redshop_fields'))->set($fields)->where($conditions);
+				$db->setQuery($query)->execute();
+			}
+		}
+	}
 }
