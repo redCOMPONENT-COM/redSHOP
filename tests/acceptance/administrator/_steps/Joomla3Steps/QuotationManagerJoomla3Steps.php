c3cd7d8d24be3e22e4bd9f7890a83b6373fb3685
unknown <trang.nguyen@redweb.dk>
2019-05-10 15:26:55 +0700

    fix Quotation

--- tests/acceptance/administrator/_steps/Joomla3Steps/QuotationManagerJoomla3Steps.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/QuotationManagerJoomla3Steps.php
@@ -66,18 +66,19 @@ class QuotationManagerJoomla3Steps extends AdminManagerJoomla3Steps
         $I->click(QuotationManagerPage::$quotationId);
         $I->waitForElement(QuotationManagerPage::$quantityp1,30);
         $I->scrollTo(QuotationManagerPage::$quantityp1);
-        $I->pressKey(QuotationManagerPage::$quantityp1, \Facebook\WebDriver\WebDriverKeys::BACKSPACE);
-        $I->wait(0.5);
-        $I->pressKey(QuotationManagerPage::$quantityp1, \Facebook\WebDriver\WebDriverKeys::BACKSPACE);
-        $I->wait(0.5);
-        $I->pressKey(QuotationManagerPage::$quantityp1, \Facebook\WebDriver\WebDriverKeys::BACKSPACE);
-        $I->wait(0.5);
-        $I->fillField(QuotationManagerPage::$quantityp1, $newQuantity);
+//        $I->click(QuotationManagerPage::$quantityp1);
+//        $I->pressKey(QuotationManagerPage::$quantityp1, \Facebook\WebDriver\WebDriverKeys::BACKSPACE);
+//        $I->wait(0.5);
+//        $I->pressKey(QuotationManagerPage::$quantityp1, \Facebook\WebDriver\WebDriverKeys::BACKSPACE);
+//        $I->wait(0.5);
+//        $I->pressKey(QuotationManagerPage::$quantityp1, \Facebook\WebDriver\WebDriverKeys::BACKSPACE);
+//        $I->wait(0.5);
+        $I->addValueForField(QuotationManagerPage::$quantityp1, $newQuantity, 3);
         $I->click(QuotationManagerPage::$buttonSave);
         $I->scrollTo(QuotationManagerPage::$quantityp1);
         $I->seeInField(QuotationManagerPage::$quantityp1, $newQuantity);
     }
-    
+
     public function editStatus($status)
     {
         $I = $this;
