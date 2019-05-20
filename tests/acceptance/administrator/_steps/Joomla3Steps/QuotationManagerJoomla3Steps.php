976a5d95c7b7db28e7aa2375fa288f8169dbb5dc
hoanghairedweb <hai.le@redweb.dk>
2019-05-20 18:32:50 +0700

    check with phone number is string

--- tests/acceptance/administrator/_steps/Joomla3Steps/QuotationManagerJoomla3Steps.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/QuotationManagerJoomla3Steps.php
@@ -8,6 +8,7 @@
 
 namespace AcceptanceTester;
 use QuotationManagerPage as QuotationManagerPage;
+use UserManagerJoomla3Page as UserManagerJoomla3Page;
 /**
  * Class QuotationManagerJoomla3Steps
  *
@@ -19,81 +20,113 @@ use QuotationManagerPage as QuotationManagerPage;
  */
 class QuotationManagerJoomla3Steps extends AdminManagerJoomla3Steps
 {
-    /**
-     * Function to add a New Quotation
-     *
-     * @return void
-     */
-    public function addQuotation($nameUser, $nameProduct, $quantity)
-    {
-        $I = $this;
-        $I->amOnPage(QuotationManagerPage::$URL);
-        $I->click(QuotationManagerPage::$buttonNew);
-        $I->click(QuotationManagerPage::$userId);
-        $I->waitForElement(QuotationManagerPage::$userSearch, 30);
-        $userQuotationPage = new QuotationManagerPage();
-        $I->fillField(QuotationManagerPage::$userSearch, $nameUser);
-        $I->amOnPage(\QuotationManagerPage::$URL);
-        $I->click(\QuotationManagerPage::$buttonNew);
-        $I->click(\QuotationManagerPage::$userId);
-        $I->waitForElement(\QuotationManagerPage::$userSearch, 30);
-        $I->fillField(\QuotationManagerPage::$userSearch, $nameUser);
-        $I->waitForElement($userQuotationPage->xPathSearch($nameUser), 30);
-        
-        $I->click($userQuotationPage->xPathSearch($nameUser));
-        $I->scrollTo(QuotationManagerPage::$newProductLink);
-        $I->click(QuotationManagerPage::$productId);
-        $I->waitForElement(QuotationManagerPage::$productsSearch, 30);
-        $I->fillField(QuotationManagerPage::$productsSearch, $nameProduct);
-        $I->waitForElement($userQuotationPage->xPathSearch($nameProduct), 60);
-        $I->click($userQuotationPage->xPathSearch($nameProduct));
-        $I->fillField(QuotationManagerPage::$quanlityFirst, $quantity);
+	/**
+	 * Function to add a New Quotation
+	 *
+	 * @return void
+	 */
+	public function addQuotation($nameUser, $nameProduct, $quantity)
+	{
+		$I = $this;
+		$I->amOnPage(QuotationManagerPage::$URL);
+		$I->click(QuotationManagerPage::$buttonNew);
+		$I->click(QuotationManagerPage::$userId);
+		$I->waitForElement(QuotationManagerPage::$userSearch, 30);
+		$userQuotationPage = new QuotationManagerPage();
+		$I->fillField(QuotationManagerPage::$userSearch, $nameUser);
+		$I->amOnPage(\QuotationManagerPage::$URL);
+		$I->click(\QuotationManagerPage::$buttonNew);
+		$I->click(\QuotationManagerPage::$userId);
+		$I->waitForElement(\QuotationManagerPage::$userSearch, 30);
+		$I->fillField(\QuotationManagerPage::$userSearch, $nameUser);
+		$I->waitForElement($userQuotationPage->xPathSearch($nameUser), 30);
 
-        $I->click(QuotationManagerPage::$buttonSave);
-        try{
-            $I->waitForText(QuotationManagerPage::$messageSaveSuccess,5, QuotationManagerPage::$selectorSuccess);
-        }catch (\Exception $e)
-        {
-            $I->click(QuotationManagerPage::$buttonSave);
-            $I->see(QuotationManagerPage::$messageSaveSuccess, QuotationManagerPage::$selectorSuccess);
-        }
-    }
+		$I->click($userQuotationPage->xPathSearch($nameUser));
+		$I->scrollTo(QuotationManagerPage::$newProductLink);
+		$I->click(QuotationManagerPage::$productId);
+		$I->waitForElement(QuotationManagerPage::$productsSearch, 30);
+		$I->fillField(QuotationManagerPage::$productsSearch, $nameProduct);
+		$I->waitForElement($userQuotationPage->xPathSearch($nameProduct), 60);
+		$I->click($userQuotationPage->xPathSearch($nameProduct));
+		$I->fillField(QuotationManagerPage::$quanlityFirst, $quantity);
 
-    public function editQuotation($newQuantity)
-    {
-        $I = $this;
-        $I->amOnPage(QuotationManagerPage::$URL);
-        $I->click(QuotationManagerPage::$quotationId);
-        $I->waitForElement(QuotationManagerPage::$quantityp1,30);
-        $I->scrollTo(QuotationManagerPage::$quantityp1);
-        $I->addValueForField(QuotationManagerPage::$quantityp1, $newQuantity, 5);
-        $I->click(QuotationManagerPage::$buttonSave);
-        $I->scrollTo(QuotationManagerPage::$quantityp1);
-        $I->seeInField(QuotationManagerPage::$quantityp1, $newQuantity);
-    }
-    
-    public function editStatus($status)
-    {
-        $I = $this;
-        $I->amOnPage(QuotationManagerPage::$URL);
-        $I->click(QuotationManagerPage::$quotationId);
-        $I->waitForElement(QuotationManagerPage::$quantityp1,30);
-        $I->click(QuotationManagerPage::$quotationStatusDropDown);
-        $I->waitForElement(QuotationManagerPage::$quotationStatusSearch, 30);
-        $I->fillField(QuotationManagerPage::$quotationStatusSearch, $status);
-        $userQuotationPage = new QuotationManagerPage();
-        $I->waitForElement($userQuotationPage->xPathSearch($status), 30);
-        $I->click($userQuotationPage->xPathSearch($status));
-        $I->click(QuotationManagerPage::$buttonSave);
-        $I->click(QuotationManagerPage::$buttonSaveClose);
-        $I->waitForText($status, 30, QuotationManagerPage::$quotationStatus);
-    }
+		$I->click(QuotationManagerPage::$buttonSave);
+		try{
+			$I->waitForText(QuotationManagerPage::$messageSaveSuccess,5, QuotationManagerPage::$selectorSuccess);
+		}catch (\Exception $e)
+		{
+			$I->click(QuotationManagerPage::$buttonSave);
+			$I->see(QuotationManagerPage::$messageSaveSuccess, QuotationManagerPage::$selectorSuccess);
+		}
+	}
 
-    public function deleteQuotation()
-    {
-        $I = $this;
-        $I->amOnPage(QuotationManagerPage::$URL);
-        $I->checkAllResults();
-        $I->click(QuotationManagerPage::$buttonDelete);
-    }
+	public function editQuotation($newQuantity)
+	{
+		$I = $this;
+		$I->amOnPage(QuotationManagerPage::$URL);
+		$I->click(QuotationManagerPage::$quotationId);
+		$I->waitForElement(QuotationManagerPage::$quantityp1,30);
+		$I->scrollTo(QuotationManagerPage::$quantityp1);
+		$I->addValueForField(QuotationManagerPage::$quantityp1, $newQuantity, 5);
+		$I->click(QuotationManagerPage::$buttonSave);
+		$I->scrollTo(QuotationManagerPage::$quantityp1);
+		$I->seeInField(QuotationManagerPage::$quantityp1, $newQuantity);
+	}
+
+	public function editStatus($status)
+	{
+		$I = $this;
+		$I->amOnPage(QuotationManagerPage::$URL);
+		$I->click(QuotationManagerPage::$quotationId);
+		$I->waitForElement(QuotationManagerPage::$quantityp1,30);
+		$I->click(QuotationManagerPage::$quotationStatusDropDown);
+		$I->waitForElement(QuotationManagerPage::$quotationStatusSearch, 30);
+		$I->fillField(QuotationManagerPage::$quotationStatusSearch, $status);
+		$userQuotationPage = new QuotationManagerPage();
+		$I->waitForElement($userQuotationPage->xPathSearch($status), 30);
+		$I->click($userQuotationPage->xPathSearch($status));
+		$I->click(QuotationManagerPage::$buttonSave);
+		$I->click(QuotationManagerPage::$buttonSaveClose);
+		$I->waitForText($status, 30, QuotationManagerPage::$quotationStatus);
+	}
+
+	public function deleteQuotation()
+	{
+		$I = $this;
+		$I->amOnPage(QuotationManagerPage::$URL);
+		$I->checkAllResults();
+		$I->click(QuotationManagerPage::$buttonDelete);
+	}
+
+	/**
+	 * @param $firstname
+	 * @param $lastName
+	 * @param $address
+	 * @param $postalcode
+	 * @param $city
+	 * @param $stringPhone
+	 * @param $email
+	 * @param $userName
+	 * @param $passWord
+	 * @throws \Codeception\Exception\ModuleException
+	 * since 2.1.2
+	 */
+	public function checkPhoneNumber($firstname, $lastName, $address, $postalcode, $city, $stringPhone, $email, $userName, $passWord)
+	{
+		$I = $this;
+		$I->amOnPage(QuotationManagerPage::$URL);
+		$I->click(QuotationManagerPage::$buttonNew);
+		$I->fillField(UserManagerJoomla3Page::$firstName, $firstname);
+		$I->fillField(UserManagerJoomla3Page::$lastName, $lastName);
+		$I->fillField(UserManagerJoomla3Page::$address, $address);
+		$I->fillField(UserManagerJoomla3Page::$zipcode, $postalcode);
+		$I->fillField(UserManagerJoomla3Page::$city , $city);
+		$I->fillField(UserManagerJoomla3Page::$phone , $stringPhone);
+		$I->fillField(QuotationManagerPage::$email, $email);
+		$I->fillField(UserManagerJoomla3Page::$userName, $userName);
+		$I->fillField(UserManagerJoomla3Page::$newPassword , $passWord);
+		$I->fillField(UserManagerJoomla3Page::$confirmNewPassword, $passWord);
+		$I->click(UserManagerJoomla3Page::$saveButton);
+		$I->seeInPopup(QuotationManagerPage::$enterPhoneNumber);
+	}
 }
---
+++ tests/acceptance/administrator/g05/Orders/Quotation/QuotationCheckPhoneStringCest.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * @package     RedShop
+ * @subpackage  Cest
+ * @copyright   Copyright (C) 2008 - 2019 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+/**
+ * Class QuotationCheckPhoneStringCest
+ * since 2.1.2
+ */
+class QuotationCheckPhoneStringCest
+{
+	/**
+	 * @var
+	 * since 2.1.2
+	 */
+	protected $firstname;
+
+	/**
+	 * @var string
+	 * since 2.1.2
+	 */
+	protected $lastName;
+
+	/**
+	 * @var
+	 * since 2.1.2
+	 */
+	protected $address;
+
+	/**
+	 * @var
+	 * since 2.1.2
+	 */
+	protected $postalcode;
+
+	/**
+	 * @var
+	 * since 2.1.2
+	 */
+	protected $city;
+
+	/**
+	 * @var
+	 * since 2.1.2
+	 */
+	protected $stringPhone;
+
+	/**
+	 * @var string
+	 * since 2.1.2
+	 */
+	protected $email;
+	/**
+	 * @var string
+	 * since 2.1.2
+	 */
+	protected $username;
+
+	/**
+	 * @var string
+	 * since 2.1.2
+	 */
+	protected $passWord;
+
+	/**
+	 * Function to Test Quotation Creation in Backend
+	 *
+	 */
+	public function __construct()
+	{
+		$this->faker           = Faker\Factory::create();
+		$this->email           = $this->faker->email;
+		$this->firstname        = $this->faker->bothify('ManageUserAdministratorCest ?##?');
+		$this->lastName        = 'Last';
+		$this->address         = $this->faker->streetAddress;
+		$this->postalcode      = $this->faker->numberBetween(9999,999999);
+		$this->city            = $this->faker->city;
+		$this->stringPhone     = $this->faker->name;
+		$this->username        = $this->faker->bothify('ManageUserAdministratorCest ?##?');
+		$this->passWord        = $this->faker->bothify('Password ?##?');
+	}
+
+	/**
+	 * @param AcceptanceTester $I
+	 * @throws Exception
+	 */
+	public function _before(AcceptanceTester $I)
+	{
+		$I->doAdministratorLogin();
+	}
+
+	/**
+	 * @param \AcceptanceTester\QuotationManagerJoomla3Steps $I
+	 * @throws \Codeception\Exception\ModuleException
+	 */
+	public function checkStringPhone(\AcceptanceTester\QuotationManagerJoomla3Steps $I)
+	{
+		$I->wantTo('test string phone number');
+		$I->checkPhoneNumber($this->firstname, $this->lastName, $this->address, $this->postalcode, $this->city, $this->stringPhone, $this->email,$this->username, $this->passWord, $this->passWord );
+	}
+}
