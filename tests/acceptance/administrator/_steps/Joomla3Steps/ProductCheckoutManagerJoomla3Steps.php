a1194f4597b51cd16f37c9df0a06d03fd796c099
unknown <trang.nguyen@redweb.dk>
2019-05-22 14:52:14 +0700

    check run

--- tests/acceptance/administrator/_steps/Joomla3Steps/ProductCheckoutManagerJoomla3Steps.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/ProductCheckoutManagerJoomla3Steps.php
@@ -515,6 +515,7 @@ class ProductCheckoutManagerJoomla3Steps extends AdminManagerJoomla3Steps
 		$I->waitForElement(\FrontEndProductManagerJoomla3Page::$productList, 30);
 		$I->click($productFrontEndManagerPage->product($productName));
 		$I->waitForElement(\FrontEndProductManagerJoomla3Page::$addToCart, 30);
+		$I->waitForElementVisible(\FrontEndProductManagerJoomla3Page::$addToCart, 30);
 		$I->click(\FrontEndProductManagerJoomla3Page::$addToCart);
 		try{
 			$I->waitForElement(\FrontEndProductManagerJoomla3Page::$selectorSuccess, 60, \FrontEndProductManagerJoomla3Page::$selectorSuccess);
