38956035944c0e41870631bb7a2830f7c2b77b9e
unknown <trang.nguyen@redweb.dk>
2019-06-24 16:11:56 +0700

    run again

--- tests/acceptance/administrator/_steps/Joomla3Steps/Frontend/payment/CheckoutWithtAuthorizeDPMPayment.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/Frontend/payment/CheckoutWithtAuthorizeDPMPayment.php
@@ -46,6 +46,7 @@ class CheckoutWithtAuthorizeDPMPayment extends CheckoutMissingData
 				break;
 		}
 		$I->waitForElementVisible(AuthorizeDPMPaymentPage::$labelPayment, 30);
+		$I->scrollTo(AuthorizeDPMPaymentPage::$labelPayment);
 		$I->waitForElementVisible(AuthorizeDPMPaymentPage::$paymentAuthorizeDPM, 30);
 		$I->wait(0.5);
 		$I->click(AuthorizeDPMPaymentPage::$paymentAuthorizeDPM);
