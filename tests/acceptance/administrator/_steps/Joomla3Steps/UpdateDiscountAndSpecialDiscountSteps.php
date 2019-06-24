fa5ce27eafdd0689a555f16e01de844671b6c38e
VanTrieuThanh <thanh@redweb.dk>
2019-06-24 11:01:51 +0700

    update discount special order

--- tests/acceptance/administrator/_steps/Joomla3Steps/UpdateDiscountAndSpecialDiscountSteps.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/UpdateDiscountAndSpecialDiscountSteps.php
@@ -18,71 +18,77 @@ namespace AcceptanceTester;
  */
 class UpdateDiscountAndSpecialDiscountSteps extends OrderManagerJoomla3Steps
 {
-    public function updateDiscountAndSpecialDiscount($userName, $productName, $firstName, $discountUpdate, $specialUpdate)
-    {
-        $I = $this;
-        $I->amOnPage(\OrderManagerPage::$URL);
-        $I->click(\OrderManagerPage::$buttonNew);
-        $I->click(\OrderManagerPage::$userId);
-        $I->waitForElement(\OrderManagerPage::$userSearch, 30);
-        $userOrderPage = new \OrderManagerPage();
+	/**
+	 * @param $userName
+	 * @param $productName
+	 * @param $firstName
+	 * @param $discountUpdate
+	 * @param $specialUpdate
+	 * @param $randomProductPrice
+	 */
+	public function updateDiscountAndSpecialDiscount($userName, $productName, $firstName, $discountUpdate, $specialUpdate, $randomProductPrice)
+	{
+		$I = $this;
+		$I->amOnPage(\OrderManagerPage::$URL);
+		$I->click(\OrderManagerPage::$buttonNew);
+		$I->click(\OrderManagerPage::$userId);
+		$I->waitForElement(\OrderManagerPage::$userSearch, 30);
+		$userOrderPage = new \OrderManagerPage();
 
-        $I->fillField(\OrderManagerPage::$userSearch, $userName);
-        $I->waitForElement($userOrderPage->returnSearch($userName), 30);
-        $I->pressKey(\OrderManagerPage::$userSearch, \Facebook\WebDriver\WebDriverKeys::ENTER);
-        $I->waitForElement(\OrderManagerPage::$fistName, 30);
-        $I->see($userName);
-        $I->wait(2);
+		$I->fillField(\OrderManagerPage::$userSearch, $userName);
+		$I->waitForElement($userOrderPage->returnSearch($userName), 30);
+		$I->pressKey(\OrderManagerPage::$userSearch, \Facebook\WebDriver\WebDriverKeys::ENTER);
+		$I->waitForElement(\OrderManagerPage::$fistName, 30);
+		$I->see($userName);
+		$I->wait(2);
 
-        $I->waitForElement(\OrderManagerPage::$address);
-        $I->waitForElementVisible(\OrderManagerPage::$address);
-        $I->fillField(\OrderManagerPage::$address, 'address');
-        $I->fillField(\OrderManagerPage::$zipcode, 1201010);
-        $I->fillField(\OrderManagerPage::$city, "address");
-        $I->fillField(\OrderManagerPage::$phone, '123100120101');
+		$I->waitForElement(\OrderManagerPage::$address, 30);
+		$I->waitForElementVisible(\OrderManagerPage::$address, 30);
+		$I->fillField(\OrderManagerPage::$address, 'address');
+		$I->fillField(\OrderManagerPage::$zipcode, 1201010);
+		$I->fillField(\OrderManagerPage::$city, "address");
+		$I->fillField(\OrderManagerPage::$phone, '123100120101');
 
-        $I->waitForElement(\OrderManagerPage::$applyUser, 30);
-        $I->executeJS("jQuery('.button-apply').click()");
-        $I->waitForElement(\OrderManagerPage::$productId, 30);
-        $I->scrollTo(\OrderManagerPage::$productId);
-        $I->waitForElement(\OrderManagerPage::$productId, 30);
-        $I->click(\OrderManagerPage::$productId);
-        $I->waitForElement(\OrderManagerPage::$productsSearch, 30);
-        $I->fillField(\OrderManagerPage::$productsSearch, $productName);
-        $I->waitForElement($userOrderPage->returnSearch($productName), 30);
-        $I->click($userOrderPage->returnSearch($productName));
+		$I->waitForElement(\OrderManagerPage::$applyUser, 30);
+		$I->executeJS("jQuery('.button-apply').click()");
+		$I->waitForElement(\OrderManagerPage::$productId, 30);
+		$I->executeJS('window.scrollTo(65,80);');
+		$I->waitForElementVisible(\OrderManagerPage::$productId, 30);
+		$I->click(\OrderManagerPage::$productId);
+		$I->waitForElement(\OrderManagerPage::$productsSearch, 30);
+		$I->fillField(\OrderManagerPage::$productsSearch, $productName);
+		$I->waitForElement($userOrderPage->returnSearch($productName), 30);
+		$I->click($userOrderPage->returnSearch($productName));
+		$I->wait(0.2);
+		$I->click(\OrderManagerPage::$buttonSave);
 
-        $I->click(\OrderManagerPage::$buttonSave);
+		$I->click(\OrderManagerPage::$buttonClose);
+		$I->searchOrder($firstName);
+		$id = $I->grabTextFrom("//div[@class='table-responsive']//td[3]//a[1]");
+		$I->fillField(\OrderManagerPage::$filter, $firstName);
+		$I->click("//div[@class='table-responsive']//td[3]//a[1]");
 
-        $I->click(\OrderManagerPage::$buttonClose);
-        $I->searchOrder($firstName);
-        $id = $I->grabTextFrom("//div[@class='table-responsive']//td[3]//a[1]");
-        $I->fillField(\OrderManagerPage::$filter, $firstName);
-        $I->click("//div[@class='table-responsive']//td[3]//a[1]");
-        $I->pauseExecution();
+		$I->scrollTo(\OrderManagerPage::$discountUpdate);
+		$I->waitForElementVisible(\OrderManagerPage::$discountUpdate, 30);
+		$I->fillField(\OrderManagerPage::$discountUpdate, $discountUpdate);
+		$I->waitForElement($userOrderPage->returnButtonUpdateDiscount($id), 30);
+		$I->executeJS('window.scrollTo(65,80);');
+		$I->waitForElementVisible(\OrderManagerPage::$discountUpdate, 30);
+		$I->click($userOrderPage->returnButtonUpdateDiscount($id));
 
-        $I->scrollTo(\OrderManagerPage::$discountUpdate);
-        $I->click(\OrderManagerPage::$discountUpdate);
-        $I->fillField(\OrderManagerPage::$discountUpdate, $discountUpdate);
-        $I->waitForElement($userOrderPage->returnButtonUpdateDiscount($id), 30);
-        $I->click($userOrderPage->returnButtonUpdateDiscount($id));
-        $I->scrollTo(\OrderManagerPage::$discountUpdate);
-        $I->see(\OrderManagerPage::$discountUpdate, $discountUpdate);
+		$I->scrollTo(\OrderManagerPage::$specialUpdate);
+		$I->waitForElementVisible(\OrderManagerPage::$specialUpdate, 30);
+		$I->fillField(\OrderManagerPage::$specialUpdate, $specialUpdate);
+		$I->waitForElement($userOrderPage->returnButtonSpecialDiscount($id), 30);
+		$I->executeJS('window.scrollTo(65,80);');
+		$I->waitForElementVisible(\OrderManagerPage::$specialUpdate, 30);
+		$I->click($userOrderPage->returnButtonSpecialDiscount($id));
+		$I->scrollTo(\OrderManagerPage::$specialUpdate);
+		$adminFinalPriceEnd = $randomProductPrice-($discountUpdate+$specialUpdate);
+		$I->see($adminFinalPriceEnd);
 
+		$I->waitForElement(\OrderManagerPage::$close, 30);
+		$I->waitForText(\OrderManagerPage::$buttonClose, 10, \OrderManagerPage::$close);
 
-        $I->scrollTo(\OrderManagerPage::$specialUpdate);
-        $I->click(\OrderManagerPage::$specialUpdate);
-        $I->fillField(\OrderManagerPage::$specialUpdate, $specialUpdate);
-        $I->waitForElement($userOrderPage->returnButtonSpecialDiscount($id), 30);
-        $I->click($userOrderPage->returnButtonSpecialDiscount($id));
-        $I->scrollTo(\OrderManagerPage::$specialUpdate);
-        $I->see(\OrderManagerPage::$specialUpdate, $specialUpdate);
-
-        $I->scrollTo(\OrderManagerPage::$specialUpdate);
-        $I->waitForText('60');
-
-        $I->waitForElement(\OrderManagerPage::$close, 30);
-        $I->waitForText(\OrderManagerPage::$buttonClose, 10, \OrderManagerPage::$close);
-
-    }
+	}
 }
