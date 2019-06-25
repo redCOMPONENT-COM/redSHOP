3d7c2f7f72d571bfc472402c9dbc12bdb583feb0
hoanghairedweb <hai.le@redweb.dk>
2019-06-25 11:37:56 +0700

    run again

--- tests/acceptance/administrator/g07/Configuration/ProductsConfigurationCest.php
+++ tests/acceptance/administrator/g07/Configuration/ProductsConfigurationCest.php
@@ -94,7 +94,7 @@ class ProductsConfigurationCest
 	 */
 	public function checkDisplayOutOfStockAttributeYes(AcceptanceTester $I,$scenario )
 	{
-		$I->wantTo('Test enable Stockroom in configuration');
+		$I->wantTo('Test enable Stockroom in Configuration');
 		$I = new ConfigurationSteps($scenario);
 		$I->featureUsedStockRoom();
 
