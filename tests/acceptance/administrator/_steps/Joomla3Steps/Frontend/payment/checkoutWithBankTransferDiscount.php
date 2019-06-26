9cae1ba088d3f9df781e8190f4f1629e7e1e2c90
unknown <trang.nguyen@redweb.dk>
2019-06-26 09:43:53 +0700

    add code

--- tests/acceptance/administrator/_steps/Joomla3Steps/Frontend/payment/checkoutWithBankTransferDiscount.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/Frontend/payment/checkoutWithBankTransferDiscount.php
@@ -38,6 +38,19 @@ class checkoutWithBankTransferDiscount extends CheckoutMissingData
 				$I->amOnPage(FrontEndProductManagerJoomla3Page:: $checkoutURL);
 				break;
 			case 'OneStepCheckout':
+				$I->waitForElement(FrontEndProductManagerJoomla3Page::$radioCompany, 30);
+				$I->comment('Business');
+				$I->waitForElement(FrontEndProductManagerJoomla3Page::$radioCompany, 30);
+				$I->wait(1);
+				$I->click(FrontEndProductManagerJoomla3Page::$radioCompany);
+				try
+				{
+					$I->waitForElementVisible(FrontEndProductManagerJoomla3Page::$idCompanyNameOnePage, 30);
+				}catch (\Exception $e)
+				{
+					$I->click(FrontEndProductManagerJoomla3Page::$radioIDCompany);
+					$I->waitForElementVisible(FrontEndProductManagerJoomla3Page::$idCompanyNameOnePage, 30);
+				}
 				$I->fillInformationBusiness($customerInformation);
 				break;
 		}
