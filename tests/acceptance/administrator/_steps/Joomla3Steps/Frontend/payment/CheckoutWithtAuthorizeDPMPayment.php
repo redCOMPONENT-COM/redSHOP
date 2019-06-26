887c765eed6b355d791e4da837526abc9dd30278
unknown <trang.nguyen@redweb.dk>
2019-06-26 13:11:14 +0700

    fix fail random

--- tests/acceptance/administrator/_steps/Joomla3Steps/Frontend/payment/CheckoutWithtAuthorizeDPMPayment.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/Frontend/payment/CheckoutWithtAuthorizeDPMPayment.php
@@ -46,20 +46,20 @@ class CheckoutWithtAuthorizeDPMPayment extends CheckoutMissingData
 		$I->waitForElementVisible(AuthorizeDPMPaymentPage::$labelPayment, 30);
 		$I->executeJS("window.scrollTo(150,150);");
 		$I->waitForElementVisible(AuthorizeDPMPaymentPage::$paymentAuthorizeDPM, 30);
-		$I->wait(0.5);
 		$I->click(AuthorizeDPMPaymentPage::$paymentAuthorizeDPM);
-		$I->waitForElementVisible(AuthorizeDPMPaymentPage::$typeCard, 30);
-		$I->click(AuthorizeDPMPaymentPage::$typeCard);
-		$I->waitForElementVisible(AuthorizeDPMPaymentPage:: $selectExpireMonth, 30);
-		$I->selectOption(AuthorizeDPMPaymentPage:: $selectExpireMonth, $checkoutAccountDetail['cardExpiryMonth']);
-		$I->waitForElementVisible(AuthorizeDPMPaymentPage:: $selectExpireYear, 30);
-		$I->selectOption(AuthorizeDPMPaymentPage:: $selectExpireYear, $checkoutAccountDetail['cardExpiryYear']);
+		$I->wait(0.5);
 		$I->waitForElementVisible(AuthorizeDPMPaymentPage:: $cardName, 30);
 		$I->fillField(AuthorizeDPMPaymentPage:: $cardName, $checkoutAccountDetail['customerName']);
 		$I->waitForElementVisible(AuthorizeDPMPaymentPage:: $cardNumber, 30);
 		$I->fillField(AuthorizeDPMPaymentPage:: $cardNumber, $checkoutAccountDetail['debitCardNumber']);
 		$I->waitForElementVisible(AuthorizeDPMPaymentPage:: $cardCode, 30);
 		$I->fillField( AuthorizeDPMPaymentPage::$cardCode, $checkoutAccountDetail['cvv']);
+		$I->waitForElementVisible(AuthorizeDPMPaymentPage::$typeCard, 30);
+		$I->click(AuthorizeDPMPaymentPage::$typeCard);
+		$I->waitForElementVisible(AuthorizeDPMPaymentPage:: $selectExpireMonth, 30);
+		$I->selectOption(AuthorizeDPMPaymentPage:: $selectExpireMonth, $checkoutAccountDetail['cardExpiryMonth']);
+		$I->waitForElementVisible(AuthorizeDPMPaymentPage:: $selectExpireYear, 30);
+		$I->selectOption(AuthorizeDPMPaymentPage:: $selectExpireYear, $checkoutAccountDetail['cardExpiryYear']);
 		$I->waitForElementVisible($productFrontEndManagerPage->product($productName), 30);
 
 		$I->waitForElementVisible(AuthorizeDPMPaymentPage::$acceptTerms, 30);
