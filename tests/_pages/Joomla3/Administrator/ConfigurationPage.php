6417610018a6419e98ec723cda9260fbb3cc4133
hoanghairedweb <hai.le@redweb.dk>
2019-07-15 09:59:14 +0700

    fix

--- tests/_pages/Joomla3/Administrator/ConfigurationPage.php
+++ tests/_pages/Joomla3/Administrator/ConfigurationPage.php
@@ -469,7 +469,7 @@ class ConfigurationPage extends AdminJ3Page
 	public static $productTab = '//a[@href="#producttab"]';
 	/**
 	 * @var string
-	 * @sice 2.1.2
+	 * @since 2.1.2
 	 */
 	public static $stockRoomAttributeYes = '//label[@id="display_out_of_stock_attribute_data_1-lbl"]';
 
