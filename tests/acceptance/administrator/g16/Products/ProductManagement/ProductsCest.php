7c2ef62c085a58107fbf6039e1afea15148bde01
hoanghairedweb <hai.le@redweb.dk>
2019-07-22 17:56:54 +0700

    run

--- tests/acceptance/administrator/g16/Products/ProductManagement/ProductsCest.php
+++ tests/acceptance/administrator/g16/Products/ProductManagement/ProductsCest.php
@@ -263,7 +263,7 @@ class ProductsCest
 	{
 		$I->wantTo('Test create Product with attribute Save Manager in Administrator');
 		$I = new AcceptanceTester\ProductManagerJoomla3Steps($scenario);
-		$I->wantTo('I Want To Add product inside the category');
+		$I->wantTo('I Want To Add Product inside the category');
 		$I->createProductWithAttribute($this->randomProductNameAttribute, $this->randomCategoryName, $this->randomProductAttributeNumber, $this->randomProductPrice, $this->nameAttribute, $this->valueAttribute, $this->priceAttribute);
 		$I->checkStatusCopyProductAttrbute($this->randomProductNameAttribute);
 	}
