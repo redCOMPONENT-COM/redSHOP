a9f6b307f0227c94480065c17a27cbb4fe087684
hoanghairedweb <hai.le@redweb.dk>
2019-07-24 10:26:14 +0700

    run

--- tests/acceptance/administrator/g16/Products/ProductManagement/ProductsCest.php
+++ tests/acceptance/administrator/g16/Products/ProductManagement/ProductsCest.php
@@ -275,27 +275,4 @@ class ProductsCest
 		$I->createProductWithAttribute($this->randomProductNameAttribute, $this->randomCategoryName, $this->ProductAttributeCoppyNumber, $this->randomProductPrice, $this->nameAttribute, $this->valueAttribute, $this->priceAttribute);
 		$I->checkStatusCopyProductAttrbute($this->randomProductNameAttribute);
 	}
-
-	/**
-	 * @param ProductManagerJoomla3Steps $I
-	 * @throws Exception
-	 * @since 2.1.3
-	 */
-	public function deteleAllProducts(ProductManagerSteps $I)
-	{
-		$I->wantTo('Delete Product Manager in Administrator');
-		$I->deteleAllProducts();
-	}
-
-	/**
-	 * @param AcceptanceTester $I
-	 * @param $scenario
-	 * @since 2.1.3
-	 */
-	public function deteleCatagory(AcceptanceTester $I, $scenario)
-	{
-		$I->wantTo('Delete Category');
-		$I = new CategoryManagerJoomla3Steps($scenario);
-		$I->deleteCategory($this->randomCategoryName);
-	}
 }
