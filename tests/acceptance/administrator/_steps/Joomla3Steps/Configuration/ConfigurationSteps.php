1e964bac669e55d4b1234743c06f64e1cb1aeda6
hoanghairedweb <hai.le@redweb.dk>
2019-08-19 13:04:25 +0700

    run

--- tests/acceptance/administrator/_steps/Joomla3Steps/Configuration/ConfigurationSteps.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/Configuration/ConfigurationSteps.php
@@ -551,7 +551,7 @@ class ConfigurationSteps extends AdminManagerJoomla3Steps
 			$I->waitForElementVisible(ConfigurationPage::$resetOderId, 30);
 			$I->click(ConfigurationPage::$resetOderId);
 			$I->acceptPopup();
-			$I->wait(1);
+			$I->wait(2);
 			$I->canSeeInPopup(ConfigurationPage::$messagePopup);
 			$I->seeInPopup(ConfigurationPage::$messagePopup);
 			$I->acceptPopup();
