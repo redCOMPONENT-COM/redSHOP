d05e04912476aef307c24a8ad56a6ec40351a8ec
hoanghairedweb <hai.le@redweb.dk>
2019-09-03 09:20:23 +0700

    oder receipt

--- tests/acceptance/administrator/_steps/Joomla3Steps/ProductCheckoutManagerJoomla3Steps.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/ProductCheckoutManagerJoomla3Steps.php
@@ -1061,7 +1061,8 @@ class ProductCheckoutManagerJoomla3Steps extends AdminManagerJoomla3Steps
 		$I->waitForElement(\FrontEndProductManagerJoomla3Page::$acceptTerms, 30);
 		$I->click(\FrontEndProductManagerJoomla3Page::$acceptTerms);
 		$I->click(\FrontEndProductManagerJoomla3Page::$checkoutFinalStep);
-		$I->waitForElement(\FrontEndProductManagerJoomla3Page::$orderReceiptTitle, 30);
+		$I->waitfortext("Order receipt",30);
+//		$I->waitForElement(\FrontEndProductManagerJoomla3Page::$orderReceiptTitle, 30);
 		$I->seeElement(['link' => $productName]);
 	}
 
