223a6461ee2c193c784430ed27d1aa9fb93151db
Pandalove <phamminhphuc102@gmail.com>
2019-09-04 10:24:32 +0700

    REDCOMSITE-325: Send information for alert_notifications plugin (low stock alert): test: automation

--- plugins/redshop_alert/low_stock_alert/low_stock_alert.php
+++ plugins/redshop_alert/low_stock_alert/low_stock_alert.php
@@ -28,7 +28,7 @@ class PlgRedshop_AlertLow_Stock_Alert extends JPlugin
 		}
 	}
 
-	public function storeLowStockAlert($cart)
+	public function storeLowStockAlert(&$cart)
 	{
 		// get ID Custom Field Min Stock:
 		$id_custom_field_min_stock = $this->params->get('id_low_stock_alert');
@@ -142,8 +142,6 @@ class PlgRedshop_AlertLow_Stock_Alert extends JPlugin
 				}
 			}
 		}
-
-		return $cart;
 	}
 
 	public function sendMail( $message= null , $mail = null )
