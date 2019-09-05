13933f2e30e437c82d7b0b89849f6c9f9f5fc181
VanTrieuThanh <thanh@redweb.dk>
2019-09-05 09:25:53 +0700

    update style

--- tests/acceptance/administrator/Oder_Backend/OrderDiscount/OrderDiscountAndSpecialUpdateCest.php
+++ tests/acceptance/administrator/Oder_Backend/OrderDiscount/OrderDiscountAndSpecialUpdateCest.php
@@ -5,6 +5,7 @@
  * @copyright   Copyright (C) 2008 - 2019 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
+
 use AcceptanceTester\CategoryManagerJoomla3Steps;
 use AcceptanceTester\ProductManagerJoomla3Steps;
 use AcceptanceTester\UserManagerJoomla3Steps;
--- tests/acceptance/administrator/_steps/Joomla3Steps/OrderUpdateDiscountAndSpecialDiscountSteps.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/OrderUpdateDiscountAndSpecialDiscountSteps.php
@@ -5,8 +5,11 @@
  * @copyright   Copyright (C) 2008 - 2019 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
+
 namespace AcceptanceTester;
+
 use OrderManagerPage;
+
 /**
  * Class ProductManagerJoomla3Steps
  *
