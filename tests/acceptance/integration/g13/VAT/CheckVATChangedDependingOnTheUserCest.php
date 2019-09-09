c62207954956f1f5b59949fd07aad6fff370aa7c
hoanghairedweb <hai.le@redweb.dk>
2019-09-09 15:40:59 +0700

    fix fail delete

--- tests/acceptance/integration/g13/VAT/CheckVATChangedDependingOnTheUserCest.php
+++ tests/acceptance/integration/g13/VAT/CheckVATChangedDependingOnTheUserCest.php
@@ -344,8 +344,8 @@ class CheckVATChangedDependingOnTheUserCest
 
 		$I->wantTo('Delete User');
 		$I = new UserManagerJoomla3Steps($scenario);
-		$I->deleteUser($this->firstNameDM);
-		$I->deleteUser($this->firstNameVN);
+		$I->deleteUser($this->userDM["userName"]);
+		$I->deleteUser($this->userVN["userName"]);
 
 		$I->wantTo('Delete Product');
 		$I = new ProductManagerJoomla3Steps($scenario);
