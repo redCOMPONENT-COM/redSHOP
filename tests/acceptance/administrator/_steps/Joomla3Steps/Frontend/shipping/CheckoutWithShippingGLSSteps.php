672d4ec9e9a7a909e0a453248b6c7935b6f61a13
NganCao <en.pyrish.ym@gmail.com>
2019-09-10 15:37:12 +0700

    run

--- tests/acceptance/administrator/_steps/Joomla3Steps/Frontend/shipping/CheckoutWithShippingGLSSteps.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/Frontend/shipping/CheckoutWithShippingGLSSteps.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     redSHOP
- * @subpackage  sTEP CheckoutWithShippingGLS
+ * @subpackage  Step CheckoutWithShippingGLS
  * @copyright   Copyright (C) 2008 - 2019 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
