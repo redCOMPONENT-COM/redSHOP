cf29038a94ad583de90098032aafb38540b5bfd9
hoanghairedweb <hai.le@redweb.dk>
2019-09-11 13:19:55 +0700

    run full file drone

--- tests/_pages/Joomla3/Administrator/UserManagerJoomla3Page.php
+++ tests/_pages/Joomla3/Administrator/UserManagerJoomla3Page.php
@@ -15,285 +15,285 @@
  */
 class UserManagerJoomla3Page
 {
-    /**
-     * @var string
-     */
-    public static $namePage = "User Management";
+	/**
+	 * @var string
+	 */
+	public static $namePage = "User Management";
 
-    /**
-     * @var string
-     */
-    public static $pageNotice = "User Manager Page";
+	/**
+	 * @var string
+	 */
+	public static $pageNotice = "User Manager Page";
 
-    /**
-     * @var string
-     */
-    public static $URL = '/administrator/index.php?option=com_redshop&view=user';
+	/**
+	 * @var string
+	 */
+	public static $URL = '/administrator/index.php?option=com_redshop&view=user';
 
-    /**
-     * @var string
-     */
-    public static $URLJoomla = '/administrator/index.php?option=com_users&view=users';
+	/**
+	 * @var string
+	 */
+	public static $URLJoomla = '/administrator/index.php?option=com_users&view=users';
 
-    /**
-     * @var string
-     */
-    public static $userJoomla = "//table[@id='userList']//tbody/tr[1]";
+	/**
+	 * @var string
+	 */
+	public static $userJoomla = "//table[@id='userList']//tbody/tr[1]";
 
-    /**
-     * @var string
-     */
-    public static $filter = "#filter";
+	/**
+	 * @var string
+	 */
+	public static $filter = "#filter";
 
-    /**
-     * @var string
-     */
-    public static $URLShipping = '/administrator/index.php?option=com_redshop&view=user_detail&task=edit&shipping=1&info_id=1&cid[]=0';
+	/**
+	 * @var string
+	 */
+	public static $URLShipping = '/administrator/index.php?option=com_redshop&view=user_detail&task=edit&shipping=1&info_id=1&cid[]=0';
 
-    /**
-     * @var string
-     */
-    public static $generalUserInformationTab = "//a[contains(text(), 'General User Information')]";
+	/**
+	 * @var string
+	 */
+	public static $generalUserInformationTab = "//a[contains(text(), 'General User Information')]";
 
-    /**
-     * @var string
-     */
-    public static $billingInformationTab = "//a[contains(text(), 'Billing Information')]";
+	/**
+	 * @var string
+	 */
+	public static $billingInformationTab = "//a[contains(text(), 'Billing Information')]";
 
-    /**
-     * @var string
-     */
-    public static $shippingInformation = "//a[contains(text(),'Shipping Information')]";
+	/**
+	 * @var string
+	 */
+	public static $shippingInformation = "//a[contains(text(),'Shipping Information')]";
 
-    /**
-     * @var string
-     */
-    public static $userName = "//input[@id='username']";
+	/**
+	 * @var string
+	 */
+	public static $userName = "//input[@id='username']";
 
-    /**
-     * @var string
-     */
-    public static $newPassword = "//input[@id='password']";
+	/**
+	 * @var string
+	 */
+	public static $newPassword = "//input[@id='password']";
 
-    /**
-     * @var string
-     */
-    public static $confirmNewPassword = "//input[@id='password2']";
+	/**
+	 * @var string
+	 */
+	public static $confirmNewPassword = "//input[@id='password2']";
 
-    /**
-     * @var string
-     */
-    public static $email = "//input[@id='email']";
+	/**
+	 * @var string
+	 */
+	public static $email = "//input[@id='email']";
 
-    /**
-     * @var string
-     */
-    public static $groupRadioButton = "//form[@id='adminForm']/div[1]/dd[1]/div/fieldset/div/table/tbody/tr[6]/td[2]";
+	/**
+	 * @var string
+	 */
+	public static $groupRadioButton = "//form[@id='adminForm']/div[1]/dd[1]/div/fieldset/div/table/tbody/tr[6]/td[2]";
 
-    /**
-     * @var string
-     */
-    public static $shopperGroupDropDown = "//div[@id='s2id_shopper_group_id']/a";
+	/**
+	 * @var string
+	 */
+	public static $shopperGroupDropDown = "//div[@id='s2id_shopper_group_id']/a";
 
-    /**
-     * @var string
-     */
-    public static $firstName = "//input[@id='firstname']";
+	/**
+	 * @var string
+	 */
+	public static $firstName = "//input[@id='firstname']";
 
-    /**
-     * @var string
-     */
-    public static $lastName = "//input[@id='lastname']";
+	/**
+	 * @var string
+	 */
+	public static $lastName = "//input[@id='lastname']";
 
-    /**
-     * @var string
-     */
-    public static $address = "//input[@id='address']";
+	/**
+	 * @var string
+	 */
+	public static $address = "//input[@id='address']";
 
-    /**
-     * @var string
-     */
-    public static $phone = "//input[@id='phone']";
+	/**
+	 * @var string
+	 */
+	public static $phone = "//input[@id='phone']";
 
-    /**
-     * @var string
-     */
-    public static $zipcode = "//input[@id='zipcode']";
+	/**
+	 * @var string
+	 */
+	public static $zipcode = "//input[@id='zipcode']";
 
-    /**
-     * @var string
-     */
-    public static $city = "//input[@id='city']";
+	/**
+	 * @var string
+	 */
+	public static $city = "//input[@id='city']";
 
-    /**
-     * @var string
-     */
-    public static $userSuccessMessage = 'User detail saved';
+	/**
+	 * @var string
+	 */
+	public static $userSuccessMessage = 'User detail saved';
 
-    /**
-     * @var string
-     */
-    public static $saveErrorEmailAlready = "Email already exists ";
+	/**
+	 * @var string
+	 */
+	public static $saveErrorEmailAlready = "Email already exists ";
 
-    /**
-     * @var string
-     */
-    public static $saveErrorUserAlready = "Username already exists";
+	/**
+	 * @var string
+	 */
+	public static $saveErrorUserAlready = "Username already exists";
 
-    /**
-     * @var string
-     */
-    public static $saveError = "Error";
+	/**
+	 * @var string
+	 */
+	public static $saveError = "Error";
 
-    /**
-     * @var string
-     */
-    public static $emailInvalid = "The email address you entered is invalid. Please enter another email address.";
+	/**
+	 * @var string
+	 */
+	public static $emailInvalid = "The email address you entered is invalid. Please enter another email address.";
 
-    /**
-     * @var string
-     */
-    public static $firstResultRow = "//div[@id='editcell']//table[1]//tbody/tr[1]";
+	/**
+	 * @var string
+	 */
+	public static $firstResultRow = "//div[@id='editcell']//table[1]//tbody/tr[1]";
 
-    /**
-     * @var string
-     */
-    public static $selectFirst = "//input[@id='cb0']";
+	/**
+	 * @var string
+	 */
+	public static $selectFirst = "//input[@id='cb0']";
 
-    /**
-     * @var string
-     */
-    public static $headPage = "//h1";
-
-
-    //button
-
-    /**
-     * @var string
-     */
-    public static $newButton = "New";
-
-    /**
-     * @var string
-     */
-    public static $resetButton = ".reset";
-
-    /**
-     * @var string
-     */
-    public static $saveButton = "Save";
-
-    /**
-     * @var string
-     */
-    public static $unpublishButton = "Unpublish";
-
-    /**
-     * @var string
-     */
-    public static $publishButton = "Publish";
-
-    /**
-     * @var string
-     */
-    public static $saveCloseButton = "Save & Close";
-
-    /**
-     * @var string
-     */
-    public static $deleteButton = "Delete";
-
-    /**
-     * @var string
-     */
-    public static $editButton = "Edit";
-
-    /**
-     * @var string
-     */
-    public static $cancelButton = "Cancel";
-
-    /**
-     * @var string
-     */
-    public static $closeButton = "Close";
-
-    /**
-     * @var string
-     */
-    public static $addButton = "Add";
-
-    //selector
-
-    /**
-     * @var string
-     */
-    public static $selectorSuccess = '.alert-success';
-
-    /**
-     * @var string
-     */
-    public static $selectorError = '.alert-danger';
-
-    /**
-     * @var string
-     */
-    public static $selectorNamePage = '.page-title';
+	/**
+	 * @var string
+	 */
+	public static $headPage = "//h1";
 
 
-    //selector
+	//button
 
-    /**
-     * @var string
-     */
-    public static $xPathError = "//div[@id='system-message']/div/div/p[1]";
+	/**
+	 * @var string
+	 */
+	public static $newButton = "New";
 
-    /**
-     * @var string
-     */
-    public static $errorUserReady = "//div[@id='system-message']/div/h4";
+	/**
+	 * @var string
+	 */
+	public static $resetButton = ".reset";
 
-    /**
-     * @var string
-     */
-    public static $selectorPageManagement = '.page-title';
+	/**
+	 * @var string
+	 */
+	public static $saveButton = "Save";
+
+	/**
+	 * @var string
+	 */
+	public static $unpublishButton = "Unpublish";
+
+	/**
+	 * @var string
+	 */
+	public static $publishButton = "Publish";
+
+	/**
+	 * @var string
+	 */
+	public static $saveCloseButton = "Save & Close";
+
+	/**
+	 * @var string
+	 */
+	public static $deleteButton = "Delete";
+
+	/**
+	 * @var string
+	 */
+	public static $editButton = "Edit";
+
+	/**
+	 * @var string
+	 */
+	public static $cancelButton = "Cancel";
+
+	/**
+	 * @var string
+	 */
+	public static $closeButton = "Close";
+
+	/**
+	 * @var string
+	 */
+	public static $addButton = "Add";
+
+	//selector
+
+	/**
+	 * @var string
+	 */
+	public static $selectorSuccess = '.alert-success';
+
+	/**
+	 * @var string
+	 */
+	public static $selectorError = '.alert-danger';
+
+	/**
+	 * @var string
+	 */
+	public static $selectorNamePage = '.page-title';
 
 
-    //page title
+	//selector
 
-    /**
-     * @var string
-     */
-    public static $pageDetail = "First Name";
+	/**
+	 * @var string
+	 */
+	public static $xPathError = "//div[@id='system-message']/div/div/p[1]";
 
-    /**
-     * @var string
-     */
-    public static $pageDetailSelector = '.title';
+	/**
+	 * @var string
+	 */
+	public static $errorUserReady = "//div[@id='system-message']/div/h4";
 
-    //get link user
+	/**
+	 * @var string
+	 */
+	public static $selectorPageManagement = '.page-title';
 
-    /**
-     * @var array
-     */
-    public static $linkUser = ['link' => 'ID'];
 
-    /**
-     * @var string
-     */
-    public static $btnPlaceOder = "//button[@class='btn btn-small button-redshop_order32']";
+	//page title
 
-    /**
-     * Function to get the path for Shopper Group
-     *
-     * @param   String $shopperGroup Group of Shopper
-     *
-     * @return string
-     */
-    public function shopperGroup($shopperGroup)
-    {
-        $path = "//div[@class='select2-result-label'][contains(text(), '" . $shopperGroup . "')]";
+	/**
+	 * @var string
+	 */
+	public static $pageDetail = "First Name";
 
-        return $path;
-    }
+	/**
+	 * @var string
+	 */
+	public static $pageDetailSelector = '.title';
+
+	//get link user
+
+	/**
+	 * @var array
+	 */
+	public static $linkUser = ['link' => 'ID'];
+
+	/**
+	 * @var string
+	 */
+	public static $btnPlaceOder = "//button[@class='btn btn-small button-redshop_order32']";
+
+	/**
+	 * Function to get the path for Shopper Group
+	 *
+	 * @param   String $shopperGroup Group of Shopper
+	 *
+	 * @return string
+	 */
+	public function shopperGroup($shopperGroup)
+	{
+		$path = "//div[@class='select2-result-label'][contains(text(), '" . $shopperGroup . "')]";
+
+		return $path;
+	}
 }
--- tests/acceptance/integration/g17/Module/ModuleMultiCurrenciesCest.php
+++ tests/acceptance/integration/g17/Module/ModuleMultiCurrenciesCest.php
@@ -18,151 +18,151 @@ use Frontend\Module\MultiCurrenciesSteps;
  */
 class ModuleMultiCurrenciesCest
 {
-    /**
-     * @var \Faker\Generator
-     * @since 2.1.3
-     */
-    protected $faker;
+	/**
+	 * @var \Faker\Generator
+	 * @since 2.1.3
+	 */
+	protected $faker;
 
-    /**
-     * @var string
-     * @since 2.1.3
-     */
-    protected $categoryName;
+	/**
+	 * @var string
+	 * @since 2.1.3
+	 */
+	protected $categoryName;
 
-    /**
-     * @var string
-     * @since 2.1.3
-     */
-    protected $productName;
+	/**
+	 * @var string
+	 * @since 2.1.3
+	 */
+	protected $productName;
 
-    /**
-     * @var int
-     * @since 2.1.3
-     */
-    protected $productNumber;
+	/**
+	 * @var int
+	 * @since 2.1.3
+	 */
+	protected $productNumber;
 
-    /**
-     * @var int
-     * @since 2.1.3
-     */
-    protected $productPrice;
+	/**
+	 * @var int
+	 * @since 2.1.3
+	 */
+	protected $productPrice;
 
-    /**
-     * @var int
-     * @since 2.1.3
-     */
-    protected $minimumQuantity;
+	/**
+	 * @var int
+	 * @since 2.1.3
+	 */
+	protected $minimumQuantity;
 
-    /**
-     * @var int
-     * @since 2.1.3
-     */
-    protected $maximumQuantity;
+	/**
+	 * @var int
+	 * @since 2.1.3
+	 */
+	protected $maximumQuantity;
 
-    /**
-     * @var string
-     * @since 2.1.3
-     */
-    protected $extensionURL;
+	/**
+	 * @var string
+	 * @since 2.1.3
+	 */
+	protected $extensionURL;
 
-    /**
-     * @var string
-     * @since 2.1.3
-     */
-    protected $moduleName;
+	/**
+	 * @var string
+	 * @since 2.1.3
+	 */
+	protected $moduleName;
 
-    /**
-     * @var string
-     * @since 2.1.3
-     */
-    protected $moduleURL;
+	/**
+	 * @var string
+	 * @since 2.1.3
+	 */
+	protected $moduleURL;
 
-    /**
-     * @var string
-     * @since 2.1.3
-     */
-    protected $package;
+	/**
+	 * @var string
+	 * @since 2.1.3
+	 */
+	protected $package;
 
-    /**
-     * ModuleMultiCurrenciesCest constructor.
-     * @since 2.1.3
-     */
-    public function __construct()
-    {
-        $this->faker            = Faker\Factory::create();
-        $this->categoryName     = $this->faker->bothify('CategoryName ?###?');
-        $this->productName      = $this->faker->bothify('Testing Product ??####?');
-        $this->productNumber    = $this->faker->numberBetween(999, 9999);
-        $this->productPrice     = $this->faker->numberBetween(999, 9999);
-        $this->minimumQuantity  = $this->faker->numberBetween(1, 10);
-        $this->maximumQuantity  = $this->faker->numberBetween(11, 100);
+	/**
+	 * ModuleMultiCurrenciesCest constructor.
+	 * @since 2.1.3
+	 */
+	public function __construct()
+	{
+		$this->faker            = Faker\Factory::create();
+		$this->categoryName     = $this->faker->bothify('CategoryName ?###?');
+		$this->productName      = $this->faker->bothify('Testing Product ??####?');
+		$this->productNumber    = $this->faker->numberBetween(999, 9999);
+		$this->productPrice     = $this->faker->numberBetween(999, 9999);
+		$this->minimumQuantity  = $this->faker->numberBetween(1, 10);
+		$this->maximumQuantity  = $this->faker->numberBetween(11, 100);
 
-        //install module
-        $this->extensionURL   = 'extension url';
-        $this->moduleName     = 'Redshop Multi Currencies';
-        $this->moduleURL      = 'paid-extensions/tests/releases/modules/site/';
-        $this->package        = 'mod_redshop_currencies.zip';
-    }
+		//install module
+		$this->extensionURL   = 'extension url';
+		$this->moduleName     = 'Redshop Multi Currencies';
+		$this->moduleURL      = 'paid-extensions/tests/releases/modules/site/';
+		$this->package        = 'mod_redshop_currencies.zip';
+	}
 
-    /**
-     * @param AcceptanceTester $I
-     * @throws Exception
-     * @since 2.1.3
-     */
-    public function _before(AcceptanceTester $I)
-    {
-        $I->doAdministratorLogin();
-    }
+	/**
+	 * @param AcceptanceTester $I
+	 * @throws Exception
+	 * @since 2.1.3
+	 */
+	public function _before(AcceptanceTester $I)
+	{
+		$I->doAdministratorLogin();
+	}
 
-    /**
-     * @param AdminManagerJoomla3Steps $I
-     * @param $scenario
-     * @throws Exception
-     * @since 2.1.3
-     */
-    public function installModule(AdminManagerJoomla3Steps $I, $scenario)
-    {
-        $I->wantTo("install module Multi Currencies");
-        $I->installExtensionPackageFromURL($this->extensionURL, $this->moduleURL, $this->package);
-        $I->waitForText(AdminJ3Page::$messageInstallModuleSuccess, 120, AdminJ3Page::$idInstallSuccess);
-        $I->publishModule($this->moduleName);
-        $I = new ModuleManagerJoomla($scenario);
-        $I->configurationCurrent($this->moduleName);
-        $I->setModulePosition($this->moduleName);
-        $I->displayModuleOnAllPages($this->moduleName);
-    }
+	/**
+	 * @param AdminManagerJoomla3Steps $I
+	 * @param $scenario
+	 * @throws Exception
+	 * @since 2.1.3
+	 */
+	public function installModule(AdminManagerJoomla3Steps $I, $scenario)
+	{
+		$I->wantTo("install module Multi Currencies");
+		$I->installExtensionPackageFromURL($this->extensionURL, $this->moduleURL, $this->package);
+		$I->waitForText(AdminJ3Page::$messageInstallModuleSuccess, 120, AdminJ3Page::$idInstallSuccess);
+		$I->publishModule($this->moduleName);
+		$I = new ModuleManagerJoomla($scenario);
+		$I->configurationCurrent($this->moduleName);
+		$I->setModulePosition($this->moduleName);
+		$I->displayModuleOnAllPages($this->moduleName);
+	}
 
-    /**
-     * @param AcceptanceTester $I
-     * @param $scenario
-     * @throws Exception
-     * @since 2.1.3
-     */
-    public function checkModuleMultiCurrencies(AcceptanceTester $I, $scenario)
-    {
-        $I->wantTo('Check Module Currencies');
-        $I = new CategoryManagerJoomla3Steps($scenario);
-        $I->addCategorySave($this->categoryName);
-        $I = new ProductManagerJoomla3Steps($scenario);
-        $I->createProductSaveClose($this->productName, $this->categoryName, $this->productNumber, $this->productPrice);
-        $I = new MultiCurrenciesSteps($scenario);
-        $I->checkModuleCurrencies($this->categoryName, $this->productName);
-    }
+	/**
+	 * @param AcceptanceTester $I
+	 * @param $scenario
+	 * @throws Exception
+	 * @since 2.1.3
+	 */
+	public function checkModuleMultiCurrencies(AcceptanceTester $I, $scenario)
+	{
+		$I->wantTo('Check Module Currencies');
+		$I = new CategoryManagerJoomla3Steps($scenario);
+		$I->addCategorySave($this->categoryName);
+		$I = new ProductManagerJoomla3Steps($scenario);
+		$I->createProductSaveClose($this->productName, $this->categoryName, $this->productNumber, $this->productPrice);
+		$I = new MultiCurrenciesSteps($scenario);
+		$I->checkModuleCurrencies($this->categoryName, $this->productName);
+	}
 
-    /**
-     * @param AcceptanceTester $I
-     * @param $scenario
-     * @throws Exception
-     * @since 2.1.3
-     */
-    public function clearAllData(AcceptanceTester $I, $scenario)
-    {
-        $I->wantTo('Delete data');
-        $I = new ProductManagerJoomla3Steps($scenario);
-        $I->deleteProduct($this->productName);
+	/**
+	 * @param AcceptanceTester $I
+	 * @param $scenario
+	 * @throws Exception
+	 * @since 2.1.3
+	 */
+	public function clearAllData(AcceptanceTester $I, $scenario)
+	{
+		$I->wantTo('Delete data');
+		$I = new ProductManagerJoomla3Steps($scenario);
+		$I->deleteProduct($this->productName);
 
-        $I = new CategoryManagerJoomla3Steps($scenario);
-        $I->deleteCategory($this->categoryName);
-    }
+		$I = new CategoryManagerJoomla3Steps($scenario);
+		$I->deleteCategory($this->categoryName);
+	}
 }
