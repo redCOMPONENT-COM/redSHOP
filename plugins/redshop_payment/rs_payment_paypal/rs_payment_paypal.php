2f6818e7a7d4a59ff0aea7aa3707f2ebb24ec242
hungnguyen <hung@redweb.dk>
2019-09-11 11:29:35 +0700

    update code

--- plugins/redshop_payment/rs_payment_paypal/rs_payment_paypal.php
+++ plugins/redshop_payment/rs_payment_paypal/rs_payment_paypal.php
@@ -60,15 +60,15 @@ class plgRedshop_paymentrs_payment_paypal extends JPlugin
 		{
 			$values->order_status_code = $verify_status;
 			$values->order_payment_status_code = 'Paid';
-			$values->log  = JText::_('PLG_RS_PAYMENT_PAYPAL_ORDER_PLACED');
-			$values->msg  = JText::_('PLG_RS_PAYMENT_PAYPAL_ORDER_PLACED');
+			$values->log = JText::_('PLG_RS_PAYMENT_PAYPAL_ORDER_PLACED');
+			$values->msg = JText::_('PLG_RS_PAYMENT_PAYPAL_ORDER_PLACED');
 		}
 		else
 		{
 			$values->order_status_code = $invalid_status;
 			$values->order_payment_status_code = 'Unpaid';
-			$values->log = JText::_('PLG_RS_PAYMENT_PAYPAL_NOT_PLACED');
-			$values->msg = JText::_('PLG_RS_PAYMENT_PAYPAL_NOT_PLACED');
+			$values->log  = JText::_('PLG_RS_PAYMENT_PAYPAL_NOT_PLACED');
+			$values->msg  = JText::_('PLG_RS_PAYMENT_PAYPAL_NOT_PLACED');
 			$values->type = 'error';
 		}
 
