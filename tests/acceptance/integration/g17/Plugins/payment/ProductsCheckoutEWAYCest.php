12b9debef75c837c4dae5ad977511b6dbc96caeb
unknown <trang.nguyen@redweb.dk>
2019-09-12 17:54:49 +0700

    opend install payment

--- tests/acceptance/integration/g17/Plugins/payment/ProductsCheckoutEWAYCest.php
+++ tests/acceptance/integration/g17/Plugins/payment/ProductsCheckoutEWAYCest.php
@@ -192,13 +192,13 @@ class ProductsCheckoutEWAYCest
 	 */
 	public function installPlugin(AdminManagerJoomla3Steps $I, $scenario)
 	{
-//		$I->wantTo("install plugin payment E-Way");
-//		$I->installExtensionPackageFromURL($this->extensionURL, $this->pluginURL, $this->package);
-//		$I->waitForText(AdminJ3Page:: $messageInstallPluginSuccess, 120, AdminJ3Page::$idInstallSuccess);
-//		$I->wantTo('Enable Plugin E-Way Payments in Administrator');
+		$I->wantTo("install plugin payment E-Way");
+		$I->installExtensionPackageFromURL($this->extensionURL, $this->pluginURL, $this->package);
+		$I->waitForText(AdminJ3Page:: $messageInstallPluginSuccess, 120, AdminJ3Page::$idInstallSuccess);
+		$I->wantTo('Enable Plugin E-Way Payments in Administrator');
 		$I->enablePlugin($this->pluginName);
-//		$I = new PluginPaymentManagerJoomla($scenario);
-//		$I->configEWayPlugin($this->pluginName, $this->checkoutAccountInformation['customerID']);
+		$I = new PluginPaymentManagerJoomla($scenario);
+		$I->configEWayPlugin($this->pluginName, $this->checkoutAccountInformation['customerID']);
 	}
 
 	/**
@@ -209,7 +209,7 @@ class ProductsCheckoutEWAYCest
 	 */
 	public function testEWAYPaymentPlugin(ConfigurationSteps $I, $scenario)
 	{
-//		$I->cartSetting($this->cartSetting);
+		$I->cartSetting($this->cartSetting);
 
 		$I->wantTo('Create Category in Administrator');
 		$I = new CategoryManagerJoomla3Steps($scenario);
