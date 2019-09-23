af6be49b30c7509488e257911cad04ff2aeebc70
VanTrieuThanh <thanh@redweb.dk>
2019-09-23 16:12:46 +0700

    add execute to scroll field

--- tests/acceptance/administrator/_steps/Joomla3Steps/OrderUpdateDiscountAndSpecialDiscountSteps.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/OrderUpdateDiscountAndSpecialDiscountSteps.php
@@ -76,6 +76,7 @@ class OrderUpdateDiscountAndSpecialDiscountSteps extends OrderManagerJoomla3Step
 		$I->click(OrderManagerPage::$orderID);
 		$I->scrollTo(OrderManagerPage::$discountUpdate);
 		$I->waitForElementVisible(OrderManagerPage::$discountUpdate, 30);
+        $I->executeJS('window.scrollTo(65,80);');
 		$I->fillField(OrderManagerPage::$discountUpdate, $discountUpdate);
 		$I->waitForElementVisible($userOrderPage->returnButtonUpdateDiscount($id), 30);
 		$I->executeJS('window.scrollTo(65,80);');
@@ -83,6 +84,7 @@ class OrderUpdateDiscountAndSpecialDiscountSteps extends OrderManagerJoomla3Step
 		$I->click($userOrderPage->returnButtonUpdateDiscount($id));
 		$I->scrollTo(OrderManagerPage::$specialUpdate);
 		$I->waitForElementVisible(OrderManagerPage::$specialUpdate, 30);
+        $I->executeJS('window.scrollTo(65,80);');
 		$I->fillField(OrderManagerPage::$specialUpdate, $specialUpdate);
 		$I->waitForElementVisible($userOrderPage->returnButtonSpecialDiscount($id), 30);
 		$I->executeJS('window.scrollTo(65,80);');
