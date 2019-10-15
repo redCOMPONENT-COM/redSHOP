09f6c0a1608c92f7fe7a50d67f344c85fe2e77b9
unknown <trang.nguyen@redweb.dk>
2019-10-15 12:07:27 +0700

    fix fail delete category

--- tests/acceptance/administrator/_steps/Joomla3Steps/CategoryManagerJoomla3Steps.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/CategoryManagerJoomla3Steps.php
@@ -330,6 +330,7 @@ class CategoryManagerJoomla3Steps extends AdminManagerJoomla3Steps
 	 * @param   String $categoryName Name of the Category
 	 *
 	 * @return void
+	 * @throws \Exception
 	 */
 	public function deleteCategory($categoryName)
 	{
@@ -339,9 +340,22 @@ class CategoryManagerJoomla3Steps extends AdminManagerJoomla3Steps
 		$I->checkAllResults();
 		$I->click(\CategoryManagerJ3Page::$deleteButton);
 		$I->acceptPopup();
-//		$I->assertSystemMessageContains(\CategoryManagerJ3Page::$messageDeleteSuccess);
-		$I->fillField(\CategoryManagerJ3Page::$categoryFilter, $categoryName);
-		$I->pressKey(\CategoryManagerJ3Page::$categoryFilter, \Facebook\WebDriver\WebDriverKeys::ENTER);
+
+		try
+		{
+			$I->waitForElementVisible(CategoryManagerJ3Page::$categoryFilter, 30);
+			$I->fillField(\CategoryManagerJ3Page::$categoryFilter, $categoryName);
+			$I->pressKey(\CategoryManagerJ3Page::$categoryFilter, \Facebook\WebDriver\WebDriverKeys::ENTER);
+			$I->dontSee($categoryName);
+
+		}catch (\Exception $e)
+		{
+		   $I->waitForElementVisible(CategoryManagerJ3Page::$checkAllXpath, 30);
+		   $I->click(CategoryManagerJ3Page::$checkAllXpath);
+			$I->click(\CategoryManagerJ3Page::$deleteButton);
+			$I->acceptPopup();
+		}
+
 		$I->dontSee($categoryName);
 	}
 
