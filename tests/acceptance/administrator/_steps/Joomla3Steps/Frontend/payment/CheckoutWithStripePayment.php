4c075aa14bddbee26f049cb569362725b5854241
hoanghairedweb <hai.le@redweb.dk>
2019-10-17 17:23:35 +0700

    fix fail on sv

--- tests/acceptance/administrator/_steps/Joomla3Steps/Frontend/payment/CheckoutWithStripePayment.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/Frontend/payment/CheckoutWithStripePayment.php
@@ -5,6 +5,7 @@
  * @copyright   Copyright (C) 2008 - 2019 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
+
 namespace Frontend\payment;
 use FrontEndProductManagerJoomla3Page;
 
