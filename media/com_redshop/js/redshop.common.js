d7fb110256a9d9645f8223d037512c6973d4e6c4
phuc.pham <phuc.pham@redweb.dk>
2019-10-28 15:17:29 +0700

    SWD-174: Edit Information form - display duplicate

--- media/com_redshop/js/redshop.common.js
+++ media/com_redshop/js/redshop.common.js
@@ -994,7 +994,7 @@ function onestepCheckoutProcess(objectname, classname, anonymous)
 
 			SqueezeBox.initialize({});
 
-			$$('a.modal:only-child').each(function(el) {
+			$$('a.modal:last-child').each(function(el) {
 				el.addEvent('click', function(e) {
 					e.preventDefault();
 					SqueezeBox.fromElement(el);
