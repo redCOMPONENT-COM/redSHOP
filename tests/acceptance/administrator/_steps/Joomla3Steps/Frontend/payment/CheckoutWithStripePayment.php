b2d0e652c33f85174eb8f56ff483f80816c3b4ec
hoanghairedweb <hai.le@redweb.dk>
2019-10-29 10:51:02 +0700

    fix fail random

--- tests/acceptance/administrator/_steps/Joomla3Steps/Frontend/payment/CheckoutWithStripePayment.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/Frontend/payment/CheckoutWithStripePayment.php
@@ -39,6 +39,17 @@ class CheckoutWithStripePayment extends CheckoutWithEWAYPayment
 		$I->waitForElementVisible(FrontEndProductManagerJoomla3Page::$labelPayment, 30);
 		$I->waitForElementVisible(StripePaymentPage::$paymentStripe, 30);
 		$I->click(StripePaymentPage::$paymentStripe);
+
+		try
+		{
+			$I->seeCheckboxIsChecked(StripePaymentPage::$paymentStripe);
+		}catch (\Exception $e)
+		{
+			$I->waitForElementVisible(StripePaymentPage::$paymentStripe, 30);
+			$I->wait(0.5);
+			$I->click(PStripePaymentPage::$paymentStripe);
+		}
+
 		$I->wait(0.5);
 		$I->waitForElementVisible(FrontEndProductManagerJoomla3Page::$acceptTerms, 30);
 		$I->scrollTo(FrontEndProductManagerJoomla3Page::$acceptTerms);
@@ -51,8 +62,7 @@ class CheckoutWithStripePayment extends CheckoutWithEWAYPayment
 		try
 		{
 			$I->canSeeInPopup(StripePaymentPage::$messagePopupStripe);
-		}
-		catch (\Exception $e)
+		}catch (\Exception $e)
 		{
 			$I->wait(2);
 			$I->canSeeInPopup(StripePaymentPage::$messagePopupStripe);
