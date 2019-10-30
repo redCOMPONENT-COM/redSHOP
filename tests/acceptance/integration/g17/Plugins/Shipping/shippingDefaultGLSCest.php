7db9eb0b60d8ca13e33ce75c403dd9ac3137255f
hoanghairedweb <hai.le@redweb.dk>
2019-10-30 11:22:58 +0700

    edit style

--- tests/acceptance/integration/g17/Plugins/Shipping/shippingDefaultGLSCest.php
+++ tests/acceptance/integration/g17/Plugins/Shipping/shippingDefaultGLSCest.php
@@ -143,9 +143,9 @@ class shippingDefaultGLSCest
 	 */
 	public function installPlugin(AdminManagerJoomla3Steps $I, $scenario)
 	{
-//		$I->wantTo("install plugin shipping default GLS");
-//		$I->installExtensionPackageFromURL($this->extensionURL, $this->pluginURL, $this->package);
-//		$I->waitForText(AdminJ3Page::$messageInstallPluginSuccess, 120, AdminJ3Page::$idInstallSuccess);
+		$I->wantTo("install plugin shipping default GLS");
+		$I->installExtensionPackageFromURL($this->extensionURL, $this->pluginURL, $this->package);
+		$I->waitForText(AdminJ3Page::$messageInstallPluginSuccess, 120, AdminJ3Page::$idInstallSuccess);
 		$I->wantTo('Enable plugin shipping default GLS');
 		$I->enablePlugin($this->pluginName);
 
