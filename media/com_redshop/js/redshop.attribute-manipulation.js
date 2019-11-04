d877b5d01d00a6d986c6209ce58d5f4df5eeb5b2
hungnguyen <hung@redweb.dk>
2019-11-04 13:28:40 +0700

    update cdoe

--- media/com_redshop/js/redshop.attribute-manipulation.js
+++ media/com_redshop/js/redshop.attribute-manipulation.js
@@ -23,8 +23,8 @@
 			+ '<a href="#" class="showhidearrow">' + Joomla.JText._('COM_REDSHOP_SUB_ATTRIBUTE') + ': <span class="propertyName"></span><img class="arrowimg" src="' + redSHOP.RSConfig._('SITE_URL') + 'media/com_redshop/images/arrow.png" alt=""/></a>'
 
 			+ '<div class="attr_tbody form-inline divInspectFromHideShow">'
-                + '<input type="hidden" value="0" name="' + propPref + '[count_subprop]" class="count_subprop" />'
-                + '<input type="hidden" value="' + sh + '" name="' + propPref + '[key_prop]" class="key_prop" />'
+				+ '<input type="hidden" value="0" name="' + propPref + '[count_subprop]" class="count_subprop" />'
+				+ '<input type="hidden" value="' + sh + '" name="' + propPref + '[key_prop]" class="key_prop" />'
 			+ '<div class="row">'
 			+ '<div class="col-sm-4">'
 			+ '<div class="form-group"><label>' + Joomla.JText._('COM_REDSHOP_SUB_ATTRIBUTE') + '</label><input type="text" class="form-control propertyInput" name="' + propPref + '[name]" value=""><input type="hidden"  name="attribute[' + gh + '][property][' + sh + '][mainImage]" value="" id="propmainImage' + gh + sh + '"><input type="hidden" name="attribute[' + gh + '][property][' + sh + '][property_image]" id="propertyImageName' + gh + sh + '" /></div>'
@@ -166,7 +166,7 @@
 				if ($this.closest('.attribute_parameter_tr').find('.sub_attribute_table').size() == 1) {
 					$this.closest('.attribute_parameter_tr').find('.showsubproperty').css({'display': 'none'});
 				}
-                $this.closest('.sub_property_table').remove();
+				$this.closest('.sub_property_table').remove();
 			}
 		}).on('click', '.deletePropertyMainImage', function () {
 			if (confirm(Joomla.JText._('COM_REDSHOP_DO_WANT_TO_DELETE'))) {
@@ -208,7 +208,7 @@
 				}
 
 				$this.closest('.attr_tbody').prev('.showhidearrow').remove();
-                $this.closest('.attr_tbody').prev('.add_property').remove();
+				$this.closest('.attr_tbody').prev('.add_property').remove();
 				$this.closest('.attr_tbody').remove();
 
 			}
