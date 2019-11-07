b36019f2c62b883711721c517b4d7db26469b8c4
hoanghairedweb <hai.le@redweb.dk>
2019-11-07 16:10:41 +0700

    run gr11

--- tests/acceptance/integration/g17/Plugins/Shipping/shippingGiaoHangNhanhCest.php
+++ tests/acceptance/integration/g17/Plugins/Shipping/shippingGiaoHangNhanhCest.php
@@ -178,7 +178,7 @@ class shippingGiaoHangNhanhCest
 	 */
 	public function installPlugin(AdminManagerJoomla3Steps $I)
 	{
-		$I->wantTo("install plugin shipping Giao Hang Nhanh");
+		$I->wantTo("Install plugin shipping Giao Hang Nhanh");
 		$I->installExtensionPackageFromURL($this->extensionURL, $this->pluginURL, $this->package);
 		$I->waitForText(AdminJ3Page::$messageInstallPluginSuccess, 120, AdminJ3Page::$idInstallSuccess);
 		$I->enablePlugin($this->pluginName);
