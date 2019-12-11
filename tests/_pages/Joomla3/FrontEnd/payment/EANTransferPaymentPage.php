ca943460866a0e08b475dc726feb16a0a99bcbe8
VanTrieuThanh <thanh@redweb.dk>
2019-12-11 11:04:09 +0700

    Create codeception for EAN Payment

---
+++ tests/_pages/Joomla3/FrontEnd/payment/EANTransferPaymentPage.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package     redSHOP
+ * @subpackage  Page
+ * @copyright   Copyright (C) 2008 - 2019 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+/**
+ * Class EANTransferPaymentPage
+ * @since 2.1.4
+ */
+class EANTransferPaymentPage extends FrontEndProductManagerJoomla3Page
+{
+	/**
+	 * @var string
+	 * @since 2.1.4
+	 */
+	public static $eanPayment = "#rs_payment_eantransfer2";
+}
--- tests/acceptance/administrator/_steps/Joomla3Steps/Administrator/plugins/PluginPaymentManagerJoomla.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/Administrator/plugins/PluginPaymentManagerJoomla.php
@@ -312,4 +312,21 @@ class PluginPaymentManagerJoomla extends AdminManagerJoomla3Steps
 		$I->clickToolbarButton(PluginManagerJoomla3Page::$buttonSaveClose);
 		$I->waitForText(PluginManagerJoomla3Page::$pluginSaveSuccessMessage, 30, PluginManagerJoomla3Page::$idInstallSuccess);
 	}
+
+	public function configEANTranferPayment($pluginName)
+	{
+		$I = $this;
+		$I->amOnPage(PluginManagerJoomla3Page::$URL);
+		$I->checkForPhpNoticesOrWarnings();
+		$I->searchForItem($pluginName);
+		$pluginManagerPage = new PluginManagerJoomla3Page;
+		$I->waitForElement($pluginManagerPage->searchResultPluginName($pluginName), 30);
+		$I->checkExistenceOf($pluginName);
+		$I->waitForText($pluginName, 30, PluginManagerJoomla3Page::$searchResultRow);
+		$I->waitForElementVisible($pluginManagerPage->searchResultPluginName($pluginName), 30);
+		$I->click($pluginName);
+
+		$I->clickToolbarButton(PluginManagerJoomla3Page::$buttonSaveClose);
+		$I->waitForText(PluginManagerJoomla3Page::$pluginSaveSuccessMessage, 30, PluginManagerJoomla3Page::$idInstallSuccess);
+	}
 }
