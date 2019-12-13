9802c412aa74ef8adcd9919e542cdb41da92fb93
VanTrieuThanh <thanh@redweb.dk>
2019-12-13 14:12:18 +0700

    write codeception for Klarna

---
+++ tests/acceptance/administrator/_steps/Joomla3Steps/Frontend/payment/CheckoutWithKlarnaPayment.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * @package     redSHOP
+ * @subpackage  Steps
+ * @copyright   Copyright (C) 2008 - 2019 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Frontend\payment;
+use CheckoutMissingData;
+use FrontEndProductManagerJoomla3Page;
+
+class CheckoutwithKlarnaPayment extends CheckoutMissingData
+{
+
+	public function checkoutProductWithKlarnaPayment($productName, $categoryName, $customerInformation, $pno)
+	{
+		$I = $this;
+		$I->amOnPage(FrontEndProductManagerJoomla3Page::$URL);
+		$I->waitForElement(FrontEndProductManagerJoomla3Page::$categoryDiv, 60);
+		$I->click($categoryName);
+		$productFrontEndManagerPage = new FrontEndProductManagerJoomla3Page;
+		$I->addToCart($categoryName, $productName);
+		$I->amOnPage(FrontEndProductManagerJoomla3Page::$cartPageUrL);
+		$I->waitForElementVisible(['link' => $productName], 30);
+		$I->click(FrontEndProductManagerJoomla3Page::$checkoutButton);
+		$I->waitForElementVisible(FrontEndProductManagerJoomla3Page::$newCustomerSpan, 30);
+		$I->click(FrontEndProductManagerJoomla3Page::$newCustomerSpan);
+		$I->wait(1);
+		$I->fillInformationPrivate($customerInformation);
+		$I->wait(0.5);
+		$I->waitForElementVisible(FrontEndProductManagerJoomla3Page::$proceedButtonId, 30);
+		$I->click(FrontEndProductManagerJoomla3Page::$proceedButtonId);
+		$I->waitForElementVisible(FrontEndProductManagerJoomla3Page::$labelPayment, 30);
+		$I->scrollTo(FrontEndProductManagerJoomla3Page::$labelPayment);
+		$I->waitForElementVisible(FrontEndProductManagerJoomla3Page::$klarnaPayment, 30);
+		$I->click(FrontEndProductManagerJoomla3Page::$klarnaPayment);
+		$I->waitForElementVisible(FrontEndProductManagerJoomla3Page::$fieldPNO, 30);
+		$I->fillField(FrontEndProductManagerJoomla3Page::$fieldPNO, $pno);
+		$I->waitForElementVisible(FrontEndProductManagerJoomla3Page::$checkoutButton, 30);
+		$I->click(FrontEndProductManagerJoomla3Page::$checkoutButton);
+		$I->wait(0.5);
+		$I->waitForElementVisible(FrontEndProductManagerJoomla3Page::$acceptTerms, 30);
+		$I->scrollTo(FrontEndProductManagerJoomla3Page::$acceptTerms);
+		$I->executeJS($productFrontEndManagerPage->radioCheckID(FrontEndProductManagerJoomla3Page::$termAndConditionsId));
+		$I->wait(0.5);
+		$I->waitForElementVisible(FrontEndProductManagerJoomla3Page::$checkoutFinalStep, 30);
+		$I->click(FrontEndProductManagerJoomla3Page::$checkoutFinalStep);
+		try
+		{
+			$I->waitForElementNotVisible(FrontEndProductManagerJoomla3Page::$checkoutFinalStep, 10);
+		}
+		catch (\Exception $e)
+		{
+			try
+			{
+				$I->waitForText(FrontEndProductManagerJoomla3Page::$messageAcceptTerms, 5, FrontEndProductManagerJoomla3Page::$locatorMessageAcceptTerms);
+			}
+			catch (\Exception $e)
+			{
+				$I->waitForElementVisible(FrontEndProductManagerJoomla3Page::$termAndConditions, 5);
+				$I->click(FrontEndProductManagerJoomla3Page::$termAndConditions);
+			}
+			$I->waitForElementVisible(FrontEndProductManagerJoomla3Page::$checkoutFinalStep, 30);
+			$I->wait(0.5);
+			$I->click(FrontEndProductManagerJoomla3Page::$checkoutFinalStep);
+		}
+		$I->waitForText(FrontEndProductManagerJoomla3Page::$orderReceipt, 30, FrontEndProductManagerJoomla3Page::$h1);
+		$I->see(FrontEndProductManagerJoomla3Page::$orderReceipt);
+	}
+}
---
+++ tests/acceptance/integration/g17/Plugins/payment/ProductCheckoutWithKlarnaPaymentCest.php
@@ -0,0 +1,215 @@
+<?php
+/**
+ * @package     redSHOP
+ * @subpackage  Cest
+ * @copyright   Copyright (C) 2008 - 2019 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+use AcceptanceTester\AdminManagerJoomla3Steps;
+use Administrator\plugins\PluginPaymentManagerJoomla;
+use AcceptanceTester\CategoryManagerJoomla3Steps;
+use Configuration\ConfigurationSteps;
+use AcceptanceTester\OrderManagerJoomla3Steps;
+use AcceptanceTester\ProductManagerJoomla3Steps;
+use Frontend\payment\CheckoutWithKlarnaPayment;
+
+/**
+ * Class ProductCheckoutWithKlarnaPaymentCest
+ * @since 2.1.4
+ */
+class ProductCheckoutWithKlarnaPaymentCest
+{
+	/**
+	 * @var \Faker\Generator
+	 * @since 2.1.4
+	 */
+	public $faker;
+
+	/**
+	 * @var string
+	 * @since 2.1.4
+	 */
+	public $categoryName;
+
+	/**
+	 * @var string
+	 * @since 2.1.4
+	 */
+	public $productName;
+
+	/**
+	 * @var string
+	 * @since 2.1.4
+	 */
+	public $productNumber;
+
+	/**
+	 * @var string
+	 * @since 2.1.4
+	 */
+	public $productPrice;
+
+	/**
+	 * @var string
+	 * @since 2.1.4
+	 */
+	public $minimumQuantity;
+
+	/**
+	 * @var string
+	 * @since 2.1.4
+	 */
+	public $maximumQuantity;
+
+	/**
+	 * @var array
+	 * @since 2.1.4
+	 */
+	protected $customerInformation;
+
+	/**
+	 * @var string
+	 * @since 2.1.4
+	 */
+	public $extensionURL;
+
+	/**
+	 * @var string
+	 * @since 2.1.4
+	 */
+	public $pluginName;
+
+	/**
+	 * @var string
+	 * @since 2.1.4
+	 */
+	public $pluginURL;
+
+	/**
+	 * @var string
+	 * @since 2.1.4
+	 */
+	public $package;
+
+	/**
+	 * @var string
+	 * @since 2.1.4
+	 */
+	public $merchantID;
+
+	/**
+	 * @var string
+	 * @since 2.1.4
+	 */
+	public $sharedSecretKey;
+
+	/**
+	 * @var string
+	 * @since 2.1.4
+	 */
+	public $pno;
+
+	/**
+	 * ProductCheckoutWithKlarnaPaymentCest constructor.
+	 * @since 2.1.4
+	 */
+	public function __construct()
+	{
+		$this->faker            = Faker\Factory::create();
+		$this->categoryName     = $this->faker->bothify('CategoryName ?###?');
+		$this->productName      = $this->faker->bothify('Product Name ?###?');
+		$this->productNumber    = $this->faker->numberBetween(999, 9999);
+		$this->productPrice     = 100;
+		$this->minimumQuantity  = 1;
+		$this->maximumQuantity  = $this->faker->numberBetween(11, 100);
+
+		//configuration enable and page checkout
+		$this->customerInformation = array(
+			"email"      => "test@test" . rand() . ".com",
+			"firstName"  => $this->faker->bothify('firstNameCustomer ?####?'),
+			"lastName"   => $this->faker->bothify('lastNameCustomer ?####?'),
+			"address"    => "Some Place in the World",
+			"postalCode" => "5000",
+			"city"       => "HCM",
+			"country"    => "Denmark",
+			"state"      => "Karnataka",
+			"phone"      => "8787878787"
+		);
+
+		$this->extensionURL   = 'extension url';
+		$this->pluginName     = 'redSHOP Payment - Integrate Klarna Invoice, Part payment and our Order handling system';
+		$this->pluginURL      = 'paid-extensions/tests/releases/plugins/';
+		$this->package        = 'plg_redshop_payment_klarna.zip';
+
+		$this->merchantID       = '4046';
+		$this->sharedSecretKey  = 'Zf8wWib8Lo4INpZ';
+
+		$this->pno              = '01-01-1970';
+	}
+
+	/**
+	 * @param AcceptanceTester $I
+	 * @throws Exception
+	 * @since 2.1.4
+	 */
+	public function _before(AcceptanceTester $I)
+	{
+		$I->doAdministratorLogin();
+	}
+
+	/**
+	 * @param AdminManagerJoomla3Steps $I
+	 * @param $scenario
+	 * @throws Exception
+	 * @since 2.1.4
+	 */
+	public function installPlugin(AdminManagerJoomla3Steps $I, $scenario)
+	{
+		$I->wantTo("install Klarna Payment Plugin");
+		$I->installExtensionPackageFromURL($this->extensionURL, $this->pluginURL, $this->package);
+		$I->waitForText(AdminJ3Page::$messageInstallPluginSuccess, 120, AdminJ3Page::$idInstallSuccess);
+		$I = new PluginPaymentManagerJoomla($scenario);
+		$I->enablePlugin($this->pluginName);
+		$I->wantTo('Enable Plugin Klarna Payments in Administrator');
+		$I->configKlarnaPayment($this->pluginName, $this->merchantID, $this->sharedSecretKey);
+	}
+
+	/**
+	 * @param AcceptanceTester $I
+	 * @param $scenario
+	 * @throws Exception
+	 * @since 2.1.4
+	 */
+	public function testCheckoutWithKlarnaPayment(AcceptanceTester $I, $scenario)
+	{
+		$I->wantTo('setup up one page checkout at admin');
+		$I->wantTo('Create Category in Administrator');
+		$I = new CategoryManagerJoomla3Steps($scenario);
+		$I->addCategorySave($this->categoryName);
+		$I->wantTo('I Want to add product inside the category');
+		$I = new ProductManagerJoomla3Steps($scenario);
+		$I->createProductSaveClose($this->productName, $this->categoryName, $this->productNumber, $this->productPrice);
+
+		$I->wantTo('Checkout with Authorize payment');
+		$I = new CheckoutwithKlarnaPayment($scenario);
+		$I->checkoutProductWithKlarnaPayment($this->productName, $this->categoryName, $this->customerInformation, $this->pno);
+
+		$I->wantTo('Check Order');
+		$I = new ConfigurationSteps($scenario);
+		$I->checkPriceTotal($this->productPrice, $this->customerInformation["firstName"], $this->customerInformation["firstName"], $this->customerInformation["lastName"], $this->productName, $this->categoryName, $this->pluginName);
+
+		$I->comment('Delete data has created');
+		$I->wantTo('Deletion of Order in Administrator');
+		$I = new OrderManagerJoomla3Steps($scenario);
+		$I->deleteOrder( $this->customerInformation['firstName']);
+		$I->wantTo('Delete product');
+		$I = new ProductManagerJoomla3Steps($scenario);
+		$I->deleteProduct($this->productName);
+		$I->wantTo('Delete Category');
+		$I = new CategoryManagerJoomla3Steps($scenario);
+		$I->deleteCategory($this->categoryName);
+		$I->wantTo('Disable Plugin');
+		$I->disablePlugin($this->pluginName);
+	}
+}
