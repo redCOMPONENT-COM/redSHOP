66ad8b529984fb550e9adfcb57a3e0ccfe51f13a
VanTrieuThanh <thanh@redweb.dk>
2020-01-09 16:09:53 +0700

    write codeception for REDSHOP-5666

--- tests/acceptance/administrator/_steps/Joomla3Steps/ShippingWithOneItemSteps.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/ShippingWithOneItemSteps.php
@@ -6,7 +6,6 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-use AcceptanceTester\AdminManagerJoomla3Steps;
 use Frontend\payment\CheckoutWithEWAYPayment;
 
 /**
@@ -15,55 +14,82 @@ use Frontend\payment\CheckoutWithEWAYPayment;
  */
 class ShippingWithOneItemSteps extends CheckoutWithEWAYPayment
 {
-    public function ChekoutWithOneItemShippingSteps($categoryName, $product, $customerInformation, $shipping, $shippingName)
-    {
-        $I = $this;
-        $currencyUnit = $I->getCurrencyValue();
-        $I->amOnPage(FrontEndProductManagerJoomla3Page::$URL);
-        $productFrontEndManagerPage = new FrontEndProductManagerJoomla3Page;
-        $I->waitForElement(FrontEndProductManagerJoomla3Page::$categoryDiv, 60);
-        $I->addToCart($categoryName, $product['name']);
+	/**
+	 * @param $categoryName
+	 * @param $product
+	 * @param $customerInformation
+	 * @param $shipping
+	 * @param $shippingMethod
+	 * @throws Exception
+	 * @since 2.1.5
+	 */
+	public function ChekoutWithOneItemShippingSteps($categoryName, $product, $customerInformation, $shipping, $shippingMethod)
+	{
+		$I = $this;
+		$currencyUnit = $I->getCurrencyValue();
+		$I->amOnPage(FrontEndProductManagerJoomla3Page::$URL);
+		$productFrontEndManagerPage = new FrontEndProductManagerJoomla3Page;
+		$I->waitForElement(FrontEndProductManagerJoomla3Page::$categoryDiv, 60);
+		$I->addToCart($categoryName, $product['name']);
 
-        $I->amOnPage(FrontEndProductManagerJoomla3Page::$cartPageUrL);
-        $I->waitForElementVisible(['link' => $product['name']], 30);
-        $I->waitForElementVisible(FrontEndProductManagerJoomla3Page::$quantity1, 30);
-        $quantity = $I->grabTextFrom(FrontEndProductManagerJoomla3Page::$quantity1);
+		$I->amOnPage(FrontEndProductManagerJoomla3Page::$cartPageUrL);
+//        $I->waitForElementVisible(['link' => $product['name']], 30);
+//        $I->waitForElementVisible(FrontEndProductManagerJoomla3Page::$quantity1, 30);
+//        $quantity = $I->grabTextFrom(FrontEndProductManagerJoomla3Page::$quantity1);
 
-        $I->waitForElementVisible(FrontEndProductManagerJoomla3Page::$checkoutButton, 30);
-        $I->click(FrontEndProductManagerJoomla3Page::$checkoutButton);
-        $I->fillInformationPrivate($customerInformation);
+		$I->waitForElementVisible(FrontEndProductManagerJoomla3Page::$checkoutButton, 30);
+		$I->click(FrontEndProductManagerJoomla3Page::$checkoutButton);
+		$I->fillInformationPrivate($customerInformation);
 
-        $I->waitForElement(FrontEndProductManagerJoomla3Page::$bankTransfer, 30);
-        $I->executeJS($productFrontEndManagerPage->radioCheckID(FrontEndProductManagerJoomla3Page::$bankTransferId));
-        $I->waitForElementVisible(FrontEndProductManagerJoomla3Page::$termAndConditions, 30);
-        $I->scrollTo(FrontEndProductManagerJoomla3Page::$termAndConditions);
-        $I->click(FrontEndProductManagerJoomla3Page::$termAndConditions);
+		$I->waitForElement(FrontEndProductManagerJoomla3Page::$bankTransfer, 30);
+		$I->executeJS($productFrontEndManagerPage->radioCheckID(FrontEndProductManagerJoomla3Page::$bankTransferId));
+		$I->waitForElementVisible(FrontEndProductManagerJoomla3Page::$termAndConditions, 30);
+		$I->scrollTo(FrontEndProductManagerJoomla3Page::$termAndConditions);
+		$I->click(FrontEndProductManagerJoomla3Page::$termAndConditions);
 
-        try
-        {
-            $I->canSeeCheckboxIsChecked(FrontEndProductManagerJoomla3Page::$termAndConditions);
-        }
+		try
+		{
+			$I->canSeeCheckboxIsChecked(FrontEndProductManagerJoomla3Page::$termAndConditions);
+		}
 
-        catch (\Exception $e)
-        {
-            $I->click(FrontEndProductManagerJoomla3Page::$termAndConditions);
-            $I->canSeeCheckboxIsChecked(FrontEndProductManagerJoomla3Page::$termAndConditions);
-        }
+		catch (\Exception $e)
+		{
+			$I->click(FrontEndProductManagerJoomla3Page::$termAndConditions);
+			$I->canSeeCheckboxIsChecked(FrontEndProductManagerJoomla3Page::$termAndConditions);
+		}
 
-        $I->waitForElementVisible(FrontEndProductManagerJoomla3Page::$checkoutFinalStep, 30);
-        $I->pauseExecution();
-        $I->click(FrontEndProductManagerJoomla3Page::$checkoutFinalStep);
-        $I->waitForElementVisible(FrontEndProductManagerJoomla3Page::$orderReceiptTitle, 30);
-        $I->waitForText(FrontEndProductManagerJoomla3Page::$orderReceipt, 30, FrontEndProductManagerJoomla3Page::$h1);
+		$I->waitForElementVisible(FrontEndProductManagerJoomla3Page::$checkoutFinalStep, 30);
+		$I->click(FrontEndProductManagerJoomla3Page::$checkoutFinalStep);
+		$I->waitForElementVisible(FrontEndProductManagerJoomla3Page::$orderReceiptTitle, 30);
+		$I->waitForText(FrontEndProductManagerJoomla3Page::$orderReceipt, 30, FrontEndProductManagerJoomla3Page::$h1);
 
-        $total = $product['price']*$quantity + $shipping['shippingRate'];
-        $priceTotal = $currencyUnit['currencySymbol'].' '.($total).$currencyUnit['decimalSeparator'].$currencyUnit['numberZero'];
-        $priceRate = $currencyUnit['currencySymbol'].' '.($shipping['shippingRate']).$currencyUnit['decimalSeparator'].$currencyUnit['numberZero'];
+		$total = $product['price'] + $shipping['shippingRate'];
+		$priceTotal = $currencyUnit['currencySymbol'].' '.($total).$currencyUnit['decimalSeparator'].$currencyUnit['numberZero'];
+		$priceRate = $currencyUnit['currencySymbol'].' '.($shipping['shippingRate']).$currencyUnit['decimalSeparator'].$currencyUnit['numberZero'];
 
-        $I->waitForText($shippingName, 30);
-        $I->pauseExecution();
-        $I->see($shippingName);
-        $I->see($priceRate);
-        $I->see($priceTotal);
-    }
+		$I->waitForText($shippingMethod, 30);
+		$I->see($shippingMethod);
+		$I->see($priceRate);
+		$I->see($priceTotal);
+	}
+
+	/**
+	 * @param $customerInformation
+	 * @param $shippingMethod
+	 * @param $shipping
+	 * @throws Exception
+	 * @since 2.1.5
+	 */
+	public function checkOrderShipping($customerInformation, $shippingMethod, $shipping)
+	{
+		$I = $this;
+		$I->amOnPage(OrderManagerPage::$URL);
+		$I->filterListBySearchOrder($customerInformation['firstName']);
+		$I->click(OrderManagerPage::$orderID);
+		$I->scrollTo(OrderManagerPage::$shippingInfor);
+		$I->see($shippingMethod);
+		$I->see($shipping['shippingRate']);
+		$I->waitForElement(OrderManagerPage::$close, 30);
+		$I->waitForText(OrderManagerPage::$buttonClose, 10, OrderManagerPage::$close);
+	}
 }
--- tests/acceptance/administrator/g03/Shippings/Shipping/ShippingWithOneItemCest.php
+++ tests/acceptance/administrator/g03/Shippings/Shipping/ShippingWithOneItemCest.php
@@ -6,13 +6,13 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-use AcceptanceTester\AdminManagerJoomla3Steps;
 use AcceptanceTester\CategoryManagerJoomla3Steps;
 use AcceptanceTester\OrderManagerJoomla3Steps;
 use AcceptanceTester\ProductManagerJoomla3Steps;
 use AcceptanceTester\ShippingSteps;
 use AcceptanceTester\UserManagerJoomla3Steps;
 use Configuration\ConfigurationSteps;
+
 /**
  * Class ShippingWithOneItemCest
  * @since 2.1.5
@@ -20,136 +20,184 @@ use Configuration\ConfigurationSteps;
 class ShippingWithOneItemCest
 {
 
-    /**
-     * ShippingWithOneItemCest constructor.
-     * @since 2.1.5
-     */
-    public function __construct()
-    {
-        $this->faker        = Faker\Factory::create();
-        $this->categoryName = $this->faker->bothify("Category Demo ?##?");
+	/**
+	 * @var \Faker\Generator
+	 * @since 2.1.5
+	 */
+	public $faker;
 
-        $this->product = array(
-            "name"          => $this->faker->bothify("Product Demo ?##?"),
-            "number"        => $this->faker->numberBetween(999,9999),
-            "price"         => $this->faker->numberBetween(1,990)
-        );
+	/**
+	 * @var string
+	 * @since 2.1.5
+	 */
+	public $categoryName;
 
-        $this->customerInformation = array(
-            "userName"     => $this->faker->userName,
-            "email"        => $this->faker->email,
-            "firstName"    => $this->faker->firstName,
-            "lastName"     => $this->faker->lastName,
-            "address"      => $this->faker->address,
-            "postalCode"   => "700000",
-            "city"         => "HCM",
-            "country"      => "Denmark",
-            "state"        => "Karnataka",
-            "phone"        => "0909909999",
-            "shopperGroup" => 'Default Private',
-            'group'        => 'Registered'
-        );
+	/**
+	 * @var array
+	 * @since 2.1.5
+	 */
+	public $product;
 
-        $this->cartSetting = array(
-            "addCart"          => 'product',
-            "allowPreOrder"    => 'yes',
-            "cartTimeOut"      => $this->faker->numberBetween(100, 10000),
-            "enabledAjax"      => 'no',
-            "defaultCart"      => null,
-            "buttonCartLead"   => 'Back to current view',
-            "onePage"          => 'yes',
-            "showShippingCart" => 'no',
-            "attributeImage"   => 'no',
-            "quantityChange"   => 'no',
-            "quantityInCart"   => 0,
-            "minimumOrder"     => 0,
-            "enableQuotation"  => 'no'
-        );
+	/**
+	 * @var array
+	 * @since 2.1.5
+	 */
+	public $customerInformation;
 
-        $this->shippingMethod = 'redSHOP - Standard Shipping';
+	/**
+	 * @var array
+	 * @since 2.1.5
+	 */
+	public $cartSetting;
 
-        // Shipping info
-        $this->shipping = array(
-            'shippingName' => 'Shipping Default for Viet Nam',
-            'shippingRate' => 10,
-            'country' => 'Canada'
-        );
+	/**
+	 * @var string
+	 * $since 2.1.5
+	 */
+	public $shippingMethod;
 
-        $this->paymentMethod = 'RedSHOP - Bank Transfer Payment';
-        $this->function      = 'saveclose';
-    }
+	/**
+	 * @var array
+	 * @since 2.1.5
+	 */
+	public $shipping;
 
-    /**
-     * @param AcceptanceTester $I
-     * @throws Exception
-     * @since 2.1.5
-     */
-    public function _before(AcceptanceTester $I)
-    {
-        $I->doAdministratorLogin();
-    }
+	/**
+	 * @var string
+	 * @since 2.1.5
+	 */
+	public $paymentMethod;
 
-    /**
-     * @param AcceptanceTester $I
-     * @param $scenario
-     * @throws Exception
-     * @since 2.1.5
-     */
-    public function checkoutWithShipping(AcceptanceTester $I, $scenario)
-    {
-//        $I->comment('Setting one page checkout');
-//        $I = new ConfigurationSteps($scenario);
-//        $I->cartSetting($this->cartSetting);
+	/**
+	 * @var string
+	 * @since 2.1.5
+	 */
+	public $function;
 
-        $I->comment('Create Category');
-        $I = new CategoryManagerJoomla3Steps($scenario);
-        $I->addCategorySave($this->categoryName);
+	/**
+	 * ShippingWithOneItemCest constructor.
+	 * @since 2.1.5
+	 */
+	public function __construct()
+	{
+		$this->faker        = Faker\Factory::create();
+		$this->categoryName = $this->faker->bothify("Category Demo ?##?");
 
-        $I->comment("Create Product");
-        $I = new ProductManagerJoomla3Steps($scenario);
-        $I->createProductSaveClose($this->product['name'], $this->categoryName, $this->product['number'], $this->product['price']);
+		$this->product = array(
+			"name"          => $this->faker->bothify("Product Demo ?##?"),
+			"number"        => $this->faker->numberBetween(999,9999),
+			"price"         => $this->faker->numberBetween(1,990)
+		);
 
-        $I = new ShippingSteps($scenario);
-        $I->comment('Check create new Shipping rate');
-        $I->createShippingRateStandard($this->shippingMethod, $this->shipping, $this->function);
+		$this->customerInformation = array(
+			"email"        => $this->faker->email,
+			"firstName"    => $this->faker->firstName,
+			"lastName"     => $this->faker->lastName,
+			"address"      => $this->faker->address,
+			"postalCode"   => "5000",
+			"city"         => "HCM",
+			"country"      => "Denmark",
+			"phone"        => "0909909999",
+		);
 
-        $I->comment("Checkout Shipping when has 1 item shipping in checkout final page");
-        $I = new ShippingWithOneItemSteps($scenario);
-        $I->ChekoutWithOneItemShippingSteps($this->categoryName, $this->product, $this->customerInformation, $this->shipping, $this->shippingMethod);
+		$this->cartSetting = array(
+			"addCart"          => 'product',
+			"allowPreOrder"    => 'yes',
+			"cartTimeOut"      => $this->faker->numberBetween(100, 10000),
+			"enabledAjax"      => 'no',
+			"defaultCart"      => null,
+			"buttonCartLead"   => 'Back to current view',
+			"onePage"          => 'yes',
+			"showShippingCart" => 'no',
+			"attributeImage"   => 'no',
+			"quantityChange"   => 'yes',
+			"quantityInCart"   => 3,
+			"minimumOrder"     => 0,
+			"enableQuotation"  => 'no'
+		);
 
-        $I->comment('Check Order when has exist 1 Shipping item');
-        $I = new ConfigurationSteps($scenario);
-        $I->checkShippingTotal($this->shipping["shippingName"], $this->product["name"], $this->customerInformation, $this->categoryName, $this->paymentMethod, $this->shippingMethod);
+		$this->shippingMethod = 'redSHOP - Standard Shipping';
 
-//        $I->comment('Checkout with the has exist 1 Shipping item on Shipping method');
-//        $I = new
-    }
+		// Shipping info
+		$this->shipping = array(
+			'shippingName' => $this->faker->bothify('TestingShippingRate ?##?'),
+			'zipCodeStart' => '5000',
+			'zipCodeEnd'   => '5000',
+			'shippingRate' => 10
+		);
 
-    /**
-     * @param ProductManagerJoomla3Steps $I
-     * @param $scenario
-     * @throws Exception
-     * @since 2.1.5
-     */
-    public function clearAll(ProductManagerJoomla3Steps $I, $scenario)
-    {
-        $I->wantToTest('Delete Product');
-        $I->deleteProduct($this->product['name']);
+		$this->paymentMethod = 'RedSHOP - Bank Transfer Payment';
+		$this->function      = 'saveclose';
+	}
 
-        $I->wantToTest('Delete Category');
-        $I = new CategoryManagerJoomla3Steps($scenario);
-        $I->deleteCategory($this->categoryName);
+	/**
+	 * @param AcceptanceTester $I
+	 * @throws Exception
+	 * @since 2.1.5
+	 */
+	public function _before(AcceptanceTester $I)
+	{
+		$I->doAdministratorLogin();
+	}
 
-        $I->wantToTest('Delete User');
-        $I = new UserManagerJoomla3Steps($scenario);
-        $I->deleteUser($this->customerInformation['firstName']);
+	/**
+	 * @param AcceptanceTester $I
+	 * @param $scenario
+	 * @throws Exception
+	 * @since 2.1.5
+	 */
+	public function checkoutWithShipping(AcceptanceTester $I, $scenario)
+	{
+		$I->comment('Setting one page checkout');
+		$I = new ConfigurationSteps($scenario);
+		$I->cartSetting($this->cartSetting);
 
-        $I->wantToTest('Delete Shipping Rate');
-        $I = new ShippingSteps($scenario);
-        $I->deleteShippingRate($this->shipping["shippingName"], $this->shipping['shippingRate']);
+		$I->comment('Create Category');
+		$I = new CategoryManagerJoomla3Steps($scenario);
+		$I->addCategorySave($this->categoryName);
 
-        $I->wantToTest('Delete Order');
-        $I = new OrderManagerJoomla3Steps($scenario);
-        $I->deleteOrder($this->customerInformation['firstName']);
-    }
+		$I->comment("Create Product");
+		$I = new ProductManagerJoomla3Steps($scenario);
+		$I->createProductSaveClose($this->product['name'], $this->categoryName, $this->product['number'], $this->product['price']);
+
+		$I = new ShippingSteps($scenario);
+		$I->comment('Check create new Shipping rate');
+		$I->createShippingRateStandard($this->shippingMethod, $this->shipping, $this->function);
+
+		$I->comment("Checkout Shipping when has 1 item shipping in checkout final page");
+		$I = new ShippingWithOneItemSteps($scenario);
+		$I->ChekoutWithOneItemShippingSteps($this->categoryName, $this->product, $this->customerInformation, $this->shipping, $this->shippingMethod);
+
+		$I->comment('Check Order detail in backend');
+		$I = new ShippingWithOneItemSteps($scenario);
+		$I->checkOrderShipping($this->customerInformation, $this->shippingMethod, $this->shipping);
+	}
+
+	/**
+	 * @param ProductManagerJoomla3Steps $I
+	 * @param $scenario
+	 * @throws Exception
+	 * @since 2.1.5
+	 */
+	public function clearAll(ProductManagerJoomla3Steps $I, $scenario)
+	{
+		$I->wantToTest('Delete Product');
+		$I->deleteProduct($this->product['name']);
+
+		$I->wantToTest('Delete Category');
+		$I = new CategoryManagerJoomla3Steps($scenario);
+		$I->deleteCategory($this->categoryName);
+
+		$I->wantToTest('Delete User');
+		$I = new UserManagerJoomla3Steps($scenario);
+		$I->deleteUser($this->customerInformation['firstName']);
+
+		$I->wantToTest('Delete Shipping Rate');
+		$I = new ShippingSteps($scenario);
+		$I->deleteShippingRate($this->shippingMethod, $this->shipping['shippingName']);
+
+		$I->wantToTest('Delete Order');
+		$I = new OrderManagerJoomla3Steps($scenario);
+		$I->deleteOrder($this->customerInformation['firstName']);
+	}
 }
