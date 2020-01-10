d9ab8874a39c9b15d0a676b211db7618c4699684
VanTrieuThanh <thanh@redweb.dk>
2020-01-10 16:40:50 +0700

    edit redSHOP

--- tests/acceptance/administrator/g03/Shippings/Shipping/ShippingWithOneItemCest.php
+++ tests/acceptance/administrator/g03/Shippings/Shipping/ShippingWithOneItemCest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package     RedShop
+ * @package     redSHOP
  * @subpackage  Cest
  * @copyright   Copyright (C) 2008 - 2020 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
