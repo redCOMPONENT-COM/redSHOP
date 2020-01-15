c7b5ea6abe76383873c0c06c7395a355752f6d15
hoang.tran <hoang.tran@redweb.dk>
2020-01-15 14:50:58 +0700

    fix scrutinizer

--- component/site/views/category/tmpl/detail.php
+++ component/site/views/category/tmpl/detail.php
@@ -1095,7 +1095,7 @@ $template_desc = str_replace("{redproductfinderfilter:rp_myfilter}", "", $templa
 $template_desc = RedshopHelperTemplate::parseRedshopPlugin($template_desc);
 
 $template_desc = RedshopHelperText::replaceTexts($template_desc);
-echo /** @scrutinizer ignore-deprecated */ eval("?>" . $template_desc . "<?php ");
+echo /** @scrutinizer ignore-type */ eval("?>" . $template_desc . "<?php ");
 
 if ($slide)
 {
