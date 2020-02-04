b2a20d76082edf039874f97b256e79e168c96c81
unknown <trang.nguyen@redweb.dk>
2020-02-04 17:41:18 +0700

    Fix fail random

--- tests/acceptance/uninstall/UninstallExtensionCest.php
+++ tests/acceptance/uninstall/UninstallExtensionCest.php
@@ -32,11 +32,11 @@ class UninstallExtensionCest
 		$I->click(\ExtensionManagerJoomla3Page::$buttonClear);
 
 		$I->waitForJS("return window.jQuery && jQuery.active == 0;", 30);
-		$I->waitForElement(\ExtensionManagerJoomla3Page::$searchTools, 30);
+		$I->waitForElementVisible(\ExtensionManagerJoomla3Page::$searchTools, 30);
 		$I->wait(0.5);
 		$I->click(\ExtensionManagerJoomla3Page::$searchTools);
 		$I->waitForJS("return window.jQuery && jQuery.active == 0;", 30);
-		$I->waitForElementVisible('#filter_type', 30);
+		$I->waitForElement('#filter_type', 30);
 		$I->selectOptionInChosen('#filter_type', 'Component');
 		$I->fillField('#filter_search', 'redSHOP');
 		$I->click(\ExtensionManagerJoomla3Page::$searchButtonJ3);
