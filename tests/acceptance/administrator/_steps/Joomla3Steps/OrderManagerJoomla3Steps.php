5d9651d570bbfdbe64f0f795f4ed68a1bfde0df9
unknown <trang.nguyen@redweb.dk>
2020-02-05 09:04:26 +0700

    Fix fail random

--- tests/acceptance/administrator/_steps/Joomla3Steps/OrderManagerJoomla3Steps.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/OrderManagerJoomla3Steps.php
@@ -383,6 +383,7 @@ class OrderManagerJoomla3Steps extends AdminManagerJoomla3Steps
 		$I->waitForElementVisible($userOrderPage->returnXpathAttributeValue($product['size']), 30);
 		$I->click($userOrderPage->returnXpathAttributeValue($product['size']));
 		$I->wait(0.5);
+		$I->waitForJS("return window.jQuery && jQuery.active == 0;", 30);
 
 		switch ($function)
 		{
