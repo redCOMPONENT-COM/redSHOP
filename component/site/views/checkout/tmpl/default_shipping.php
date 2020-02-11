9fc65d3100298c597e4282ce583559c9c8245d20
hungnguyen <hung@redweb.dk>
2020-02-11 10:18:20 +0700

    REDSHOP-5738 [Refactor] - Tags System - Refactoring Code - Template section: Shipping box

--- component/site/views/checkout/tmpl/default_shipping.php
+++ component/site/views/checkout/tmpl/default_shipping.php
@@ -18,60 +18,67 @@ $user   = JFactory::getUser();
 $jinput = JFactory::getApplication()->input;
 $post   = $jinput->getArray($_POST);
 
-$shippingbox_template = RedshopHelperTemplate::getTemplate("shippingbox");
+$shippingboxTemplate = RedshopHelperTemplate::getTemplate("shippingbox");
 
-if (count($shippingbox_template) > 0 && $shippingbox_template[0]->template_desc)
+if (count($shippingboxTemplate) > 0 && $shippingboxTemplate[0]->template_desc)
 {
-	$box_template_desc = $shippingbox_template[0]->template_desc;
+	$boxTemplateDesc = $shippingboxTemplate[0]->template_desc;
 }
 else
 {
-	$box_template_desc = "<fieldset class=\"adminform\"> <legend><strong>{shipping_box_heading}</strong></legend>\r\n<div>{shipping_box_list}</div>\r\n</fieldset>";
+	$boxTemplateDesc = "<fieldset class=\"adminform\"> <legend><strong>{shipping_box_heading}</strong></legend>\r\n<div>{shipping_box_list}</div>\r\n</fieldset>";
 }
 
-$shipping_template = RedshopHelperTemplate::getTemplate("redshop_shipping");
+$shippingTemplate = RedshopHelperTemplate::getTemplate("redshop_shipping");
 
-if (count($shipping_template) > 0 && $shipping_template[0]->template_desc)
+if (count($shippingTemplate) > 0 && $shippingTemplate[0]->template_desc)
 {
-	$template_desc = $shipping_template[0]->template_desc;
+	$templateDesc = $shippingTemplate[0]->template_desc;
 }
 else
 {
-	$template_desc = "<fieldset class=\"adminform\"><legend><strong>{shipping_heading}</strong></legend>\r\n<div>{shipping_method_loop_start}\r\n<h3>{shipping_method_title}</h3>\r\n<div>{shipping_rate_loop_start}\r\n<div>{shipping_rate_name} {shipping_rate}</div>\r\n{shipping_rate_loop_end}</div>\r\n{shipping_method_loop_end}</div>\r\n</fieldset>";
+	$templateDesc = "<fieldset class=\"adminform\"><legend><strong>{shipping_heading}</strong></legend>\r\n<div>{shipping_method_loop_start}\r\n<h3>{shipping_method_title}</h3>\r\n<div>{shipping_rate_loop_start}\r\n<div>{shipping_rate_name} {shipping_rate}</div>\r\n{shipping_rate_loop_end}</div>\r\n{shipping_method_loop_end}</div>\r\n</fieldset>";
 }
 
 if ($this->users_info_id > 0)
 {
-	$shippinghelper          = shipping::getInstance();
+	$shippingHelper          = shipping::getInstance();
 	$shippingBoxes           = RedshopHelperShipping::getShippingBox();
-	$selshipping_box_post_id = 0;
+	$selShippingBoxPostId = 0;
 
 	if (count($shippingBoxes) > 0)
 	{
-		$selshipping_box_post_id = $shippingBoxes[0]->shipping_box_id;
+		$selShippingBoxPostId = $shippingBoxes[0]->shipping_box_id;
 	}
 
 	if (isset($post['shipping_box_id']))
 	{
-		$shipping_box_post_id = $post['shipping_box_id'];
+		$shippingBoxPostId = $post['shipping_box_id'];
 	}
 	else
 	{
-		$shipping_box_post_id = $selshipping_box_post_id;
+		$shippingBoxPostId = $selShippingBoxPostId;
 	}
 
-	$box_template_desc = $carthelper->replaceShippingBoxTemplate($box_template_desc, $shipping_box_post_id);
-	echo eval("?>" . $box_template_desc . "<?php ");
+	$boxTemplateDesc = RedshopTagsReplacer::_(
+		'shippingbox',
+		$boxTemplateDesc,
+		array(
+			'shippingBoxPostId' => $shippingBoxPostId
+		)
+	);
 
-	$returnarr              = $carthelper->replaceShippingTemplate($template_desc, $this->shipping_rate_id, $shipping_box_post_id, $user->id, $this->users_info_id, $this->ordertotal, $this->order_subtotal);
-	$template_desc          = $returnarr['template_desc'];
-	$this->shipping_rate_id = $returnarr['shipping_rate_id'];
+	echo eval("?>" . $boxTemplateDesc . "<?php ");
 
-	echo eval("?>" . $template_desc . "<?php ");
+	$returnArr              = $carthelper->replaceShippingTemplate($templateDesc, $this->shipping_rate_id, $shippingBoxPostId, $user->id, $this->users_info_id, $this->ordertotal, $this->order_subtotal);
+	$templateDesc          = $returnArr['template_desc'];
+	$this->shipping_rate_id = $returnArr['shipping_rate_id'];
+
+	echo eval("?>" . $templateDesc . "<?php ");
 }
 else
 {
 	?>
 	<div class="shipnotice"><?php echo JText::_('COM_REDSHOP_FILL_SHIPPING_ADDRESS'); ?></div>
-<?php
+	<?php
 }
