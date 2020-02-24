ea5eb4cedb7f0564748856dcfad1ad4ca8ebdcf9
unknown <trang.nguyen@redweb.dk>
2020-02-24 11:57:42 +0700

    Change xpath

--- tests/_pages/Joomla3/Administrator/AdminJ3Page.php
+++ tests/_pages/Joomla3/Administrator/AdminJ3Page.php
@@ -553,7 +553,7 @@ abstract class AdminJ3Page
 	 * @var array
 	 * @since 2.1.2
 	 */
-	public static $shippingRate = "//span[@id='spnShippingrate']";
+	public static $shippingRate = "#spnShippingrate";
 
 	/**
 	 * @var string
