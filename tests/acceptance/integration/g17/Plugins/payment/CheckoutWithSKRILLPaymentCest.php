b50e30978bbbc04e2846921f100c4c20b0707975
Nguyen Trang <nguyentrang091296@gmail.com>
2020-04-20 17:52:29 +0700

    move function stockroom

--- tests/acceptance/integration/g17/Plugins/payment/CheckoutWithSKRILLPaymentCest.php
+++ tests/acceptance/integration/g17/Plugins/payment/CheckoutWithSKRILLPaymentCest.php
@@ -2,7 +2,7 @@
 /**
  * @package     redSHOP
  * @subpackage  Cest
- * @copyright   Copyright (C) 2008 - 2019 redCOMPONENT.com. All rights reserved.
+ * @copyright   Copyright (C) 2008 - 2020 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
