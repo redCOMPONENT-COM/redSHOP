8e3330759dc3f7df636a733037c4a5c30ae411cb
Alberrt <alberrt@VNDTRK570-2.local>
2020-05-04 16:32:13 +0700

    Save code

---
+++ modules/site/mod_redshop_login/mod_redshop_login.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @package     RedSHOP.Frontend
+ * @subpackage  mod_redshop_login
+ *
+ * @copyright   Copyright (C) 2008 - 2020 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('_JEXEC') or die;
+
+// Include the login functions only once
+require_once 'helper.php';
+
+$params->def('greeting', 1);
+$moduleName = 'mod_redshop_login';
+
+$type             = \ModRedshopLoginHelper::getType();
+$return           = \ModRedshopLoginHelper::getReturnUrl($params, $type);
+$twofactormethods = JAuthenticationHelper::getTwoFactorMethods();
+$user             = JFactory::getUser();
+$layout           = $params->get('layout', 'default');
+
+// Logged users must load the logout sub layout
+if (!$user->guest)
+{
+    $layout .= '_logout';
+}
+
+require JModuleHelper::getLayoutPath('mod_redshop_login', $layout);
+/*
+$twigParams = [
+
+];
+print RedshopLayoutHelper::render(
+    $layout,
+    $twigParams,
+    '',
+    array(
+        'component'  => 'com_redshop',
+        'layoutType' => 'Twig',
+        'layoutOf'   => 'module',
+        'prefix'     => $moduleName
+    )
+);*/
