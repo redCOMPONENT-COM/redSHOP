8a7eaa3cf6c9abadb89d01a22e953083b7d5b803
VanTrieuThanh <thanh@redweb.dk>
2020-05-08 13:58:27 +0700

    change cest add question for product

--- tests/acceptance/administrator/g17/CustomerInput/QuestionManagerCest.php
+++ tests/acceptance/administrator/g17/CustomerInput/QuestionManagerCest.php
@@ -9,6 +9,7 @@
 use AcceptanceTester\CategoryManagerJoomla3Steps;
 use AcceptanceTester\ProductManagerJoomla3Steps;
 use AcceptanceTester\QuestionManagerJoomla3Steps;
+use AcceptanceTester\UserManagerJoomla3Steps;
 
 /**
  * Class QuestionManagerCest
@@ -82,6 +83,12 @@ class QuestionManagerCest
 	 */
 	protected $questionInformation ;
 
+    /**
+     * @var array
+     * @since 3.0.2
+     */
+	protected $user;
+
 	/**
 	 * QuestionManagerCest constructor.
 	 * @since 3.0.2
@@ -103,18 +110,27 @@ class QuestionManagerCest
 			"address"       => $this->faker->address,
 			"phone"         => $this->faker->phoneNumber,
 			"question"      => $this->faker->bothify('Why is this Happening ??####??'),
-			"edit"          => $this->faker->bothify('Edit Why is this Happening ??####??')
+			"edit"          => $this->faker->bothify('Edit Why is this Happening ??####??'),
+            "userName"     => $this->faker->bothify('User name ?####?'),
+			"email"        => $this->faker->email,
+			"question1"    => $this->faker->bothify('I not login account and comment question product ?####?'),
+			"question2"    => $this->faker->bothify('I login account and comment question product ??##??')
 		);
-	}
 
-	/**
-	 * @param AcceptanceTester $I
-	 * @throws Exception
-	 * @since 3.0.2
-	 */
-	public function _before(AcceptanceTester $I)
-	{
-		$I->doAdministratorLogin();
+        $this->user        = array (
+            "userName"     => $this->faker->bothify('User name ?####?'),
+            "password"     => $this->faker->bothify('Password ?##?'),
+            "email"        => $this->faker->email,
+            "group"        => 'Registered',
+            "shopperGroup" => 'Default Private',
+            "firstName"    => $this->faker->bothify('First name ?##?'),
+            "lastName"     => $this->faker->bothify('LastName ?####?'),
+            "address"      => $this->faker->address,
+            "zipcode"      => $this->faker->postcode,
+            "city"         => 'Ho Chi Minh',
+            "phone"        => $this->faker->phoneNumber,
+            "country"      => 'Viet Nam'
+        );
 	}
 
 	/**
@@ -125,6 +141,9 @@ class QuestionManagerCest
 	 */
 	public function createData(AcceptanceTester $I, $scenario)
 	{
+	    //Login Administrator Page
+        $I->doAdministratorLogin();
+
 		//Create data
 		$I->wantTo('Create category in administrator');
 		$I = new CategoryManagerJoomla3Steps($scenario);
@@ -133,6 +152,10 @@ class QuestionManagerCest
 		$I = new ProductManagerJoomla3Steps($scenario);
 		$I->wantTo('I want to add product inside the category');
 		$I->createProductSaveClose($this->productName, $this->categoryName, $this->randomProductNumber, $this->randomProductPrice);
+
+        $I = new UserManagerJoomla3Steps($scenario);
+        $I->wantTo('Create user in Administrator page');
+        $I->addUser($this->user['userName'], $this->user['password'], $this->user['email'], $this->user['group'], $this->user['shopperGroup'], $this->user['firstName'], $this->user['lastName'], 'save');
 
 		//Working with CRUD
 		$I->wantTo('Create question in backend');
@@ -147,14 +170,54 @@ class QuestionManagerCest
 
 		$I->wantTo('Delete question');
 		$I->deleteQuestion($this->questionInformation);
-
-		// Delete data
-		$I->wantTo('Delete product');
-		$I = new ProductManagerJoomla3Steps($scenario);
-		$I->deleteProduct($this->productName);
-
-		$I->wantTo('Delete Category');
-		$I = new CategoryManagerJoomla3Steps($scenario);
-		$I->deleteAllCategory();
 	}
+
+    /**
+     * @param AcceptanceTester $I
+     * @param $scenario
+     * @throws Exception
+     * @since 3.0.2
+     */
+    public function addQuestionInProductDetail(AcceptanceTester $I, $scenario)
+    {
+        $I->wantTo('Create Question in Frontend page with Missing login account');
+        $I = new QuestionManagerJoomla3Steps($scenario);
+        $I->addQuestionOnProductDetailOnFrontendMissingLogin($this->productName, $this->categoryName, $this->questionInformation);
+
+        $I->wantTo('Create Question in Frontend page with login account');
+        $I = new QuestionManagerJoomla3Steps($scenario);
+        $I->addQuestionOnProductDetailOnFrontendLogin($this->productName, $this->categoryName, $this->questionInformation, $this->user);
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     * @param $scenario
+     * @throws Exception
+     * @since 3.0.2
+     */
+    public function checkQuestionInBackend(AcceptanceTester $I, $scenario)
+    {
+        $I->doAdministratorLogin();
+        $I->wantTo('Check Question in Backend page');
+        $I = new QuestionManagerJoomla3Steps($scenario);
+        $I->checkQuestionInAdministrator($this->productName, $this->questionInformation);
+
+        //Delete data
+
+        $I->wantTo('Delete Question item');
+        $I = new QuestionManagerJoomla3Steps($scenario);
+        $I->deleteAll();
+
+        $I->wantTo('Delete product');
+        $I = new ProductManagerJoomla3Steps($scenario);
+        $I->deleteProduct($this->productName);
+
+        $I->wantTo('Delete Category');
+        $I = new CategoryManagerJoomla3Steps($scenario);
+        $I->deleteAllCategory();
+
+        $I->wantTo('Delete User');
+        $I = new UserManagerJoomla3Steps($scenario);
+        $I->deleteUser($this->user['firstName'],  false);
+    }
 }
