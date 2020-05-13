e8bfdc062a28034c29149812b6297929d0811ae5
hoang.tran <hoang.tran@redweb.dk>
2020-05-13 14:46:22 +0700

    fix not save description fields

--- component/admin/sql/updates/mysql/3.0.2.sql
+++ component/admin/sql/updates/mysql/3.0.2.sql
@@ -8,4 +8,9 @@ CALL redSHOP_Column_Update('#__redshop_tax_rate', 'checked_out_time', 'checked_o
 CALL redSHOP_Column_Update('#__redshop_tax_rate', 'created_date', 'created_date', "DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP");
 CALL redSHOP_Column_Update('#__redshop_tax_rate', 'shopper_group_id', 'shopper_group_id', "INT(11) NOT NULL");
 
+-- -----------------------------------------------------
+-- Table `#__redshop_fields`
+-- -----------------------------------------------------
+CALL redSHOP_Column_Update('#__redshop_fields', 'desc', 'description', 'VARCHAR(20) NOT NULL');
+
 SET FOREIGN_KEY_CHECKS = 1;
