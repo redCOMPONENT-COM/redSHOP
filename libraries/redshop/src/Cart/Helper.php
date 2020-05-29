c434056e3227d50cbc7487cc2ce8866c91132469
hungnguyen <hung@redweb.dk>
2020-05-29 14:58:41 +0700

    SWD-257 Don't get 'Leg Colour' label when user not select in Product detail

--- libraries/redshop/src/Cart/Helper.php
+++ libraries/redshop/src/Cart/Helper.php
@@ -442,6 +442,10 @@ class Helper
                             $subSubPropertyData = explode('::', $subPropertiesData[$propIndex]);
 
                             foreach ($subSubPropertyData as $supPropIndex => $subSubProperty) {
+                                if (!$subSubProperty)  {
+                                    continue;
+                                }
+
                                 $subproperty = \RedshopHelperProduct_Attribute::getAttributeSubProperties(
                                     $subSubProperty
                                 );
