fb51dcaae4acf5f7bee9e9945850155c4fc6b169
hungnguyen <hung@redweb.dk>
2020-07-02 18:08:45 +0700

    REDSHOP-6039 [Reractor][Component][Site] Move to use Twig for view Account shipto

--- component/site/views/account_shipto/view.html.php
+++ component/site/views/account_shipto/view.html.php
@@ -48,7 +48,7 @@ class RedshopViewAccount_Shipto extends RedshopView
     /**
      * Execute and display a template script.
      *
-     * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+     * @param string $tpl The name of the template file to parse; automatically searches through the template paths.
      *
      * @return  mixed         A string if successful, otherwise an Error object.
      * @throws  Exception
@@ -59,15 +59,20 @@ class RedshopViewAccount_Shipto extends RedshopView
     public function display($tpl = null)
     {
         /** @var JApplicationSite $app */
-        $app  = JFactory::getApplication();
-        $task = $app->input->getCmd('task');
-        $user = JFactory::getUser();
-
-        // Preform security checks
+        $app            = JFactory::getApplication();
+        $input          = $app->input;
+        $task           = $input->getCmd('task');
+        $user           = JFactory::getUser();
         $session        = JFactory::getSession();
         $auth           = $session->get('auth');
-        $lists          = array();
+        $lists          = [];
+        $layout         = 'default';
+        $itemId         = $input->getInt('Itemid', 0);
+        $isEdit         = $input->getInt('is_edit', 0);
+        $return         = $input->getString('return', "");
+        $infoId         = $input->getInt('infoid', 0);
         $billingAddress = new stdClass;
+        $twigParams     = [];
 
         if ($user->id) {
             $billingAddress = RedshopHelperOrder::getBillingAddress($user->id);
@@ -76,21 +81,17 @@ class RedshopViewAccount_Shipto extends RedshopView
             $billingAddress = $model->_loadData($auth['users_info_id']);
         } else {
             $app->redirect(
-                JRoute::_('index.php?option=com_redshop&view=login&Itemid=' . $app->input->getInt('Itemid', 0))
+                JRoute::_('index.php?option=com_redshop&view=login&Itemid=' . $itemId)
             );
             $app->close();
         }
 
         if ($task == 'addshipping') {
             JHtml::_('redshopjquery.framework');
-            /** @scrutinizer ignore-deprecated */
-            JHtml::script('com_redshop/jquery.validate.min.js', false, true);
-            /** @scrutinizer ignore-deprecated */
-            JHtml::script('com_redshop/redshop.common.min.js', false, true);
-            /** @scrutinizer ignore-deprecated */
-            JHtml::script('com_redshop/redshop.registration.min.js', false, true);
-            /** @scrutinizer ignore-deprecated */
-            JHtml::stylesheet('com_redshop/redshop.validation.min.css', array(), true);
+            JHtml::_('script', 'com_redshop/jquery.validate.min.js', false, true);
+            JHtml::_('script', 'com_redshop/redshop.common.min.js', false, true);
+            JHtml::_('script', 'com_redshop/redshop.registration.min.js', false, true);
+            JHtml::_('stylesheet', 'com_redshop/redshop.validation.min.css', array(), true);
 
             $shippingAddresses = $this->get('Data');
 
@@ -109,23 +110,83 @@ class RedshopViewAccount_Shipto extends RedshopView
                 $shippingAddresses->users_info_id
             );
 
-            $this->setLayout('form');
+            $post['firstname_ST']    = $shippingAddresses->firstname;
+            $post['lastname_ST']     = $shippingAddresses->lastname;
+            $post['address_ST']      = $shippingAddresses->address;
+            $post['city_ST']         = $shippingAddresses->city;
+            $post['zipcode_ST']      = $shippingAddresses->zipcode;
+            $post['phone_ST']        = $shippingAddresses->phone;
+            $post['country_code_ST'] = $shippingAddresses->country_code;
+            $post['state_code_ST']   = $shippingAddresses->state_code;
+            $layout                  = 'form';
+
+            $dispatcher = RedshopHelperUtility::getDispatcher();
+            JPluginHelper::importPlugin('redshop_shipping');
+            $dispatcher->trigger('onRenderCustomField', array($infoId));
+
+            $shippingTable = RedshopTagsReplacer::_(
+                'shippingtable',
+                '',
+                array(
+                    'data'      => $post,
+                    'isCompany' => $billingAddress->is_company,
+                    'lists'     => $lists
+                )
+            );
+
+            $twigParams = array_merge(
+                $twigParams,
+                [
+                    'post'          => $post,
+                    'shippingTable' => $shippingTable
+                ]
+            );
+        } elseif ($user->id) {
+            $shippingAddresses = RedshopHelperOrder::getShippingAddress($user->id);
         } else {
-            if ($user->id) {
-                $shippingAddresses = RedshopHelperOrder::getShippingAddress($user->id);
-            } else {
-                $shippingAddresses = RedshopHelperOrder::getShippingAddress(-$auth['users_info_id']);
-            }
+            $shippingAddresses = RedshopHelperOrder::getShippingAddress(-$auth['users_info_id']);
         }
 
-        $this->lists             = $lists;
-        $this->shippingAddresses = $shippingAddresses;
-        $this->billingAddresses  = $billingAddress;
-        $this->request_url       = JUri::getInstance()->toString();
+        JHtml::_('script', 'system/core.js', false, true);
+        JHtml::_('script', 'com_redshop/account/shipto.min.js', false, true);
+        $optionShipTo = [
+            'isEdit' => $isEdit,
+            'link'   => JRoute::_("index.php?option=com_redshop&view=" . $return . "&Itemid" . $itemId)
+        ];
 
-        JFilterOutput::cleanText($this->request_url);
-        $this->params = $app->getParams();
 
-        parent::display($tpl);
+        JFactory::getDocument()->addScriptOptions('optionShipTo', $optionShipTo);
+
+
+        $requestUrl = JUri::getInstance()->toString();
+        JFilterOutput::cleanText($requestUrl);
+
+        $twigParams = array_merge(
+            $twigParams,
+            [
+                'lists'             => $lists,
+                'shippingAddresses' => $shippingAddresses,
+                'billingAddresses'  => $billingAddress,
+                'requestUrl'        => $requestUrl,
+                'params'            => $app->getParams(),
+                'itemId'            => $itemId,
+                'isEdit'            => $isEdit,
+                'return'            => $return,
+                'infoId'            => $infoId
+            ]
+        );
+
+        print \RedshopLayoutHelper::render(
+            $layout,
+            $twigParams,
+            '',
+            array(
+                'component'  => 'com_redshop',
+                'layoutType' => 'Twig',
+                'layoutOf'   => 'component',
+                'prefix'     => 'com_redshop/account_shipto'
+            )
+        );
     }
+
 }
