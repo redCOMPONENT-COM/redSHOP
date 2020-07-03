a3c209ef817be7cfdcef9376ff8fe54fd8bccebf
NguyenBao <bao.tran@redweb.dk>
2020-07-03 11:46:52 +0700

    check isset

--- libraries/redshop/helper/product/price.php
+++ libraries/redshop/helper/product/price.php
@@ -140,11 +140,11 @@ class RedshopHelperProductPrice
                     . '%</span>';
             }
 
-            if ($productPrices['product_price_novat'] != 0) {
+            if (isset($productPrices['product_price_novat'])) {
                 $displayPriceNoVAT = '<span id="display_product_price_no_vat' . $productId . '">' . $priceNoVAT . '</span>';
             }
 
-            if ($productPrices['product_price_incl_vat'] != 0) {
+            if (isset($productPrices['product_price_incl_vat'])) {
                 $displayPriceWithVAT = '<span id="product_price_incl_vat' . $productId . '">' . $priceWithVAT . '</span>';
             }
         }
