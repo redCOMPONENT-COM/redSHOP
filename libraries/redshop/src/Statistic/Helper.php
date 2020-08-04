b849a8df7110e93bcc882debcc2cb2acda4360ae
hoang.tran <hoang.tran@redweb.dk>
2020-08-04 11:15:10 +0700

    fix total sale in backend

--- libraries/redshop/src/Statistic/Helper.php
+++ libraries/redshop/src/Statistic/Helper.php
@@ -29,10 +29,9 @@ class Helper
         // Todo: We didn't use JDatabase because $query->unionAll() is not working, please change to use $query->unionAll() when Joomla fixed it
         $query->select('SUM(' . $db->qn('order_total') . ') AS total')
             ->from($db->qn('#__redshop_orders'))
-            ->where(
-                $db->qn('order_status')
-                . ' IN (' . $db->q(implode(',', ['C', 'PR', 'S'])) . ')'
-            );
+	        ->where($db->qn('order_status') . ' = ' . $db->q('C'))
+	        ->orWhere($db->qn('order_status') . ' = ' . $db->q('PR'))
+	        ->orWhere($db->qn('order_status') . ' = ' . $db->q('S'));
 
         // Orders
         $q1 = $db->getQuery(true);
