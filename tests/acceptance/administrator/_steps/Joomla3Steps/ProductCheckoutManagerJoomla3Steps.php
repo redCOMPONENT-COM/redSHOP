71f302967ee0bd399c645158a15bf2118aa42c16
unknown <trang.nguyen@redweb.dk>
2020-08-05 17:15:09 +0700

    Fix style

--- tests/acceptance/administrator/_steps/Joomla3Steps/ProductCheckoutManagerJoomla3Steps.php
+++ tests/acceptance/administrator/_steps/Joomla3Steps/ProductCheckoutManagerJoomla3Steps.php
@@ -1184,7 +1184,7 @@ class ProductCheckoutManagerJoomla3Steps extends AdminManagerJoomla3Steps
 		$I->click(FrontEndProductManagerJoomla3Page::$checkoutButton);
 
 		try
-        {
+		{
 			$I->waitForElementVisible(FrontEndProductManagerJoomla3Page::$billingFinal, 30);
 			$I->waitForElement(FrontEndProductManagerJoomla3Page::$bankTransfer, 30);
 			$I->executeJS($productFrontEndManagerPage->radioCheckID(FrontEndProductManagerJoomla3Page::$bankTransferId));
