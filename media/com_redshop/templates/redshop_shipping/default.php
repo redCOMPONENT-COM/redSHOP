4cb3923444f46ef4df5a7f2d814c01f672c3cc3a
hungnguyen <hungnguyen.ckc@gmail.com>
2020-12-14 11:39:23 +0700

    update code

--- media/com_redshop/templates/redshop_shipping/default.php
+++ media/com_redshop/templates/redshop_shipping/default.php
@@ -1,5 +1,5 @@
-<div style="display: {show_when_one_rate}">
-    <fieldset class="shipping-method-container">
+<div class="shipping-method-container" style="display: {show_when_one_rate}">
+    <fieldset>
         <legend class="shipping-method-heading"><strong>{shipping_heading}</strong></legend>
         <div>{shipping_method_loop_start}
             <h3 class="shipping-method-title">{shipping_method_title}</h3>
