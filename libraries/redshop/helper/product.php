1c8fde873b6f9ab9e830cefa7ae6579bc2e2c0ce
HoangTran <hoang.tran@redweb.dk>
2020-12-29 17:23:34 +0700

    fix accessory price incorect in cart

--- libraries/redshop/helper/product.php
+++ libraries/redshop/helper/product.php
@@ -3244,7 +3244,7 @@ class RedshopHelperProduct
 
                 if (!empty($chktag)) {
                     $accessory_price     = $attArr[$i]['accessory_price'] + $acc_vat;
-                    $accessory_vat_price = $acc_vat;
+                    $accessory_vat_price += $acc_vat;
                 }
 
                 $attchildArr = $attArr[$i]['accessory_childs'];
