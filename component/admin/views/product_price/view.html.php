a15f04c45f4eb980a07b06c9b57ef0d054b8e5d8
HoangTran <hoang.tran@redweb.dk>
2020-12-29 14:49:06 +0700

    change query use Joomla Query

--- component/admin/views/product_price/view.html.php
+++ component/admin/views/product_price/view.html.php
@@ -24,13 +24,24 @@ class RedshopViewProduct_price extends RedshopViewAdmin
         jimport('joomla.html.pagination');
         JToolBarHelper::title(JText::_('COM_REDSHOP_PRODUCT_PRICE'), 'redshop_vatrates48');
 
-        $sql = "SELECT * FROM #__redshop_product WHERE product_id = '$productId'";
-        $db->setQuery($sql);
-        $product = $db->loadObject();
+	    $query = $db->getQuery(true)
+		    ->select('*')
+		    ->from($db->qn('#__redshop_product'))
+		    ->where($db->qn('product_id') . ' = ' . $db->q($productId));
+	    $product = $db->setQuery($query)->loadObject();
 
-        $sql = "SELECT g.*,p.product_price,p.price_id,p.price_quantity_end,p.price_quantity_start FROM #__redshop_shopper_group g LEFT JOIN #__redshop_product_price p ON g.id = p.shopper_group_id   AND product_id = '$productId'";
-        $db->setQuery($sql);
-        $prices = $db->loadObjectList();
+	    $query = $db->getQuery(true)
+		    ->select('g.*')
+		    ->select('p.*')
+		    ->select('p.price_id')
+		    ->select('p.price_quantity_end')
+		    ->select('p.price_quantity_start')
+		    ->from($db->qn('#__redshop_shopper_group', 'g'))
+		    ->leftjoin(
+			    $db->qn('#__redshop_product_price', 'p') . ' ON ' . $db->qn('g.id') . ' = ' . $db->qn('p.shopper_group_id') . ' AND product_id' . ' = ' . $productId
+		    );
+
+	    $prices =  $db->setQuery($query)->loadObjectList();
 
         $this->product = $product;
 
