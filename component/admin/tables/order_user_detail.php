e09de79e8b5d012a475272c159d0281b18ffb801
Shardul <shardul.patel01@gmail.com>
2023-10-18 18:48:15 +0530

    define variable type
    For PHP 8.2 compatibility

--- component/admin/tables/order_user_detail.php
+++ component/admin/tables/order_user_detail.php
@@ -70,7 +70,7 @@ class Tableorder_user_detail extends JTable
 
     public function bind($array, $ignore = '')
     {
-        if (array_key_exists('params', $array) && is_array($array['params'])) {
+        if (array_key_exists('params', (array) $array) && is_array($array['params'])) {
             $registry = new JRegistry;
             $registry->loadArray($array['params']);
             $array['params'] = $registry->toString();
